f60f84dee787f8e6b05c49320838a7ae2f29accb
nicolas-raoul <nicolas.raoul@gmail.com>
2009-10-01 14:10:47 +0900

    I just commented two @override markers that somehow prevented Eclipse from compiling: - The method accept(File) of type DeckPicker.AnkiFilter must override a superclass method - The method compare(HashMap<String,String>, HashMap<String,String>) of type DeckPicker.HashMapCompare must override a superclass method
    I don't understand why, these @override are right in my opinion. Maybe a problem in my environment? I am using compiler compliance level 1.5 on the latest Eclipse, and Java HotSpot 1.6.0_16 on Linux.

--- src/com/ichi2/anki/DeckPicker.java
+++ src/com/ichi2/anki/DeckPicker.java
@@ -121,7 +121,7 @@ public class DeckPicker extends Activity {
     
     public static final class AnkiFilter implements FileFilter {
 
-		@Override
+		//@Override
 		public boolean accept(File pathname) {
 			if (pathname.isFile() && pathname.getName().endsWith(".anki"))
 				return true;
@@ -132,7 +132,7 @@ public class DeckPicker extends Activity {
     
     public static final class HashMapCompare implements Comparator<HashMap<String,String>> {
 
-		@Override
+		//@Override
 		public int compare(HashMap<String, String> object1,
 				HashMap<String, String> object2) {
 			return (int) (Float.parseFloat(object2.get("mod")) - Float.parseFloat(object1.get("mod")));
