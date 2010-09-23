aa6668652066d908414cfe25400699acac9464d7
Kostas Spyropoulos <inigo.aldana@gmail.com>
2010-09-23 14:16:45 -0400

    fix for issue 223 - AnkiDroid always deletes first download when a download finishes

--- src/com/ichi2/anki/services/DownloadManagerService.java
+++ src/com/ichi2/anki/services/DownloadManagerService.java
@@ -909,7 +909,7 @@ public class DownloadManagerService extends Service {
 			}
 			avgBatchTime /= usedForAvg;
 			download.setEstTimeToCompletion(Math.max(0, totalBatches - currentBatch - 1) * avgBatchTime / 1000.0);
-			Log.i(TAG, "TotalBatches: " + totalBatches + " Current: " + currentBatch + " LastBatch: " + lastBatchTime/1000.0 + " RunningAvg: " + avgBatchTime/1000.0 + " Elapsed: " + elapsedTime/1000.0 + " TotalEstimated: " + (elapsedTime + Math.max(0, totalBatches - currentBatch - 1) * avgBatchTime) / 1000.0 + " sec");
+			//Log.i(TAG, "TotalBatches: " + totalBatches + " Current: " + currentBatch + " LastBatch: " + lastBatchTime/1000.0 + " RunningAvg: " + avgBatchTime/1000.0 + " Elapsed: " + elapsedTime/1000.0 + " TotalEstimated: " + (elapsedTime + Math.max(0, totalBatches - currentBatch - 1) * avgBatchTime) / 1000.0 + " sec");
 		}
 		
 		private Payload doInBackgroundLoadDeck(Payload... params)
