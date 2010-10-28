122257c0c267cec742ddd9d463dfc21db7e563b0
Martin Andre <martin.andre@gmail.com>
2010-10-28 18:23:58 +0900

    Added missing license headers

--- src/com/ichi2/anki/Card.java
+++ src/com/ichi2/anki/Card.java
@@ -1,5 +1,5 @@
 /****************************************************************************************
- * Copyright (c) 2009 Daniel Sv�rd <daniel.svard@gmail.com>                             *
+ * Copyright (c) 2009 Daniel Svärd <daniel.svard@gmail.com>                             *
  *                                                                                      *
  * This program is free software; you can redistribute it and/or modify it under        *
  * the terms of the GNU General Public License as published by the Free Software        *
--- src/com/ichi2/anki/CardModel.java
+++ src/com/ichi2/anki/CardModel.java
@@ -1,5 +1,5 @@
 /****************************************************************************************
- * Copyright (c) 2009 Daniel Svärd <daniel.svard@gmail.com>                             *
+ * Copyright (c) 2009 Daniel Sv√§rd <daniel.svard@gmail.com>                             *
  * Copyright (c) 2010 Rick Gruber-Riemer <rick@vanosten.net>                            *
  *                                                                                      *
  * This program is free software; you can redistribute it and/or modify it under        *
