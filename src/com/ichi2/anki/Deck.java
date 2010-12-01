60937183a37f8c7bae52b57c4f5f6bb9150bd8c8
Kostas Spyropoulos <inigo.aldana@gmail.com>
2010-12-01 10:03:18 -0500

    fix typo in new rendering code in deck.java, thanks martin

--- src/com/ichi2/anki/Deck.java
+++ src/com/ichi2/anki/Deck.java
@@ -3251,7 +3251,7 @@ public class Deck {
         String col = row.getString(3);
         int align = row.getInt(4);
         String rtl = row.getString(5);
-        int pre = row.getInt(4);
+        int pre = row.getInt(6);
         if (fam != null) {
             t += "font-family:\"" + fam + "\";";
         }
