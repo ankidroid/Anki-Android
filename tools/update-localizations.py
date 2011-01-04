460c6168d3786b0e3078268a03d3a249e8498648
nobnago <norbertnagold@gmail.com>
2011-01-04 14:48:04 +0100

    hint for download-deck-search localisation

--- tools/update-localizations.py
+++ tools/update-localizations.py
@@ -16,11 +16,10 @@
 #
 # This script extract localization from ankidroid.zip into the right folders.
 # http://crowdin.net/download/project/ankidroid.zip
-# Unfortunately, the arrays.xml files' 0,1,2,3,4,5 values must be fixed manually (Linux command: meld git) --> seems to work now
 
 # Below is the list of official AnkiDroid localizations.
 # Add a language if it is more than 50% translated.
-languages = ['pt-PT', 'fr', 'ru', 'ca', 'es-ES', 'el', 'it', 'pl', 'de', 'ro', 'sv-SE', 'zh-CN', 'zh-TW', 'cs', 'fi'];
+languages = ['pt-PT', 'fr', 'ru', 'ca', 'es-ES', 'el', 'it', 'pl', 'de', 'ro', 'sv-SE', 'zh-CN', 'zh-TW', 'cs', 'fi', 'sr'];
 
 
 
@@ -36,11 +35,8 @@ def replacechars(filename):
 	fin = open(newfilename,"w")
 	for line in s.readlines():
 		if line.startswith("<?xml"):
-			line = "<?xml version=\"1.0\" encoding=\"utf-8\"?> \n <!-- \n ~ Copyright (c) 2009 Andrew <andrewdubya@gmail> \n ~ Copyright (c) 2009 Edu Zamora <edu.zasu@gmail.com> \n ~ Copyright (c) 2009 Daniel Svaerd <daniel.svard@gmail.com> \n ~ Copyright (c) 2009 Nicolas Raoul <nicolas.raoul@gmail.com> \n ~ This program is free software; you can redistribute it and/or modify it under \n ~ the terms of the GNU General Public License as published by the Free Software \n ~ Foundation; either version 3 of the License, or (at your option) any later \n ~ version. \n ~ \n ~ This program is distributed in the hope that it will be useful, but WITHOUT ANY \n ~ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A \n ~ PARTICULAR PURPOSE. See the GNU General Public License for more details. \n ~ \n ~ You should have received a copy of the GNU General Public License along with \n ~ this program.  If not, see <http://www.gnu.org/licenses/>. \n --> \n \n"
+			line = "<?xml version=\"1.0\" encoding=\"utf-8\"?> \n <!-- \n ~ Copyright (c) 2009 Andrew <andrewdubya@gmail> \n ~ Copyright (c) 2009 Edu Zamora <edu.zasu@gmail.com> \n ~ Copyright (c) 2009 Daniel Svaerd <daniel.svard@gmail.com> \n ~ Copyright (c) 2009 Nicolas Raoul <nicolas.raoul@gmail.com> \n ~ Copyright (c) 2010 Norbert Nagold <norbert.nagold@gmail.com> \n ~ This program is free software; you can redistribute it and/or modify it under \n ~ the terms of the GNU General Public License as published by the Free Software \n ~ Foundation; either version 3 of the License, or (at your option) any later \n ~ version. \n ~ \n ~ This program is distributed in the hope that it will be useful, but WITHOUT ANY \n ~ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A \n ~ PARTICULAR PURPOSE. See the GNU General Public License for more details. \n ~ \n ~ You should have received a copy of the GNU General Public License along with \n ~ this program.  If not, see <http://www.gnu.org/licenses/>. \n --> \n \n"
 		else:
-			# crowdin doesn't seem to fill in the right email-adress, thus this workaround			
-			if line.startswith("  <string name=\"error_email\">report@example.org"):
-				line = "  <string name=\"error_email\">ankidroid@gmail.com</string>\n"
 			# some people outwitted crowdin's "0"-bug by filling in "0 ", this changes it back:
 			if line.startswith("    <item>0 </item>"): 
 				line = "    <item>0</item>\n"
