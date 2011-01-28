1c2a8d9e78d6a03081879ca02a02100563e1c631
Nicolas Raoul <nicolas.raoul@gmail.com>
2011-01-28 15:48:04 +0900

    Fixed quotes problem and improved filtering

--- tools/find-broken-strings-variables.sh
+++ tools/find-broken-strings-variables.sh
@@ -3,22 +3,24 @@
 # Hopefully it will prevent this kind of bugs: https://code.google.com/p/ankidroid/issues/detail?id=359
 
 cd ../res
-grep -R "%" values* \
-| sed -e "s/%/\n%/" \
-| grep "%" \
-| grep -v " % " \
-| grep -v "%<" \
-| grep -v "% " \
-| grep -v "%s" \
-| grep -v "%d" \
-| grep -v "%1$s"
+grep -R '%' values* \
+| sed -e 's/%/\n%/g' \
+| grep '%' \
+| grep -v ' % ' \
+| grep -v '%<' \
+| grep -v '% ' \
+| grep -v '%s' \
+| grep -v '%d' \
+| grep -v '%[0-9]$s' \
+| grep -v '%[0-9]$d'
 
-#grep -R "%" values* \
-#| sed -e "s/%/\n%/" \ # Split lines that contain several expressions
-#| grep "%" \ # Filter out lines that do not contain expressions
-#| grep -v " % " \ # Lone % character, not a variable
-#| grep -v "%<" \ # Same, at the end of the string
-#| grep -v "% " \ # Same, at the beginning of the string
-#| grep -v "%s" \ # Single string variable
-#| grep -v "%d" \ # Single decimal variable
-#| grep -v "%1$s" # First variable, string
+#grep -R '%' values* \
+#| sed -e 's/%/\n%/g' \ # Split lines that contain several expressions
+#| grep '%' \ # Filter out lines that do not contain expressions
+#| grep -v ' % ' \ # Lone % character, not a variable
+#| grep -v '%<' \ # Same, at the end of the string
+#| grep -v '% ' \ # Same, at the beginning of the string
+#| grep -v '%s' \ # Single string variable
+#| grep -v '%d' \ # Single decimal variable
+#| grep -v '%[0-9]$s' \ # Multiple string variable
+#| grep -v '%[0-9]$d' # Multiple deceimal variable
