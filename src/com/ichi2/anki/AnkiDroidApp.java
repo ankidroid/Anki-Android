5b569c0e0a8e57d2bc50e943d031ac504a09b945
Nicolas Raoul <nicolas.raoul@gmail.com>
2011-02-01 18:48:48 +0900

    Fixed issue 368: crash on Android 1.5

--- src/com/ichi2/anki/AnkiDroidApp.java
+++ src/com/ichi2/anki/AnkiDroidApp.java
@@ -95,7 +95,9 @@ public class AnkiDroidApp extends Application {
             // "AnkiDroid" folder
             editor.putString("deckPath", getStorageDirectory() + "/AnkiDroid");
 
-            editor.apply();
+            // Using commit instead of apply even though we don't need a return value.
+            // Reason: apply() not available on Android 1.5
+            editor.commit();
         }
 
         // Reschedule the checks - we need to do this if the settings have
