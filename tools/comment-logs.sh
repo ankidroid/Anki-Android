04c6ab972a38d5ca137dccaa3ddf8305a0af8ebe
Nicolas Raoul <nicolas.raoul@gmail.com>
2011-02-03 17:56:24 +0900

    Fixed script path problem

--- tools/comment-logs.sh
+++ tools/comment-logs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-find . -name "*\.java" | xargs grep -l 'Log\.d' | xargs sed -i '' -e 's/Log\.d/\/\/ Log\.d/g'
-find . -name "*\.java" | xargs grep -l 'Log\.v' | xargs sed -i '' -e 's/Log\.v/\/\/ Log\.d/v'
+find ../src -name "*\.java" | xargs grep -l 'Log\.d' | xargs sed -i '' -e 's/Log\.d/\/\/ Log\.d/g'
+find ../src -name "*\.java" | xargs grep -l 'Log\.v' | xargs sed -i '' -e 's/Log\.v/\/\/ Log\.d/v'
 
