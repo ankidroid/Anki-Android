1afdb192fc533cb42be35d71652d6ea07f7a7217
Nicolas Raoul <nicolas.raoul@gmail.com>
2011-02-03 18:15:56 +0900

    Fixed log scripts

--- tools/uncomment-logs.sh
+++ tools/uncomment-logs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find ../src -name "*\.java" | xargs grep -l 'Log\.d' | xargs sed -i '' -e 's/\/\/ Log\.d/Log\.d/g'
-find ../src -name "*\.java" | xargs grep -l 'Log\.v' | xargs sed -i '' -e 's/\/\/ Log\.v/Log\.v/g'
+find ../src -name "*\.java" | xargs grep -l 'Log\.d' | xargs sed -i 's/\/\/ Log\.d/Log\.d/g'
+find ../src -name "*\.java" | xargs grep -l 'Log\.v' | xargs sed -i 's/\/\/ Log\.v/Log\.v/g'
