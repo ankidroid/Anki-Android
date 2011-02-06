d6d399b2c69ce147b898a9a9d6ea1a6a941c7089
nobnago <norbertnagold@gmail.com>
2011-02-06 13:19:53 +0100

    fix for tts

--- src/com/ichi2/anki/Reviewer.java
+++ src/com/ichi2/anki/Reviewer.java
@@ -1370,7 +1370,7 @@ public class Reviewer extends Activity {
         Log.i(AnkiDroidApp.TAG, "base url = " + baseUrl );
         mCard.loadDataWithBaseURL(baseUrl, card, "text/html", "utf-8", null);
       
-        if (!mSpeakText && Integer.valueOf(android.os.Build.VERSION.SDK) > 3) {
+        if (mSpeakText && Integer.valueOf(android.os.Build.VERSION.SDK) > 3) {
             ReadText.setLanguageInformation(Model.getModel(AnkiDroidApp.deck(), mCurrentCard.getCardModelId(), false).getId(), mCurrentCard.getCardModelId());       	
         }
         
