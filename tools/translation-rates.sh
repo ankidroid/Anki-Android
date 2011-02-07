5b64bc5841c1dfeb88cf2d712e5f659de444e821
nobnago <norbertnagold@gmail.com>
2011-02-07 10:19:45 +0100

    forgot translation-rates

--- tools/translation-rates.sh
+++ tools/translation-rates.sh
@@ -2,7 +2,7 @@
 #
 # Shows the completion rate of the translation for each language.
 #
-wget -O tmp-translations-page.html http://crowdin.net/project/ankidroidv0-5
+wget -O tmp-translations-page.html http://crowdin.net/project/ankidroidv0-6
 cat tmp-translations-page.html |
  grep "Completed on" |
  grep -v "Completed on  0%" |
