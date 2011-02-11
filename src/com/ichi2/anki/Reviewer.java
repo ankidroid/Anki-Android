40bd3acf678b80ff9b5b92d8d63ab866d79d25dc
nobnago <norbertnagold@gmail.com>
2011-02-11 20:17:55 +0100

    Reviewer title

--- src/com/ichi2/anki/Reviewer.java
+++ src/com/ichi2/anki/Reviewer.java
@@ -1189,8 +1189,8 @@ public class Reviewer extends Activity {
 
     private void updateScreenCounts() {
         Deck deck = AnkiDroidApp.deck();
-        String unformattedTitle = getResources().getString(R.plurals.studyoptions_window_title);
-        setTitle(String.format(unformattedTitle, deck.getDeckName(), deck.getDueCount(), deck.getCardCount()));
+        int due = deck.getDueCount();
+        setTitle(getResources().getQuantityString(R.plurals.studyoptions_window_title, due, deck.getDeckName(), due, deck.getCardCount()));
 
         int _failedSoonCount = deck.getFailedSoonCount();
         int _revCount = deck.getRevCount();
