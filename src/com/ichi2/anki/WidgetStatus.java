a6fd7ed63ae85f8ceb7f9bb937b9d57564074aa3
Flavio Lerda <flerda@google.com>
2011-04-22 18:37:32 +0100

    Fetch deck status asynchronously.
    The deck status is now stored in the MetaDB and computed using an
    AsyncTask (therefore not in the main thread as it used to be).
    
    This AsyncTask is started when an update is necessary and it triggers
    the AnkiDroidWidget.UpdateService when it is done.
    
    The deck status is stored in the MetaDB.

--- src/com/ichi2/anki/WidgetStatus.java
+++ src/com/ichi2/anki/WidgetStatus.java
@@ -20,6 +20,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.SQLException;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import java.io.File;
@@ -33,82 +34,106 @@ import java.util.Collections;
  * It contains the status of each of the decks.
  */
 public final class WidgetStatus {
-    private static final DeckStatus[] EMPTY_DECK_STATUS = new DeckStatus[0];
-
     /** This class should not be instantiated. */
     private WidgetStatus() {}
 
     /** Request the widget to update its status. */
     public static void update(Context context) {
-        Intent intent = new Intent(context, AnkiDroidWidget.UpdateService.class);
-        intent.setAction(AnkiDroidWidget.UpdateService.ACTION_UPDATE);
-        context.startService(intent);
+        Log.d(AnkiDroidApp.TAG, "WidgetStatus.update()");
+        AsyncTask<Context,Void,Context> updateDeckStatusAsyncTask = new UpdateDeckStatusAsyncTask();
+        updateDeckStatusAsyncTask.execute(context);
     }
 
     /** Returns the status of each of the decks. */
     public static DeckStatus[] fetch(Context context) {
-        Log.i(AnkiDroidApp.TAG, "fetchDeckStatus");
+        return MetaDB.getWidgetStatus(context);
+    }
 
-        SharedPreferences preferences = PrefSettings.getSharedPrefs(context);
-        String deckPath = preferences.getString("deckPath",
-                AnkiDroidApp.getStorageDirectory() + "/AnkiDroid");
+    private static class UpdateDeckStatusAsyncTask extends AsyncTask<Context, Void, Context> {
+        private static final DeckStatus[] EMPTY_DECK_STATUS = new DeckStatus[0];
 
-        File dir = new File(deckPath);
+        private static DeckStatus[] mDecks = EMPTY_DECK_STATUS;
 
-        File[] fileList = dir.listFiles(new AnkiFileFilter());
+        @Override
+        protected Context doInBackground(Context... params) {
+            Log.d(AnkiDroidApp.TAG, "WidgetStatus.UpdateDeckStatusAsyncTask.doInBackground()");
 
-        if (fileList == null || fileList.length == 0) {
-            return EMPTY_DECK_STATUS;
+            Context context = params[0];
+
+            SharedPreferences preferences = PrefSettings.getSharedPrefs(context);
+            String deckPath = preferences.getString("deckPath",
+                    AnkiDroidApp.getStorageDirectory() + "/AnkiDroid");
+
+            File dir = new File(deckPath);
+
+            File[] fileList = dir.listFiles(new AnkiFileFilter());
+
+            if (fileList == null || fileList.length == 0) {
+                mDecks = EMPTY_DECK_STATUS;
+                return context;
+            }
+
+            // For the deck information
+            ArrayList<DeckStatus> decks = new ArrayList<DeckStatus>(fileList.length);
+
+            for (File file : fileList) {
+                try {
+                    // Run through the decks and get the information
+                    String absPath = file.getAbsolutePath();
+                    String deckName = file.getName().replaceAll(".anki", "");
+
+                    Log.i(AnkiDroidApp.TAG, "Found deck: " + absPath);
+
+                    Deck deck = Deck.openDeck(absPath);
+                    int dueCards = deck.getDueCount();
+                    int newCards = deck.getNewCountToday();
+                    int failedCards = deck.getFailedSoonCount();
+                    deck.closeDeck();
+
+                    // Add the information about the deck
+                    decks.add(new DeckStatus(deckName, newCards, dueCards, failedCards));
+                } catch (SQLException e) {
+                    Log.i(AnkiDroidApp.TAG, "Could not open deck");
+                    Log.e(AnkiDroidApp.TAG, e.toString());
+                }
+            }
+
+            if (!decks.isEmpty() && decks.size() > 1) {
+                // Sort and reverse the list if there are decks
+                Log.i(AnkiDroidApp.TAG, "Sorting deck");
+
+                // Ordered by reverse due cards number
+                Collections.sort(decks, new ByDueComparator());
+            }
+
+            mDecks = decks.toArray(EMPTY_DECK_STATUS);
+            return context;
         }
 
-        // For the deck information
-        ArrayList<DeckStatus> decks = new ArrayList<DeckStatus>(fileList.length);
+        @Override
+        protected void onPostExecute(Context context) {
+            Log.d(AnkiDroidApp.TAG, "WidgetStatus.UpdateDeckStatusAsyncTask.onPostExecute()");
+            MetaDB.storeWidgetStatus(context, mDecks);
+            Intent intent = new Intent(context, AnkiDroidWidget.UpdateService.class);
+            intent.setAction(AnkiDroidWidget.UpdateService.ACTION_UPDATE);
+            context.startService(intent);
+        }
 
-        for (File file : fileList) {
-            try {
-                // Run through the decks and get the information
-                String absPath = file.getAbsolutePath();
-                String deckName = file.getName().replaceAll(".anki", "");
-
-                Deck deck = Deck.openDeck(absPath);
-                int dueCards = deck.getDueCount();
-                int newCards = deck.getNewCountToday();
-                int failedCards = deck.getFailedSoonCount();
-                deck.closeDeck();
-
-                // Add the information about the deck
-                decks.add(new DeckStatus(deckName, newCards, dueCards, failedCards));
-            } catch (SQLException e) {
-                Log.i(AnkiDroidApp.TAG, "Could not open deck");
-                Log.e(AnkiDroidApp.TAG, e.toString());
+        /** Comparator that sorts instances of {@link DeckStatus} based on number of due cards. */
+        private static class ByDueComparator implements java.util.Comparator<DeckStatus> {
+            @Override
+            public int compare(DeckStatus deck1, DeckStatus deck2) {
+                // Reverse due cards number order
+                return deck2.mDueCards - deck1.mDueCards;
             }
         }
 
-        if (!decks.isEmpty() && decks.size() > 1) {
-            // Sort and reverse the list if there are decks
-            Log.i(AnkiDroidApp.TAG, "Sorting deck");
-
-            // Ordered by reverse due cards number
-            Collections.sort(decks, new ByDueComparator());
-        }
-
-        return decks.toArray(EMPTY_DECK_STATUS);
-    }
-
-    /** Comparator that sorts instances of {@link DeckStatus} based on number of due cards. */
-    private static class ByDueComparator implements java.util.Comparator<DeckStatus> {
-        @Override
-        public int compare(DeckStatus deck1, DeckStatus deck2) {
-            // Reverse due cards number order
-            return deck2.mDueCards - deck1.mDueCards;
-        }
-    }
-
-    /** Filter for Anki files. */
-    private static final class AnkiFileFilter implements FileFilter {
-        @Override
-        public boolean accept(File pathname) {
-            return pathname.isFile() && pathname.getName().endsWith(".anki");
+        /** Filter for Anki files. */
+        private static final class AnkiFileFilter implements FileFilter {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.isFile() && pathname.getName().endsWith(".anki");
+            }
         }
     }
 }
