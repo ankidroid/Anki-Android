d75d7780c3265bc8b4f9d683291c44906836371a
Nicolas Raoul <nicolas.raoul@gmail.com>
2011-06-06 14:44:56 +0900

    Small tool for quickly getting changes from other people

---
+++ tools/pull-compile-push.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Small utility to quickly merge changes from others:
+# 1) Pull from master of given branch
+# 2) Check whether it compiles or not
+# 3) If it compiles, push to server
+#
+# Usage (from the main AnkiDroid directory):
+#   tools/pull-compile-push.sh flerda
+#
+# It assumes that your local name for flerda's branch is called bflerda,
+# you might have to adapt it to your own conventions.
+#
+# Branch selection
+DEFAULT_BRANCH="nobnago"
+BRANCH=${1:-$DEFAULT_BRANCH}
+
+git pull b$BRANCH master
+if [ "$?" -ne "0" ]; then zenity --error --text 'pull failed'; exit 1; fi 
+
+ant compile
+if [ "$?" -ne "0" ]; then zenity --error --text 'compile failed'; exit 1; fi 
+
+git push
+if [ "$?" -ne "0" ]; then zenity --error --text 'push failed'; exit 1; fi 
