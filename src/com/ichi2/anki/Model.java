f4d1fda92e7afcd3664df700c64d52772fb85a72
Rick Gruber-Riemer <rick@vanosten.net>
2011-06-15 20:47:12 +0200

    Fix to issue 635: alingment of text not equal to CardModel in Anki (in answer).

--- src/com/ichi2/anki/Model.java
+++ src/com/ichi2/anki/Model.java
@@ -389,10 +389,10 @@ public class Model {
         sb.append(calculateDisplay(percentage, myCardModel.getQuestionFontFamily(), myCardModel.getQuestionFontSize(),
                 myCardModel.getQuestionFontColour(), myCardModel.getQuestionAlign(), false, invertedColors));
         sb.append("}\n");
-        // answer
+        // answer (alignment is stored in question as alignment is shared in question and answer)
         sb.append(".").append(Reviewer.ANSWER_CLASS).append(" {\n");
         sb.append(calculateDisplay(percentage, myCardModel.getAnswerFontFamily(), myCardModel.getAnswerFontSize(),
-                myCardModel.getAnswerFontColour(), myCardModel.getAnswerAlign(), false, invertedColors));
+                myCardModel.getAnswerFontColour(), myCardModel.getQuestionAlign(), false, invertedColors));
         sb.append("}\n");
         // css for fields. Gets css for all fields no matter whether they actually are used in a given card model
         FieldModel myFieldModel = null;
