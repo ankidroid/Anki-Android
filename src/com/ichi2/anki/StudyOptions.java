ea25013ac8869079bdd6c8994613a7691ca4830e
nobnago <norbert.nagold@gmail.com>
2011-08-28 02:20:44 +0200

    fix wrong menu entry

--- src/com/ichi2/anki/StudyOptions.java
+++ src/com/ichi2/anki/StudyOptions.java
@@ -1788,7 +1788,7 @@ public class StudyOptions extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         Utils.addMenuItemInActionBar(menu, Menu.NONE, MENU_OPEN, Menu.NONE, R.string.menu_open_deck,
                 R.drawable.ic_menu_manage);
-        Utils.addMenuItem(menu, Menu.NONE, MENU_SYNC, Menu.NONE, R.string.menu_rotate,
+        Utils.addMenuItem(menu, Menu.NONE, MENU_ROTATE, Menu.NONE, R.string.menu_rotate,
                 android.R.drawable.ic_menu_always_landscape_portrait);        
         Utils.addMenuItemInActionBar(menu, Menu.NONE, MENU_SYNC, Menu.NONE, R.string.menu_sync,
                 R.drawable.ic_menu_refresh);        	
