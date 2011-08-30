fefdaecf2a5aeca4b1585cf7f0f7d1ca720b0b9d
nobnago <norbertnagold@gmail.com>
2011-08-30 15:41:52 +0200

    refactored string order

--- tools/update-localizations.py
+++ tools/update-localizations.py
@@ -25,7 +25,7 @@
 languages = ['ar', 'ca', 'cs', 'de', 'el', 'es-ES', 'fi', 'fr', 'hu', 'it', 'ja', 'ko', 'nl', 'pl', 'pt-PT', 'ro', 'ru', 'sr', 'sv-SE', 'vi', 'zh-CN', 'zh-TW'];
 #languages = ['ar', 'ca', 'cs', 'de', 'el', 'es-ES', 'fi', 'fr', 'hu', 'it', 'ja', 'ko', 'nl', 'pl', 'pt-PT', 'ro', 'ru', 'sr', 'sv-SE', 'vi', 'zh-CN', 'zh-TW', 'th', 'sk', 'da', 'ko', 'he', 'uk'];
 
-fileNames = ['01-core', '02-strings', '03-statistics', '04-cardbrowser', '05-dialogs', '06-network', '07-feedback', '08-widget', '09-backup', '10-preferences', '11-arrays'];
+fileNames = ['01-core', '02-strings', '03-dialogs', '04-network', '05-feedback', '06-statistics', '07-cardbrowser', '08-widget', '09-backup', '10-preferences', '11-arrays']
 
 
 import os
