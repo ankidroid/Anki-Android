6f818b6358c9fd9357b997d5a01b90c337298ded
nobnago <norbert.nagold@gmail.com>
2011-10-05 22:21:50 +0200

    fix crash after fact addition in card editor

--- src/com/ichi2/anki/DeckTask.java
+++ src/com/ichi2/anki/DeckTask.java
@@ -224,7 +224,7 @@ public class DeckTask extends AsyncTask<DeckTask.TaskData, DeckTask.TaskData, De
         AnkiDb ankiDB = AnkiDatabaseManager.getDatabase(deck.getDeckPath());
         ankiDB.getDatabase().beginTransaction();
         try {
-        	publishProgress(new TaskData(deck.addFact(editFact, cardModels) != null));
+        	publishProgress(new TaskData(deck.addFact(editFact, cardModels, false) != null));
             ankiDB.getDatabase().setTransactionSuccessful();
         } finally {
             ankiDB.getDatabase().endTransaction();
