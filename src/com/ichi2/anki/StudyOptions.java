bde4f8696e82d6a72c2a4c22038b32ee943b0266
nobnago <norbertnagold@gmail.com>
2011-10-12 10:18:10 +0200

    fix issue 822

--- src/com/ichi2/anki/StudyOptions.java
+++ src/com/ichi2/anki/StudyOptions.java
@@ -1684,7 +1684,7 @@ public class StudyOptions extends Activity implements IButtonListener {
 	        if (allTags == null) {
 	            allTags = deck3.allTags_();
 	            Log.i(AnkiDroidApp.TAG, "all tags: " + Arrays.toString(allTags));
-		        if (allCramTags == null) {
+		        if (allTags == null) {
 		        	Themes.showThemedToast(StudyOptions.this, getResources().getString(R.string.error_insufficient_memory), false);
 		        	ad.setEnabled(false);
 		        	return;
