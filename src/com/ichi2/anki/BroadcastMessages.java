9484f249c4fb67e98e960c11d1725ed97cd5c526
nobnago <norbertnagold@gmail.com>
2011-10-21 13:30:51 +0200

    fix wrong log

--- src/com/ichi2/anki/BroadcastMessages.java
+++ src/com/ichi2/anki/BroadcastMessages.java
@@ -176,12 +176,12 @@ public class BroadcastMessages {
     					// get message version info
     					mMinVersion = getXmlValue(el, MIN_VERSION);
     					if (mMinVersion != null && mMinVersion.length() > 0 && compareVersions(mMinVersion, currentVersion) > 0) {
-        			            Log.d(AnkiDroidApp.TAG, "BroadcastMessage - too low AnkiDroid version (" + currentVersion + "), message " + mNum + " only for > " + mMinVersion);
+        			            Log.d(AnkiDroidApp.TAG, "BroadcastMessage - too low AnkiDroid version (" + currentVersion + "), message " + mNum + " only for >= " + mMinVersion);
         			            continue;
     					}
     					mMaxVersion = getXmlValue(el, MAX_VERSION);
     					if (mMaxVersion != null && mMaxVersion.length() > 0 && compareVersions(mMaxVersion, currentVersion) < 0) {
-        			            Log.d(AnkiDroidApp.TAG, "BroadcastMessage - too high AnkiDroid version (" + currentVersion + "), message " + mNum + " only for > " + mMaxVersion);
+        			            Log.d(AnkiDroidApp.TAG, "BroadcastMessage - too high AnkiDroid version (" + currentVersion + "), message " + mNum + " only for <= " + mMaxVersion);
         			            continue;
     					}
 
