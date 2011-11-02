079b7d886cdb9c96b5b1242c8f02b468a49ec03a
nobnago <norbertnagold@gmail.com>
2011-11-02 09:14:13 +0100

    fix problem on context menu in deckpicker

--- src/com/ichi2/anki/DeckPicker.java
+++ src/com/ichi2/anki/DeckPicker.java
@@ -913,7 +913,7 @@ public class DeckPicker extends Activity implements Runnable, IButtonListener {
 				dialog = null;
 				break;
 			}
-			String[] entries = new String[10];
+			String[] entries = new String[9];
 			entries[CONTEXT_MENU_OPTIMIZE] = res.getString(R.string.contextmenu_deckpicker_optimize_deck);
 			entries[CONTEXT_MENU_CUSTOM_DICTIONARY] = res.getString(R.string.contextmenu_deckpicker_set_custom_dictionary);
 			entries[CONTEXT_MENU_DOWNLOAD_MEDIA] = res.getString(R.string.contextmenu_deckpicker_download_missing_media);
