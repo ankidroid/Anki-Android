f5b6fe680b035b96fa8ef76ac1842a7da118ea1b
nobnago <norbertnagold@gmail.com>
2011-11-15 12:17:03 +0100

    improve localisation skript in order to fix line breaks in tutorials, add norwegian localisation

--- tools/update-localizations.py
+++ tools/update-localizations.py
@@ -23,7 +23,7 @@
 # Do not remove languages.
 # When you add a language, please also add it to mAppLanguages in Preferences.java
 
-languages = ['ar', 'ca', 'cs', 'de', 'el', 'es-ES', 'fi', 'fr', 'hu', 'id', 'it', 'ja', 'ko', 'nl', 'pl', 'pt-PT', 'ro', 'ru', 'sr', 'sv-SE', 'th', 'tr', 'vi', 'zh-CN', 'zh-TW'];
+languages = ['ar', 'ca', 'cs', 'de', 'el', 'es-ES', 'fi', 'fr', 'hu', 'id', 'it', 'ja', 'ko', 'nl', 'no', 'pl', 'pt-PT', 'ro', 'ru', 'sr', 'sv-SE', 'th', 'tr', 'vi', 'zh-CN', 'zh-TW'];
 #languages = ['ar', 'ca', 'cs', 'de', 'el', 'es-ES', 'fi', 'fr', 'hu', 'it', 'ja', 'ko', 'nl', 'pl', 'pt-PT', 'ro', 'ru', 'sr', 'sv-SE', 'vi', 'zh-CN', 'zh-TW', 'th', 'sk', 'da', 'ko', 'he', 'uk'];
 
 fileNames = ['01-core', '02-strings', '03-dialogs', '04-network', '05-feedback', '06-statistics', '07-cardbrowser', '08-widget', '09-backup', '10-preferences', '11-arrays', '12-tutorial', '13-newfeatures', '14-marketdescription', '15-markettitle']
@@ -61,7 +61,7 @@ def replacechars(filename, fileExt, isCrowdin):
 			fin.write(line)
 	else:
 		fin.write("<?xml version=\"1.0\" encoding=\"utf-8\"?> \n <!-- \n ~ Copyright (c) 2011 Norbert Nagold <norbert.nagold@gmail.com> \n ~ This program is free software; you can redistribute it and/or modify it under \n ~ the terms of the GNU General Public License as published by the Free Software \n ~ Foundation; either version 3 of the License, or (at your option) any later \n ~ version. \n ~ \n ~ This program is distributed in the hope that it will be useful, but WITHOUT ANY \n ~ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A \n ~ PARTICULAR PURPOSE. See the GNU General Public License for more details. \n ~ \n ~ You should have received a copy of the GNU General Public License along with \n ~ this program.  If not, see <http://www.gnu.org/licenses/>. \n --> \n \n \n<resources> \n <string-array name=\"tutorial_questions\"> \n")
-		content = s.read().split("\n")
+		content = re.sub('([^\"])\n', "\\1", s.read()).split("\n")
 		length = len(content)
 		line = []
 		for i in range(length - 1):
@@ -69,7 +69,6 @@ def replacechars(filename, fileExt, isCrowdin):
 				start = content[i].rfind('\",\"') + 3
 			else:
 				start=content[i].find('\"') + 1
-
 			contentLine = content[i][start:len(content[i])-1]
 			sepPos = contentLine.find('<separator>')
 			if sepPos == -1:
@@ -94,7 +93,7 @@ def replacechars(filename, fileExt, isCrowdin):
 	fin.close()
 	os.rename(newfilename, filename)
 	if errorOccured:
-		os.remove(filename)
+		#os.remove(filename)
 		print 'error in file ' + filename
 		return False
 	else:
@@ -193,5 +192,6 @@ os.remove(zipname)
 
 if anyError:
 	print "At least one file contained an error\nPlease check!\n"
+	
 
 
