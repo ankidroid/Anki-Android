d452d3011112a9a4a8f3fb8be222ff66dd94b0bf
nobnago <norbert.nagold@gmail.com>
2012-06-29 19:25:51 +0200

    fix reviewer bug

--- src/com/ichi2/anki/Reviewer.java
+++ src/com/ichi2/anki/Reviewer.java
@@ -2257,7 +2257,7 @@ public class Reviewer extends AnkiActivity {
 	            mSimpleCard.setBackgroundColor(mCurrentBackgroundColor);
 	            mSimpleCard.setTextColor(mForegroundColor);
 			}
-			if (mSimpleCard.getVisibility() != View.VISIBLE) {
+			if (mSimpleCard.getVisibility() != View.VISIBLE || (mCard != null && mCard.getVisibility() == View .VISIBLE)) {
 				mSimpleCard.setVisibility(View.VISIBLE);
 				mCard.setVisibility(View.GONE);				
 			}
@@ -2274,7 +2274,7 @@ public class Reviewer extends AnkiActivity {
 	                mCustomFontStyle = getCustomFontsStyle() + getDefaultFontStyle();
 	            }
 			}
-			if (mCard.getVisibility() != View.VISIBLE) {
+			if (mCard.getVisibility() != View.VISIBLE || (mSimpleCard != null && mSimpleCard.getVisibility() == View .VISIBLE)) {
 				mSimpleCard.setVisibility(View.GONE);
 				mCard.setVisibility(View.VISIBLE);				
 			}
