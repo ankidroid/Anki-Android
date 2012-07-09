41d5474139532a76273c99de7525fc56db0f6042
iniju <inigo.aldana@gmail.com>
2012-07-09 08:42:54 +0100

    localizing show hint link

--- src/com/ichi2/libanki/hooks/HintFilter.java
+++ src/com/ichi2/libanki/hooks/HintFilter.java
@@ -16,6 +16,10 @@
 
 package com.ichi2.libanki.hooks;
 
+import android.content.res.Resources;
+
+import com.ichi2.anki2.R;
+
 import java.util.Locale;
 
 public class HintFilter {
@@ -30,13 +34,13 @@ public class HintFilter {
             if (txt.trim().length() == 0) {
                 return "";
             }
-            String tag = (String) args[2];
+            Resources res = (Resources) args[1];
             // random id
             String domid = String.format(Locale.US, "hint%d", txt.hashCode());
             return String.format(Locale.US, "<a href=\"#\" onclick=\"" +
             		"this.style.display='none';document.getElementById('%s').style.display='block';" +
             		"return false;\">%s</a><div id=\"%s\" style=\"display: none\">%s</div>",
-            		domid, String.format(Locale.US, "Show %s", tag), domid, txt);
+            		domid, res.getString(R.string.show_hint), domid, txt);
         }
     }
 }
