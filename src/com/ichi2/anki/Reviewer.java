38937176e3b6ac0c640992d06d6378a457232bf5
nobnago <norbert.nagold@gmail.com>
2012-07-25 19:43:21 +0200

    fix bug 3464008

--- src/com/ichi2/anki/Reviewer.java
+++ src/com/ichi2/anki/Reviewer.java
@@ -1375,7 +1375,7 @@ public class Reviewer extends AnkiActivity {
 
     // These three methods use a deprecated API - they should be updated to possibly use its more modern version.
     private boolean clipboardHasText() {
-        return mClipboard.hasText();
+        return mClipboard != null && mClipboard.hasText();
     }
 
 
