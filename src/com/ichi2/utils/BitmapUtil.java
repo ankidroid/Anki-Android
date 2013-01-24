5b19dd6b723f265ac6dc5be1142e358da0974f11
Bibek Shrestha <bibekshrestha@gmail.com>
2013-01-24 18:33:28 +0100

    preventing null pointer in bitmaputil

--- src/com/ichi2/utils/BitmapUtil.java
+++ src/com/ichi2/utils/BitmapUtil.java
@@ -11,45 +11,58 @@ import android.widget.ImageView;
 
 /**
  * @author zaur
- *
+ * 
  */
 public class BitmapUtil
 {
 
-    public static Bitmap decodeFile(File f, int IMAGE_MAX_SIZE){
-        Bitmap b = null;
-        try {
-            //Decode image size
-            BitmapFactory.Options o = new BitmapFactory.Options();
-            o.inJustDecodeBounds = true;
+	public static Bitmap decodeFile(File f, int IMAGE_MAX_SIZE)
+	{
+		Bitmap b = null;
+		try
+		{
+			// Decode image size
+			BitmapFactory.Options o = new BitmapFactory.Options();
+			o.inJustDecodeBounds = true;
 
-            FileInputStream fis = new FileInputStream(f);
-            BitmapFactory.decodeStream(fis, null, o);
-            fis.close();
+			FileInputStream fis = new FileInputStream(f);
+			BitmapFactory.decodeStream(fis, null, o);
+			fis.close();
 
-            int scale = 1;
-            if (o.outHeight > IMAGE_MAX_SIZE || o.outWidth > IMAGE_MAX_SIZE) {
-                scale = (int)Math.pow(2, (int) Math.round(Math.log(IMAGE_MAX_SIZE / 
-                   (double) Math.max(o.outHeight, o.outWidth)) / Math.log(0.5)));
-            }
+			int scale = 1;
+			if (o.outHeight > IMAGE_MAX_SIZE || o.outWidth > IMAGE_MAX_SIZE)
+			{
+				scale = (int) Math.pow(
+						2,
+						(int) Math.round(Math.log(IMAGE_MAX_SIZE / (double) Math.max(o.outHeight, o.outWidth))
+								/ Math.log(0.5)));
+			}
 
-            //Decode with inSampleSize
-            BitmapFactory.Options o2 = new BitmapFactory.Options();
-            o2.inSampleSize = scale;
-            fis = new FileInputStream(f);
-            b = BitmapFactory.decodeStream(fis, null, o2);
-            fis.close();
-        } catch (IOException e) {
-        }
-        return b;
-    }
+			// Decode with inSampleSize
+			BitmapFactory.Options o2 = new BitmapFactory.Options();
+			o2.inSampleSize = scale;
+			fis = new FileInputStream(f);
+			b = BitmapFactory.decodeStream(fis, null, o2);
+			fis.close();
+		}
+		catch (IOException e)
+		{
+		}
+		return b;
+	}
 
-    public static void freeImageView(ImageView imageView)
-    {
-        BitmapDrawable bd = (BitmapDrawable)imageView.getDrawable();
-        bd.getBitmap().recycle();
-        imageView.setImageBitmap(null);
-    }
-    
-    
+	public static void freeImageView(ImageView imageView)
+	{
+		if (imageView != null)
+		{
+			BitmapDrawable bd = (BitmapDrawable) imageView.getDrawable();
+			if (bd.getBitmap() != null)
+			{
+				bd.getBitmap().recycle();
+				imageView.setImageBitmap(null);
+			}
+		}
+
+	}
+
 }
