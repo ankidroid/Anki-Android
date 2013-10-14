a499f6c14912d0214b934112d53cb33ba37a3de3
Houssam Salem <houssam.salem.au@gmail.com>
2013-10-15 07:21:47 +1100

    fix bug where 2 buttons shown on second learn step (#864) (dae/anki a7b9a7b)

--- src/com/ichi2/libanki/Sched.java
+++ src/com/ichi2/libanki/Sched.java
@@ -267,7 +267,7 @@ public class Sched {
             if (card.getODid() != 0 && card.getQueue() == 2) {
                 return 4;
             }
-            JSONObject conf = _lapseConf(card);
+            JSONObject conf = _lrnConf(card);
             try {
                 if (card.getType() == 0 || conf.getJSONArray("delays").length() > 1) {
                     return 3;
