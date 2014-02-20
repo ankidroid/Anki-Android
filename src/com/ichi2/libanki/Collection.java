24391baffbdc088ee8ea3465b7e3d4e862ef8d4d
Nicolas Raoul <nicolas.raoul@gmail.com>
2014-02-20 15:57:04 +0900

    Revert last commit as it was causing a display bug: https://groups.google.com/forum/#!msg/anki-android/7fjgazYdFbs/wxcsHTvNL9UJ
    This reverts commit d3c534f8a779aa0e1b3cb8b2a78017068b35d071.

--- src/com/ichi2/libanki/Collection.java
+++ src/com/ichi2/libanki/Collection.java
@@ -955,10 +955,7 @@ public class Collection {
                         d.put("q", "Please edit this note and add some cloze deletions.");
                     }
                 }
-                
-                // The line below is not needed. This is different from the original ankilib, because
-                // in Anki you press 'r' to replay sounds, but in AnkiDroid you need a visible button.
-                // fields.put("FrontSide", mMedia.stripAudio(d.get("q")));
+                fields.put("FrontSide", mMedia.stripAudio(d.get("q")));
     
                 // runFilter mungeFields for type "a"
                 fparser = new Models.fieldParser(fields);
