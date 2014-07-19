422a8c67d40f7def3ccb4c9bdc3a469307336ba4
Houssam Salem <houssam.salem.au@gmail.com>
2014-07-20 04:53:55 +1000

    force media resync on deauth deauthing should reset lastUsn to 0
    (https://github.com/dae/anki/commit/a5d91314459d6945568757713ef2431841299f94)
    (https://github.com/dae/anki/commit/f54f5870fa94e05e96bf72210a70934ebd2b734c)

--- src/com/ichi2/libanki/Media.java
+++ src/com/ichi2/libanki/Media.java
@@ -742,6 +742,7 @@ public class Media {
 
     public void forceResync() {
         mDb.execute("delete from media");
+        mDb.execute("update meta set lastUsn=0,dirMod=0");
         mDb.execute("vacuum analyze");
         mDb.commit();
     }
