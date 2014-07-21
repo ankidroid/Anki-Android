dbfd48f95aeeef79d0d283ad0066a4dc62b16493
Nicolas Raoul <nicolas.raoul@gmail.com>
2014-07-21 17:12:11 +0900

    sed: no-run-if-empty

--- tools/uncomment-logs.sh
+++ tools/uncomment-logs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-find src -name "*\.java" | xargs grep -l 'Log.d' | xargs sed -i 's/\/\/ Log\.d/Log\.d/g'
-find src -name "*\.java" | xargs grep -l 'Log.v' | xargs sed -i 's/\/\/ Log\.v/Log\.v/g'
-find src -name "*\.java" | xargs grep -l 'Log.i' | xargs sed -i 's/\/\/ Log\.i/Log\.i/g'
+find src -name "*\.java" | xargs grep -l 'Log.d' | xargs sed --no-run-if-empty -i 's/\/\/ Log\.d/Log\.d/g'
+find src -name "*\.java" | xargs grep -l 'Log.v' | xargs sed --no-run-if-empty -i 's/\/\/ Log\.v/Log\.v/g'
+find src -name "*\.java" | xargs grep -l 'Log.i' | xargs sed --no-run-if-empty -i 's/\/\/ Log\.i/Log\.i/g'
