f27a795cc6d05d754cc5a00f2d7c3c8309542c3e
Houssam Salem <houssam.salem.au@gmail.com>
2014-07-27 20:36:03 +1000

    Assume we are on FAT32 for every sync attempt
    Temporary measure until we can find a reliable way to detect the file system type.

--- src/com/ichi2/libanki/Media.java
+++ src/com/ichi2/libanki/Media.java
@@ -202,9 +202,11 @@ public class Media {
     }
 
 
-    // TODO: This might be needed since some SD cards can be formatted as FAT32
+    // TODO: Assume we are on FAT32 for every sync until we can find a reliable way to detect
+    // the file system type. This will trigger a media scan on every sync attempt even on devices
+    // that didn't need it, so it makes syncing a little slower.
     public boolean _isFAT32() {
-        return false;
+        return true;
     }
 
 
