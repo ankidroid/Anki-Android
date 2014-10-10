7469f9a7950eb17f874bfe29592331b0b0a8b61c
timrae <perceptualchaos2@gmail.com>
2014-10-10 15:16:25 +0900

    Another bug when switching collection path

--- src/com/ichi2/anki/NavigationDrawerActivity.java
+++ src/com/ichi2/anki/NavigationDrawerActivity.java
@@ -305,6 +305,7 @@ public class NavigationDrawerActivity extends AnkiActivity {
                 restartActivity();
             } else {
                 // collection path has changed so kick the user back to the DeckPicker
+                AnkiDroidApp.getCol().close(true);
                 Intent deckPicker = new Intent(this, DeckPicker.class);
                 deckPicker.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivityWithoutAnimation(deckPicker);
