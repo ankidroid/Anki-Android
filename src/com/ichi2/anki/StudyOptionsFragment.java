530b68f0e0f834823f7843c3eb5f71c97590bd11
timrae <perceptualchaos2@gmail.com>
2014-10-24 07:25:32 +0900

    Fix double reloading of counts via onResume of StudyOptionsFragment

--- src/com/ichi2/anki/StudyOptionsFragment.java
+++ src/com/ichi2/anki/StudyOptionsFragment.java
@@ -365,7 +365,8 @@ public class StudyOptionsFragment extends Fragment implements LoaderManager.Load
     @Override
     public void onResume() {
         super.onResume();
-        if (colOpen()) {
+        if (colOpen() && !mFragmented) {
+            // If not in tablet mode then reload deck counts (reload is taken care of by DeckPicker when mFragmented)
             if (Utils.now() > getCol().getSched().getDayCutoff()) {
                 updateValuesFromDeck(true);
             } else {
