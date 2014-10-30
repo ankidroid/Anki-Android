35ecf9945ad3d6842961415fa42ac6847cac9e13
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2014-10-30 14:32:03 -0500

    Add detection method for Chromebooks.

--- src/com/ichi2/anki/AnkiDroidApp.java
+++ src/com/ichi2/anki/AnkiDroidApp.java
@@ -194,6 +194,11 @@ public class AnkiDroidApp extends Application {
     }
 
 
+    public static boolean isChromebook() {
+        return android.os.Build.BRAND.equalsIgnoreCase("chromium") || android.os.Build.MANUFACTURER.equalsIgnoreCase("chromium");
+    }
+
+
     /**
      * Convenience method for accessing Shared preferences
      * 
