ankidroid/Anki-Android

commit 65deaef2ce4afe8246ffe43692a218c9e805ef29

Make custom study options more consistent with AnkiDesktop

AnkiDroid/src/main/java/com/ichi2/anki/StudyOptionsFragment.java
@@ -476,7 +476,11 @@ public class StudyOptionsFragment extends Fragment implements LoaderManager.Load
                     dyn = col.getDecks().get(did);
                 }
                 // and then set various options
-                dyn.put("delays", delays);
+                if (delays.length() > 0) {
+                    dyn.put("delays", delays);
+                } else {
+                    dyn.remove("delays");
+                }
                 JSONArray ar = dyn.getJSONArray("terms");
                 ar.getJSONArray(0).put(0,
                         new StringBuilder("deck:\"").append(deckName).append("\" ").append(terms[0]).toString());

AnkiDroid/src/main/java/com/ichi2/anki/dialogs/CustomStudyDialog.java
@@ -198,9 +198,9 @@ public class CustomStudyDialog extends DialogFragment {
             case CUSTOM_STUDY_NEW:
                 return Integer.toString(prefs.getInt("extendNew", 10));
             case CUSTOM_STUDY_REV:
-                return Integer.toString(prefs.getInt("extendRev", 10));
+                return Integer.toString(prefs.getInt("extendRev", 50));
             case CUSTOM_STUDY_FORGOT:
-                return Integer.toString(prefs.getInt("forgottenDays", 2));
+                return Integer.toString(prefs.getInt("forgottenDays", 1));
             case CUSTOM_STUDY_AHEAD:
                 return Integer.toString(prefs.getInt("aheadDays", 1));
             case CUSTOM_STUDY_RANDOM:

commit bbb52770dcf2e18afa4d9e378d37f52daae08b04

Fix a bug introduced in 65deaef

AnkiDroid/src/main/java/com/ichi2/anki/StudyOptionsFragment.java
@@ -479,7 +479,7 @@ public class StudyOptionsFragment extends Fragment implements LoaderManager.Load
                 if (delays.length() > 0) {
                     dyn.put("delays", delays);
                 } else {
-                    dyn.remove("delays");
+                    dyn.put("delays", JSONObject.NULL);
                 }
                 JSONArray ar = dyn.getJSONArray("terms");
                 ar.getJSONArray(0).put(0,