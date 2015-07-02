a63f7bebcf84aa5bac1bd4c15d58dec3a6fd95cb
timrae <perceptualchaos2@gmail.com>
2015-07-03 00:18:23 +0900

    Fix some bugs in the preferences

--- AnkiDroid/src/main/java/com/ichi2/anki/NavigationDrawerActivity.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/NavigationDrawerActivity.java
@@ -26,7 +26,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
