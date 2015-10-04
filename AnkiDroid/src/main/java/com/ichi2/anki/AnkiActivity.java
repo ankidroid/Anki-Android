d3b3b534d6cc7e9f0b29e586a94d90b6e542ded3
timrae <perceptualchaos2@gmail.com>
2015-10-05 00:14:46 +0900

    Fix #3697 infinite recursion in ShowSnackbar method

--- AnkiDroid/src/main/java/com/ichi2/anki/AnkiActivity.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/AnkiActivity.java
@@ -373,7 +373,7 @@ public class AnkiActivity extends AppCompatActivity implements LoaderManager.Loa
      */
     protected void showSnackbar(int mainTextResource, boolean shortLength, int actionTextResource,
                                 View.OnClickListener listener, View root) {
-        showSnackbar(mainTextResource,shortLength,actionTextResource,listener,root);
+        showSnackbar(mainTextResource,shortLength,actionTextResource,listener,root, null);
     }
     protected void showSnackbar(int mainTextResource, boolean shortLength, int actionTextResource,
                                 View.OnClickListener listener, View root, Snackbar.Callback callback) {
