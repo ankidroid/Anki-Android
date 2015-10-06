01195624abc62f26f11e92a722102374701417d1
timrae <perceptualchaos2@gmail.com>
2015-10-06 16:54:35 +0900

    Hide action bar together with system status bar in fullscreen mode

--- AnkiDroid/src/main/java/com/ichi2/compat/CompatV11.java
+++ AnkiDroid/src/main/java/com/ichi2/compat/CompatV11.java
@@ -5,9 +5,12 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.ActionBar;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.ichi2.anki.AnkiActivity;
+import com.ichi2.anki.NavigationDrawerActivity;
 
 import java.text.Normalizer;
 
@@ -28,6 +31,35 @@ public class CompatV11 extends CompatV10 implements Compat {
         stackBuilder.addNextIntentWithParentStack(intent);
         stackBuilder.startActivities(new Bundle());
         activity.finishWithoutAnimation();
+    }
+
+
+    @Override
+    public void setFullScreen(NavigationDrawerActivity activity) {
+        super.setFullScreen(activity);
+        registerHideActionBarListener(activity);
+    }
+
+    protected void registerHideActionBarListener(final NavigationDrawerActivity activity) {
+        View decorView = activity.getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener
+                (new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        // Note that system bars will only be "visible" if none of the
+                        // LOW_PROFILE, HIDE_NAVIGATION, or FULLSCREEN flags are set.
+                        ActionBar ab = activity.getSupportActionBar();
+                        if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                            if (ab != null) {
+                                ab.show();
+                            }
+                        } else {
+                            if (ab != null) {
+                                ab.hide();
+                            }
+                        }
+                    }
+                });
     }
 
 }
