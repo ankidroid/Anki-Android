b83da2b659b7d0328b4cb7f52be95eac230ac9b3
timrae <perceptualchaos2@gmail.com>
2015-10-31 00:22:51 +0900

    Fix export dialog Fixes #3540

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/ExportDialog.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/ExportDialog.java
@@ -3,8 +3,10 @@ package com.ichi2.anki.dialogs;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 
+import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.ichi2.anki.R;
 
@@ -12,15 +14,13 @@ public class ExportDialog extends DialogFragment {
 
     public interface ExportDialogListener {
 
-        public void exportApkg(String path, Long did, boolean includeSched, boolean includeMedia);
-
-
-        public void dismissAllDialogFragments();
+        void exportApkg(String path, Long did, boolean includeSched, boolean includeMedia);
+        void dismissAllDialogFragments();
     }
 
     private final int INCLUDE_SCHED = 0;
     private final int INCLUDE_MEDIA = 1;
-    private boolean mIncludeSched = true;
+    private boolean mIncludeSched = false;
     private boolean mIncludeMedia = false;
 
 
@@ -30,7 +30,7 @@ public class ExportDialog extends DialogFragment {
      * @param did An integer which specifies which of the sub-dialogs to show
      * @param dialogMessage An optional string which can be used to show a custom message or specify import path
      */
-    public static ExportDialog newInstance(String dialogMessage, Long did) {
+    public static ExportDialog newInstance(@NonNull String dialogMessage, Long did) {
         ExportDialog f = new ExportDialog();
         Bundle args = new Bundle();
         args.putLong("did", did);
@@ -40,7 +40,7 @@ public class ExportDialog extends DialogFragment {
     }
 
 
-    public static ExportDialog newInstance(String dialogMessage) {
+    public static ExportDialog newInstance(@NonNull String dialogMessage) {
         ExportDialog f = new ExportDialog();
         Bundle args = new Bundle();
         args.putString("dialogMessage", dialogMessage);
@@ -54,47 +54,56 @@ public class ExportDialog extends DialogFragment {
         super.onCreate(savedInstanceState);
         Resources res = getResources();
         final Long did = getArguments().getLong("did", -1L);
-        mIncludeSched = did == -1L;
+        Integer[] checked;
+        if (did != -1L) {
+            mIncludeSched = false;
+            checked = new Integer[]{};
+        } else {
+            mIncludeSched = true;
+            checked = new Integer[]{ INCLUDE_SCHED };
+        }
         final String[] items = { res.getString(R.string.export_include_schedule),
                 res.getString(R.string.export_include_media) };
-        final boolean[] checked = { mIncludeSched, mIncludeMedia };
+
         MaterialDialog.Builder builder = new MaterialDialog.Builder(getActivity())
-                .title(res.getString(R.string.export))
+                .title(R.string.export)
                 .content(getArguments().getString("dialogMessage"))
-                .positiveText(res.getString(android.R.string.ok))
-                .negativeText(res.getString(R.string.cancel))
+                .positiveText(android.R.string.ok)
+                .negativeText(android.R.string.cancel)
                 .cancelable(true)
                 .items(items)
-                .itemsCallbackMultiChoice(new Integer[]{INCLUDE_SCHED},
+                .alwaysCallMultiChoiceCallback()
+                .itemsCallbackMultiChoice(checked,
                         new MaterialDialog.ListCallbackMultiChoice() {
                             @Override
                             public boolean onSelection(MaterialDialog materialDialog,
-                                    Integer[] integers,
-                                    CharSequence[] charSequences) {
+                                                       Integer[] integers, CharSequence[] charSequences) {
+                                mIncludeMedia = false;
+                                mIncludeSched = false;
                                 for (int i = 0; i < integers.length; i++) {
                                     switch (integers[i]) {
                                         case INCLUDE_SCHED:
-                                            mIncludeSched = !mIncludeSched;
+                                            mIncludeSched = true;
                                             break;
                                         case INCLUDE_MEDIA:
-                                            mIncludeMedia = !mIncludeMedia;
+                                            mIncludeMedia = true;
                                             break;
                                     }
                                 }
                                 return true;
                             }
                         })
-                .callback(new MaterialDialog.ButtonCallback() {
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
                     @Override
-                    public void onPositive(MaterialDialog dialog) {
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
                         ((ExportDialogListener) getActivity())
-                                .exportApkg(null, did != -1L ? did : null, mIncludeSched,
-                                        mIncludeMedia);
+                                .exportApkg(null, did != -1L ? did : null, mIncludeSched, mIncludeMedia);
                         dismissAllDialogFragments();
                     }
-
+                })
+                .onNegative(new MaterialDialog.SingleButtonCallback() {
                     @Override
-                    public void onNegative(MaterialDialog dialog) {
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
                         dismissAllDialogFragments();
                     }
                 });
