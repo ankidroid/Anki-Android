9f6a3b1b764e8c780a9b1a3544269ad5f2b0c03e
Timothy Rae <perceptualchaos2@gmail.com>
2015-11-20 20:29:11 +0900

    Add maximum value check to NoteEditorReschedule dialog

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/NoteEditorRescheduleCard.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/NoteEditorRescheduleCard.java
@@ -1,7 +1,6 @@
 
 package com.ichi2.anki.dialogs;
 
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.text.InputType;
@@ -24,7 +23,7 @@ public class NoteEditorRescheduleCard extends DialogFragment {
                 .positiveText(getResources().getString(R.string.dialog_ok))
                 .negativeText(R.string.cancel)
                 .inputType(InputType.TYPE_CLASS_NUMBER)
-                .inputRange(1, -1)
+                .inputRange(1, 9999)
                 .input(R.string.reschedule_card_dialog_message, R.string.empty_string, new MaterialDialog.InputCallback() {
                     @Override
                     public void onInput(MaterialDialog dialog, CharSequence text) {
