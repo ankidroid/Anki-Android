6f8fe2fd22dc4dffeabdb577365931dbdcfc6d14
Timothy Rae <perceptualchaos2@gmail.com>
2015-12-03 20:10:03 +0900

    Use down animation when exiting deck picker

--- AnkiDroid/src/main/java/com/ichi2/anki/DeckPicker.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/DeckPicker.java
@@ -741,7 +741,7 @@ public class DeckPicker extends NavigationDrawerActivity implements
 
 
     private void finishWithAnimation() {
-        super.finishWithAnimation(ActivityTransitionAnimation.DIALOG_EXIT);
+        super.finishWithAnimation(ActivityTransitionAnimation.DOWN);
     }
 
 
