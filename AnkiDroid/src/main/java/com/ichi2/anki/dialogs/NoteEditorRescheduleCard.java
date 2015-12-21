3fc2d0c8b61a4d5be5baaab7d9e86ba83a16fa98
Houssam Salem <houssam.salem.au@gmail.com>
2015-12-21 17:55:49 +1100

    Fix max characters on reschedule note dialog

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/NoteEditorRescheduleCard.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/NoteEditorRescheduleCard.java
@@ -23,7 +23,7 @@ public class NoteEditorRescheduleCard extends DialogFragment {
                 .positiveText(getResources().getString(R.string.dialog_ok))
                 .negativeText(R.string.cancel)
                 .inputType(InputType.TYPE_CLASS_NUMBER)
-                .inputRange(1, 9999)
+                .inputRange(1, 4) // max 4 characters (i.e., 9999)
                 .input(R.string.reschedule_card_dialog_message, R.string.empty_string, new MaterialDialog.InputCallback() {
                     @Override
                     public void onInput(MaterialDialog dialog, CharSequence text) {
