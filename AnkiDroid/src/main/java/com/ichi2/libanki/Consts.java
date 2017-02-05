6972cda9463223f9d94f185ff0f5fbfb920c41a2
Timothy Rae <perceptualchaos2@gmail.com>
2017-02-05 20:43:17 +0900

    use sync.ankiweb.net for syncing media sync goes through the same domain now, avoiding a slow extra SSL handshake dae/anki@04bc1a7

--- AnkiDroid/src/main/java/com/ichi2/libanki/Consts.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/Consts.java
@@ -61,8 +61,8 @@ public class Consts {
     public static final int SCHEMA_VERSION = 11;
     public static final int SYNC_ZIP_SIZE = (int)(2.5*1024*1024);
     public static final int SYNC_ZIP_COUNT = 25;
-    public static final String SYNC_BASE = "https://ankiweb.net/";
-    public static final String SYNC_MEDIA_BASE = "https://msync.ankiweb.net/";
+    public static final String SYNC_BASE = "https://sync.ankiweb.net/";
+    public static final String SYNC_MEDIA_BASE = "https://sync.ankiweb.net/msync/";
     public static final int SYNC_VER = 8;
 
     public static final String HELP_SITE = "http://ankisrs.net/docs/manual.html";
