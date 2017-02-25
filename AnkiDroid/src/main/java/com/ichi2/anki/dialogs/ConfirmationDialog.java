5c8a30999eba23661d3e3a64072c64438ebf91a8
Timothy Rae <perceptualchaos2@gmail.com>
2017-02-25 10:59:28 +0900

    Fix IllegalStateException crashes with ConfirmationDialog Android can't instantiate fragments that are anonymous classes. It was somehow working fine until they updated the support library. Here we use Runnables instead. Once we move to Java8 we can make the code much less verbose by using lambdas.

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/ConfirmationDialog.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/ConfirmationDialog.java
@@ -8,59 +8,63 @@ import android.support.v4.app.DialogFragment;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.ichi2.anki.R;
 
-/**
- * This is a reusable convenience class which makes it easy to show a confirmation dialog as a DialogFragment.
- * To use, create a new instance which overrides the confirm() method, call setArgs(...), and then show
- * the DialogFragment via the fragment manager as usual.
- */
-public abstract class ConfirmationDialog extends DialogFragment {
-
-    public void setArgs(String message) {
-        setArgs("" , message);
-    }
-
-    public void setArgs(String title, String message) {
-        Bundle args = new Bundle();
-        args.putString("message", message);
-        args.putString("title", title);
-        setArguments(args);
-    }
-
-
-    @Override
-    public MaterialDialog onCreateDialog(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Resources res = getActivity().getResources();
-        String title = getArguments().getString("title");
-        return new MaterialDialog.Builder(getActivity())
-                .title(title.equals("") ? res.getString(R.string.app_name) : title)
-                .content(getArguments().getString("message"))
-                .positiveText(res.getString(R.string.dialog_ok))
-                .negativeText(res.getString(R.string.dialog_cancel))
-                .callback(new MaterialDialog.ButtonCallback() {
-                    @Override
-                    public void onPositive(MaterialDialog dialog) {
-                        confirm();
-                    }
-
-                    @Override
-                    public void onNegative(MaterialDialog dialog) {
-                        cancel();
-                    }
-                })
-                .show();
-    }
-
-
     /**
-     * Must override this method to handle confirmation event
+     * This is a reusable convenience class which makes it easy to show a confirmation dialog as a DialogFragment.
+     * Create a new instance, call setArgs(...), setConfirm(), and setCancel() then show it via the fragment manager as usual.
      */
-    public abstract void confirm();
+    public class ConfirmationDialog extends DialogFragment {
+        // TODO: Replace these with lambdas
+        private Runnable mConfirm = new Runnable() {
+            @Override
+            public void run() { // Do nothing by default
+            }
+        };
+        private Runnable mCancel = new Runnable() {
+            @Override
+            public void run() { // Do nothing by default
+            }
+        };
 
+        public void setArgs(String message) {
+            setArgs("" , message);
+        }
 
-    /**
-     * Optionally override this method to do something special when operation cancelled
-     */
-    public void cancel() {
+        public void setArgs(String title, String message) {
+            Bundle args = new Bundle();
+            args.putString("message", message);
+            args.putString("title", title);
+            setArguments(args);
+        }
+
+        public void setConfirm(Runnable confirm) {
+            mConfirm = confirm;
+        }
+
+        public void setCancel(Runnable cancel) {
+            mCancel = cancel;
+        }
+
+        @Override
+        public MaterialDialog onCreateDialog(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            Resources res = getActivity().getResources();
+            String title = getArguments().getString("title");
+            return new MaterialDialog.Builder(getActivity())
+                    .title(title.equals("") ? res.getString(R.string.app_name) : title)
+                    .content(getArguments().getString("message"))
+                    .positiveText(res.getString(R.string.dialog_ok))
+                    .negativeText(res.getString(R.string.dialog_cancel))
+                    .callback(new MaterialDialog.ButtonCallback() {
+                        @Override
+                        public void onPositive(MaterialDialog dialog) {
+                            mConfirm.run();
+                        }
+
+                        @Override
+                        public void onNegative(MaterialDialog dialog) {
+                            mCancel.run();
+                        }
+                    })
+                    .show();
+        }
     }
-}
