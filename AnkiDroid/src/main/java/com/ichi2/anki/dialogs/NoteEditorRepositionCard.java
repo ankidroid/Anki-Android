fc6ec794a892bea931e9d1198c5d02fced5c0253
Jordan Cleeve <darkechosx@outlook.com>
2017-12-28 15:07:51 +1300

    Added card repositioning
    Can reposition the card from the note edit context menu

---
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/NoteEditorRepositionCard.java
@@ -0,0 +1,37 @@
+
+package com.ichi2.anki.dialogs;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.InputType;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.ichi2.anki.NoteEditor;
+import com.ichi2.anki.R;
+
+public class NoteEditorRepositionCard extends DialogFragment {
+    public static NoteEditorRepositionCard newInstance() {
+        return new NoteEditorRepositionCard();
+    }
+
+
+    @Override
+    public MaterialDialog onCreateDialog(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        return new MaterialDialog.Builder(getActivity())
+                .title(R.string.reposition_card_dialog_title)
+                .positiveText(getResources().getString(R.string.dialog_ok))
+                .negativeText(R.string.cancel)
+                .inputType(InputType.TYPE_CLASS_NUMBER)
+                .inputRange(1, 5) // max 5 characters (i.e., 99999)
+                .input(R.string.reposition_card_dialog_message, R.string.empty_string, new MaterialDialog.InputCallback() {
+                    @Override
+                    public void onInput(MaterialDialog dialog, CharSequence text) {
+                        int position = Integer.parseInt(text.toString());
+                        ((NoteEditor) getActivity()).onRepositionCard(position);
+
+                    }
+                })
+                .show();
+    }
+    }
