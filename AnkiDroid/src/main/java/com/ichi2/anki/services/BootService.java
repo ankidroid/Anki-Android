2c8adfab51c4fe6d04aff1b175e08bbdff5cb7d1
Mike Hardy <github@mikehardy.net>
2018-02-22 21:02:52 -0500

    Change notifications to setRepeating, may fix issue #4525

--- AnkiDroid/src/main/java/com/ichi2/anki/services/BootService.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/services/BootService.java
@@ -72,7 +72,7 @@ public class BootService extends IntentService {
                         calendar.set(Calendar.MINUTE, reminder.getJSONArray("time").getInt(1));
                         calendar.set(Calendar.SECOND, 0);
 
-                        alarmManager.setInexactRepeating(
+                        alarmManager.setRepeating(
                                 AlarmManager.RTC_WAKEUP,
                                 calendar.getTimeInMillis(),
                                 AlarmManager.INTERVAL_DAY,
@@ -99,7 +99,7 @@ public class BootService extends IntentService {
         calendar.set(Calendar.SECOND, 0);
         final PendingIntent notificationIntent =
                 PendingIntent.getBroadcast(context, 0, new Intent(context, NotificationReceiver.class), 0);
-        alarmManager.setInexactRepeating(
+        alarmManager.setRepeating(
                 AlarmManager.RTC_WAKEUP,
                 calendar.getTimeInMillis(),
                 AlarmManager.INTERVAL_DAY,
