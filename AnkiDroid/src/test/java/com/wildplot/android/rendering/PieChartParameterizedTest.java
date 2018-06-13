cdaf424dcc3dd878cb117382412a4395b06cfae5
Mike Hardy <github@mikehardy.net>
2018-06-13 16:33:07 -0500

    Upgrade test dependencies and migrate tests to new APIs
        - Junit 4 -> Junit 5 using Vintage Engine
        - Mockito -> 1.10.x -> 2.18.x
        - Powermock 1.6.x -> Powermock 2.0.x

--- AnkiDroid/src/test/java/com/wildplot/android/rendering/PieChartParameterizedTest.java
+++ AnkiDroid/src/test/java/com/wildplot/android/rendering/PieChartParameterizedTest.java
@@ -24,19 +24,19 @@ import java.util.Collection;
 
 import static com.wildplot.android.rendering.PieChartTest.createRectangleMock;
 import static java.util.Arrays.asList;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyFloat;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.floatThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyFloat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.floatThat;
 import static org.mockito.Mockito.inOrder;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(PowerMockRunner.class)
-@PowerMockRunnerDelegate(value = Parameterized.class)
-@PrepareForTest({RectangleWrap.class, GraphicsWrap.class, ColorWrap.class,
-        android.graphics.Color.class})
+@PowerMockRunnerDelegate(Parameterized.class)
+@PrepareForTest({RectangleWrap.class, GraphicsWrap.class, ColorWrap.class, PlotSheet.class,
+        Color.class})
 @SuppressWarnings("WeakerAccess")
 public class PieChartParameterizedTest {
     private static final double PRECISION = 2 * 1E-3F;
--- AnkiDroid/src/test/java/com/wildplot/android/rendering/PieChartTest.java
+++ AnkiDroid/src/test/java/com/wildplot/android/rendering/PieChartTest.java
@@ -14,10 +14,10 @@ import org.mockito.Mock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyFloat;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.floatThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyFloat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.floatThat;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.mock;
@@ -25,8 +25,8 @@ import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({RectangleWrap.class, GraphicsWrap.class, ColorWrap.class,
-        android.graphics.Color.class})
+@PrepareForTest({RectangleWrap.class, GraphicsWrap.class, ColorWrap.class, PlotSheet.class,
+        Color.class})
 public class PieChartTest {
     private static final double PRECISION = 1E-3F;
     @Mock
