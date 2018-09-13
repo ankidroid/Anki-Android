ce0e4f98b3f13c4bcfc034c5ec62ef57bfbfaef8
Mike Hardy <mike@mikehardy.net>
2018-09-13 00:45:53 -0500

    Fix all test deprecation / AndroidStudio lint

--- AnkiDroid/src/androidTest/java/com.ichi2.anki.tests/libanki/ImportTest.java
+++ AnkiDroid/src/androidTest/java/com.ichi2.anki.tests/libanki/ImportTest.java
@@ -20,21 +20,16 @@ import android.Manifest;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.GrantPermissionRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.Suppress;
 
-import com.ichi2.anki.exception.ConfirmModSchemaException;
 import com.ichi2.anki.tests.Shared;
 import com.ichi2.libanki.Collection;
-import com.ichi2.libanki.Models;
 import com.ichi2.libanki.Note;
 import com.ichi2.libanki.Utils;
 import com.ichi2.libanki.importer.Anki2Importer;
 import com.ichi2.libanki.importer.AnkiPackageImporter;
 import com.ichi2.libanki.importer.Importer;
-import com.ichi2.libanki.importer.TextImporter;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -43,6 +38,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -90,7 +86,7 @@ public class ImportTest {
         Collection empty = Shared.getEmptyCol(InstrumentationRegistry.getTargetContext());
         Importer imp = new Anki2Importer(empty, tmp.getPath());
         imp.run();
-        expected = Arrays.asList("foo.mp3");
+        expected = Collections.singletonList("foo.mp3");
         actual = Arrays.asList(new File(empty.getMedia().dir()).list());
         actual.retainAll(expected);
         assertEquals(expected.size(), actual.size());
@@ -98,7 +94,7 @@ public class ImportTest {
         empty.remCards(Utils.arrayList2array(empty.getDb().queryColumn(Long.class, "select id from cards", 0)));
         imp = new Anki2Importer(empty, tmp.getPath());
         imp.run();
-        expected = Arrays.asList("foo.mp3");
+        expected = Collections.singletonList("foo.mp3");
         actual = Arrays.asList(new File(empty.getMedia().dir()).list());
         actual.retainAll(expected);
         assertEquals(expected.size(), actual.size());
@@ -140,12 +136,12 @@ public class ImportTest {
         Collection tmp = Shared.getEmptyCol(InstrumentationRegistry.getTargetContext());
         String apkg = Shared.getTestFilePath(InstrumentationRegistry.getTargetContext(), "media.apkg");
         Importer imp = new AnkiPackageImporter(tmp, apkg);
-        expected = Arrays.asList();
+        expected = Collections.emptyList();
         actual = Arrays.asList(new File(tmp.getMedia().dir()).list());
         actual.retainAll(expected);
         assertEquals(actual.size(), expected.size());
         imp.run();
-        expected = Arrays.asList("foo.wav");
+        expected = Collections.singletonList("foo.wav");
         actual = Arrays.asList(new File(tmp.getMedia().dir()).list());
         actual.retainAll(expected);
         assertEquals(expected.size(), actual.size());
@@ -153,7 +149,7 @@ public class ImportTest {
         tmp.remCards(Utils.arrayList2array(tmp.getDb().queryColumn(Long.class, "select id from cards", 0)));
         imp = new AnkiPackageImporter(tmp, apkg);
         imp.run();
-        expected = Arrays.asList("foo.wav");
+        expected = Collections.singletonList("foo.wav");
         actual = Arrays.asList(new File(tmp.getMedia().dir()).list());
         actual.retainAll(expected);
         assertEquals(actual.size(), expected.size());
@@ -254,67 +250,67 @@ public class ImportTest {
     }
 
     // Exchange @Suppress for @Test when csv importer is implemented
-    @Suppress
-    public void testCsv() throws IOException {
-        Collection deck = Shared.getEmptyCol(InstrumentationRegistry.getTargetContext());
-        String file = Shared.getTestFilePath(InstrumentationRegistry.getTargetContext(), "text-2fields.txt");
-        TextImporter i = new TextImporter(deck, file);
-        i.initMapping();
-        i.run();
-        // four problems - too many & too few fields, a missing front, and a
-        // duplicate entry
-        assertTrue(i.getLog().size() == 5);
-        assertTrue(i.getTotal() == 5);
-        // if we run the import again, it should update instead
-        i.run();
-        assertTrue(i.getLog().size() == 10);
-        assertTrue(i.getTotal() == 5);
-        // but importing should not clobber tags if they're unmapped
-        Note n = deck.getNote(deck.getDb().queryLongScalar("select id from notes"));
-        n.addTag("test");
-        n.flush();
-        i.run();
-        n.load();
-        assertTrue((n.getTags().size() == 1) && (n.getTags().get(0) == "test"));
-        // if add-only mode, count will be 0
-        i.setImportMode(1);
-        i.run();
-        assertTrue(i.getTotal() == 0);
-        // and if dupes mode, will reimport everything
-        assertTrue(deck.cardCount() == 5);
-        i.setImportMode(2);
-        i.run();
-        // includes repeated field
-        assertTrue(i.getTotal() == 6);
-        assertTrue(deck.cardCount() == 11);
-        deck.close();
-    }
-
-    // Exchange @Suppress for @Test when csv importer is implemented
-    @Suppress
-    public void testCsv2() throws  IOException, ConfirmModSchemaException {
-        Collection deck = Shared.getEmptyCol(InstrumentationRegistry.getTargetContext());
-        Models mm = deck.getModels();
-        JSONObject m = mm.current();
-        JSONObject f = mm.newField("Three");
-        mm.addField(m, f);
-        mm.save(m);
-        Note n = deck.newNote();
-        n.setItem("Front", "1");
-        n.setItem("Back", "2");
-        n.setItem("Three", "3");
-        deck.addNote(n);
-        // an update with unmapped fields should not clobber those fields
-        String file = Shared.getTestFilePath(InstrumentationRegistry.getTargetContext(), "text-update.txt");
-        TextImporter i = new TextImporter(deck, file);
-        i.initMapping();
-        i.run();
-        n.load();
-        assertTrue(n.getItem("Front").equals("1"));
-        assertTrue(n.getItem("Back").equals("x"));
-        assertTrue(n.getItem("Three").equals("3"));
-        deck.close();
-    }
+//    @Suppress
+//    public void testCsv() throws IOException {
+//        Collection deck = Shared.getEmptyCol(InstrumentationRegistry.getTargetContext());
+//        String file = Shared.getTestFilePath(InstrumentationRegistry.getTargetContext(), "text-2fields.txt");
+//        TextImporter i = new TextImporter(deck, file);
+//        i.initMapping();
+//        i.run();
+//        // four problems - too many & too few fields, a missing front, and a
+//        // duplicate entry
+//        assertTrue(i.getLog().size() == 5);
+//        assertTrue(i.getTotal() == 5);
+//        // if we run the import again, it should update instead
+//        i.run();
+//        assertTrue(i.getLog().size() == 10);
+//        assertTrue(i.getTotal() == 5);
+//        // but importing should not clobber tags if they're unmapped
+//        Note n = deck.getNote(deck.getDb().queryLongScalar("select id from notes"));
+//        n.addTag("test");
+//        n.flush();
+//        i.run();
+//        n.load();
+//        assertTrue((n.getTags().size() == 1) && (n.getTags().get(0) == "test"));
+//        // if add-only mode, count will be 0
+//        i.setImportMode(1);
+//        i.run();
+//        assertTrue(i.getTotal() == 0);
+//        // and if dupes mode, will reimport everything
+//        assertTrue(deck.cardCount() == 5);
+//        i.setImportMode(2);
+//        i.run();
+//        // includes repeated field
+//        assertTrue(i.getTotal() == 6);
+//        assertTrue(deck.cardCount() == 11);
+//        deck.close();
+//    }
+//
+//    // Exchange @Suppress for @Test when csv importer is implemented
+//    @Suppress
+//    public void testCsv2() throws  IOException, ConfirmModSchemaException {
+//        Collection deck = Shared.getEmptyCol(InstrumentationRegistry.getTargetContext());
+//        Models mm = deck.getModels();
+//        JSONObject m = mm.current();
+//        JSONObject f = mm.newField("Three");
+//        mm.addField(m, f);
+//        mm.save(m);
+//        Note n = deck.newNote();
+//        n.setItem("Front", "1");
+//        n.setItem("Back", "2");
+//        n.setItem("Three", "3");
+//        deck.addNote(n);
+//        // an update with unmapped fields should not clobber those fields
+//        String file = Shared.getTestFilePath(InstrumentationRegistry.getTargetContext(), "text-update.txt");
+//        TextImporter i = new TextImporter(deck, file);
+//        i.initMapping();
+//        i.run();
+//        n.load();
+//        assertTrue(n.getItem("Front").equals("1"));
+//        assertTrue(n.getItem("Back").equals("x"));
+//        assertTrue(n.getItem("Three").equals("3"));
+//        deck.close();
+//    }
 
     /**
      * Custom tests for AnkiDroid.
