824df0c53975d429ad4eb698410a2c527bd96b2a
Mike Hardy <mike@mikehardy.net>
2018-09-18 16:01:47 -0500

    Add Robolectric test that opens each Activity

---
+++ AnkiDroid/src/androidTest/java/com.ichi2.anki.tests/ActivityTestUI.java
@@ -0,0 +1,341 @@
+package com.ichi2.anki.tests;
+
+
+import android.Manifest;
+import android.support.test.espresso.DataInteraction;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.GrantPermissionRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.azimolabs.conditionwatcher.ConditionWatcher;
+import com.ichi2.anki.IntentHandler;
+import com.ichi2.anki.R;
+
+import junit.framework.Assert;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ActivityTestUI {
+
+    @Rule
+    public GrantPermissionRule mRuntimePermissionRule =
+            GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+    @Rule
+    public ActivityTestRule<IntentHandler> mActivityTestRule = new ActivityTestRule<>(IntentHandler.class);
+
+
+    @Test
+    public void activityTestUI() throws Exception {
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(allOf(withId(R.id.toolbar),
+                                childAtPosition(withId(R.id.deckpicker_view), 0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction navigationMenuItemView = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.navdrawer_items_container),
+                                        0)),
+                        2),
+                        isDisplayed()));
+        navigationMenuItemView.perform(click());
+
+        pressBack();
+
+        ConditionWatcher.waitForCondition(new Shared.ViewItemWaitingInstruction(R.id.deckpicker_view));
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withId(R.id.deckpicker_view),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton2.perform(click());
+
+        ViewInteraction navigationMenuItemView2 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.navdrawer_items_container),
+                                        0)),
+                        3),
+                        isDisplayed()));
+        navigationMenuItemView2.perform(click());
+
+        ViewInteraction appCompatImageButton3 = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton3.perform(click());
+
+        ViewInteraction navigationMenuItemView3 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.navdrawer_items_container),
+                                        0)),
+                        6),
+                        isDisplayed()));
+        navigationMenuItemView3.perform(click());
+
+        ViewInteraction appCompatImageButton4 = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.action_bar),
+                                        childAtPosition(
+                                                withId(R.id.action_bar_container),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton4.perform(click());
+
+        ViewInteraction appCompatImageButton5 = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton5.perform(click());
+
+        ViewInteraction navigationMenuItemView4 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.navdrawer_items_container),
+                                        0)),
+                        6),
+                        isDisplayed()));
+        navigationMenuItemView4.perform(click());
+
+        DataInteraction linearLayout = onData(anything())
+                .inAdapterView(allOf(withId(android.R.id.list),
+                        childAtPosition(
+                                withClassName(is("android.widget.LinearLayout")),
+                                0)))
+                .atPosition(4);
+        linearLayout.perform(click());
+
+        ConditionWatcher.waitForCondition(new Shared.ViewItemWaitingInstruction(android.R.id.list_container));
+        DataInteraction linearLayout2 = onData(anything())
+                .inAdapterView(allOf(withId(android.R.id.list),
+                        childAtPosition(
+                                withId(android.R.id.list_container),
+                                0)))
+                .atPosition(16);
+        linearLayout2.perform(click());
+
+        ViewInteraction appCompatImageButton6 = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton6.perform(click());
+
+        ViewInteraction appCompatImageButton7 = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.action_bar),
+                                        childAtPosition(
+                                                withId(R.id.action_bar_container),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton7.perform(click());
+
+        ViewInteraction appCompatImageButton8 = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.action_bar),
+                                        childAtPosition(
+                                                withId(R.id.action_bar_container),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton8.perform(click());
+
+        ViewInteraction appCompatImageButton9 = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton9.perform(click());
+
+        ViewInteraction appCompatImageButton61 = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton61.perform(click());
+
+        ViewInteraction navigationMenuItemView123 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.navdrawer_items_container),
+                                        0)),
+                        1),
+                        isDisplayed()));
+        navigationMenuItemView123.perform(click());
+
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
+        ViewInteraction appCompatTextView6 = onView(
+                allOf(withId(R.id.title), withText("Manage note types"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.v7.view.menu.ListMenuItemView")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatTextView6.perform(click());
+
+        DataInteraction linearLayout1 = onData(anything())
+                .inAdapterView(allOf(withId(R.id.note_type_browser_list),
+                        childAtPosition(
+                                withClassName(is("android.widget.LinearLayout")),
+                                1)))
+                .atPosition(0);
+        linearLayout1.perform(click());
+
+        ViewInteraction appCompatImageButton35 = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton35.perform(click());
+
+        ViewInteraction appCompatImageButton42 = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton42.perform(click());
+
+        ViewInteraction viewInteraction = onView(
+                allOf(withId(R.id.fab_expand_menu_button), withContentDescription("Add"),
+                        childAtPosition(
+                                allOf(withId(R.id.add_content_menu),
+                                        childAtPosition(
+                                                withId(R.id.root_layout),
+                                                2)),
+                                3),
+                        isDisplayed()));
+        viewInteraction.perform(click());
+
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.add_note_action), withContentDescription("Add"),
+                        childAtPosition(
+                                allOf(withId(R.id.add_content_menu),
+                                        childAtPosition(
+                                                withId(R.id.root_layout),
+                                                2)),
+                                2),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(R.id.CardEditorCardsText), withText("Cards: Card 1"),
+                        childAtPosition(
+                                allOf(withId(R.id.CardEditorCardsButton),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
+                                0)));
+        appCompatTextView.perform(scrollTo(), click());
+
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.action_preview), withContentDescription("Preview"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.toolbar),
+                                        5),
+                                0),
+                        isDisplayed()));
+        Assert.assertNotNull("Did not make it to the end of the UI run", actionMenuItemView);
+        actionMenuItemView.perform(click());
+    }
+
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
--- AnkiDroid/src/androidTest/java/com.ichi2.anki.tests/Shared.java
+++ AnkiDroid/src/androidTest/java/com.ichi2.anki.tests/Shared.java
@@ -16,9 +16,13 @@
 
 package com.ichi2.anki.tests;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.text.TextUtils;
+import android.view.View;
 
+import com.azimolabs.conditionwatcher.Instruction;
 import com.ichi2.libanki.Collection;
 import com.ichi2.libanki.Storage;
 import com.ichi2.libanki.Utils;
@@ -27,12 +31,63 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static org.junit.Assert.assertTrue;
 
 /**
  * Shared methods for unit tests.
  */
 public class Shared {
+
+
+    /**
+     * Instantiate one of these with the View id of the item you want Robolectric to wait on, and it will
+     * fast-poll until the item is inflated and visible
+     */
+    public static class ViewItemWaitingInstruction extends Instruction {
+
+        private int viewId;
+
+
+        public  String getDescription() {
+            return "Waits for an id to be visible";
+        }
+
+
+        /**
+         * Instantiate this with the Resource id of the view to wait for
+         */
+        public ViewItemWaitingInstruction(int viewId) {
+            this.viewId = viewId;
+        }
+
+
+        @Override
+        public boolean checkCondition() {
+            Activity activity = getCurrentActivity();
+            return ((activity != null) && (activity.findViewById(viewId) != null) &&
+                    (activity.findViewById(viewId).getVisibility() == View.VISIBLE));
+        }
+
+
+        /** Get the current activity so, to inspect for View ids */
+        private Activity getCurrentActivity() {
+            final Activity[] activity = new Activity[1];
+            onView(isRoot()).check((view, noViewFoundException) ->
+                    activity[0] = scanForActivity(view.findViewById(android.R.id.content).getContext()));
+            return activity[0];
+        }
+
+
+        /** If the Context is a wrapper, recursively go for the Activity */
+        private Activity scanForActivity(Context cont) {
+            if (cont == null) return null;
+            else if (cont instanceof Activity) return (Activity)cont;
+            else if (cont instanceof ContextWrapper) return scanForActivity(((ContextWrapper)cont).getBaseContext());
+            return null;
+        }
+    }
 
     public static Collection getEmptyCol(Context context) throws IOException {
         File f = File.createTempFile("test", ".anki2");
