9832b1817f5984afb033576c612bbf545527f6f6
Luc Wastiaux <luc.wastiaux@xsmail.com>
2018-09-23 16:22:22 +0800

    remove unused import (junit ignore)

--- AnkiDroid/src/androidTest/java/com.ichi2.anki.tests/ContentProviderTest.java
+++ AnkiDroid/src/androidTest/java/com.ichi2.anki.tests/ContentProviderTest.java
@@ -48,7 +48,6 @@ import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
