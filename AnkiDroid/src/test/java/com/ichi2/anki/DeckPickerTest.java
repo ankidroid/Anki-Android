af3b929a6952cfd4fde811d9acfa5c0d5250a932
Mike Hardy <github@mikehardy.net>
2018-10-26 12:06:15 -0500

    Use robolectric to avoid resource string duplication

--- AnkiDroid/src/test/java/com/ichi2/anki/DeckPickerTest.java
+++ AnkiDroid/src/test/java/com/ichi2/anki/DeckPickerTest.java
@@ -1,16 +1,13 @@
 package com.ichi2.anki;
 
-import android.content.res.Resources;
+import android.content.Context;
 
-import org.junit.After;
-import org.junit.Before;
+import androidx.test.core.app.ApplicationProvider;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,87 +15,36 @@ import java.util.Map;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(RobolectricTestRunner.class)
 public class DeckPickerTest {
-
-    private Map<Integer, String> mCodeResponsePairs = new HashMap<>();
-    private int[] mCodes;
-
-    @Mock
-    private Resources mMockResources;
-
-    @InjectMocks
-    private DeckPicker deckPicker;
-
-    @Before
-    public void setUp() {
-
-        MockitoAnnotations.initMocks(this);
-
-        Mockito.when(mMockResources.getString(R.string.sync_error_407_proxy_required))
-                .thenReturn("Proxy authentication required.");
-        Mockito.when(mMockResources.getString(R.string.sync_error_409))
-                .thenReturn("Only one client can access AnkiWeb at a time. If a previous sync failed, please try again in a few minutes.");
-        Mockito.when(mMockResources.getString(R.string.sync_error_413_collection_size))
-                .thenReturn("Your collection or a media file is too large to sync.");
-        Mockito.when(mMockResources.getString(R.string.sync_error_500_unknown))
-                .thenReturn("AnkiWeb encountered an error. Please try again in a few minutes, and if the problem persists, please file a bug report.");
-        Mockito.when(mMockResources.getString(R.string.sync_error_501_upgrade_required))
-                .thenReturn("Please upgrade to the latest version of AnkiDroid");
-        Mockito.when(mMockResources.getString(R.string.sync_error_502_maintenance))
-                .thenReturn("AnkiWeb is under maintenance. Please try again in a few minutes.");
-        Mockito.when(mMockResources.getString(R.string.sync_too_busy))
-                .thenReturn("Server busy. Try again later.");
-        Mockito.when(mMockResources.getString(R.string.sync_error_504_gateway_timeout))
-                .thenReturn("504 gateway timeout error received.");
-
-        mCodes = new int[] {407, 409, 413, 500, 501, 502, 503, 504};
-
-        mCodeResponsePairs.put(407, "Proxy authentication required.");
-        mCodeResponsePairs.put(409, "Only one client can access AnkiWeb at a time. If a previous sync failed, please try again in a few minutes.");
-        mCodeResponsePairs.put(413, "Your collection or a media file is too large to sync.");
-        mCodeResponsePairs.put(500, "AnkiWeb encountered an error. Please try again in a few minutes, and if the problem persists, please file a bug report.");
-        mCodeResponsePairs.put(501, "Please upgrade to the latest version of AnkiDroid");
-        mCodeResponsePairs.put(502, "AnkiWeb is under maintenance. Please try again in a few minutes.");
-        mCodeResponsePairs.put(503, "Server busy. Try again later.");
-        mCodeResponsePairs.put(504, "504 gateway timeout error received.");
-    }
 
     @Test
     public void verifyCodeMessages() {
-        // verify the all codes return the correct strings
-        for (int code : mCodes) {
-            String msg = deckPicker.rewriteError(code);
-            assertEquals(mCodeResponsePairs.get(code), msg);
+
+        Map<Integer, String> mCodeResponsePairs = new HashMap<>();
+        final Context context = ApplicationProvider.getApplicationContext();
+        mCodeResponsePairs.put(407, context.getString(R.string.sync_error_407_proxy_required));
+        mCodeResponsePairs.put(409, context.getString(R.string.sync_error_409));
+        mCodeResponsePairs.put(413, context.getString(R.string.sync_error_413_collection_size));
+        mCodeResponsePairs.put(500, context.getString(R.string.sync_error_500_unknown));
+        mCodeResponsePairs.put(501, context.getString(R.string.sync_error_501_upgrade_required));
+        mCodeResponsePairs.put(502, context.getString(R.string.sync_error_502_maintenance));
+        mCodeResponsePairs.put(503, context.getString(R.string.sync_too_busy));
+        mCodeResponsePairs.put(504, context.getString(R.string.sync_error_504_gateway_timeout));
+
+        DeckPicker deckPicker = Robolectric.setupActivity(NoDatabaseDeckPicker.class);
+        for (Map.Entry<Integer, String> entry : mCodeResponsePairs.entrySet()) {
+            assertEquals(deckPicker.rewriteError(entry.getKey()), entry.getValue());
         }
     }
 
     @Test
     public void verifyBadCodesNoMessage() {
-        // verify the unknown codes always return null
-        String msg = "";
-        msg = deckPicker.rewriteError(0);
-        assertNull(msg);
-
-        msg = "";
-        msg = deckPicker.rewriteError(-1);
-        assertNull(msg);
-
-        msg = "";
-        msg = deckPicker.rewriteError(1);
-        assertNull(msg);
-
-        msg = "";
-        msg = deckPicker.rewriteError(Integer.MIN_VALUE);
-        assertNull(msg);
-
-        msg = "";
-        msg = deckPicker.rewriteError(Integer.MAX_VALUE);
-        assertNull(msg);
-    }
-
-    @After
-    public void validate() {
-        Mockito.validateMockitoUsage();
+        DeckPicker deckPicker = Robolectric.setupActivity(NoDatabaseDeckPicker.class);
+        assertNull(deckPicker.rewriteError(0));
+        assertNull(deckPicker.rewriteError(-1));
+        assertNull(deckPicker.rewriteError(1));
+        assertNull(deckPicker.rewriteError(Integer.MIN_VALUE));
+        assertNull(deckPicker.rewriteError(Integer.MAX_VALUE));
     }
 }
