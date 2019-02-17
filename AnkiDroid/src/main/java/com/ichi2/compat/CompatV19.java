b2a15c4e12101b3f8b863207e3ab3cad11135160
Timothy Rae <perceptualchaos2@gmail.com>
2019-02-17 22:38:11 +0900

    Don't use SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION in fullscreen mode
    Fixes #5245
    Previously this flag, which forces the answer button to display underneath the navigation bar on swiping during fullscreen mode,
    was working correctly, but currently it's causing the issue seen in #5245.
    SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN is still working correctly

--- AnkiDroid/src/main/java/com/ichi2/compat/CompatV19.java
+++ AnkiDroid/src/main/java/com/ichi2/compat/CompatV19.java
@@ -27,7 +27,7 @@ public class CompatV19 extends CompatV18 implements Compat {
         // Set appropriate flags to enable Sticky Immersive mode.
         a.getWindow().getDecorView().setSystemUiVisibility(
                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        //| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION // temporarily disabled due to #5245
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
