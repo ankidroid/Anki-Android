dc8edd75f686619940a1f46254825c789c9ce8b6
Arthur Milchior <Arthur@Milchior.fr>
2019-10-11 15:48:25 +0200

    New card with same due are shown according to their ord
    This commit corrects the following bug:
    
    * Create a note N in an empty deck D of type basic (reverse), with
    only some back, no front. Only card 2 will be generated.
    * Edit the note, add a front. Card 1 will be generated.
    * In the main window, try to learn deck D. In anki, currently, you'll
      see Card 2 first. According to the manual (and to my common sens),
      it should be Card 1 first.
    
    This commit correct this bug, and ensure that new cards are seen
    according to their order, and not to their creation date.
    
    It follows commit dae/anki@8e0077335a6f1fc03b8045b41fe916bd9dab912d

--- AnkiDroid/src/main/java/com/ichi2/libanki/Sched.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/Sched.java
@@ -619,7 +619,7 @@ public class Sched {
                     cur = mCol
                             .getDb()
                             .getDatabase()
-                            .query("SELECT id FROM cards WHERE did = " + did + " AND queue = 0 order by due LIMIT " + lim,
+                            .query("SELECT id FROM cards WHERE did = " + did + " AND queue = 0 order by due, ord LIMIT " + lim,
                                     null);
                     while (cur.moveToNext()) {
                         mNewQueue.add(cur.getLong(0));
