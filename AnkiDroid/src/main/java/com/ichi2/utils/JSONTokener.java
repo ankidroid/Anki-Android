ec7b0356bdc6217212832bfd75a4853a6b476051
Arthur Milchior <Arthur@Milchior.fr>
2019-10-21 04:29:42 +0200

    Add JSON classes throwing unchecked exceptions.
    I also added a tokener which ensures that only this kind of objects is created

---
+++ AnkiDroid/src/main/java/com/ichi2/utils/JSONTokener.java
@@ -0,0 +1,25 @@
+package com.ichi2.utils;
+
+
+import org.json.JSONException;
+
+public class JSONTokener extends org.json.JSONTokener {
+    public JSONTokener(String s) {
+        super(s);
+    }
+
+
+    public Object nextValue() throws org.json.JSONException {
+        char c = this.nextClean();
+        this.back();
+
+        switch (c) {
+            case '{':
+                return new JSONObject(this);
+            case '[':
+                return new JSONArray(this);
+            default:
+                return super.nextValue();
+        }
+    }
+}
