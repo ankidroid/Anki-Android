2733857b87384ff6391c01f953d38f93e2859cfe
Mike Hardy <github@mikehardy.net>
2020-02-14 18:49:41 -0500

    Avoid crashing if initialization of CustomTabs fails

--- AnkiDroid/src/main/java/com/ichi2/compat/customtabs/CustomTabActivityHelper.java
+++ AnkiDroid/src/main/java/com/ichi2/compat/customtabs/CustomTabActivityHelper.java
@@ -31,6 +31,7 @@ import timber.log.Timber;
  * This is a helper class to manage the connection to the Custom Tabs Service.
  */
 public class CustomTabActivityHelper implements ServiceConnectionCallback {
+    private static boolean sCustomTabsFailed = false;
     private CustomTabsSession mCustomTabsSession;
     private CustomTabsClient mClient;
     private CustomTabsServiceConnection mConnection;
@@ -49,9 +50,9 @@ public class CustomTabActivityHelper implements ServiceConnectionCallback {
                                      CustomTabFallback fallback) {
         String packageName = CustomTabsHelper.getPackageNameToUse(activity);
 
-        //If we cant find a package name, it means theres no browser that supports
-        //Chrome Custom Tabs installed. So, we fallback to the webview
-        if (packageName == null) {
+        //If we cant find a package name or there was a serious failure during init, we don't support
+        //Chrome Custom Tabs. So, we fallback to the webview
+        if (packageName == null || sCustomTabsFailed) {
             if (fallback != null) {
                 fallback.openUri(activity, uri);
             } else {
@@ -100,7 +101,15 @@ public class CustomTabActivityHelper implements ServiceConnectionCallback {
         if (packageName == null) return;
 
         mConnection = new ServiceConnection(this);
-        CustomTabsClient.bindCustomTabsService(activity, packageName, mConnection);
+        try {
+            CustomTabsClient.bindCustomTabsService(activity, packageName, mConnection);
+        } catch (SecurityException e) {
+            Timber.w(e, "CustomTabsService bind attempt failed, using fallback");
+            sCustomTabsFailed = true;
+            mClient = null;
+            mCustomTabsSession = null;
+            mConnection = null;
+        }
     }
 
     /**
