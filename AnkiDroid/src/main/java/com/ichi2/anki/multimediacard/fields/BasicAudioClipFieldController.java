4ad2913668da36db752742bee686c80a64716820
Mike Hardy <github@mikehardy.net>
2020-02-15 10:45:46 -0500

    Improve file copy / error handling for audio clips

--- AnkiDroid/src/main/java/com/ichi2/anki/multimediacard/fields/BasicAudioClipFieldController.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/multimediacard/fields/BasicAudioClipFieldController.java
@@ -28,16 +28,16 @@ import android.widget.LinearLayout;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.ichi2.anki.AnkiDroidApp;
 import com.ichi2.anki.CollectionHelper;
 import com.ichi2.anki.R;
+import com.ichi2.anki.UIUtils;
+import com.ichi2.compat.CompatHelper;
 import com.ichi2.libanki.Collection;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 import timber.log.Timber;
 
@@ -99,65 +99,42 @@ public class BasicAudioClipFieldController extends FieldControllerBase implement
             cursor.close();
 
             // We may receive documents we can't access directly, we have to copy to a temp file
-            File clipCopy = null;
+            File clipCopy;
             try {
                 clipCopy = File.createTempFile("ankidroid_audioclip_" + audioClipFullNameParts[0],
                         "." + audioClipFullNameParts[1],
                         storingDirectory);
-            } catch (IOException e) {
+                Timber.d("audio clip picker file path is: %s", clipCopy.getAbsolutePath());
+            } catch (Exception e) {
                 Timber.e(e, "Could not create temporary audio file. ");
+                UIUtils.showThemedToast(AnkiDroidApp.getInstance().getApplicationContext(),
+                        AnkiDroidApp.getInstance().getString(R.string.multimedia_editor_something_wrong), true);
+                return;
             }
 
             // Copy file contents into new temp file. Possibly check file size first and warn if large?
             InputStream inputStream = null;
             try {
                 inputStream = mActivity.getContentResolver().openInputStream(selectedClip);
+                CompatHelper.getCompat().copyFile(inputStream, clipCopy.getAbsolutePath());
 
-                // copy the picked file contents to internal media iteratively using Buffered Streams?
-                OutputStream outputStream = null;
-                try {
-                    // FIXME no no no no
-                    clipCopy.setWritable(true, false);
-                    outputStream = new FileOutputStream(clipCopy);
-                    byte buffer[] = new byte[1024];
-                    int length = 0;
-
-                    while((length = inputStream.read(buffer)) > 0) {
-                        outputStream.write(buffer,0,length);
-                    }
-                }
-                catch (IOException e) {
-                    Timber.e(e, "error in creating a file");
-                }
-                finally {
-                    try {
-                        outputStream.close();
-                    }
-                    catch (IOException ioe) {
-                        // nothing?
-                    }
-                }
-
+                // If everything worked, hand off the information
                 mField.setHasTemporaryMedia(true);
-            }
-            catch (FileNotFoundException fnfe) {
-                Timber.e(fnfe, "Unable to open selected audio clip");
+                mField.setAudioPath(clipCopy.getAbsolutePath());
+                mTvAudioClip.setText(mField.getFormattedValue());
+                mTvAudioClip.setVisibility(View.VISIBLE);
+            } catch (Exception e) {
+                Timber.e(e, "Unable to copy audio file from ContentProvider");
+                UIUtils.showThemedToast(AnkiDroidApp.getInstance().getApplicationContext(),
+                        AnkiDroidApp.getInstance().getString(R.string.multimedia_editor_something_wrong), true);
             }
             finally {
-                // close the file descriptor here?
                 try {
                     inputStream.close();
-                }
-                catch (IOException ioe) {
+                } catch (IOException ioe) {
                     // nothing
                 }
             }
-
-            Timber.d("audio clip picker file path is: %s", clipCopy.getAbsolutePath());
-            mField.setAudioPath(clipCopy.getAbsolutePath());
-
-            mTvAudioClip.setText(mField.getFormattedValue());
-            mTvAudioClip.setVisibility(View.VISIBLE);
         }
     }
 
