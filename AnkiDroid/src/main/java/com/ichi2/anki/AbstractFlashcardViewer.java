f8041a93bb3249736811d14659250d93a9c76ca4
Mani <12841290+infinyte7@users.noreply.github.com>
2020-03-15 23:55:09 +0530

    Update AbstractFlashcardViewer.java

--- AnkiDroid/src/main/java/com/ichi2/anki/AbstractFlashcardViewer.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/AbstractFlashcardViewer.java
@@ -1475,11 +1475,10 @@ public abstract class AbstractFlashcardViewer extends NavigationDrawerActivity {
                     mFlipCardLayout.setVisibility(View.GONE);
                     return true;
                 }
-
-                 /**
-                  *  Call displayCardAnswer() and answerCard() from anki deck template using javascript
-                  *  See card.js in assets/scripts folder
-                  */
+                /**
+                 *  Call displayCardAnswer() and answerCard() from anki deck template using javascript
+                 *  See card.js in assets/scripts folder
+                 */
                 if ("signal:show_answer".equals(url)) {
                     // display answer when showAnswer() called from card.js
                     if (!sDisplayAnswer) {
@@ -1509,7 +1508,6 @@ public abstract class AbstractFlashcardViewer extends NavigationDrawerActivity {
                         return true;
                     }
                 }
-
                 Intent intent = null;
                 try {
                     if (url.startsWith("intent:")) {
