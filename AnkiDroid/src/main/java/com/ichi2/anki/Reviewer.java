d37532fa6396bbdf6f71b7be121cde2c1a065289
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-03-21 06:21:47 +0000

    #5756 - Fix Dynamic Card Preview Crash
    Previewing a card currently performs displayAnswerBottomBar which
    requires an ease calculation.
    
    The ease calculation depends on the current deck, and is not necessary
    in the previewer as we don't show ease buttons, we get a crash as the
    scheduler is pointed at the wrong deck, so don't calculate either.
    
    Note: This only fixes the crash, it doesn't touch the root cause:
    We rely on libAnki to get cards for previews, which relies on the
    current deck for setting certain data.
    
    This information is neither required, nor correct when previewing.
    
    We can't change the scheduler while previewing as we may be previewing
    during a review session, which would cause a scheduler reset.

--- AnkiDroid/src/main/java/com/ichi2/anki/Reviewer.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/Reviewer.java
@@ -534,6 +534,91 @@ public class Reviewer extends AbstractFlashcardViewer {
         return super.onKeyUp(keyCode, event);
     }
 
+    @Override
+    protected void displayAnswerBottomBar() {
+        super.displayAnswerBottomBar();
+        int buttonCount;
+        try {
+            buttonCount = mSched.answerButtons(mCurrentCard);
+        } catch (RuntimeException e) {
+            AnkiDroidApp.sendExceptionReport(e, "AbstractReviewer-showEaseButtons");
+            closeReviewer(DeckPicker.RESULT_DB_ERROR, true);
+            return;
+        }
+
+        // Set correct label and background resource for each button
+        // Note that it's necessary to set the resource dynamically as the ease2 / ease3 buttons
+        // (which libanki expects ease to be 2 and 3) can either be hard, good, or easy - depending on num buttons shown
+        final int[] background = Themes.getResFromAttr(this, new int [] {
+                R.attr.againButtonRef,
+                R.attr.hardButtonRef,
+                R.attr.goodButtonRef,
+                R.attr.easyButtonRef});
+        final int[] textColor = Themes.getColorFromAttr(this, new int [] {
+                R.attr.againButtonTextColor,
+                R.attr.hardButtonTextColor,
+                R.attr.goodButtonTextColor,
+                R.attr.easyButtonTextColor});
+        mEase1Layout.setVisibility(View.VISIBLE);
+        mEase1Layout.setBackgroundResource(background[0]);
+        mEase4Layout.setBackgroundResource(background[3]);
+        switch (buttonCount) {
+            case 2:
+                // Ease 2 is "good"
+                mEase2Layout.setVisibility(View.VISIBLE);
+                mEase2Layout.setBackgroundResource(background[2]);
+                mEase2.setText(R.string.ease_button_good);
+                mEase2.setTextColor(textColor[2]);
+                mNext2.setTextColor(textColor[2]);
+                mEase2Layout.requestFocus();
+                break;
+            case 3:
+                // Ease 2 is good
+                mEase2Layout.setVisibility(View.VISIBLE);
+                mEase2Layout.setBackgroundResource(background[2]);
+                mEase2.setText(R.string.ease_button_good);
+                mEase2.setTextColor(textColor[2]);
+                mNext2.setTextColor(textColor[2]);
+                // Ease 3 is easy
+                mEase3Layout.setVisibility(View.VISIBLE);
+                mEase3Layout.setBackgroundResource(background[3]);
+                mEase3.setText(R.string.ease_button_easy);
+                mEase3.setTextColor(textColor[3]);
+                mNext3.setTextColor(textColor[3]);
+                mEase2Layout.requestFocus();
+                break;
+            default:
+                mEase2Layout.setVisibility(View.VISIBLE);
+                // Ease 2 is "hard"
+                mEase2Layout.setVisibility(View.VISIBLE);
+                mEase2Layout.setBackgroundResource(background[1]);
+                mEase2.setText(R.string.ease_button_hard);
+                mEase2.setTextColor(textColor[1]);
+                mNext2.setTextColor(textColor[1]);
+                mEase2Layout.requestFocus();
+                // Ease 3 is good
+                mEase3Layout.setVisibility(View.VISIBLE);
+                mEase3Layout.setBackgroundResource(background[2]);
+                mEase3.setText(R.string.ease_button_good);
+                mEase3.setTextColor(textColor[2]);
+                mNext3.setTextColor(textColor[2]);
+                mEase4Layout.setVisibility(View.VISIBLE);
+                mEase3Layout.requestFocus();
+                break;
+        }
+
+        // Show next review time
+        if (shouldShowNextReviewTime()) {
+            mNext1.setText(mSched.nextIvlStr(this, mCurrentCard, 1));
+            mNext2.setText(mSched.nextIvlStr(this, mCurrentCard, 2));
+            if (buttonCount > 2) {
+                mNext3.setText(mSched.nextIvlStr(this, mCurrentCard, 3));
+            }
+            if (buttonCount > 3) {
+                mNext4.setText(mSched.nextIvlStr(this, mCurrentCard, 4));
+            }
+        }
+    }
 
     @Override
     protected SharedPreferences restorePreferences() {
