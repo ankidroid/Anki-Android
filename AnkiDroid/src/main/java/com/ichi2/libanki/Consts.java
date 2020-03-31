934d9052c5073683d99c5b42da5a072ffd3c1c31
Arthur Milchior <Arthur@Milchior.fr>
2020-03-31 20:40:37 +0200

    REVLOG_CRAM

--- AnkiDroid/src/main/java/com/ichi2/libanki/Consts.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/Consts.java
@@ -81,6 +81,7 @@ public class Consts {
     public static final int REVLOG_LRN = 0;
     public static final int REVLOG_REV = 1;
     public static final int REVLOG_RELRN = 2;
+    public static final int REVLOG_CRAM = 3;
 
     // The labels defined in consts.py are in AnkiDroid's resources files.
 }
