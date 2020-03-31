4acec611396296783197935d1639d5a813759202
Arthur Milchior <Arthur@Milchior.fr>
2020-03-31 20:36:33 +0200

    REVLOG_RELRN

--- AnkiDroid/src/main/java/com/ichi2/libanki/Consts.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/Consts.java
@@ -80,6 +80,7 @@ public class Consts {
     // Revlog types
     public static final int REVLOG_LRN = 0;
     public static final int REVLOG_REV = 1;
+    public static final int REVLOG_RELRN = 2;
 
     // The labels defined in consts.py are in AnkiDroid's resources files.
 }
