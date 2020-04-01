8df7393a57da5d73cf4d1d850d17a82ddbf9c4aa
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-04-01 09:34:56 +0100

    NF: RenderBrowserQA invert condition for clarity
    Explain why we don't ned to continue if the card already has data
    rendered

--- AnkiDroid/src/main/java/com/ichi2/async/DeckTask.java
+++ AnkiDroid/src/main/java/com/ichi2/async/DeckTask.java
@@ -907,22 +907,24 @@ public class DeckTask extends BaseAsyncTask<DeckTask.TaskData, DeckTask.TaskData
                 //we won't reach any more cards.
                 continue;
             }
-            if (card.get("answer") == null) {
-                // Extract card item
-                Card c;
-                try {
-                    c = col.getCard(Long.parseLong(card.get("id")));
-                } catch (Exception e) {
-                    //#5891 - card can be inconsistent between the deck browser screen and the collection.
-                    //Realistically, we can skip any exception as it's a rendering task which should not kill the
-                    //process
-                    continue;
-                }
-                // Update item
-                CardBrowser.updateSearchItemQA(mContext, card, c);
-                float progress = (float) i / n * 100;
-                publishProgress(new TaskData((int) progress));
+            if (card.get("answer") != null) {
+                //We've already rendered the answer, we don't need to do it again.
+                continue;
             }
+            // Extract card item
+            Card c;
+            try {
+                c = col.getCard(Long.parseLong(card.get("id")));
+            } catch (Exception e) {
+                //#5891 - card can be inconsistent between the deck browser screen and the collection.
+                //Realistically, we can skip any exception as it's a rendering task which should not kill the
+                //process
+                continue;
+            }
+            // Update item
+            CardBrowser.updateSearchItemQA(mContext, card, c);
+            float progress = (float) i / n * 100;
+            publishProgress(new TaskData((int) progress));
         }
         return new TaskData(cards);
     }
