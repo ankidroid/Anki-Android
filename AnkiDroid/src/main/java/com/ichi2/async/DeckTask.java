6cd92cbd4fb0e74115ee956d08302f44557c8873
david-allison-1 <62114487+david-allison-1@users.noreply.github.com>
2020-04-01 15:14:18 +0100

    Fix RenderBrowserQA Race Condition: Crash on no card
    * NF RenderBrowserQA: Invert condition for clarity
    * NF: Remove default radix
        Long.parseLong defaults to radix: 10, we remove this for code clarity
    * RenderBrowserQA: Handle missing card
        Fixes #5891 - concurrency bug: We can request a card which no longer
        exists
    * NF: Added TODO to improve function
        Currently, we accept a List, and a range of positions. This list is
        updated concurrently and it's not explicit that this is the case, which
        raises the potential for bugs.
    
        We also pass in two boxed Integers, whereas a Range<> structure    
         (or even an IntRange) would cut down on memory.
    * RenderBrowserQA: fix IndexOutOfBounds
        I don't know if it has happened, but it can.
    * NF: RenderBrowserQA invert condition for clarity
        Explain why we don't ned to continue if the card already has data
        rendered

--- AnkiDroid/src/main/java/com/ichi2/async/DeckTask.java
+++ AnkiDroid/src/main/java/com/ichi2/async/DeckTask.java
@@ -864,7 +864,7 @@ public class DeckTask extends BaseAsyncTask<DeckTask.TaskData, DeckTask.TaskData
         List<Map<String,String>> searchResult = col.findCardsForCardBrowser(query, order, deckNames);
         // Render the first few items
         for (int i = 0; i < Math.min(numCardsToRender, searchResult.size()); i++) {
-            Card c = col.getCard(Long.parseLong(searchResult.get(i).get("id"), 10));
+            Card c = col.getCard(Long.parseLong(searchResult.get(i).get("id")));
             CardBrowser.updateSearchItemQA(mContext, searchResult.get(i), c);
         }
         // Finish off the task
@@ -879,6 +879,8 @@ public class DeckTask extends BaseAsyncTask<DeckTask.TaskData, DeckTask.TaskData
 
 
     private TaskData doInBackgroundRenderBrowserQA(TaskData... params) {
+        //TODO: Convert this to accept the following to make thread-safe:
+        //(Range<Position>, Function<Position, BrowserCard>)
         Timber.d("doInBackgroundRenderBrowserQA");
         Collection col = CollectionHelper.getInstance().getCol(mContext);
         List<Map<String, String>> cards = (List<Map<String, String>>) params[0].getObjArray()[0];
@@ -892,17 +894,37 @@ public class DeckTask extends BaseAsyncTask<DeckTask.TaskData, DeckTask.TaskData
                 Timber.d("doInBackgroundRenderBrowserQA was aborted");
                 return null;
             }
-            if (i >= 0 && i < cards.size()) {
-                Map<String, String> card = cards.get(i);
-                if (card.get("answer") == null) {
-                    // Extract card item
-                    Card c = col.getCard(Long.parseLong(card.get("id"), 10));
-                    // Update item
-                    CardBrowser.updateSearchItemQA(mContext, card, c);
-                    float progress = (float) i / n * 100;
-                    publishProgress(new TaskData((int) progress));
-                }
+            if (i < 0 || i >= cards.size()) {
+                continue;
             }
+            Map<String, String> card;
+            try {
+                card = cards.get(i);
+            }
+            catch (IndexOutOfBoundsException e) {
+                //even though we test against card.size() above, there's still a race condition
+                //We might be able to optimise this to return here. Logically if we're past the end of the collection,
+                //we won't reach any more cards.
+                continue;
+            }
+            if (card.get("answer") != null) {
+                //We've already rendered the answer, we don't need to do it again.
+                continue;
+            }
+            // Extract card item
+            Card c;
+            try {
+                c = col.getCard(Long.parseLong(card.get("id")));
+            } catch (Exception e) {
+                //#5891 - card can be inconsistent between the deck browser screen and the collection.
+                //Realistically, we can skip any exception as it's a rendering task which should not kill the
+                //process
+                continue;
+            }
+            // Update item
+            CardBrowser.updateSearchItemQA(mContext, card, c);
+            float progress = (float) i / n * 100;
+            publishProgress(new TaskData((int) progress));
         }
         return new TaskData(cards);
     }
