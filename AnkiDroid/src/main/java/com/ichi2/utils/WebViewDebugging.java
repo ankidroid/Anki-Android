20e9143f34b64a1417efb50dcd27e1fb249e9a02
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-04-10 17:54:06 +0100

    WebViewDebugging: Disable on unset preference
    If the preference is unset, disable it the next time we use it.

--- AnkiDroid/src/main/java/com/ichi2/utils/WebViewDebugging.java
+++ AnkiDroid/src/main/java/com/ichi2/utils/WebViewDebugging.java
@@ -9,9 +9,9 @@ public class WebViewDebugging {
     public static void initializeDebugging(SharedPreferences sharedPrefs) {
         // DEFECT: We might be able to cache this value: check what happens on WebView Renderer crash
         // On your desktop use chrome://inspect to connect to emulator WebViews
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT &&
-                sharedPrefs.getBoolean("html_javascript_debugging", false)) {
-            WebView.setWebContentsDebuggingEnabled(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            boolean enableDebugging = sharedPrefs.getBoolean("html_javascript_debugging", false);
+            WebView.setWebContentsDebuggingEnabled(enableDebugging);
         }
     }
 }
