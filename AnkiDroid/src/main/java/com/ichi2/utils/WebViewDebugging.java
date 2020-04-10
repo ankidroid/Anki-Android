ff5c68e8a9038619fe70e8a272e5a7f6d7387365
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-04-10 17:50:12 +0100

    Extract: WebViewDebugging.initializeDebugging
    I want to use this API from #1377

---
+++ AnkiDroid/src/main/java/com/ichi2/utils/WebViewDebugging.java
@@ -0,0 +1,17 @@
+package com.ichi2.utils;
+
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.webkit.WebView;
+
+public class WebViewDebugging {
+
+    public static void initializeDebugging(SharedPreferences sharedPrefs) {
+        // DEFECT: We might be able to cache this value: check what happens on WebView Renderer crash
+        // On your desktop use chrome://inspect to connect to emulator WebViews
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT &&
+                sharedPrefs.getBoolean("html_javascript_debugging", false)) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+    }
+}
