c1e83c111a43dd73597b2fdd3b9f6bedc2fd0dd8
Mike Hardy <github@mikehardy.net>
2020-04-12 11:34:31 -0500

    Remove JSONException handling boilerplate

--- AnkiDroid/src/main/java/com/ichi2/anki/ModelFieldEditor.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/ModelFieldEditor.java
@@ -445,17 +445,13 @@ public class ModelFieldEditor extends AnkiActivity {
      * Toggle the "Remember last input" setting AKA the "Sticky" setting
      */
     private void toggleStickyField() {
-        try {
-            // Get the current field
-            JSONObject field = (JSONObject) mNoteFields.get(mCurrentPos);
-            // If the sticky setting is enabled then disable it, otherwise enable it
-            if (field.getBoolean("sticky")){
-                field.put("sticky", false);
-            } else {
-                field.put("sticky", true);
-            }
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
+        // Get the current field
+        JSONObject field = (JSONObject) mNoteFields.get(mCurrentPos);
+        // If the sticky setting is enabled then disable it, otherwise enable it
+        if (field.getBoolean("sticky")) {
+            field.put("sticky", false);
+        } else {
+            field.put("sticky", true);
         }
     }
 
