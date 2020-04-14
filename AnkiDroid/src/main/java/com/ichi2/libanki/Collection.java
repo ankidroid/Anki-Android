89b407c14c5eff9f2dd06d66af82703bec9bc39e
Arthur Milchior <Arthur@Milchior.fr>
2020-04-14 13:21:41 +0200

    reset -> planifyReset in col.reset
    Col.reset is called 5 times. In no case the result of the scheduler is
    directly needed. So it's an acceptable change

--- AnkiDroid/src/main/java/com/ichi2/libanki/Collection.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/Collection.java
@@ -504,7 +504,7 @@ public class Collection {
      * Rebuild the queue and reload data after DB modified.
      */
     public void reset() {
-        mSched.reset();
+        mSched.deferReset();
     }
 
 
