700691ba93708e8001a63b463dccf925fb4a6e35
Arthur Milchior <Arthur@Milchior.fr>
2020-04-15 01:56:45 +0200

    TASK_TYPE_UPDATE_FACTS_MULTI to TASK_TYPE_UPDATE_NOTES_MULTI

--- AnkiDroid/src/main/java/com/ichi2/async/CollectionTask.java
+++ AnkiDroid/src/main/java/com/ichi2/async/CollectionTask.java
@@ -75,7 +75,7 @@ public class CollectionTask extends BaseAsyncTask<CollectionTask.TaskData, Colle
     public static final int TASK_TYPE_ANSWER_CARD = 3;
     public static final int TASK_TYPE_ADD_NOTE = 6;
     public static final int TASK_TYPE_UPDATE_NOTE = 7;
-    public static final int TASK_TYPE_UPDATE_FACTS_MULTI = 9;
+    public static final int TASK_TYPE_UPDATE_NOTES_MULTI = 9;
     public static final int TASK_TYPE_UNDO = 8;
     public static final int TASK_TYPE_DISMISS = 11;
     public static final int TASK_TYPE_DISMISS_MULTI = 12;
@@ -255,7 +255,7 @@ public class CollectionTask extends BaseAsyncTask<CollectionTask.TaskData, Colle
             case TASK_TYPE_UPDATE_NOTE:
                 return doInBackgroundUpdateNote(params);
 
-            case TASK_TYPE_UPDATE_FACTS_MULTI:
+            case TASK_TYPE_UPDATE_NOTES_MULTI:
                 return doInBackgroundUpdateNotes(params);
 
             case TASK_TYPE_UNDO:
