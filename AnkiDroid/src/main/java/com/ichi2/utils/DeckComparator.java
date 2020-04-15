848856b8faf4d63c008ec2c8ea64113c6dbf554a
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-04-15 18:09:19 +0100

    AUTOLINT: DeckComparator

--- AnkiDroid/src/main/java/com/ichi2/utils/DeckComparator.java
+++ AnkiDroid/src/main/java/com/ichi2/utils/DeckComparator.java
@@ -1,8 +1,5 @@
 package com.ichi2.utils;
 
-import com.ichi2.utils.DeckNameComparator;
-import com.ichi2.utils.JSONObject;
-
 import java.util.Comparator;
 
 public class DeckComparator implements Comparator<JSONObject> {
