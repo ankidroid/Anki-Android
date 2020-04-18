1c290738373c6a19fb8f9fa16c543097ec8c9da9
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-04-18 19:53:25 +0100

    Add additional tests to ActionBarOverflow

--- AnkiDroid/src/androidTest/java/com/ichi2/ui/ActionBarOverflowTest.java
+++ AnkiDroid/src/androidTest/java/com/ichi2/ui/ActionBarOverflowTest.java
@@ -1,10 +1,21 @@
 package com.ichi2.ui;
 
+import android.content.Context;
+import android.view.MenuItem;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import androidx.appcompat.view.menu.MenuBuilder;
+import androidx.test.platform.app.InstrumentationRegistry;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 @RunWith(androidx.test.ext.junit.runners.AndroidJUnit4.class)
 public class ActionBarOverflowTest {
@@ -21,5 +32,37 @@ public class ActionBarOverflowTest {
         //https://developer.android.com/distribute/best-practices/develop/restrictions-non-sdk-interfaces
         //Once this throws, errors are being thrown on a currently graylisted method
         ActionBarOverflow.setupMethods(ActionBarOverflow::getPrivateMethodOnlyHandleExceptions);
+    }
+
+    @Test
+    public void testAndroidXMenuItem() {
+        Context targetContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        MenuBuilder b = new MenuBuilder(targetContext);
+        MenuItem i = b.add("Test");
+
+        Boolean value = ActionBarOverflow.isActionButton(i);
+
+        assertThat(value, notNullValue());
+    }
+
+    @Test
+    public void testAndroidMenuItem() throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Context targetContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        String clazz = "com.android.internal.view.menu.MenuBuilder";
+        Class<?> c = Class.forName(clazz);
+
+        Constructor constructor = c.getConstructor(Context.class);
+
+        Object mmb = constructor.newInstance(targetContext);
+
+        Method add = mmb.getClass().getMethod("add", CharSequence.class);
+        add.setAccessible(true);
+
+        MenuItem mi = (MenuItem) add.invoke(mmb, "Add");
+
+        Boolean value = ActionBarOverflow.isActionButton(mi);
+
+        assertThat(value, notNullValue());
     }
 }
