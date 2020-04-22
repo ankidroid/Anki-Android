d654f78912517304f74ceabe47e2940881c6f542
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-04-22 18:10:33 +0100

    NF: Only log URLS in debug mode
    This was intended to catch signals, not all resources.

--- AnkiDroid/src/main/java/com/ichi2/anki/AbstractFlashcardViewer.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/AbstractFlashcardViewer.java
@@ -2785,7 +2785,7 @@ public abstract class AbstractFlashcardViewer extends NavigationDrawerActivity i
         @TargetApi(Build.VERSION_CODES.N)
         public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
             String url = request.getUrl().toString();
-            Timber.i("Obtained URL from card: '%s'", url);
+            Timber.d("Obtained URL from card: '%s'", url);
             return filterUrl(url);
         }
 
