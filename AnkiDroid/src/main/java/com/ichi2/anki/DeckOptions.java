5d0eb69ca943b646a1141ba808e7426e443202e5
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-04-30 16:27:40 +0100

    Handle DeckConfig."timer" as boolean
    Anki 2.1.24 converted this to an boolean, reverted in 2.1.25, but we
    need to coerce it back to avoid a crash.
    
    Fixes #6089

--- AnkiDroid/src/main/java/com/ichi2/anki/DeckOptions.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/DeckOptions.java
@@ -46,6 +46,7 @@ import com.ichi2.anki.services.ReminderService;
 import com.ichi2.async.CollectionTask;
 import com.ichi2.libanki.Collection;
 import com.ichi2.libanki.Consts;
+import com.ichi2.libanki.DeckConfig;
 import com.ichi2.preferences.StepsPreference;
 import com.ichi2.preferences.TimePreference;
 import com.ichi2.themes.StyledProgressDialog;
@@ -106,7 +107,7 @@ public class DeckOptions extends AppCompatPreferenceActivity implements OnShared
                 mValues.put("deckConf", mDeck.getString("conf"));
                 // general
                 mValues.put("maxAnswerTime", mOptions.getString("maxTaken"));
-                mValues.put("showAnswerTimer", Boolean.toString(mOptions.getInt("timer") == 1));
+                mValues.put("showAnswerTimer", Boolean.toString(parseTimerValue(mOptions)));
                 mValues.put("autoPlayAudio", Boolean.toString(mOptions.getBoolean("autoplay")));
                 mValues.put("replayQuestion", Boolean.toString(mOptions.optBoolean("replayq", true)));
                 // new
@@ -155,6 +156,12 @@ public class DeckOptions extends AppCompatPreferenceActivity implements OnShared
                 finish();
             }
         }
+
+
+        private boolean parseTimerValue(JSONObject options) {
+            return DeckConfig.parseTimerOpt(options, true);
+        }
+
 
         public class Editor implements SharedPreferences.Editor {
 
---
+++ AnkiDroid/src/main/java/com/ichi2/libanki/DeckConfig.java
@@ -0,0 +1,46 @@
+/*
+ Copyright (c) 2020 David Allison <davidallisongithub@gmail.com>
+
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
+ Foundation; either version 3 of the License, or (at your option) any later
+ version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with
+ this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ichi2.libanki;
+
+import com.ichi2.utils.JSONObject;
+
+import androidx.annotation.Nullable;
+
+public class DeckConfig {
+
+    public static @Nullable Boolean parseTimer(JSONObject config) {
+        //Note: Card.py used != 0, DeckOptions used == 1
+        try {
+            //#6089 - Anki 2.1.24 changed this to a bool, reverted in 2.1.25.
+            return config.getInt("timer") != 0;
+        } catch (Exception e) {
+            try {
+                return config.getBoolean("timer");
+            } catch (Exception ex) {
+                return null;
+            }
+        }
+    }
+
+    public static boolean parseTimerOpt(JSONObject config, boolean defaultValue) {
+        Boolean ret = parseTimer(config);
+        if (ret == null) {
+            ret = defaultValue;
+        }
+        return ret;
+    }
+}
