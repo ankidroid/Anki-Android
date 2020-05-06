82117e7eebf9c3db5ca52a766ac05507eac81265
Mike Hardy <github@mikehardy.net>
2020-05-06 11:47:21 -0500

    Only restricted devices require system browser
    (cherry picked from commit 27f90024aba6900e7c4ed4c54bd33d754cdbcb00)

--- AnkiDroid/src/main/java/com/ichi2/utils/AdaptionUtil.java
+++ AnkiDroid/src/main/java/com/ichi2/utils/AdaptionUtil.java
@@ -1,3 +1,19 @@
+/****************************************************************************************
+ * Copyright (c) 2020 gaoyingjun@xiaomi.com                                             *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 3 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
 package com.ichi2.utils;
 
 import android.content.Context;
@@ -12,6 +28,8 @@ import android.os.Build;
 import java.util.List;
 
 public class AdaptionUtil {
+    private static boolean sHasRunRestrictedLearningDeviceCheck = false;
+    private static boolean sIsRestrictedLearningDevice = false;
     private static boolean sHasRunWebBrowserCheck = false;
     private static boolean sHasWebBrowser = true;
 
@@ -23,23 +41,24 @@ public class AdaptionUtil {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
         PackageManager pm = context.getPackageManager();
         List<ResolveInfo> list = pm.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
-        if (list.size() == 0) {
-            sHasWebBrowser = false;
-        } else {
-            sHasWebBrowser = false;
-            for (ResolveInfo ri:list) {
-                String pacagename = ri.activityInfo.packageName;
-                if (isSystemApp(pacagename, pm)) {
-                    sHasWebBrowser = true;
-                    break;
-                }
+        sHasWebBrowser = false;
+        for (ResolveInfo ri : list) {
+            // If we aren't a restricted device, any browser will do
+            if (!isRestrictedLearningDevice()) {
+                sHasWebBrowser = true;
+                break;
+            }
+            // If we are a restricted device, only a system browser will do
+            if (isSystemApp(ri.activityInfo.packageName, pm)) {
+                sHasWebBrowser = true;
+                break;
             }
         }
         sHasRunWebBrowserCheck = true;
         return sHasWebBrowser;
     }
 
-    private static boolean isSystemApp(String packageName, PackageManager pm){
+    private static boolean isSystemApp(String packageName, PackageManager pm) {
         if (packageName != null) {
             try {
                 PackageInfo info = pm.getPackageInfo(packageName, 0);
@@ -53,7 +72,13 @@ public class AdaptionUtil {
         }
     }
 
-    public static boolean hasReducedPreferences(){
-        return "Xiaomi".equalsIgnoreCase(Build.MANUFACTURER) && ("Archytas".equalsIgnoreCase(Build.PRODUCT) || "Archimedes".equalsIgnoreCase(Build.PRODUCT));
+    public static boolean isRestrictedLearningDevice() {
+        if (!sHasRunRestrictedLearningDeviceCheck) {
+            sIsRestrictedLearningDevice =
+                    "Xiaomi".equalsIgnoreCase(Build.MANUFACTURER) &&
+                            ("Archytas".equalsIgnoreCase(Build.PRODUCT) || "Archimedes".equalsIgnoreCase(Build.PRODUCT));
+            sHasRunRestrictedLearningDeviceCheck = true;
+        }
+        return sIsRestrictedLearningDevice;
     }
 }
