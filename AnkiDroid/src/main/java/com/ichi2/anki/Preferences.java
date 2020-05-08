f5a04d4e6144e98ebbae8fa816752aa0f0231ac4
infinyte7 <manikant.gautam.712@gmail.com>
2020-05-08 21:07:57 +0800

    Update Preferences.java

--- AnkiDroid/src/main/java/com/ichi2/anki/Preferences.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/Preferences.java
@@ -77,7 +77,6 @@ import com.ichi2.utils.JSONObject;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.nio.channels.FileChannel;
 import java.sql.Timestamp;
 import java.util.Arrays;
