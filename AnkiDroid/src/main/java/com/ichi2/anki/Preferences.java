519b21c6e7eb00a4777bc373718aefd864389dfa
Mani <12841290+infinyte7@users.noreply.github.com>
2020-05-09 11:43:16 +0800

    Update Preferences.java

--- AnkiDroid/src/main/java/com/ichi2/anki/Preferences.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/Preferences.java
@@ -30,7 +30,6 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
@@ -41,7 +40,6 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
-import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.WindowManager.BadTokenException;
@@ -74,10 +72,6 @@ import com.ichi2.utils.VersionUtils;
 
 import com.ichi2.utils.JSONObject;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.nio.channels.FileChannel;
 import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -114,7 +108,6 @@ public class Preferences extends AppCompatPreferenceActivity implements Preferen
     private static final String [] sCollectionPreferences = {"showEstimates", "showProgress",
             "learnCutoff", "timeLimit", "useCurrent", "newSpread", "dayOffset", "schedVer"};
 
-    private static final int RESULT_LOAD_IMG = 111;
 
     // ----------------------------------------------------------------------------
     // Overridden methods
@@ -248,32 +241,6 @@ public class Preferences extends AppCompatPreferenceActivity implements Preferen
             case "com.ichi2.anki.prefs.appearance":
                 listener.addPreferencesFromResource(R.xml.preferences_appearance);
                 screen = listener.getPreferenceScreen();
-                CheckBoxPreference backgroundImage = (CheckBoxPreference) screen.findPreference("deckPickerBackground");
-                backgroundImage.setOnPreferenceClickListener(preference -> {
-                    if (backgroundImage.isChecked()) {
-                        try {
-                            Intent galleryIntent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                            startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
-                            backgroundImage.setChecked(true);
-                        } catch (Exception ex) {
-                            Timber.e("%s",ex.getLocalizedMessage());
-                        }
-                    } else {
-                        backgroundImage.setChecked(false);
-                        String currentAnkiDroidDirectory = CollectionHelper.getCurrentAnkiDroidDirectory(this);
-                        File imgFile = new File(currentAnkiDroidDirectory, "DeckPickerBackground.png" );
-                        if(imgFile.exists()) {
-                            if(imgFile.delete()) {
-                                UIUtils.showThemedToast(this, getString(R.string.background_image_removed), false);
-                            } else {
-                                UIUtils.showThemedToast(this, getString(R.string.error_deleting_image), false);
-                            }
-                        } else {
-                            UIUtils.showThemedToast(this, getString(R.string.background_image_removed), false);
-                        }
-                    }
-                    return true;
-                });
                 initializeCustomFontsDialog(screen);
                 break;
             case "com.ichi2.anki.prefs.gestures":
@@ -405,44 +372,6 @@ public class Preferences extends AppCompatPreferenceActivity implements Preferen
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        try {
-            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK && null != data) {
-                Cursor cursor = null;
-                try {
-                    Uri selectedImage = data.getData();
-                    String[] filePathColumn = { MediaStore.Images.Media.DATA };
-                    cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
-                    cursor.moveToFirst();
-                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                    String imgPathString = cursor.getString(columnIndex);
-                    File sourceFile = new File(imgPathString);
-
-                    String currentAnkiDroidDirectory = CollectionHelper.getCurrentAnkiDroidDirectory(this);
-                    String imageName = "DeckPickerBackground.png";
-                    File destFile = new File(currentAnkiDroidDirectory, imageName);
-
-                    try (FileChannel sourceChannel = new FileInputStream(sourceFile).getChannel();
-                         FileChannel destChannel = new FileOutputStream(destFile).getChannel()) {
-                        destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
-                        UIUtils.showThemedToast(this, getString(R.string.background_image_applied), false);
-                    }
-                } catch (Exception ex1) {
-                    Timber.e("%s",ex1.getLocalizedMessage());
-                } finally {
-                    if (cursor != null) {
-                        cursor.close();
-                    }
-                }
-            } else {
-                UIUtils.showThemedToast(this, getString(R.string.no_image_selected), false);
-            }
-        } catch (OutOfMemoryError | Exception e) {
-            UIUtils.showThemedToast(this, getString(R.string.error_selecting_image), false);
-        }
-    }
 
     private void addThirdPartyAppsListener(PreferenceScreen screen) {
         //#5864 - some people don't have a browser so we can't use <intent>
