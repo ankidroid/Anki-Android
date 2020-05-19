9d5c7812f28a1ebb53e488f6b160727ee73415e5
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-05-20 00:43:32 +0100

    LOG: startLoadingCollection - sync/async

--- AnkiDroid/src/main/java/com/ichi2/anki/AnkiActivity.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/AnkiActivity.java
@@ -269,6 +269,7 @@ public class AnkiActivity extends AppCompatActivity implements SimpleMessageDial
     public void startLoadingCollection() {
         Timber.d("AnkiActivity.startLoadingCollection()");
         if (colIsOpen()) {
+            Timber.d("Synchronously calling onCollectionLoaded");
             onCollectionLoaded(getCol());
             return;
         }
@@ -276,6 +277,7 @@ public class AnkiActivity extends AppCompatActivity implements SimpleMessageDial
         showProgressBar();
         CollectionLoader.load(this, col -> {
             if (col != null) {
+                Timber.d("Asynchronously calling onCollectionLoaded");
                 onCollectionLoaded(col);
             } else {
                 Intent deckPicker = new Intent(this, DeckPicker.class);
