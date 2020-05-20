8fb4f6d222f1850610191a4ac2d00b359aa66faa
Mike Hardy <github@mikehardy.net>
2020-05-20 11:39:56 -0500

    Add note on broken string finder that it is macOS dependent
    Not sure exactly why it is mac dependent but it seems to be now,
    so I made the one search that was failing not be a hard-fail and
    gave it a notice for any developers using it to scan

--- tools/find-broken-strings-variables.sh
+++ tools/find-broken-strings-variables.sh
@@ -11,24 +11,25 @@ if grep -RHn "%1$ d" res/values*; then EXIT_STATUS=$((EXIT_STATUS + 1)); fi
 if grep -RHn "%1" res/values* | grep -v "%1\\$"; then EXIT_STATUS=$((EXIT_STATUS + 1)); fi
 
 # This is currently not working and I'm not sure why? It worked a couple days ago as of 20200516
-#if grep -RHn '%' res/values* |
-# sed -e 's/%/\n%/g' | # Split lines that contain several expressions
-# grep '%'           | # Filter out lines that do not contain expressions
-# grep -v ' n% '     | # Lone % character, not a variable
-# grep -v '(n%)'     | # Lone % character, not a variable
-# grep -v 'n%<'      | # Same, at the end of the string
-# grep -v '>n% '     | # Same, at the beginning of the string
-# grep -v '%で'      | # Same, no spaces in Japanese
-# grep -v '%s'       | # Single string variable
-# grep -v '%d'       | # Single decimal variable
-# grep -v '%[0-9][0-9]\?$s' | # Multiple string variable
-# grep -v '%[0-9][0-9]\?$d' |  # Multiple decimal variable
-# grep -v '%1$.1f'   | # ?
-# grep -v '%.1f'     |
-# grep -v '%\\n'
-#then
-# EXIT_STATUS=$((EXIT_STATUS + 1))
-#fi
+echo "The next test will likely only run correctly on macOS. On Ubuntu it does not work well."
+if grep -RHn '%' res/values* |
+ sed -e 's/%/\n%/g' | # Split lines that contain several expressions
+ grep '%'           | # Filter out lines that do not contain expressions
+ grep -v ' n% '     | # Lone % character, not a variable
+ grep -v '(n%)'     | # Lone % character, not a variable
+ grep -v 'n%<'      | # Same, at the end of the string
+ grep -v '>n% '     | # Same, at the beginning of the string
+ grep -v '%で'      | # Same, no spaces in Japanese
+ grep -v '%s'       | # Single string variable
+ grep -v '%d'       | # Single decimal variable
+ grep -v '%[0-9][0-9]\?$s' | # Multiple string variable
+ grep -v '%[0-9][0-9]\?$d' |  # Multiple decimal variable
+ grep -v '%1$.1f'   | # ?
+ grep -v '%.1f'     |
+ grep -v '%\\n'
+then
+ echo "Found errors but if you are not on macOS they might be false positive." #EXIT_STATUS=$((EXIT_STATUS + 1))
+fi
 
 if grep -RHn '％' res/values*; then EXIT_STATUS=$((EXIT_STATUS + 1)); fi
 
