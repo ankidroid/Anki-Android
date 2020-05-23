621f2964f7b289415ea10393071fd668df014934
Arthur Milchior <Arthur@Milchior.fr>
2020-05-23 05:13:02 +0200

    NF: use a mandatory if

--- AnkiDroid/src/main/java/com/ichi2/anki/multimediacard/activity/TranslationActivity.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/multimediacard/activity/TranslationActivity.java
@@ -303,21 +303,7 @@ public class TranslationActivity extends FragmentActivity implements DialogInter
          * here only the translations to the destination language.
          */
 
-        List<Tuc> tucs = null;
-
-        if (tucs == null) {
-            return res;
-        }
-
-        String desiredLang = LanguagesListerGlosbe.requestToResponseLangCode(languageCodeTo);
-
-        for (Tuc tuc : tucs) {
-            if (tuc == null) {
-                continue;
-            }
-        }
-
-        return res;
+         return res;
     }
 
 
