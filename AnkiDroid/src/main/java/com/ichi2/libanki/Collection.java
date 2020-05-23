75bf744fa1ef20f22d441370934e4e7f1605afb1
Arthur Milchior <Arthur@Milchior.fr>
2020-05-23 08:02:28 +0200

    NF: remove unused emptyCardReport

--- AnkiDroid/src/main/java/com/ichi2/libanki/Collection.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/Collection.java
@@ -922,26 +922,6 @@ public class Collection {
     }
 
 
-    public String emptyCardReport(List<Long> cids) {
-        StringBuilder rep = new StringBuilder();
-        Cursor cur = null;
-        try {
-            cur = mDb.getDatabase().query("select group_concat(ord+1), count(), flds from cards c, notes n "
-                                           + "where c.nid = n.id and c.id in " + Utils.ids2str(cids) + " group by nid", null);
-            while (cur.moveToNext()) {
-                String ords = cur.getString(0);
-                //int cnt = cur.getInt(1);  // present but unused upstream as well
-                String flds = cur.getString(2);
-                rep.append(String.format("Empty card numbers: %s\nFields: %s\n\n", ords, flds.replace("\u001F", " / ")));
-            }
-        } finally {
-            if (cur != null && !cur.isClosed()) {
-                cur.close();
-            }
-        }
-        return rep.toString();
-    }
-
     /**
      * Field checksums and sorting fields ***************************************
      * ********************************************************
