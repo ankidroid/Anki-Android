cfba5a6a1f9de46a35814442312b509fe48b1bc3
Arthur Milchior <Arthur@Milchior.fr>
2020-05-23 07:32:48 +0200

    NF: remove unused StorageAccessException constructor

--- AnkiDroid/src/main/java/com/ichi2/anki/exception/StorageAccessException.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/exception/StorageAccessException.java
@@ -3,10 +3,6 @@ package com.ichi2.anki.exception;
 
 public class StorageAccessException extends Exception {
 
-    public StorageAccessException() {
-    }
-
-
     public StorageAccessException(String msg) {
         super(msg);
     }
