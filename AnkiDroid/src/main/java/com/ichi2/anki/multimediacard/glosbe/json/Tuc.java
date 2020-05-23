facb17e56e42720a1442e2c073bc25abfd4746cd
Arthur Milchior <Arthur@Milchior.fr>
2020-05-23 05:06:25 +0200

    NF: Tuc: remove unused method

--- AnkiDroid/src/main/java/com/ichi2/anki/multimediacard/glosbe/json/Tuc.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/multimediacard/glosbe/json/Tuc.java
@@ -29,44 +29,12 @@ public class Tuc {
     private Number meaningId;
     private List<Meaning> meanings;
     private Phrase phrase;
-
-
-    public List<Number> getAuthors() {
-        return this.authors;
-    }
-
-
-    public void setAuthors(List<Number> authors) {
-        this.authors = authors;
-    }
-
-
-    public Number getMeaningId() {
-        return this.meaningId;
-    }
-
-
-    public void setMeaningId(Number meaningId) {
-        this.meaningId = meaningId;
-    }
-
-
     public List<Meaning> getMeanings() {
         return this.meanings;
     }
 
 
-    public void setMeanings(List<Meaning> meanings) {
-        this.meanings = meanings;
-    }
-
-
     public Phrase getPhrase() {
         return this.phrase;
-    }
-
-
-    public void setPhrase(Phrase phrase) {
-        this.phrase = phrase;
     }
 }
