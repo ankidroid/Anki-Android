11a248058123a0126234492ffacaa718a85cf9fb
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-05-24 14:18:19 +0100

    Don't init app if launched from ACRA sender
    This was mentioned in #6266 and seems like a reasonable action to take
    We get a second application launched to perform the sending of ACRA
    reports, and we don't want this to perform our normal setup code.

--- AnkiDroid/src/main/java/com/ichi2/anki/AnkiDroidApp.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/AnkiDroidApp.java
@@ -237,6 +237,12 @@ public class AnkiDroidApp extends Application {
             UsageAnalytics.setDryRun(true);
         }
 
+        //Stop after analytics and logging are initialised.
+        if (ACRA.isACRASenderServiceProcess()) {
+            Timber.d("Skipping AnkiDroidApp.onCreate from ACRA sender process");
+            return;
+        }
+
         CardBrowserContextMenu.ensureConsistentStateWithSharedPreferences(this);
         NotificationChannels.setup(getApplicationContext());
 
