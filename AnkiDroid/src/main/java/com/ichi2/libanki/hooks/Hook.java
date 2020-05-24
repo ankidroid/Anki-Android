5539bd34d5d41a53c0c11e9be943ebb06659dcd2
Arthur Milchior <Arthur@Milchior.fr>
2020-05-24 09:03:36 +0200

    NF: rename vars to "txt"

--- AnkiDroid/src/main/java/com/ichi2/libanki/hooks/Hook.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/hooks/Hook.java
@@ -65,7 +65,7 @@ public class Hook {
         // do nothing
     }
 
-    public String runFilter(String arg, String tag) {
-        return arg;
+    public String runFilter(String txt, String tag) {
+        return txt;
     }
 }
