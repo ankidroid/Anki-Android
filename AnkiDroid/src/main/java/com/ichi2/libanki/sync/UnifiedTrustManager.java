4f7452fd4cde65087fa9ccb598c0272942ddddea
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-05-31 15:14:55 +0100

    Add USERTrust certificate for older Android devices
    Cert from: https://crt.sh/?id=1199354
    
    The previous root certificate expired on 2020-05-30
    https://support.sectigo.com/articles/Knowledge/Sectigo-AddTrust-External-CA-Root-Expiring-May-30-2020
    
    Fixes #6317

---
+++ AnkiDroid/src/main/java/com/ichi2/libanki/sync/UnifiedTrustManager.java
@@ -0,0 +1,76 @@
+/*
+ Copyright (c) 2020 David Allison <davidallisongithub@gmail.com>
+
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
+ Foundation; either version 3 of the License, or (at your option) any later
+ version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with
+ this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ichi2.libanki.sync;
+
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
+
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+// https://stackoverflow.com/questions/27562666/programmatically-add-a-certificate-authority-while-keeping-android-system-ssl-ce
+// Changes:
+// We try the local manager first.
+// Cached the accepted issuers.
+// Did not ignore NoSuchAlgorithmException
+class UnifiedTrustManager implements X509TrustManager {
+    private X509TrustManager defaultTrustManager;
+    private X509TrustManager localTrustManager;
+    private X509Certificate[] mAcceptedIssuers;
+
+    public UnifiedTrustManager(KeyStore localKeyStore) throws KeyStoreException, NoSuchAlgorithmException {
+        this.defaultTrustManager = createTrustManager(null);
+        this.localTrustManager = createTrustManager(localKeyStore);
+        X509Certificate[] first = defaultTrustManager.getAcceptedIssuers();
+        X509Certificate[] second = localTrustManager.getAcceptedIssuers();
+        mAcceptedIssuers = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, mAcceptedIssuers, first.length, second.length);
+    }
+
+    private X509TrustManager createTrustManager(KeyStore store) throws NoSuchAlgorithmException, KeyStoreException {
+        String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+        tmf.init(store);
+        TrustManager[] trustManagers = tmf.getTrustManagers();
+        return (X509TrustManager) trustManagers[0];
+    }
+
+    public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        try {
+            localTrustManager.checkServerTrusted(chain, authType);
+        } catch (CertificateException ce) {
+            defaultTrustManager.checkServerTrusted(chain, authType);
+        }
+    }
+    @Override
+    public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        try {
+            localTrustManager.checkClientTrusted(chain, authType);
+        } catch (CertificateException ce) {
+            defaultTrustManager.checkClientTrusted(chain, authType);
+        }
+    }
+    @Override
+    public X509Certificate[] getAcceptedIssuers() {
+        return mAcceptedIssuers;
+    }
+}
