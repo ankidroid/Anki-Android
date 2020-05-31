9d5ec3cd8b1fcec3aedbde4a354b1b8e81609d70
IcaursMoo <Icarus.momo@gmail.com>
2020-05-31 21:25:11 +0800

    Implement a simple Preview AutoPlay function.

--- AnkiDroid/src/main/java/com/ichi2/anki/Previewer.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/Previewer.java
@@ -19,10 +19,22 @@
 package com.ichi2.anki;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Layout;
 import android.view.View;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.ichi2.libanki.Card;
 import com.ichi2.libanki.Collection;
 import com.ichi2.themes.Themes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Collections;
 
 import timber.log.Timber;
 
@@ -31,10 +43,38 @@ import timber.log.Timber;
  * to begin showing them. Special rules are applied if the list size is 1 (i.e., no scrolling
  * buttons will be shown).
  */
+
+/** Implement the AutoPlay function, including basic AutoPlay and shuffle AutoPlay.
+ *
+ * Basic AutoPlay automatically plays the CardList from the beginning to the end once.
+ * Shuffle AutoPlay plays the CardList from the beginning to the end once with a random order.
+ *
+ * By SUSTech Group, 05/31/20.
+ * */
 public class Previewer extends AbstractFlashcardViewer {
     private long[] mCardList;
     private int mIndex;
     private boolean mShowingAnswer;
+
+    /** LinearLayout of Shuffle AutoPlay mode.*/
+    private LinearLayout mShufflePlayLayout;
+
+    /** LinearLayout of Basic AutoPlay mode.*/
+    private LinearLayout mAutoPlayLayout;
+
+    /** Represent the review play model, default is 0.
+     * 0 is Manual mode, 1 is AutoPlay mode, 2 is Shuffle AutoPlay Mode.*/
+    private int mPlayMode;
+
+    /** Flag to represent Manual Mode or Auto Mode*/
+    private boolean mAuto;
+
+    /** Timer for Auto Play mode.*/
+    private Timer mTimer;
+
+    /** Contain the main function code of 2 types of AutoPlay.*/
+    private TimerTask mTimerTask;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +83,12 @@ public class Previewer extends AbstractFlashcardViewer {
 
         mCardList = getIntent().getLongArrayExtra("cardList");
         mIndex = getIntent().getIntExtra("index", -1);
+
+        // Get two LinearLayouts to implement the auto play function.
+        mShufflePlayLayout = findViewById(R.id.shufflePlay_layout);
+        mAutoPlayLayout = findViewById(R.id.autoPlay_layout);
+        // Initial play mode is 0.
+        mPlayMode = 0;
 
         if (savedInstanceState != null){
             mIndex = savedInstanceState.getInt("index", mIndex);
@@ -54,6 +100,7 @@ public class Previewer extends AbstractFlashcardViewer {
             finishWithoutAnimation();
             return;
         }
+
         showBackIcon();
         // Ensure navigation drawer can't be opened. Various actions in the drawer cause crashes.
         disableDrawerSwipe();
@@ -71,6 +118,72 @@ public class Previewer extends AbstractFlashcardViewer {
             displayCardQuestion();
         }
         showBackIcon();
+    }
+
+    /** Using Timer + Handler to implement AutoPlay and Modify the UI.
+     * The Default time of each card is  2s for question, 2s for answer, 4s in total.
+     * */
+    protected void autoPlay(){
+        // The basic autoPlay mode, no shuffle cards.
+        Handler handler = new Handler();
+        mTimer = new Timer();
+
+        if (mPlayMode == 1){
+
+            mTimerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    if (mIndex < mCardList.length-1){
+                        handler.postDelayed(() -> {
+                            if (mShowingAnswer) {
+                                mIndex++;
+                                mCurrentCard = getCol().getCard(mCardList[mIndex]);
+                                displayCardQuestion();
+                            } else {
+                                displayCardAnswer();
+                            }
+                        },1);
+                    }else if (!mShowingAnswer){
+                        handler.postDelayed(()->{
+                            displayCardAnswer();
+                        },1);
+                    }else {
+                        this.cancel();
+                    }
+                }
+            };
+            mTimer.schedule(mTimerTask,2000,2000);
+        }else if (mPlayMode == 2){
+            // Shuffle AutoPlay Mode.
+            List<Integer> shuffle = new ArrayList<>();
+            for (int i=0; i<mCardList.length; i++){
+                shuffle.add(i);
+            }
+            Collections.shuffle(shuffle);
+            mTimerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    if (mIndex < mCardList.length-1){
+                        handler.postDelayed(() -> {
+                            if (mShowingAnswer) {
+                                mIndex++;
+                                mCurrentCard = getCol().getCard(mCardList[shuffle.get(mIndex)]);
+                                displayCardQuestion();
+                            } else {
+                                displayCardAnswer();
+                            }
+                        },1);
+                    }else if (!mShowingAnswer){
+                        handler.postDelayed(()->{
+                            displayCardAnswer();
+                        },1);
+                    }else {
+                        this.cancel();
+                    }
+                }
+            };
+            mTimer.schedule(mTimerTask, 2000,2000);
+        }
     }
 
 
@@ -146,9 +259,61 @@ public class Previewer extends AbstractFlashcardViewer {
         }
     };
 
+    /** Set the ClickListener of mAutoPlay layout.
+     *  Switch to Auto Play mode, if in Manual mode. Otherwise, do not modify the mPlayMode.
+     *  Change flag mAuto each click, which mean a switch a AutoPlay and Manual Play.
+     * */
+    private View.OnClickListener mAutoPlayHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mAuto = !mAuto;
+            if (mPlayMode!=2){
+                mPlayMode = 1;
+            }
+            if (mAuto){
+                mAutoPlayLayout.setBackgroundColor(0xffcfd8dc);
+                autoPlay();
+            }else {
+                mAutoPlayLayout.setBackgroundColor(0xffeceff1);
+                mTimer.cancel();
+                mTimer = null;
+                mTimerTask.cancel();
+                mTimerTask = null;
+            }
+        }
+    };
+
+
+    /**Set the ClickListener of mShuffleAutoPlay layout.
+     * Only change the value of mPlayMode, do not directly invoke autoPlay() function,
+     * change the background color with the click event, to give a feedback.
+     * One click to set Shuffle AutoPlay mode, another click to cancel, like a Switch.
+     * */
+    private View.OnClickListener mShuffleHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (mPlayMode!=2){
+                mShufflePlayLayout.setBackgroundColor(0xffcfd8dc);
+                mPlayMode = 2;
+            }else {
+                mShufflePlayLayout.setBackgroundColor(0xffeceff1);
+                mPlayMode = 1;
+            }
+        }
+    };
+
     private void updateButtonState() {
         // If we are in single-card mode, we show the "Show Answer" button on the question side
         // and hide all the button s on the answer side.
+
+        //Visualize the two layout
+        mShufflePlayLayout.setVisibility(View.VISIBLE);
+        mAutoPlayLayout.setVisibility(View.VISIBLE);
+
+        //Set ClickListener for the Two Layouts.
+        mShufflePlayLayout.setOnClickListener(mShuffleHandler);
+        mAutoPlayLayout.setOnClickListener(mAutoPlayHandler);
+
         if (mCardList.length == 1) {
             if (!mShowingAnswer) {
                 mFlipCardLayout.setVisibility(View.VISIBLE);
