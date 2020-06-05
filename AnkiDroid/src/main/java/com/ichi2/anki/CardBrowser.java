0f22f0c14b2353cefc2a8c107315daa6f9cc979a
Arthur Milchior <Arthur@Milchior.fr>
2020-06-05 12:19:49 +0200

    NF: remove getFlagOrDefault

--- AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.java
@@ -1989,7 +1989,7 @@ public class CardBrowser extends NavigationDrawerActivity implements
          * @return index into TypedArray specifying the background color
          */
         private int getColor(CardCache cardProperties) {
-            int flag = cardProperties.getFlag();
+            int flag = cardProperties.getCard().userFlag();
             switch (flag) {
                 case 1:
                    return R.attr.flagRed;
@@ -2136,11 +2136,6 @@ public class CardBrowser extends NavigationDrawerActivity implements
                 mCard = mCol.getCard(mId);
             }
             return mCard;
-        }
-
-        @VisibleForTesting
-        int getFlag() {
-            return getCard().userFlag();
         }
     }
 
