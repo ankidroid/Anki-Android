a62e43eb02517a716c126fb61b8fa9745b1a6238
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-06-15 13:24:55 +0100

    LINT: getAvailableBytes: CheckResult annotation

--- AnkiDroid/src/main/java/com/ichi2/compat/CompatV18.java
+++ AnkiDroid/src/main/java/com/ichi2/compat/CompatV18.java
@@ -3,9 +3,12 @@ package com.ichi2.compat;
 import android.annotation.TargetApi;
 import android.os.StatFs;
 
+import androidx.annotation.CheckResult;
+
 @TargetApi(18)
 public class CompatV18 extends CompatV17 implements Compat {
 
     @Override
+    @CheckResult
     public long getAvailableBytes(StatFs stat) { return stat.getAvailableBytes(); }
 }
