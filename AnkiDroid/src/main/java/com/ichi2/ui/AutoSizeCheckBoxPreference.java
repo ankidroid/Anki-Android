1b0ea25372922f019c9ec46cf58b72f8cceb0233
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-06-29 09:51:02 +0100

    Stop truncating deck reminder option text
    Fixes 6576

---
+++ AnkiDroid/src/main/java/com/ichi2/ui/AutoSizeCheckBoxPreference.java
@@ -0,0 +1,78 @@
+/*
+ Copyright (c) 2020 David Allison <davidallisongithub@gmail.com>
+
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
+ Foundation; either version 3 of the License, or (at your option) any later
+ version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with
+ this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ichi2.ui;
+
+import android.content.Context;
+import android.os.Build;
+import android.preference.CheckBoxPreference;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ichi2.utils.ViewGroupUtils;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+// extending androidx.preference didn't work:
+// java.lang.ClassCastException: com.ichi2.ui.AutoSizeCheckBoxPreference cannot be cast to android.preference.Preference
+public class AutoSizeCheckBoxPreference extends CheckBoxPreference {
+    @SuppressWarnings("unused")
+    public AutoSizeCheckBoxPreference(Context context) {
+        super(context);
+    }
+
+    @SuppressWarnings("unused")
+    public AutoSizeCheckBoxPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    @SuppressWarnings("unused")
+    public AutoSizeCheckBoxPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @SuppressWarnings("unused")
+    public AutoSizeCheckBoxPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+    @Override
+    protected void onBindView(@NonNull View view) {
+        makeMultiline(view);
+        super.onBindView(view);
+    }
+
+
+    protected void makeMultiline(@NonNull View view) {
+        // https://stackoverflow.com/q/4267939/13121290
+        if (view instanceof ViewGroup) {
+            for (View child : ViewGroupUtils.getAllChildren((ViewGroup) view)) {
+                makeMultiline(child);
+            }
+        } else if (view instanceof TextView) {
+            TextView t = (TextView) view;
+            t.setSingleLine(false);
+            t.setEllipsize(null);
+        }
+    }
+}
