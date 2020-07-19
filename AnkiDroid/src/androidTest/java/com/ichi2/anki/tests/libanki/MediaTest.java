da4522044dfb5283d99df9e9861cf77d16f2ee15
Arthur Milchior <Arthur@Milchior.fr>
2020-07-19 05:54:21 +0200

    NF: longList and stringList
    In order to make code simpler to read

--- AnkiDroid/src/androidTest/java/com/ichi2/anki/tests/libanki/MediaTest.java
+++ AnkiDroid/src/androidTest/java/com/ichi2/anki/tests/libanki/MediaTest.java
@@ -183,12 +183,11 @@ public class MediaTest {
     }
 
     private List<String> added(Collection d) {
-
-        return d.getMedia().getDb().list(String.class, "select fname from media where csum is not null");
+        return d.getMedia().getDb().queryStringList("select fname from media where csum is not null");
     }
 
     private List<String> removed(Collection d) {
-        return d.getMedia().getDb().list(String.class, "select fname from media where csum is null");
+        return d.getMedia().getDb().queryStringList("select fname from media where csum is null");
     }
 
     @Test
