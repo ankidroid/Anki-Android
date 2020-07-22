6e73c9ef1fa641e560f0dd2034e450268d3fe140
Colin P. Hill <colin.patrick.hill@gmail.com>
2020-07-22 08:13:11 -0400

    Removed recursive template processing. This brings the behavior closer to Anki Desktop.

--- AnkiDroid/src/main/java/com/ichi2/libanki/template/Template.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/template/Template.java
@@ -160,33 +160,16 @@ public class Template {
     }
 
     /**
-     * Expands all tags, iteratively until all tags (even tags that are replaced by tags) are resolved.
-     */
-    private @NonNull String render_tags(@NonNull String template, @NonNull Map<String, String> context) {
-        /* Apply render_some_tags to the tags, until
-           render_some_tags states that it does not find tags to replace anymore
-           anymore. Return the last template state */
-        String previous_template = null;
-        while (template != null) {
-            previous_template = template;
-            template = render_some_tags(template, context);
-        }
-        return previous_template;
-    }
-
-    /**
      * Replaces all the tags in a template in a single pass for the values in the given context map.
      */
-    private @Nullable String render_some_tags(@NonNull String template, @NonNull Map<String, String> context) {
+    private @NonNull String render_tags(@NonNull String template, @NonNull Map<String, String> context) {
         String ALT_HANDLEBAR_DIRECTIVE = "{{=<% %>=}}";
         if (template.contains(ALT_HANDLEBAR_DIRECTIVE)) {
             template = template.replace(ALT_HANDLEBAR_DIRECTIVE, "").replace("<%", "{{").replace("%>", "}}");
         }
         StringBuffer sb = new StringBuffer();
         Matcher match = sTag_re.matcher(template);
-        boolean found = false;
         while (match.find()) {
-            found = true;
             String tag_type = match.group(1);
             String tag_name = match.group(2).trim();
             String replacement;
@@ -200,9 +183,6 @@ public class Template {
                 return "{{invalid template}}";
             }
             match.appendReplacement(sb, Matcher.quoteReplacement(replacement));
-        }
-        if (!found) {
-            return null;
         }
         match.appendTail(sb);
         return sb.toString();
