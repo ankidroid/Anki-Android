32389da528b76db38d1296778c4d5ef5c05336c9
Arthur Milchior <Arthur@Milchior.fr>
2020-08-07 00:10:50 +0200

    NF: remove upstream duplicate

--- AnkiDroid/src/main/java/com/ichi2/libanki/sched/SchedV2.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/sched/SchedV2.java
@@ -102,7 +102,7 @@ public class SchedV2 extends AbstractSched {
     // When an action is undone, reset counts need to take the card into account
     private Card mUndidCard = null;
 
-    private double[] mEtaCache = new double[] { -1, -1, -1, -1, -1, -1 };
+    protected double[] mEtaCache = new double[] { -1, -1, -1, -1, -1, -1 };
 
     // Queues
     protected final LinkedList<Long> mNewQueue = new LinkedList<>();
@@ -1006,6 +1006,7 @@ public class SchedV2 extends AbstractSched {
     }
 
 
+    // Overidden: no _maybeResetLrn in V1
     protected Card _getLrnCard(boolean collapse) {
         _maybeResetLrn(collapse && mLrnCount == 0);
         if (_fillLrn()) {
@@ -1304,7 +1305,7 @@ public class SchedV2 extends AbstractSched {
     }
 
 
-    private int _graduatingIvl(Card card, JSONObject conf, boolean early) {
+    protected int _graduatingIvl(Card card, JSONObject conf, boolean early) {
         return _graduatingIvl(card, conf, early, true);
     }
 
@@ -1342,14 +1343,14 @@ public class SchedV2 extends AbstractSched {
     }
 
 
-    private void _logLrn(Card card, @Consts.BUTTON_TYPE int ease, JSONObject conf, boolean leaving, int type, int lastLeft) {
+    protected void _logLrn(Card card, @Consts.BUTTON_TYPE int ease, JSONObject conf, boolean leaving, @Consts.REVLOG_TYPE int type, int lastLeft) {
         int lastIvl = -(_delayForGrade(conf, lastLeft));
         int ivl = leaving ? card.getIvl() : -(_delayForGrade(conf, card.getLeft()));
         log(card.getId(), mCol.usn(), ease, ivl, lastIvl, card.getFactor(), card.timeTaken(), type);
     }
 
 
-    private void log(long id, int usn, @Consts.BUTTON_TYPE int ease, int ivl, int lastIvl, int factor, int timeTaken, @Consts.REVLOG_TYPE int type) {
+    protected void log(long id, int usn, @Consts.BUTTON_TYPE int ease, int ivl, int lastIvl, int factor, int timeTaken, @Consts.REVLOG_TYPE int type) {
         try {
             mCol.getDb().execute("INSERT INTO revlog VALUES (?,?,?,?,?,?,?,?,?)",
                     mTime.now() * 1000, id, usn, ease, ivl, lastIvl, factor, timeTaken, type);
