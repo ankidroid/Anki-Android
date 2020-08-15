fa357d594bf5fd054c95ce96c0c1439c525b33a2
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-08-15 12:55:27 +0100

    Speed Up Tests: Add NullApplication

---
+++ AnkiDroid/src/test/java/com/ichi2/testutils/NullApplication.java
@@ -0,0 +1,7 @@
+package com.ichi2.testutils;
+
+import android.app.Application;
+
+/** Used when an application class is not required for a test to run */
+public class NullApplication extends Application {
+}
