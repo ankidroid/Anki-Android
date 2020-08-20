523fa6f5b0ef186b40a7cf72fe7a128e299d1d8f
Arthur Milchior <Arthur@Milchior.fr>
2020-08-20 10:16:07 +0200

    NF: test for daily increase of limit
    This tries to deal with #6769 by reproducing the bug I understood and other similar feature
    
    It also shows how propagating limit and daily increase may have strange behaviour that are still what is expected (but
    we can't expect user to understand that without a good knowledge of datastructure)

--- AnkiDroid/src/test/java/com/ichi2/libanki/sched/AbstractSchedTest.java
+++ AnkiDroid/src/test/java/com/ichi2/libanki/sched/AbstractSchedTest.java
@@ -26,6 +26,8 @@ import com.ichi2.libanki.Collection;
 import com.ichi2.libanki.Consts;
 import com.ichi2.libanki.Deck;
 import com.ichi2.libanki.DeckConfig;
+import com.ichi2.libanki.Decks;
+import com.ichi2.libanki.Model;
 import com.ichi2.libanki.Models;
 import com.ichi2.libanki.Note;
 import com.ichi2.utils.JSONArray;
@@ -202,5 +204,103 @@ public class AbstractSchedTest extends RobolectricTest {
 
         Card card = sched.getCard();
         assertNull(card);
+    }
+
+    private class IncreaseToday {
+        private final long aId, bId, cId, dId;
+        private final Decks decks;
+        private final AbstractSched sched;
+
+        public IncreaseToday() {
+            decks = getCol().getDecks();
+            sched = getCol().getSched();
+            aId = decks.id("A");
+            bId = decks.id("A::B");
+            cId = decks.id("A::B::C");
+            dId = decks.id("A::B::D");
+        }
+
+        private void assertNewCountIs(String explanation, long did, int expected) {
+            decks.select(did);
+            sched.resetCounts();
+            assertThat(explanation, sched.counts()[0], is(expected));
+        }
+
+        private void increaseAndAssertNewCountsIs(String explanation, long did, int a, int b, int c, int d) {
+            extendNew(did);
+            assertNewCountsIs(explanation, a, b, c, d);
+        }
+
+        private void assertNewCountsIs(String explanation, int a, int b, int c, int d) {
+            assertNewCountIs(explanation, aId, a);
+            assertNewCountIs(explanation, bId, b);
+            assertNewCountIs(explanation, cId, c);
+            assertNewCountIs(explanation, dId, d);
+        }
+
+        private void extendNew(long did) {
+            decks.select(did);
+            sched.extendLimits(1, 0);
+        }
+
+        public void test() {
+            Models models = getCol().getModels();
+
+            DeckConfig dconf = decks.getConf(1);
+            dconf.getJSONObject("new").put("perDay", 0);
+
+
+            Model model = models.byName("Basic");
+            for (long did : new long[]{cId, dId}) {
+                // The note is added in model's did. So change model's did.
+                model.put("did", did);
+                for (int i = 0; i < 4; i++) {
+                    addNoteUsingBasicModel("foo", "bar");
+                }
+            }
+
+            assertNewCountsIs("All daily limits are 0", 0, 0, 0, 0);
+            increaseAndAssertNewCountsIs("Adding a review in C add it in its parents too", cId, 1, 1, 1, 0);
+            increaseAndAssertNewCountsIs("Adding a review in A add it in its children too", aId, 2, 2, 2, 1);
+            increaseAndAssertNewCountsIs("Adding a review in B add it in its parents and children too", bId,3, 3, 3, 2);
+            increaseAndAssertNewCountsIs("Adding a review in D add it in its parents too", dId, 4, 4, 3, 3);
+            increaseAndAssertNewCountsIs("Adding a review in D add it in its parents too", dId, 5, 5, 3, 4);
+
+            decks.select(cId);
+            sched.reset();
+            for (int i = 0; i < 3; i++) {
+                Card card = sched.getCard();
+                sched.answerCard(card, sched.answerButtons(card));
+            }
+            assertNewCountsIs("All cards from C are reviewed. Still 4 cards to review in D, but only two available because of A's limit.", 2, 2, 0, 2);
+
+            increaseAndAssertNewCountsIs("Increasing the number of card in C increase it in its parents too. This allow for more review in any children, and in particular in D.", cId, 3, 3, 1, 3);
+            // D increase because A's limit changed.
+            // This means that increasing C, which is not related to D, can increase D
+            // This follows upstream but is really counter-intuitive.
+
+
+            increaseAndAssertNewCountsIs("Adding a review in C add it in its parents too, even if c has no more card. This allow one more card in d.", cId, 4, 4, 1, 4);
+            /* I would have expected :
+             increaseAndAssertNewCountsIs("", cId, 3, 3, 1, 3);
+             But it seems that applying "increase c", while not actually increasing c (because there are no more new card)
+             still increases A.
+
+             Upstream, the number of new card to add is limited to the number of cards in the deck not already planned for today.
+             So, to reproduce it, you either need to temporary add a card in A::B::C, increase today limit and delete the card
+             or to do
+```python
+from aqt import mw
+c = mw.col.decks.byName("A::B::C")
+mw.col.sched.extendLimits(1, 0)
+```
+             */
+        }
+    }
+
+    /** Those test may be unintuitive, but they follow upstream as close as possible. */
+    @Test
+    public void increaseToday() {
+        new IncreaseToday().test();
     }
 }
