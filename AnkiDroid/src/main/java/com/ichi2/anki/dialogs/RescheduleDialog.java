2efe95acd283c18da7ac087c1429fcaaa7f42a41
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-09-03 23:59:45 +0100

    NF-LINT: Fix annotations and add copyright

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/RescheduleDialog.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/RescheduleDialog.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright (c) 2020 David Allison <davidallisongithub@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.ichi2.anki.dialogs;
 
 import android.content.res.Resources;
@@ -7,8 +23,8 @@ import com.ichi2.libanki.Card;
 import com.ichi2.libanki.sched.SchedV2;
 import com.ichi2.utils.FunctionalInterfaces.Consumer;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import androidx.annotation.CheckResult;
 
@@ -17,7 +33,7 @@ public class RescheduleDialog extends IntegerDialog {
         super();
     }
 
-    @NotNull
+    @NonNull
     @CheckResult
     public static RescheduleDialog rescheduleSingleCard(Resources resources, Card currentCard,
                                                         Consumer<Integer> consumer) {
@@ -38,7 +54,7 @@ public class RescheduleDialog extends IntegerDialog {
         return rescheduleDialog;
     }
 
-    @NotNull
+    @NonNull
     @CheckResult
     public static RescheduleDialog rescheduleMultipleCards(Resources resources, Consumer<Integer> consumer, int cardCount) {
         RescheduleDialog rescheduleDialog = new RescheduleDialog();
