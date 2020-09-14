3ea1a2f3a158b7128c6a14608f52518d8886084a
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-09-14 16:06:05 +0100

    [BugFix]: Restore backups as writable database
    Sadly, I could not get the test to complete on Windows
    
    Fixes 7083

---
+++ AnkiDroid/src/test/java/com/ichi2/anki/BackupManagerIntegrationTest.java
@@ -0,0 +1,78 @@
+/*
+ *  Copyright (c) 2020 David Allison <davidallisongithub@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ichi2.anki;
+
+import com.ichi2.async.CollectionTask.TASK_TYPE;
+import com.ichi2.async.TaskData;
+import com.ichi2.testutils.AnkiAssert;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.shadows.ShadowStatFs;
+
+import java.io.File;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(AndroidJUnit4.class)
+public class BackupManagerIntegrationTest extends RobolectricTest {
+
+    @Test
+    @Ignore("Fails on line: if (!f.renameTo(new File(colPath))) {")
+    public void restoreBackupLeavesCollectionWritable() throws InterruptedException {
+        getCol();
+        String path = createBackup();
+
+        // Perform a write
+        addNoteUsingBasicModel("Hello", "World");
+
+        waitForTask(TASK_TYPE.IMPORT_REPLACE, new TaskData(path), 1000);
+
+        assertThat("database should be read-write", getCol().getDb().getDatabase().isReadOnly(), is(false));
+        AnkiAssert.assertDoesNotThrow(() -> addNoteUsingBasicModel("Hello", "World"));
+    }
+
+
+    private String createBackup() {
+        int blockCount = 100000;
+        ShadowStatFs.registerStats(new File(getCol().getPath()).getParentFile().getPath(), blockCount, blockCount, blockCount);
+
+        assertThat("Backup should work", BackupManager.performBackupInBackground(getCol().getPath(), getCol().getTime()), is(true));
+
+        return spinUntilBackupExists(1000);
+    }
+
+
+    private String spinUntilBackupExists(int timeoutMs) {
+        long time = System.currentTimeMillis();
+        while (true) {
+            File colFile = new File(getCol().getPath());
+            File[] backups = BackupManager.getBackups(colFile);
+            if (backups.length > 0) {
+                return backups[0].getAbsolutePath();
+            }
+
+            if (System.currentTimeMillis() - time > timeoutMs) {
+                throw new RuntimeException("span for longer than " + timeoutMs);
+            }
+        }
+    }
+}
