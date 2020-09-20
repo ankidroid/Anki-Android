26677de8f1b836af9eda6b97cce5b9b03ee6817d
Arthur Milchior <Arthur@Milchior.fr>
2020-09-20 11:25:07 +0200

    NF: suppres wraning about >>> 32

--- AnkiDroid/src/main/java/com/ichi2/libanki/sched/AbstractDeckTreeNode.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/sched/AbstractDeckTreeNode.java
@@ -142,7 +142,7 @@ public abstract class AbstractDeckTreeNode<T extends AbstractDeckTreeNode<T>> im
     @Override
     public int hashCode() {
         int childrenHash = mChildren == null ? 0 : mChildren.hashCode();
-        return getFullDeckName().hashCode() + (int) (childrenHash ^ (childrenHash >>> 32));
+        return getFullDeckName().hashCode() + childrenHash;
     }
 
 
