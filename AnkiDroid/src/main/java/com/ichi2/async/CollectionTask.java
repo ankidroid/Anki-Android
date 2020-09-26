93675b1a1f3106c19a92a376a875f6f27f24b0e7
tda1009 <turboeighteen@gmail.com>
2020-09-26 10:37:22 +0700

    Add a result dialog after deleting media files
    (cherry picked from commit 646572f5d6c71ecc75d1259711a7e2df95dc0214)

--- AnkiDroid/src/main/java/com/ichi2/async/CollectionTask.java
+++ AnkiDroid/src/main/java/com/ichi2/async/CollectionTask.java
@@ -391,8 +391,7 @@ public class CollectionTask extends BaseAsyncTask<TaskData, TaskData, TaskData> 
                 break;
 
             case DELETE_MEDIA:
-                doInBackgroundDeleteMedia(param);
-                break;
+                return doInBackgroundDeleteMedia(param);
 
             case REBUILD_CRAM:
                 return doInBackgroundRebuildCram();
@@ -1665,14 +1664,15 @@ public class CollectionTask extends BaseAsyncTask<TaskData, TaskData, TaskData> 
         return new TaskData(0, new Object[]{result}, true);
     }
 
-    
-    private void doInBackgroundDeleteMedia(TaskData param) {
+
+    private TaskData doInBackgroundDeleteMedia(TaskData param) {
         Object[] objects = param.getObjArray();
         List<String> unused = (List<String>)objects[0];
         com.ichi2.libanki.Media m = getCol().getMedia();
         for (String fname : unused) {
             m.removeFile(fname);
         }
+        return new TaskData(unused.size());
     }
 
 
