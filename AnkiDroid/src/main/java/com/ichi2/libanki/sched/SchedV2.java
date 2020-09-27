75aa452c06f27fec038ba833fe17f35455a8d04e
Arthur Milchior <Arthur@Milchior.fr>
2020-09-27 08:23:57 +0200

    NF: repair counts of preview deck
    (cherry picked from commit 252b91bab8d38904918a90342129f3c7a005d1cd)

--- AnkiDroid/src/main/java/com/ichi2/libanki/sched/SchedV2.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/sched/SchedV2.java
@@ -2992,7 +2992,10 @@ public class SchedV2 extends AbstractSched {
             mNewCount--;
             break;
         case Consts.QUEUE_TYPE_LRN:
-            mLrnCount --; // -= discardCard.getLeft() / 1000; in sched v1
+        case Consts.QUEUE_TYPE_DAY_LEARN_RELEARN:
+        case Consts.QUEUE_TYPE_PREVIEW:
+            mLrnCount --;
+            // In the case of QUEUE_TYPE_LRN, it is -= discardCard.getLeft() / 1000; in sched v1
             break;
         case Consts.QUEUE_TYPE_REV:
             mRevCount--;
