21722bc21b9bb100e9267bffd7c209d4611ee398
TacoTheDank <SkytkRSfan3895@gmail.com>
2020-10-01 09:55:36 -0400

    Lint: Conditional can be pushed inside branch expression

--- AnkiDroid/src/main/java/com/wildplot/android/rendering/XAxis.java
+++ AnkiDroid/src/main/java/com/wildplot/android/rendering/XAxis.java
@@ -177,7 +177,7 @@ public class XAxis implements Drawable {
 
 
         //arrow
-        float[] arowheadPos = {(plotSheet.getxRange()[1] >= this.end) ? plotSheet.xToGraphic(this.end, field) : plotSheet.xToGraphic(plotSheet.getxRange()[1], field), plotSheet.yToGraphic(yOffset, field)};
+        float[] arowheadPos = {plotSheet.xToGraphic(plotSheet.getxRange()[1] >= this.end ? this.end : plotSheet.getxRange()[1], field), plotSheet.yToGraphic(yOffset, field)};
 
         FontMetricsWrap fm = g.getFontMetrics();
         float fontHeigth = fm.getHeight();
