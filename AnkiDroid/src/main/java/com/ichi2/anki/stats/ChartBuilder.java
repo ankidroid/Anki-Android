e885f7b4aa1fae35052f534307523d8d46c8e316
Jackson Popkin <jackson@donorschoose.org>
2020-10-04 13:17:20 -0400

    Split buried/suspended in statistics view
    Divide the current "Suspended+Buried" stat in the "Card Types" section
    of "Statistics" into separate "Suspended" and "Buried" stats. The latest
    versions of desktop Anki do this.

--- AnkiDroid/src/main/java/com/ichi2/anki/stats/ChartBuilder.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/stats/ChartBuilder.java
@@ -173,26 +173,31 @@ public class ChartBuilder {
         ColorWrap[] colors = {new ColorWrap(Themes.getColorFromAttr(mChartView.getContext(), mColors[0])),
                               new ColorWrap(Themes.getColorFromAttr(mChartView.getContext(), mColors[1])),
                               new ColorWrap(Themes.getColorFromAttr(mChartView.getContext(), mColors[2])),
-                              new ColorWrap(Themes.getColorFromAttr(mChartView.getContext(), mColors[3]))};
+                              new ColorWrap(Themes.getColorFromAttr(mChartView.getContext(), mColors[3])),
+                              new ColorWrap(Themes.getColorFromAttr(mChartView.getContext(), mColors[4]))};
 
         PieChart pieChart = new PieChart(plotSheet, mSeriesList[0], colors);
         pieChart.setName(mChartView.getResources().getString(mValueLabels[0]) + ": " + (int) mSeriesList[0][0]);
         LegendDrawable legendDrawable1 = new LegendDrawable();
         LegendDrawable legendDrawable2 = new LegendDrawable();
         LegendDrawable legendDrawable3 = new LegendDrawable();
+        LegendDrawable legendDrawable4 = new LegendDrawable();
         legendDrawable1.setColor(new ColorWrap(Themes.getColorFromAttr(mChartView.getContext(), mColors[1])));
         legendDrawable2.setColor(new ColorWrap(Themes.getColorFromAttr(mChartView.getContext(), mColors[2])));
         legendDrawable3.setColor(new ColorWrap(Themes.getColorFromAttr(mChartView.getContext(), mColors[3])));
+        legendDrawable4.setColor(new ColorWrap(Themes.getColorFromAttr(mChartView.getContext(), mColors[4])));
 
         legendDrawable1.setName(mChartView.getResources().getString(mValueLabels[1]) + ": " + (int) mSeriesList[0][1]);
         legendDrawable2.setName(mChartView.getResources().getString(mValueLabels[2]) + ": " + (int) mSeriesList[0][2]);
         legendDrawable3.setName(mChartView.getResources().getString(mValueLabels[3]) + ": " + (int) mSeriesList[0][3]);
+        legendDrawable4.setName(mChartView.getResources().getString(mValueLabels[4]) + ": " + (int) mSeriesList[0][4]);
 
         plotSheet.unsetBorder();
         plotSheet.addDrawable(pieChart);
         plotSheet.addDrawable(legendDrawable1);
         plotSheet.addDrawable(legendDrawable2);
         plotSheet.addDrawable(legendDrawable3);
+        plotSheet.addDrawable(legendDrawable4);
 
         return plotSheet;
     }
