a5f29339b4e5865c26c1e1317e5bfd62fe58751c
Mike Hardy <github@mikehardy.net>
2020-10-10 09:46:57 -0500

    Fix AnkiDroidApp.sInstance-is-null exception during dialog inflation
    The crash trace indicated that somehow database dialog fragment inflator
    was called via an Activity.onCreate call chain, and somehow it was before
    Application.onCreate got to the point where sInstance was set.
    
    How? It's only on Huawei phones and they appear to have a separate ActivityThread
    so I'm guessing it's a vendor-specific race. In this case resources have fallback
    access via the Dialog inheritance chain so I implement a fallback
    
    This seems like a major violation of lifecycle order of operations so I'm not
    inclined to dive much deeper but we can avoid a crash and provide a reasonable fallback
    at least

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/AsyncDialogFragment.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/AsyncDialogFragment.java
@@ -5,6 +5,8 @@ import android.os.Message;
 import com.ichi2.anki.AnkiDroidApp;
 import com.ichi2.anki.analytics.AnalyticsDialogFragment;
 
+import timber.log.Timber;
+
 public abstract class AsyncDialogFragment extends AnalyticsDialogFragment {
     /* provide methods for text to show in notification bar when the DialogFragment
        can't be shown due to the host activity being in stopped state.
@@ -19,6 +21,11 @@ public abstract class AsyncDialogFragment extends AnalyticsDialogFragment {
     }
 
     protected Resources res() {
-        return AnkiDroidApp.getAppResources();
+        try {
+            return AnkiDroidApp.getAppResources();
+        } catch (Exception e) {
+            Timber.w(e, "AnkiDroidApp.getAppResources failure. Returning Fragment resources as fallback.");
+            return getResources();
+        }
     }
 } 
