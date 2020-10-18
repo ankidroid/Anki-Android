17a2c1d660ab803136c8cc3686c2c0d9f6eec3af
Arthur Milchior <Arthur@Milchior.fr>
2020-10-18 05:45:59 +0200

    NF: remove duplicate "help"
    Those strings appear in different context but always means the same thing, so probably should get the same translation

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/SyncErrorDialog.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/SyncErrorDialog.java
@@ -163,7 +163,7 @@ public class SyncErrorDialog extends AsyncDialogFragment {
             case DIALOG_SYNC_CORRUPT_COLLECTION: {
                 return
                         builder.positiveText(R.string.dialog_ok)
-                        .neutralText(R.string.sync_corrupt_collection_get_help)
+                        .neutralText(R.string.help)
                         .onNeutral((dialog, which) -> ((AnkiActivity)(requireActivity())).openUrl(Uri.parse(getString(R.string.repair_deck))))
                         .cancelable(false)
                         .show();
