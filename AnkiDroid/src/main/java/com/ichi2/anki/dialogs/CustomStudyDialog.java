d43791c9390f9dfc4453b4d815c1d5648895ba72
bguneys <42909556+bguneys@users.noreply.github.com>
2020-11-14 23:25:50 +0300

    Deck Picker: Convert the dialog to a Toast

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/CustomStudyDialog.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/CustomStudyDialog.java
@@ -439,11 +439,6 @@ public class CustomStudyDialog extends AnalyticsDialogFragment {
             if (cur.getInt("dyn") != 1) {
                 Timber.w("Deck: '%s' was non-dynamic", customStudyDeck);
                 UIUtils.showThemedToast(getActivity(), getString(R.string.custom_study_deck_exists), true);
-/*
-                Toast toast = Toast.makeText(getActivity(), R.string.custom_study_deck_exists, Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 50);
-                toast.show();
-*/
                 return;
             } else {
                 Timber.i("Emptying dynamic deck '%s' for custom study", customStudyDeck);
