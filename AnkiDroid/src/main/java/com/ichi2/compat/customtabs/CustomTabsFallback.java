5f05802735563b64c7e8da6952936335241c6f25
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-11-19 11:24:43 +0000

    Handle open URI failure
    Fixes 7721

--- AnkiDroid/src/main/java/com/ichi2/compat/customtabs/CustomTabsFallback.java
+++ AnkiDroid/src/main/java/com/ichi2/compat/customtabs/CustomTabsFallback.java
@@ -17,13 +17,26 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 
+import com.ichi2.anki.AnkiDroidApp;
+import com.ichi2.anki.R;
+import com.ichi2.anki.UIUtils;
+
 /**
  * A Fallback that opens a Webview when Custom Tabs is not available
  */
 public class CustomTabsFallback implements CustomTabActivityHelper.CustomTabFallback {
     @Override
     public void openUri(Activity activity, Uri uri) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        activity.startActivity(intent);
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            activity.startActivity(intent);
+        } catch (Exception e) {
+            // This can occur if the provider is not exported: #7721
+            // this should not happen as we don't reach here if there's no valid browser.
+            // and I assume an exported intent will take priority over a non-exported intent.
+            // Add an exception report to see if I'm wrong
+            AnkiDroidApp.sendExceptionReport(e, "CustomTabsFallback::openUri");
+            UIUtils.showThemedToast(activity, activity.getString(R.string.web_page_error, uri), false);
+        }
     }
 }
