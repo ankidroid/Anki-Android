ankidroid/Anki-Android

commit 95af4ee94952982fd40ce37765ca76c9e190acbb

NF: avoid computing two stripHTMLMedia when first field is sort field

AnkiDroid/src/main/java/com/ichi2/libanki/Note.java
@@ -129,7 +129,8 @@ public class Note implements Cloneable {
         if (changeUsn) {
             mUsn = mCol.usn();
         }
-        String sfld = Utils.stripHTMLMedia(mFields[mCol.getModels().sortIdx(mModel)]);
+        Pair<String, Long> csumAndStrippedFieldField = Utils.sfieldAndCsum(mFields, getCol().getModels().sortIdx(mModel));
+        String sfld = csumAndStrippedFieldField.first;
         String tags = stringTags();
         String fields = joinedFields();
         if (mod == null && mCol.getDb().queryScalar(
@@ -137,7 +138,7 @@ public class Note implements Cloneable {
                 Long.toString(mId), tags, fields) > 0) {
             return;
         }
-        long csum = Utils.fieldChecksum(mFields[0]);
+        long csum = csumAndStrippedFieldField.second;
         mMod = mod != null ? mod : mCol.getTime().intTime();
         mCol.getDb().execute("insert or replace into notes values (?,?,?,?,?,?,?,?,?,?,?)",
                 mId, mGuId, mMid, mMod, mUsn, tags, fields, sfld, csum, mFlags, mData);
@@ -292,9 +293,10 @@ public class Note implements Cloneable {
         if (val.trim().length() == 0) {
             return DupeOrEmpty.EMPTY;
         }
-        long csum = Utils.fieldChecksum(val);
+        Pair<String, Long> csumAndStrippedFieldField = Utils.sfieldAndCsum(mFields, getCol().getModels().sortIdx(mModel));
+        long csum = csumAndStrippedFieldField.second;
         // find any matching csums and compare
-        String strippedFirstField = Utils.stripHTMLMedia(mFields[0]);
+        String strippedFirstField = csumAndStrippedFieldField.first;
         for (String flds : mCol.getDb().queryStringList(
                 "SELECT flds FROM notes WHERE csum = ? AND id != ? AND mid = ?",
                 csum, (mId), mMid)) {

AnkiDroid/src/main/java/com/ichi2/libanki/Collection.java
@@ -1131,7 +1131,8 @@ public class Collection {
                 // note point to invalid model
                 continue;
             }
-            r.add(new Object[] { Utils.stripHTMLMedia(fields[getModels().sortIdx(model)]), Utils.fieldChecksum(fields[0]), o[0] });
+            Pair<String, Long> csumAndStrippedFieldField = Utils.sfieldAndCsum(fields, getModels().sortIdx(model));
+            r.add(new Object[] {csumAndStrippedFieldField.first, csumAndStrippedFieldField.second, o[0] });
         }
         // apply, relying on calling code to bump usn+mod
         mDb.executeMany("UPDATE notes SET sfld=?, csum=? WHERE id=?", r);

AnkiDroid/src/main/java/com/ichi2/libanki/Utils.java
@@ -31,6 +31,7 @@ import android.text.Spanned;
 
 import androidx.annotation.NonNull;
 import android.os.StatFs;
+import android.util.Pair;
 
 import com.ichi2.anki.AnkiFont;
 import com.ichi2.anki.CollectionHelper;

commit 4d0485d41b26c0f5af15dbb9eac4c5f77caf68e6

Fix dupeOrEmpty
Broken in 95af4ee94952982fd40ce37765ca76c9e190acbb

IMO, no point in regression testing this

Fixes 7746

AnkiDroid/src/main/java/com/ichi2/libanki/Note.java
@@ -298,7 +298,7 @@ public class Note implements Cloneable {
         if (val.trim().length() == 0) {
             return DupeOrEmpty.EMPTY;
         }
-        Pair<String, Long> csumAndStrippedFieldField = Utils.sfieldAndCsum(mFields, getCol().getModels().sortIdx(mModel));
+        Pair<String, Long> csumAndStrippedFieldField = Utils.sfieldAndCsum(mFields, 0);
         long csum = csumAndStrippedFieldField.second;
         // find any matching csums and compare
         String strippedFirstField = csumAndStrippedFieldField.first;