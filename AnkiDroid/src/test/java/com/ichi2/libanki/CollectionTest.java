ankidroid/Anki-Android

commit 554ce84211efc17b8503acf45be600497f873d47

NF: avoid multiple conversions to Long

AnkiDroid/src/main/java/com/ichi2/libanki/Collection.java
@@ -754,11 +754,11 @@ public class Collection {
                     Timber.v("Empty card cancelled");
                     return null;
                 }
-                long id = cur.getLong(0);
-                long nid = cur.getLong(1);
-                int ord = cur.getInt(2);
-                long did = cur.getLong(3);
-                long due = cur.getLong(4);
+                Long id = cur.getLong(0);
+                Long nid = cur.getLong(1);
+                Integer ord = cur.getInt(2);
+                Long did = cur.getLong(3);
+                Long due = cur.getLong(4);
                 @Consts.CARD_TYPE int type = cur.getInt(5);
 
                 // existing cards
@@ -799,7 +799,7 @@ public class Collection {
                     Timber.v("Empty card cancelled");
                     return null;
                 }
-                long nid = cur.getLong(0);
+                Long nid = cur.getLong(0);
                 long mid = cur.getLong(1);
                 String flds = cur.getString(2);
                 Model model = getModels().get(mid);
@@ -809,11 +809,11 @@ public class Collection {
                 }
                 Long did = dids.get(nid);
                 // use sibling due if there is one, else use a new id
-                long due;
+                Long due;
                 if (dues.containsKey(nid)) {
                     due = dues.get(nid);
                 } else {
-                    due = nextID("pos");
+                    due = (long) nextID("pos");
                 }
                 if (did == null || did == 0L) {
                     did = model.getLong("did");

commit 85750c64d469ae3adb261e1a06be9d91a8226381

BugFix: New Cloze Cards go to correct deck
This was broken in 554ce84211efc17b8503acf45be600497f873d47 as we moved to
Long comparison, rather than long comparison

This meant that newly generated cards (mostly cloze cards) went to the default
deck of the model, even if all the cards were in the same deck.

Fixes 7781

AnkiDroid/src/main/java/com/ichi2/libanki/Collection.java
@@ -809,7 +809,7 @@ public class Collection {
                 have.get(nid).put(ord, id);
                 // and their dids
                 if (dids.containsKey(nid)) {
-                    if (dids.get(nid) != 0 && dids.get(nid) != did) {
+                    if (dids.get(nid) != 0 && !Utils.equals(dids.get(nid), did)) {
                         // cards are in two or more different decks; revert to model default
                         dids.put(nid, 0L);
                     }

AnkiDroid/src/test/java/com/ichi2/libanki/CollectionTest.java
@@ -3,20 +3,18 @@ package com.ichi2.libanki;
 import com.ichi2.anki.RobolectricTest;
 import com.ichi2.utils.JSONObject;
 
-import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
-import static org.junit.Assert.assertArrayEquals;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
@@ -24,6 +22,32 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class CollectionTest extends RobolectricTest {
+
+    @Test
+    public void editClozeGenerateCardsInSameDeck() {
+        // #7781
+        // Technically, editing a card with conditional fields can also cause this, but cloze cards are much more common
+
+        Note n = addNoteUsingModelName("Cloze", "{{c1::Hello}} {{c2::World}}", "Extra");
+
+        long did = addDeck("Testing");
+
+        for (Card c : n.cards()) {
+            c.setDid(did);
+            c.flush();
+        }
+
+        assertThat("two cloze notes should be generated", n.numberOfCards(), is(2));
+
+        // create card 3
+        n.setField(0, n.getFields()[0] + "{{c3::third}}");
+
+        n.flush();
+
+        assertThat("A new card should be generated", n.numberOfCards(), is(3));
+
+        assertThat("The new card should have the same did as the previous cards", n.cards().get(2).getDid(), is(did));
+    }
 
     /*******************
      ** autogenerated from https://github.com/ankitects/anki/blob/2c73dcb2e547c44d9e02c20a00f3c52419dc277b/pylib/tests/test_cards.py *