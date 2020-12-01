418b9b160f15bdc075ed8e430cd31031c8e175ab
Arthur Milchior <Arthur@Milchior.fr>
2020-12-01 14:43:58 +0100

    NF: uniqifyNote return Pair

--- AnkiDroid/src/main/java/com/ichi2/libanki/importer/Anki2Importer.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/importer/Anki2Importer.java
@@ -54,6 +54,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import androidx.annotation.NonNull;
+import it.unimi.dsi.fastutil.booleans.BooleanLongImmutablePair;
 import timber.log.Timber;
 
 import com.ichi2.async.TaskData;
@@ -252,9 +253,9 @@ public class Anki2Importer extends Importer {
                 int flag = cur.getInt(8);
                 String data = cur.getString(9);
 
-                Pair<Boolean, Long> shouldAddAndNewMid = _uniquifyNote(guid, mid);
-                boolean shouldAdd = shouldAddAndNewMid.first;
-                mid = shouldAddAndNewMid.second;
+                BooleanLongImmutablePair shouldAddAndNewMid = _uniquifyNote(guid, mid);
+                boolean shouldAdd = shouldAddAndNewMid.firstBoolean();
+                mid = shouldAddAndNewMid.secondLong();
                 if (shouldAdd) {
                     // ensure nid is unique
                     while (existing.contains(nid)) {
@@ -369,19 +370,19 @@ public class Anki2Importer extends Importer {
 
     // determine if note is a duplicate, and adjust mid and/or guid as required
     // returns true if note should be added and its mid
-    private Pair<Boolean, Long> _uniquifyNote(@NonNull String origGuid, long srcMid) {
+    private BooleanLongImmutablePair _uniquifyNote(@NonNull String origGuid, long srcMid) {
         long dstMid = _mid(srcMid);
         // duplicate Schemas?
         if (srcMid == dstMid) {
-            return new Pair<>(!mNotes.containsKey(origGuid), srcMid);
+            return new BooleanLongImmutablePair(!mNotes.containsKey(origGuid), srcMid);
         }
         // differing schemas and note doesn't exist?
         if (!mNotes.containsKey(origGuid)) {
-            return new Pair<>(true, dstMid);
+            return new BooleanLongImmutablePair(true, dstMid);
         }
 		// schema changed; don't import
 		mIgnoredGuids.put(origGuid, true);
-		return new Pair<>(false, dstMid);
+		return new BooleanLongImmutablePair(false, dstMid);
     }
 
     /*
