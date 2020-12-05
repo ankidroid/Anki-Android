e5da78e94d5a4671aa4c31e90595662a25238a4f
Arthur Milchior <Arthur@Milchior.fr>
2020-12-05 13:49:23 +0100

    NF: replace JSONObject by deck where possible

--- AnkiDroid/src/main/java/com/ichi2/libanki/sched/DeckDueTreeNode.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/sched/DeckDueTreeNode.java
@@ -1,6 +1,7 @@
 package com.ichi2.libanki.sched;
 
 import com.ichi2.libanki.Collection;
+import com.ichi2.libanki.Deck;
 import com.ichi2.libanki.Decks;
 import com.ichi2.utils.JSONObject;
 
@@ -71,7 +72,7 @@ public class DeckDueTreeNode extends AbstractDeckTreeNode<DeckDueTreeNode> {
         // limit the counts to the deck's limits
         JSONObject conf = getCol().getDecks().confForDid(getDid());
         if (conf.getInt("dyn") == 0) {
-            JSONObject deck = getCol().getDecks().get(getDid());
+            Deck deck = getCol().getDecks().get(getDid());
             limitNewCount(conf.getJSONObject("new").getInt("perDay") - deck.getJSONArray("newToday").getInt(1));
             if (addRev) {
                 limitRevCount(conf.getJSONObject("rev").getInt("perDay") - deck.getJSONArray("revToday").getInt(1));
