f934a26cb1e87981b78e8c245d9aa6adb54f50d5
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-12-27 08:09:02 +0000

    Visual Editor: Selection modes and image deletion

--- AnkiDroid/src/main/assets/visualeditor/visual_editor_functions.js
+++ AnkiDroid/src/main/assets/visualeditor/visual_editor_functions.js
@@ -1,3 +1,5 @@
+/* global RTextEditorView */
+
 /**
 To understand summernote, look at the API surface of document.execCommand:
 https://developer.mozilla.org/en-US/docs/Web/API/document/execCommand#Commands
@@ -33,3 +35,40 @@ var cloze = function(clozeId) {
     insertAtPoint(startPoint, clozePrefix, 0);
     insertAtPoint(endPoint, clozeSuffix, clozePrefix.length);
 };
+
+/**
+https://gist.github.com/jed/982883
+https://gist.github.com/jed/982883#file-license-txt
+*/
+function createGuid(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,createGuid);}
+
+
+/**
+I want to serialise a reference to the currently selected image to avoid race conditions.
+This means we need an ID to pass in
+*/
+function getGuid(target) {
+    var guid =  target.getAttribute("data-ankidroid-guid");
+    if (guid) {
+        return guid;
+    }
+    guid = createGuid();
+    target.setAttribute("data-ankidroid-guid", guid);
+    return guid;
+}
+
+function escape(str) {
+    return str.replace(/[^0-9a-z]/gi, "");
+}
+
+function getTargetByGuid(guid) {
+    return $("*[data-ankidroid-guid='" + escape(guid) + "']").first();
+}
+
+function sendMouseDownToClient(e) {
+    if (e.target.nodeName.toUpperCase() === "IMG") {
+        RTextEditorView.onImageSelection(getGuid(e.target), e.target.src);
+        return;
+    }
+    RTextEditorView.onRegularSelection();
+}
