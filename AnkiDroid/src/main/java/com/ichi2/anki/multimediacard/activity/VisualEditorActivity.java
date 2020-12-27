0b0ecbc9aa1df4ca586b26cee427de3816836bbc
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-12-27 07:42:35 +0000

    Visual Editor: Initial Commit
    Contains Basic SummerNote WebView, Bold, Underline, Italic

---
+++ AnkiDroid/src/main/java/com/ichi2/anki/multimediacard/activity/VisualEditorActivity.java
@@ -0,0 +1,276 @@
+package com.ichi2.anki.multimediacard.activity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.ichi2.anki.AnkiActivity;
+import com.ichi2.anki.AnkiDroidApp;
+import com.ichi2.anki.R;
+import com.ichi2.anki.cardviewer.CardAppearance;
+import com.ichi2.anki.UIUtils;
+import com.ichi2.anki.multimediacard.fields.IField;
+import com.ichi2.anki.multimediacard.fields.TextField;
+import com.ichi2.anki.multimediacard.visualeditor.VisualEditorFunctionality;
+import com.ichi2.anki.multimediacard.visualeditor.VisualEditorWebView;
+import com.ichi2.anki.reviewer.ReviewerCustomFonts;
+import com.ichi2.libanki.Collection;
+import com.ichi2.libanki.Utils;
+import com.ichi2.utils.WebViewDebugging;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
+
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+import timber.log.Timber;
+
+import static com.ichi2.anki.multimediacard.visualeditor.VisualEditorFunctionality.*;
+
+//NOTE: Remove formatting on "{{c1::" will cause a failure to detect the cloze deletion, this is the same as Anki.
+public class VisualEditorActivity extends AnkiActivity {
+
+    public static final String EXTRA_FIELD = "visual.card.ed.extra.current.field";
+    public static final String EXTRA_FIELD_INDEX = "visual.card.ed.extra.current.field.index";
+    /** The Id of the current model (long) */
+    public static final String EXTRA_MODEL_ID = "visual.card.ed.extra.model.id";
+    /** All fields in a (string[])  */
+    public static final String EXTRA_ALL_FIELDS = "visual.card.ed.extra.all.fields";
+
+    private String mCurrentText;
+    private int mIndex;
+    private VisualEditorWebView mWebView;
+    private long mModelId;
+    private String[] mFields;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.visual_editor);
+
+        if (!setFieldsOnStartup()) {
+            failStartingVisualEditor();
+            return;
+        }
+
+        setupWebView(mWebView);
+
+        setupEditorScrollbarButtons();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+        
+        startLoadingCollection();
+    }
+
+
+    private void setupEditorScrollbarButtons() {
+        SimpleListenerSetup setupAction = (id, functionality) -> {
+            String jsName = mWebView.getJsFunctionName(functionality);
+            if (jsName == null) {
+                Timber.d("Skipping functionality: %s", functionality);
+            }
+            View view = findViewById(id);
+            view.setOnClickListener(v -> mWebView.execFunction(jsName));
+        };
+
+        setupAction.apply(R.id.editor_button_bold, BOLD);
+        setupAction.apply(R.id.editor_button_italic, ITALIC);
+        setupAction.apply(R.id.editor_button_underline, UNDERLINE);
+    }
+
+
+    private void setupWebView(VisualEditorWebView webView) {
+        WebViewDebugging.initializeDebugging(AnkiDroidApp.getSharedPrefs(this));
+
+        SharedPreferences preferences = AnkiDroidApp.getSharedPrefs(this);
+        CardAppearance cardAppearance = CardAppearance.create(new ReviewerCustomFonts(this), preferences);
+        String css = cardAppearance.getStyle();
+        webView.injectCss(css);
+        webView.setOnTextChangeListener(s -> this.mCurrentText = s);
+
+        webView.setHtml(mCurrentText);
+
+        //Could be better, this is done per card in AbstractFlashCardViewer
+        webView.getSettings().setDefaultFontSize(CardAppearance.calculateDynamicFontSize(mCurrentText));
+    }
+
+
+    private boolean setFieldsOnStartup() {
+        Bundle extras = this.getIntent().getExtras();
+        if (extras == null) {
+            Timber.w("No Extras in Bundle");
+            return false;
+        }
+
+        //TODO: Save past data for later so we can see if we've changed.
+        mCurrentText = (String) extras.getSerializable(VisualEditorActivity.EXTRA_FIELD);
+        Integer index = (Integer) extras.getSerializable(VisualEditorActivity.EXTRA_FIELD_INDEX);
+
+        this.mFields = (String[]) extras.getSerializable(VisualEditorActivity.EXTRA_ALL_FIELDS);
+        Long modelId = (Long) extras.getSerializable(VisualEditorActivity.EXTRA_MODEL_ID);
+
+        if (mCurrentText == null) {
+            Timber.w("Failed to find mField");
+            return false;
+        }
+        if (index == null) {
+            Timber.w("Failed to find index");
+            return false;
+        }
+        if (mFields == null) {
+            return false;
+        }
+        if (modelId == null) {
+            return false;
+        }
+
+        this.mModelId = modelId;
+
+        mIndex = index;
+
+        mWebView = this.findViewById(R.id.editor_view);
+
+        if (mWebView == null) {
+            Timber.w("Failed to find WebView");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void failStartingVisualEditor() {
+        UIUtils.showThemedToast(this, "Unable to start visual editor", false);
+        finishCancel();
+    }
+
+    @Override
+    protected void onCollectionLoaded(Collection col) {
+        super.onCollectionLoaded(col);
+        Timber.d("onCollectionLoaded");
+        initWebView(col);
+
+        String css = getModelCss(col);
+        mWebView.injectCss(css);
+    }
+
+
+    private String getModelCss(Collection col) {
+        try {
+            String css = col.getModels().get(mModelId).getString("css");
+            return css.replace(".card", ".note-editable ");
+        } catch (Exception e) {
+            UIUtils.showThemedToast(this, "Failed to load template CSS", false);
+            return getDefaultCss();
+        }
+    }
+
+    private String getDefaultCss() {
+        return ".note-editable {\n"
+                + " font-family: arial;\n"
+                + " font-size: 20px;\n"
+                + " text-align: center;\n"
+                + " color: black;\n"
+                + " background-color: white;\n }";
+    }
+
+
+    private void initWebView(Collection col) {
+        String mBaseUrl = Utils.getBaseUrl(col.getMedia().dir());
+        InputStream is = getInputStream();
+        try {
+            byte[] inputData = readFile(is);
+            String asString = new String(inputData, "UTF-8");
+            mWebView.init(asString, mBaseUrl);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @NonNull
+    private InputStream getInputStream() {
+        InputStream is;
+        try {
+            is = getAssets().open("visualeditor/visual_editor.html");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return is;
+    }
+
+
+    private byte[] readFile(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream buffer = null;
+        try {
+            buffer = new ByteArrayOutputStream();
+
+            int nRead;
+            byte[] data = new byte[16384];
+
+            while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+                buffer.write(data, 0, nRead);
+            }
+
+            return buffer.toByteArray();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (buffer != null) {
+                buffer.close();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // NOTE: This is called every time a new question is shown via invalidate options menu
+        getMenuInflater().inflate(R.menu.visual_editor, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_save) {
+            Timber.i("Save button pressed");
+            finishWithSuccess();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void finishWithSuccess() {
+        IField f = new TextField();
+        f.setText(mCurrentText);
+        Intent resultData = new Intent();
+        resultData.putExtra(MultimediaEditFieldActivity.EXTRA_RESULT_FIELD, f);
+        resultData.putExtra(MultimediaEditFieldActivity.EXTRA_RESULT_FIELD_INDEX, this.mIndex);
+        setResult(RESULT_OK, resultData);
+        finishActivityWithFade(this);
+    }
+
+    private void finishCancel() {
+        setResult(RESULT_CANCELED);
+        finishActivityWithFade(this);
+    }
+
+
+    @FunctionalInterface
+    protected interface SimpleListenerSetup {
+        void apply(@IdRes int buttonId, VisualEditorFunctionality function);
+    }
+}
