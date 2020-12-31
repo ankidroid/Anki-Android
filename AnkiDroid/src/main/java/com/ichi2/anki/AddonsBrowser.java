a11d93d86b1dcced640d843c8adf1e0c903ed5be
infinyte7 <manikant.gautam.712@gmail.com>
2020-12-31 20:53:53 +0800

    Updated for getting valid addon package file

--- AnkiDroid/src/main/java/com/ichi2/anki/AddonsBrowser.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/AddonsBrowser.java
@@ -19,6 +19,7 @@ import android.widget.EditText;
 import com.ichi2.anki.widgets.DeckDropDownAdapter;
 import com.ichi2.themes.Themes;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.rauschig.jarchivelib.Archiver;
@@ -45,6 +46,7 @@ public class AddonsBrowser extends NavigationDrawerActivity implements DeckDropD
     @Nullable
     private Menu mActionBarMenu;
     private MenuItem mInstallAddon;
+    private MenuItem mGetMoreAddons;
 
     private long queueId;
     DownloadManager downloadManager;
@@ -52,6 +54,10 @@ public class AddonsBrowser extends NavigationDrawerActivity implements DeckDropD
     private String currentAnkiDroidDirectory;
     private File addonsHomeDir;
     private Dialog downloadDialog;
+
+    // Update if api get updated
+    private String AnkiDroidJsAPI = "0.0.1";
+    private String AnkiDroidJsAddonKeywords = "ankidroid-js-addon";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,14 +106,15 @@ public class AddonsBrowser extends NavigationDrawerActivity implements DeckDropD
         mActionBarMenu = menu;
         getMenuInflater().inflate(R.menu.addon_browser, menu);
         mInstallAddon = menu.findItem(R.id.action_install_addon);
-        mInstallAddon.setOnMenuItemClickListener(item -> {
+        mGetMoreAddons = menu.findItem(R.id.action_get_more_addons);
 
+        mInstallAddon.setOnMenuItemClickListener(item -> {
 
             downloadDialog.setCanceledOnTouchOutside(true);
             downloadDialog.setContentView(R.layout.addon_install_from_npm);
 
-            EditText downloadEditText = (EditText) downloadDialog.findViewById(R.id.addon_download_edit_text);
-            Button downloadButton = (Button) downloadDialog.findViewById(R.id.addon_download_button);
+            EditText downloadEditText = downloadDialog.findViewById(R.id.addon_download_edit_text);
+            Button downloadButton = downloadDialog.findViewById(R.id.addon_download_button);
 
             downloadButton.setOnClickListener(v->{
 
@@ -135,10 +142,23 @@ public class AddonsBrowser extends NavigationDrawerActivity implements DeckDropD
             downloadDialog.show();
             return true;
         });
+
+        mGetMoreAddons.setOnMenuItemClickListener(item -> {
+            String baseUrl = "https://www.npmjs.com/search?q=keywords:";
+            String url = baseUrl + AnkiDroidJsAddonKeywords;
+            Uri uri = Uri.parse(url);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+            return true;
+        });
+
         return super.onCreateOptionsMenu(menu);
     }
 
-
+    /* list addons with valid packge.json, i.e contains ankidroid_js_api = 0.0.1, keywords='ankidroid-js-addon'
+       and non empty string.
+       Then that addon will available for enable/disable
+     */
     public void listAddonsFromDir() {
         currentAnkiDroidDirectory = CollectionHelper.getCurrentAnkiDroidDirectory(this);
         addonsHomeDir = new File(currentAnkiDroidDirectory, "addons" );
@@ -153,23 +173,22 @@ public class AddonsBrowser extends NavigationDrawerActivity implements DeckDropD
         if (success) {
             File directory = new File(String.valueOf(addonsHomeDir));
             File[] files = directory.listFiles();
-            for (int i = 0; i < files.length; i++) {
-                Timber.d("Addons:%s", files[i].getName());
+            for (File file : files) {
+                Timber.d("Addons:%s", file.getName());
 
                 // Read package.json from
                 // AnkiDroid/addons/ankidroid-addon-../package/package.json
-                File addonsPackageDir = new File(files[i], "package" );
+                File addonsPackageDir = new File(file, "package");
                 File addonsPackageJson = new File(addonsPackageDir, "package.json");
 
                 AddonModel addonModel;
+                if (isValidAddonPackage(addonsPackageJson)) {
 
-                if (addonsPackageJson.exists()) {
-                    JSONObject jsonObject  = packageJsonReader(addonsPackageJson);
-
+                    JSONObject jsonObject = packageJsonReader(addonsPackageJson);
                     String addonName = jsonObject.optString("name", "");
                     String addonVersion = jsonObject.optString("version", "");
                     String addonDev = jsonObject.optString("author", "");
-                    String addonAnkiDroidAPI = jsonObject.optString("ankidroid_api", "");
+                    String addonAnkiDroidAPI = jsonObject.optString("ankidroid_js_api", "");
                     String addonHomepage = jsonObject.optString("homepage", "");
 
                     addonModel = new AddonModel(addonName, addonVersion, addonDev, addonAnkiDroidAPI, addonHomepage);
@@ -184,15 +203,11 @@ public class AddonsBrowser extends NavigationDrawerActivity implements DeckDropD
         }
     }
 
+    // download package.json and addon.tgz file from url
     public void downloadFileFromUrl(String url, String fileName) {
         downloadManager = (DownloadManager)getSystemService(DOWNLOAD_SERVICE);
         DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
         request.setTitle(fileName);
-
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-//            request.allowScanningByMediaScanner();
-//            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-//        }
 
         File downloadFile = new File(Environment.DIRECTORY_DOWNLOADS + fileName);
 
@@ -229,8 +244,13 @@ public class AddonsBrowser extends NavigationDrawerActivity implements DeckDropD
                         String tarballLink;
 
                         if (uri.endsWith(".json")) {
+
                             tarballLink = getNpmTarball(uri);
-                            downloadFileFromUrl(tarballLink, uri.replace(".json", "") + ".tgz");
+                            // tarball contains valid url for download
+                            if (!tarballLink.isEmpty()) {
+                                downloadFileFromUrl(tarballLink, uri.replace(".json", "") + ".tgz");
+                            }
+
                         } else if (uri.endsWith(".tgz")) {
                             extractTarball(uri);
                         }
@@ -239,18 +259,20 @@ public class AddonsBrowser extends NavigationDrawerActivity implements DeckDropD
                     }
                 }
 
+            } else {
+                UIUtils.showThemedToast(context, getString(R.string.error_downloading_file), false);
             }
         }
     };
 
-
+    // extract ankidroid-js-addon....tgz file and copy it AnkiDroid/addons folder
     private void extractTarball(String uri) {
         File addonsDir = new File(addonsHomeDir, uri.replace(".tgz", ""));
 
         String tarballPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/" + Uri.parse(uri).getPath();
         File tarballFile = new File(tarballPath);
 
-        // https://github.com/thrau/jarchivelib
+        // extracting using library https://github.com/thrau/jarchivelib
         try {
             Archiver archiver = ArchiverFactory.createArchiver(new File(tarballPath));
             archiver.extract(tarballFile, addonsDir);
@@ -269,12 +291,12 @@ public class AddonsBrowser extends NavigationDrawerActivity implements DeckDropD
         }
     }
 
-
+    // get tarball info from package.json of ankidroid-js-addon...
     private String getNpmTarball(String uri) {
         String tarballUrl = "";
         File packageJsonFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), Uri.parse(uri).getPath());
 
-        if (packageJsonFile.exists()) {
+        if (isValidAddonPackage(packageJsonFile)) {
             try {
                 JSONObject jsonObject  = packageJsonReader(packageJsonFile);
                 JSONObject dist = jsonObject.getJSONObject("dist");
@@ -288,12 +310,12 @@ public class AddonsBrowser extends NavigationDrawerActivity implements DeckDropD
             } catch (JSONException e) {
                 Timber.e(e.getLocalizedMessage());
             }
-
         }
 
         return tarballUrl;
     }
-    
+
+    // read package.json file of ankidroid-js-addon...
     public static JSONObject packageJsonReader(File addonsFiles) {
         JSONObject jsonObject = null;
         try {
@@ -314,5 +336,44 @@ public class AddonsBrowser extends NavigationDrawerActivity implements DeckDropD
             Timber.e(e.getLocalizedMessage());
         }
         return jsonObject;
+    }
+
+    // is package.json of ankidroid-js-addon... contains valid ankidroid_js_api==0.0.1 and keywords 'ankidroid-js-addon'
+    public boolean isValidAddonPackage(File addonsPackageJson) {
+
+        if (addonsPackageJson.exists()) {
+
+            JSONObject jsonObject  = packageJsonReader(addonsPackageJson);
+
+            String addonName = jsonObject.optString("name", "");
+            String addonVersion = jsonObject.optString("version", "");
+            String addonDev = jsonObject.optString("author", "");
+            String addonAnkiDroidAPI = jsonObject.optString("ankidroid_js_api", "");
+            String addonHomepage = jsonObject.optString("homepage", "");
+            boolean jsAddonKeywordsPresent = false;
+
+            try {
+                JSONArray keywords = jsonObject.getJSONArray("keywords");
+                for (int j = 0; j < keywords.length(); j++) {
+                    String addonKeyword = keywords.getString(j);
+                    if (addonKeyword.equals(AnkiDroidJsAddonKeywords)) {
+                        jsAddonKeywordsPresent = true;
+                        break;
+                    }
+                }
+                Timber.d("keywords %s", keywords.toString());
+            } catch (JSONException e) {
+                Timber.e(e.getLocalizedMessage());
+            }
+
+            // if ankidroid_js_api == 0.0.1 (current js api version) and package.json contains ankidroid-js-addon keywords
+            if (addonAnkiDroidAPI.equals(AnkiDroidJsAPI) && jsAddonKeywordsPresent) {
+                // if other strings are non empty
+                if (!addonName.isEmpty() && !addonVersion.isEmpty() && !addonDev.isEmpty() && !addonHomepage.isEmpty()) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
