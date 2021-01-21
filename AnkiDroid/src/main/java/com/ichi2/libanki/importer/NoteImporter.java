ef04471c6a6d9c3d69634cb5bb3507ff88f3a38e
Arthur Milchior <Arthur@Milchior.fr>
2021-01-21 12:08:33 +0100

    Notes are imported even if they don't generate cards
    This will allow to import buggy deck and correct them. It also follows 2.1.28

--- AnkiDroid/src/main/java/com/ichi2/libanki/importer/NoteImporter.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/importer/NoteImporter.java
@@ -379,7 +379,7 @@ public class NoteImporter extends Importer {
             }
         }
         note.fieldsStr = joinFields(fields);
-        ArrayList<Integer> ords = Models.availOrds(mModel, fields, mTemplateParsed, Models.AllowEmpty.ONLY_CLOZE);
+        ArrayList<Integer> ords = Models.availOrds(mModel, fields, mTemplateParsed, Models.AllowEmpty.TRUE);
         if (ords.isEmpty()) {
             mEmptyNotes = true;
             return false;
