3fff56830cd2bc37173ba1f7e3fa005f494e66f7
Abanoub-Asaad <Abanob2018170001@cis.asu.edu.eg>
2021-03-12 03:26:17 +0200

    Current deck has a different colour
    Fixes #7931

--- AnkiDroid/src/main/java/com/ichi2/anki/NoteEditor.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/NoteEditor.java
@@ -27,6 +27,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -52,6 +53,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewGroup.MarginLayoutParams;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
@@ -559,7 +561,22 @@ public class NoteEditor extends AnkiActivity {
             mAllModelIds.add(m.getLong("id"));
         }
 
-        ArrayAdapter<String> noteTypeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, modelNames);
+        ArrayAdapter<String> noteTypeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, modelNames){
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent){
+                // Cast the drop down items (popup items) as text view
+                TextView tv = (TextView) super.getDropDownView(position,convertView,parent);
+
+                // If this item is selected item
+                if(position == mNoteTypeSpinner.getSelectedItemPosition()){
+                    // Set spinner selected popup item's text color
+                    tv.setBackgroundColor(Color.GRAY);
+                }
+
+                // Return the modified view
+                return tv;
+            }
+        };
         mNoteTypeSpinner.setAdapter(noteTypeAdapter);
         noteTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
@@ -592,7 +609,22 @@ public class NoteEditor extends AnkiActivity {
             mAllDeckIds.add(thisDid);
         }
 
-        ArrayAdapter<String> noteDeckAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, deckNames);
+        ArrayAdapter<String> noteDeckAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, deckNames){
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent){
+                // Cast the drop down items (popup items) as text view
+                TextView tv = (TextView) super.getDropDownView(position,convertView,parent);
+
+                // If this item is selected
+                if(position == mNoteDeckSpinner.getSelectedItemPosition()){
+                    // Set spinner selected popup item's text color
+                    tv.setBackgroundColor(Color.GRAY);
+                }
+
+                // Return the modified view
+                return tv;
+            }
+        };
         mNoteDeckSpinner.setAdapter(noteDeckAdapter);
         noteDeckAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mNoteDeckSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
