187c6855314eb35b716e56a09c7e802bbe8133f3
Akshay <jadhavAkshay0701@gmail.com>
2021-03-14 09:52:21 +0530

    Fixes Bug #8192.

--- AnkiDroid/src/main/java/com/ichi2/anki/MyAccount.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/MyAccount.java
@@ -233,7 +233,7 @@ public class MyAccount extends AnkiActivity {
         public void onPreExecute() {
             Timber.d("loginListener.onPreExecute()");
             if (mProgressDialog == null || !mProgressDialog.isShowing()) {
-                mProgressDialog = StyledProgressDialog.show(MyAccount.this, "",
+                mProgressDialog = StyledProgressDialog.show(MyAccount.this, null,
                         getResources().getString(R.string.alert_logging_message), false);
             }
         }
