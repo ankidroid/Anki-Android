c81382ff94901003470a9df97934f84fa34fab38
ritvij14 <ritvij2001@gmail.com>
2021-03-20 23:04:49 +0530

    removing extra analytics triggers

--- AnkiDroid/src/main/java/com/ichi2/anki/Lookup.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/Lookup.java
@@ -82,13 +82,11 @@ public class Lookup {
         text = text.trim().replaceAll("[,;:\\s(\\[)\\].]*$", "").replaceAll("^[,;:\\s(\\[)\\].]*", "");
         switch (mDictionary) {
             case DICTIONARY_NONE:
-                UsageAnalytics.sendAnalyticsEvent(Lookup.class.getSimpleName(), UsageAnalytics.Actions.NONE);
                 return false;
             case DICTIONARY_AEDICT:
                 Intent aedictSearchIntent = new Intent(mDictionaryAction);
                 aedictSearchIntent.putExtra("kanjis", text);
                 mContext.startActivity(aedictSearchIntent);
-                UsageAnalytics.sendAnalyticsEvent(Lookup.class.getSimpleName(), UsageAnalytics.Actions.AEDICT);
                 return true;
             case DICTIONARY_LEO_WEB:
             case DICTIONARY_LEO_APP:
@@ -116,30 +114,25 @@ public class Lookup {
                             mLookupText = "";
                         })
                         .build().show();
-                UsageAnalytics.sendAnalyticsEvent(Lookup.class.getSimpleName(), UsageAnalytics.Actions.LEO);
                 return true;
             case DICTIONARY_COLORDICT:
                 Intent colordictSearchIntent = new Intent(mDictionaryAction);
                 colordictSearchIntent.putExtra("EXTRA_QUERY", text);
                 mContext.startActivity(colordictSearchIntent);
-                UsageAnalytics.sendAnalyticsEvent(Lookup.class.getSimpleName(), UsageAnalytics.Actions.COLORDICT);
                 return true;
             case DICTIONARY_FORA:
                 Intent foraSearchIntent = new Intent(mDictionaryAction);
                 foraSearchIntent.putExtra("HEADWORD", text.trim());
                 mContext.startActivity(foraSearchIntent);
-                UsageAnalytics.sendAnalyticsEvent(Lookup.class.getSimpleName(), UsageAnalytics.Actions.FORA);
                 return true;
             case DICTIONARY_NCIKU_WEB:
                 Intent ncikuWebIntent = new Intent(mDictionaryAction, Uri.parse("http://m.nciku.com/en/entry/?query="
                         + text));
                 mContext.startActivity(ncikuWebIntent);
-                UsageAnalytics.sendAnalyticsEvent(Lookup.class.getSimpleName(), UsageAnalytics.Actions.NCIKU);
                 return true;
             case DICTIONARY_EIJIRO_WEB:
                 Intent eijiroWebIntent = new Intent(mDictionaryAction, Uri.parse("http://eow.alc.co.jp/" + text));
                 mContext.startActivity(eijiroWebIntent);
-                UsageAnalytics.sendAnalyticsEvent(Lookup.class.getSimpleName(), UsageAnalytics.Actions.EIJIRO);
                 return true;
         }
         return false;
