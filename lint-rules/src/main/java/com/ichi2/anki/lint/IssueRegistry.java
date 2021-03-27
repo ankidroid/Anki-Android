6f4fa37c2003a81bf68d67ad6fee1ff7675c4308
almas33 <ahmadalmas.786.aa@gmail.com>
2021-03-27 18:34:06 +0530

    Lint: Ensure Copyright Header Exists
    This is a common issue in code reviews
    The lint check explains how to fix the issue
    so reduces review load and helps onboarding
    
    Fixes 8359

--- lint-rules/src/main/java/com/ichi2/anki/lint/IssueRegistry.java
+++ lint-rules/src/main/java/com/ichi2/anki/lint/IssueRegistry.java
@@ -3,6 +3,7 @@ package com.ichi2.anki.lint;
 import com.android.tools.lint.detector.api.ApiKt;
 import com.android.tools.lint.detector.api.Issue;
 import com.ichi2.anki.lint.rules.ConstantFieldDetector;
+import com.ichi2.anki.lint.rules.CopyrightHeaderExists;
 import com.ichi2.anki.lint.rules.DirectCalendarInstanceUsage;
 import com.ichi2.anki.lint.rules.DirectSnackbarMakeUsage;
 import com.ichi2.anki.lint.rules.DirectSystemTimeInstantiation;
@@ -28,6 +29,7 @@ public class IssueRegistry extends com.android.tools.lint.client.api.IssueRegist
     public List<Issue> getIssues() {
         // Keep this list lexicographically ordered.
         List<Issue> issues = new ArrayList<>();
+        issues.add(CopyrightHeaderExists.ISSUE);
         issues.add(DirectCalendarInstanceUsage.ISSUE);
         issues.add(DirectDateInstantiation.ISSUE);
         issues.add(DirectGregorianInstantiation.ISSUE);
