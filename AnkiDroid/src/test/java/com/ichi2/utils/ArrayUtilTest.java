936701c090556ce38bdbaaef3336838af6f6ede9
mrudultora <mrudultora@gmail.com>
2021-03-28 22:57:55 +0530

    Unit Test for ArrayUtil

---
+++ AnkiDroid/src/test/java/com/ichi2/utils/ArrayUtilTest.java
@@ -0,0 +1,50 @@
+/*
+ Copyright (c) 2021 Mrudul Tora <mrudultora@gmail.com>
+
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
+ Foundation; either version 3 of the License, or (at your option) any later
+ version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with
+ this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ichi2.utils;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import static com.ichi2.utils.ArrayUtil.toArrayList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(AndroidJUnit4.class)
+public class ArrayUtilTest {
+    private static Integer[] sampleItems;
+
+
+    @Before
+    public void initializeArray() {
+        sampleItems = new Integer[] {1, 2, 3, 4, 5, 6};
+    }
+
+
+    @Test
+    public void arrayToArrayList() {
+        List<Integer> list = new ArrayList<>();
+        Collections.addAll(list, sampleItems);
+        assertThat(toArrayList(sampleItems), is(list));
+    }
+}
