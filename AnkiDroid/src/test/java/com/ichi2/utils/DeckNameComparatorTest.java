7a80a7e11fb5585c3290f9a58a21b877a9c4665b
AyaanJaved <80831951+AyaanJaved@users.noreply.github.com>
2021-03-30 09:42:30 +0530

    Created Unit Test for DeckNameComparator.java
    Added Unit Testing for DeckNameComparator class.

---
+++ AnkiDroid/src/test/java/com/ichi2/utils/DeckNameComparatorTest.java
@@ -0,0 +1,34 @@
+package com.ichi2.utils;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class DeckNameComparatorTest {
+    DeckNameComparator deckNameComparator;
+
+    @Before
+    public void setUp(){
+        deckNameComparator = new DeckNameComparator();
+    }
+
+    //Tests the comparision of two strings Lexicographically (Dictionary Order)
+    @Test
+    public void compareLexicographically() {
+       assertThat(deckNameComparator.compare("Aa", "Az"), is(-25));
+    }
+
+    //Tests the comparision of two strings by Length
+    @Test
+    public void compareByLength() {
+        assertThat(deckNameComparator.compare("AnkiDroid", "Anki"), is(5));
+    }
+
+    //Tests the comparision of two strings when they are equal
+    @Test
+    public void compareEqual() {
+        assertThat(deckNameComparator.compare("AnkiDroid", "AnkiDroid"), is(0));
+    }
+}
