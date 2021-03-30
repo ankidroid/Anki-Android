9a0facd0c94379a42235073ae8868aa5fa73cee3
AyaanJaved <80831951+AyaanJaved@users.noreply.github.com>
2021-03-31 00:58:35 +0530

    Updated Unit test for DeckNameComparator.java

--- AnkiDroid/src/test/java/com/ichi2/utils/DeckNameComparatorTest.java
+++ AnkiDroid/src/test/java/com/ichi2/utils/DeckNameComparatorTest.java
@@ -5,30 +5,40 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
 
 public class DeckNameComparatorTest {
     private DeckNameComparator deckNameComparator;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         deckNameComparator = new DeckNameComparator();
     }
 
-    //Tests the comparision of two strings Lexicographically (Dictionary Order)
     @Test
-    public void compareLexicographically() {
-       assertThat(deckNameComparator.compare("Aa", "Az"), is(-25));
+    public void compareLessThan() {
+       assertThat(deckNameComparator.compare("aa", "aa:bb"), is(lessThan(0)));
     }
 
-    //Tests the comparision of two strings by Length
     @Test
-    public void compareByLength() {
-        assertThat(deckNameComparator.compare("AnkiDroid", "Anki"), is(5));
+    public void compareGreaterThan() {
+        assertThat(deckNameComparator.compare("aa:bb", "aa"), is(greaterThan(0)));
     }
 
-    //Tests the comparision of two strings when they are equal
     @Test
-    public void compareEqual() {
-        assertThat(deckNameComparator.compare("AnkiDroid", "AnkiDroid"), is(0));
+    public void compareCaseLessThan() {
+        assertThat(deckNameComparator.compare("ab", "BB"), is(lessThan(0)));
+    }
+
+    @Test
+    public void compareCaseGreaterThan() {
+        assertThat(deckNameComparator.compare("AB", "Aa"), is(greaterThan(0)));
+    }
+
+    //Test to further check implementation of case insensitivity.
+    @Test
+    public void compareCaseInsensitivity() {
+        assertThat(deckNameComparator.compare("a", "A"), is(0));
     }
 }
