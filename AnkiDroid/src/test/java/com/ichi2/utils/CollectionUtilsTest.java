07f3628342409d1c741a614a2762603bb366d2ab
Aditya-Srivastav <54016427+4D17Y4@users.noreply.github.com>
2021-04-01 01:39:55 +0530

    Add unit test for CollectionUtils (#8402)
    * CollectionUtils test added
    * List matcher
    * Added IndexOutOfBoundsException and ArrayListCheck

---
+++ AnkiDroid/src/test/java/com/ichi2/utils/CollectionUtilsTest.java
@@ -0,0 +1,54 @@
+/*
+ *  Copyright (c) 2021 Aditya Srivastav <iamaditya2009@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ichi2.utils;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.ichi2.testutils.AnkiAssert.assertEqualsArrayList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class CollectionUtilsTest {
+
+    List<Integer> testList = new ArrayList<Integer>() {{
+        add(1);
+        add(2);
+        add(3);
+    }};
+
+    @Test
+    public void testGetLastListElement() {
+        assertThat(CollectionUtils.getLastListElement(testList), is(3));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetLastOnEmptyList() {
+        List<Integer> emptyList = new ArrayList<>();
+        CollectionUtils.getLastListElement(emptyList);
+    }
+
+    @Test
+    public void testAddAll() {
+        List<Integer> toTest = new ArrayList<>();
+        CollectionUtils.addAll(toTest, testList);
+        assertEqualsArrayList(new Integer [] {1, 2, 3}, toTest);
+    }
+}
