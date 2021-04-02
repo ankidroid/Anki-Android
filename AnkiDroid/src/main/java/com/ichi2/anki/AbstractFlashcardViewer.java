becd003af8b4e6ff0fa389e9a75e7e79b5f7071c
Mohaned hassan <Mohanedhassan1999@gmail.com>
2021-04-02 22:50:50 +0100

    first commit

--- AnkiDroid/src/main/java/com/ichi2/anki/AbstractFlashcardViewer.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/AbstractFlashcardViewer.java
@@ -1338,7 +1338,7 @@ public abstract class AbstractFlashcardViewer extends NavigationDrawerActivity i
             Timber.d("Clipboard has text = %b", clipboardHasText());
             lookUp();
         } else {
-            selectAndCopyText();
+            UIUtils.showThemedToast(AbstractFlashcardViewer.this, getApplicationContext().getString(R.string.dialog_clipboard_empty) , false);
         }
     }
 
