f62de1341a8a4d589405ab490c06427c90b9fa0a
Trung Dang <bill081001@gmail.com>
2021-04-05 14:59:03 -0400

    Handling cancel actions properly and address issues

--- AnkiDroid/src/main/java/com/ichi2/anki/AbstractFlashcardViewer.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/AbstractFlashcardViewer.java
@@ -648,17 +648,16 @@ public abstract class AbstractFlashcardViewer extends NavigationDrawerActivity i
                 int nMins = elapsed.first / 60;
                 String mins = res.getQuantityString(R.plurals.in_minutes, nMins, nMins);
                 String timeboxMessage = res.getQuantityString(R.plurals.timebox_reached, nCards, nCards, mins);
-                // Restarting Timebox in .onPositive is not necessary,
-                // because the following line will restart it (along with restarting after cancelling the dialog box)
                 new MaterialDialog.Builder(AbstractFlashcardViewer.this)
                         .title(res.getString(R.string.timebox_reached_title))
                         .content(timeboxMessage)
                         .positiveText(R.string.dialog_continue)
-                        .negativeText(R.string.dialog_close)
+                        .negativeText(R.string.close)
                         .cancelable(true)
                         .onNegative((materialDialog, dialogAction) -> finishWithAnimation(RIGHT))
+                        .onPositive((materialDialog, dialogAction) -> getCol().startTimebox())
+                        .cancelListener(materialDialog -> getCol().startTimebox())
                         .show();
-                getCol().startTimebox();
             }
         }
 
