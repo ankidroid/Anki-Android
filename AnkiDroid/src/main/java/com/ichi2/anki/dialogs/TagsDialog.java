af97bd25e80cb1cf32c0e4864e447c835060562f
Arthur Milchior <Arthur@Milchior.fr>
2021-04-05 04:07:58 +0200

    NF: TagsList takes a constructor

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/TagsDialog.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/TagsDialog.java
@@ -51,11 +51,23 @@ public class TagsDialog extends AnalyticsDialogFragment {
         /**
          * A Set containing the currently selected tags
          */
-        private TreeSet<String> mCurrentTags;
+        private final @NonNull TreeSet<String> mCurrentTags;
         /**
          * List of all available tags
          */
-        private List<String> mAllTags;
+        private final @NonNull List<String> mAllTags;
+
+
+        /**
+         * Construct a new {@link TagsList}
+         *
+         * @param allTags A list of all available tags
+         * @param currentTags a Set containing the currently selected tags
+         */
+        public TagsList(@NonNull List<String> allTags, @NonNull TreeSet<String> currentTags) {
+            mCurrentTags = currentTags;
+            mAllTags = allTags;
+        }
 
 
         /**
@@ -203,7 +215,7 @@ public class TagsDialog extends AnalyticsDialogFragment {
     private static final String ALL_TAGS_KEY = "all_tags";
 
     private DialogType mType;
-    private TagsList mTags = new TagsList();
+    private TagsList mTags;
 
     private String mPositiveText;
     private String mDialogTitle;
@@ -247,16 +259,18 @@ public class TagsDialog extends AnalyticsDialogFragment {
 
         mType = DialogType.values()[requireArguments().getInt(DIALOG_TYPE_KEY)];
 
-        mTags.mCurrentTags = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
-        mTags.mCurrentTags.addAll(requireArguments().getStringArrayList(CHECKED_TAGS_KEY));
+        TreeSet<String> currentTags = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+        currentTags.addAll(requireArguments().getStringArrayList(CHECKED_TAGS_KEY));
 
-        mTags.mAllTags = requireArguments().getStringArrayList(ALL_TAGS_KEY);
+        ArrayList<String> allTags = requireArguments().getStringArrayList(ALL_TAGS_KEY);
 
-        for (String tag : mTags.mCurrentTags) {
-            if (!mTags.mAllTags.contains(tag)) {
-                mTags.mAllTags.add(tag);
+        for (String tag : currentTags) {
+            if (!allTags.contains(tag)) {
+                allTags.add(tag);
             }
         }
+
+        mTags = new TagsList(allTags, currentTags);
 
         setCancelable(true);
     }
