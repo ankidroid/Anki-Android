07b81f5053bb87a750e7e0af71b3964307c8c29f
vinish vishwanath <vinishvishwanath252@outlook.com>
2021-04-06 20:36:34 +0530

    Reverse activity transition for rtl language

--- AnkiDroid/src/main/java/com/ichi2/anim/ActivityTransitionAnimation.java
+++ AnkiDroid/src/main/java/com/ichi2/anim/ActivityTransitionAnimation.java
@@ -1,82 +1,73 @@
 package com.ichi2.anim;
 
 import android.app.Activity;
-
 import android.content.Context;
+import android.content.res.Resources;
+import android.os.Build;
+import android.text.Layout;
 import android.util.LayoutDirection;
-import com.ichi2.anki.R;
+import android.view.View;
 
-import androidx.core.app.ActivityOptionsCompat;
+import com.ichi2.anki.R;
+import com.ichi2.utils.LanguageUtil;
+
+import java.security.DomainLoadStoreParameter;
+import java.util.Locale;
+
+import androidx.core.os.ConfigurationCompat;
+import androidx.core.text.TextUtilsCompat;
+
 
 public class ActivityTransitionAnimation {
     public static void slide(Activity activity, Direction direction) {
         switch (direction) {
-        case START:
-            if (isRightToLeft(activity)) {
-               activity.overridePendingTransition(R.anim.slide_right_in, R.anim.slide_right_out);
-            } else {
-               activity.overridePendingTransition(R.anim.slide_left_in, R.anim.slide_left_out);
-            }
-            break;
-        case END:
-            if (isRightToLeft(activity)) {
-               activity.overridePendingTransition(R.anim.slide_left_in, R.anim.slide_left_out);
-            } else {
-               activity.overridePendingTransition(R.anim.slide_right_in, R.anim.slide_right_out);
-            }
-            break;
-        case FADE:
-            activity.overridePendingTransition(R.anim.fade_out, R.anim.fade_in);
-            break;
-        case UP:
-            activity.overridePendingTransition(R.anim.slide_up_in, R.anim.slide_up_out);
-            break;
-        case DIALOG_EXIT:
-            activity.overridePendingTransition(R.anim.none, R.anim.dialog_exit);
-            break;
-        case NONE:
-            activity.overridePendingTransition(R.anim.none, R.anim.none);
-            break;
-        case DOWN:
-        default:
-            // this is the default animation, we shouldn't try to override it
-        }
-    }
-
-
-    public static ActivityOptionsCompat getAnimationOptions(Activity activity, Direction direction) {
-        switch (direction) {
             case START:
-                return isRightToLeft(activity)
-                    ? ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.slide_right_in, R.anim.slide_right_out)
-                    : ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.slide_left_in, R.anim.slide_left_out);
+                if (isRightToLeft(activity)) {
+                        activity.overridePendingTransition(R.anim.slide_right_in, R.anim.slide_right_out);
+                }
+                else{
+                    activity.overridePendingTransition(R.anim.slide_left_in, R.anim.slide_left_out);
+                }
+                break;
             case END:
-                return isRightToLeft(activity)
-                    ? ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.slide_left_in, R.anim.slide_left_out)
-                    : ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.slide_right_in, R.anim.slide_right_out);
+                if(isRightToLeft(activity))
+                {
+                    activity.overridePendingTransition(R.anim.slide_left_in, R.anim.slide_left_out);
+                }
+                else{
+                activity.overridePendingTransition(R.anim.slide_right_in, R.anim.slide_right_out);
+                }
+
+                break;
             case FADE:
-                return ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.fade_out, R.anim.fade_in);
+                activity.overridePendingTransition(R.anim.fade_out, R.anim.fade_in);
+                break;
             case UP:
-                return ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.slide_up_in, R.anim.slide_up_out);
+                activity.overridePendingTransition(R.anim.slide_up_in, R.anim.slide_up_out);
+                break;
             case DIALOG_EXIT:
-                return ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.none, R.anim.dialog_exit);
+                activity.overridePendingTransition(R.anim.none, R.anim.dialog_exit);
+                break;
             case NONE:
-                return ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.none, R.anim.none);
-            case DOWN:
-            default:
+                activity.overridePendingTransition(R.anim.none, R.anim.none);
+                break;
+            default: //DOWN:
                 // this is the default animation, we shouldn't try to override it
-                return ActivityOptionsCompat.makeBasic();
         }
     }
-
-
     public enum Direction {
-        LEFT,
-        RIGHT,
+        START,
+        END,
         FADE,
         UP,
         DOWN,
         DIALOG_EXIT,
         NONE
     }
+
+    static boolean isRightToLeft(Context c)
+    {
+        return c.getResources().getConfiguration().getLayoutDirection() == LayoutDirection.RTL;
+    }
 }
+
