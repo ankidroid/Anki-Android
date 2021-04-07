3fdb4e657b41355b204cd8ad719c0989f463acc4
Shridhar <shridhar.goel@gmail.com>
2021-04-07 15:15:11 +0530

    Update

--- AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.java
@@ -687,17 +687,17 @@ public class CardBrowser extends NavigationDrawerActivity implements
         });
         mCardsListView.setOnItemLongClickListener((adapterView, view, position, id) -> {
             if (mInMultiSelectMode) {
-                for (int i = Math.min(mLastSelectedPosition, position); i <= Math.max(mLastSelectedPosition, position); i++) {
-                    // Fetch card at position i.
-                    CardCache card = (CardCache) mCardsListView.getItemAtPosition(i);
+                if (!mCheckedCards.contains(mCardsListView.getItemAtPosition(position))) {
+                    for (int i = Math.min(mLastSelectedPosition, position); i <= Math.max(mLastSelectedPosition, position); i++) {
+                        // Fetch card at position i.
+                        CardCache card = (CardCache) mCardsListView.getItemAtPosition(i);
 
-                    // If card is not in the list of checked cards, then add it, otherwise remove it.
-                    if (!mCheckedCards.contains(card)) {
-                        mCheckedCards.add(card);
-                    } else {
-                        mCheckedCards.remove(card);
+                        // If card is not in the list of checked cards, then add it.
+                        if (!mCheckedCards.contains(card)) {
+                            mCheckedCards.add(card);
+                            onSelectionChanged();
+                        }
                     }
-                    onSelectionChanged();
                 }
             } else {
                 mLastSelectedPosition = position;
