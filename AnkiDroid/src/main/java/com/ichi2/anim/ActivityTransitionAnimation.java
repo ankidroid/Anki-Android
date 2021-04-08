18f5cd7cd0f9267b4c346eee3724fe1f22e23d80
VINISH <76252038+vinishhub@users.noreply.github.com>
2021-04-08 06:34:26 +0530

    Update ActivityTransitionAnimation.java

--- AnkiDroid/src/main/java/com/ichi2/anim/ActivityTransitionAnimation.java
+++ AnkiDroid/src/main/java/com/ichi2/anim/ActivityTransitionAnimation.java
@@ -7,34 +7,34 @@ import com.ichi2.anki.R;
 public class ActivityTransitionAnimation {
     public static void slide(Activity activity, Direction direction) {
         switch (direction) {
-            case START:
-                if (isRightToLeft(activity)) {
-                    activity.overridePendingTransition(R.anim.slide_right_in, R.anim.slide_right_out);
-                } else {
-                    activity.overridePendingTransition(R.anim.slide_left_in, R.anim.slide_left_out);
-                }
-                break;
-            case END:
-                if (isRightToLeft(activity)) {
-                    activity.overridePendingTransition(R.anim.slide_left_in, R.anim.slide_left_out);
-                } else {
-                    activity.overridePendingTransition(R.anim.slide_right_in, R.anim.slide_right_out);
-                }
-                break;
-            case FADE:
-                activity.overridePendingTransition(R.anim.fade_out, R.anim.fade_in);
-                break;
-            case UP:
-                activity.overridePendingTransition(R.anim.slide_up_in, R.anim.slide_up_out);
-                break;
-            case DIALOG_EXIT:
-                activity.overridePendingTransition(R.anim.none, R.anim.dialog_exit);
-                break;
-            case NONE:
-                activity.overridePendingTransition(R.anim.none, R.anim.none);
-                break;
-            default: //DOWN:
-                // this is the default animation, we shouldn't try to override it
+        case START:
+            if (isRightToLeft(activity)) {
+               activity.overridePendingTransition(R.anim.slide_right_in, R.anim.slide_right_out);
+            } else {
+               activity.overridePendingTransition(R.anim.slide_left_in, R.anim.slide_left_out);
+             }
+               break;
+        case END:
+            if (isRightToLeft(activity)) {
+               activity.overridePendingTransition(R.anim.slide_left_in, R.anim.slide_left_out);
+            } else {
+               activity.overridePendingTransition(R.anim.slide_right_in, R.anim.slide_right_out);
+             }
+               break;
+        case FADE:
+            activity.overridePendingTransition(R.anim.fade_out, R.anim.fade_in);
+            break;
+        case UP:
+            activity.overridePendingTransition(R.anim.slide_up_in, R.anim.slide_up_out);
+            break;
+        case DIALOG_EXIT:
+            activity.overridePendingTransition(R.anim.none, R.anim.dialog_exit);
+            break;
+        case NONE:
+            activity.overridePendingTransition(R.anim.none, R.anim.none);
+            break;
+        default: //DOWN:
+            // this is the default animation, we shouldn't try to override it
         }
     }
     public enum Direction {
