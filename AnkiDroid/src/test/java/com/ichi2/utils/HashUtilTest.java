3afd7e8abbe946ef7e92dcd522593b233fc3fa6e
Arthur Milchior <Arthur@Milchior.fr>
2021-04-12 12:04:01 +0200

    NF: add unit test
    I know it fails, I want help

---
+++ AnkiDroid/src/test/java/com/ichi2/utils/HashUtilTest.java
@@ -0,0 +1,40 @@
+package com.ichi2.utils;
+
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.HashSet;
+import static org.junit.Assert.assertEquals;
+
+public class HashUtilTest {
+
+    /** Size of the table used to save a map.*/
+    private int mapThreshold(HashMap<?, ?> map) throws NoSuchFieldException, IllegalAccessException {
+        Field thresholdField = HashMap.class.getField("threshold");
+        return  thresholdField.getInt(map);
+    }
+
+    /** Threshold of a set. Not a setter*/
+    private int setThreshold(HashSet<?> set) throws NoSuchFieldException, IllegalAccessException {
+        return mapThreshold((HashMap) HashMap.class.getField("map").get(set));
+    }
+
+    @Test
+    public void NoReThreshold() throws NoSuchFieldException, IllegalAccessException {
+        for (int threshold : new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20, 100, 1000}) {
+            HashSet<Integer> set = HashUtil.HashSetInit(threshold);
+            HashMap<Integer, Integer> map = HashUtil.HashMapInit(threshold);
+            int init = mapThreshold(map);
+            //assertEquals(init, setThreshold(set));
+            for (Integer i = 0; i < threshold; i++) {
+                set.add(i);
+                map.put(i, i);
+            }
+            assertEquals(init, mapThreshold(map));
+            //assertEquals(init, setThreshold(set));
+        }
+    }
+
+
+}
