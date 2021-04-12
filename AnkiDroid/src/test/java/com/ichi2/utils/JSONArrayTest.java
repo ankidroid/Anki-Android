bf5a0eb753bdb1670a0fd7e28897de22c2c5920e
ammar <45490495+3mmar7amed@users.noreply.github.com>
2021-04-12 06:38:41 +0200

    Update code style

--- AnkiDroid/src/test/java/com/ichi2/utils/JSONArrayTest.java
+++ AnkiDroid/src/test/java/com/ichi2/utils/JSONArrayTest.java
@@ -1,100 +1,128 @@
+/****************************************************************************************
+ * Copyright (c) 2021 Ammar Hamed <ammarhamed64@gmail.com>                              *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 3 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
 package com.ichi2.utils;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 @RunWith(AndroidJUnit4.class)
-public class JSONArrayTest{
-    JSONArray x ;
-    JSONArray array ;
-    int initialLength;
+public class JSONArrayTest {
+    JSONArray mArray;
+    int mInitialArrayLength;
+
 
     @Before
-    public void init(){
-        x = new JSONArray() ;
+    public void initializeNewObject() {
+        mArray = new JSONArray();
     }
 
-    public void initializeArray(){
-        for(int index = 0 , value = 1 ; index<100 ; index++  ){
-            array = x.put(index ,value) ;
+
+    public void initializeArray() {
+        for (int i = 0, value = 1; i < 100; i++) {
+            mArray.put(i, value);
         }
-        initialLength = 100 ;
+        mInitialArrayLength = 100;
     }
 
-     @Test
-    public void putTestInExistingArray(){
-        initializeArray();
-        for(int i = 10 ; i < 20 ; i++ ) {
-             x.put(i , 20) ;
-        }
-         for(int i =10 ; i < 20 ; i++){
-             Assert.assertEquals(20 , array.get(i));
-         }
-         for(int i = 0 , j =20 ;i <10 ; i++ , j++ ){
-             Assert.assertEquals(1 , array.get(i));
-         }
-         Assert.assertEquals(initialLength , array.length());
 
-    }
     @Test
-    public void putTestInEmptyArray(){
-        array = x.put(200 , null);
-        Assert.assertEquals(201 , array.length());
+    public void testPut_existingArray() {
+        initializeArray();
+        for (int i = 10; i < 20; i++) {
+            mArray.put(i, 20);
+        }
+        for (int i = 10; i < 20; i++) {
+            Assert.assertEquals(20, mArray.get(i));
+        }
+        for (int i = 0, j = 20; i < 10; i++, j++) {
+            Assert.assertEquals(1, mArray.get(i));
+        }
+        Assert.assertEquals(mInitialArrayLength, mArray.length());
+
     }
+
+
+    @Test
+    public void testPut_emptyArray() {
+        mArray.put(200, null);
+        Assert.assertEquals(201, mArray.length());
+    }
+
 
     /**
      * Actually the array will not expand as this value is set to the in the index in arraylist not add to .
      */
     @Test(expected = AssertionError.class)
-    public void putTestAtTheBeginningOfExistingArrayButNotExpands(){
+    public void testPut_atTheBeginningAndNotExpand() {
         initializeArray();
-        array = x.put(0 , 200) ;
-        Assert.assertEquals(initialLength +1 , array.length());
+        mArray.put(0, 200);
+        Assert.assertEquals(mInitialArrayLength + 1, mArray.length());
     }
+
 
     /**
      * Actually the array will not expand as this value is set to the in the index in arraylist not add to .
      */
     @Test(expected = AssertionError.class)
-    public void putTestAtTheMidleOfExistingArrayButNotExpands(){
+    public void testPut_atTheMiddleAndNotExpand() {
         initializeArray();
-        array = x.put(array.length()/2,1);
-        Assert.assertEquals(initialLength +1  , array.length());
+        mArray.put(mArray.length() / 2, 1);
+        Assert.assertEquals(mInitialArrayLength + 1, mArray.length());
     }
+
 
     /**
      * Actually the array will  expand as this value is set to the in the index in arraylist,
      * but he check first if the array.length < index , then fill every empty indexes with null until reach index  .
      */
     @Test
-    public void putTestAtTheEndOfExistingArray(){
+    public void testPut_atTheEndOAndExpand() {
         initializeArray();
-        array = x.put(102,1);
-        Assert.assertEquals(103 , array.length());
+        mArray.put(102, 1);
+        Assert.assertEquals(103, mArray.length());
     }
+
 
     @Test(expected = IndexOutOfBoundsException.class)
-    public void putTestOutOfBoundary(){
-        x.put(-1 ,100) ;
+    public void testPut_indexOutOfBoundary() {
+        mArray.put(-1, 100);
     }
+
 
     @Test
-    public void getTest(){
-        x.put(200 , 1150) ;
-        Assert.assertEquals(1150 , x.get(200));
+    public void testGet_existingValue() {
+        mArray.put(200, 1150);
+        Assert.assertEquals(1150, mArray.get(200));
 
     }
-    @Test(expected = JSONException.class)
-    public void getTestNull(){
-        x.put(199 , null) ;
-        x.get(199);
-    }
+
 
     @Test(expected = JSONException.class)
-    public void getTestValueOutOfIndex(){
-        x.get(200) ;
+    public void testGet_null() {
+        mArray.put(199, null);
+        mArray.get(199);
     }
 
+
+    @Test(expected = JSONException.class)
+    public void testGet_valueOutOfIndex() {
+        mArray.get(200);
+    }
 }
