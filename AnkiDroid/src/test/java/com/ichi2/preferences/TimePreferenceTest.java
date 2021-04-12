ca60b9ee10e1d18ab528787bd6d262862b0c8e98
Diego Rodriguez <diego.vincent.rodriguez@gmail.com>
2021-04-12 21:51:08 +0200

    test: parsing hours as JUnit parameterized test

---
+++ AnkiDroid/src/test/java/com/ichi2/preferences/TimePreferenceTest.java
@@ -0,0 +1,39 @@
+package com.ichi2.preferences;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(value = Parameterized.class)
+public class TimePreferenceTest {
+    private final String parsableHour;
+    private final int expectedHour;
+
+
+    public TimePreferenceTest(String parsableHour, int expectedHour) {
+        this.parsableHour = parsableHour;
+        this.expectedHour = expectedHour;
+    }
+
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {"00:00", 0},
+                {"01:00", 1},
+                {"24:00", 24}, // should this be "0"?
+        });
+    }
+    
+    @Test
+    public void shouldParseHours() {
+        int actualHour = TimePreference.parseHours(this.parsableHour);
+
+        assertEquals(expectedHour, actualHour);
+    }
+}
