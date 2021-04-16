548370b9cca3a72bdd68fe2eabc6ea9b325f9f93
Mike Hardy <github@mikehardy.net>
2021-04-15 22:27:54 -0500

    NF: remove unused espresso / androidTest view watcher
    this allows us to purge our last jcenter dependency

--- AnkiDroid/src/androidTest/java/com/ichi2/anki/tests/Shared.java
+++ AnkiDroid/src/androidTest/java/com/ichi2/anki/tests/Shared.java
@@ -16,13 +16,9 @@
 
 package com.ichi2.anki.tests;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.ContextWrapper;
 import android.text.TextUtils;
-import android.view.View;
 
-import com.azimolabs.conditionwatcher.Instruction;
 import com.ichi2.libanki.Collection;
 import com.ichi2.libanki.Storage;
 import com.ichi2.libanki.Utils;
@@ -32,67 +28,12 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static org.junit.Assert.assertTrue;
 
 /**
  * Shared methods for unit tests.
  */
 public class Shared {
-
-
-    /**
-     * Instantiate one of these with the View id of the item you want Robolectric to wait on, and it will
-     * fast-poll until the item is inflated and visible
-     */
-    public static class ViewItemWaitingInstruction extends Instruction {
-
-        private final int viewId;
-
-
-        public  String getDescription() {
-            return "Waits for an id to be visible";
-        }
-
-
-        /**
-         * Instantiate this with the Resource id of the view to wait for
-         */
-        public ViewItemWaitingInstruction(int viewId) {
-            this.viewId = viewId;
-        }
-
-
-        @Override
-        public boolean checkCondition() {
-            Activity activity = getCurrentActivity();
-            return ((activity != null) && (activity.findViewById(viewId) != null) &&
-                    (activity.findViewById(viewId).getVisibility() == View.VISIBLE));
-        }
-
-
-        /** Get the current activity so, to inspect for View ids */
-        private Activity getCurrentActivity() {
-            final Activity[] activity = new Activity[1];
-            onView(isRoot()).check((view, noViewFoundException) -> {
-                    View content = view.findViewById(android.R.id.content);
-                    if (content != null) {
-                        activity[0] = scanForActivity(content.getContext());
-                    }
-            });
-            return activity[0];
-        }
-
-
-        /** If the Context is a wrapper, recursively go for the Activity */
-        private Activity scanForActivity(Context cont) {
-            if (cont == null) return null;
-            else if (cont instanceof Activity) return (Activity)cont;
-            else if (cont instanceof ContextWrapper) return scanForActivity(((ContextWrapper)cont).getBaseContext());
-            return null;
-        }
-    }
 
     public static Collection getEmptyCol(Context context) throws IOException {
         File f = File.createTempFile("test", ".anki2");
