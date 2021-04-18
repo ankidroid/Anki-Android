9f5242b03d6046ef4c59bde9303324cf47bae456
Arthur Milchior <Arthur@Milchior.fr>
2021-04-18 17:25:58 +0200

    NF: PartialSearch.doProgress introduce nextPosition

--- AnkiDroid/src/main/java/com/ichi2/async/CollectionTask.java
+++ AnkiDroid/src/main/java/com/ichi2/async/CollectionTask.java
@@ -1083,7 +1083,8 @@ public class CollectionTask<ProgressListener, ProgressBackground extends Progres
             // PERF: This performs an individual query to load each note
             while (mCards.size() < cards.size()) {
                 int nextPosition = mCards.size();
-                mCards.add(new CardBrowser.CardCache(cards.get(nextPosition), mCol, mCards.size()));
+                Long nextCardId = cards.get(nextPosition);
+                mCards.add(new CardBrowser.CardCache(nextCardId, mCol, mCards.size()));
             }
             for (CardBrowser.CardCache card : mCards) {
                 if (isCancelled()) {
