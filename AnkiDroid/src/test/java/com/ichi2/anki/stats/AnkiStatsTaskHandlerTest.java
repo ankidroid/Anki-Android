9e0af07400905b95ad0916c784293d32669c2c76
rodrigo-sp17 <rodrigosilvap93@gmail.com>
2021-04-22 21:31:24 -0300

    Replace shadowMainLooper for advanceRobolectricRunner

--- AnkiDroid/src/test/java/com/ichi2/anki/stats/AnkiStatsTaskHandlerTest.java
+++ AnkiDroid/src/test/java/com/ichi2/anki/stats/AnkiStatsTaskHandlerTest.java
@@ -1,9 +1,26 @@
+/****************************************************************************************
+ * Copyright (c) 2021 Rodrigo Silva <dev,rodrigosp@gmail.com>                              *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 3 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
 package com.ichi2.anki.stats;
 
 import android.os.AsyncTask;
 import android.util.Pair;
 import android.widget.TextView;
 
+import com.ichi2.anki.RobolectricTest;
 import com.ichi2.libanki.Collection;
 
 import org.junit.Before;
@@ -18,10 +35,9 @@ import java.util.concurrent.ExecutionException;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static org.mockito.Mockito.*;
-import static org.robolectric.shadows.ShadowLooper.shadowMainLooper;
 
 @RunWith(AndroidJUnit4.class)
-public class AnkiStatsTaskHandlerTest {
+public class AnkiStatsTaskHandlerTest extends RobolectricTest {
 
     @Mock
     private Collection col;
@@ -42,7 +58,7 @@ public class AnkiStatsTaskHandlerTest {
                 .createReviewSummaryStatistics(col, view);
 
         result.get();
-        shadowMainLooper().idle();
+        advanceRobolectricLooper();
 
         Mockito.verify(col, atLeast(1)).getDb();
     }
