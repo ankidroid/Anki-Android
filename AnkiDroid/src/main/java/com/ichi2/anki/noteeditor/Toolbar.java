955bcad12336266a4aec787d84a4948125871cb8
sakshi-1604 <65847879+sakshi-1604@users.noreply.github.com>
2021-04-28 13:20:15 +0530

    Remove external library

--- AnkiDroid/src/main/java/com/ichi2/anki/noteeditor/Toolbar.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/noteeditor/Toolbar.java
@@ -36,11 +36,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
-import com.flask.colorpicker.ColorPickerView;
-import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 import com.ichi2.anki.CardTemplateEditor;
 import com.ichi2.anki.R;
 import com.ichi2.anki.TemporaryModel;
@@ -128,24 +125,6 @@ public class Toolbar extends FrameLayout {
         setClick(R.id.note_editor_toolbar_color_yellow, "<span style=\"color:yellow;\">", "</span>");
         setClick(R.id.note_editor_toolbar_color_white, "<span style=\"color:white;\">", "</span>");
         setClick(R.id.note_editor_toolbar_color_black, "<span style=\"color:black;\">", "</span>");
-        findViewById(R.id.note_editor_toolbar_color_custom).setOnClickListener(l -> {
-            ColorPickerDialogBuilder
-                    .with(getContext())
-                    .setTitle("Choose color")
-                    .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
-                    .density(12)
-                    .setOnColorSelectedListener(selectedColor ->  {
-                        // Do nothing
-                    })
-                    .setPositiveButton("Choose", (d, selectedColor, allColors) -> {
-                        onFormat(new TextWrapper("<span style=\"color:" + "#" + Integer.toHexString(selectedColor) + ";\">", "</span>"));
-                    })
-                    .setNegativeButton("Cancel", (dialog, which) ->  {
-                        dialog.dismiss();
-                    })
-                    .build()
-                    .show();
-        });
         this.mClozeIcon = findViewById(R.id.note_editor_toolbar_button_cloze);
     }
 
