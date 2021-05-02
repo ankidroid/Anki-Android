7880f15e2b9f0c39e56a2d0635eb8a0be9bdfedd
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-05-02 19:50:12 +0200

    NF: rename ADD_TAG to EDIT_TAGS
    now the name and comment reflects the meaning of the tags
    
    since we can add/remove tags from multiple notes using this dialog type

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/tags/TagsDialog.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/tags/TagsDialog.java
@@ -38,9 +38,9 @@ public class TagsDialog extends AnalyticsDialogFragment {
      */
     public enum DialogType {
         /**
-         * Adding tag to a single note
+         * Adding tag to a note/s
          */
-        ADD_TAG,
+        EDIT_TAGS,
         /**
          * Filter notes by tags
          */
@@ -182,7 +182,7 @@ public class TagsDialog extends AnalyticsDialogFragment {
         mSelectedOption = mOptionsGroup.getCheckedRadioButtonId();
         mOptionsGroup.setOnCheckedChangeListener((radioGroup, checkedId) -> mSelectedOption = checkedId);
 
-        if (mType == DialogType.ADD_TAG) {
+        if (mType == DialogType.EDIT_TAGS) {
             mDialogTitle = getResources().getString(R.string.card_details_tags);
             mOptionsGroup.setVisibility(View.GONE);
             mPositiveText = getString(R.string.dialog_ok);
@@ -271,7 +271,7 @@ public class TagsDialog extends AnalyticsDialogFragment {
             return true;
         });
 
-        if (mType == DialogType.ADD_TAG) {
+        if (mType == DialogType.EDIT_TAGS) {
             mToolbarSearchView.setQueryHint(getString(R.string.add_new_filter_tags));
         } else {
             mToolbarAddItem.setVisible(false);
