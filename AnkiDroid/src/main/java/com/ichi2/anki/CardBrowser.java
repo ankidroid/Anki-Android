a5550c939ab686604121f3f8f83a420fa38c0e67
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-05-02 21:41:08 +0200

    feat: add "Edit Tags" in CardBrowser
    Select multiple cards and change the tags of their notes

--- AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.java
@@ -80,6 +80,7 @@ import com.ichi2.libanki.Card;
 import com.ichi2.libanki.Collection;
 import com.ichi2.libanki.Consts;
 import com.ichi2.libanki.Decks;
+import com.ichi2.libanki.Note;
 import com.ichi2.libanki.Utils;
 import com.ichi2.libanki.Deck;
 import com.ichi2.themes.Themes;
@@ -89,6 +90,7 @@ import com.ichi2.utils.FunctionalInterfaces;
 import com.ichi2.utils.LanguageUtil;
 import com.ichi2.utils.Computation;
 import com.ichi2.utils.Permissions;
+import com.ichi2.utils.TagsUtil;
 import com.ichi2.widget.WidgetStatus;
 
 import com.ichi2.utils.JSONException;
@@ -105,6 +107,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import timber.log.Timber;
 
@@ -152,7 +156,8 @@ public class CardBrowser extends NavigationDrawerActivity implements
     }
 
     private enum TagsDialogListenerAction {
-        FILTER
+        FILTER,
+        EDIT_TAGS,
     }
 
     /** List of cards in the browser.
@@ -1287,6 +1292,8 @@ public class CardBrowser extends NavigationDrawerActivity implements
                 startActivityWithAnimation(intent, FADE);
             }
             return true;
+        } else if (itemId == R.id.action_edit_tags) {
+            showEditTagsDialog();
         }
         return super.onOptionsItemSelected(item);
     }
@@ -1454,6 +1461,48 @@ public class CardBrowser extends NavigationDrawerActivity implements
         }
     }
 
+    private void showEditTagsDialog() {
+        if (getSelectedCardIds().isEmpty()) {
+            Timber.d("showEditTagsDialog: called with empty selection");
+        }
+
+        final ArrayList<String> allTags = new ArrayList<>(getCol().getTags().all());
+
+
+        List<Note> selectedNotes = getSelectedCardIds()
+                .stream()
+                .map(cardId -> getCol().getCard(cardId).note())
+                .distinct()
+                .collect(Collectors.toList());
+
+        final ArrayList<String> checkedTags = selectedNotes
+                .stream()
+                .flatMap(note -> note.getTags().stream())
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        if (selectedNotes.size() == 1) {
+            Timber.d("showEditTagsDialog: edit tags for one note");
+            mTagsDialogListenerAction = TagsDialogListenerAction.EDIT_TAGS;
+            TagsDialog dialog = mTagsDialogFactory.newTagsDialog().withArguments(TagsDialog.DialogType.EDIT_TAGS, checkedTags, allTags);
+            showDialogFragment(dialog);
+            return;
+        }
+
+        final ArrayList<String> uncheckedTags = selectedNotes
+                .stream()
+                .flatMap(note -> {
+                    final List<String> noteTags = note.getTags();
+                    return allTags.stream().filter(t -> !noteTags.contains(t));
+                })
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        Timber.d("showEditTagsDialog: edit tags for multiple note");
+        mTagsDialogListenerAction = TagsDialogListenerAction.EDIT_TAGS;
+        TagsDialog dialog = mTagsDialogFactory.newTagsDialog().withArguments(TagsDialog.DialogType.EDIT_TAGS,
+                checkedTags, uncheckedTags, allTags);
+        showDialogFragment(dialog);
+    }
+
     private void showFilterByTagsDialog() {
         mTagsDialogListenerAction = TagsDialogListenerAction.FILTER;
         TagsDialog dialog = mTagsDialogFactory.newTagsDialog().withArguments(
@@ -1588,14 +1637,35 @@ public class CardBrowser extends NavigationDrawerActivity implements
         return nonDynamicDecks;
     }
 
-
     @Override
     public void onSelectedTags(List<String> selectedTags, List<String> indeterminateTags, int option) {
         switch (mTagsDialogListenerAction) {
             case FILTER:
                 filterByTags(selectedTags, option);
                 break;
+            case EDIT_TAGS:
+                editSelectedCardsTags(selectedTags, indeterminateTags);
+                break;
         }
+    }
+
+
+    private void editSelectedCardsTags(List<String> selectedTags, List<String> indeterminateTags) {
+        List<Note> selectedNotes = getSelectedCardIds()
+                .stream()
+                .map(cardId -> getCol().getCard(cardId).note())
+                .distinct()
+                .collect(Collectors.toList());
+
+        for (Note note : selectedNotes) {
+            List<String> previousTags = note.getTags();
+            List<String> updatedTags = TagsUtil.getUpdatedTags(previousTags, selectedTags, indeterminateTags);
+            note.setTagsFromStr(getCol().getTags().join(updatedTags));
+        }
+
+        Timber.i("CardBrowser:: editSelectedCardsTags: Saving note/s tags...");
+        TaskManager.launchCollectionTask(new CollectionTask.UpdateMultipleNotes(selectedNotes),
+                updateMultipleNotesHandler());
     }
 
 
@@ -1706,6 +1776,30 @@ public class CardBrowser extends NavigationDrawerActivity implements
         }
 
         updateList();
+    }
+
+    private UpdateMultipleNotesHandler updateMultipleNotesHandler() {
+        return new UpdateMultipleNotesHandler(this);
+    }
+
+    private static class UpdateMultipleNotesHandler extends ListenerWithProgressBarCloseOnFalse<List<Note>, Computation<?>> {
+        public UpdateMultipleNotesHandler(CardBrowser browser) {
+            super("Card Browser - UpdateMultipleNotesHandler.actualOnPostExecute(CardBrowser browser)", browser);
+        }
+
+        @Override
+        public void actualOnProgressUpdate(@NonNull CardBrowser browser, List<Note> notes) {
+            List<Card> cardsToUpdate = notes
+                    .stream()
+                    .flatMap(n -> n.cards().stream())
+                    .collect(Collectors.toList());
+            browser.updateCardsInList(cardsToUpdate);
+        }
+
+        @Override
+        protected void actualOnValidPostExecute(CardBrowser browser, Computation<?> result) {
+            browser.hideProgressBar();
+        }
     }
 
     private UpdateCardHandler updateCardHandler() {
