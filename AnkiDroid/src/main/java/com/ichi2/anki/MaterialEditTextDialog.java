509a966d157c4ef1a55f442f24aaa08daf393ab6
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-05-02 23:41:10 +0100

    MaterialEditTextDialog: works on .build()
    .show() calls .build() so this still works on show

--- AnkiDroid/src/main/java/com/ichi2/anki/MaterialEditTextDialog.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/MaterialEditTextDialog.java
@@ -27,35 +27,39 @@ import java.util.Objects;
 
 import androidx.annotation.NonNull;
 
+/**
+ * A Dialog containing an EditText which displays a keyboard when opened
+ */
 public class MaterialEditTextDialog extends MaterialDialog {
     protected MaterialEditTextDialog(Builder builder) {
         super(builder);
     }
 
-    public static class Builder extends MaterialDialog.Builder {
+    /**
+     * Method to display keyboard when dialog shows.
+     * @param editText EditText present in the dialog.
+     * @param materialDialog Dialog which contains the EditText and needs the keyboard to be displayed.
+     */
+    public static void displayKeyboard(EditText editText, MaterialDialog materialDialog) {
+        AndroidUiUtils.setFocusAndOpenKeyboard(editText, Objects.requireNonNull(materialDialog.getWindow()));
+    }
 
-        public EditText editText;
+    public static class Builder extends MaterialDialog.Builder {
 
         public Builder(@NonNull Context context, EditText editText) {
             super(context);
-            this.editText = editText;
+            customView(editText, true);
         }
 
         @Override
-        public MaterialDialog show() {
-            customView(editText, true);
-            MaterialDialog materialDialog = super.show();
-            displayKeyboard(editText, materialDialog);
-            return materialDialog;
+        public MaterialDialog build() {
+            return new MaterialEditTextDialog(this);
         }
+    }
 
-        /**
-         * Method to display keyboard when dialog shows.
-         * @param editText EditText present in the dialog.
-         * @param materialDialog Dialog which contains the EditText and needs the keyboard to be displayed.
-         */
-        public void displayKeyboard(EditText editText, MaterialDialog materialDialog) {
-            AndroidUiUtils.setFocusAndOpenKeyboard(editText, Objects.requireNonNull(materialDialog.getWindow()));
-        }
+    @Override
+    public void show() {
+        super.show();
+        displayKeyboard((EditText) this.getCustomView(), this);
     }
 }
