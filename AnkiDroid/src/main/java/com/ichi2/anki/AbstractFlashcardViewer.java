3ba01b96f33e7a530f7f1e2ee8880a68c0780780
Mohaned hassan <Mohanedhassan1999@gmail.com>
2021-07-11 15:48:30 +0200

    added feedback on undo

--- AnkiDroid/src/main/java/com/ichi2/anki/AbstractFlashcardViewer.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/AbstractFlashcardViewer.java
@@ -1363,6 +1363,7 @@ public abstract class AbstractFlashcardViewer extends NavigationDrawerActivity i
     protected void undo() {
         if (isUndoAvailable()) {
             TaskManager.launchCollectionTask(new CollectionTask.Undo(), new AnswerCardHandler(false));
+            UIUtils.showThemedToast(getApplicationContext(), getApplicationContext().getString(R.string.undo_done_msg) , false);
         }
     }
 
