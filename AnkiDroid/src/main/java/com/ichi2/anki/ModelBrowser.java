eb20a713f2e8a3755926692e65ad725dd702fa6b
Akshay <jadhavAkshay0701@gmail.com>
2021-07-12 11:26:02 +0530

    Rename Note Type: randomize name if it is already existed

--- AnkiDroid/src/main/java/com/ichi2/anki/ModelBrowser.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/ModelBrowser.java
@@ -453,6 +453,11 @@ public class ModelBrowser extends AnkiActivity {
      * Displays a confirmation box asking if you want to rename the note type and then renames it if confirmed
      */
     private void renameModelDialog() {
+
+        if (mExistingModelsNames == null) {
+            return;
+        }
+
         mModelNameInput = new FixedEditText(this);
         mModelNameInput.setSingleLine(true);
         mModelNameInput.setText(mModels.get(mModelListPosition).getString("name"));
@@ -466,6 +471,11 @@ public class ModelBrowser extends AnkiActivity {
                                     String deckName = mModelNameInput.getText().toString()
                                             // Anki desktop doesn't allow double quote characters in deck names
                                             .replaceAll("[\"\\n\\r]", "");
+
+                                    if (mExistingModelsNames.contains(deckName)) {
+                                        deckName = randomizeName(deckName);
+                                    }
+
                                     if (deckName.length() > 0) {
                                         model.put("name", deckName);
                                         mCol.getModels().update(model);
