ab4a40536ab066da2492d72cfca34b2c5f1875b0
Shridhar Goel <shridhar.goel@gmail.com>
2021-07-18 01:13:58 +0530

    Browse AnkiWeb shared decks in app
    This will allow the downloading of decks and let AnkiDroid import them

--- AnkiDroid/src/main/java/com/ichi2/anki/DeckPicker.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/DeckPicker.java
@@ -2185,8 +2185,9 @@ public class DeckPicker extends NavigationDrawerActivity implements
     }
 
 
-    public void addSharedDeck() {
-        openUrl(Uri.parse(getResources().getString(R.string.shared_decks_url)));
+    public void openAnkiWebSharedDecks() {
+        Intent intent = new Intent(this, SharedDecksActivity.class);
+        startActivityWithoutAnimation(intent);
     }
 
 
