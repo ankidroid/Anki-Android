659f6b3a2299267a77ea348a09808d44c2e7a1b0
Farjad Ilyas <ilyasfarjad@gmail.com>
2021-07-26 05:02:58 +0500

    Add copyDirectory() method
    - Expected to be used to copy user data from legacy to app-specific
      dirs that will be accessible under scoped storage

--- AnkiDroid/src/main/java/com/ichi2/compat/CompatV26.java
+++ AnkiDroid/src/main/java/com/ichi2/compat/CompatV26.java
@@ -25,16 +25,25 @@ import android.media.AudioManager;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 
+import com.ichi2.async.ProgressSenderAndCancelListener;
+import com.ichi2.utils.FileUtil;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
 
 import timber.log.Timber;
 
@@ -85,6 +94,52 @@ public class CompatV26 extends CompatV23 implements Compat {
     @Override
     public long copyFile(@NonNull InputStream source, @NonNull String target) throws IOException {
         return Files.copy(source, Paths.get(target), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    // Explores the source directory tree recursively and copies each directory and each file inside each directory
+    @Override
+    public void copyDirectory(@NonNull File srcDir, @NonNull File destDir, @NonNull ProgressSenderAndCancelListener<Integer> ioTask, boolean deleteAfterCopy) throws IOException {
+        // If destDir exists, it must be a directory. If not, create it
+        FileUtil.ensureFileIsDirectory(destDir);
+
+        Path sourceDirPath = srcDir.toPath();
+        Path destinationDirPath = destDir.toPath();
+
+        Files.walkFileTree(sourceDirPath, new SimpleFileVisitor<Path>() {
+
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                Files.createDirectories(destinationDirPath.resolve(sourceDirPath.relativize(dir)));
+                return FileVisitResult.CONTINUE;
+            }
+
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                File destFile = destinationDirPath.resolve(sourceDirPath.relativize(file)).toFile();
+
+                // Copy if source file and destination file aren't of the same length
+                // i.e., copy if destination file wasn't copied completely
+                if (file.toFile().length() != destFile.length()) {
+                    OutputStream outputStream = new FileOutputStream(destFile, false);
+                    long bytesCopied = copyFile(file.toString(), outputStream);
+                    ioTask.doProgress((int) bytesCopied / 1024);
+                    outputStream.close();
+                }
+                if (deleteAfterCopy) {
+                    Files.delete(file);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                if (deleteAfterCopy) {
+                    Files.delete(dir);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
     }
 
     @Override
