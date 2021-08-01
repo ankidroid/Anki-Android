17149cb78e43d9f47619923906973a47959f9c44
TheItsyBitsySpider <72374564+TheItsyBitsySpider@users.noreply.github.com>
2021-07-31 19:07:06 -0500

    Added/updated copyright statements to files (#9346)

--- AnkiDroid/src/test/java/com/ichi2/anki/cardviewer/MissingImageHandlerTest.java
+++ AnkiDroid/src/test/java/com/ichi2/anki/cardviewer/MissingImageHandlerTest.java
@@ -1,5 +1,6 @@
 /*
  *  Copyright (c) 2020 David Allison <davidallisongithub@gmail.com>
+ *  Copyright (c) 2021 Kael Madar <itsybitsyspider@madarhome.com>
  *
  *  This program is free software; you can redistribute it and/or modify it under
  *  the terms of the GNU General Public License as published by the Free Software
