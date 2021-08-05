2cab1077fbda3097d0cfa6749fd4ce12539a1e09
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-08-06 00:22:19 +0100

    lint: Only enforce variable naming in java
    We want to move away from AOSP naming for Kotlin
    
    As it makes getters and primary constructors less useful
    as the naming conflicts with other policies we want to enforce
    AOSP naming is also against Google suggestions
    
    Fixes 9377

--- lint-rules/src/test/java/com/ichi2/anki/lint/rules/JavaNonPublicNonStaticFieldDetectorTest.java
+++ lint-rules/src/test/java/com/ichi2/anki/lint/rules/JavaNonPublicNonStaticFieldDetectorTest.java
@@ -16,13 +16,15 @@
 
 package com.ichi2.anki.lint.rules;
 
+import com.android.tools.lint.checks.infrastructure.TestFile;
+
 import org.intellij.lang.annotations.Language;
 import org.junit.Test;
 
 import static com.android.tools.lint.checks.infrastructure.TestFile.JavaTestFile.create;
 import static com.android.tools.lint.checks.infrastructure.TestLintTask.lint;
 
-public class NonPublicNonStaticFieldDetectorTest {
+public class JavaNonPublicNonStaticFieldDetectorTest {
 
     @Language("JAVA")
     private static final String BADLY_NAMED_VARIABLE = "public class Xx { " +
@@ -42,13 +44,26 @@ public class NonPublicNonStaticFieldDetectorTest {
             "}" +
             "}";
 
+    @Language("kotlin")
+    private static final String BADLY_NAMED_VARIABLE_KOTLIN = "class Xx { " +
+            "/**" +
+            "*/" +
+            "private val withCommentShows: Int = -1\n" +
+            "val publicIsFine: Int = 1\n" +
+            "private val ad: Int = 1\n" +
+            "private val mad: Int = 1\n" +
+            "private val a : Int = 1\n" +
+            "private val m: Int = 1\n" +
+            "private val mOk: String\n" +
+            "}";
+
     @Test
     public void showsErrorForNullable() {
         lint()
                 .allowMissingSdk()
                 .allowCompilationErrors()
                 .files(create(BADLY_NAMED_VARIABLE))
-                .issues(NonPublicNonStaticFieldDetector.ISSUE)
+                .issues(NonPublicNonStaticJavaFieldDetector.ISSUE)
                 .run()
                 .expectErrorCount(5)
                 .check(output -> {
@@ -56,4 +71,15 @@ public class NonPublicNonStaticFieldDetectorTest {
                 });
     }
 
+    @Test
+    public void kotlin_no_errors() {
+        // #9377 - Kotlin explicitly should not follow our style guide
+        lint()
+                .allowMissingSdk()
+                .allowCompilationErrors()
+                .files(TestFile.KotlinTestFile.create(BADLY_NAMED_VARIABLE_KOTLIN))
+                .issues(NonPublicNonStaticJavaFieldDetector.ISSUE)
+                .run()
+                .expectClean();
+    }
 }
