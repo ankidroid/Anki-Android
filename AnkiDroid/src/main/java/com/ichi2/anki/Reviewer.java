f7cd5b268770803519fc1e3e228dbb50034c4200
Noah Weber <noah.weber@mni.thm.de>
2021-08-13 16:15:26 +0200

    replace deprecated code

--- AnkiDroid/src/main/java/com/ichi2/anki/Reviewer.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/Reviewer.java
@@ -41,6 +41,7 @@ import android.view.SubMenu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.webkit.WebView;
 import android.webkit.JavascriptInterface;
 import android.widget.FrameLayout;
@@ -1165,16 +1166,13 @@ public class Reviewer extends AbstractFlashcardViewer {
     @RequiresApi(api = Build.VERSION_CODES.R)
     @SuppressWarnings("deprecation") // #9332: UI Visibility -> Insets
     private void setFullScreen(final AbstractFlashcardViewer a) {
-        // Set appropriate flags to enable Sticky Immersive mode.
-        a.getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        //| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION // temporarily disabled due to #5245
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
-        );
+        //Hide System Bars
+        getWindow().setDecorFitsSystemWindows(false);
+        WindowInsetsController controller = getWindow().getInsetsController();
+        if (controller != null) {
+            controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+            controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        }
         // Show / hide the Action bar together with the status bar
         SharedPreferences prefs = AnkiDroidApp.getSharedPrefs(a);
         FullScreenMode fullscreenMode = FullScreenMode.fromPreference(prefs);
