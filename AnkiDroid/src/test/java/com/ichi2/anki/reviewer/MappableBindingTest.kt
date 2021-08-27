47f421cd053f2b4e8bf3e971ae8e12bf62f3f294
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-08-27 18:13:51 +0100

    fix: MappableBinding equality

--- AnkiDroid/src/test/java/com/ichi2/anki/reviewer/MappableBindingTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/reviewer/MappableBindingTest.kt
@@ -18,19 +18,30 @@ package com.ichi2.anki.reviewer
 import android.view.KeyEvent
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.hasItem
+import org.hamcrest.Matchers.not
 import org.junit.Test
 
 class MappableBindingTest {
     @Test
     fun equalityTest() {
-        val allBindings = PeripheralCommand.getDefaultCommands()
-            .map { x -> x.binding }
-            .map(this::fromBinding)
-            .toList()
+        val allBindings = getAllBindings()
 
         assertThat(allBindings, hasItem(unicodeCharacter('@')))
         assertThat(allBindings, hasItem(keyCode(KeyEvent.KEYCODE_1)))
     }
+
+    @Test
+    fun inequalityTest() {
+        val allBindings = getAllBindings()
+        // pick an arbitrary key which is not mapped
+        assertThat(allBindings, not(hasItem(unicodeCharacter('a'))))
+        assertThat(allBindings, not(hasItem(keyCode(KeyEvent.KEYCODE_A))))
+    }
+
+    private fun getAllBindings() = PeripheralCommand.getDefaultCommands()
+        .map { x -> x.binding }
+        .map(this::fromBinding)
+        .toList()
 
     @Suppress("SameParameterValue")
     private fun keyCode(code: Int) = fromBinding(BindingTest.keyCode(code))
