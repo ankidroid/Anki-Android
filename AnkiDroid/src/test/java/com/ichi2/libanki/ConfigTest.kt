12bce36712d212fa14e06e777e8886c8908c69cf
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-09-15 01:47:49 +0100

    Implement ConfigV16  V16 implementation of Config Untested

---
+++ AnkiDroid/src/test/java/com/ichi2/libanki/ConfigTest.kt
@@ -0,0 +1,58 @@
+/*
+ *  Copyright (c) 2021 David Allison <davidallisongithub@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ichi2.libanki
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.anki.RobolectricTest
+import com.ichi2.utils.JSONObject
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ConfigTest : RobolectricTest() {
+
+    @Test
+    fun string_serialization() {
+        assertThat(col.get_config_string("sortType"), equalTo("noteFld"))
+
+        assertThat(col.conf.getString("sortType"), equalTo("noteFld"))
+
+        col.set_config("sortType", "noteFld2")
+
+        assertThat(col.get_config_string("sortType"), equalTo("noteFld2"))
+    }
+
+    @Test
+    fun has_config_not_null() {
+        // empty
+        assertThat("no key - false", col.has_config_not_null("aa"), equalTo(false))
+
+        val json = col.conf
+        json.put("aa", JSONObject.NULL)
+        col.conf = json
+
+        assertThat("has key but null - false", col.has_config_not_null("aa"), equalTo(false))
+
+        col.set_config("aa", "bb")
+        assertThat("has key with value - true", col.has_config_not_null("aa"), equalTo(true))
+        col.remove_config("aa")
+
+        assertThat("key removed", col.has_config_not_null("aa"), equalTo(false))
+    }
+}
