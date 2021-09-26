e15c3ed12cc008c8c2d33896e869f4b4b90ca1fa
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-09-26 21:00:21 +0100

    Extract broken "decks" tests to LegacyDecksTest
    We've moved most of the business logic to the V16 backend, so we
    don't need to test these

--- AnkiDroid/src/test/java/com/ichi2/libanki/DecksTest.java
+++ AnkiDroid/src/test/java/com/ichi2/libanki/DecksTest.java
@@ -33,18 +33,6 @@ public class DecksTest extends RobolectricTest {
             "cmxieunwoogyxsctnjmv::abcdefgh::ZYXW",
             "cmxieunwoogyxsctnjmv::INSBGDS",
     };
-
-    @Test
-    public void duplicateName() {
-        DeckManager decks = getCol().getDecks();
-        decks.load("{\"2\": {\"name\": \"A\", \"id\":2}, \"3\": {\"name\": \"A\", \"id\":3}, \"4\": {\"name\": \"A::B\", \"id\":4}}", "{}");
-        decks.checkIntegrity();
-        JSONObject deckA = decks.byName("A");
-        Asserts.notNull(deckA, "A deck with name \"A\" should still exists");
-        assertThat("A deck with name \"A\" should have name \"A\"", deckA.getString("name"), is("A"));
-        JSONObject deckAPlus = decks.byName("A+");
-        Asserts.notNull(deckAPlus, "A deck with name \"A+\" should still exists");
-    }
     @Test
     public void ensureDeckList() {
         DeckManager decks = getCol().getDecks();
@@ -264,55 +252,6 @@ public class DecksTest extends RobolectricTest {
         assertThat(filtered_config.isStd(), is(Boolean.valueOf(false)));
         assertThat(filtered_config.isDyn(), is(Boolean.valueOf(true)));
 
-    }
-
-    @Test
-    public void testEnsureParents() throws DeckRenameException {
-        Collection col = getCol();
-        Decks decks = (Decks) col.getDecks();
-        addDeck("test");
-        String subsubdeck_name = decks._ensureParents("  tESt :: sub :: subdeck");
-        assertEquals("test::sub:: subdeck", subsubdeck_name);// Only parents are renamed, not the last deck.
-        assertNotNull(decks.byName("test::sub"));
-        assertNull(decks.byName("test::sub:: subdeck"));
-        assertNull(decks.byName("  test :: sub :: subdeck"));
-        assertNull(decks.byName("  test :: sub "));
-
-        decks.newDyn("filtered");
-        assertThrows(DeckRenameException.class, () -> decks._ensureParents("filtered:: sub :: subdeck"));
-    }
-
-    @Test
-    public void descendantOfFiltered() throws DeckRenameException {
-        Collection col = getCol();
-        DeckManager decks = col.getDecks();
-        long filtered_id = decks.newDyn("filtered");
-        assertThrows(DeckRenameException.class,  () -> decks.id("filtered::subdeck::subsubdeck"));
-
-        long subdeck_id = decks.id_safe("filtered::subdeck::subsubdeck");
-        Deck subdeck = decks.get(subdeck_id);
-        assertEquals("filtered'::subdeck::subsubdeck", subdeck.getString("name"));
-    }
-
-    @Test
-    public void testEnsureParentsNotFiltered() throws DeckRenameException {
-        Collection col = getCol();
-        Decks decks = (Decks) col.getDecks();
-        addDeck("test");
-        String subsubdeck_name = decks._ensureParentsNotFiltered("  tESt :: sub :: subdeck");
-        assertEquals("test::sub:: subdeck", subsubdeck_name);// Only parents are renamed, not the last deck.
-        assertNotNull(decks.byName("test::sub"));
-        assertNull(decks.byName("test::sub:: subdeck"));
-        assertNull(decks.byName("  test :: sub :: subdeck"));
-        assertNull(decks.byName("  test :: sub "));
-
-        decks.newDyn("filtered");
-        String filtered_subdeck_name = decks._ensureParentsNotFiltered("filtered:: sub :: subdeck");
-        assertEquals("filtered'::sub:: subdeck", filtered_subdeck_name);// Only parents are renamed, not the last deck.
-        assertNotNull(decks.byName("filtered'::sub"));
-        assertNotNull(decks.byName("filtered'"));
-        assertNull(decks.byName("filtered::sub:: subdeck"));
-        assertNull(decks.byName("filtered::sub"));
     }
 
     @Test
