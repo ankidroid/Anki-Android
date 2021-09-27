4d5e2069b7ab1c2e59fa7e0ae6fa36b303412dfa
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-09-27 03:46:52 +0100

    nf: extract CollectionData
    Better than using an array, will allow for better comparisons
    in v16

--- AnkiDroid/src/test/java/com/ichi2/libanki/StorageTest.java
+++ AnkiDroid/src/test/java/com/ichi2/libanki/StorageTest.java
@@ -22,17 +22,18 @@ import com.ichi2.anki.CollectionHelper;
 import com.ichi2.anki.RobolectricTest;
 import com.ichi2.utils.JSONObject;
 
-import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
 import androidx.core.util.Pair;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -54,9 +55,9 @@ public class StorageTest extends RobolectricTest {
 
 
     @Test
-    public void compareNewDatabases() throws JSONException {
+    public void compareNewDatabases() {
 
-        List<Object> expected = getResults();
+        CollectionData expected = getResults();
 
         // If you don't tear down the database you'll get unexpected IllegalStateExceptions related to connections
         CollectionHelper.getInstance().closeCollection(false, "compareNewDatabases");
@@ -66,42 +67,9 @@ public class StorageTest extends RobolectricTest {
 
         Storage.setUseBackend(true);
 
-        List<Object> actual = getResults();
+        CollectionData actual = getResults();
 
-
-        for (int i = 0; i < expected.size(); i++) {
-            if (i == 1 || i == 2 || i == 3) {
-                continue;
-            }
-            if (i == 8) {
-                JSONObject actualJson = new JSONObject(actual.get(i).toString());
-                JSONObject expectedJson = new JSONObject(expected.get(i).toString());
-
-                remove(actualJson, expectedJson, "curModel");
-                remove(actualJson, expectedJson, "creationOffset");
-                remove(actualJson, expectedJson, "localOffset");
-
-                assertThat(actualJson.toString(), is(expectedJson.toString()));
-                continue;
-            }
-
-            if (i == 9) {
-                JSONObject actualJson = new JSONObject(actual.get(i).toString());
-                JSONObject expectedJson = new JSONObject(expected.get(i).toString());
-
-                renameKeys(actualJson);
-                renameKeys(expectedJson);
-
-                for (String k : actualJson) {
-                    remove(actualJson.getJSONObject(k), expectedJson.getJSONObject(k), "id");
-                }
-
-                assertThat(actualJson.toString(4), is(expectedJson.toString(4)));
-                continue;
-            }
-
-            assertThat(Integer.toString(i), actual.get(i), is(expected.get(i)));
-        }
+        actual.assertEqualTo(expected);
     }
 
 
@@ -119,7 +87,7 @@ public class StorageTest extends RobolectricTest {
             keys.add(new Pair<>(name, actualJson.getJSONObject(name).getString("name")));
         }
 
-        keys.sort((x,y) -> x.second.compareTo(y.second));
+        keys.sort(Comparator.comparing(x -> x.second));
 
         for (int i = 0; i < keys.size(); i++) {
             String keyName = keys.get(i).first;
@@ -130,17 +98,100 @@ public class StorageTest extends RobolectricTest {
     }
 
 
-    protected List<Object> getResults() {
-        List<Object> results = new ArrayList<>();
+    protected CollectionData getResults() {
+        CollectionData results = new CollectionData();
         try (Cursor c = getCol().getDb().query("select * from col")) {
             c.moveToFirst();
 
 
             for (int i = 0; i < c.getColumnCount(); i++) {
-                results.add(c.getString(i));
+                results.loadV11(i, c.getString(i));
             }
         }
         return results;
+    }
+
+    public class CollectionData {
+        String mId;
+        String mCrt;
+        String mMod;
+        String mScm;
+        String mVer;
+        String mDty;
+        String mUsn;
+        String mLs;
+        String mConf;
+        String mModels;
+        String mDecks;
+        String mDConf;
+        String mTags;
+
+
+        public void loadV11(int i, String string) {
+            switch (i) {
+                case 0: mId = string; return;
+                case 1: mCrt = string; return;
+                case 2: mMod = string; return;
+                case 3: mScm = string; return;
+                case 4: mVer = string; return;
+                case 5: mDty = string; return;
+                case 6: mUsn = string; return;
+                case 7: mLs = string; return;
+                case 8: mConf = string; return;
+                case 9: mModels = string; return;
+                case 10: mDecks = string; return;
+                case 11: mDConf = string; return;
+                case 12: mTags = string; return;
+                default: throw new IllegalStateException("unknown i: " + i);
+            }
+        }
+
+
+        public void assertEqualTo(CollectionData expected) {
+            assertThat(this.mId, equalTo(expected.mId));
+            // ignore due to timestamp: mCrt
+            // ignore due to timestamp: mMod
+            // ignore due to timestamp: mScm
+            assertThat(this.mVer, equalTo(expected.mVer));
+            assertThat(this.mDty, equalTo(expected.mDty));
+            assertThat(this.mUsn, equalTo(expected.mUsn));
+            assertThat(this.mLs, equalTo(expected.mLs));
+
+            assertConfEqual(expected);
+
+            assertModelsEqual(expected);
+
+            assertThat(this.mDecks, equalTo(expected.mDecks));
+            assertThat(this.mDConf, equalTo(expected.mDConf));
+            assertThat(this.mTags, equalTo(expected.mTags));
+        }
+
+
+        protected void assertModelsEqual(CollectionData expected) {
+            JSONObject actualJson = new JSONObject(this.mModels);
+            JSONObject expectedJson = new JSONObject(expected.mModels);
+
+            renameKeys(actualJson);
+            renameKeys(expectedJson);
+
+            for (String k : actualJson) {
+                remove(actualJson.getJSONObject(k), expectedJson.getJSONObject(k), "id");
+            }
+
+            assertThat(actualJson.toString(4), is(expectedJson.toString(4)));
+        }
+
+
+        protected void assertConfEqual(CollectionData expected) {
+            JSONObject actualJson = new JSONObject(this.mConf);
+            JSONObject expectedJson = new JSONObject(expected.mConf);
+
+            remove(actualJson, expectedJson, "curModel");
+            remove(actualJson, expectedJson, "creationOffset");
+            remove(actualJson, expectedJson, "localOffset");
+
+            assertThat(actualJson.toString(), is(expectedJson.toString()));
+        }
     }
 
 }
