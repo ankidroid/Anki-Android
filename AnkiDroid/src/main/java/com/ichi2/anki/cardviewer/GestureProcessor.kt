82d4778b91b0d7eab323fe13ed73ed15b19b97fc
Timo Obereder <timo.obereder@gmail.com>
2021-10-06 20:16:06 +0200

    Change to kotlin code

--- AnkiDroid/src/main/java/com/ichi2/anki/cardviewer/GestureProcessor.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/cardviewer/GestureProcessor.kt
@@ -13,149 +13,121 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.anki.cardviewer
 
-package com.ichi2.anki.cardviewer;
+import android.content.SharedPreferences
+import com.ichi2.anki.reviewer.GestureMapper
 
-import android.content.SharedPreferences;
-
-import com.ichi2.anki.reviewer.GestureMapper;
-
-import static com.ichi2.anki.cardviewer.ViewerCommand.COMMAND_NOTHING;
-
-public class GestureProcessor {
-    private ViewerCommand mGestureDoubleTap;
-    private ViewerCommand mGestureLongclick;
-    private ViewerCommand mGestureSwipeUp;
-    private ViewerCommand mGestureSwipeDown;
-    private ViewerCommand mGestureSwipeLeft;
-    private ViewerCommand mGestureSwipeRight;
-    private ViewerCommand mGestureTapLeft;
-    private ViewerCommand mGestureTapRight;
-    private ViewerCommand mGestureTapTop;
-    private ViewerCommand mGestureTapBottom;
-    private ViewerCommand mGestureTapTopLeft;
-    private ViewerCommand mGestureTapTopRight;
-    private ViewerCommand mGestureTapCenter;
-    private ViewerCommand mGestureTapBottomLeft;
-    private ViewerCommand mGestureTapBottomRight;
-
-    private final GestureMapper mGestureMapper = new GestureMapper();
-
-    private final ViewerCommand.CommandProcessor mProcessor;
-
-    private boolean mEnabled = false;
-
-    public GestureProcessor(ViewerCommand.CommandProcessor processor) {
-        mProcessor = processor;
-    }
-
-    public void init(SharedPreferences preferences) {
-        mEnabled = preferences.getBoolean("gestures", false);
-        mGestureDoubleTap = Gesture.DOUBLE_TAP.fromPreference(preferences);
-        mGestureLongclick = Gesture.LONG_TAP.fromPreference(preferences);
-
-        mGestureSwipeUp = Gesture.SWIPE_UP.fromPreference(preferences);
-        mGestureSwipeDown = Gesture.SWIPE_DOWN.fromPreference(preferences);
-        mGestureSwipeLeft = Gesture.SWIPE_LEFT.fromPreference(preferences);
-        mGestureSwipeRight = Gesture.SWIPE_RIGHT.fromPreference(preferences);
-
-        mGestureMapper.init(preferences);
-
-        mGestureTapLeft = Gesture.TAP_LEFT.fromPreference(preferences);
-        mGestureTapRight = Gesture.TAP_RIGHT.fromPreference(preferences);
-        mGestureTapTop = Gesture.TAP_TOP.fromPreference(preferences);
-        mGestureTapBottom = Gesture.TAP_BOTTOM.fromPreference(preferences);
-
-        boolean useCornerTouch = preferences.getBoolean("gestureCornerTouch", false);
-        if (useCornerTouch) {
-            mGestureTapTopLeft = Gesture.TAP_TOP_LEFT.fromPreference(preferences);
-            mGestureTapTopRight = Gesture.TAP_TOP_RIGHT.fromPreference(preferences);
-            mGestureTapCenter = Gesture.TAP_CENTER.fromPreference(preferences);
-            mGestureTapBottomLeft = Gesture.TAP_BOTTOM_LEFT.fromPreference(preferences);
-            mGestureTapBottomRight = Gesture.TAP_BOTTOM_RIGHT.fromPreference(preferences);
-        }
-    }
-
-    public boolean onTap(int height, int width, float posX, float posY) {
-        Gesture gesture = mGestureMapper.gesture(height, width, posX, posY);
-
-        if (gesture == null) {
-            return false;
-        }
-
-        return execute(gesture);
-    }
-
-    public boolean onDoubleTap() {
-        return execute(Gesture.DOUBLE_TAP);
-    }
-
-
-    public boolean onLongTap() {
-        return execute(Gesture.LONG_TAP);
-    }
-
-    public boolean onFling(float dx, float dy, float velocityX, float velocityY, boolean isSelecting, boolean isXScrolling, boolean isYScrolling) {
-        Gesture gesture = this.mGestureMapper.gesture(dx, dy, velocityX, velocityY, isSelecting, isXScrolling, isYScrolling);
-
-        return execute(gesture);
-    }
-
-
-    protected boolean execute(Gesture gesture) {
-        ViewerCommand command = mapGestureToCommand(gesture);
-        return mProcessor.executeCommand(command);
-    }
-
-
-    private ViewerCommand mapGestureToCommand(Gesture gesture) {
-        switch (gesture) {
-            case SWIPE_UP: return mGestureSwipeUp;
-            case SWIPE_DOWN: return mGestureSwipeDown;
-            case SWIPE_LEFT: return mGestureSwipeLeft;
-            case SWIPE_RIGHT: return mGestureSwipeRight;
-            case TAP_TOP: return mGestureTapTop;
-            case TAP_TOP_LEFT: return mGestureTapTopLeft;
-            case TAP_TOP_RIGHT: return mGestureTapTopRight;
-            case TAP_LEFT: return mGestureTapLeft;
-            case TAP_CENTER: return mGestureTapCenter;
-            case TAP_RIGHT: return mGestureTapRight;
-            case TAP_BOTTOM: return mGestureTapBottom;
-            case TAP_BOTTOM_LEFT: return mGestureTapBottomLeft;
-            case TAP_BOTTOM_RIGHT: return mGestureTapBottomRight;
-            case DOUBLE_TAP: return mGestureDoubleTap;
-            case LONG_TAP: return mGestureLongclick;
-            default: return COMMAND_NOTHING;
-        }
-    }
-
+class GestureProcessor(private val processor: ViewerCommand.CommandProcessor?) {
+    private var gestureDoubleTap: ViewerCommand? = null
+    private var gestureLongclick: ViewerCommand? = null
+    private var gestureSwipeUp: ViewerCommand? = null
+    private var gestureSwipeDown: ViewerCommand? = null
+    private var gestureSwipeLeft: ViewerCommand? = null
+    private var gestureSwipeRight: ViewerCommand? = null
+    private var gestureTapLeft: ViewerCommand? = null
+    private var gestureTapRight: ViewerCommand? = null
+    private var gestureTapTop: ViewerCommand? = null
+    private var gestureTapBottom: ViewerCommand? = null
+    private var gestureTapTopLeft: ViewerCommand? = null
+    private var gestureTapTopRight: ViewerCommand? = null
+    private var gestureTapCenter: ViewerCommand? = null
+    private var gestureTapBottomLeft: ViewerCommand? = null
+    private var gestureTapBottomRight: ViewerCommand? = null
+    private val gestureMapper = GestureMapper()
 
     /**
      * Whether the class has been enabled.
      * This requires the "gestures" preference is enabled,
-     * and {@link GestureProcessor#init(SharedPreferences)} has been called
+     * and [GestureProcessor.init] has been called
      */
-    public boolean isEnabled() {
-        return mEnabled;
+    var isEnabled = false
+        private set
+
+    fun init(preferences: SharedPreferences) {
+        isEnabled = preferences.getBoolean("gestures", false)
+        gestureDoubleTap = Gesture.DOUBLE_TAP.fromPreference(preferences)
+        gestureLongclick = Gesture.LONG_TAP.fromPreference(preferences)
+        gestureSwipeUp = Gesture.SWIPE_UP.fromPreference(preferences)
+        gestureSwipeDown = Gesture.SWIPE_DOWN.fromPreference(preferences)
+        gestureSwipeLeft = Gesture.SWIPE_LEFT.fromPreference(preferences)
+        gestureSwipeRight = Gesture.SWIPE_RIGHT.fromPreference(preferences)
+        gestureMapper.init(preferences)
+        gestureTapLeft = Gesture.TAP_LEFT.fromPreference(preferences)
+        gestureTapRight = Gesture.TAP_RIGHT.fromPreference(preferences)
+        gestureTapTop = Gesture.TAP_TOP.fromPreference(preferences)
+        gestureTapBottom = Gesture.TAP_BOTTOM.fromPreference(preferences)
+        val useCornerTouch = preferences.getBoolean("gestureCornerTouch", false)
+        if (useCornerTouch) {
+            gestureTapTopLeft = Gesture.TAP_TOP_LEFT.fromPreference(preferences)
+            gestureTapTopRight = Gesture.TAP_TOP_RIGHT.fromPreference(preferences)
+            gestureTapCenter = Gesture.TAP_CENTER.fromPreference(preferences)
+            gestureTapBottomLeft = Gesture.TAP_BOTTOM_LEFT.fromPreference(preferences)
+            gestureTapBottomRight = Gesture.TAP_BOTTOM_RIGHT.fromPreference(preferences)
+        }
     }
 
+    fun onTap(height: Int, width: Int, posX: Float, posY: Float): Boolean? {
+        val gesture = gestureMapper.gesture(height, width, posX, posY) ?: return false
+        return execute(gesture)
+    }
+
+    fun onDoubleTap(): Boolean? {
+        return execute(Gesture.DOUBLE_TAP)
+    }
+
+    fun onLongTap(): Boolean? {
+        return execute(Gesture.LONG_TAP)
+    }
+
+    fun onFling(dx: Float, dy: Float, velocityX: Float, velocityY: Float, isSelecting: Boolean, isXScrolling: Boolean, isYScrolling: Boolean): Boolean? {
+        val gesture = gestureMapper.gesture(dx, dy, velocityX, velocityY, isSelecting, isXScrolling, isYScrolling)
+        return execute(gesture)
+    }
+
+    private fun execute(gesture: Gesture): Boolean? {
+        val command = mapGestureToCommand(gesture)
+        return if (command != null) {
+            processor?.executeCommand(command)
+        } else {
+            false
+        }
+    }
+
+    private fun mapGestureToCommand(gesture: Gesture): ViewerCommand? {
+        return when (gesture) {
+            Gesture.SWIPE_UP -> gestureSwipeUp
+            Gesture.SWIPE_DOWN -> gestureSwipeDown
+            Gesture.SWIPE_LEFT -> gestureSwipeLeft
+            Gesture.SWIPE_RIGHT -> gestureSwipeRight
+            Gesture.TAP_TOP -> gestureTapTop
+            Gesture.TAP_TOP_LEFT -> gestureTapTopLeft
+            Gesture.TAP_TOP_RIGHT -> gestureTapTopRight
+            Gesture.TAP_LEFT -> gestureTapLeft
+            Gesture.TAP_CENTER -> gestureTapCenter
+            Gesture.TAP_RIGHT -> gestureTapRight
+            Gesture.TAP_BOTTOM -> gestureTapBottom
+            Gesture.TAP_BOTTOM_LEFT -> gestureTapBottomLeft
+            Gesture.TAP_BOTTOM_RIGHT -> gestureTapBottomRight
+            Gesture.DOUBLE_TAP -> gestureDoubleTap
+            Gesture.LONG_TAP -> gestureLongclick
+        }
+    }
 
     /**
      * Whether one of the provided gestures is bound
      * @param gestures the gestures to check
-     * @return <code>false</code> if none of the gestures are bound. <code>true</code> otherwise
+     * @return `false` if none of the gestures are bound. `true` otherwise
      */
-    public boolean isBound(Gesture... gestures) {
-        if (!isEnabled()) {
-            return false;
+    fun isBound(vararg gestures: Gesture): Boolean {
+        if (!isEnabled) {
+            return false
         }
-
-        for (Gesture gesture : gestures) {
-            if (mapGestureToCommand(gesture) != COMMAND_NOTHING) {
-                return true;
+        for (gesture in gestures) {
+            if (mapGestureToCommand(gesture) != ViewerCommand.COMMAND_NOTHING) {
+                return true
             }
         }
-
-        return false;
+        return false
     }
 }
