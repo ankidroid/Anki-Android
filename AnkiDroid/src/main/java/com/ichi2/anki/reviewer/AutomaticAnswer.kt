9962e649cfd8f7d69a83c8c4d65aa895958ee25c
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-10-07 02:32:59 +0100

    docs: AutomaticAnswer

--- AnkiDroid/src/main/java/com/ichi2/anki/reviewer/AutomaticAnswer.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/reviewer/AutomaticAnswer.kt
@@ -21,6 +21,37 @@ import android.os.Handler
 import androidx.annotation.VisibleForTesting
 import com.ichi2.libanki.Collection
 
+/**
+ * AnkiDroid contains a setting: "Automatic display answer" which displays the
+ * question/answer automatically after a set period of time.
+ *
+ * ## Settings
+ * See: [AutomaticAnswerSettings]
+ *
+ * ## Implementation
+ *
+ * If "Enabled" is disabled, then nothing happens
+ * If Time to show Answer is 0, the automatic flipping code is disabled
+ * If Time to show next question is 0, the automatic answering code is disabled
+ *
+ * When a card is displayed (Front)
+ *
+ * If it has neither TTS nor audio, schedule the answer to display in "Time to show answer" seconds
+ * If it has audio, calculate the time for the audio to play
+ *   * schedule the answer to display in "Time to show answer" + "audio runtime" seconds
+ * If it has TTS, add a handler to the completion of TTS
+ *   * schedule the answer to display in "Time to show answer" seconds
+ *
+ * Do the same for the Back, but use the "Time to show next question" setting.
+ *
+ * If the "next question" is shown, "Again" is pressed.
+ *
+ * The implementation originally had multiple bugs regarding double presses of the answer buttons.
+ * This is because the user can press show the card at the same instant that the automatic code executes
+ *
+ * This has been worked around via the calls: [onSelectEase] and [onShowAnswer].
+ * Both of which clear the task queue for the question ([onSelectEase] or vice-versa)
+ */
 class AutomaticAnswer(
     target: AutomaticallyAnswered,
     private val settings: AutomaticAnswerSettings
@@ -47,58 +78,70 @@ class AutomaticAnswer(
         timeoutHandler.postDelayed(showAnswerTask, delay)
     }
 
-    private fun stopShowingQuestion() {
+    private fun stopShowQuestionTask() {
         timeoutHandler.removeCallbacks(showQuestionTask)
     }
 
-    private fun stopShowingAnswer() {
+    private fun stopShowAnswerTask() {
         timeoutHandler.removeCallbacks(showAnswerTask)
     }
 
     fun stopAll() {
-        stopShowingAnswer()
-        stopShowingQuestion()
+        stopShowAnswerTask()
+        stopShowQuestionTask()
     }
 
+    /** Stop any "Automatic show answer" tasks in order to avoid race conditions */
     fun onDisplayQuestion() {
         if (!settings.useTimer) return
         if (!settings.autoAdvanceAnswer) return
 
-        stopShowingAnswer()
+        stopShowAnswerTask()
     }
 
+    /** Stop any "Automatic show question" tasks in order to avoid race conditions */
     fun onDisplayAnswer() {
         if (!settings.useTimer) return
         if (!settings.autoAdvanceQuestion) return
 
-        stopShowingQuestion()
+        stopShowQuestionTask()
     }
 
     // region TODO: These attempt to stop a race condition between a manual answer and the automated answer
     // I don't believe this is thread-safe
 
+    /**  Stops the current automatic display if the user has selected an answer */
     fun onSelectEase() {
-        stopShowingQuestion()
+        stopShowQuestionTask()
     }
 
+    /** Stops the current automatic display if the user has flipped the card */
     fun onShowAnswer() {
-        stopShowingAnswer()
+        stopShowAnswerTask()
     }
 
     // endregion
 
+    /**
+     * If enabled in preferences, call [AutomaticallyAnswered.automaticShowAnswer]
+     * after a user-specified duration, plus an additional delay for media
+     */
     @JvmOverloads
-    fun scheduleDisplayAnswer(additionalDelay: Long = 0) {
+    fun scheduleAutomaticDisplayAnswer(additionalDelay: Long = 0) {
         if (!settings.useTimer) return
         if (!settings.autoAdvanceAnswer) return
         delayedShowAnswer(settings.answerDelayMilliseconds + additionalDelay)
     }
 
+    /**
+     * If enabled in preferences, call [AutomaticallyAnswered.automaticShowQuestion]
+     * after a user-specified duration, plus an additional delay for media
+     */
     @JvmOverloads
-    fun scheduleDisplayQuestion(additionalDelay: Long = 0) {
+    fun scheduleAutomaticDisplayQuestion(additionalMediaDelay: Long = 0) {
         if (!settings.useTimer) return
         if (!settings.autoAdvanceQuestion) return
-        delayedShowQuestion(settings.questionDelayMilliseconds + additionalDelay)
+        delayedShowQuestion(settings.questionDelayMilliseconds + additionalMediaDelay)
     }
 
     fun isEnabled(): Boolean {
@@ -124,6 +167,21 @@ class AutomaticAnswer(
     }
 }
 
+/**
+ * ### Reviewing - Automatic display answer
+ *
+ * * Enabled
+ * * Time to show answer (20s)
+ * * Time to show next question (60s)
+ *
+ * ### Deck Options - Reviews - Automatic Display Answer
+ *
+ * These settings also exist in an options group
+ * Although they're under "reviews", it counts for all cards
+ *
+ * * Use General settings (using the settings above - enabled by default)
+ * * Enabled, etc...
+ */
 class AutomaticAnswerSettings(
     @get:JvmName("useTimer") val useTimer: Boolean = false,
     private val questionDelaySeconds: Int = 60,
@@ -138,6 +196,11 @@ class AutomaticAnswerSettings(
     val autoAdvanceQuestion; get() = questionDelaySeconds > 0
 
     companion object {
+        /**
+         * Obtains the options for [AutomaticAnswer] in the deck config ("review" section)
+         * @return null if the deck is dynamic (use global settings),
+         * or if "useGeneralTimeoutSettings" is set
+         */
         @JvmStatic
         fun queryDeckSpecificOptions(
             col: Collection,
