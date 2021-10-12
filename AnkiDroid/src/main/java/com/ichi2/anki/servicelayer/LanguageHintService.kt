e44f1fccb22546aa5f07aa05aa5dcc5d7e0e7e84
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-10-12 01:44:53 +0100

    refactor: extract getLanguageHintForField
    Moves code to a concern, allows for reuse

---
+++ AnkiDroid/src/main/java/com/ichi2/anki/servicelayer/LanguageHintService.kt
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (c) 2021 David Allison <davidallisongithub@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ichi2.anki.servicelayer
+
+import android.widget.EditText
+import androidx.annotation.CheckResult
+import com.ichi2.utils.JSONObject
+import java.util.*
+
+/**
+ * The language that a keyboard should open with when an [EditText] is selected
+ * Used for a workflow improvement when adding a note
+ */
+typealias LanguageHint = Locale
+
+object LanguageHintService {
+    @JvmStatic
+    @CheckResult
+    fun getLanguageHintForField(field: JSONObject): LanguageHint? {
+        if (!field.has("ad-hint-locale")) {
+            return null
+        }
+        return Locale.forLanguageTag(field.getString("ad-hint-locale"))
+    }
+}
