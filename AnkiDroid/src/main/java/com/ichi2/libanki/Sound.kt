253706a403cae606e6db455055a8b44e3b7ed662
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-10-30 09:49:32 +0100

    Convert sound.py from Anki Desktop
    https://github.com/ankitects/anki/blob/2.1.34/pylib/anki/sound.py

---
+++ AnkiDroid/src/main/java/com/ichi2/libanki/Sound.kt
@@ -0,0 +1,53 @@
+/*
+ *  Copyright (c) 2021 David Allison <davidallisongithub@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  This file incorporates code under the following license
+ *  https://github.com/ankitects/anki/blob/2.1.34/pylib/anki/sound.py
+ *
+ *    Copyright: Ankitects Pty Ltd and contributors
+ *    License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+ */
+
+@file:Suppress("unused")
+
+package com.ichi2.libanki
+
+/**
+ * Records information about a text to speech tag.
+ */
+data class TTSTag(
+    val fieldText: str,
+    val lang: str,
+    val voices: List<str>,
+    val speed: Float,
+    /** each arg should be in the form 'foo=bar' */
+    val otherArgs: List<str>
+) : AvTag()
+
+/**
+ * Contains the filename inside a [sound:...] tag.
+ *
+ * Video files also use [sound:...].
+ */
+data class SoundOrVideoTag(val filename: str) : AvTag()
+
+/** In python, this is a union of [TTSTag] and [SoundOrVideoTag] */
+open class AvTag
+
+/* Methods */
+
+val AV_REF_RE = Regex("\\[anki:(play:(.):(\\d+))]")
+
+fun strip_av_refs(text: str) = AV_REF_RE.replace("", text)
