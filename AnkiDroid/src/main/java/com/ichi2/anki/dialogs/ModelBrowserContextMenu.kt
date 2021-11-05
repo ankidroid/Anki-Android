e002fbb734fb9d01e430546a2e227e292ed8f404
Piyush Goel <piyushgoel2008@gmail.com>
2021-11-05 10:47:14 +0530

    Migrated ModelBrowserContextMenu to kotlin

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/ModelBrowserContextMenu.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/ModelBrowserContextMenu.kt
@@ -1,46 +1,40 @@
 //noinspection MissingCopyrightHeader #8659
-package com.ichi2.anki.dialogs;
+package com.ichi2.anki.dialogs
 
-import android.app.Dialog;
-import android.os.Bundle;
+import android.app.Dialog
+import android.os.Bundle
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.MaterialDialog.ListCallback
+import com.ichi2.anki.R
+import com.ichi2.anki.analytics.AnalyticsDialogFragment
 
-import com.afollestad.materialdialogs.MaterialDialog;
-import com.ichi2.anki.R;
-import com.ichi2.anki.analytics.AnalyticsDialogFragment;
-
-import androidx.annotation.NonNull;
-
-public class ModelBrowserContextMenu extends AnalyticsDialogFragment {
-
-    public final static int MODEL_TEMPLATE = 0;
-    public final static int MODEL_RENAME = 1;
-    public final static int MODEL_DELETE = 2;
-
-    private static MaterialDialog.ListCallback mContextMenuListener;
-
-    public static ModelBrowserContextMenu newInstance(String label, MaterialDialog.ListCallback contextMenuListener) {
-        mContextMenuListener = contextMenuListener;
-        ModelBrowserContextMenu n = new ModelBrowserContextMenu();
-        Bundle b = new Bundle();
-        b.putString("label", label);
-        n.setArguments(b);
-        return n;
+class ModelBrowserContextMenu : AnalyticsDialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        super.onCreate(savedInstanceState)
+        val entries = arrayOfNulls<String>(3)
+        entries[MODEL_TEMPLATE] = resources.getString(R.string.model_browser_template)
+        entries[MODEL_RENAME] = resources.getString(R.string.model_browser_rename)
+        entries[MODEL_DELETE] = resources.getString(R.string.model_browser_delete)
+        return MaterialDialog.Builder(requireActivity())
+            .title(requireArguments().getString("label")!!)
+            .items(*entries)
+            .itemsCallback(mContextMenuListener!!)
+            .build()
     }
 
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        String[] entries = new String[3];
-        entries[MODEL_TEMPLATE] = getResources().getString(R.string.model_browser_template);
-        entries[MODEL_RENAME] = getResources().getString(R.string.model_browser_rename);
-        entries[MODEL_DELETE] = getResources().getString(R.string.model_browser_delete);
-
-        return new MaterialDialog.Builder(getActivity())
-                .title(getArguments().getString("label"))
-                .items(entries)
-                .itemsCallback(mContextMenuListener)
-                .build();
+    companion object {
+        const val MODEL_TEMPLATE = 0
+        const val MODEL_RENAME = 1
+        const val MODEL_DELETE = 2
+        private var mContextMenuListener: ListCallback? = null
+        @JvmStatic
+        fun newInstance(label: String?, contextMenuListener: ListCallback?): ModelBrowserContextMenu {
+            mContextMenuListener = contextMenuListener
+            val n = ModelBrowserContextMenu()
+            val b = Bundle()
+            b.putString("label", label)
+            n.arguments = b
+            return n
+        }
     }
 }
