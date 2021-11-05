b71890bdfb2f207d7621ed24fcb48e9e4849d6f7
Piyush Goel <piyushgoel2008@gmail.com>
2021-11-05 21:55:42 +0530

    Migrated ModelEditorContextMenu to kotlin.

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/ModelEditorContextMenu.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/ModelEditorContextMenu.kt
@@ -1,68 +1,63 @@
 //noinspection MissingCopyrightHeader #8659
-package com.ichi2.anki.dialogs;
 
-import android.app.Dialog;
-import android.os.Build;
-import android.os.Bundle;
+package com.ichi2.anki.dialogs
 
-import com.afollestad.materialdialogs.MaterialDialog;
-import com.ichi2.anki.R;
-import com.ichi2.anki.analytics.AnalyticsDialogFragment;
+import android.app.Dialog
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.MaterialDialog.ListCallback
+import com.ichi2.anki.R
+import com.ichi2.anki.analytics.AnalyticsDialogFragment
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-
-public class ModelEditorContextMenu extends AnalyticsDialogFragment {
-
-    public final static int FIELD_REPOSITION = 0;
-    public final static int SORT_FIELD = 1;
-    public final static int FIELD_RENAME = 2;
-    public final static int FIELD_DELETE = 3;
-    public final static int FIELD_TOGGLE_STICKY = 4;
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public final static int FIELD_ADD_LANGUAGE_HINT = 5;
-
-
-    private static MaterialDialog.ListCallback mContextMenuListener;
-
-    public static ModelEditorContextMenu newInstance(String label, MaterialDialog.ListCallback contextMenuListener) {
-        ModelEditorContextMenu n = new ModelEditorContextMenu();
-        mContextMenuListener = contextMenuListener;
-        Bundle b = new Bundle();
-        b.putString("label", label);
-        mContextMenuListener = contextMenuListener;
-        n.setArguments(b);
-        return n;
+class ModelEditorContextMenu : AnalyticsDialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        super.onCreate(savedInstanceState)
+        val entries = arrayOfNulls<String>(entryCount)
+        entries[FIELD_REPOSITION] = resources.getString(R.string.model_field_editor_reposition_menu)
+        entries[SORT_FIELD] = resources.getString(R.string.model_field_editor_sort_field)
+        entries[FIELD_RENAME] = resources.getString(R.string.model_field_editor_rename)
+        entries[FIELD_DELETE] = resources.getString(R.string.model_field_editor_delete)
+        entries[FIELD_TOGGLE_STICKY] = resources.getString(R.string.model_field_editor_toggle_sticky)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            entries[FIELD_ADD_LANGUAGE_HINT] = resources.getString(R.string.model_field_editor_language_hint)
+        }
+        return MaterialDialog.Builder(requireActivity())
+            .title(requireArguments().getString("label")!!)
+            .items(*entries)
+            .itemsCallback(mContextMenuListener!!)
+            .build()
     }
 
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        String[] entries = new String[getEntryCount()];
-        entries[FIELD_REPOSITION] = getResources().getString(R.string.model_field_editor_reposition_menu);
-        entries[SORT_FIELD] = getResources().getString(R.string.model_field_editor_sort_field);
-        entries[FIELD_RENAME] = getResources().getString(R.string.model_field_editor_rename);
-        entries[FIELD_DELETE] = getResources().getString(R.string.model_field_editor_delete);
-        entries[FIELD_TOGGLE_STICKY] = getResources().getString(R.string.model_field_editor_toggle_sticky);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            entries[FIELD_ADD_LANGUAGE_HINT] = getResources().getString(R.string.model_field_editor_language_hint);
+    private val entryCount: Int
+        get() {
+            var entryCount = 5
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                entryCount++
+            }
+            return entryCount
         }
 
-        return new MaterialDialog.Builder(getActivity())
-                .title(getArguments().getString("label"))
-                .items(entries)
-                .itemsCallback(mContextMenuListener)
-                .build();
-    }
+    companion object {
+        const val FIELD_REPOSITION = 0
+        const val SORT_FIELD = 1
+        const val FIELD_RENAME = 2
+        const val FIELD_DELETE = 3
+        const val FIELD_TOGGLE_STICKY = 4
 
-
-    private int getEntryCount() {
-        int entryCount = 5;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            entryCount++;
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        const val FIELD_ADD_LANGUAGE_HINT = 5
+        private var mContextMenuListener: ListCallback? = null
+        @JvmStatic
+        fun newInstance(label: String?, contextMenuListener: ListCallback?): ModelEditorContextMenu {
+            val n = ModelEditorContextMenu()
+            mContextMenuListener = contextMenuListener
+            val b = Bundle()
+            b.putString("label", label)
+            mContextMenuListener = contextMenuListener
+            n.arguments = b
+            return n
         }
-        return entryCount;
     }
 }
