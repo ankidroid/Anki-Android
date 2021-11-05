42fa2112b0f734bebd002869fa2d1935af444cd7
Piyush Goel <piyushgoel2008@gmail.com>
2021-11-05 20:58:44 +0530

    Migrated DeckPickerBackupNoSpaceLeftDialog to kotlin.

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/DeckPickerBackupNoSpaceLeftDialog.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/DeckPickerBackupNoSpaceLeftDialog.kt
@@ -14,38 +14,36 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-package com.ichi2.anki.dialogs;
+package com.ichi2.anki.dialogs
 
-import android.content.res.Resources;
-import android.os.Bundle;
+import android.os.Bundle
+import com.afollestad.materialdialogs.DialogAction
+import com.afollestad.materialdialogs.MaterialDialog
+import com.ichi2.anki.BackupManager
+import com.ichi2.anki.CollectionHelper
+import com.ichi2.anki.DeckPicker
+import com.ichi2.anki.R
+import com.ichi2.anki.analytics.AnalyticsDialogFragment
 
-import com.afollestad.materialdialogs.MaterialDialog;
-import com.ichi2.anki.BackupManager;
-import com.ichi2.anki.CollectionHelper;
-import com.ichi2.anki.DeckPicker;
-import com.ichi2.anki.R;
-import com.ichi2.anki.analytics.AnalyticsDialogFragment;
-
-import androidx.annotation.NonNull;
-
-public class DeckPickerBackupNoSpaceLeftDialog extends AnalyticsDialogFragment {
-    public static DeckPickerBackupNoSpaceLeftDialog newInstance() {
-        return new DeckPickerBackupNoSpaceLeftDialog();
+class DeckPickerBackupNoSpaceLeftDialog : AnalyticsDialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): MaterialDialog {
+        super.onCreate(savedInstanceState)
+        val res = resources
+        val space = BackupManager.getFreeDiscSpace(CollectionHelper.getCollectionPath(activity))
+        return MaterialDialog.Builder(requireActivity())
+            .title(res.getString(R.string.sd_card_almost_full_title))
+            .content(res.getString(R.string.sd_space_warning, space / 1024 / 1024))
+            .positiveText(R.string.dialog_ok)
+            .onPositive { _: MaterialDialog?, _: DialogAction? -> (activity as DeckPicker?)!!.finishWithoutAnimation() }
+            .cancelable(true)
+            .cancelListener { (activity as DeckPicker?)!!.finishWithoutAnimation() }
+            .show()
     }
-    
-    @NonNull
-    @Override
-    public MaterialDialog onCreateDialog(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Resources res = getResources();
-        long space = BackupManager.getFreeDiscSpace(CollectionHelper.getCollectionPath(getActivity()));
-        return new MaterialDialog.Builder(getActivity())
-                .title(res.getString(R.string.sd_card_almost_full_title))
-                .content(res.getString(R.string.sd_space_warning, space/1024/1024))
-                .positiveText(R.string.dialog_ok)
-                .onPositive((dialog, which) -> ((DeckPicker) getActivity()).finishWithoutAnimation())
-                .cancelable(true)
-                .cancelListener(dialog -> ((DeckPicker) getActivity()).finishWithoutAnimation())
-                .show();
+
+    companion object {
+        @JvmStatic
+        fun newInstance(): DeckPickerBackupNoSpaceLeftDialog {
+            return DeckPickerBackupNoSpaceLeftDialog()
+        }
     }
 }
