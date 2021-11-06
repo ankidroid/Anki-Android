59a3d3024217e2055346670b8bd214c95a54af89
codingtosh <codingtosh@gmail.com>
2021-11-07 00:36:15 +0530

    [Kotlin Migration] ExceptionUtil
    Package: com.ichi2.utils

--- AnkiDroid/src/main/java/com/ichi2/utils/ExceptionUtil.kt
+++ AnkiDroid/src/main/java/com/ichi2/utils/ExceptionUtil.kt
@@ -13,73 +13,62 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.utils
 
-package com.ichi2.utils;
+import androidx.annotation.CheckResult
+import java.io.PrintWriter
+import java.io.StringWriter
+import java.lang.StringBuilder
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
-import androidx.annotation.CheckResult;
-import androidx.annotation.NonNull;
-
-public class ExceptionUtil {
-    public static boolean containsMessage(Throwable e, String needle) {
+object ExceptionUtil {
+    @JvmStatic
+    fun containsMessage(e: Throwable?, needle: String?): Boolean {
         if (e == null) {
-            return false;
+            return false
         }
-
-        if (containsMessage(e.getCause(), needle)) {
-            return true;
+        if (containsMessage(e.cause, needle)) {
+            return true
         }
-
-        String message = e.getMessage();
-        return message != null && message.contains(needle);
+        val message = e.message
+        return message != null && message.contains(needle!!)
     }
 
-    @NonNull
     @CheckResult
-    public static String getExceptionMessage(Throwable e) {
-        return getExceptionMessage(e, "\n");
+    @JvmStatic
+    fun getExceptionMessage(e: Throwable?): String {
+        return getExceptionMessage(e, "\n")
     }
 
-    @NonNull
     @CheckResult
-    public static String getExceptionMessage(Throwable e, String separator) {
-        StringBuilder ret = new StringBuilder();
-        Throwable cause = e;
+    fun getExceptionMessage(e: Throwable?, separator: String?): String {
+        val ret = StringBuilder()
+        var cause: Throwable? = e
         while (cause != null) {
-
-            if (cause.getLocalizedMessage() != null || cause == e) {
-                if (cause != e) {
-                    ret.append(separator);
+            if (cause.localizedMessage != null || cause === e) {
+                if (cause !== e) {
+                    ret.append(separator)
                 }
-                ret.append(cause.getLocalizedMessage());
+                ret.append(cause.localizedMessage)
             }
-            cause = cause.getCause();
+            cause = cause.cause
         }
-
-        return ret.toString();
+        return ret.toString()
     }
 
-    /** Whether the exception is, or contains a cause of a given type */
-    public static <T> boolean containsCause(@NonNull Throwable ex, @NonNull Class<T> clazz) {
+    /** Whether the exception is, or contains a cause of a given type  */
+    @JvmStatic
+    fun <T> containsCause(ex: Throwable, clazz: Class<T>): Boolean {
         if (clazz.isInstance(ex)) {
-            return true;
+            return true
         }
-
-        Throwable cause = ex.getCause();
-        if (cause == null) {
-            return false;
-        }
-
-        return containsCause(cause, clazz);
+        val cause = ex.cause ?: return false
+        return containsCause(cause, clazz)
     }
 
-
-    @NonNull
-    public static String getFullStackTrace(@NonNull Throwable ex) {
-        StringWriter sw = new StringWriter();
-        ex.printStackTrace(new PrintWriter(sw));
-        return sw.toString();
+    @JvmStatic
+    fun getFullStackTrace(ex: Throwable): String {
+        val sw = StringWriter()
+        ex.printStackTrace(PrintWriter(sw))
+        return sw.toString()
     }
 }
