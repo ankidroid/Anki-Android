9342517e17d7d6d213c7fbe8e31e04c317cdfca9
codingtosh <codingtosh@gmail.com>
2021-11-06 22:42:48 +0530

    [Kotlin Migration] BundleUtils
    Package: com.ichi2.utils

--- AnkiDroid/src/main/java/com/ichi2/utils/BundleUtils.kt
+++ AnkiDroid/src/main/java/com/ichi2/utils/BundleUtils.kt
@@ -13,34 +13,26 @@
  You should have received a copy of the GNU General Public License along with
  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.ichi2.utils;
+package com.ichi2.utils
 
-import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import android.os.Bundle
 
 /**
- * Collection of useful methods to be used with {@link android.os.Bundle}
+ * Collection of useful methods to be used with [android.os.Bundle]
  */
-public class BundleUtils {
-
-
+object BundleUtils {
     /**
-     * Retrieves a {@link Long} value from a {@link Bundle} using a key, returns null if not found
+     * Retrieves a [Long] value from a [Bundle] using a key, returns null if not found
      *
      * @param bundle the bundle to look into
-     *               can be null to support nullable bundles like {@link Fragment#getArguments()}
+     * can be null to support nullable bundles like [Fragment.getArguments]
      * @param key the key to use
      * @return the long value, or null if not found
      */
-    @Nullable
-    public static Long getNullableLong(@Nullable Bundle bundle, @NonNull String key) {
-        if (bundle == null || !bundle.containsKey(key)) {
-            return null;
-        }
-        return bundle.getLong(key);
+    @JvmStatic
+    fun getNullableLong(bundle: Bundle?, key: String): Long? {
+        return if (bundle == null || !bundle.containsKey(key)) {
+            null
+        } else bundle.getLong(key)
     }
-
 }
