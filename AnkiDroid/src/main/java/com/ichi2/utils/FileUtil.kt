6e76684751c0e8ce4a8e244a47f9711fb9f3c118
codingtosh <codingtosh@gmail.com>
2021-11-07 01:14:28 +0530

    [Kotlin Migration] FileUtil
    Package: com.ichi2.utils

--- AnkiDroid/src/main/java/com/ichi2/utils/FileUtil.kt
+++ AnkiDroid/src/main/java/com/ichi2/utils/FileUtil.kt
@@ -14,35 +14,30 @@
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.ichi2.utils;
+package com.ichi2.utils
 
-import android.content.ContentResolver;
-import android.net.Uri;
-import android.os.StatFs;
+import android.content.ContentResolver
+import android.net.Uri
+import android.os.StatFs
+import com.ichi2.compat.CompatHelper
+import timber.log.Timber
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
+import java.lang.Exception
+import java.lang.IllegalArgumentException
+import java.util.AbstractMap
+import kotlin.Throws
 
-import com.ichi2.compat.CompatHelper;
-
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.AbstractMap;
-import java.util.Map;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import timber.log.Timber;
-
-public class FileUtil {
-    /** Gets the free disk space given a file */
-    public static long getFreeDiskSpace(File file, long defaultValue) {
-        try {
-            return new StatFs(file.getParentFile().getPath()).getAvailableBytes();
-        } catch (IllegalArgumentException e) {
-            Timber.e(e, "Free space could not be retrieved");
-            return defaultValue;
+object FileUtil {
+    /** Gets the free disk space given a file  */
+    @JvmStatic
+    fun getFreeDiskSpace(file: File, defaultValue: Long): Long {
+        return try {
+            StatFs(file.parentFile?.path).availableBytes
+        } catch (e: IllegalArgumentException) {
+            Timber.e(e, "Free space could not be retrieved")
+            defaultValue
         }
     }
 
@@ -51,45 +46,42 @@ public class FileUtil {
      * @param uri               uri to the content to be internalized, used if filePath not real/doesn't work.
      * @param internalFile      an internal cache temp file that the data is copied/internalized into.
      * @param contentResolver   this is needed to open an inputStream on the content uri.
-     * @return  the internal file after copying the data across.
+     * @return the internal file after copying the data across.
      * @throws IOException
      */
-    @NonNull
-    public static File internalizeUri(Uri uri, File internalFile, ContentResolver contentResolver) throws IOException {
+    @JvmStatic
+    @Throws(IOException::class)
+    @KotlinCleanup("nonnull uri")
+    fun internalizeUri(uri: Uri?, internalFile: File, contentResolver: ContentResolver): File {
         // If we got a real file name, do a copy from it
-        InputStream inputStream;
-
-        try {
-            inputStream = contentResolver.openInputStream(uri);
-        } catch (Exception e) {
-            Timber.w(e, "internalizeUri() unable to open input stream from content resolver for Uri %s", uri);
-            throw e;
+        val inputStream: InputStream?
+        inputStream = try {
+            contentResolver.openInputStream(uri!!)
+        } catch (e: Exception) {
+            Timber.w(e, "internalizeUri() unable to open input stream from content resolver for Uri %s", uri)
+            throw e
         }
-
         try {
-            CompatHelper.getCompat().copyFile(inputStream, internalFile.getAbsolutePath());
-        } catch (Exception e) {
-            Timber.w(e, "internalizeUri() unable to internalize file from Uri %s to File %s", uri, internalFile.getAbsolutePath());
-            throw e;
+            CompatHelper.getCompat().copyFile(inputStream, internalFile.absolutePath)
+        } catch (e: Exception) {
+            Timber.w(e, "internalizeUri() unable to internalize file from Uri %s to File %s", uri, internalFile.absolutePath)
+            throw e
         }
-        return internalFile;
+        return internalFile
     }
-
 
     /**
      * @return Key: Filename; Value: extension including dot
      */
-    @Nullable
-    public static Map.Entry<String, String> getFileNameAndExtension(@Nullable String fileName) {
+    @JvmStatic
+    fun getFileNameAndExtension(fileName: String?): Map.Entry<String, String>? {
         if (fileName == null) {
-            return null;
+            return null
         }
-        int index = fileName.lastIndexOf(".");
-        if (index < 1) {
-            return null;
-        }
-
-        return new AbstractMap.SimpleEntry<>(fileName.substring(0, index), fileName.substring(index));
+        val index = fileName.lastIndexOf(".")
+        return if (index < 1) {
+            null
+        } else AbstractMap.SimpleEntry(fileName.substring(0, index), fileName.substring(index))
     }
 
     /**
@@ -102,38 +94,41 @@ public class FileUtil {
      * @param directory Abstract representation of the file/directory whose size needs to be calculated
      * @return Size of the directory in bytes
      */
-    public static long getDirectorySize(File directory) throws IOException {
-        long directorySize = 0;
-        File[] files = listFiles(directory);
-
-        for (File file : files) {
-            if (file.isDirectory()) {
-                directorySize += getDirectorySize(file);
+    @JvmStatic
+    @Throws(IOException::class)
+    fun getDirectorySize(directory: File): Long {
+        var directorySize: Long = 0
+        val files = listFiles(directory)
+        for (file in files) {
+            directorySize += if (file.isDirectory) {
+                getDirectorySize(file)
             } else {
-                directorySize += file.length();
+                file.length()
             }
         }
-        return directorySize;
+        return directorySize
     }
 
     /**
      * If dir exists, it must be a directory.
-     * If not, it is created, along with any necessary parent directories (see {@link File#mkdirs()}).
+     * If not, it is created, along with any necessary parent directories (see [File.mkdirs]).
      * @param dir Abstract representation of a directory
      * @throws IOException if dir is not a directory or could not be created
      */
-    public static void ensureFileIsDirectory(@NonNull File dir) throws IOException {
+    @JvmStatic
+    @Throws(IOException::class)
+    fun ensureFileIsDirectory(dir: File) {
         if (dir.exists()) {
-            if (!dir.isDirectory()) {
-                throw new IOException(dir + " exists but is not a directory");
+            if (!dir.isDirectory) {
+                throw IOException("$dir exists but is not a directory")
             }
-        } else if (!dir.mkdirs() && !dir.isDirectory()) {
-            throw new IOException(dir + " directory cannot be created");
+        } else if (!dir.mkdirs() && !dir.isDirectory) {
+            throw IOException("$dir directory cannot be created")
         }
     }
 
     /**
-     * Wraps {@link File#listFiles()} and throws an exception instead of returning <code>null</code> if dir does not
+     * Wraps [File.listFiles] and throws an exception instead of returning `null` if dir does not
      * denote an actual directory.
      *
      * @throws IOException if the contents of dir cannot be listed
@@ -141,11 +136,10 @@ public class FileUtil {
      * @return An array of abstract representations of the files / directories present in the directory represented
      * by dir
      */
-    public static @NonNull File[] listFiles(@NonNull File dir) throws IOException {
-        File[] children = dir.listFiles();
-        if (children == null) {
-            throw new IOException("Failed to list the contents of '" + dir + "'");
-        }
-        return children;
+    @JvmStatic
+    @Throws(IOException::class)
+    fun listFiles(dir: File): Array<File> {
+        return dir.listFiles()
+            ?: throw IOException("Failed to list the contents of '$dir'")
     }
 }
