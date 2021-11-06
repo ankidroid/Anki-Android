6b1b89b5ed10f34e2932888ca95aaefc9c17d84d
codingtosh <codingtosh@gmail.com>
2021-11-07 01:33:01 +0530

    [Kotlin Migration] HashUtil
    Package: com.ichi2.utils

--- AnkiDroid/src/main/java/com/ichi2/utils/HashUtil.kt
+++ AnkiDroid/src/main/java/com/ichi2/utils/HashUtil.kt
@@ -13,26 +13,27 @@
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
+package com.ichi2.utils
 
+import java.util.HashMap
+import java.util.HashSet
 
-package com.ichi2.utils;
-
-import java.util.HashMap;
-import java.util.HashSet;
-
-public class HashUtil {
+object HashUtil {
     /**
      * @param size Number of elements expected in the hash structure
      * @return Initial capacity for the hash structure. Copied from HashMap code
      */
-    private static int capacity(int size) {
-        return Math.max((int) (size/.75f) + 1, 16);
+    private fun capacity(size: Int): Int {
+        return Math.max((size / .75f).toInt() + 1, 16)
     }
 
-    public static <T> HashSet<T> HashSetInit(int size) {
-        return new HashSet<T>(capacity(size));
+    @JvmStatic
+    fun <T> HashSetInit(size: Int): HashSet<T> {
+        return HashSet(capacity(size))
     }
-    public static <T, U> HashMap<T, U> HashMapInit(int size) {
-        return new HashMap<T, U>(capacity(size));
+
+    @JvmStatic
+    fun <T, U> HashMapInit(size: Int): HashMap<T, U> {
+        return HashMap(capacity(size))
     }
 }
