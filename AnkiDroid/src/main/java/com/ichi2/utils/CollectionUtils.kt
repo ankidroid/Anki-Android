ed6ff60ab3548befbc371785395ac4eef8f6f129
codingtosh <codingtosh@gmail.com>
2021-11-06 23:20:32 +0530

    [Kotlin Migration] CollectionUtils
    Package: com.ichi2.utils

--- AnkiDroid/src/main/java/com/ichi2/utils/CollectionUtils.kt
+++ AnkiDroid/src/main/java/com/ichi2/utils/CollectionUtils.kt
@@ -13,25 +13,24 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.utils
 
-package com.ichi2.utils;
-
-import java.util.Collection;
-import java.util.List;
-
-public class CollectionUtils {
-    /** Throws IndexOutOfBoundsException on empty list*/
-    public static <T> T getLastListElement(List<T> l) {
-        return l.get(l.size()-1);
+object CollectionUtils {
+    /** Throws IndexOutOfBoundsException on empty list */
+    @JvmStatic
+    fun <T> getLastListElement(l: List<T>): T {
+        return l[l.size - 1]
     }
+
     /**
      * @param c A collection in which to add elements of it
      * @param it An iterator returning things to add to C
      * @param <T> Type of elements to copy from iterator to collection
-     */
-    public static <T> void addAll(Collection<T> c, Iterable<T> it) {
-        for (T elt : it) {
-            c.add(elt);
+     </T> */
+    @JvmStatic
+    fun <T> addAll(c: MutableCollection<T>, it: Iterable<T>) {
+        for (elt in it) {
+            c.add(elt)
         }
     }
 }
