be56511e88a658097348b06886a9428b50f29d53
Piyush Goel <piyushgoel2008@gmail.com>
2021-11-07 07:59:51 +0530

    Migrated InsertFieldDialog to kotlin.

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/InsertFieldDialog.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/InsertFieldDialog.kt
@@ -14,98 +14,67 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-package com.ichi2.anki.dialogs;
+package com.ichi2.anki.dialogs
 
-import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.DialogFragment;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.afollestad.materialdialogs.MaterialDialog;
-import com.ichi2.anki.R;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
+import androidx.recyclerview.widget.RecyclerView
+import com.afollestad.materialdialogs.MaterialDialog
+import com.ichi2.anki.R
+import java.util.*
 
 /**
  * This is a reusable convenience class which makes it easy to show a insert field dialog as a DialogFragment.
  * Create a new instance with required fields list, then show it via the fragment manager as usual.
  */
-public class InsertFieldDialog extends DialogFragment {
-
-    private MaterialDialog mDialog;
-    private List<String> mFieldList;
-    private final InsertFieldListener mInsertFieldListener;
-
-
-    public InsertFieldDialog(@NonNull InsertFieldListener insertFieldListener) {
-        this.mInsertFieldListener = insertFieldListener;
-    }
-
-    @NonNull
-    public InsertFieldDialog withArguments(@NonNull List<String> fieldItems) {
-        Bundle args = new Bundle();
-        args.putStringArrayList("fieldItems", new ArrayList<>(fieldItems));
-        this.setArguments(args);
-        return this;
+class InsertFieldDialog(private val insertFieldListener: InsertFieldListener) : DialogFragment() {
+    private var mDialog: MaterialDialog? = null
+    private var mFieldList: List<String>? = null
+    fun withArguments(fieldItems: List<String>): InsertFieldDialog {
+        val args = Bundle()
+        args.putStringArrayList("fieldItems", ArrayList(fieldItems))
+        this.arguments = args
+        return this
     }
 
     /**
      * A dialog for inserting field in card template editor
      */
-    @NonNull
-    @Override
-    public MaterialDialog onCreateDialog(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        mFieldList = getArguments().getStringArrayList("fieldItems");
-        RecyclerView.Adapter<?> adapter = new RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-            @NonNull
-            @Override
-            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View root = getLayoutInflater().inflate(R.layout.material_dialog_list_item, parent, false);
-                return new RecyclerView.ViewHolder(root) { };
+    override fun onCreateDialog(savedInstanceState: Bundle?): MaterialDialog {
+        super.onCreate(savedInstanceState)
+        mFieldList = requireArguments().getStringArrayList("fieldItems")
+        val adapter: RecyclerView.Adapter<*> = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+                val root = layoutInflater.inflate(R.layout.material_dialog_list_item, parent, false)
+                return object : RecyclerView.ViewHolder(root) {}
             }
 
-
-            @Override
-            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-                TextView textView = (TextView) holder.itemView;
-                textView.setText(mFieldList.get(position));
-                textView.setOnClickListener((l) -> {
-                    selectFieldAndClose(textView);
-                });
+            override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+                val textView = holder.itemView as TextView
+                textView.text = mFieldList!![position]
+                textView.setOnClickListener { selectFieldAndClose(textView) }
             }
 
-
-            @Override
-            public int getItemCount() {
-                return mFieldList.size();
+            override fun getItemCount(): Int {
+                return mFieldList!!.size
             }
-        };
-
-        mDialog = new MaterialDialog.Builder(requireContext())
-                .title(R.string.card_template_editor_select_field)
-                .negativeText(R.string.dialog_cancel)
-                .adapter(adapter, null)
-                .build();
-
-        return mDialog;
+        }
+        mDialog = MaterialDialog.Builder(requireContext())
+            .title(R.string.card_template_editor_select_field)
+            .negativeText(R.string.dialog_cancel)
+            .adapter(adapter, null)
+            .build()
+        return mDialog!!
     }
 
-
-    private void selectFieldAndClose(TextView textView) {
-        mInsertFieldListener.insertField(textView.getText().toString());
-        mDialog.dismiss();
+    private fun selectFieldAndClose(textView: TextView) {
+        insertFieldListener.insertField(textView.text.toString())
+        mDialog!!.dismiss()
     }
 
-    public interface InsertFieldListener {
-        void insertField(String field);
+    interface InsertFieldListener {
+        fun insertField(field: String?)
     }
-
 }
