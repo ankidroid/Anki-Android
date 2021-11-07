d9746e5cd178c7d9ba14bb939d71e092d7a72334
Piyush Goel <piyushgoel2008@gmail.com>
2021-11-07 08:33:42 +0530

    Migrated ImportDialog to kotlin

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/ImportDialog.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/ImportDialog.kt
@@ -14,115 +14,103 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-package com.ichi2.anki.dialogs;
+package com.ichi2.anki.dialogs
 
-import android.content.res.Resources;
-import android.os.Bundle;
+import android.os.Bundle
+import com.afollestad.materialdialogs.DialogAction
+import com.afollestad.materialdialogs.MaterialDialog
+import com.ichi2.anki.R
+import timber.log.Timber
+import java.net.URLDecoder
 
-import com.afollestad.materialdialogs.MaterialDialog;
-import com.ichi2.anki.R;
-
-import java.net.URLDecoder;
-
-import androidx.annotation.NonNull;
-import timber.log.Timber;
-
-public class ImportDialog extends AsyncDialogFragment {
-
-    public static final int DIALOG_IMPORT_ADD_CONFIRM = 2;
-    public static final int DIALOG_IMPORT_REPLACE_CONFIRM = 3;
-
-    public interface ImportDialogListener {
-        void importAdd(String importPath);
-        void importReplace(String importPath);
-        void dismissAllDialogFragments();
+class ImportDialog : AsyncDialogFragment() {
+    interface ImportDialogListener {
+        fun importAdd(importPath: String?)
+        fun importReplace(importPath: String?)
+        fun dismissAllDialogFragments()
     }
 
-
-    /**
-     * A set of dialogs which deal with importing a file
-     * 
-     * @param dialogType An integer which specifies which of the sub-dialogs to show
-     * @param dialogMessage An optional string which can be used to show a custom message
-     * or specify import path
-     */
-    public static ImportDialog newInstance(int dialogType, String dialogMessage) {
-        ImportDialog f = new ImportDialog();
-        Bundle args = new Bundle();
-        args.putInt("dialogType", dialogType);
-        args.putString("dialogMessage", dialogMessage);
-        f.setArguments(args);
-        return f;
-    }
-
-
-    @NonNull
-    @Override
-    public MaterialDialog onCreateDialog(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        int type = getArguments().getInt("dialogType");
-        Resources res = getResources();
-        MaterialDialog.Builder builder = new MaterialDialog.Builder(getActivity());
-        builder.cancelable(true);
-
-        switch (type) {
-            case DIALOG_IMPORT_ADD_CONFIRM: {
-                String displayFileName = convertToDisplayName(getArguments().getString("dialogMessage"));
-                return builder.title(res.getString(R.string.import_title))
-                        .content(res.getString(R.string.import_message_add_confirm, filenameFromPath(displayFileName)))
-                        .positiveText(R.string.import_message_add)
-                        .negativeText(R.string.dialog_cancel)
-                        .onPositive((dialog, which) -> {
-                            ((ImportDialogListener) getActivity()).importAdd(getArguments().getString("dialogMessage"));
-                            dismissAllDialogFragments();
-                        })
-                        .show();
+    override fun onCreateDialog(savedInstanceState: Bundle?): MaterialDialog {
+        super.onCreate(savedInstanceState)
+        val type = requireArguments().getInt("dialogType")
+        val res = resources
+        val builder = MaterialDialog.Builder(requireActivity())
+        builder.cancelable(true)
+        return when (type) {
+            DIALOG_IMPORT_ADD_CONFIRM -> {
+                val displayFileName = convertToDisplayName(requireArguments().getString("dialogMessage"))
+                builder.title(res.getString(R.string.import_title))
+                    .content(res.getString(R.string.import_message_add_confirm, filenameFromPath(displayFileName)))
+                    .positiveText(R.string.import_message_add)
+                    .negativeText(R.string.dialog_cancel)
+                    .onPositive { _: MaterialDialog?, _: DialogAction? ->
+                        (activity as ImportDialogListener?)!!.importAdd(requireArguments().getString("dialogMessage"))
+                        dismissAllDialogFragments()
+                    }
+                    .show()
             }
-            case DIALOG_IMPORT_REPLACE_CONFIRM: {
-                String displayFileName = convertToDisplayName(getArguments().getString("dialogMessage"));
-                return builder.title(res.getString(R.string.import_title))
-                        .content(res.getString(R.string.import_message_replace_confirm, displayFileName))
-                        .positiveText(R.string.dialog_positive_replace)
-                        .negativeText(R.string.dialog_cancel)
-                        .onPositive((dialog, which) -> {
-                            ((ImportDialogListener) getActivity()).importReplace(getArguments().getString("dialogMessage"));
-                            dismissAllDialogFragments();
-                        })
-                        .show();
+            DIALOG_IMPORT_REPLACE_CONFIRM -> {
+                val displayFileName = convertToDisplayName(requireArguments().getString("dialogMessage"))
+                builder.title(res.getString(R.string.import_title))
+                    .content(res.getString(R.string.import_message_replace_confirm, displayFileName))
+                    .positiveText(R.string.dialog_positive_replace)
+                    .negativeText(R.string.dialog_cancel)
+                    .onPositive { _: MaterialDialog?, _: DialogAction? ->
+                        (activity as ImportDialogListener?)!!.importReplace(requireArguments().getString("dialogMessage"))
+                        dismissAllDialogFragments()
+                    }
+                    .show()
             }
-            default:
-                return null;
+            else -> null!!
         }
     }
 
-
-    private String convertToDisplayName(String name) {
-        //ImportUtils URLEncodes names, which isn't great for display.
-        //NICE_TO_HAVE: Pass in the DisplayFileName closer to the source of the bad data, rather than fixing it here.
-        try {
-            return URLDecoder.decode(name, "UTF-8");
-        } catch (Exception e) {
-            Timber.w(e,"Failed to convert filename to displayable string");
-            return name;
+    private fun convertToDisplayName(name: String?): String? {
+        // ImportUtils URLEncodes names, which isn't great for display.
+        // NICE_TO_HAVE: Pass in the DisplayFileName closer to the source of the bad data, rather than fixing it here.
+        return try {
+            URLDecoder.decode(name, "UTF-8")
+        } catch (e: Exception) {
+            Timber.w(e, "Failed to convert filename to displayable string")
+            name
         }
     }
 
-
-    @Override
-    public String getNotificationMessage() {
-        return res().getString(R.string.import_interrupted);
+    override fun getNotificationMessage(): String {
+        return res().getString(R.string.import_interrupted)
     }
 
-    @Override
-    public String getNotificationTitle() {
-        return res().getString(R.string.import_title);
-    }
-    
-    public void dismissAllDialogFragments() {
-        ((ImportDialogListener) getActivity()).dismissAllDialogFragments();        
+    override fun getNotificationTitle(): String {
+        return res().getString(R.string.import_title)
     }
 
-    private static String filenameFromPath (String path) {
-        return path.split("/")[path.split("/").length - 1];
+    fun dismissAllDialogFragments() {
+        (activity as ImportDialogListener?)!!.dismissAllDialogFragments()
+    }
+
+    companion object {
+        const val DIALOG_IMPORT_ADD_CONFIRM = 2
+        const val DIALOG_IMPORT_REPLACE_CONFIRM = 3
+
+        /**
+         * A set of dialogs which deal with importing a file
+         *
+         * @param dialogType An integer which specifies which of the sub-dialogs to show
+         * @param dialogMessage An optional string which can be used to show a custom message
+         * or specify import path
+         */
+        @JvmStatic
+        fun newInstance(dialogType: Int, dialogMessage: String?): ImportDialog {
+            val f = ImportDialog()
+            val args = Bundle()
+            args.putInt("dialogType", dialogType)
+            args.putString("dialogMessage", dialogMessage)
+            f.arguments = args
+            return f
+        }
+
+        private fun filenameFromPath(path: String?): String {
+            return path!!.split("/").toTypedArray()[path.split("/").toTypedArray().size - 1]
+        }
     }
 }
