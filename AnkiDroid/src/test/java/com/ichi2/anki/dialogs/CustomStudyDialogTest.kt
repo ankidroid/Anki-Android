46cdd774703c88fa7aadd1c67ea207bd8a95a7b8
Piyush Goel <piyushgoel2008@gmail.com>
2021-11-09 20:47:10 +0530

    Migrated CustomStudyDialogTest to kotlin.

--- AnkiDroid/src/test/java/com/ichi2/anki/dialogs/CustomStudyDialogTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/dialogs/CustomStudyDialogTest.kt
@@ -13,136 +13,110 @@
  You should have received a copy of the GNU General Public License along with
  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.anki.dialogs
 
-package com.ichi2.anki.dialogs;
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.lifecycle.Lifecycle
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.afollestad.materialdialogs.DialogAction
+import com.afollestad.materialdialogs.MaterialDialog
+import com.ichi2.anki.R
+import com.ichi2.anki.RobolectricTest
+import com.ichi2.anki.dialogs.customstudy.CustomStudyDialog
+import com.ichi2.anki.dialogs.customstudy.CustomStudyDialog.CustomStudyListener
+import com.ichi2.anki.dialogs.customstudy.CustomStudyDialogFactory
+import com.ichi2.libanki.Collection
+import com.ichi2.libanki.sched.AbstractSched
+import com.ichi2.testutils.JsonUtils.toOrderedString
+import com.ichi2.testutils.ParametersUtils
+import com.ichi2.utils.KotlinCleanup
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.hamcrest.core.IsNull
+import org.junit.After
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.robolectric.annotation.Config
 
-import android.os.Bundle;
-
-import com.afollestad.materialdialogs.DialogAction;
-import com.afollestad.materialdialogs.MaterialDialog;
-import com.ichi2.anki.R;
-import com.ichi2.anki.RobolectricTest;
-import com.ichi2.anki.dialogs.customstudy.CustomStudyDialog;
-import com.ichi2.anki.dialogs.customstudy.CustomStudyDialog.CustomStudyListener;
-import com.ichi2.anki.dialogs.customstudy.CustomStudyDialogFactory;
-import com.ichi2.libanki.Collection;
-import com.ichi2.libanki.Deck;
-import com.ichi2.libanki.sched.AbstractSched;
-import com.ichi2.testutils.JsonUtils;
-
-import org.hamcrest.Matchers;
-import org.junit.After;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.robolectric.annotation.Config;
-
-import androidx.fragment.app.testing.FragmentScenario;
-import androidx.lifecycle.Lifecycle;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import static com.ichi2.testutils.ParametersUtils.whatever;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.core.IsNull.notNullValue;
-import static org.mockito.Mockito.*;
-
-@RunWith(AndroidJUnit4.class)
-public class CustomStudyDialogTest extends RobolectricTest {
-
-    CustomStudyListener mMockListener;
-
-
-    @Override
-    public void setUp() {
-        super.setUp();
-        mMockListener = mock(CustomStudyListener.class);
+@RunWith(AndroidJUnit4::class)
+class CustomStudyDialogTest : RobolectricTest() {
+    private var mMockListener: CustomStudyListener? = null
+    override fun setUp() {
+        super.setUp()
+        mMockListener = Mockito.mock(CustomStudyListener::class.java)
     }
 
-
-    @Override
     @After
-    public void tearDown() {
-        super.tearDown();
-        reset(mMockListener);
+    override fun tearDown() {
+        super.tearDown()
+        Mockito.reset(mMockListener)
     }
-
 
     @Test
-    public void learnAheadCardsRegressionTest() {
+    fun learnAheadCardsRegressionTest() {
         // #6289 - Regression Test
-        Bundle args = new CustomStudyDialog(whatever(), whatever())
-                .withArguments(CustomStudyDialog.CUSTOM_STUDY_AHEAD, 1)
-                .getArguments();
-
-
-        CustomStudyDialogFactory factory = new CustomStudyDialogFactory(this::getCol, mMockListener);
-        FragmentScenario<CustomStudyDialog> scenario = FragmentScenario.launch(CustomStudyDialog.class, args, factory);
-
-        scenario.moveToState(Lifecycle.State.STARTED);
-
-        scenario.onFragment(f -> {
-            MaterialDialog dialog = (MaterialDialog) f.getDialog();
-            assertThat(dialog, notNullValue());
-            dialog.getActionButton(DialogAction.POSITIVE).callOnClick();
-        });
-
-
-        Deck customStudy = getCol().getDecks().current();
-        assertThat("Custom Study should be dynamic", customStudy.isDyn());
-        assertThat("could not find deck: Custom study session", customStudy, notNullValue());
-        customStudy.remove("id");
-        customStudy.remove("mod");
-        customStudy.remove("name");
-
-        String expected = "{" +
-                "\"browserCollapsed\":false," +
-                "\"collapsed\":false," +
-                "\"delays\":null," +
-                "\"desc\":\"\"," +
-                "\"dyn\":1," +
-                "\"lrnToday\":[0,0]," +
-                "\"newToday\":[0,0]," +
-                "\"previewDelay\":10," +
-                "\"resched\":true," +
-                "\"revToday\":[0,0]," +
-                "\"separate\":true," +
-                "\"terms\":[[\"deck:\\\"Default\\\" prop:due<=1\",99999,6]]," +
-                "\"timeToday\":[0,0]," +
-                "\"usn\":-1" +
-                "}";
-        assertThat(JsonUtils.toOrderedString(customStudy), is(expected));
+        val args = CustomStudyDialog(ParametersUtils.whatever(), ParametersUtils.whatever())
+            .withArguments(CustomStudyDialog.CUSTOM_STUDY_AHEAD, 1)
+            .arguments
+        val factory = CustomStudyDialogFactory({ this.col }, mMockListener)
+        val scenario = FragmentScenario.launch(CustomStudyDialog::class.java, args, factory)
+        scenario.moveToState(Lifecycle.State.STARTED)
+        scenario.onFragment { f: CustomStudyDialog ->
+            val dialog = f.dialog as MaterialDialog?
+            MatcherAssert.assertThat(dialog, IsNull.notNullValue())
+            dialog!!.getActionButton(DialogAction.POSITIVE).callOnClick()
+        }
+        val customStudy = col.decks.current()
+        MatcherAssert.assertThat("Custom Study should be dynamic", customStudy.isDyn)
+        MatcherAssert.assertThat("could not find deck: Custom study session", customStudy, notNullValue())
+        customStudy.remove("id")
+        customStudy.remove("mod")
+        customStudy.remove("name")
+        val expected = "{" +
+            "\"browserCollapsed\":false," +
+            "\"collapsed\":false," +
+            "\"delays\":null," +
+            "\"desc\":\"\"," +
+            "\"dyn\":1," +
+            "\"lrnToday\":[0,0]," +
+            "\"newToday\":[0,0]," +
+            "\"previewDelay\":10," +
+            "\"resched\":true," +
+            "\"revToday\":[0,0]," +
+            "\"separate\":true," +
+            "\"terms\":[[\"deck:\\\"Default\\\" prop:due<=1\",99999,6]]," +
+            "\"timeToday\":[0,0]," +
+            "\"usn\":-1" +
+            "}"
+        MatcherAssert.assertThat(customStudy.toOrderedString(), Matchers.`is`(expected))
     }
-
 
     @Test
     @Config(qualifiers = "en")
-    public void increaseNewCardLimitRegressionTest(){
+    @KotlinCleanup("Use kotlin based Mockito extensions")
+    fun increaseNewCardLimitRegressionTest() {
         // #8338 - Regression Test
-        Bundle args = new CustomStudyDialog(whatever(), whatever())
-                .withArguments(CustomStudyDialog.CONTEXT_MENU_STANDARD, 1)
-                .getArguments();
+        val args = CustomStudyDialog(ParametersUtils.whatever(), ParametersUtils.whatever())
+            .withArguments(CustomStudyDialog.CONTEXT_MENU_STANDARD, 1)
+            .arguments
 
         // we are using mock collection for the CustomStudyDialog but still other parts of the code
         // access a real collection, so we must ensure that collection is loaded first
         // so we don't get net/ankiweb/rsdroid/BackendException$BackendDbException$BackendDbLockedException
-        ensureCollectionLoadIsSynchronous();
-
-        Collection mockCollection = mock(Collection.class, Mockito.RETURNS_DEEP_STUBS);
-        AbstractSched mockSched = mock(AbstractSched.class);
-        when(mockCollection.getSched()).thenReturn(mockSched);
-        when(mockSched.newCount()).thenReturn(0);
-
-
-        CustomStudyDialogFactory factory = new CustomStudyDialogFactory(() -> mockCollection, mMockListener);
-        FragmentScenario<CustomStudyDialog> scenario = FragmentScenario.launch(CustomStudyDialog.class, args, R.style.Theme_AppCompat, factory);
-
-        scenario.moveToState(Lifecycle.State.STARTED);
-
-        scenario.onFragment(f -> {
-            MaterialDialog dialog = (MaterialDialog)f.getDialog();
-            assertThat(dialog,notNullValue());
-            assertThat(dialog.getItems(), Matchers.not(Matchers.hasItem(getResourceString(R.string.custom_study_increase_new_limit))));
-        });
+        ensureCollectionLoadIsSynchronous()
+        val mockCollection = Mockito.mock(Collection::class.java, Mockito.RETURNS_DEEP_STUBS)
+        val mockSched = Mockito.mock(AbstractSched::class.java)
+        Mockito.`when`(mockCollection.sched).thenReturn(mockSched)
+        Mockito.`when`(mockSched.newCount()).thenReturn(0)
+        val factory = CustomStudyDialogFactory({ mockCollection }, mMockListener)
+        val scenario = FragmentScenario.launch(CustomStudyDialog::class.java, args, R.style.Theme_AppCompat, factory)
+        scenario.moveToState(Lifecycle.State.STARTED)
+        scenario.onFragment { f: CustomStudyDialog ->
+            val dialog = f.dialog as MaterialDialog?
+            MatcherAssert.assertThat(dialog, IsNull.notNullValue())
+            MatcherAssert.assertThat(dialog!!.items, Matchers.not(Matchers.hasItem(getResourceString(R.string.custom_study_increase_new_limit))))
+        }
     }
 }
