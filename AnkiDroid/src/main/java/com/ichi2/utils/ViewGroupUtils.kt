cd3bc5bf1134d1686e10b7593ac54dc52ab6f729
codingtosh <codingtosh@gmail.com>
2021-11-10 23:51:22 +0530

    [Kotlin Migration] ViewGroupUtils
    Package: com.ichi2.utils

--- AnkiDroid/src/main/java/com/ichi2/utils/ViewGroupUtils.kt
+++ AnkiDroid/src/main/java/com/ichi2/utils/ViewGroupUtils.kt
@@ -14,50 +14,47 @@
  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.ichi2.utils;
+package com.ichi2.utils
 
-import android.app.Activity;
-import android.view.View;
-import android.view.ViewGroup;
+import android.R
+import android.app.Activity
+import android.view.View
+import android.view.ViewGroup
+import com.ichi2.anki.AnkiDroidApp
+import timber.log.Timber
+import java.util.ArrayList
 
-import com.ichi2.anki.AnkiDroidApp;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import androidx.annotation.NonNull;
-import timber.log.Timber;
-
-public class ViewGroupUtils {
-    @NonNull
-    public static List<View> getAllChildren(@NonNull ViewGroup viewGroup) {
-        int childrenCount = viewGroup.getChildCount();
-        List<View> views = new ArrayList<>(childrenCount);
-        for (int i = 0; i < childrenCount; i++) {
-            views.add(viewGroup.getChildAt(i));
+object ViewGroupUtils {
+    @JvmStatic
+    fun getAllChildren(viewGroup: ViewGroup): List<View> {
+        val childrenCount = viewGroup.childCount
+        val views: MutableList<View> = ArrayList(childrenCount)
+        for (i in 0 until childrenCount) {
+            views.add(viewGroup.getChildAt(i))
         }
-        return views;
+        return views
     }
 
-    @NonNull
-    public static List<View> getAllChildrenRecursive(@NonNull ViewGroup viewGroup) {
-        List<View> views = new ArrayList<>();
-        for (int i = 0; i < viewGroup.getChildCount(); i++) {
-            View child = viewGroup.getChildAt(i);
-            views.add(child);
-            if (child instanceof ViewGroup) {
-                views.addAll(getAllChildrenRecursive((ViewGroup) child));
+    @JvmStatic
+    fun getAllChildrenRecursive(viewGroup: ViewGroup): MutableList<View> {
+        val views: MutableList<View> = ArrayList()
+        for (i in 0 until viewGroup.childCount) {
+            val child = viewGroup.getChildAt(i)
+            views.add(child)
+            if (child is ViewGroup) {
+                views.addAll(getAllChildrenRecursive(child))
             }
         }
-        return views;
+        return views
     }
 
-    public static void setRenderWorkaround(Activity activity) {
+    @JvmStatic
+    fun setRenderWorkaround(activity: Activity) {
         if (AnkiDroidApp.getSharedPrefs(activity).getBoolean("softwareRender", false)) {
-            Timber.i("ViewGroupUtils::setRenderWorkaround - software render requested, altering Views...");
-            ViewGroupUtils.setContentViewLayerTypeSoftware(activity);
+            Timber.i("ViewGroupUtils::setRenderWorkaround - software render requested, altering Views...")
+            setContentViewLayerTypeSoftware(activity)
         } else {
-            Timber.i("ViewGroupUtils::setRenderWorkaround - using default / hardware rendering");
+            Timber.i("ViewGroupUtils::setRenderWorkaround - using default / hardware rendering")
         }
     }
 
@@ -67,16 +64,17 @@ public class ViewGroupUtils {
      *
      * @param activity Activity containing the View hierarchy to alter
      */
-    private static void setContentViewLayerTypeSoftware(@NonNull Activity activity) {
-        ViewGroup rootViewGroup =
-                (ViewGroup) ((ViewGroup) activity.findViewById(android.R.id.content))
-                        .getChildAt(0);
-        List<View> allViews = getAllChildrenRecursive(rootViewGroup);
-        allViews.add(rootViewGroup);
-        for (View v : allViews) {
-            Timber.d("ViewGroupUtils::setContentViewLayerTypeSoftware for view %s",
-                    v.getId());
-            v.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+    private fun setContentViewLayerTypeSoftware(activity: Activity) {
+        val rootViewGroup = (activity.findViewById<View>(R.id.content) as ViewGroup)
+            .getChildAt(0) as ViewGroup
+        val allViews = getAllChildrenRecursive(rootViewGroup)
+        allViews.add(rootViewGroup)
+        for (v in allViews) {
+            Timber.d(
+                "ViewGroupUtils::setContentViewLayerTypeSoftware for view %s",
+                v.id
+            )
+            v.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
         }
     }
 }
