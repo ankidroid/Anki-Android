effe98771a40cd12286a55e5a3677cbd8eeb194a
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-11-11 02:02:57 +0000

    Kotlin Migration: TtsParser.java -> .kt com.ichi2.anki.TtsParser

--- AnkiDroid/src/main/java/com/ichi2/anki/TtsParser.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/TtsParser.kt
@@ -1,88 +1,76 @@
 //noinspection MissingCopyrightHeader #8659
-package com.ichi2.anki;
-import com.ichi2.libanki.template.TemplateFilters;
+package com.ichi2.anki
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Element;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.ichi2.libanki.template.TemplateFilters
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
+import java.util.ArrayList
 
 /**
  * Parse card sides, extracting text snippets that should be read using a text-to-speech engine.
  */
-public final class TtsParser {
+object TtsParser {
     /**
      * Returns the list of text snippets contained in the given HTML fragment that should be read
      * using the Android text-to-speech engine, together with the languages they are in.
-     * <p>
+     *
+     *
      * Each returned LocalisedText object contains the text extracted from a &lt;tts&gt; element
      * whose 'service' attribute is set to 'android', and the localeCode taken from the 'voice'
      * attribute of that element. This holds unless the HTML fragment contains no such &lt;tts&gt;
      * elements; in that case the function returns a single LocalisedText object containing the
      * text extracted from the whole HTML fragment, with the localeCode set to an empty string.
      */
-    public static List<LocalisedText> getTextsToRead(String html, String clozeReplacement) {
-        List<LocalisedText> textsToRead = new ArrayList<>();
-
-        Element elem = Jsoup.parseBodyFragment(html).body();
-        parseTtsElements(elem, textsToRead);
+    @JvmStatic
+    fun getTextsToRead(html: String, clozeReplacement: String?): List<LocalisedText> {
+        val textsToRead: MutableList<LocalisedText> = ArrayList()
+        val elem = Jsoup.parseBodyFragment(html).body()
+        parseTtsElements(elem, textsToRead)
         if (textsToRead.isEmpty()) {
             // No <tts service="android"> elements found: return the text of the whole HTML fragment
-            textsToRead.add(new LocalisedText(elem.text().replace(TemplateFilters.CLOZE_DELETION_REPLACEMENT, clozeReplacement)));
+            textsToRead.add(LocalisedText(elem.text().replace(TemplateFilters.CLOZE_DELETION_REPLACEMENT, clozeReplacement!!)))
         }
-
-        return textsToRead;
+        return textsToRead
     }
 
-    private static void parseTtsElements(Element element, List<LocalisedText> textsToRead) {
-        if ("tts".equalsIgnoreCase(element.tagName()) &&
-                "android".equalsIgnoreCase(element.attr("service"))) {
-            textsToRead.add(new LocalisedText(element.text(), element.attr("voice")));
-            return; // ignore any children
+    private fun parseTtsElements(element: Element, textsToRead: MutableList<LocalisedText>) {
+        if ("tts".equals(element.tagName(), ignoreCase = true) &&
+            "android".equals(element.attr("service"), ignoreCase = true)
+        ) {
+            textsToRead.add(LocalisedText(element.text(), element.attr("voice")))
+            return // ignore any children
         }
-
-        for (Element child : element.children()) {
-            parseTtsElements(child, textsToRead);
+        for (child in element.children()) {
+            parseTtsElements(child, textsToRead)
         }
     }
-
     // ----------------------------------------------------------------------------
     // INNER CLASSES
     // ----------------------------------------------------------------------------
-
     /**
      * Snippet of text accompanied by its locale code (if known).
      */
-    public static final class LocalisedText {
-        private final String mText;
-        private final String mLocaleCode;
+    class LocalisedText {
+        val text: String
+        val localeCode: String
 
         /**
          * Construct an object representing a snippet of text in an unknown locale.
          */
-        public LocalisedText(String text) {
-            mText = text;
-            mLocaleCode = "";
+        constructor(text: String) {
+            this.text = text
+            localeCode = ""
         }
 
         /**
          * Construct an object representing a snippet of text in a particular locale.
          *
          * @param localeCode A string representation of a locale in the format returned by
-         *                   Locale.toString().
+         * Locale.toString().
          */
-        public LocalisedText(String text, String localeCode) {
-            mText = text;
-            mLocaleCode = localeCode;
-        }
-
-        public String getText() {
-            return mText;
-        }
-
-        public String getLocaleCode() {
-            return mLocaleCode;
+        constructor(text: String, localeCode: String) {
+            this.text = text
+            this.localeCode = localeCode
         }
     }
 }
