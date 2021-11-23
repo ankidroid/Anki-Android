1564ce667b8f0dfef5a76e95dedc3615e2d5009b
codingtosh <codingtosh@gmail.com>
2021-11-23 23:05:52 +0530

    [Kotlin Migration] DebugInfoService
    Package: com.ichi2.anki.servicelayer

--- AnkiDroid/src/main/java/com/ichi2/anki/servicelayer/DebugInfoService.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/servicelayer/DebugInfoService.kt
@@ -14,65 +14,68 @@
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.ichi2.anki.servicelayer;
+package com.ichi2.anki.servicelayer
 
-import android.content.Context;
-import android.os.Build;
-import android.webkit.WebView;
+import android.content.Context
+import android.os.Build
+import android.webkit.WebView
+import com.ichi2.anki.AnkiDroidApp
+import com.ichi2.libanki.Collection
+import com.ichi2.utils.VersionUtils.pkgVersionName
+import org.acra.util.Installation
+import timber.log.Timber
+import java.util.function.Supplier
 
-import com.ichi2.anki.AnkiDroidApp;
-import com.ichi2.libanki.Collection;
-import com.ichi2.utils.VersionUtils;
-
-import org.acra.util.Installation;
-
-import java.util.function.Supplier;
-
-import androidx.annotation.NonNull;
-import timber.log.Timber;
-
-public class DebugInfoService {
-    @NonNull
-    public static String getDebugInfo(Context info, Supplier<Collection> col) {
-        String schedName = "Not found";
+object DebugInfoService {
+    @JvmStatic
+    fun getDebugInfo(info: Context, col: Supplier<Collection>): String {
+        var schedName = "Not found"
         try {
-            schedName = col.get().getSched().getName();
-        } catch (Throwable e) {
-            Timber.e(e, "Sched name not found");
+            schedName = col.get().sched.name
+        } catch (e: Throwable) {
+            Timber.e(e, "Sched name not found")
         }
-
-        Boolean dbV2Enabled = null;
+        var dbV2Enabled: Boolean? = null
         try {
-            dbV2Enabled = col.get().isUsingRustBackend();
-        } catch (Throwable e) {
-            Timber.w(e, "Unable to detect Rust Backend");
+            dbV2Enabled = col.get().isUsingRustBackend
+        } catch (e: Throwable) {
+            Timber.w(e, "Unable to detect Rust Backend")
         }
-
-        String webviewUserAgent = getWebviewUserAgent(info);
-        return "AnkiDroid Version = " + VersionUtils.getPkgVersionName() + "\n\n" +
-                "Android Version = " + Build.VERSION.RELEASE + "\n\n" +
-                "Manufacturer = " + Build.MANUFACTURER + "\n\n" +
-                "Model = " + Build.MODEL + "\n\n" +
-                "Hardware = " + Build.HARDWARE + "\n\n" +
-                "Webview User Agent = " + webviewUserAgent + "\n\n" +
-                "ACRA UUID = " + Installation.id(info) + "\n\n" +
-                "Scheduler = " + schedName + "\n\n" +
-                "Crash Reports Enabled = " + isSendingCrashReports(info) + "\n\n" +
-                "DatabaseV2 Enabled = " + dbV2Enabled + "\n";
+        val webviewUserAgent = getWebviewUserAgent(info)
+        return """
+               AnkiDroid Version = $pkgVersionName
+               
+               Android Version = ${Build.VERSION.RELEASE}
+               
+               Manufacturer = ${Build.MANUFACTURER}
+               
+               Model = ${Build.MODEL}
+               
+               Hardware = ${Build.HARDWARE}
+               
+               Webview User Agent = $webviewUserAgent
+               
+               ACRA UUID = ${Installation.id(info)}
+               
+               Scheduler = $schedName
+               
+               Crash Reports Enabled = ${isSendingCrashReports(info)}
+               
+               DatabaseV2 Enabled = $dbV2Enabled
+               
+        """.trimIndent()
     }
 
-
-    private static String getWebviewUserAgent(Context context) {
+    private fun getWebviewUserAgent(context: Context): String? {
         try {
-            return new WebView(context).getSettings().getUserAgentString();
-        } catch (Throwable e) {
-            AnkiDroidApp.sendExceptionReport(e, "Info::copyDebugInfo()", "some issue occured while extracting webview user agent");
+            return WebView(context).settings.userAgentString
+        } catch (e: Throwable) {
+            AnkiDroidApp.sendExceptionReport(e, "Info::copyDebugInfo()", "some issue occured while extracting webview user agent")
         }
-        return null;
+        return null
     }
 
-    private static boolean isSendingCrashReports(Context context) {
-        return AnkiDroidApp.isAcraEnabled(context, false);
+    private fun isSendingCrashReports(context: Context): Boolean {
+        return AnkiDroidApp.isAcraEnabled(context, false)
     }
-
 }
