f4217b431441356115b901e4297c81f3d665b514
codingtosh <codingtosh@gmail.com>
2021-11-24 17:07:21 +0530

    [Kotlin Migration] NoteService
    Package: com.ichi2.anki.servicelayer

--- AnkiDroid/src/main/java/com/ichi2/anki/servicelayer/NoteService.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/servicelayer/NoteService.kt
@@ -17,122 +17,109 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-package com.ichi2.anki.servicelayer;
+package com.ichi2.anki.servicelayer
 
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.annotation.CheckResult
+import androidx.annotation.VisibleForTesting
+import com.ichi2.anki.AnkiDroidApp
+import com.ichi2.anki.FieldEditText
+import com.ichi2.anki.multimediacard.IMultimediaEditableNote
+import com.ichi2.anki.multimediacard.fields.*
+import com.ichi2.anki.multimediacard.impl.MultimediaEditableNote
+import com.ichi2.libanki.Note
+import com.ichi2.libanki.exception.EmptyMediaException
+import com.ichi2.utils.JSONException
+import com.ichi2.utils.JSONObject
+import timber.log.Timber
+import java.io.File
+import java.io.IOException
 
-import com.ichi2.anki.AnkiDroidApp;
-import com.ichi2.anki.FieldEditText;
-import com.ichi2.anki.multimediacard.IMultimediaEditableNote;
-import com.ichi2.anki.multimediacard.fields.AudioClipField;
-import com.ichi2.anki.multimediacard.fields.AudioRecordingField;
-import com.ichi2.anki.multimediacard.fields.IField;
-import com.ichi2.anki.multimediacard.fields.ImageField;
-import com.ichi2.anki.multimediacard.fields.TextField;
-import com.ichi2.anki.multimediacard.impl.MultimediaEditableNote;
-import com.ichi2.libanki.Collection;
-import com.ichi2.libanki.Note;
-
-import com.ichi2.libanki.exception.EmptyMediaException;
-import com.ichi2.utils.JSONArray;
-import com.ichi2.utils.JSONException;
-import com.ichi2.utils.JSONObject;
-
-import java.io.File;
-import java.io.IOException;
-
-import androidx.annotation.CheckResult;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.VisibleForTesting;
-import timber.log.Timber;
-
-public class NoteService {
+object NoteService {
     /**
      * Creates an empty Note from given Model
-     * 
+     *
      * @param model the model in JSOBObject format
      * @return a new note instance
      */
-    public static MultimediaEditableNote createEmptyNote(JSONObject model) {
+    @JvmStatic
+    fun createEmptyNote(model: JSONObject): MultimediaEditableNote? {
         try {
-            JSONArray fieldsArray = model.getJSONArray("flds");
-            int numOfFields = fieldsArray.length();
+            val fieldsArray = model.getJSONArray("flds")
+            val numOfFields = fieldsArray.length()
             if (numOfFields > 0) {
-                MultimediaEditableNote note = new MultimediaEditableNote();
-                note.setNumFields(numOfFields);
-
-                for (int i = 0; i < numOfFields; i++) {
-                    JSONObject fieldObject = fieldsArray.getJSONObject(i);
-                    TextField uiTextField = new TextField();
-                    uiTextField.setName(fieldObject.getString("name"));
-                    uiTextField.setText(fieldObject.getString("name"));
-                    note.setField(i, uiTextField);
+                val note = MultimediaEditableNote()
+                note.setNumFields(numOfFields)
+                for (i in 0 until numOfFields) {
+                    val fieldObject = fieldsArray.getJSONObject(i)
+                    val uiTextField = TextField()
+                    uiTextField.name = fieldObject.getString("name")
+                    uiTextField.text = fieldObject.getString("name")
+                    note.setField(i, uiTextField)
                 }
-                note.setModelId(model.getLong("id"));
-                return note;
+                note.modelId = model.getLong("id")
+                return note
             }
-        } catch (JSONException e) {
+        } catch (e: JSONException) {
             // TODO Auto-generated catch block
-            Timber.w(e);
+            Timber.w(e)
         }
-        return null;
+        return null
     }
 
-
-    public static void updateMultimediaNoteFromFields(Collection col, String[] fields, long modelId, MultimediaEditableNote mmNote) {
-        for (int i = 0; i < fields.length; i++) {
-            String value = fields[i];
-            IField field = null;
-            if (value.startsWith("<img")) {
-                field = new ImageField();
+    @JvmStatic
+    fun updateMultimediaNoteFromFields(col: com.ichi2.libanki.Collection?, fields: Array<String>, modelId: Long, mmNote: MultimediaEditableNote) {
+        for (i in fields.indices) {
+            val value = fields[i]
+            var field: IField?
+            field = if (value.startsWith("<img")) {
+                ImageField()
             } else if (value.startsWith("[sound:") && value.contains("rec")) {
-                field = new AudioRecordingField();
+                AudioRecordingField()
             } else if (value.startsWith("[sound:")) {
-                field = new AudioClipField();
+                AudioClipField()
             } else {
-                field = new TextField();
+                TextField()
             }
-            field.setFormattedString(col, value);
-            mmNote.setField(i, field);
+            field.setFormattedString(col, value)
+            mmNote.setField(i, field)
         }
-        mmNote.setModelId(modelId);
-        mmNote.freezeInitialFieldValues();
+        mmNote.modelId = modelId
+        mmNote.freezeInitialFieldValues()
         // TODO: set current id of the note as well
     }
-
 
     /**
      * Updates the JsonNote field values from MultimediaEditableNote When both notes are using the same Model, it updaes
      * the destination field values with source values. If models are different it throws an Exception
-     * 
+     *
      * @param noteSrc
      * @param editorNoteDst
      */
-    public static void updateJsonNoteFromMultimediaNote(final IMultimediaEditableNote noteSrc, final Note editorNoteDst) {
-        if (noteSrc instanceof MultimediaEditableNote) {
-            MultimediaEditableNote mmNote = (MultimediaEditableNote) noteSrc;
-            if (mmNote.getModelId() != editorNoteDst.getMid()) {
-                throw new RuntimeException("Source and Destination Note ID do not match.");
+    @JvmStatic
+    fun updateJsonNoteFromMultimediaNote(noteSrc: IMultimediaEditableNote?, editorNoteDst: Note) {
+        if (noteSrc is MultimediaEditableNote) {
+            val mmNote = noteSrc
+            if (mmNote.modelId != editorNoteDst.mid) {
+                throw RuntimeException("Source and Destination Note ID do not match.")
             }
-
-            int totalFields = mmNote.getNumberOfFields();
-            for (int i = 0; i < totalFields; i++) {
-                editorNoteDst.values()[i] = mmNote.getField(i).getFormattedValue();
+            val totalFields: Int = mmNote.numberOfFields
+            for (i in 0 until totalFields) {
+                editorNoteDst.values()[i] = mmNote.getField(i)!!.formattedValue
             }
         }
     }
-
 
     /**
      * Saves the multimedia associated with this card to proper path inside anki folder. For each field associated with
      * the note it checks for the following condition a. The field content should have changed b. The field content does
      * not already point to a media inside anki media path If both condition satisfies then it copies the file inside
      * the media path and deletes the file referenced by the note
-     * 
+     *
      * @param noteNew
      */
-    public static void saveMedia(Collection col, final MultimediaEditableNote noteNew) {
+    @JvmStatic
+    fun saveMedia(col: com.ichi2.libanki.Collection, noteNew: MultimediaEditableNote) {
         // if (noteNew.getModelId() == noteOld.getModelId())
         // {
         // int fieldCount = noteNew.getNumberOfFields();
@@ -150,121 +137,105 @@ public class NoteService {
         // }
         // else
         // {
-        int fieldCount = noteNew.getNumberOfFields();
-        for (int i = 0; i < fieldCount; i++) {
-            IField newField = noteNew.getField(i);
-            importMediaToDirectory(col, newField);
+        val fieldCount: Int = noteNew.numberOfFields
+        for (i in 0 until fieldCount) {
+            val newField = noteNew.getField(i)
+            importMediaToDirectory(col, newField)
         }
         // }
     }
 
-
     /**
      * Considering the field is new, if it has media handle it
-     * 
+     *
      * @param field
      */
-    private static void importMediaToDirectory(Collection col, IField field) {
-        String tmpMediaPath = null;
-        switch (field.getType()) {
-            case AUDIO_RECORDING:
-            case AUDIO_CLIP:
-                tmpMediaPath = field.getAudioPath();
-                break;
-
-            case IMAGE:
-                tmpMediaPath = field.getImagePath();
-                break;
-
-            case TEXT:
-            default:
-                break;
+    private fun importMediaToDirectory(col: com.ichi2.libanki.Collection, field: IField?) {
+        var tmpMediaPath: String? = null
+        when (field!!.type) {
+            EFieldType.AUDIO_RECORDING, EFieldType.AUDIO_CLIP -> tmpMediaPath = field.audioPath
+            EFieldType.IMAGE -> tmpMediaPath = field.imagePath
+            EFieldType.TEXT -> {
+            }
+            else -> {
+            }
         }
         if (tmpMediaPath != null) {
             try {
-                File inFile = new File(tmpMediaPath);
+                val inFile = File(tmpMediaPath)
                 if (inFile.exists() && inFile.length() > 0) {
-                    String fname = col.getMedia().addFile(inFile);
-                    File outFile = new File(col.getMedia().dir(), fname);
-                    if (field.hasTemporaryMedia() && !outFile.getAbsolutePath().equals(tmpMediaPath)) {
+                    val fname = col.media.addFile(inFile)
+                    val outFile = File(col.media.dir(), fname)
+                    if (field.hasTemporaryMedia() && outFile.absolutePath != tmpMediaPath) {
                         // Delete original
-                        inFile.delete();
+                        inFile.delete()
                     }
-                    switch (field.getType()) {
-                        case AUDIO_RECORDING:
-                        case AUDIO_CLIP:
-                            field.setAudioPath(outFile.getAbsolutePath());
-                            break;
-                        case IMAGE:
-                            field.setImagePath(outFile.getAbsolutePath());
-                            break;
-                        default:
-                            break;
+                    when (field.type) {
+                        EFieldType.AUDIO_RECORDING, EFieldType.AUDIO_CLIP -> field.audioPath = outFile.absolutePath
+                        EFieldType.IMAGE -> field.imagePath = outFile.absolutePath
+                        else -> {
+                        }
                     }
                 }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } catch (EmptyMediaException mediaException) {
+            } catch (e: IOException) {
+                throw RuntimeException(e)
+            } catch (mediaException: EmptyMediaException) {
                 // This shouldn't happen, but we're fine to ignore it if it does.
-                Timber.w(mediaException);
-                AnkiDroidApp.sendExceptionReport(mediaException, "noteService::importMediaToDirectory");
+                Timber.w(mediaException)
+                AnkiDroidApp.sendExceptionReport(mediaException, "noteService::importMediaToDirectory")
             }
         }
     }
 
-
-
     /**
-     * @param replaceNewlines Converts {@link FieldEditText#NEW_LINE} to HTML linebreaks
+     * @param replaceNewlines Converts [FieldEditText.NEW_LINE] to HTML linebreaks
      */
+    @JvmStatic
     @VisibleForTesting
-    @NonNull
     @CheckResult
-    public static Bundle getFieldsAsBundleForPreview(java.util.Collection<? extends NoteField> editFields, boolean replaceNewlines) {
-        Bundle fields = new Bundle();
+    fun getFieldsAsBundleForPreview(editFields: Collection<NoteField?>?, replaceNewlines: Boolean): Bundle {
+        val fields = Bundle()
         // Save the content of all the note fields. We use the field's ord as the key to
         // easily map the fields correctly later.
         if (editFields == null) {
-            return fields;
+            return fields
         }
-        for (NoteField e : editFields) {
-            if (e == null || e.getFieldText() == null) {
-                continue;
+        for (e in editFields) {
+            if (e == null || e.fieldText == null) {
+                continue
             }
-
-            String fieldValue = convertToHtmlNewline(e.getFieldText(), replaceNewlines);
-            fields.putString(Integer.toString(e.getOrd()), fieldValue);
+            val fieldValue = convertToHtmlNewline(e.fieldText!!, replaceNewlines)
+            fields.putString(Integer.toString(e.ord), fieldValue)
         }
-        return fields;
+        return fields
     }
 
-    @NonNull
-    public static String convertToHtmlNewline(@NonNull String fieldData, boolean replaceNewlines) {
-        if (!replaceNewlines) {
-            return fieldData;
-        }
-        return fieldData.replace(FieldEditText.NEW_LINE, "<br>");
+    @JvmStatic
+    fun convertToHtmlNewline(fieldData: String, replaceNewlines: Boolean): String {
+        return if (!replaceNewlines) {
+            fieldData
+        } else fieldData.replace(FieldEditText.NEW_LINE, "<br>")
     }
 
-
-    public static void toggleMark(@NonNull Note note) {
+    @JvmStatic
+    fun toggleMark(note: Note) {
         if (isMarked(note)) {
-            note.delTag("marked");
+            note.delTag("marked")
         } else {
-            note.addTag("marked");
+            note.addTag("marked")
         }
-        note.flush();
+        note.flush()
     }
 
-    public static boolean isMarked(@NonNull Note note) {
-        return note.hasTag("marked");
+    @JvmStatic
+    fun isMarked(note: Note): Boolean {
+        return note.hasTag("marked")
     }
 
+    interface NoteField {
+        val ord: Int
 
-    public interface NoteField {
-        int getOrd();
         // ideally shouldn't be nullable
-        @Nullable
-        String getFieldText();
+        val fieldText: String?
     }
 }
