62b91f2a22f6dbf00baf3695e21859bf4f753ac0
codingtosh <codingtosh@gmail.com>
2021-11-29 14:45:16 +0530

    [Kotlin Migration] CustomSyncServer
    Package: com.ichi2.anki.web

--- AnkiDroid/src/main/java/com/ichi2/anki/web/CustomSyncServer.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/web/CustomSyncServer.kt
@@ -14,42 +14,42 @@
  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.ichi2.anki.web;
+package com.ichi2.anki.web
 
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.content.Context
+import android.content.SharedPreferences
+import timber.log.Timber
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import timber.log.Timber;
+object CustomSyncServer {
+    const val PREFERENCE_CUSTOM_SYNC_BASE = "syncBaseUrl"
+    const val PREFERENCE_CUSTOM_MEDIA_SYNC_URL = "syncMediaUrl"
+    const val PREFERENCE_ENABLE_CUSTOM_SYNC_SERVER = "useCustomSyncServer"
 
-public class CustomSyncServer {
-    public static final String PREFERENCE_CUSTOM_SYNC_BASE = "syncBaseUrl";
-    public static final String PREFERENCE_CUSTOM_MEDIA_SYNC_URL = "syncMediaUrl";
-    public static final String PREFERENCE_ENABLE_CUSTOM_SYNC_SERVER = "useCustomSyncServer";
-
-    @Nullable
-    public static String getMediaSyncUrl(@NonNull SharedPreferences preferences) {
-        return preferences.getString(PREFERENCE_CUSTOM_MEDIA_SYNC_URL, null);
+    @JvmStatic
+    fun getMediaSyncUrl(preferences: SharedPreferences): String? {
+        return preferences.getString(PREFERENCE_CUSTOM_MEDIA_SYNC_URL, null)
     }
 
-    @Nullable
-    public static String getSyncBaseUrl(@NonNull SharedPreferences preferences) {
-        return getSyncBaseUrlOrDefault( preferences, null);
+    @JvmStatic
+    fun getSyncBaseUrl(preferences: SharedPreferences): String? {
+        return getSyncBaseUrlOrDefault(preferences, null)
     }
 
-    public static String getSyncBaseUrlOrDefault(@NonNull SharedPreferences userPreferences, String defaultValue) {
-        return userPreferences.getString(PREFERENCE_CUSTOM_SYNC_BASE, defaultValue);
+    @JvmStatic
+    fun getSyncBaseUrlOrDefault(userPreferences: SharedPreferences, defaultValue: String?): String? {
+        return userPreferences.getString(PREFERENCE_CUSTOM_SYNC_BASE, defaultValue)
     }
 
-    public static boolean isEnabled(@NonNull SharedPreferences userPreferences) {
-        return userPreferences.getBoolean(PREFERENCE_ENABLE_CUSTOM_SYNC_SERVER, false);
+    @JvmStatic
+    fun isEnabled(userPreferences: SharedPreferences): Boolean {
+        return userPreferences.getBoolean(PREFERENCE_ENABLE_CUSTOM_SYNC_SERVER, false)
     }
 
-    public static void handleSyncServerPreferenceChange(Context context) {
-        Timber.i("Sync Server Preferences updated.");
+    @JvmStatic
+    fun handleSyncServerPreferenceChange(context: Context?) {
+        Timber.i("Sync Server Preferences updated.")
         // #4921 - if any of the preferences change, we should reset the HostNum.
         // This is because different servers use different HostNums for data mappings.
-        HostNumFactory.getInstance(context).reset();
+        HostNumFactory.getInstance(context).reset()
     }
 }
