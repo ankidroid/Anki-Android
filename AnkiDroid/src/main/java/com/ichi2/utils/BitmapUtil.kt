438ad73e3226a68e1870f9dfa2335bbefaea6ec2
codingtosh <codingtosh@gmail.com>
2021-12-22 15:23:31 +0530

    [Kotlin Migration] BitmapUtil
    Package: com.ichi2.utils

--- AnkiDroid/src/main/java/com/ichi2/utils/BitmapUtil.kt
+++ AnkiDroid/src/main/java/com/ichi2/utils/BitmapUtil.kt
@@ -17,95 +17,79 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-package com.ichi2.utils;
+package com.ichi2.utils
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.widget.ImageView
+import com.ichi2.anki.AnkiDroidApp
+import timber.log.Timber
+import java.io.File
+import java.io.FileInputStream
+import java.lang.Exception
 
-import android.widget.ImageView;
-
-import com.ichi2.anki.AnkiDroidApp;
-
-import java.io.File;
-import java.io.FileInputStream;
-
-import androidx.annotation.Nullable;
-import timber.log.Timber;
-
-public class BitmapUtil {
-
-    @Nullable
-    public static Bitmap decodeFile(File theFile, int IMAGE_MAX_SIZE) {
-        Bitmap bmp = null;
+object BitmapUtil {
+    @JvmStatic
+    fun decodeFile(theFile: File, IMAGE_MAX_SIZE: Int): Bitmap? {
+        var bmp: Bitmap? = null
         try {
             if (!theFile.exists()) {
-                Timber.i("not displaying preview - image does not exist: '%s'", theFile.getPath());
-                return null;
+                Timber.i("not displaying preview - image does not exist: '%s'", theFile.path)
+                return null
             }
             // Decode image size
-            BitmapFactory.Options o = new BitmapFactory.Options();
-            o.inJustDecodeBounds = true;
-
-            FileInputStream fis = null;
+            val o = BitmapFactory.Options()
+            o.inJustDecodeBounds = true
+            var fis: FileInputStream? = null
             try {
-                fis = new FileInputStream(theFile);
-                BitmapFactory.decodeStream(fis, null, o);
+                fis = FileInputStream(theFile)
+                BitmapFactory.decodeStream(fis, null, o)
             } finally {
-                if (fis != null) {
-                    fis.close();
-                }
+                fis?.close()
             }
-
-            int scale = 1;
+            var scale = 1
             if (o.outHeight > IMAGE_MAX_SIZE || o.outWidth > IMAGE_MAX_SIZE) {
-                scale = (int) Math.pow(
-                        2,
-                        (int) Math.round(Math.log(IMAGE_MAX_SIZE / (double) Math.max(o.outHeight, o.outWidth))
-                                / Math.log(0.5)));
+                scale = Math.pow(
+                    2.0,
+                    Math.round(
+                        Math.log(IMAGE_MAX_SIZE / Math.max(o.outHeight, o.outWidth).toDouble()) /
+                            Math.log(0.5)
+                    ).toDouble()
+                ).toInt()
             }
 
             // Decode with inSampleSize
-            BitmapFactory.Options o2 = new BitmapFactory.Options();
-            o2.inSampleSize = scale;
-
+            val o2 = BitmapFactory.Options()
+            o2.inSampleSize = scale
             try {
-                fis = new FileInputStream(theFile);
-                bmp = BitmapFactory.decodeStream(fis, null, o2);
+                fis = FileInputStream(theFile)
+                bmp = BitmapFactory.decodeStream(fis, null, o2)
             } finally {
-                fis.close(); //don't need a null check, as we reuse the variable.
+                fis!!.close() // don't need a null check, as we reuse the variable.
             }
-        } catch (Exception e) {
-            //#5513 - We don't know the reason for the crash, let's find out.
-            AnkiDroidApp.sendExceptionReport(e, "BitmapUtil decodeFile");
+        } catch (e: Exception) {
+            // #5513 - We don't know the reason for the crash, let's find out.
+            AnkiDroidApp.sendExceptionReport(e, "BitmapUtil decodeFile")
         }
-        return bmp;
+        return bmp
     }
 
-
-    public static void freeImageView(ImageView imageView) {
+    @JvmStatic
+    fun freeImageView(imageView: ImageView?) {
         // This code behaves differently on various OS builds. That is why put into try catch.
         try {
             if (imageView != null) {
-                Drawable dr = imageView.getDrawable();
-
-                if (dr == null) {
-                    return;
-                }
-
-                if (!(dr instanceof BitmapDrawable)) {
-                    return;
-                }
-                BitmapDrawable bd = (BitmapDrawable) imageView.getDrawable();
-                if (bd.getBitmap() != null) {
-                    bd.getBitmap().recycle();
-                    imageView.setImageBitmap(null);
+                @Suppress("UNUSED_VARIABLE")
+                val dr = (imageView.drawable ?: return) as? BitmapDrawable ?: return
+                val bd = imageView.drawable as BitmapDrawable
+                if (bd.bitmap != null) {
+                    bd.bitmap.recycle()
+                    imageView.setImageBitmap(null)
                 }
             }
-        } catch (Exception e) {
-            Timber.e(e);
+        } catch (e: Exception) {
+            Timber.e(e)
         }
     }
-
 }
