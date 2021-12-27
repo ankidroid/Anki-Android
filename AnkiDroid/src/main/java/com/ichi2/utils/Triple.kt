2b7edbc5c606053320170b7e7d2f22792d93d140
codingtosh <codingtosh@gmail.com>
2021-12-27 23:45:05 +0530

    [Kotlin Migration] Triple
    Package: com.ichi2.utils

--- AnkiDroid/src/main/java/com/ichi2/utils/Triple.kt
+++ AnkiDroid/src/main/java/com/ichi2/utils/Triple.kt
@@ -14,16 +14,6 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-package com.ichi2.utils;
+package com.ichi2.utils
 
-public class Triple<First, Second, Triple> {
-    public final First first;
-    public final Second second;
-    public final Triple third;
-
-    public Triple(First first, Second second, Triple third) {
-        this.first = first;
-        this.second = second;
-        this.third = third;
-    }
-}
+class Triple<First, Second, Triple> constructor(@JvmField val first: First, @JvmField val second: Second, @JvmField val third: Triple)
