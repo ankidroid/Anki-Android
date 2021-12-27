dc9f8f861a8af32ac5bc871ac7264f69015b6cf1
codingtosh <codingtosh@gmail.com>
2021-12-27 22:09:56 +0530

    [Kotlin Migration] SyncStatus
    Package: com.ichi2.utils

--- AnkiDroid/src/main/java/com/ichi2/utils/SyncStatus.kt
+++ AnkiDroid/src/main/java/com/ichi2/utils/SyncStatus.kt
@@ -14,112 +14,97 @@
  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.ichi2.utils;
+package com.ichi2.utils
 
-import android.content.SharedPreferences;
+import com.ichi2.anki.AnkiDroidApp
+import com.ichi2.libanki.Collection
+import timber.log.Timber
+import java.util.function.Supplier
 
-import com.ichi2.anki.AnkiDroidApp;
-import com.ichi2.libanki.Collection;
+enum class SyncStatus {
+    INCONCLUSIVE, NO_ACCOUNT, NO_CHANGES, HAS_CHANGES, FULL_SYNC, BADGE_DISABLED;
 
-import java.util.function.Supplier;
+    companion object {
+        private var sPauseCheckingDatabase = false
+        private var sMarkedInMemory = false
 
-import androidx.annotation.NonNull;
-import timber.log.Timber;
-
-public enum SyncStatus {
-    INCONCLUSIVE,
-    NO_ACCOUNT,
-    NO_CHANGES,
-    HAS_CHANGES,
-    FULL_SYNC,
-    BADGE_DISABLED;
-
-    private static boolean sPauseCheckingDatabase = false;
-    private static boolean sMarkedInMemory = false;
-
-
-    @NonNull
-    public static SyncStatus getSyncStatus(@NonNull Supplier<Collection> getCol) {
-        Collection col;
-        try {
-            col = getCol.get();
-        } catch (Exception e) {
-            Timber.w(e);
-            return SyncStatus.INCONCLUSIVE;
+        @JvmStatic
+        fun getSyncStatus(getCol: Supplier<Collection>): SyncStatus {
+            val col: Collection
+            col = try {
+                getCol.get()
+            } catch (e: Exception) {
+                Timber.w(e)
+                return INCONCLUSIVE
+            }
+            return getSyncStatus(col)
         }
 
-        return getSyncStatus(col);
-    }
-
-
-    @NonNull
-    public static SyncStatus getSyncStatus(@NonNull Collection col) {
-        if (isDisabled()) {
-            return SyncStatus.BADGE_DISABLED;
+        @JvmStatic
+        fun getSyncStatus(col: Collection): SyncStatus {
+            if (isDisabled) {
+                return BADGE_DISABLED
+            }
+            if (!isLoggedIn) {
+                return NO_ACCOUNT
+            }
+            if (col.schemaChanged()) {
+                return FULL_SYNC
+            }
+            return if (hasDatabaseChanges()) {
+                HAS_CHANGES
+            } else {
+                NO_CHANGES
+            }
         }
 
-        if (!isLoggedIn()) {
-            return SyncStatus.NO_ACCOUNT;
+        private val isDisabled: Boolean
+            get() {
+                val preferences = AnkiDroidApp.getSharedPrefs(AnkiDroidApp.getInstance())
+                return !preferences.getBoolean("showSyncStatusBadge", true)
+            }
+        private val isLoggedIn: Boolean
+            get() {
+                val preferences = AnkiDroidApp.getSharedPrefs(AnkiDroidApp.getInstance())
+                val hkey = preferences.getString("hkey", "")
+                return hkey != null && hkey.length != 0
+            }
+
+        /** Whether data has been changed - to be converted to Rust  */
+        fun hasDatabaseChanges(): Boolean {
+            return AnkiDroidApp.getSharedPrefs(AnkiDroidApp.getInstance()).getBoolean("changesSinceLastSync", false)
         }
 
-        if (col.schemaChanged()) {
-            return SyncStatus.FULL_SYNC;
+        /** To be converted to Rust  */
+        fun markDataAsChanged() {
+            if (sPauseCheckingDatabase) {
+                return
+            }
+            sMarkedInMemory = true
+            AnkiDroidApp.getSharedPrefs(AnkiDroidApp.getInstance()).edit().putBoolean("changesSinceLastSync", true).apply()
         }
 
-        if (hasDatabaseChanges()) {
-            return SyncStatus.HAS_CHANGES;
-        } else {
-            return SyncStatus.NO_CHANGES;
+        /** To be converted to Rust  */
+        @KotlinCleanup("Convert these to @RustCleanup")
+        @JvmStatic
+        fun markSyncCompleted() {
+            sMarkedInMemory = false
+            AnkiDroidApp.getSharedPrefs(AnkiDroidApp.getInstance()).edit().putBoolean("changesSinceLastSync", false).apply()
         }
-    }
 
-
-    private static boolean isDisabled() {
-        SharedPreferences preferences = AnkiDroidApp.getSharedPrefs(AnkiDroidApp.getInstance());
-        return !preferences.getBoolean("showSyncStatusBadge", true);
-    }
-
-
-    private static boolean isLoggedIn() {
-        SharedPreferences preferences = AnkiDroidApp.getSharedPrefs(AnkiDroidApp.getInstance());
-        String hkey = preferences.getString("hkey", "");
-        return hkey != null && hkey.length() != 0;
-    }
-
-
-    /** Whether data has been changed - to be converted to Rust */
-    public static boolean hasDatabaseChanges() {
-        return AnkiDroidApp.getSharedPrefs(AnkiDroidApp.getInstance()).getBoolean("changesSinceLastSync", false);
-    }
-
-    /** To be converted to Rust */
-    public static void markDataAsChanged() {
-        if (sPauseCheckingDatabase) {
-            return;
+        @JvmStatic
+        fun ignoreDatabaseModification(runnable: Runnable) {
+            sPauseCheckingDatabase = true
+            try {
+                runnable.run()
+            } finally {
+                sPauseCheckingDatabase = false
+            }
         }
-        sMarkedInMemory = true;
-        AnkiDroidApp.getSharedPrefs(AnkiDroidApp.getInstance()).edit().putBoolean("changesSinceLastSync", true).apply();
-    }
 
-
-    /** To be converted to Rust */
-    public static void markSyncCompleted() {
-        sMarkedInMemory = false;
-        AnkiDroidApp.getSharedPrefs(AnkiDroidApp.getInstance()).edit().putBoolean("changesSinceLastSync", false).apply();
-    }
-
-
-    public static void ignoreDatabaseModification(@NonNull Runnable runnable) {
-        sPauseCheckingDatabase = true;
-        try {
-            runnable.run();
-        } finally {
-            sPauseCheckingDatabase = false;
+        /** Whether a change in data has been detected - used as a heuristic to stop slow operations  */
+        fun hasBeenMarkedAsChangedInMemory(): Boolean {
+            return sMarkedInMemory
         }
-    }
-
-    /** Whether a change in data has been detected - used as a heuristic to stop slow operations */
-    public static boolean hasBeenMarkedAsChangedInMemory() {
-        return sMarkedInMemory;
     }
 }
