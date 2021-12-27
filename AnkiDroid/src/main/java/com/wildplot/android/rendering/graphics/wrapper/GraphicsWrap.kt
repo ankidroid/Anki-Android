f796d5c0a9bab4a09b31ad5490bbddb3a326cd6e
codingtosh <codingtosh@gmail.com>
2021-12-28 03:26:12 +0530

    [Kotlin Migration] GraphicsWrap
    package: com.wildplot.android.rendering.graphics.wrapper

--- AnkiDroid/src/main/java/com/wildplot/android/rendering/graphics/wrapper/GraphicsWrap.kt
+++ AnkiDroid/src/main/java/com/wildplot/android/rendering/graphics/wrapper/GraphicsWrap.kt
@@ -13,155 +13,109 @@
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
-package com.wildplot.android.rendering.graphics.wrapper;
 
-import android.annotation.SuppressLint;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Paint.Style;
-import android.graphics.RectF;
-import android.graphics.Typeface;
+package com.wildplot.android.rendering.graphics.wrapper
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Typeface
 
 /**
  * Wrapper of swing/awt graphics class for android use
  *
  * @author Michael Goldbach
  */
-@SuppressLint("NonPublicNonStaticFieldName")
-public class GraphicsWrap {
-    private final Canvas canvas;
-    private final Paint paint;
-
-
-    public GraphicsWrap(Canvas canvas, Paint paint) {
-        super();
-        this.canvas = canvas;
-        this.paint = paint;
-    }
-
-
-    public void drawLine(float x1, float y1, float x2, float y2) {
-        Style oldStyle = paint.getStyle();
-        paint.setStyle(Style.FILL_AND_STROKE);
-        canvas.drawLine(x1, y1, x2, y2, paint);
-        paint.setStyle(oldStyle);
-    }
-
-
-    public void drawRect(float x, float y, float width, float height) {
-        Style oldStyle = paint.getStyle();
-        paint.setStyle(Style.STROKE);
-        canvas.drawRect(x, y, x + width, y + height, paint);
-        paint.setStyle(oldStyle);
-    }
-
-
-    public void fillRect(float x, float y, float width, float height) {
-
-        Style oldStyle = paint.getStyle();
-        paint.setStyle(Style.FILL);
-        canvas.drawRect(x, y, x + width, y + height, paint);
-        paint.setStyle(oldStyle);
-    }
-
-
-    public StrokeWrap getStroke() {
-        return new StrokeWrap(paint.getStrokeWidth());
-    }
-
-
-    public void setStroke(StrokeWrap stroke) {
-        paint.setStrokeWidth(stroke.getStrokeSize());
-    }
-
-
-    public RectangleWrap getClipBounds() {
-        return new RectangleWrap(canvas.getClipBounds());
-    }
-    
-    public ColorWrap getColor(){
-        return new ColorWrap(paint.getColor());
-    }
-
-
-    public void setColor(ColorWrap color) {
-        paint.setColor(color.getColorValue());
-    }
-
-
-    public void drawArc(float x, float y, float width, float height, float startAngle, float arcAngle) {
-        if (arcAngle == 0) {
-            return;
+class GraphicsWrap(private val canvas: Canvas, val paint: Paint) {
+    var stroke: StrokeWrap
+        get() = StrokeWrap(paint.strokeWidth)
+        set(stroke) {
+            paint.strokeWidth = stroke.strokeSize
         }
-        Style oldStyle = paint.getStyle();
-        paint.setStyle(Style.STROKE);
-        RectF rectF = new RectF(x, y, x + width, y + height);
-        canvas.drawArc(rectF, startAngle, arcAngle, true, paint);
-        paint.setStyle(oldStyle);
-    }
 
+    val clipBounds: RectangleWrap
+        get() = RectangleWrap(canvas.clipBounds)
 
-    public void fillArc(float x, float y, float width, float height, float startAngle, float arcAngle) {
-        if (arcAngle == 0) {
-            return;
+    var color: ColorWrap
+        get() = ColorWrap(paint.color)
+        set(color) {
+            paint.color = color.colorValue
         }
-        Style oldStyle = paint.getStyle();
-        paint.setStyle(Style.FILL);
-        RectF rectF = new RectF(x, y, x + width, y + height);
-        canvas.drawArc(rectF, startAngle, arcAngle, true, paint);
-        paint.setStyle(oldStyle);
+
+    val fontMetrics: FontMetricsWrap
+        get() = FontMetricsWrap(this)
+
+    var fontSize: Float
+        get() = paint.textSize
+        set(size) {
+            paint.textSize = size
+        }
+
+    var typeface: Typeface?
+        get() = paint.typeface
+        set(typeface) {
+            paint.typeface = typeface
+        }
+
+    fun drawLine(x1: Float, y1: Float, x2: Float, y2: Float) {
+        val oldStyle = paint.style
+        paint.style = Paint.Style.FILL_AND_STROKE
+        canvas.drawLine(x1, y1, x2, y2, paint)
+        paint.style = oldStyle
     }
 
-
-    public void drawString(String text, float x, float y) {
-        Style oldStyle = paint.getStyle();
-        paint.setStyle(Style.FILL);
-        canvas.drawText(text, x, y, paint);
-        paint.setStyle(oldStyle);
-
+    fun drawRect(x: Float, y: Float, width: Float, height: Float) {
+        val oldStyle = paint.style
+        paint.style = Paint.Style.STROKE
+        canvas.drawRect(x, y, x + width, y + height, paint)
+        paint.style = oldStyle
     }
 
-    public Paint getPaint() {
-        return paint;
+    fun fillRect(x: Float, y: Float, width: Float, height: Float) {
+        val oldStyle = paint.style
+        paint.style = Paint.Style.FILL
+        canvas.drawRect(x, y, x + width, y + height, paint)
+        paint.style = oldStyle
     }
 
-
-    public FontMetricsWrap getFontMetrics() {
-        return new FontMetricsWrap(this);
+    fun drawArc(x: Float, y: Float, width: Float, height: Float, startAngle: Float, arcAngle: Float) {
+        if (arcAngle == 0f) {
+            return
+        }
+        val oldStyle = paint.style
+        paint.style = Paint.Style.STROKE
+        val rectF = RectF(x, y, x + width, y + height)
+        canvas.drawArc(rectF, startAngle, arcAngle, true, paint)
+        paint.style = oldStyle
     }
 
-
-    public int save() {
-        return canvas.save();
+    fun fillArc(x: Float, y: Float, width: Float, height: Float, startAngle: Float, arcAngle: Float) {
+        if (arcAngle == 0f) {
+            return
+        }
+        val oldStyle = paint.style
+        paint.style = Paint.Style.FILL
+        val rectF = RectF(x, y, x + width, y + height)
+        canvas.drawArc(rectF, startAngle, arcAngle, true, paint)
+        paint.style = oldStyle
     }
 
-
-    public void restore() {
-        canvas.restore();
+    fun drawString(text: String?, x: Float, y: Float) {
+        val oldStyle = paint.style
+        paint.style = Paint.Style.FILL
+        canvas.drawText(text!!, x, y, paint)
+        paint.style = oldStyle
     }
 
-
-    public void rotate(float degree, float x, float y) {
-        canvas.rotate(degree, x, y);
+    fun save(): Int {
+        return canvas.save()
     }
 
-
-    public float getFontSize() {
-        return paint.getTextSize();
+    fun restore() {
+        canvas.restore()
     }
 
-
-    public void setFontSize(float size) {
-        paint.setTextSize(size);
-    }
-
-
-    public void setTypeface(Typeface typeface) {
-        paint.setTypeface(typeface);
-    }
-
-
-    public Typeface getTypeface() {
-        return paint.getTypeface();
+    fun rotate(degree: Float, x: Float, y: Float) {
+        canvas.rotate(degree, x, y)
     }
 }
