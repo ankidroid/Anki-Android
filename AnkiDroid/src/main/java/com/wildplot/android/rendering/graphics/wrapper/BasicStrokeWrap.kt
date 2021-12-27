62d90fb653266a66f52164cdfeff1cf55eb0208b
codingtosh <codingtosh@gmail.com>
2021-12-28 01:33:02 +0530

    [Kotlin Migration] BasicStrokeWrap
    package: com.wildplot.android.rendering.graphics.wrapper

--- AnkiDroid/src/main/java/com/wildplot/android/rendering/graphics/wrapper/BasicStrokeWrap.kt
+++ AnkiDroid/src/main/java/com/wildplot/android/rendering/graphics/wrapper/BasicStrokeWrap.kt
@@ -13,12 +13,7 @@
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
-package com.wildplot.android.rendering.graphics.wrapper;
 
-public class BasicStrokeWrap extends StrokeWrap {
+package com.wildplot.android.rendering.graphics.wrapper
 
-    public BasicStrokeWrap(float strokeSize) {
-        super(strokeSize);
-    }
-
-}
+class BasicStrokeWrap(strokeSize: Float) : StrokeWrap(strokeSize)
