c638177d25b261bf7eb584bb7c525256faec4196
codingtosh <codingtosh@gmail.com>
2021-12-28 12:59:14 +0530

    [Kotlin Migration] Function3D
    package: com.wildplot.android.rendering.interfaces

--- AnkiDroid/src/main/java/com/wildplot/android/rendering/interfaces/Function3D.kt
+++ AnkiDroid/src/main/java/com/wildplot/android/rendering/interfaces/Function3D.kt
@@ -13,19 +13,19 @@
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
-package com.wildplot.android.rendering.interfaces;
+
+package com.wildplot.android.rendering.interfaces
 
 /**
  * Interface for functions with two dependent variables
  *
  */
-public interface Function3D {
-	
-	/**
-	 * calculate function value with given x and y values
-	 * @param x given x value
-	 * @param y given y value
-	 * @return function value calculated with given x and y values
-	 */
- 	double f(double x, double y);
+interface Function3D {
+    /**
+     * calculate function value with given x and y values
+     * @param x given x value
+     * @param y given y value
+     * @return function value calculated with given x and y values
+     */
+    fun f(x: Double, y: Double): Double
 }
