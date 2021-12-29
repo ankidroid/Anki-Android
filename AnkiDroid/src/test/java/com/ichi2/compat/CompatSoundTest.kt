3055979446e67b3d1352debd932ced7178b7a2b4
raddi1972 <dixitrudransh01@gmail.com>
2021-12-29 17:48:27 +0530

    Added @TargetApi and fixed testAbandonAudioFocus()

--- AnkiDroid/src/test/java/com/ichi2/compat/CompatSoundTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/compat/CompatSoundTest.kt
@@ -16,6 +16,7 @@
 
 package com.ichi2.compat
 
+import android.annotation.TargetApi
 import android.content.Context
 import android.media.AudioManager
 import android.media.AudioManager.OnAudioFocusChangeListener
@@ -23,10 +24,9 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.ichi2.anki.AnkiDroidApp
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
-@Config(sdk = [21, 26])
+@TargetApi(26)
 class CompatSoundTest {
     private val compat: Compat = CompatHelper.getCompat()
     private val audioManager: AudioManager = AnkiDroidApp.getInstance().applicationContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
@@ -39,6 +39,6 @@ class CompatSoundTest {
 
     @Test
     fun testAbandonAudioFocus() {
-        compat.requestAudioFocus(audioManager, afChangeListener, null)
+        compat.abandonAudioFocus(audioManager, afChangeListener, null)
     }
 }
