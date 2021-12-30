6096bab41f4cf6b4734ad4488c5e24e91e4b5bc0
codingtosh <codingtosh@gmail.com>
2021-12-30 13:36:22 +0530

    [Kotlin Migration] DroidBackendFactory
    package: com.ichi2.libanki.backend

--- AnkiDroid/src/main/java/com/ichi2/libanki/backend/DroidBackendFactory.kt
+++ AnkiDroid/src/main/java/com/ichi2/libanki/backend/DroidBackendFactory.kt
@@ -14,72 +14,61 @@
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.ichi2.libanki.backend;
+package com.ichi2.libanki.backend
 
-import com.ichi2.anki.AnkiDroidApp;
-import com.ichi2.libanki.Consts;
+import androidx.annotation.VisibleForTesting
+import com.ichi2.anki.AnkiDroidApp
+import com.ichi2.libanki.Consts
+import net.ankiweb.rsdroid.BackendFactory
+import net.ankiweb.rsdroid.RustBackendFailedException
+import net.ankiweb.rsdroid.RustCleanup
+import timber.log.Timber
 
-import net.ankiweb.rsdroid.BackendFactory;
-import net.ankiweb.rsdroid.RustBackendFailedException;
-import net.ankiweb.rsdroid.RustCleanup;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.VisibleForTesting;
-import timber.log.Timber;
-
-/** Responsible for selection of either the Rust or Java-based backend */
-public class DroidBackendFactory {
-
-    private static DroidBackend sBackendForTesting;
-
-
-    /** Intentionally private - use {@link DroidBackendFactory#getInstance(boolean)}} */
-    private DroidBackendFactory() {
-
-    }
+/** Responsible for selection of either the Rust or Java-based backend  */
+object DroidBackendFactory {
+    @JvmStatic
+    private var sBackendForTesting: DroidBackend? = null
 
     /**
-     * Obtains an instance of a {@link DroidBackend}.
+     * Obtains an instance of a [DroidBackend].
      * Each call to this method will generate a separate instance which can handle a new Anki collection
      */
-    @NonNull
+    @JvmStatic
     @RustCleanup("Change back to a constant SYNC_VER")
-    public static DroidBackend getInstance(boolean useBackend) {
+    fun getInstance(useBackend: Boolean): DroidBackend {
         if (sBackendForTesting != null) {
-            return sBackendForTesting;
+            return sBackendForTesting!!
         }
-
-        BackendFactory backendFactory = null;
+        var backendFactory: BackendFactory? = null
         if (useBackend) {
             try {
-                backendFactory = BackendFactory.createInstance();
-            } catch (RustBackendFailedException e) {
-                Timber.w(e, "Rust backend failed to load - falling back to Java");
-                AnkiDroidApp.sendExceptionReport(e, "DroidBackendFactory::getInstance");
+                backendFactory = BackendFactory.createInstance()
+            } catch (e: RustBackendFailedException) {
+                Timber.w(e, "Rust backend failed to load - falling back to Java")
+                AnkiDroidApp.sendExceptionReport(e, "DroidBackendFactory::getInstance")
             }
         }
-
-        DroidBackend instance = getInstance(backendFactory);
+        val instance = getInstance(backendFactory)
         // Update the Sync version if we can load the Rust
-        Consts.SYNC_VER = backendFactory == null ? 9 : 10;
-        return instance;
+        Consts.SYNC_VER = if (backendFactory == null) 9 else 10
+        return instance
     }
 
-    private static DroidBackend getInstance(@Nullable BackendFactory backendFactory) {
+    @JvmStatic
+    private fun getInstance(backendFactory: BackendFactory?): DroidBackend {
         if (backendFactory == null) {
-            return new JavaDroidBackend();
+            return JavaDroidBackend()
         }
-
-        if (AnkiDroidApp.TESTING_USE_V16_BACKEND) {
-            return new RustDroidV16Backend(backendFactory);
+        return if (AnkiDroidApp.TESTING_USE_V16_BACKEND) {
+            RustDroidV16Backend(backendFactory)
         } else {
-            return new RustDroidBackend(backendFactory);
+            RustDroidBackend(backendFactory)
         }
     }
 
+    @JvmStatic
     @VisibleForTesting
-    public static void setOverride(DroidBackend backend) {
-        sBackendForTesting = backend;
+    fun setOverride(backend: DroidBackend?) {
+        sBackendForTesting = backend
     }
 }
