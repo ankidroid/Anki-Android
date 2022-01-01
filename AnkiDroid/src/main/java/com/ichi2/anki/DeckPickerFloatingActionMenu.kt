e7acf937506a80e8d972c71b363855db7fb43f60
Akshay <52353967+Akshay0701@users.noreply.github.com>
2022-01-01 12:58:16 +0530

    Migrate DeckPickerFloatingActionMenu to Kotlin

--- AnkiDroid/src/main/java/com/ichi2/anki/DeckPickerFloatingActionMenu.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/DeckPickerFloatingActionMenu.kt
@@ -13,181 +13,147 @@
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.anki
 
-package com.ichi2.anki;
+import android.animation.Animator
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.ichi2.anki.dialogs.CreateDeckDialog
+import com.ichi2.utils.KotlinCleanup
+import timber.log.Timber
 
-import android.animation.Animator;
-import android.content.SharedPreferences;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.ichi2.anki.dialogs.CreateDeckDialog;
-
-import timber.log.Timber;
-
-public class DeckPickerFloatingActionMenu {
-
-    private final FloatingActionButton mFabMain;
-    private final LinearLayout mAddSharedLayout;
-    private final LinearLayout mAddDeckLayout;
-    private final LinearLayout mAddNoteLayout;
-    private final View mFabBGLayout;
-    private final View mStudyoptionsFrame;
-    private boolean mIsFABOpen = false;
-    private boolean mFragmented = false;
-
-    private final DeckPicker mDeckPicker;
-    private final LinearLayout mLinearLayout;
-
-    public DeckPickerFloatingActionMenu(View view, DeckPicker deckPicker) {
-        mDeckPicker = deckPicker;
-        mAddNoteLayout = (LinearLayout)view.findViewById(R.id.add_note_layout);
-        mAddSharedLayout = (LinearLayout)view.findViewById(R.id.add_shared_layout);
-        mAddDeckLayout = (LinearLayout)view.findViewById(R.id.add_deck_layout);
-        mFabMain = (FloatingActionButton)view.findViewById(R.id.fab_main);
-        FloatingActionButton addNoteButton = (FloatingActionButton) view.findViewById(R.id.add_note_action);
-        FloatingActionButton addSharedButton = (FloatingActionButton) view.findViewById(R.id.add_shared_action);
-        FloatingActionButton addDeckButton = (FloatingActionButton) view.findViewById(R.id.add_deck_action);
-        mFabBGLayout = view.findViewById(R.id.fabBGLayout);
-        mLinearLayout = view.findViewById(R.id.deckpicker_view);  // Layout deck_picker.xml is attached here
-
-        // Check to see if the view was correctly located
-        mStudyoptionsFrame = view.findViewById(R.id.studyoptions_fragment);
-        mFragmented = mStudyoptionsFrame != null ? true : false;
-
-        TextView addNoteLabel = view.findViewById(R.id.add_note_label);
-        TextView addSharedLabel = view.findViewById(R.id.add_shared_label);
-        TextView addDeckLabel = view.findViewById(R.id.add_deck_label);
-
-        mFabMain.setOnClickListener(v -> {
-            if (!mIsFABOpen) {
-                showFloatingActionMenu();
-            } else {
-                closeFloatingActionMenu();
-            }
-        });
-
-        mFabBGLayout.setOnClickListener(v -> closeFloatingActionMenu());
-
-        View.OnClickListener addDeckListener = addDeckButtonView -> {
-            if (mIsFABOpen) {
-                closeFloatingActionMenu();
-                CreateDeckDialog createDeckDialog = new CreateDeckDialog(mDeckPicker, R.string.new_deck, CreateDeckDialog.DeckDialogType.DECK, null);
-                createDeckDialog.setOnNewDeckCreated((i) -> deckPicker.updateDeckList());
-                createDeckDialog.showDialog();
-            }
-        };
-        addDeckButton.setOnClickListener(addDeckListener);
-        addDeckLabel.setOnClickListener(addDeckListener);
-
-        View.OnClickListener addSharedListener = addSharedButtonView -> {
-            Timber.d("configureFloatingActionsMenu::addSharedButton::onClickListener - Adding Shared Deck");
-            closeFloatingActionMenu();
-            deckPicker.openAnkiWebSharedDecks();
-        };
-        addSharedButton.setOnClickListener(addSharedListener);
-        addSharedLabel.setOnClickListener(addSharedListener);
-
-        View.OnClickListener addNoteListener = addNoteButtonView -> {
-            Timber.d("configureFloatingActionsMenu::addNoteButton::onClickListener - Adding Note");
-            closeFloatingActionMenu();
-            deckPicker.addNote();
-        };
-        addNoteButton.setOnClickListener(addNoteListener);
-        addNoteLabel.setOnClickListener(addNoteListener);
+class DeckPickerFloatingActionMenu(view: View, private val deckPicker: DeckPicker) {
+    @KotlinCleanup("can be changed to view.findViewById<FloatingActionButton>")
+    private val mFabMain: FloatingActionButton = view.findViewById<View>(R.id.fab_main) as FloatingActionButton
+    private val mAddSharedLayout: LinearLayout = view.findViewById<View>(R.id.add_shared_layout) as LinearLayout
+    private val mAddDeckLayout: LinearLayout = view.findViewById<View>(R.id.add_deck_layout) as LinearLayout
+    private val mAddNoteLayout: LinearLayout = view.findViewById<View>(R.id.add_note_layout) as LinearLayout
+    private val mFabBGLayout: View
+    private val mStudyoptionsFrame: View?
+    var isFABOpen = false
+    private var mFragmented = false
+    private val mLinearLayout: LinearLayout
+    private fun animationEnabled(): Boolean {
+        val preferences = AnkiDroidApp.getSharedPrefs(deckPicker)
+        return !preferences.getBoolean("safeDisplay", false)
     }
 
-    private boolean animationEnabled() {
-        SharedPreferences preferences = AnkiDroidApp.getSharedPrefs(mDeckPicker);
-        return !preferences.getBoolean("safeDisplay", false);
-    }
-
-
-    public boolean isFABOpen() {
-        return mIsFABOpen;
-    }
-
-
-    public void setIsFABOpen(boolean isFABOpen) {
-        this.mIsFABOpen = isFABOpen;
-    }
-
-
-    private void showFloatingActionMenu() {
-        mLinearLayout.setAlpha(0.5f);
+    private fun showFloatingActionMenu() {
+        mLinearLayout.alpha = 0.5f
         if (mFragmented) {
-            mStudyoptionsFrame.setAlpha(0.5f);
+            mStudyoptionsFrame!!.alpha = 0.5f
         }
-        mIsFABOpen = true;
+        isFABOpen = true
         if (animationEnabled()) {
             // Show with animation
-            mAddNoteLayout.setVisibility(View.VISIBLE);
-            mAddSharedLayout.setVisibility(View.VISIBLE);
-            mAddDeckLayout.setVisibility(View.VISIBLE);
-            mFabBGLayout.setVisibility(View.VISIBLE);
-            mFabMain.animate().rotationBy(135); // 135 = 90 + 45
-            mAddNoteLayout.animate().translationY(0).setDuration(30);
-            mAddSharedLayout.animate().translationY(0).setDuration(50);
-            mAddDeckLayout.animate().translationY(0).setDuration(100);
-            mAddDeckLayout.animate().alpha(1f).setDuration(100);
-            mAddSharedLayout.animate().alpha(1f).setDuration(50);
-            mAddNoteLayout.animate().alpha(1f).setDuration(30);
+            mAddNoteLayout.visibility = View.VISIBLE
+            mAddSharedLayout.visibility = View.VISIBLE
+            mAddDeckLayout.visibility = View.VISIBLE
+            mFabBGLayout.visibility = View.VISIBLE
+            mFabMain.animate().rotationBy(135f) // 135 = 90 + 45
+            mAddNoteLayout.animate().translationY(0f).duration = 30
+            mAddSharedLayout.animate().translationY(0f).duration = 50
+            mAddDeckLayout.animate().translationY(0f).duration = 100
+            mAddDeckLayout.animate().alpha(1f).duration = 100
+            mAddSharedLayout.animate().alpha(1f).duration = 50
+            mAddNoteLayout.animate().alpha(1f).duration = 30
         } else {
             // Show without animation
-            mAddNoteLayout.setVisibility(View.VISIBLE);
-            mAddSharedLayout.setVisibility(View.VISIBLE);
-            mAddDeckLayout.setVisibility(View.VISIBLE);
-            mFabBGLayout.setVisibility(View.VISIBLE);
-            mAddNoteLayout.setAlpha(1f);
-            mAddSharedLayout.setAlpha(1f);
-            mAddDeckLayout.setAlpha(1f);
-            mAddNoteLayout.setTranslationY(0);
-            mAddSharedLayout.setTranslationY(0);
-            mAddDeckLayout.setTranslationY(0);
+            mAddNoteLayout.visibility = View.VISIBLE
+            mAddSharedLayout.visibility = View.VISIBLE
+            mAddDeckLayout.visibility = View.VISIBLE
+            mFabBGLayout.visibility = View.VISIBLE
+            mAddNoteLayout.alpha = 1f
+            mAddSharedLayout.alpha = 1f
+            mAddDeckLayout.alpha = 1f
+            mAddNoteLayout.translationY = 0f
+            mAddSharedLayout.translationY = 0f
+            mAddDeckLayout.translationY = 0f
         }
     }
 
-    protected void closeFloatingActionMenu() {
-        mLinearLayout.setAlpha(1f);
+    fun closeFloatingActionMenu() {
+        mLinearLayout.alpha = 1f
         if (mFragmented) {
-            mStudyoptionsFrame.setAlpha(1f);
+            mStudyoptionsFrame!!.alpha = 1f
         }
-        mIsFABOpen = false;
-        mFabBGLayout.setVisibility(View.GONE);
+        isFABOpen = false
+        mFabBGLayout.visibility = View.GONE
         if (animationEnabled()) {
             // Close with animation
-            mFabMain.animate().rotation(0);
-            mAddNoteLayout.animate().translationY(200f).setDuration(30);
-            mAddSharedLayout.animate().translationY(400f).setDuration(50);
-            mAddDeckLayout.animate().alpha(0f).setDuration(100);
-            mAddSharedLayout.animate().alpha(0f).setDuration(50);
-            mAddNoteLayout.animate().alpha(0f).setDuration(30);
-            mAddDeckLayout.animate().translationY(600f).setDuration(100).setListener(new Animator.AnimatorListener() {
-                @Override
-                public void onAnimationStart(Animator animator) { }
-
-                @Override
-                public void onAnimationEnd(Animator animator) {
-                    if (!mIsFABOpen) {
-                        mAddNoteLayout.setVisibility(View.GONE);
-                        mAddSharedLayout.setVisibility(View.GONE);
-                        mAddDeckLayout.setVisibility(View.GONE);
+            mFabMain.animate().rotation(0f)
+            mAddNoteLayout.animate().translationY(200f).duration = 30
+            mAddSharedLayout.animate().translationY(400f).duration = 50
+            mAddDeckLayout.animate().alpha(0f).duration = 100
+            mAddSharedLayout.animate().alpha(0f).duration = 50
+            mAddNoteLayout.animate().alpha(0f).duration = 30
+            mAddDeckLayout.animate().translationY(600f).setDuration(100).setListener(object : Animator.AnimatorListener {
+                override fun onAnimationStart(animator: Animator) {}
+                override fun onAnimationEnd(animator: Animator) {
+                    if (!isFABOpen) {
+                        mAddNoteLayout.visibility = View.GONE
+                        mAddSharedLayout.visibility = View.GONE
+                        mAddDeckLayout.visibility = View.GONE
                     }
                 }
 
-                @Override
-                public void onAnimationCancel(Animator animator) { }
-
-                @Override
-                public void onAnimationRepeat(Animator animator) { }
-            });
+                override fun onAnimationCancel(animator: Animator) {}
+                override fun onAnimationRepeat(animator: Animator) {}
+            })
         } else {
             // Close without animation
-            mAddNoteLayout.setVisibility(View.GONE);
-            mAddSharedLayout.setVisibility(View.GONE);
-            mAddDeckLayout.setVisibility(View.GONE);
+            mAddNoteLayout.visibility = View.GONE
+            mAddSharedLayout.visibility = View.GONE
+            mAddDeckLayout.visibility = View.GONE
         }
+    }
+
+    init {
+        val addNoteButton = view.findViewById<View>(R.id.add_note_action) as FloatingActionButton
+        val addSharedButton = view.findViewById<View>(R.id.add_shared_action) as FloatingActionButton
+        val addDeckButton = view.findViewById<View>(R.id.add_deck_action) as FloatingActionButton
+        mFabBGLayout = view.findViewById(R.id.fabBGLayout)
+        mLinearLayout = view.findViewById(R.id.deckpicker_view) // Layout deck_picker.xml is attached here
+
+        // Check to see if the view was correctly located
+        mStudyoptionsFrame = view.findViewById(R.id.studyoptions_fragment)
+        mFragmented = mStudyoptionsFrame != null
+        val addNoteLabel = view.findViewById<TextView>(R.id.add_note_label)
+        val addSharedLabel = view.findViewById<TextView>(R.id.add_shared_label)
+        val addDeckLabel = view.findViewById<TextView>(R.id.add_deck_label)
+        mFabMain.setOnClickListener {
+            if (!isFABOpen) {
+                showFloatingActionMenu()
+            } else {
+                closeFloatingActionMenu()
+            }
+        }
+        mFabBGLayout.setOnClickListener { closeFloatingActionMenu() }
+        val addDeckListener = View.OnClickListener {
+            if (isFABOpen) {
+                closeFloatingActionMenu()
+                val createDeckDialog = CreateDeckDialog(deckPicker, R.string.new_deck, CreateDeckDialog.DeckDialogType.DECK, null)
+                createDeckDialog.setOnNewDeckCreated { deckPicker.updateDeckList() }
+                createDeckDialog.showDialog()
+            }
+        }
+        addDeckButton.setOnClickListener(addDeckListener)
+        addDeckLabel.setOnClickListener(addDeckListener)
+        val addSharedListener = View.OnClickListener {
+            Timber.d("configureFloatingActionsMenu::addSharedButton::onClickListener - Adding Shared Deck")
+            closeFloatingActionMenu()
+            deckPicker.openAnkiWebSharedDecks()
+        }
+        addSharedButton.setOnClickListener(addSharedListener)
+        addSharedLabel.setOnClickListener(addSharedListener)
+        val addNoteListener = View.OnClickListener {
+            Timber.d("configureFloatingActionsMenu::addNoteButton::onClickListener - Adding Note")
+            closeFloatingActionMenu()
+            deckPicker.addNote()
+        }
+        addNoteButton.setOnClickListener(addNoteListener)
+        addNoteLabel.setOnClickListener(addNoteListener)
     }
 }
