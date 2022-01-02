4481c662669ba2be7f3d5065b862f9fdefe24dd9
Akshay <52353967+Akshay0701@users.noreply.github.com>
2022-01-02 13:03:25 +0530

    [Kotlin Migration] TagsArrayAdapter

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/tags/TagsArrayAdapter.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/tags/TagsArrayAdapter.kt
@@ -13,160 +13,121 @@
  You should have received a copy of the GNU General Public License along with
  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.ichi2.anki.dialogs.tags;
+package com.ichi2.anki.dialogs.tags
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Filter;
-import android.widget.Filterable;
-import android.widget.TextView;
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import android.widget.TextView
+import androidx.annotation.VisibleForTesting
+import androidx.recyclerview.widget.RecyclerView
+import com.ichi2.anki.R
+import com.ichi2.ui.CheckBoxTriStates
+import com.ichi2.utils.FilterResultsUtils
+import java.util.*
+import kotlin.collections.ArrayList
 
-import com.ichi2.anki.R;
-import com.ichi2.ui.CheckBoxTriStates;
-import com.ichi2.utils.FilterResultsUtils;
+/**
+ * @param tags A reference to the {@link TagsList} passed.
+ */
+class TagsArrayAdapter(private val tags: TagsList) : RecyclerView.Adapter<TagsArrayAdapter.ViewHolder>(), Filterable {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        internal val mCheckBoxView: CheckBoxTriStates = itemView.findViewById(R.id.tags_dialog_tag_item_checkbox)
+        internal val mTextView: TextView = itemView.findViewById(R.id.tags_dialog_tag_item_text)
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
+        @get:VisibleForTesting(otherwise = VisibleForTesting.NONE)
+        val text: String
+            get() = mTextView.text.toString()
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.VisibleForTesting;
-import androidx.recyclerview.widget.RecyclerView;
+        @get:VisibleForTesting(otherwise = VisibleForTesting.NONE)
+        val isChecked: Boolean
+            get() = mCheckBoxView.isChecked
 
-import static androidx.annotation.VisibleForTesting.*;
-
-public class TagsArrayAdapter extends  RecyclerView.Adapter<TagsArrayAdapter.ViewHolder> implements Filterable {
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        private final CheckBoxTriStates mCheckBoxView;
-        private final TextView mTextView;
-        public ViewHolder(View itemView) {
-            super(itemView);
-            mCheckBoxView = itemView.findViewById(R.id.tags_dialog_tag_item_checkbox);
-            mTextView = itemView.findViewById(R.id.tags_dialog_tag_item_text);
-        }
-
-        @VisibleForTesting(otherwise = NONE)
-        public String getText() {
-            return mTextView.getText().toString();
-        }
-
-
-        @VisibleForTesting(otherwise = NONE)
-        public boolean isChecked() {
-            return mCheckBoxView.isChecked();
-        }
-
-
-        @VisibleForTesting(otherwise = NONE)
-        public CheckBoxTriStates.State getCheckboxState() {
-            return mCheckBoxView.getState();
-        }
+        @get:VisibleForTesting(otherwise = VisibleForTesting.NONE)
+        val checkboxState: CheckBoxTriStates.State
+            get() = mCheckBoxView.state
     }
 
-
-
     /**
-     * A reference to the {@link TagsList} passed.
-     */
-    @NonNull
-    private final TagsList mTags;
-    /**
-     * A subset of all tags in {@link #mTags} satisfying the user's search.
+     * A subset of all tags in [.mTags] satisfying the user's search.
      *
      * it will be null if the user search term is empty, in that case
-     * the adapter should use {@link #mTags} instead to access full list.
+     * the adapter should use [.mTags] instead to access full list.
      */
-    @Nullable
-    private List<String> mFilteredList;
-
-    public TagsArrayAdapter(@NonNull TagsList tags) {
-        mTags = tags;
-        mFilteredList = null;
-        sortData();
+    private var mFilteredList: ArrayList<String>? = null
+    fun sortData() {
+        tags.sort()
     }
 
-    public void sortData() {
-        mTags.sort();
-    }
-
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.tags_item_list_dialog, parent, false);
-
-        ViewHolder vh = new ViewHolder(v.findViewById(R.id.tags_dialog_tag_item));
-        vh.itemView.setOnClickListener(view -> {
-            CheckBoxTriStates checkBox = vh.mCheckBoxView;
-            String tag = vh.mTextView.getText().toString();
-            checkBox.toggle();
-            if (checkBox.getState() == CheckBoxTriStates.State.CHECKED) {
-                mTags.check(tag);
-            } else if (checkBox.getState() == CheckBoxTriStates.State.UNCHECKED) {
-                mTags.uncheck(tag);
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.tags_item_list_dialog, parent, false)
+        val vh = ViewHolder(v.findViewById(R.id.tags_dialog_tag_item))
+        vh.itemView.setOnClickListener {
+            val checkBox = vh.mCheckBoxView
+            val tag = vh.mTextView.text.toString()
+            checkBox.toggle()
+            if (checkBox.state == CheckBoxTriStates.State.CHECKED) {
+                tags.check(tag)
+            } else if (checkBox.state == CheckBoxTriStates.State.UNCHECKED) {
+                tags.uncheck(tag)
             }
-        });
-        return vh;
+        }
+        return vh
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        String tag = getTagByIndex(position);
-        holder.mTextView.setText(tag);
-        if (mTags.isIndeterminate(tag)) {
-            holder.mCheckBoxView.setState(CheckBoxTriStates.State.INDETERMINATE);
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val tag = getTagByIndex(position)
+        holder.mTextView.text = tag
+        if (tags.isIndeterminate(tag)) {
+            holder.mCheckBoxView.state = CheckBoxTriStates.State.INDETERMINATE
         } else {
-            holder.mCheckBoxView.setState(mTags.isChecked(tag) ?
-                            CheckBoxTriStates.State.CHECKED :
-                            CheckBoxTriStates.State.UNCHECKED);
+            holder.mCheckBoxView.state = if (tags.isChecked(tag)) CheckBoxTriStates.State.CHECKED else CheckBoxTriStates.State.UNCHECKED
         }
     }
 
-    private String getTagByIndex(int index) {
-        if (mFilteredList == null) {
-            return mTags.get(index);
-        }
-        return mFilteredList.get(index);
+    private fun getTagByIndex(index: Int): String {
+        return if (mFilteredList == null) {
+            tags[index]
+        } else mFilteredList!![index]
     }
 
-    @Override
-    public int getItemCount() {
-        if (mFilteredList == null) {
-            return mTags.size();
-        }
-        return mFilteredList.size();
+    override fun getItemCount(): Int {
+        return if (mFilteredList == null) {
+            tags.size()
+        } else mFilteredList!!.size
     }
 
-    @Override
-    public Filter getFilter() {
-        return new TagsFilter();
+    override fun getFilter(): Filter {
+        return TagsFilter()
     }
 
     /* Custom Filter class - as seen in http://stackoverflow.com/a/29792313/1332026 */
-    private class TagsFilter extends Filter {
-        @Override
-        protected FilterResults performFiltering(CharSequence constraint) {
-            if (constraint.length() == 0) {
-                mFilteredList = null;
+    private inner class TagsFilter : Filter() {
+        override fun performFiltering(constraint: CharSequence): FilterResults {
+            if (constraint.isEmpty()) {
+                mFilteredList = null
             } else {
-                mFilteredList = new ArrayList<>();
-                final String filterPattern = constraint.toString().toLowerCase(Locale.getDefault()).trim();
-                for (String tag : mTags) {
-                    if (tag.toLowerCase(Locale.getDefault()).contains(filterPattern)) {
-                        mFilteredList.add(tag);
+                mFilteredList = ArrayList()
+                val filterPattern = constraint.toString().lowercase(Locale.getDefault()).trim { it <= ' ' }
+                for (tag in tags) {
+                    if (tag.lowercase(Locale.getDefault()).contains(filterPattern)) {
+                        mFilteredList!!.add(tag)
                     }
                 }
             }
-
-            return FilterResultsUtils.fromCollection(mFilteredList);
+            return FilterResultsUtils.fromCollection(mFilteredList)
         }
 
-        @Override
-        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
-            sortData();
-            notifyDataSetChanged();
+        override fun publishResults(charSequence: CharSequence, filterResults: FilterResults) {
+            sortData()
+            notifyDataSetChanged()
         }
+    }
+
+    init {
+        sortData()
     }
 }
