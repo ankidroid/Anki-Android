f33958ae255fa1cc80daf80673fe2dd9bf7ed9c4
Akshay <52353967+Akshay0701@users.noreply.github.com>
2022-01-04 12:25:14 +0530

    [Kotlin Migration] StepsPreference

--- AnkiDroid/src/main/java/com/ichi2/preferences/StepsPreference.kt
+++ AnkiDroid/src/main/java/com/ichi2/preferences/StepsPreference.kt
@@ -14,154 +14,146 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-package com.ichi2.preferences;
+package com.ichi2.preferences
 
-import android.content.Context;
-import android.text.InputType;
-import android.text.TextUtils;
-import android.util.AttributeSet;
-import android.util.Log;
+import android.content.Context
+import android.text.InputType
+import android.text.TextUtils
+import android.util.AttributeSet
+import com.ichi2.anki.AnkiDroidApp
+import com.ichi2.anki.R
+import com.ichi2.anki.UIUtils.showThemedToast
+import com.ichi2.utils.JSONArray
+import com.ichi2.utils.JSONException
+import timber.log.Timber
 
-import com.ichi2.anki.AnkiDroidApp;
-import com.ichi2.anki.R;
-import com.ichi2.anki.UIUtils;
+@Suppress("deprecation") // TODO Tracked in https://github.com/ankidroid/Anki-Android/issues/5019
+class StepsPreference : android.preference.EditTextPreference {
+    private val mAllowEmpty: Boolean
 
-import com.ichi2.utils.JSONArray;
-import com.ichi2.utils.JSONException;
-
-import timber.log.Timber;
-
-@SuppressWarnings("deprecation") // TODO Tracked in https://github.com/ankidroid/Anki-Android/issues/5019
-public class StepsPreference extends android.preference.EditTextPreference {
-
-    private final boolean mAllowEmpty;
-
-
-    public StepsPreference(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        mAllowEmpty = getAllowEmptyFromAttributes(attrs);
-        updateSettings();
+    @Suppress("unused")
+    constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle) {
+        mAllowEmpty = getAllowEmptyFromAttributes(attrs)
+        updateSettings()
     }
 
-
-    public StepsPreference(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        mAllowEmpty = getAllowEmptyFromAttributes(attrs);
-        updateSettings();
+    @Suppress("unused")
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        mAllowEmpty = getAllowEmptyFromAttributes(attrs)
+        updateSettings()
     }
 
-
-    public StepsPreference(Context context) {
-        super(context);
-        mAllowEmpty = getAllowEmptyFromAttributes(null);
-        updateSettings();
+    @Suppress("unused")
+    constructor(context: Context?) : super(context) {
+        mAllowEmpty = getAllowEmptyFromAttributes(null)
+        updateSettings()
     }
-
 
     /**
      * Update settings to show a numeric keyboard instead of the default keyboard.
      * <p>
      * This method should only be called once from the constructor.
      */
-    private void updateSettings() {
+    private fun updateSettings() {
         // Use the number pad but still allow normal text for spaces and decimals.
-        getEditText().setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_CLASS_TEXT);
+        editText.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_CLASS_TEXT
     }
 
-
-    @Override
-    protected void onDialogClosed(boolean positiveResult) {
+    override fun onDialogClosed(positiveResult: Boolean) {
         if (positiveResult) {
-            String validated = getValidatedStepsInput(getEditText().getText().toString());
+            val validated = getValidatedStepsInput(editText.text.toString())
             if (validated == null) {
-                UIUtils.showThemedToast(getContext(), getContext().getResources().getString(R.string.steps_error), false);
+                showThemedToast(context, context.resources.getString(R.string.steps_error), false)
             } else if (TextUtils.isEmpty(validated) && !mAllowEmpty) {
-                UIUtils.showThemedToast(getContext(), getContext().getResources().getString(R.string.steps_min_error),
-                        false);
+                showThemedToast(
+                    context, context.resources.getString(R.string.steps_min_error),
+                    false
+                )
             } else {
-                setText(validated);
+                text = validated
             }
         }
     }
-
 
     /**
      * Check if the string is a valid format for steps and return that string, reformatted for better usability if
      * needed.
-     * 
+     *
      * @param steps User input in text editor.
      * @return The correctly formatted string or null if the input is not valid.
      */
-    private String getValidatedStepsInput(String steps) {
-        JSONArray stepsAr = convertToJSON(steps);
-        if (stepsAr == null) {
-            return null;
+    private fun getValidatedStepsInput(steps: String): String? {
+        val stepsAr = convertToJSON(steps)
+        return if (stepsAr == null) {
+            null
         } else {
-            StringBuilder sb = new StringBuilder();
-            for (String step: stepsAr.stringIterable()) {
-                sb.append(step).append(" ");
+            val sb = StringBuilder()
+            for (step in stepsAr.stringIterable()) {
+                sb.append(step).append(" ")
             }
-            return sb.toString().trim();
+            sb.toString().trim { it <= ' ' }
         }
     }
 
-
-    /**
-     * Convert steps format.
-     * 
-     * @param a JSONArray representation of steps.
-     * @return The steps as a space-separated string.
-     */
-    public static String convertFromJSON(JSONArray a) {
-        StringBuilder sb = new StringBuilder();
-        for (String s: a.stringIterable()) {
-            sb.append(s).append(" ");
-        }
-        return sb.toString().trim();
+    private fun getAllowEmptyFromAttributes(attrs: AttributeSet?): Boolean {
+        return attrs?.getAttributeBooleanValue(AnkiDroidApp.XML_CUSTOM_NAMESPACE, "allowEmpty", true)
+            ?: true
     }
 
-
-    /**
-     * Convert steps format. For better usability, rounded floats are converted to integers (e.g., 1.0 is converted to
-     * 1).
-     * 
-     * @param steps String representation of steps.
-     * @return The steps as a JSONArray or null if the steps are not valid.
-     */
-    public static JSONArray convertToJSON(String steps) {
-        JSONArray stepsAr = new JSONArray();
-        steps = steps.trim();
-        if (TextUtils.isEmpty(steps)) {
-            return stepsAr;
-        }
-        try {
-            for (String s : steps.split("\\s+")) {
-                double d = Double.parseDouble(s);
-                // 0 or less is not a valid step.
-                if (d <= 0) {
-                    return null;
-                }
-                // Use whole numbers if we can (but still allow decimals)
-                int i = (int) d;
-                if (i == d) {
-                    stepsAr.put(i);
-                } else {
-                    stepsAr.put(d);
-                }
+    companion object {
+        /**
+         * Convert steps format.
+         *
+         * @param a JSONArray representation of steps.
+         * @return The steps as a space-separated string.
+         */
+        @JvmStatic
+        fun convertFromJSON(a: JSONArray): String {
+            val sb = StringBuilder()
+            for (s in a.stringIterable()) {
+                sb.append(s).append(" ")
             }
-        } catch (NumberFormatException | JSONException e) {
-            // Can't serialize float. Value likely too big/small.
-            Timber.w(e);
-            return null;
+            return sb.toString().trim { it <= ' ' }
         }
-        return stepsAr;
-    }
 
-
-    private boolean getAllowEmptyFromAttributes(AttributeSet attrs) {
-        if (attrs == null) {
-            return true;
+        /**
+         * Convert steps format. For better usability, rounded floats are converted to integers (e.g., 1.0 is converted to
+         * 1).
+         *
+         * @param steps String representation of steps.
+         * @return The steps as a JSONArray or null if the steps are not valid.
+         */
+        @JvmStatic
+        fun convertToJSON(steps: String): JSONArray? {
+            val stepsAr = JSONArray()
+            val stepsTrim = steps.trim { it <= ' ' }
+            if (TextUtils.isEmpty(steps)) {
+                return stepsAr
+            }
+            try {
+                for (s in stepsTrim.split("\\s+".toRegex()).toTypedArray()) {
+                    val d = s.toDouble()
+                    // 0 or less is not a valid step.
+                    if (d <= 0) {
+                        return null
+                    }
+                    // Use whole numbers if we can (but still allow decimals)
+                    val i = d.toInt()
+                    if (i.toDouble() == d) {
+                        stepsAr.put(i)
+                    } else {
+                        stepsAr.put(d)
+                    }
+                }
+            } catch (e: NumberFormatException) {
+                // Can't serialize float. Value likely too big/small.
+                Timber.w(e)
+                return null
+            } catch (e: JSONException) {
+                Timber.w(e)
+                return null
+            }
+            return stepsAr
         }
-        return attrs.getAttributeBooleanValue(AnkiDroidApp.XML_CUSTOM_NAMESPACE, "allowEmpty", true);
     }
 }
