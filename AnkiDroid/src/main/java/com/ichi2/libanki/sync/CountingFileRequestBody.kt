3f3728cee037d3e50df0b09c7b3528c86098886b
codingtosh <codingtosh@gmail.com>
2022-01-07 12:41:52 +0530

    [Kotlin Migration] CountingFileRequestBody
    Package: com.ichi2.libanki.sync

--- AnkiDroid/src/main/java/com/ichi2/libanki/sync/CountingFileRequestBody.kt
+++ AnkiDroid/src/main/java/com/ichi2/libanki/sync/CountingFileRequestBody.kt
@@ -14,60 +14,47 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-package com.ichi2.libanki.sync;
+package com.ichi2.libanki.sync
 
-import java.io.File;
-import java.io.IOException;
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+import okhttp3.internal.closeQuietly
+import okio.BufferedSink
+import okio.Source
+import okio.source
+import java.io.File
+import java.io.IOException
 
-import okhttp3.MediaType;
-import okhttp3.RequestBody;
-import okhttp3.internal.Util;
-import okio.BufferedSink;
-import okio.Okio;
-import okio.Source;
-
-
-public class CountingFileRequestBody extends RequestBody {
-
-    private static final int SEGMENT_SIZE = 8092; // okio.Segment.SIZE (internal, copy required)
-
-    private final File mFile;
-    private final ProgressListener mListener;
-    private final String mContentType;
-
-    public CountingFileRequestBody(File file, String contentType, ProgressListener listener) {
-        this.mFile = file;
-        this.mContentType = contentType;
-        this.mListener = listener;
+class CountingFileRequestBody(private val file: File, private val contentType: String, private val listener: ProgressListener) : RequestBody() {
+    override fun contentLength(): Long {
+        return file.length()
     }
 
-    @Override
-    public long contentLength() {
-        return mFile.length();
+    override fun contentType(): MediaType? {
+        return contentType.toMediaTypeOrNull()
     }
 
-    @Override
-    public MediaType contentType() {
-        return MediaType.parse(mContentType);
-    }
-
-    @Override
-    public void writeTo(BufferedSink sink) throws IOException {
-        Source source = null;
+    @Throws(IOException::class)
+    override fun writeTo(sink: BufferedSink) {
+        var source: Source? = null
         try {
-            source = Okio.source(mFile);
-            long read;
-
-            while ((read = source.read(sink.getBuffer(), SEGMENT_SIZE)) != -1) {
-                sink.flush();
-                this.mListener.transferred(read);
+            source = file.source()
+            var read: Long
+            while (source.read(sink.buffer, SEGMENT_SIZE.toLong()).also { read = it } != -1L) {
+                sink.flush()
+                listener.transferred(read)
             }
         } finally {
-            Util.closeQuietly(source);
+            source?.closeQuietly()
         }
     }
 
-    public interface ProgressListener {
-        void transferred(long num);
+    interface ProgressListener {
+        fun transferred(num: Long)
+    }
+
+    companion object {
+        private const val SEGMENT_SIZE = 8092 // okio.Segment.SIZE (internal, copy required)
     }
 }
