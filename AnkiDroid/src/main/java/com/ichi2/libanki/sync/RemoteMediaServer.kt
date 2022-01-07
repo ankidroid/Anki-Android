5ae93594e71f8d7879006e050c0b1e1d64b0f97f
codingtosh <codingtosh@gmail.com>
2022-01-07 20:56:29 +0530

    [Kotlin Migration] RemoteMediaServer
    Package: com.ichi2.libanki.sync

--- AnkiDroid/src/main/java/com/ichi2/libanki/sync/RemoteMediaServer.kt
+++ AnkiDroid/src/main/java/com/ichi2/libanki/sync/RemoteMediaServer.kt
@@ -15,185 +15,174 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-package com.ichi2.libanki.sync;
+package com.ichi2.libanki.sync
 
-import android.content.SharedPreferences;
-import android.net.Uri;
-import android.text.TextUtils;
+import android.net.Uri
+import android.text.TextUtils
+import com.ichi2.anki.AnkiDroidApp
+import com.ichi2.anki.exception.MediaSyncException
+import com.ichi2.anki.exception.UnknownHttpResponseException
+import com.ichi2.anki.web.CustomSyncServer.getMediaSyncUrl
+import com.ichi2.async.Connection
+import com.ichi2.libanki.Collection
+import com.ichi2.libanki.Utils
+import com.ichi2.utils.HashUtil.HashMapInit
+import com.ichi2.utils.JSONArray
+import com.ichi2.utils.JSONObject
+import com.ichi2.utils.VersionUtils.pkgVersionName
+import okhttp3.Response
+import timber.log.Timber
+import java.io.File
+import java.io.FileInputStream
+import java.io.IOException
+import java.util.*
+import java.util.zip.ZipFile
 
-import com.ichi2.anki.AnkiDroidApp;
-import com.ichi2.anki.exception.MediaSyncException;
-import com.ichi2.anki.exception.UnknownHttpResponseException;
-import com.ichi2.anki.web.CustomSyncServer;
-import com.ichi2.async.Connection;
-import com.ichi2.libanki.Collection;
-import com.ichi2.libanki.Utils;
-import com.ichi2.utils.HashUtil;
-import com.ichi2.utils.JSONArray;
-import com.ichi2.utils.JSONObject;
-import com.ichi2.utils.VersionUtils;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.zip.ZipFile;
-
-import okhttp3.Response;
-import timber.log.Timber;
-
-@SuppressWarnings({"PMD.AvoidThrowingRawExceptionTypes","PMD.MethodNamingConventions"})
-public class RemoteMediaServer extends HttpSyncer {
-
-    private final Collection mCol;
-
-
-    public RemoteMediaServer(Collection col, String hkey, Connection con, HostNum hostNum) {
-        super(hkey, con, hostNum);
-        mCol = col;
-    }
-
-
-    @Override
-    public String syncURL() {
+class RemoteMediaServer(private val col: Collection?, hkey: String?, con: Connection?, hostNum: HostNum?) : HttpSyncer(hkey, con, hostNum) {
+    override fun syncURL(): String {
         // Allow user to specify custom sync server
-        SharedPreferences userPreferences = AnkiDroidApp.getSharedPrefs(AnkiDroidApp.getInstance());
+        val userPreferences = AnkiDroidApp.getSharedPrefs(AnkiDroidApp.getInstance())
         if (isUsingCustomSyncServer(userPreferences)) {
-            String mediaSyncBase = CustomSyncServer.getMediaSyncUrl(userPreferences);
-            if (mediaSyncBase == null) {
-                return getDefaultAnkiWebUrl();
-            }
-            //Note: the preference did not necessarily contain /msync/, so we can't concat with the default as done in
+            val mediaSyncBase = getMediaSyncUrl(userPreferences) ?: return defaultAnkiWebUrl
+            // Note: the preference did not necessarily contain /msync/, so we can't concat with the default as done in
             // getDefaultAnkiWebUrl
-            return Uri.parse(mediaSyncBase).toString();
+            return Uri.parse(mediaSyncBase).toString()
         }
         // Usual case
-        return getDefaultAnkiWebUrl();
+        return defaultAnkiWebUrl
     }
 
-
-    public JSONObject begin() throws UnknownHttpResponseException, MediaSyncException {
-        try {
-            mPostVars = HashUtil.HashMapInit(2);
-            mPostVars.put("k", mHKey);
-            mPostVars.put("v",
-                    String.format(Locale.US, "ankidroid,%s,%s", VersionUtils.getPkgVersionName(), Utils.platDesc()));
-
-            Response resp = super.req("begin", HttpSyncer.getInputStream(Utils.jsonToString(new JSONObject())));
-            JSONObject jresp = new JSONObject(resp.body().string());
-            JSONObject ret = _dataOnly(jresp, JSONObject.class);
-            mSKey = ret.getString("sk");
-            return ret;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    @Throws(UnknownHttpResponseException::class, MediaSyncException::class)
+    fun begin(): JSONObject {
+        return try {
+            mPostVars = HashMapInit(2)
+            mPostVars["k"] = mHKey
+            mPostVars["v"] = String.format(Locale.US, "ankidroid,%s,%s", pkgVersionName, Utils.platDesc())
+            val resp = super.req("begin", getInputStream(Utils.jsonToString(JSONObject())))
+            val jresp = JSONObject(resp.body?.string())
+            val ret = _dataOnly(jresp, JSONObject::class.java)
+            mSKey = ret.getString("sk")
+            ret
+        } catch (e: IOException) {
+            throw RuntimeException(e)
         }
     }
-
 
     // args: lastUsn
-    public JSONArray mediaChanges(int lastUsn) throws UnknownHttpResponseException, MediaSyncException {
-        try {
-            mPostVars = HashUtil.HashMapInit(1);
-            mPostVars.put("sk", mSKey);
-
-            Response resp = super.req("mediaChanges",
-                    HttpSyncer.getInputStream(Utils.jsonToString(new JSONObject().put("lastUsn", lastUsn))));
-            JSONObject jresp = new JSONObject(resp.body().string());
-            return _dataOnly(jresp, JSONArray.class);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    @Throws(UnknownHttpResponseException::class, MediaSyncException::class)
+    fun mediaChanges(lastUsn: Int): JSONArray {
+        return try {
+            mPostVars = HashMapInit(1)
+            mPostVars["sk"] = mSKey
+            val resp = super.req(
+                "mediaChanges",
+                getInputStream(Utils.jsonToString(JSONObject().put("lastUsn", lastUsn)))
+            )
+            val jresp = JSONObject(resp.body?.string())
+            _dataOnly(jresp, JSONArray::class.java)
+        } catch (e: IOException) {
+            throw RuntimeException(e)
         }
     }
-
 
     /**
      * args: files
-     * <br>
+     * <br></br>
      * This method returns a ZipFile with the OPEN_DELETE flag, ensuring that the file on disk will
      * be automatically deleted when the stream is closed.
      */
-    public ZipFile downloadFiles(List<String> top) throws UnknownHttpResponseException {
-        Response resp = null;
-        try {
-            resp = super.req("downloadFiles",
-                    HttpSyncer.getInputStream(Utils.jsonToString(new JSONObject().put("files", new JSONArray(top)))));
-            String zipPath = mCol.getPath().replaceFirst("collection\\.anki2$", "tmpSyncFromServer.zip");
+    @Throws(UnknownHttpResponseException::class)
+    fun downloadFiles(top: List<String?>?): ZipFile {
+        var resp: Response? = null
+        return try {
+            resp = super.req(
+                "downloadFiles",
+                getInputStream(Utils.jsonToString(JSONObject().put("files", JSONArray(top))))
+            )
+            val zipPath = col?.path?.replaceFirst("collection\\.anki2$".toRegex(), "tmpSyncFromServer.zip")
             // retrieve contents and save to file on disk:
-            super.writeToFile(resp.body().byteStream(), zipPath);
-            return new ZipFile(new File(zipPath), ZipFile.OPEN_READ | ZipFile.OPEN_DELETE);
-        } catch (IOException | NullPointerException e) {
-            Timber.e(e, "Failed to download requested media files");
-            throw new RuntimeException(e);
+            super.writeToFile(resp.body?.byteStream(), zipPath)
+            ZipFile(File(zipPath!!), ZipFile.OPEN_READ or ZipFile.OPEN_DELETE)
+        } catch (e: IOException) {
+            Timber.e(e, "Failed to download requested media files")
+            throw RuntimeException(e)
+        } catch (e: NullPointerException) {
+            Timber.e(e, "Failed to download requested media files")
+            throw RuntimeException(e)
         } finally {
-            if (resp != null && resp.body() != null) {
-                resp.body().close();
-            }
+            resp?.body?.close()
         }
     }
 
-
-    public JSONArray uploadChanges(File zip) throws UnknownHttpResponseException, MediaSyncException {
-        try {
+    @Throws(UnknownHttpResponseException::class, MediaSyncException::class)
+    fun uploadChanges(zip: File?): JSONArray {
+        return try {
             // no compression, as we compress the zip file instead
-            Response resp = super.req("uploadChanges", new FileInputStream(zip), 0);
-            JSONObject jresp = new JSONObject(resp.body().string());
-            return _dataOnly(jresp, JSONArray.class);
-        } catch (IOException | NullPointerException e) {
-            throw new RuntimeException(e);
+            val resp = super.req("uploadChanges", FileInputStream(zip), 0)
+            val jresp = JSONObject(resp.body?.string())
+            _dataOnly(jresp, JSONArray::class.java)
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        } catch (e: NullPointerException) {
+            throw RuntimeException(e)
         }
     }
-
 
     // args: local
-    public String mediaSanity(int lcnt) throws UnknownHttpResponseException, MediaSyncException {
-        try {
-            Response resp = super.req("mediaSanity",
-                    HttpSyncer.getInputStream(Utils.jsonToString(new JSONObject().put("local", lcnt))));
-            JSONObject jresp = new JSONObject(resp.body().string());
-            return _dataOnly(jresp, String.class);
-        } catch (IOException | NullPointerException e) {
-            throw new RuntimeException(e);
+    @Throws(UnknownHttpResponseException::class, MediaSyncException::class)
+    fun mediaSanity(lcnt: Int): String {
+        return try {
+            val resp = super.req(
+                "mediaSanity",
+                getInputStream(Utils.jsonToString(JSONObject().put("local", lcnt)))
+            )
+            val jresp = JSONObject(resp.body?.string())
+            _dataOnly(jresp, String::class.java)
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        } catch (e: NullPointerException) {
+            throw RuntimeException(e)
         }
     }
-
 
     /**
      * Returns the "data" element from the JSON response from the server, or throws an exception if there is a value in
      * the "err" element.
-     * <p>
+     *
+     *
      * The python counterpart to this method is flexible with type coercion; the type of object returned is decided by
      * the content of the "data" element, and there are several such types in the various server responses. Java
      * requires us to specifically choose a type to convert to, so we need an additional parameter (returnType) to
      * specify the type we expect.
-     * 
+     *
      * @param resp The JSON response from the server
      * @param returnType The type to coerce the 'data' element to.
      * @return The "data" element from the HTTP response from the server. The type of object returned is determined by
-     *         returnType.
+     * returnType.
      */
-    @SuppressWarnings("unchecked")
-    private <T> T _dataOnly(JSONObject resp, Class<T> returnType) throws MediaSyncException {
+    @Throws(MediaSyncException::class)
+    private fun <T> _dataOnly(resp: JSONObject, returnType: Class<T>): T {
         if (!TextUtils.isEmpty(resp.optString("err"))) {
-            String err = resp.getString("err");
-            mCol.log("error returned: " + err);
-            throw new MediaSyncException("SyncError:" + err);
+            val err = resp.getString("err")
+            col?.log("error returned: $err")
+            throw MediaSyncException("SyncError:$err")
         }
-        if (returnType == String.class) {
-            return (T) resp.getString("data");
-        } else if (returnType == JSONObject.class) {
-            return (T) resp.getJSONObject("data");
-        } else if (returnType == JSONArray.class) {
-            return (T) resp.getJSONArray("data");
+        if (returnType == String::class.java) {
+            @Suppress("UNCHECKED_CAST")
+            return resp.getString("data") as T
+        } else if (returnType == JSONObject::class.java) {
+            @Suppress("UNCHECKED_CAST")
+            return resp.getJSONObject("data") as T
+        } else if (returnType == JSONArray::class.java) {
+            @Suppress("UNCHECKED_CAST")
+            return resp.getJSONArray("data") as T
         }
-        throw new RuntimeException("Did not specify a valid type for the 'data' element in resopnse");
+        throw RuntimeException("Did not specify a valid type for the 'data' element in resopnse")
     }
 
     // Difference from libAnki: we allow a custom URL to specify a different prefix, so this is only used with the
     // default URL
-    @Override
-    protected String getUrlPrefix() {
-        return "msync";
+    override fun getUrlPrefix(): String {
+        return "msync"
     }
 }
