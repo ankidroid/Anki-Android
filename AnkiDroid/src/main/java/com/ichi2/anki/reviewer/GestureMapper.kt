2a951fd9676e1746e147ac1890087ea31770018a
Akshay <52353967+Akshay0701@users.noreply.github.com>
2022-01-08 19:46:29 +0530

    [Kotlin Migration] GestureMapper

--- AnkiDroid/src/main/java/com/ichi2/anki/reviewer/GestureMapper.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/reviewer/GestureMapper.kt
@@ -13,185 +13,145 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.anki.reviewer
 
-package com.ichi2.anki.reviewer;
+import android.content.SharedPreferences
+import android.view.ViewConfiguration
+import com.ichi2.anki.AnkiDroidApp
+import com.ichi2.anki.cardviewer.Gesture
+import com.ichi2.anki.cardviewer.TapGestureMode
+import com.ichi2.anki.cardviewer.TapGestureMode.Companion.fromPreference
+import com.ichi2.utils.KotlinCleanup
+import timber.log.Timber
+import kotlin.math.abs
+import kotlin.math.floor
 
-import android.content.SharedPreferences;
-import android.view.ViewConfiguration;
-
-import com.ichi2.anki.AnkiDroidApp;
-import com.ichi2.anki.cardviewer.Gesture;
-import com.ichi2.anki.cardviewer.TapGestureMode;
-
-import androidx.annotation.NonNull;
-import timber.log.Timber;
-
-public class GestureMapper {
-
-    @NonNull
-    private TapGestureMode mTapGestureMode = TapGestureMode.NINE_POINT;
-    private int mSwipeMinDistance = -1;
-    private int mSwipeThresholdVelocity = -1;
-
-    private static ViewConfiguration VIEW_CONFIGURATION = null;
-    private static int DEFAULT_SWIPE_MIN_DISTANCE;
-    private static int DEFAULT_SWIPE_THRESHOLD_VELOCITY;
-
-    public void init(SharedPreferences preferences) {
-        int sensitivity = preferences.getInt("swipeSensitivity", 100);
-        mTapGestureMode =  TapGestureMode.fromPreference(preferences);
+class GestureMapper {
+    var tapGestureMode = TapGestureMode.NINE_POINT
+        private set
+    private var mSwipeMinDistance = -1
+    private var mSwipeThresholdVelocity = -1
+    fun init(preferences: SharedPreferences) {
+        val sensitivity = preferences.getInt("swipeSensitivity", 100)
+        tapGestureMode = fromPreference(preferences)
 
         // ViewConfiguration can be used statically but it must be initialized during Android application lifecycle
         // Else, when Robolectric executes in the CI it accesses AnkiDroidApp.getInstance before it exists #9173
         if (VIEW_CONFIGURATION == null) {
             // Set good default values for swipe detection
-            VIEW_CONFIGURATION = ViewConfiguration.get(AnkiDroidApp.getInstance());
-            DEFAULT_SWIPE_MIN_DISTANCE = VIEW_CONFIGURATION.getScaledPagingTouchSlop();
-            DEFAULT_SWIPE_THRESHOLD_VELOCITY = VIEW_CONFIGURATION.getScaledMinimumFlingVelocity();
+            VIEW_CONFIGURATION = ViewConfiguration.get(AnkiDroidApp.getInstance())
+            DEFAULT_SWIPE_MIN_DISTANCE = VIEW_CONFIGURATION!!.scaledPagingTouchSlop
+            DEFAULT_SWIPE_THRESHOLD_VELOCITY = VIEW_CONFIGURATION!!.scaledMinimumFlingVelocity
         }
-
         if (sensitivity != 100) {
-            float sens = 100.0f/sensitivity;
-            mSwipeMinDistance = (int) (DEFAULT_SWIPE_MIN_DISTANCE * sens + 0.5f);
-            mSwipeThresholdVelocity = (int) (DEFAULT_SWIPE_THRESHOLD_VELOCITY * sens  + 0.5f);
+            val sens = 100.0f / sensitivity
+            mSwipeMinDistance = (DEFAULT_SWIPE_MIN_DISTANCE * sens + 0.5f).toInt()
+            mSwipeThresholdVelocity = (DEFAULT_SWIPE_THRESHOLD_VELOCITY * sens + 0.5f).toInt()
         } else {
-            mSwipeMinDistance = DEFAULT_SWIPE_MIN_DISTANCE;
-            mSwipeThresholdVelocity = DEFAULT_SWIPE_THRESHOLD_VELOCITY;
+            mSwipeMinDistance = DEFAULT_SWIPE_MIN_DISTANCE
+            mSwipeThresholdVelocity = DEFAULT_SWIPE_THRESHOLD_VELOCITY
         }
     }
 
-    public Gesture gesture(float dx, float dy, float velocityX, float velocityY,
-                           boolean isSelecting, boolean isXScrolling, boolean isYScrolling) {
+    fun gesture(
+        dx: Float,
+        dy: Float,
+        velocityX: Float,
+        velocityY: Float,
+        isSelecting: Boolean,
+        isXScrolling: Boolean,
+        isYScrolling: Boolean
+    ): Gesture? {
         try {
-            if (Math.abs(dx) > Math.abs(dy)) {
+            if (abs(dx) > abs(dy)) {
                 // horizontal swipe if moved further in x direction than y direction
-                if (dx > mSwipeMinDistance
-                        && Math.abs(velocityX) > mSwipeThresholdVelocity
-                        && !isXScrolling && !isSelecting) {
-                    return Gesture.SWIPE_RIGHT;
-                } else if (dx < -mSwipeMinDistance
-                        && Math.abs(velocityX) > mSwipeThresholdVelocity
-                        && !isXScrolling && !isSelecting) {
-                    return Gesture.SWIPE_LEFT;
+                if (dx > mSwipeMinDistance && abs(velocityX) > mSwipeThresholdVelocity && !isXScrolling && !isSelecting) {
+                    return Gesture.SWIPE_RIGHT
+                } else if (dx < -mSwipeMinDistance && abs(velocityX) > mSwipeThresholdVelocity && !isXScrolling && !isSelecting) {
+                    return Gesture.SWIPE_LEFT
                 }
             } else {
                 // otherwise vertical swipe
-                if (dy > mSwipeMinDistance
-                        && Math.abs(velocityY) > mSwipeThresholdVelocity
-                        && !isYScrolling) {
-                    return Gesture.SWIPE_DOWN;
-                } else if (dy < -mSwipeMinDistance
-                        && Math.abs(velocityY) > mSwipeThresholdVelocity
-                        && !isYScrolling) {
-                    return Gesture.SWIPE_UP;
+                if (dy > mSwipeMinDistance && abs(velocityY) > mSwipeThresholdVelocity && !isYScrolling) {
+                    return Gesture.SWIPE_DOWN
+                } else if (dy < -mSwipeMinDistance && abs(velocityY) > mSwipeThresholdVelocity && !isYScrolling) {
+                    return Gesture.SWIPE_UP
                 }
             }
-        } catch (Exception e) {
-            Timber.e(e, "onFling Exception");
+        } catch (e: Exception) {
+            Timber.e(e, "onFling Exception")
         }
-
-        return null;
+        return null
     }
 
-
-    public Gesture gesture(int height, int width, float posX, float posY) {
-        if (width == 0 || height == 0) {
-            return null;
-        }
-
-        switch (mTapGestureMode) {
-            case FOUR_POINT: return fromTap(height, width, posX, posY);
-            case NINE_POINT: return fromTapCorners(height, width, posX, posY);
-            default: return null;
+    fun gesture(height: Int, width: Int, posX: Float, posY: Float): Gesture? {
+        return if (width == 0 || height == 0) {
+            null
+        } else when (tapGestureMode) {
+            TapGestureMode.FOUR_POINT -> fromTap(height, width, posX, posY)
+            TapGestureMode.NINE_POINT -> fromTapCorners(height, width, posX, posY)
         }
     }
 
-    private static Gesture fromTap(int height, int width, float posX, float posY) {
-        boolean gestureIsRight = posY > height * (1 - posX / width);
-        if (posX > posY / height * width) {
-            if (gestureIsRight) {
-                return Gesture.TAP_RIGHT;
+    private enum class TriState {
+        LOW, MID, HIGH
+    }
+
+    companion object {
+        private var VIEW_CONFIGURATION: ViewConfiguration? = null
+        private var DEFAULT_SWIPE_MIN_DISTANCE = 0
+        private var DEFAULT_SWIPE_THRESHOLD_VELOCITY = 0
+        private fun fromTap(height: Int, width: Int, posX: Float, posY: Float): Gesture {
+            val gestureIsRight = posY > height * (1 - posX / width)
+            return if (posX > posY / height * width) {
+                if (gestureIsRight) {
+                    Gesture.TAP_RIGHT
+                } else {
+                    Gesture.TAP_TOP
+                }
             } else {
-                return Gesture.TAP_TOP;
-            }
-        } else {
-            if (gestureIsRight) {
-                return Gesture.TAP_BOTTOM;
-            } else {
-                return Gesture.TAP_LEFT;
+                if (gestureIsRight) {
+                    Gesture.TAP_BOTTOM
+                } else {
+                    Gesture.TAP_LEFT
+                }
             }
         }
-    }
 
-    @NonNull
-    public TapGestureMode getTapGestureMode() {
-        return mTapGestureMode;
-    }
-
-    private static Gesture fromTapCorners(int height, int width, float posX, float posY) {
-
-        double heightSegment = height / 3d;
-        double widthSegment = width / 3d;
-
-        TriState wSector = clamp(posX / widthSegment);
-        TriState hSector = clamp(posY / heightSegment);
-
-        switch (wSector) {
-            case LOW:
-                //left
-                switch (hSector) {
-                    case LOW:
-                        return Gesture.TAP_TOP_LEFT;
-                    case MID:
-                        return Gesture.TAP_LEFT;
-                    case HIGH:
-                        return Gesture.TAP_BOTTOM_LEFT;
+        private fun fromTapCorners(height: Int, width: Int, posX: Float, posY: Float): Gesture {
+            val heightSegment = height / 3.0
+            val widthSegment = width / 3.0
+            val wSector = clamp(posX / widthSegment)
+            val hSector = clamp(posY / heightSegment)
+            return when (wSector) {
+                TriState.LOW -> when (hSector) {
+                    TriState.LOW -> Gesture.TAP_TOP_LEFT
+                    TriState.MID -> Gesture.TAP_LEFT
+                    TriState.HIGH -> Gesture.TAP_BOTTOM_LEFT
                 }
-                break;
-            case MID:
-                //center
-                switch (hSector) {
-                    case LOW:
-                        return Gesture.TAP_TOP;
-                    case MID:
-                        return Gesture.TAP_CENTER;
-                    case HIGH:
-                        return Gesture.TAP_BOTTOM;
+                TriState.MID -> when (hSector) {
+                    TriState.LOW -> Gesture.TAP_TOP
+                    TriState.MID -> Gesture.TAP_CENTER
+                    TriState.HIGH -> Gesture.TAP_BOTTOM
                 }
-                break;
-            case HIGH:
-                //Right
-                switch (hSector) {
-                    case LOW:
-                        return Gesture.TAP_TOP_RIGHT;
-                    case MID:
-                        return Gesture.TAP_RIGHT;
-                    case HIGH:
-                        return Gesture.TAP_BOTTOM_RIGHT;
+                TriState.HIGH -> when (hSector) {
+                    TriState.LOW -> Gesture.TAP_TOP_RIGHT
+                    TriState.MID -> Gesture.TAP_RIGHT
+                    TriState.HIGH -> Gesture.TAP_BOTTOM_RIGHT
                 }
-                break;
-            default:
-                throw new IllegalArgumentException("illegal switch state");
-        }
-        throw new IllegalArgumentException("illegal switch state");
-    }
-
-    // clamps the value from LOW-MID-HIGH
-    private static TriState clamp(double value) {
-        double val = Math.floor(value);
-        if (val >= 2) {
-            return TriState.HIGH;
-        }
-        if (val < 1) {
-            return TriState.LOW;
+            }
         }
 
-        return TriState.MID;
-    }
-
-    private enum TriState {
-        LOW,
-        MID,
-        HIGH
+        @KotlinCleanup("val")
+        // clamps the value from LOW-MID-HIGH
+        private fun clamp(value: Double): TriState {
+            val `val` = floor(value)
+            if (`val` >= 2) {
+                return TriState.HIGH
+            }
+            return if (`val` < 1) {
+                TriState.LOW
+            } else TriState.MID
+        }
     }
 }
