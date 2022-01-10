c3a8429ecee5d72f3ddacc6dee1d156f5d93c211
Akshay <52353967+Akshay0701@users.noreply.github.com>
2022-01-10 22:59:34 +0530

    [Kotlin Migration] JavaScriptTTS

--- AnkiDroid/src/main/java/com/ichi2/anki/JavaScriptTTS.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/JavaScriptTTS.kt
@@ -14,55 +14,31 @@
  * this program. If not, see <http://www.gnu.org/licenses/>.                            *
  ****************************************************************************************/
 
-package com.ichi2.anki;
+package com.ichi2.anki
 
-import android.content.Context;
-import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
-
-import androidx.annotation.IntDef;
-import androidx.annotation.NonNull;
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.speech.tts.TextToSpeech.OnInitListener
+import androidx.annotation.IntDef
 
 /**
  * Since it is assumed that only advanced users will use the JavaScript api,
  * here, Android's TextToSpeech is converted for JavaScript almost as it is, giving priority to free behavior.
  * https://developer.android.com/reference/android/speech/tts/TextToSpeech
  */
-public class JavaScriptTTS implements TextToSpeech.OnInitListener {
+class JavaScriptTTS internal constructor() : OnInitListener {
+    @IntDef(TTS_SUCCESS, TTS_ERROR)
+    annotation class ErrorOrSuccess
 
-    private static final int TTS_SUCCESS = TextToSpeech.SUCCESS;
-    private static final int TTS_ERROR = TextToSpeech.ERROR;
-    private static final int TTS_QUEUE_ADD = TextToSpeech.QUEUE_ADD;
-    private static final int TTS_QUEUE_FLUSH = TextToSpeech.QUEUE_FLUSH;
-    private static final int TTS_LANG_AVAILABLE = TextToSpeech.LANG_AVAILABLE;
-    private static final int TTS_LANG_COUNTRY_AVAILABLE = TextToSpeech.LANG_COUNTRY_AVAILABLE;
-    private static final int TTS_LANG_COUNTRY_VAR_AVAILABLE = TextToSpeech.LANG_COUNTRY_VAR_AVAILABLE;
-    private static final int TTS_LANG_MISSING_DATA = TextToSpeech.LANG_MISSING_DATA;
-    private static final int TTS_LANG_NOT_SUPPORTED = TextToSpeech.LANG_NOT_SUPPORTED;
+    @IntDef(TTS_QUEUE_ADD, TTS_QUEUE_FLUSH)
+    annotation class QueueMode
 
-    @IntDef({TTS_SUCCESS, TTS_ERROR})
-    public @interface ErrorOrSuccess {}
+    @IntDef(TTS_LANG_AVAILABLE, TTS_LANG_COUNTRY_AVAILABLE, TTS_LANG_COUNTRY_VAR_AVAILABLE, TTS_LANG_MISSING_DATA, TTS_LANG_NOT_SUPPORTED)
+    annotation class TTSLangResult
 
-    @IntDef({TTS_QUEUE_ADD, TTS_QUEUE_FLUSH})
-    public @interface QueueMode {}
-
-    @IntDef({TTS_LANG_AVAILABLE, TTS_LANG_COUNTRY_AVAILABLE, TTS_LANG_COUNTRY_VAR_AVAILABLE, TTS_LANG_MISSING_DATA, TTS_LANG_NOT_SUPPORTED})
-    public @interface TTSLangResult {}
-
-    @NonNull
-    private static TextToSpeech mTts;
-    private static boolean mTtsOk;
-    private static final Bundle mTtsParams = new Bundle();
-
-    JavaScriptTTS() {
-        Context context = AnkiDroidApp.getInstance().getApplicationContext();
-        mTts = new TextToSpeech(context, this);
-    }
-
-    /** OnInitListener method to receive the TTS engine status */
-    @Override
-    public void onInit(@ErrorOrSuccess int status) {
-        mTtsOk = status == TextToSpeech.SUCCESS;
+    /** OnInitListener method to receive the TTS engine status  */
+    override fun onInit(@ErrorOrSuccess status: Int) {
+        mTtsOk = status == TextToSpeech.SUCCESS
     }
 
     /**
@@ -72,8 +48,8 @@ public class JavaScriptTTS implements TextToSpeech.OnInitListener {
      * @return ERROR(-1) SUCCESS(0)
      */
     @ErrorOrSuccess
-    public int speak(String text, @QueueMode int queueMode) {
-        return mTts.speak(text, queueMode, mTtsParams, "stringId");
+    fun speak(text: String?, @QueueMode queueMode: Int): Int {
+        return mTts.speak(text, queueMode, mTtsParams, "stringId")
     }
 
     /**
@@ -82,27 +58,26 @@ public class JavaScriptTTS implements TextToSpeech.OnInitListener {
      * @return ERROR(-1) SUCCESS(0)
      */
     @ErrorOrSuccess
-    public int speak(String text) {
-        return mTts.speak(text, TextToSpeech.QUEUE_FLUSH, mTtsParams, "stringId");
+    fun speak(text: String?): Int {
+        return mTts.speak(text, TextToSpeech.QUEUE_FLUSH, mTtsParams, "stringId")
     }
 
     /**
      * Sets the text-to-speech language.
      * The TTS engine will try to use the closest match to the specified language as represented by the Locale, but there is no guarantee that the exact same Locale will be used.
      * @param loc Specifying the language to speak
-     * @return  0 Denotes the language is available for the language by the locale, but not the country and variant.
+     * @return 0 Denotes the language is available for the language by the locale, but not the country and variant.
      *     <li> 1 Denotes the language is available for the language and country specified by the locale, but not the variant.
      *     <li> 2 Denotes the language is available exactly as specified by the locale.
      *     <li> -1 Denotes the language data is missing.
      *     <li> -2 Denotes the language is not supported.
      */
     @TTSLangResult
-    public int setLanguage(String loc) {
+    fun setLanguage(loc: String?): Int {
         // The Int values will be returned
         // Code indicating the support status for the locale. See LANG_AVAILABLE, LANG_COUNTRY_AVAILABLE, LANG_COUNTRY_VAR_AVAILABLE, LANG_MISSING_DATA and LANG_NOT_SUPPORTED.
-        return mTts.setLanguage(LanguageUtils.localeFromStringIgnoringScriptAndExtensions(loc));
+        return mTts.setLanguage(LanguageUtils.localeFromStringIgnoringScriptAndExtensions(loc))
     }
-
 
     /**
      * Sets the speech pitch for the TextToSpeech engine. This has no effect on any pre-recorded speech.
@@ -110,10 +85,10 @@ public class JavaScriptTTS implements TextToSpeech.OnInitListener {
      * @return ERROR(-1) SUCCESS(0)
      */
     @ErrorOrSuccess
-    public int setPitch(float pitch) {
+    fun setPitch(pitch: Float): Int {
         // The following Int values will be returned
         // ERROR(-1) SUCCESS(0)
-        return mTts.setPitch(pitch);
+        return mTts.setPitch(pitch)
     }
 
     /**
@@ -122,29 +97,47 @@ public class JavaScriptTTS implements TextToSpeech.OnInitListener {
      * @return ERROR(-1) SUCCESS(0)
      */
     @ErrorOrSuccess
-    public int setSpeechRate(float speechRate) {
+    fun setSpeechRate(speechRate: Float): Int {
         // The following Int values will be returned
         // ERROR(-1) SUCCESS(0)
-        return mTts.setSpeechRate(speechRate);
+        return mTts.setSpeechRate(speechRate)
     }
 
-    /** 
-     * Checks whether the TTS engine is busy speaking. 
-     * Note that a speech item is considered complete once it's audio data has 
+    /**
+     * Checks whether the TTS engine is busy speaking.
+     * Note that a speech item is considered complete once it's audio data has
      * been sent to the audio mixer, or written to a file.
-     *  
+     *
      */
-    public boolean isSpeaking() {
-        return mTts.isSpeaking();
-    }
+    val isSpeaking: Boolean
+        get() = mTts.isSpeaking
 
     /**
      * Interrupts the current utterance (whether played or rendered to file) and discards other utterances in the queue.
      * @return ERROR(-1) SUCCESS(0)
      */
     @ErrorOrSuccess
-    public int stop() {
-        return mTts.stop();
+    fun stop(): Int {
+        return mTts.stop()
     }
 
+    companion object {
+        private const val TTS_SUCCESS = TextToSpeech.SUCCESS
+        private const val TTS_ERROR = TextToSpeech.ERROR
+        private const val TTS_QUEUE_ADD = TextToSpeech.QUEUE_ADD
+        private const val TTS_QUEUE_FLUSH = TextToSpeech.QUEUE_FLUSH
+        private const val TTS_LANG_AVAILABLE = TextToSpeech.LANG_AVAILABLE
+        private const val TTS_LANG_COUNTRY_AVAILABLE = TextToSpeech.LANG_COUNTRY_AVAILABLE
+        private const val TTS_LANG_COUNTRY_VAR_AVAILABLE = TextToSpeech.LANG_COUNTRY_VAR_AVAILABLE
+        private const val TTS_LANG_MISSING_DATA = TextToSpeech.LANG_MISSING_DATA
+        private const val TTS_LANG_NOT_SUPPORTED = TextToSpeech.LANG_NOT_SUPPORTED
+        private lateinit var mTts: TextToSpeech
+        private var mTtsOk = false
+        private val mTtsParams = Bundle()
+    }
+
+    init {
+        val context = AnkiDroidApp.getInstance().applicationContext
+        mTts = TextToSpeech(context, this)
+    }
 }
