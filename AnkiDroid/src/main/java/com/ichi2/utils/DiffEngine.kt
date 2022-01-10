15cee1ceeecfa8a71542992e16a334c6edadeddb
Akshay <52353967+Akshay0701@users.noreply.github.com>
2022-01-10 23:15:37 +0530

    [Kotlin Migration] DiffEngine

--- AnkiDroid/src/main/java/com/ichi2/utils/DiffEngine.kt
+++ AnkiDroid/src/main/java/com/ichi2/utils/DiffEngine.kt
@@ -14,23 +14,18 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-package com.ichi2.utils;
+package com.ichi2.utils
 
-import android.text.TextUtils;
-
-import org.bitbucket.cowwoc.diffmatchpatch.DiffMatchPatch;
-
-import androidx.annotation.CheckResult;
-import androidx.annotation.NonNull;
+import android.text.TextUtils
+import androidx.annotation.CheckResult
+import org.bitbucket.cowwoc.diffmatchpatch.DiffMatchPatch
 
 /**
  * Functions for diff, match and patch. Computes the difference between two texts to create a patch. Applies the patch
  * onto another text, allowing for errors.
  */
-public class DiffEngine {
-
-    private final DiffMatchPatch mDiffMatchPatch = new DiffMatchPatch();
-
+open class DiffEngine {
+    private val mDiffMatchPatch = DiffMatchPatch()
 
     /**
      * Return two strings to display as typed and correct text.
@@ -39,48 +34,45 @@ public class DiffEngine {
      * @param correct (cleaned-up) correct text
      * @return Two-element String array with HTML representation of the diffs between the inputs.
      */
-    public String[] diffedHtmlStrings(String typed, String correct) {
-        StringBuilder prettyTyped = new StringBuilder();
-        StringBuilder prettyCorrect = new StringBuilder();
-        for (DiffMatchPatch.Diff aDiff : mDiffMatchPatch.diffMain(typed, correct)) {
-            switch (aDiff.operation) {
-                case INSERT:
-                    prettyTyped.append(wrapBad(aDiff.text));
-                    break;
-                case DELETE:
-                    prettyCorrect.append(wrapMissing(aDiff.text));
-                    break;
-                case EQUAL:
-                    prettyTyped.append(wrapGood(aDiff.text));
-                    prettyCorrect.append(wrapGood(aDiff.text));
-                    break;
+    fun diffedHtmlStrings(typed: String?, correct: String?): Array<String> {
+        val prettyTyped = StringBuilder()
+        val prettyCorrect = StringBuilder()
+        for (aDiff in mDiffMatchPatch.diffMain(typed, correct)) {
+            when (aDiff.operation!!) {
+                DiffMatchPatch.Operation.INSERT -> prettyTyped.append(wrapBad(aDiff.text))
+                DiffMatchPatch.Operation.DELETE -> prettyCorrect.append(wrapMissing(aDiff.text))
+                DiffMatchPatch.Operation.EQUAL -> {
+                    prettyTyped.append(wrapGood(aDiff.text))
+                    prettyCorrect.append(wrapGood(aDiff.text))
+                }
             }
         }
-        return new String[] {prettyTyped.toString(), prettyCorrect.toString()};
+        return arrayOf(prettyTyped.toString(), prettyCorrect.toString())
     }
 
+    @KotlinCleanup("in")
+    companion object {
+        private fun wrapBad(`in`: String): String {
+            // We do the comparison with “<”s &c. in the strings, but should of course not just put those in the HTML
+            // output. Also, it looks like the Android WebView swallows single “\”s, so replace those with the entity by
+            // hand.
+            return "<span class=\"typeBad\">" + escapeHtml(`in`) + "</span>"
+        }
 
-    private static String wrapBad(String in) {
-        // We do the comparison with “<”s &c. in the strings, but should of course not just put those in the HTML
-        // output. Also, it looks like the Android WebView swallows single “\”s, so replace those with the entity by
-        // hand.
-        return "<span class=\"typeBad\">" + escapeHtml(in) + "</span>";
-    }
+        @CheckResult
+        fun wrapGood(`in`: String?): String {
+            return "<span class=\"typeGood\">" + escapeHtml(`in`) + "</span>"
+        }
 
-    @CheckResult
-    public static String wrapGood(String in) {
-        return "<span class=\"typeGood\">" + escapeHtml(in) + "</span>";
-    }
+        @JvmStatic
+        @CheckResult
+        fun wrapMissing(`in`: String?): String {
+            return "<span class=\"typeMissed\">" + escapeHtml(`in`) + "</span>"
+        }
 
-    @CheckResult
-    public static String wrapMissing(String in) {
-        return "<span class=\"typeMissed\">" + escapeHtml(in) + "</span>";
-    }
-
-
-    /** Escapes dangerous HTML tags (for XSS-like issues/rendering problems) */
-    @NonNull
-    protected static String escapeHtml(String in) {
-        return TextUtils.htmlEncode(in).replace("\\", "&#x5c;");
+        /** Escapes dangerous HTML tags (for XSS-like issues/rendering problems)  */
+        protected fun escapeHtml(`in`: String?): String {
+            return TextUtils.htmlEncode(`in`).replace("\\", "&#x5c;")
+        }
     }
 }
