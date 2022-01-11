1a4427fecd46a3c936fe68201a43985788c0d5a8
Arthur Milchior <arthur@milchior.fr>
2022-01-11 18:22:49 +0100

    NF: SlideIn is an enum
    This seems quite useless as the slide method is not used right now.

--- AnkiDroid/src/main/java/com/ichi2/anim/ViewAnimation.kt
+++ AnkiDroid/src/main/java/com/ichi2/anim/ViewAnimation.kt
@@ -9,60 +9,61 @@ import android.view.animation.DecelerateInterpolator
 import android.view.animation.TranslateAnimation
 
 object ViewAnimation {
-    const val SLIDE_IN_FROM_RIGHT = 0
-    const val SLIDE_OUT_TO_RIGHT = 1
-    const val SLIDE_IN_FROM_LEFT = 2
-    const val SLIDE_OUT_TO_LEFT = 3
-    const val SLIDE_IN_FROM_BOTTOM = 4
-    const val SLIDE_IN_FROM_TOP = 5
+    enum class SlideIn {
+        SLIDE_IN_FROM_RIGHT,
+        SLIDE_OUT_TO_RIGHT,
+        SLIDE_IN_FROM_LEFT,
+        SLIDE_OUT_TO_LEFT,
+        SLIDE_IN_FROM_BOTTOM,
+        SLIDE_IN_FROM_TOP,
+    }
     const val FADE_IN = 0
     const val FADE_OUT = 1
-    fun slide(type: Int, duration: Int, offset: Int): Animation? {
+    fun slide(type: SlideIn, duration: Int, offset: Int): Animation? {
         val animation: Animation?
         when (type) {
-            SLIDE_IN_FROM_RIGHT -> {
+            SlideIn.SLIDE_IN_FROM_RIGHT -> {
                 animation = TranslateAnimation(
                     Animation.RELATIVE_TO_SELF, +1.0f, Animation.RELATIVE_TO_SELF, 0.0f,
                     Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f
                 )
                 animation.setInterpolator(DecelerateInterpolator())
             }
-            SLIDE_OUT_TO_RIGHT -> {
+            SlideIn.SLIDE_OUT_TO_RIGHT -> {
                 animation = TranslateAnimation(
                     Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, +1.0f,
                     Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f
                 )
                 animation.setInterpolator(AccelerateInterpolator())
             }
-            SLIDE_IN_FROM_LEFT -> {
+            SlideIn.SLIDE_IN_FROM_LEFT -> {
                 animation = TranslateAnimation(
                     Animation.RELATIVE_TO_SELF, -1.0f, Animation.RELATIVE_TO_SELF, 0.0f,
                     Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f
                 )
                 animation.setInterpolator(DecelerateInterpolator())
             }
-            SLIDE_OUT_TO_LEFT -> {
+            SlideIn.SLIDE_OUT_TO_LEFT -> {
                 animation = TranslateAnimation(
                     Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, -1.0f,
                     Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f
                 )
                 animation.setInterpolator(AccelerateInterpolator())
             }
-            SLIDE_IN_FROM_BOTTOM -> {
+            SlideIn.SLIDE_IN_FROM_BOTTOM -> {
                 animation = TranslateAnimation(
                     Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f,
                     Animation.RELATIVE_TO_SELF, +1.0f, Animation.RELATIVE_TO_SELF, 0.0f
                 )
                 animation.setInterpolator(DecelerateInterpolator())
             }
-            SLIDE_IN_FROM_TOP -> {
+            SlideIn.SLIDE_IN_FROM_TOP -> {
                 animation = TranslateAnimation(
                     Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f,
                     Animation.RELATIVE_TO_SELF, -1.0f, Animation.RELATIVE_TO_SELF, 0.0f
                 )
                 animation.setInterpolator(DecelerateInterpolator())
             }
-            else -> animation = null
         }
         animation!!.duration = duration.toLong()
         animation.startOffset = offset.toLong()
