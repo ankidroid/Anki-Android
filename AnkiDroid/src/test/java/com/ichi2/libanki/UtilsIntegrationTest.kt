34ee367b0378ecc3de5ae35c27ad2223d791a556
Akshay <52353967+Akshay0701@users.noreply.github.com>
2022-01-15 20:32:07 +0530

    [Kotlin Migration] UtilsIntegrationTest

--- AnkiDroid/src/test/java/com/ichi2/libanki/UtilsIntegrationTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/libanki/UtilsIntegrationTest.kt
@@ -13,81 +13,62 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.libanki
 
-package com.ichi2.libanki;
+import androidx.annotation.CheckResult
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.anki.RobolectricTest
+import com.ichi2.testutils.NullApplication
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
-import com.ichi2.anki.RobolectricTest;
-import com.ichi2.testutils.NullApplication;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.annotation.Config;
-
-import androidx.annotation.CheckResult;
-import androidx.annotation.NonNull;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
-@RunWith(AndroidJUnit4.class)
-@Config(application = NullApplication.class, qualifiers = "en")
-public class UtilsIntegrationTest extends RobolectricTest {
-
+@RunWith(AndroidJUnit4::class)
+@Config(application = NullApplication::class, qualifiers = "en")
+class UtilsIntegrationTest : RobolectricTest() {
     @Test
-    public void deckPickerTimeOneAndHalfHours() {
-        int oneAndAHalfHours = 60 * 90;
-
-        String actual = deckPickerTime(oneAndAHalfHours);
-
-        assertThat(actual, is("1 h 30 m"));
+    fun deckPickerTimeOneAndHalfHours() {
+        val oneAndAHalfHours = 60 * 90
+        val actual = deckPickerTime(oneAndAHalfHours.toLong())
+        MatcherAssert.assertThat(actual, Matchers.`is`("1 h 30 m"))
     }
 
     @Test
-    public void deckPickerTimeOneHour() {
-        int oneAndAHalfHours = 60 * 60;
-
-        String actual = deckPickerTime(oneAndAHalfHours);
-
-        assertThat(actual, is("1 h 0 m"));
+    fun deckPickerTimeOneHour() {
+        val oneAndAHalfHours = 60 * 60
+        val actual = deckPickerTime(oneAndAHalfHours.toLong())
+        MatcherAssert.assertThat(actual, Matchers.`is`("1 h 0 m"))
     }
 
     @Test
-    public void deckPickerTime60Seconds() {
-        int oneAndAHalfHours = 60;
-
-        String actual = deckPickerTime(oneAndAHalfHours);
-
-        assertThat(actual, is("1 min"));
+    fun deckPickerTime60Seconds() {
+        val oneAndAHalfHours = 60
+        val actual = deckPickerTime(oneAndAHalfHours.toLong())
+        MatcherAssert.assertThat(actual, Matchers.`is`("1 min"))
     }
 
     @Test
-    public void deckPickerTimeOneAndAHalfDays() {
-        int oneAndAHalfHours = 60 * 60 * 36;
-
-        String actual = deckPickerTime(oneAndAHalfHours);
-
-        assertThat(actual, is("1 d 12 h"));
+    fun deckPickerTimeOneAndAHalfDays() {
+        val oneAndAHalfHours = 60 * 60 * 36
+        val actual = deckPickerTime(oneAndAHalfHours.toLong())
+        MatcherAssert.assertThat(actual, Matchers.`is`("1 d 12 h"))
     }
-
 
     @Test
     @Config(qualifiers = "en")
-    public void timeQuantityMonths() {
+    fun timeQuantityMonths() {
         // Anki Desktop 2.1.30: '\u206810.8\u2069 months'
-        assertThat(timeQuantityNextInterval(28080000), is("10.8 mo"));
+        MatcherAssert.assertThat(timeQuantityNextInterval(28080000), Matchers.`is`("10.8 mo"))
     }
 
-
-    @NonNull
-    private String timeQuantityNextInterval(@SuppressWarnings("SameParameterValue") int time_s) {
-        return Utils.timeQuantityNextIvl(getTargetContext(), time_s);
+    private fun timeQuantityNextInterval(time_s: Int): String {
+        return Utils.timeQuantityNextIvl(targetContext, time_s.toLong())
     }
 
-
-    @NonNull
     @CheckResult
-    private String deckPickerTime(long time) {
-        return Utils.timeQuantityTopDeckPicker(this.getTargetContext(), time);
+    private fun deckPickerTime(time: Long): String {
+        return Utils.timeQuantityTopDeckPicker(targetContext, time)
     }
 }
