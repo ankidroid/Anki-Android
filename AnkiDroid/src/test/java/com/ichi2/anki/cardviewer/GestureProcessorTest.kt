f14bff4d279b920ff0ee062257a4d1d3a78ca4f4
Akshay <52353967+Akshay0701@users.noreply.github.com>
2022-01-16 16:17:14 +0530

    [Kotlin Migration] GestureProcessorTest

--- AnkiDroid/src/test/java/com/ichi2/anki/cardviewer/GestureProcessorTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/cardviewer/GestureProcessorTest.kt
@@ -13,73 +13,58 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.anki.cardviewer
 
-package com.ichi2.anki.cardviewer;
+import android.content.SharedPreferences
+import android.view.ViewConfiguration
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.MockedStatic
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import java.util.*
 
-import android.content.SharedPreferences;
-import android.view.ViewConfiguration;
-
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import androidx.annotation.NonNull;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.nullable;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-public class GestureProcessorTest implements ViewerCommand.CommandProcessor {
-
-    private final GestureProcessor mSut = new GestureProcessor(this);
-    private final List<ViewerCommand> mExecutedCommands = new ArrayList<>();
-
-    @Override
-    public boolean executeCommand(@NonNull ViewerCommand which) {
-        this.mExecutedCommands.add(which);
-        return true;
+class GestureProcessorTest : ViewerCommand.CommandProcessor {
+    private val mSut = GestureProcessor(this)
+    private val mExecutedCommands: MutableList<ViewerCommand> = ArrayList()
+    override fun executeCommand(which: ViewerCommand): Boolean {
+        mExecutedCommands.add(which)
+        return true
     }
 
-    ViewerCommand singleResult() {
-        assertThat(mExecutedCommands, hasSize(1));
-        return mExecutedCommands.get(0);
-    }
-
-    private static MockedStatic<ViewConfiguration> utilities;
-
-    @BeforeClass
-    public static void before() {
-        utilities = Mockito.mockStatic(ViewConfiguration.class);
-        utilities.when(() -> ViewConfiguration.get(any())).thenReturn(mock(ViewConfiguration.class));
-    }
-
-    @AfterClass
-    public static void after() {
-        utilities.close();
+    private fun singleResult(): ViewerCommand {
+        MatcherAssert.assertThat<List<ViewerCommand>>(mExecutedCommands, Matchers.hasSize(1))
+        return mExecutedCommands[0]
     }
 
     @Test
-    public void integrationTest() {
-        SharedPreferences prefs = mock(SharedPreferences.class, Mockito.RETURNS_DEEP_STUBS);
+    fun integrationTest() {
+        val prefs = Mockito.mock(SharedPreferences::class.java, Mockito.RETURNS_DEEP_STUBS)
+        `when`(prefs.getString(ArgumentMatchers.nullable(String::class.java), ArgumentMatchers.nullable(String::class.java))).thenReturn("0")
+        `when`(prefs.getString(ArgumentMatchers.eq("gestureTapCenter"), ArgumentMatchers.nullable(String::class.java))).thenReturn("1")
+        `when`(prefs.getBoolean(ArgumentMatchers.eq("gestureCornerTouch"), ArgumentMatchers.anyBoolean())).thenReturn(true)
+        mSut.init(prefs)
+        mSut.onTap(100, 100, 50f, 50f)
+        MatcherAssert.assertThat(singleResult(), Matchers.`is`(ViewerCommand.COMMAND_SHOW_ANSWER))
+    }
 
-        when(prefs.getString(nullable(String.class), nullable(String.class))).thenReturn("0");
-        when(prefs.getString(eq("gestureTapCenter"), nullable(String.class))).thenReturn("1");
-        when(prefs.getBoolean(eq("gestureCornerTouch"), ArgumentMatchers.anyBoolean())).thenReturn(true);
+    companion object {
+        @JvmStatic
+        private lateinit var utilities: MockedStatic<ViewConfiguration>
 
-        mSut.init(prefs);
+        @BeforeClass
+        fun before() {
+            utilities = Mockito.mockStatic(ViewConfiguration::class.java)
+            utilities.`when`<Any> { ViewConfiguration.get(ArgumentMatchers.any()) }.thenReturn(Mockito.mock(ViewConfiguration::class.java))
+        }
 
-        mSut.onTap(100, 100, 50, 50);
-        assertThat(singleResult(), is(ViewerCommand.COMMAND_SHOW_ANSWER));
+        @AfterClass
+        fun after() {
+            utilities.close()
+        }
     }
 }
