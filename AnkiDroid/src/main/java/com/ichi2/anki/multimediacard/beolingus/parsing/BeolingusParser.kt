c11dce79aa8f7e16e9033ebf5240744bfa571743
Akshay <52353967+Akshay0701@users.noreply.github.com>
2022-02-02 00:07:35 +0530

    [Kotlin Migration] BeolingusParser

--- AnkiDroid/src/main/java/com/ichi2/anki/multimediacard/beolingus/parsing/BeolingusParser.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/multimediacard/beolingus/parsing/BeolingusParser.kt
@@ -17,57 +17,56 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-package com.ichi2.anki.multimediacard.beolingus.parsing;
+package com.ichi2.anki.multimediacard.beolingus.parsing
 
-import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import timber.log.Timber;
+import timber.log.Timber
+import java.util.*
+import java.util.regex.Pattern
 
 /**
  * This class parses beolingus pages
  */
-public class BeolingusParser {
-
-    private static final Pattern PRONUNC_PATTERN = Pattern.compile("" +
+object BeolingusParser {
+    private val PRONUNC_PATTERN = Pattern.compile(
+        "" +
             "<a href=\"([^\"]+)\"[^>]*>" +
-            "<img src=\"/pics/s1[.]png\"[^>]*title=\"([^\"]+)\"[^>]*>");
-    private static final Pattern MP3_PATTERN = Pattern.compile("href=\"([^\"]+\\.mp3)\">");
+            "<img src=\"/pics/s1[.]png\"[^>]*title=\"([^\"]+)\"[^>]*>"
+    )
+    private val MP3_PATTERN = Pattern.compile("href=\"([^\"]+\\.mp3)\">")
 
     /**
      * @param html HTML page from beolingus, with translation of the word we search
      * @return {@code "no"} or the pronunciation URL
      */
-    public static String getPronunciationAddressFromTranslation(String html, String wordToSearchFor) {
-        Matcher m = PRONUNC_PATTERN.matcher(html);
+    @JvmStatic
+    fun getPronunciationAddressFromTranslation(html: String?, wordToSearchFor: String): String {
+        val m = PRONUNC_PATTERN.matcher(html!!)
         while (m.find()) {
-            //Perform .contains() due to #5376 (a "%20{noun}" suffix).
-            //Perform .toLowerCase() due to #5810 ("hello" should match "Hello").
-            //See #5810 for discussion on Locale complexities. Currently unhandled.
-            if (m.group(2).toLowerCase(Locale.ROOT).contains(wordToSearchFor.toLowerCase(Locale.ROOT))) {
-                Timber.d("pronunciation URL is https://dict.tu-chemnitz.de%s", m.group(1));
-                return "https://dict.tu-chemnitz.de" + m.group(1);
+            // Perform .contains() due to #5376 (a "%20{noun}" suffix).
+            // Perform .toLowerCase() due to #5810 ("hello" should match "Hello").
+            // See #5810 for discussion on Locale complexities. Currently unhandled.
+            if (m.group(2)!!.lowercase(Locale.ROOT).contains(wordToSearchFor.lowercase(Locale.ROOT))) {
+                Timber.d("pronunciation URL is https://dict.tu-chemnitz.de%s", m.group(1))
+                return "https://dict.tu-chemnitz.de" + m.group(1)
             }
         }
-        Timber.d("Unable to find pronunciation URL");
-        return "no";
+        Timber.d("Unable to find pronunciation URL")
+        return "no"
     }
-
 
     /**
      * @return {@code "no"}, or the http address of the mp3 file
      */
-    public static String getMp3AddressFromPronounciation(String pronunciationPageHtml) {
+    @JvmStatic
+    fun getMp3AddressFromPronounciation(pronunciationPageHtml: String?): String {
         // Only log the page if you need to work with the regex
         // Timber.d("pronunciationPageHtml is %s", pronunciationPageHtml);
-        Matcher m = MP3_PATTERN.matcher(pronunciationPageHtml);
+        val m = MP3_PATTERN.matcher(pronunciationPageHtml!!)
         if (m.find()) {
-            Timber.d("MP3 address is https://dict.tu-chemnitz.de%s", m.group(1));
-            return "https://dict.tu-chemnitz.de" + m.group(1);
+            Timber.d("MP3 address is https://dict.tu-chemnitz.de%s", m.group(1))
+            return "https://dict.tu-chemnitz.de" + m.group(1)
         }
-        Timber.d("Unable to find MP3 file address");
-        return "no";
+        Timber.d("Unable to find MP3 file address")
+        return "no"
     }
-
 }
