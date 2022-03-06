8ef5372b3acedc2531ed8eb8c0f0d0b2d90388ad
krmanik <12841290+krmanik@users.noreply.github.com>
2022-03-07 06:05:09 +0800

    change to data class and update docs

--- AnkiDroid/src/main/java/com/ichi2/anki/jsaddons/AddonModel.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/jsaddons/AddonModel.kt
@@ -22,28 +22,27 @@ import com.ichi2.anki.jsaddons.NpmUtils.ANKIDROID_JS_ADDON_KEYWORDS
 import com.ichi2.anki.jsaddons.NpmUtils.NOTE_EDITOR_ADDON
 
 /**
- * This class used in NpmPackageDownloader by ObjectMapper to map npm package.json to AddonModel
  * When package.json fetched from https://registry.npmjs.org/some-addon/latest,
- * ObjectMapper (jackson) readvalue map package.json to this class
- * All the required fields in package.json mapped to variables in this class.
+ * all the required fields in package.json mapped to variables in this class.
  * The most important fields in package.json are
  * ankiDroidJsApi, addonType and keywords --> this distinguish from other npm packages
  */
 
-class AddonModel {
-    val name: String? = null // name of npm package, it unique for each package listed on npm
-    val addonTitle: String? = null // for showing in AnkiDroid
-    val icon: String? = null // only required for note editor (single character recommended)
-    val version: String? = null
-    val description: String? = null
-    val main: String? = null
-    val ankidroidJsApi: String? = null
-    val addonType: String? = null
-    val keywords: Array<String>? = null
-    val author: Map<String, String>? = null
-    val license: String? = null
-    val homepage: String? = null
+data class AddonModel(
+    val name: String? = null, // name of npm package, it unique for each package listed on npm
+    val addonTitle: String? = null, // for showing in AnkiDroid
+    val icon: String? = null, // only required for note editor (single character recommended)
+    val version: String? = null,
+    val description: String? = null,
+    val main: String? = null,
+    val ankidroidJsApi: String? = null,
+    val addonType: String? = null,
+    val keywords: Array<String>? = null,
+    val author: Map<String, String>? = null,
+    val license: String? = null,
+    val homepage: String? = null,
     val dist: Map<String, String>? = null
+) {
 
     /**
      * @param jsAddonKey  REVIEWER_ADDON_KEY
