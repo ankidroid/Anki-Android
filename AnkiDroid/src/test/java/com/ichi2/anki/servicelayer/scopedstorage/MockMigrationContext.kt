b66fc614078a8ef2f75ee372d400be79ae4f1900
David Allison <62114487+david-allison-1@users.noreply.github.com>
2022-03-10 22:23:34 +0000

    feat(scoped-storage): Retry DeleteEmptyDirectory
    If DeleteEmptyDirectory fails, we want to perform
    the MoveDirectory that spawned it
    
    This could happen for many reasons, such as a failed file
    move, or adding a file to `AnkiDroid` during the process

--- AnkiDroid/src/test/java/com/ichi2/anki/servicelayer/scopedstorage/MockMigrationContext.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/servicelayer/scopedstorage/MockMigrationContext.kt
@@ -16,7 +16,7 @@
 
 package com.ichi2.anki.servicelayer.scopedstorage
 
-class MockMigrationContext : MigrateUserData.MigrationContext() {
+open class MockMigrationContext : MigrateUserData.MigrationContext() {
     /** set [logExceptions] to populate this property */
     val exceptions = mutableListOf<Exception>()
     var logExceptions: Boolean = false
@@ -31,5 +31,30 @@ class MockMigrationContext : MigrateUserData.MigrationContext() {
 
     override fun reportProgress(transferred: NumberOfBytes) {
         progress.add(transferred)
+    }
+}
+
+/**
+ * A [MockMigrationContext] which will call [MigrateUserData.Operation.retryOperations] once on
+ * a failed operation, if any `retryOperations` are available.
+ *
+ * If a second failure occurs, or if no `retryOperations` are available, it will throw
+ */
+class RetryMigrationContext(val retry: (List<MigrateUserData.Operation>) -> Unit) : MockMigrationContext() {
+    var retryCount = 0
+
+    override fun reportError(context: MigrateUserData.Operation, ex: Exception) {
+        val opsForRetry = context.retryOperations
+        if (!opsForRetry.any()) {
+            throw ex
+        }
+        retryCount++
+        if (retryCount > 1) {
+            throw ex
+        }
+        retry(opsForRetry)
+    }
+
+    override fun reportProgress(transferred: NumberOfBytes) {
     }
 }
