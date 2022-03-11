3ea7b3630f617f20c51fc17e6cba8935b57f03cf
Murugappan <60959623+murugu-21@users.noreply.github.com>
2022-03-11 10:35:06 +0530

    Image paste clipboard (#10320)
    Co-authored-by: David Allison <62114487+david-allison-1@users.noreply.github.com>

--- AnkiDroid/src/main/java/com/ichi2/utils/ContentResolverUtil.kt
+++ AnkiDroid/src/main/java/com/ichi2/utils/ContentResolverUtil.kt
@@ -75,11 +75,23 @@ object ContentResolverUtil {
     private fun getFilenameViaDisplayName(contentResolver: ContentResolver, uri: Uri): String? {
         // 7748: android.database.sqlite.SQLiteException: no such column: _display_name (code 1 SQLITE_ERROR[1]): ...
         try {
-            contentResolver.query(uri, arrayOf(MediaStore.MediaColumns.DISPLAY_NAME), null, null, null).use { c ->
-                if (c != null) {
-                    c.moveToNext()
-                    return c.getString(0)
+            contentResolver.query(uri, null, null, null, null).use { c ->
+                if (c == null) {
+                    return null
                 }
+                c.moveToNext()
+                val mediaIndex = c.getColumnIndex(MediaStore.MediaColumns.DISPLAY_NAME)
+                if (mediaIndex != -1) {
+                    return c.getString(mediaIndex)
+                }
+
+                // use `_data` column label directly, MediaStore.MediaColumns.DATA is deprecated in API29+ but
+                // the recommended MediaStore.MediaColumns.RELATIVE_PATH does not appear to be available
+                // content uri contains only id and _data columns in samsung clipboard and not media columns
+                // gboard contains media columns and works with MediaStore.MediaColumns.DISPLAY_NAME
+                val dataIndex = c.getColumnIndexOrThrow("_data")
+                val fileUri = Uri.parse(c.getString(dataIndex))
+                return fileUri.lastPathSegment
             }
         } catch (e: SQLiteException) {
             Timber.w(e, "getFilenameViaDisplayName ContentResolver query failed.")
