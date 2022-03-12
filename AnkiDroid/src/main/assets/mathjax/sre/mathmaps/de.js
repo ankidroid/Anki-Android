5ebad84fa7988421c88967798180c913db9d41ed
tharun634 <tharun634@gmail.com>
2022-03-12 18:33:45 +0530

    feat(prettier-check): ran prettier + .prettierignore

--- AnkiDroid/src/main/assets/mathjax/sre/mathmaps/de.js
+++ AnkiDroid/src/main/assets/mathjax/sre/mathmaps/de.js
@@ -101,4 +101,3 @@
 "de/units/weight.js": 
 [{"locale":"de"},{"key":"lb","mappings":{"default":{"default":"Pfund"}},"category":"weight","names":["lb","lb."]},{"key":"oz","mappings":{"default":{"plural":"Unzen","default":"Unze"}},"category":"weight","names":["oz","oz."]},{"category":"weight","mappings":{"default":{"default":"Long ton"}},"key":"LT","names":["LT","L.T."]},{"key":"gr","mappings":{"default":{"default":"Gramm"}},"category":"weight","names":["g","gr"]},{"category":"weight","mappings":{"default":{"default":"Kilogramm"}},"key":"kg","names":["kg"]},{"category":"weight","mappings":{"default":{"default":"Mikrogramm"}},"key":"mcg","names":["mcg","µg"]},{"category":"weight","mappings":{"default":{"default":"Milligramm"}},"key":"mg","names":["mg"]},{"key":"t","mappings":{"default":{"plural":"Tonnen","default":"Tonne"}},"category":"weight","names":["t","T"]}]
 }
-
