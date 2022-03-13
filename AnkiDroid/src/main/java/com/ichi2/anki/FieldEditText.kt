ea7c62cc0143d9269c5eb9dd5b7c9d69fb49c63f
Shridhar Goel <shridhar.goel@gmail.com>
2022-03-13 20:16:01 +0530

    Migrate FieldEditText to Kotlin

--- AnkiDroid/src/main/java/com/ichi2/anki/FieldEditText.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/FieldEditText.kt
@@ -14,249 +14,185 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-package com.ichi2.anki;
+package com.ichi2.anki
 
-import android.content.ClipDescription;
-import android.content.ClipboardManager;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.os.Build;
-import android.os.LocaleList;
-import android.os.Parcel;
-import android.os.Parcelable;
-import android.text.Editable;
-import android.text.InputType;
-import android.util.AttributeSet;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputConnection;
-import android.widget.EditText;
+import android.content.ClipboardManager
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.*
+import android.text.InputType
+import android.util.AttributeSet
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputConnection
+import android.widget.EditText
+import androidx.annotation.RequiresApi
+import androidx.core.view.inputmethod.EditorInfoCompat
+import androidx.core.view.inputmethod.InputConnectionCompat
+import androidx.core.view.inputmethod.InputContentInfoCompat
+import com.ichi2.anki.servicelayer.NoteService
+import com.ichi2.themes.Themes.getColorFromAttr
+import com.ichi2.ui.FixedEditText
+import com.ichi2.utils.ClipboardUtil.IMAGE_MIME_TYPES
+import com.ichi2.utils.ClipboardUtil.getImageUri
+import com.ichi2.utils.ClipboardUtil.hasImage
+import com.ichi2.utils.KotlinCleanup
+import timber.log.Timber
+import java.lang.Exception
+import java.util.*
 
-import java.util.Locale;
+class FieldEditText : FixedEditText, NoteService.NoteField {
+    override var ord = 0
+    private var mOrigBackground: Drawable? = null
+    private var mSelectionChangeListener: TextSelectionListener? = null
+    private var mImageListener: ImagePasteListener? = null
+    private var mClipboard: ClipboardManager? = null
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
+    constructor(context: Context?) : super(context!!) {}
+    constructor(context: Context?, attr: AttributeSet?) : super(context!!, attr) {}
+    constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) : super(context!!, attrs, defStyle) {}
 
-import androidx.core.view.inputmethod.EditorInfoCompat;
-import androidx.core.view.inputmethod.InputConnectionCompat;
-import timber.log.Timber;
-
-import com.ichi2.anki.servicelayer.NoteService;
-import com.ichi2.themes.Themes;
-import com.ichi2.ui.FixedEditText;
-import com.ichi2.utils.ClipboardUtil;
-
-import java.util.Objects;
-
-import static android.view.inputmethod.EditorInfo.IME_FLAG_NO_EXTRACT_UI;
-import static com.ichi2.utils.ClipboardUtil.IMAGE_MIME_TYPES;
-
-
-public class FieldEditText extends FixedEditText implements NoteService.NoteField {
-
-    @NonNull
-    public static final String NEW_LINE = Objects.requireNonNull(System.getProperty("line.separator"));
-
-    private int mOrd;
-    private Drawable mOrigBackground;
-    @Nullable
-    private TextSelectionListener mSelectionChangeListener;
-    @Nullable
-    private ImagePasteListener mImageListener;
-    @Nullable
-    private ClipboardManager mClipboard;
-
-
-    public FieldEditText(Context context) {
-        super(context);
-    }
-
-
-    public FieldEditText(Context context, AttributeSet attr) {
-        super(context, attr);
-    }
-
-
-    public FieldEditText(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
-
-
-    @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
         if (shouldDisableExtendedTextUi()) {
-            Timber.i("Disabling Extended Text UI");
-            this.setImeOptions(this.getImeOptions() | IME_FLAG_NO_EXTRACT_UI);
+            Timber.i("Disabling Extended Text UI")
+            this.imeOptions = this.imeOptions or EditorInfo.IME_FLAG_NO_EXTRACT_UI
         }
     }
 
-    private boolean shouldDisableExtendedTextUi() {
+    @KotlinCleanup("Remove try-catch")
+    private fun shouldDisableExtendedTextUi(): Boolean {
+        return try {
+            val sp = AnkiDroidApp.getSharedPrefs(this.context)
+            sp.getBoolean("disableExtendedTextUi", false)
+        } catch (e: Exception) {
+            Timber.e(e, "Failed to get extended UI preference")
+            false
+        }
+    }
+
+    @KotlinCleanup("Simplify")
+    override val fieldText: String?
+        get() {
+            val text = text ?: return null
+            return text.toString()
+        }
+
+    fun init() {
         try {
-            SharedPreferences sp = AnkiDroidApp.getSharedPrefs(this.getContext());
-            return sp.getBoolean("disableExtendedTextUi", false);
-        } catch (Exception e) {
-            Timber.e(e, "Failed to get extended UI preference");
-            return false;
+            mClipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        } catch (e: Exception) {
+            Timber.w(e)
         }
-    }
-
-
-    public int getOrd() {
-        return mOrd;
-    }
-
-
-    @Override
-    public String getFieldText() {
-        Editable text = getText();
-        if (text == null) {
-            return null;
-        }
-        return text.toString();
-    }
-
-
-    public void init() {
-        try {
-            mClipboard = (ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
-        } catch (Exception e) {
-            Timber.w(e);
-        }
-        setMinimumWidth(400);
-        mOrigBackground = getBackground();
+        minimumWidth = 400
+        mOrigBackground = background
         // Fixes bug where new instances of this object have wrong colors, probably
         // from some reuse mechanic in Android.
-        setDefaultStyle();
+        setDefaultStyle()
     }
 
-    public void setImagePasteListener(ImagePasteListener imageListener) {
-        mImageListener = imageListener;
+    fun setImagePasteListener(imageListener: ImagePasteListener?) {
+        mImageListener = imageListener
     }
 
-    @Override
-    @SuppressWarnings("deprecation") // Tracked in #9775
-    public InputConnection onCreateInputConnection(EditorInfo editorInfo) {
-        InputConnection inputConnection = super.onCreateInputConnection(editorInfo);
-        if (inputConnection == null) {
-            return null;
-        }
-        EditorInfoCompat.setContentMimeTypes(editorInfo, IMAGE_MIME_TYPES);
-        return InputConnectionCompat.createWrapper(inputConnection, editorInfo, (contentInfo, flags, opts) -> {
-
+    // Tracked in #9775
+    @Suppress("deprecation")
+    override fun onCreateInputConnection(editorInfo: EditorInfo): InputConnection? {
+        val inputConnection = super.onCreateInputConnection(editorInfo) ?: return null
+        EditorInfoCompat.setContentMimeTypes(editorInfo, IMAGE_MIME_TYPES)
+        return InputConnectionCompat.createWrapper(inputConnection, editorInfo) { contentInfo: InputContentInfoCompat, flags: Int, opts: Bundle? ->
             if (mImageListener == null) {
-                return false;
+                return@createWrapper false
             }
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1  && (flags &
-                    InputConnection.INPUT_CONTENT_GRANT_READ_URI_PERMISSION) != 0) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1 && flags and
+                InputConnection.INPUT_CONTENT_GRANT_READ_URI_PERMISSION != 0
+            ) {
                 try {
-                    contentInfo.requestPermission();
-                }
-                catch (Exception e) {
-                    return false;
+                    contentInfo.requestPermission()
+                } catch (e: Exception) {
+                    return@createWrapper false
                 }
             }
-
-            ClipDescription description = contentInfo.getDescription();
-
-            if (!ClipboardUtil.hasImage(description)) {
-                return false;
+            val description = contentInfo.description
+            if (!hasImage(description)) {
+                return@createWrapper false
             }
-
             try {
-                if (!onImagePaste(contentInfo.getContentUri())) {
-                    return false;
+                if (!onImagePaste(contentInfo.contentUri)) {
+                    return@createWrapper false
                 }
                 // There is a timeout on this line which occurs even if we're stopped in the debugger, if we take too long we get
                 // "Ankidroid doesn't support image insertion here"
-                InputConnectionCompat.commitContent(inputConnection, editorInfo, contentInfo, flags, opts);
-                return true;
-            } catch (Exception e) {
-                Timber.w(e);
-                AnkiDroidApp.sendExceptionReport(e, "NoteEditor::onImage");
-                return false;
-            }
-        });
-
-
-    }
-
-
-    @Override
-    protected void onSelectionChanged(int selStart, int selEnd) {
-        if (mSelectionChangeListener != null) {
-            try {
-                mSelectionChangeListener.onSelectionChanged(selStart, selEnd);
-            } catch (Exception e) {
-                Timber.w(e, "mSelectionChangeListener");
+                InputConnectionCompat.commitContent(inputConnection, editorInfo, contentInfo, flags, opts)
+                return@createWrapper true
+            } catch (e: Exception) {
+                Timber.w(e)
+                AnkiDroidApp.sendExceptionReport(e, "NoteEditor::onImage")
+                return@createWrapper false
             }
         }
-        super.onSelectionChanged(selStart, selEnd);
     }
 
-
+    override fun onSelectionChanged(selStart: Int, selEnd: Int) {
+        if (mSelectionChangeListener != null) {
+            try {
+                mSelectionChangeListener!!.onSelectionChanged(selStart, selEnd)
+            } catch (e: Exception) {
+                Timber.w(e, "mSelectionChangeListener")
+            }
+        }
+        super.onSelectionChanged(selStart, selEnd)
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public void setHintLocale(@NonNull Locale locale) {
-        Timber.d("Setting hint locale to '%s'", locale);
-        setImeHintLocales(new LocaleList(locale));
+    fun setHintLocale(locale: Locale) {
+        Timber.d("Setting hint locale to '%s'", locale)
+        imeHintLocales = LocaleList(locale)
     }
 
     /**
      * Modify the style of this view to represent a duplicate field.
      */
-    public void setDupeStyle() {
-        setBackgroundColor(Themes.getColorFromAttr(getContext(), R.attr.duplicateColor));
+    fun setDupeStyle() {
+        setBackgroundColor(getColorFromAttr(context, R.attr.duplicateColor))
     }
-
 
     /**
      * Restore the default style of this view.
      */
-    public void setDefaultStyle() {
-        setBackgroundDrawable(mOrigBackground);
+    @Suppress("deprecation")
+    fun setDefaultStyle() {
+        setBackgroundDrawable(mOrigBackground)
     }
 
-    public void setSelectionChangeListener(TextSelectionListener listener) {
-        this.mSelectionChangeListener = listener;
+    fun setSelectionChangeListener(listener: TextSelectionListener?) {
+        mSelectionChangeListener = listener
     }
 
-
-    public void setContent(String content, boolean replaceNewLine) {
+    fun setContent(content: String?, replaceNewLine: Boolean) {
+        var _content = content
         if (content == null) {
-            content = "";
+            _content = ""
         } else if (replaceNewLine) {
-            content = content.replaceAll("<br(\\s*/*)>", NEW_LINE);
+            _content = content.replace("<br(\\s*/*)>".toRegex(), NEW_LINE)
         }
-        setText(content);
+        setText(_content)
     }
 
-
-    public void setOrd(int ord) {
-        mOrd = ord;
+    override fun onSaveInstanceState(): Parcelable? {
+        val state = super.onSaveInstanceState()
+        val savedState = SavedState(state)
+        savedState.ord = ord
+        return savedState
     }
 
-    @Nullable
-    @Override
-    public Parcelable onSaveInstanceState() {
-        Parcelable state = super.onSaveInstanceState();
-
-        SavedState savedState = new SavedState(state);
-        savedState.mOrd = mOrd;
-
-        return savedState;
-    }
-
-
-    @Override
-    public boolean onTextContextMenuItem(int id) {
+    @KotlinCleanup("Return instead of using _id")
+    override fun onTextContextMenuItem(id: Int): Boolean {
         // This handles both CTRL+V and "Paste"
-       if (id == android.R.id.paste && ClipboardUtil.hasImage(mClipboard)) {
-           return onImagePaste(ClipboardUtil.getImageUri(mClipboard));
-       }
+        var _id = id
+        if (id == android.R.id.paste && hasImage(mClipboard)) {
+            return onImagePaste(getImageUri(mClipboard))
+        }
 
         /*
         * The following code replaces the instruction "paste with formatting" with "paste as plan text"
@@ -268,93 +204,86 @@ public class FieldEditText extends FixedEditText implements NoteService.NoteFiel
         if (id == android.R.id.paste) {
             /**
              * Modified StackOverflow answer:
-             * Pasting as plain text for VERSION_CODES < M required modifying 
+             * Pasting as plain text for VERSION_CODES < M required modifying
              * the user's clipboard, and hence has not been used.
              *
-             * During testing, older devices pasted text as plain text by default, 
+             * During testing, older devices pasted text as plain text by default,
              * so there was no need for a TO-DO
              */
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                id = android.R.id.pasteAsPlainText;
+                _id = android.R.id.pasteAsPlainText
             }
         }
-        return super.onTextContextMenuItem(id);
+        return super.onTextContextMenuItem(_id)
     }
 
+    @KotlinCleanup("Make param non-null")
+    protected fun onImagePaste(imageUri: Uri?): Boolean {
+        return if (imageUri == null) {
+            false
+        } else mImageListener!!.onImagePaste(this, imageUri)
+    }
 
-    protected boolean onImagePaste(Uri imageUri) {
-        if (imageUri == null) {
-            return false;
+    override fun onRestoreInstanceState(state: Parcelable) {
+        if (state !is SavedState) {
+            super.onRestoreInstanceState(state)
+            return
         }
-        return mImageListener.onImagePaste(this, imageUri);
+        val ss = state
+        super.onRestoreInstanceState(ss.superState)
+        ord = ss.ord
     }
 
-
-    @Override
-    public void onRestoreInstanceState(Parcelable state) {
-        if (!(state instanceof SavedState)) {
-            super.onRestoreInstanceState(state);
-            return;
-        }
-
-        SavedState ss = (SavedState) state;
-        super.onRestoreInstanceState(ss.getSuperState());
-
-        mOrd = ss.mOrd;
-    }
-
-
-    public void setCapitalize(boolean value) {
-        int inputType = this.getInputType();
-        if (value) {
-            this.setInputType(inputType | InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
+    fun setCapitalize(value: Boolean) {
+        val inputType = this.inputType
+        this.inputType = if (value) {
+            inputType or InputType.TYPE_TEXT_FLAG_CAP_SENTENCES
         } else {
-            this.setInputType(inputType & ~InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
+            inputType and InputType.TYPE_TEXT_FLAG_CAP_SENTENCES.inv()
         }
     }
 
+    val isCapitalized: Boolean
+        get() = this.inputType and InputType.TYPE_TEXT_FLAG_CAP_SENTENCES == InputType.TYPE_TEXT_FLAG_CAP_SENTENCES
 
-    public boolean isCapitalized() {
-        return (this.getInputType() & InputType.TYPE_TEXT_FLAG_CAP_SENTENCES) == InputType.TYPE_TEXT_FLAG_CAP_SENTENCES;
-    }
+    @KotlinCleanup("Use @Parcelize")
+    internal class SavedState : BaseSavedState {
+        var ord = 0
 
+        constructor(superState: Parcelable?) : super(superState) {}
 
-    static class SavedState extends BaseSavedState {
-        private int mOrd;
-
-        SavedState(Parcelable superState) {
-            super(superState);
+        override fun writeToParcel(out: Parcel, flags: Int) {
+            super.writeToParcel(out, flags)
+            out.writeInt(ord)
         }
 
-        @Override
-        public void writeToParcel(Parcel out, int flags) {
-            super.writeToParcel(out, flags);
-            out.writeInt(mOrd);
+        private constructor(`in`: Parcel) : super(`in`) {
+            ord = `in`.readInt()
         }
 
-        public static final Parcelable.Creator<SavedState> CREATOR =
-                new Parcelable.Creator<SavedState>() {
-                    @Override
-                    public SavedState createFromParcel(Parcel source) {
-                        return new SavedState(source);
-                    }
+        companion object {
+            val CREATOR: Parcelable.Creator<SavedState> = object : Parcelable.Creator<SavedState> {
+                override fun createFromParcel(source: Parcel): SavedState {
+                    return SavedState(source)
+                }
 
-                    public SavedState[] newArray(int size) {
-                        return new SavedState[size];
-                    }
-                };
-
-        private SavedState(Parcel in) {
-            super(in);
-            this.mOrd = in.readInt();
+                override fun newArray(size: Int): Array<SavedState?> {
+                    return arrayOfNulls(size)
+                }
+            }
         }
     }
 
-    public interface TextSelectionListener {
-        void onSelectionChanged(int selStart, int selEnd);
+    interface TextSelectionListener {
+        fun onSelectionChanged(selStart: Int, selEnd: Int)
     }
 
-    public interface ImagePasteListener {
-        boolean onImagePaste(EditText editText, Uri uri);
+    interface ImagePasteListener {
+        fun onImagePaste(editText: EditText?, uri: Uri?): Boolean
+    }
+
+    companion object {
+        @JvmField
+        val NEW_LINE: String = Objects.requireNonNull(System.getProperty("line.separator"))
     }
 }
