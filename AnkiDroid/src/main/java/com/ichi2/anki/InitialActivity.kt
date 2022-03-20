7609a381ba3d482bd34558edb848585797ca9304
Prince kushwaha <kushwahaprince418@gmail.com>
2022-03-20 11:36:25 +0530

    Migrate InitialActivity.java to Kotlin

--- AnkiDroid/src/main/java/com/ichi2/anki/InitialActivity.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/InitialActivity.kt
@@ -14,147 +14,127 @@
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.ichi2.anki;
+package com.ichi2.anki
 
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.*
+import androidx.annotation.CheckResult
+import com.ichi2.anki.CollectionHelper.DatabaseVersion
+import com.ichi2.anki.exception.OutOfSpaceException
+import com.ichi2.anki.servicelayer.PreferenceUpgradeService
+import com.ichi2.anki.servicelayer.PreferenceUpgradeService.setPreferencesUpToDate
+import com.ichi2.utils.VersionUtils.pkgVersionName
+import net.ankiweb.rsdroid.BackendException.BackendDbException.BackendDbLockedException
+import net.ankiweb.rsdroid.BackendFactory
+import net.ankiweb.rsdroid.RustBackendFailedException
+import timber.log.Timber
+import java.lang.ref.WeakReference
 
-import com.ichi2.anki.exception.OutOfSpaceException;
-import com.ichi2.anki.servicelayer.PreferenceUpgradeService;
-import com.ichi2.utils.VersionUtils;
-
-import net.ankiweb.rsdroid.BackendException;
-import net.ankiweb.rsdroid.BackendFactory;
-import net.ankiweb.rsdroid.RustBackendFailedException;
-
-import java.lang.ref.WeakReference;
-
-import androidx.annotation.CheckResult;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import timber.log.Timber;
-
-/** Utilities for launching the first activity (currently the DeckPicker) */
-public class InitialActivity {
-
-    private InitialActivity() {
-
-    }
-
-    /** Returns null on success */
-    @Nullable
+/** Utilities for launching the first activity (currently the DeckPicker)  */
+object InitialActivity {
+    /** Returns null on success  */
+    @JvmStatic
     @CheckResult
-    public static StartupFailure getStartupFailureType(Context context) {
+    fun getStartupFailureType(context: Context): StartupFailure? {
 
         // A WebView failure means that we skip `AnkiDroidApp`, and therefore haven't loaded the collection
         if (AnkiDroidApp.webViewFailedToLoad()) {
-            return StartupFailure.WEBVIEW_FAILED;
+            return StartupFailure.WEBVIEW_FAILED
         }
 
         // If we're OK, return null
         if (CollectionHelper.getInstance().getColSafe(context) != null) {
-            return null;
+            return null
         }
-
         if (!AnkiDroidApp.isSdCardMounted()) {
-            return StartupFailure.SD_CARD_NOT_MOUNTED;
+            return StartupFailure.SD_CARD_NOT_MOUNTED
         } else if (!CollectionHelper.isCurrentAnkiDroidDirAccessible(context)) {
-            return StartupFailure.DIRECTORY_NOT_ACCESSIBLE;
+            return StartupFailure.DIRECTORY_NOT_ACCESSIBLE
         }
 
-        CollectionHelper.DatabaseVersion databaseVersion = isFutureAnkiDroidVersion(context);
-        switch (databaseVersion) {
-            case FUTURE_NOT_DOWNGRADABLE:
-                return StartupFailure.FUTURE_ANKIDROID_VERSION;
-            case FUTURE_DOWNGRADABLE:
-                return StartupFailure.DATABASE_DOWNGRADE_REQUIRED;
-            case UNKNOWN:
-            case USABLE:
-            default:
-                try {
-                    CollectionHelper.getInstance().getCol(context);
-                    return StartupFailure.DB_ERROR;
-                } catch (BackendException.BackendDbException.BackendDbLockedException e) {
-                    return StartupFailure.DATABASE_LOCKED;
-                } catch (Exception ignored) {
-                    return StartupFailure.DB_ERROR;
-                }
+        return when (isFutureAnkiDroidVersion(context)) {
+            DatabaseVersion.FUTURE_NOT_DOWNGRADABLE -> StartupFailure.FUTURE_ANKIDROID_VERSION
+            DatabaseVersion.FUTURE_DOWNGRADABLE -> StartupFailure.DATABASE_DOWNGRADE_REQUIRED
+            DatabaseVersion.UNKNOWN, DatabaseVersion.USABLE -> try {
+                CollectionHelper.getInstance().getCol(context)
+                StartupFailure.DB_ERROR
+            } catch (e: BackendDbLockedException) {
+                StartupFailure.DATABASE_LOCKED
+            } catch (ignored: Exception) {
+                StartupFailure.DB_ERROR
+            }
         }
     }
 
-    private static CollectionHelper.DatabaseVersion isFutureAnkiDroidVersion(Context context) {
-        try {
-            return CollectionHelper.isFutureAnkiDroidVersion(context);
-        } catch (Exception e) {
-            Timber.w(e, "Could not determine if future AnkiDroid version - assuming not");
-            return CollectionHelper.DatabaseVersion.UNKNOWN;
+    private fun isFutureAnkiDroidVersion(context: Context): DatabaseVersion {
+        return try {
+            CollectionHelper.isFutureAnkiDroidVersion(context)
+        } catch (e: Exception) {
+            Timber.w(e, "Could not determine if future AnkiDroid version - assuming not")
+            DatabaseVersion.UNKNOWN
         }
     }
-
 
     /**
      * Downgrades the database at the currently selected collection path from V16 to V11 in a background task
      */
-    @SuppressWarnings("deprecation") // #7108: AsyncTask
-    public static void downgradeBackend(DeckPicker deckPicker) {
+    // #7108: AsyncTask
+    @Suppress("deprecation")
+    @JvmStatic
+    fun downgradeBackend(deckPicker: DeckPicker) {
         // Note: This method does not require a backend pointer or an open collection
-        Timber.i("Downgrading backend");
-        new PerformDowngradeTask(new WeakReference<>(deckPicker)).execute();
+        Timber.i("Downgrading backend")
+        PerformDowngradeTask(WeakReference(deckPicker)).execute()
     }
 
-
-    protected static void downgradeCollection(DeckPicker deckPicker, BackupManager backupManager) throws OutOfSpaceException, RustBackendFailedException {
-        if (deckPicker == null) {
-            throw new IllegalArgumentException("deckPicker was null");
-        }
-
-        String collectionPath = CollectionHelper.getCollectionPath(deckPicker);
-
-        if (!backupManager.performDowngradeBackupInForeground(collectionPath)) {
-            throw new IllegalArgumentException("backup failed");
-        }
-
-        Timber.d("Downgrading database to V11: '%s'", collectionPath);
-        BackendFactory.createInstance().getBackend().downgradeBackend(collectionPath);
+    @Throws(OutOfSpaceException::class, RustBackendFailedException::class)
+    internal fun downgradeCollection(deckPicker: DeckPicker?, backupManager: BackupManager) {
+        requireNotNull(deckPicker) { "deckPicker was null" }
+        val collectionPath = CollectionHelper.getCollectionPath(deckPicker)
+        require(backupManager.performDowngradeBackupInForeground(collectionPath)) { "backup failed" }
+        Timber.d("Downgrading database to V11: '%s'", collectionPath)
+        BackendFactory.createInstance().backend.downgradeBackend(collectionPath)
     }
 
-    /** @return Whether any preferences were upgraded */
-    public static boolean upgradePreferences(Context context, long previousVersionCode) {
-        return PreferenceUpgradeService.upgradePreferences(context, previousVersionCode);
+    /** @return Whether any preferences were upgraded
+     */
+    @JvmStatic
+    fun upgradePreferences(context: Context?, previousVersionCode: Long): Boolean {
+        return PreferenceUpgradeService.upgradePreferences(context, previousVersionCode)
     }
 
     /**
-     *  @return Whether a fresh install occurred and a "fresh install" setup for preferences was performed
-     *  This only refers to a fresh install from the preferences perspective, not from the Anki data perspective.
+     * @return Whether a fresh install occurred and a "fresh install" setup for preferences was performed
+     * This only refers to a fresh install from the preferences perspective, not from the Anki data perspective.
      *
-     *  NOTE: A user can wipe app data, which will mean this returns true WITHOUT deleting their collection.
-     *  The above note will need to be reevaluated after scoped storage migration takes place
+     * NOTE: A user can wipe app data, which will mean this returns true WITHOUT deleting their collection.
+     * The above note will need to be reevaluated after scoped storage migration takes place
      *
      *
-     *  On the other hand, restoring an app backup can cause this to return true before the Anki collection is created
-     *  in practice, this doesn't occur due to CollectionHelper.getCol creating a new collection, and it's called before
-     *  this in the startup script
+     * On the other hand, restoring an app backup can cause this to return true before the Anki collection is created
+     * in practice, this doesn't occur due to CollectionHelper.getCol creating a new collection, and it's called before
+     * this in the startup script
      */
+    @JvmStatic
     @CheckResult
-    public static boolean performSetupFromFreshInstallOrClearedPreferences(@NonNull SharedPreferences preferences) {
-        boolean lastVersionWasSet = !"".equals(preferences.getString("lastVersion", ""));
-
+    fun performSetupFromFreshInstallOrClearedPreferences(preferences: SharedPreferences): Boolean {
+        val lastVersionWasSet = "" != preferences.getString("lastVersion", "")
         if (lastVersionWasSet) {
-            Timber.d("Not a fresh install [preferences]");
-            return false;
+            Timber.d("Not a fresh install [preferences]")
+            return false
         }
-
-        Timber.i("Fresh install");
-        PreferenceUpgradeService.setPreferencesUpToDate(preferences);
-        setUpgradedToLatestVersion(preferences);
-
-        return true;
+        Timber.i("Fresh install")
+        setPreferencesUpToDate(preferences)
+        setUpgradedToLatestVersion(preferences)
+        return true
     }
 
-    /** Sets the preference stating that the latest version has been applied */
-    public static void setUpgradedToLatestVersion(@NonNull SharedPreferences preferences) {
-        Timber.i("Marked prefs as upgraded to latest version: %s", VersionUtils.getPkgVersionName());
-        preferences.edit().putString("lastVersion", VersionUtils.getPkgVersionName()).apply();
+    /** Sets the preference stating that the latest version has been applied  */
+    @JvmStatic
+    fun setUpgradedToLatestVersion(preferences: SharedPreferences) {
+        Timber.i("Marked prefs as upgraded to latest version: %s", pkgVersionName)
+        preferences.edit().putString("lastVersion", pkgVersionName).apply()
     }
 
     /** @return false: The app has been upgraded since the last launch OR the app was launched for the first time.
@@ -162,84 +142,59 @@ public class InitialActivity {
      * This is not called in the case of performSetupFromFreshInstall returning true.
      * So this should not use the default value
      */
-    public static boolean isLatestVersion(SharedPreferences preferences) {
-        return preferences.getString("lastVersion", "").equals(VersionUtils.getPkgVersionName());
+    @JvmStatic
+    fun isLatestVersion(preferences: SharedPreferences): Boolean {
+        return preferences.getString("lastVersion", "") == pkgVersionName
     }
-
 
     // I disapprove, but it's best to keep consistency with the rest of the app
-    @SuppressWarnings("deprecation") // #7108: AsyncTask
-    private static class PerformDowngradeTask extends android.os.AsyncTask<Void, Void, Void> {
-
-        private final WeakReference<DeckPicker> mDeckPicker;
-        private Exception mException;
-
-
-        public PerformDowngradeTask(WeakReference<DeckPicker> deckPicker) {
-            mDeckPicker = deckPicker;
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
+    // #7108: AsyncTask
+    @Suppress("deprecation")
+    private class PerformDowngradeTask(private val deckPicker: WeakReference<DeckPicker>) : AsyncTask<Void?, Void?, Void?>() {
+        private var mException: Exception? = null
+        override fun doInBackground(vararg p0: Void?): Void? {
             // It would be great if we could catch the OutOfSpaceException here
             try {
-                DeckPicker deckPicker = mDeckPicker.get();
-                downgradeCollection(deckPicker, deckPicker.getBackupManager());
-            } catch (Exception e) {
-                Timber.w(e);
-                this.mException = e;
+                val deckPicker = deckPicker.get()
+                downgradeCollection(deckPicker, deckPicker!!.backupManager)
+            } catch (e: Exception) {
+                Timber.w(e)
+                mException = e
             }
-            return null;
+            return null
         }
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            DeckPicker d = mDeckPicker.get();
-            if (d != null) {
-                d.showProgressBar();
-            }
+        override fun onPreExecute() {
+            super.onPreExecute()
+            val d = deckPicker.get()
+            d?.showProgressBar()
         }
 
-
-
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            DeckPicker d = mDeckPicker.get();
-            if (d == null) {
-                return;
-            }
-
-            d.hideProgressBar();
+        override fun onPostExecute(result: Void?) {
+            super.onPostExecute(result)
+            val d = deckPicker.get() ?: return
+            d.hideProgressBar()
             if (mException != null) {
-                if (mException instanceof OutOfSpaceException) {
-                    d.displayDowngradeFailedNoSpace();
+                if (mException is OutOfSpaceException) {
+                    d.displayDowngradeFailedNoSpace()
                 } else {
-                    d.displayDatabaseFailure();
+                    d.displayDatabaseFailure()
                 }
-                return;
+                return
             }
-
-            Timber.i("Database downgrade successful - starting up");
+            Timber.i("Database downgrade successful - starting up")
             // no exception - continue
-            d.handleStartup();
+            d.handleStartup()
             // This call should probably be in handleStartup - but it's also called there onRefresh
             // TODO: PERF: to fix the above, add test to ensure that this is only called once on each startup path
-            d.refreshState();
+            d.refreshState()
         }
     }
 
+    enum class StartupFailure {
+        SD_CARD_NOT_MOUNTED, DIRECTORY_NOT_ACCESSIBLE, FUTURE_ANKIDROID_VERSION,
 
-    public enum StartupFailure {
-        SD_CARD_NOT_MOUNTED,
-        DIRECTORY_NOT_ACCESSIBLE,
-        FUTURE_ANKIDROID_VERSION,
-        /** A downgrade of the AnkiDroid database is required (and possible) */
-        DATABASE_DOWNGRADE_REQUIRED,
-        DB_ERROR,
-        DATABASE_LOCKED,
-        WEBVIEW_FAILED,
-        ;
+        /** A downgrade of the AnkiDroid database is required (and possible)  */
+        DATABASE_DOWNGRADE_REQUIRED, DB_ERROR, DATABASE_LOCKED, WEBVIEW_FAILED
     }
 }
