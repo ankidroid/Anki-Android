a6c448289456f3cbfed9f1f6ae8358f52a89703f
David Allison <62114487+david-allison-1@users.noreply.github.com>
2022-03-23 20:29:19 +0000

    Convert Threads to Kotlin
    com.ichi2.utils.Threads

--- AnkiDroid/src/main/java/com/ichi2/utils/Threads.kt
+++ AnkiDroid/src/main/java/com/ichi2/utils/Threads.kt
@@ -14,138 +14,117 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-package com.ichi2.utils;
+package com.ichi2.utils
 
-import android.os.Looper;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.UiThread;
-import androidx.annotation.WorkerThread;
-
-import timber.log.Timber;
+import android.os.Looper
+import androidx.annotation.UiThread
+import androidx.annotation.WorkerThread
+import timber.log.Timber
 
 /**
  * Helper class for checking for programming errors while using threads.
  */
-public class Threads {
-
-    private Threads() {
-    }
-
+@KotlinCleanup("many unused methods")
+object Threads {
     /**
-     * An object used to check a thread-access policy.
-     * <p>
-     * It will verify that calls to its {@link #checkThread()} method are done on the right thread.
-     */
-    public interface ThreadChecker {
-
-        /**
-         * Checks that it is called from the right thread and fails otherwise.
-         */
-        void checkThread();
-    }
-
-
-    /**
-     * Creates a {@link ThreadChecker} that validates all access are done on the given thread.
-     * 
+     * Creates a [ThreadChecker] that validates all access are done on the given thread.
+     *
      * @param thread on which accesses should occur
      */
-    public static ThreadChecker newSingleThreadChecker(@NonNull Thread thread) {
-        if (thread == null) {
-            throw new IllegalArgumentException("thread should not be null");
-        }
-        return new SingleThreadChecker(thread);
+    fun newSingleThreadChecker(thread: Thread): ThreadChecker {
+        requireNotNull(thread) { "thread should not be null" }
+        return SingleThreadChecker(thread)
     }
-
 
     /**
-     * Creates a {@link ThreadChecker} that validates all access are done on the calling thread.
+     * Creates a [ThreadChecker] that validates all access are done on the calling thread.
      */
-    public static ThreadChecker newCurrentThreadChecker() {
-        return new SingleThreadChecker(Thread.currentThread());
+    fun newCurrentThreadChecker(): ThreadChecker {
+        return SingleThreadChecker(Thread.currentThread())
     }
-
 
     /**
-     * Creates a {@link ThreadChecker} that validates all access on the same thread, without specifying which thread.
-     * <p>
-     * The thread will be determined by the first call to {@link ThreadChecker#checkThread()} and enforced thereafter.
+     * Creates a [ThreadChecker] that validates all access on the same thread, without specifying which thread.
+     *
+     *
+     * The thread will be determined by the first call to [ThreadChecker.checkThread] and enforced thereafter.
      */
-    public static ThreadChecker newLazySingleThreadChecker() {
-        return new SingleThreadChecker(null);
+    fun newLazySingleThreadChecker(): ThreadChecker {
+        return SingleThreadChecker(null)
     }
-
 
     /**
      * @return true if called from the application main thread
      */
-    public static boolean isOnMainThread() {
-        return Looper.getMainLooper() == Looper.myLooper();
-    }
-
+    val isOnMainThread: Boolean
+        get() = Looper.getMainLooper() == Looper.myLooper()
 
     /**
      * Checks that it is called from the main thread and fails if it is called from another thread.
      */
     @UiThread
-    public static void checkMainThread() {
-        if (!isOnMainThread()) {
-            Timber.e("must be called on the main thread instead of %s", Thread.currentThread());
+    fun checkMainThread() {
+        if (!isOnMainThread) {
+            Timber.e("must be called on the main thread instead of %s", Thread.currentThread())
         }
     }
-
 
     /**
      * Checks that it is not called from the main thread and fails if it is.
      */
     @WorkerThread
-    public static void checkNotMainThread() {
-        if (isOnMainThread()) {
-            Timber.e("must not be called on the main thread");
+    fun checkNotMainThread() {
+        if (isOnMainThread) {
+            Timber.e("must not be called on the main thread")
         }
     }
 
     /**
-     * Helper class to track access from a single thread.
-     * <p>
-     * This class can be used to validate a single-threaded access policy to a class.
-     * <p>
-     * Each method that needs to be called from a single thread can simply call {@link #checkThread()} to validate the
-     * thread it is being called.
+     * An object used to check a thread-access policy.
+     *
+     *
+     * It will verify that calls to its [.checkThread] method are done on the right thread.
      */
-    private static class SingleThreadChecker implements ThreadChecker {
-
-        /** The thread that is allowed access. */
-        private Thread mThread;
-
-
+    interface ThreadChecker {
         /**
-         * Creates a checker for the given thread.
-         * <p>
-         * If passed {@code null}, it will detect the first thread that calls {@link #checkThread()} and make sure all
-         * future accesses are from that thread.
-         * 
-         * @param thread that is allowed access
+         * Checks that it is called from the right thread and fails otherwise.
          */
-        private SingleThreadChecker(Thread thread) {
-            mThread = thread;
-        }
-
-
-        @Override
-        public void checkThread() {
-            // If this the first access and we have not specified a thread, record the current thread.
-            if (mThread == null) {
-                mThread = Thread.currentThread();
-                return;
-            }
-            if (mThread != Thread.currentThread()) {
-                throw new IllegalStateException("must be called from single thread: " + mThread + " instead of "
-                        + Thread.currentThread());
-            }
-        }
-
+        fun checkThread()
     }
 
+    /**
+     * Helper class to track access from a single thread.
+     *
+     *
+     * This class can be used to validate a single-threaded access policy to a class.
+     *
+     *
+     * Each method that needs to be called from a single thread can simply call [.checkThread] to validate the
+     * thread it is being called.
+     */
+    private class SingleThreadChecker
+    /**
+     * Creates a checker for the given thread.
+     *
+     *
+     * If passed `null`, it will detect the first thread that calls [.checkThread] and make sure all
+     * future accesses are from that thread.
+     *
+     * @param thread that is allowed access
+     */
+    constructor(private var thread: Thread?) : ThreadChecker {
+        override fun checkThread() {
+            // If this the first access and we have not specified a thread, record the current thread.
+            if (thread == null) {
+                thread = Thread.currentThread()
+                return
+            }
+            check(!(thread !== Thread.currentThread())) {
+                (
+                    "must be called from single thread: " + thread + " instead of " +
+                        Thread.currentThread()
+                    )
+            }
+        }
+    }
 }
