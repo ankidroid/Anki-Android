7bde2c1954f4113d6afb998e144c25b5ca7db3bb
Prince kushwaha <kushwahaprince418@gmail.com>
2022-03-30 17:43:01 +0530

    kotlinclearupFile-> rename val to value,make name non-null

--- AnkiDroid/src/main/java/com/ichi2/upgrade/Upgrade.kt
+++ AnkiDroid/src/main/java/com/ichi2/upgrade/Upgrade.kt
@@ -3,27 +3,25 @@ package com.ichi2.upgrade
 
 import com.ichi2.libanki.Collection
 import com.ichi2.utils.JSONException
-import com.ichi2.utils.KotlinCleanup
 import timber.log.Timber
 
 object Upgrade {
     @JvmStatic
-    @KotlinCleanup("make name non-null + rename `val` variable")
-    fun upgradeJSONIfNecessary(col: Collection, name: String?, defaultValue: Boolean): Boolean {
-        var `val` = defaultValue
+    fun upgradeJSONIfNecessary(col: Collection, name: String, defaultValue: Boolean): Boolean {
+        var value = defaultValue
         try {
-            `val` = col.get_config_boolean(name!!)
+            value = col.get_config_boolean(name)
         } catch (e: JSONException) {
             Timber.w(e)
             // workaround to repair wrong values from older libanki versions
             try {
-                col.set_config(name!!, `val`)
+                col.set_config(name, value)
             } catch (e1: JSONException) {
                 Timber.w(e1)
                 // do nothing
             }
             col.save()
         }
-        return `val`
+        return value
     }
 }
