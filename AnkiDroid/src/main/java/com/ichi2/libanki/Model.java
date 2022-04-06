73903a93bfadbe64a3e0945a0e0b2fb19ae026e3
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-06 13:13:04 +0100

    refactor: add newline
    Review comment

--- AnkiDroid/src/main/java/com/ichi2/libanki/Model.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/Model.java
@@ -36,7 +36,9 @@ import timber.log.Timber;
  * Represents a note type, a.k.a. Model.
  * The content of an object is described in https://github.com/ankidroid/Anki-Android/wiki/Database-Structure
  * Each time the object is modified, `Models.save(this)` should be called, otherwise the change will not be synchronized
- * If a change affect card generation, (i.e. any change on the list of field, or the question side of a card type), `Models.save(this, true)` should be called. However, you should do the change in batch and change only when all are done, because recomputing the list of card is an expensive operation.
+ * If a change affect card generation, (i.e. any change on the list of field, or the question side of a card type),
+ * `Models.save(this, true)` should be called. However, you should do the change in batch and change only when all are d
+ * one, because recomputing the list of card is an expensive operation.
  */
 public class Model extends JSONObject {
     /**
