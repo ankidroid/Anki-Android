45d4b726cd9f1d0b39592e6e886ee18adc7cbf04
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-04-08 09:44:03 -0500

    Rename the Reviews shortcut option to Study for clarity

--- AnkiDroid/src/main/java/com/ichi2/anki/NavigationDrawerActivity.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/NavigationDrawerActivity.java
@@ -188,8 +188,8 @@ public abstract class NavigationDrawerActivity extends AnkiActivity implements N
         intentReviewCards.setAction(Intent.ACTION_VIEW);
         intentReviewCards.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         ShortcutInfo reviewCardsShortcut = new ShortcutInfo.Builder(context, "reviewCardsShortcutId")
-                .setShortLabel(context.getString(R.string.card_info_reviews))
-                .setLongLabel(context.getString(R.string.card_info_reviews))
+                .setShortLabel(context.getString(R.string.studyoptions_start))
+                .setLongLabel(context.getString(R.string.studyoptions_start))
                 .setIcon(Icon.createWithResource(context, R.drawable.ankidroid_logo))
                 .setIntent(intentReviewCards)
                 .build();
