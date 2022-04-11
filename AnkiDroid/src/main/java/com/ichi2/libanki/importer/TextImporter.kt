144dc2950d1c1377338d4ba643f1f78e79c8b321
Arthur Milchior <arthur@milchior.fr>
2022-04-11 07:51:43 +0200

    Migrate AnkiDroid/src/main/java/com/ichi2/libanki/importer/TextImporter.kt to Kotlin

--- AnkiDroid/src/main/java/com/ichi2/libanki/importer/TextImporter.kt
+++ AnkiDroid/src/main/java/com/ichi2/libanki/importer/TextImporter.kt
@@ -1,105 +1,80 @@
 //noinspection MissingCopyrightHeader #8659
-package com.ichi2.libanki.importer;
+package com.ichi2.libanki.importer
 
-import android.os.Build;
-import android.text.TextUtils;
-
-import com.ichi2.anki.R;
-import com.ichi2.libanki.Collection;
-import com.ichi2.libanki.importer.python.CsvDialect;
-import com.ichi2.libanki.importer.python.CsvReader;
-import com.ichi2.libanki.importer.python.CsvSniffer;
-
-import org.jetbrains.annotations.Contract;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Optional;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import timber.log.Timber;
+import android.os.Build
+import android.text.TextUtils
+import androidx.annotation.RequiresApi
+import com.ichi2.anki.R
+import com.ichi2.libanki.Collection
+import com.ichi2.libanki.importer.python.CsvDialect
+import com.ichi2.libanki.importer.python.CsvReader
+import com.ichi2.libanki.importer.python.CsvReader.Companion.fromDelimiter
+import com.ichi2.libanki.importer.python.CsvReader.Companion.fromDialect
+import com.ichi2.libanki.importer.python.CsvSniffer
+import org.jetbrains.annotations.Contract
+import timber.log.Timber
+import java.io.File
+import java.io.IOException
+import java.io.UncheckedIOException
+import java.lang.Exception
+import java.lang.RuntimeException
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.*
+import java.util.regex.Pattern
+import java.util.stream.Collectors
+import java.util.stream.Stream
+import kotlin.Throws
 
 @RequiresApi(api = Build.VERSION_CODES.O)
-public class TextImporter extends NoteImporter {
-
-    private boolean mNeedDelimiter = true;
-    final String mPatterns = "\t|,;:";
-
-    private FileObj mFileobj;
-    private char mDelimiter;
-    private String[] mTagstoadd;
-
-    private CsvDialect mDialect;
-    private int mNumFields;
-
-
-    private boolean mFirstLineWasTags;
-
-
-    public TextImporter(Collection col, @NonNull String file) {
-        super(col, file);
-        mFileobj = null;
-        mDelimiter = '\0';
-        mTagstoadd = new String[0];
-    }
-
-
-    @NonNull
-    @Override
-    protected List<ForeignNote> foreignNotes() {
-        open();
+class TextImporter(col: Collection?, file: String) : NoteImporter(col!!, file) {
+    val mPatterns = "\t|,;:"
+    private var mFileobj: FileObj? = null
+    private var mDelimiter = '\u0000'
+    private var mTagstoadd: Array<String>
+    private var mDialect: CsvDialect? = null
+    private var mNumFields = 0
+    private var mFirstLineWasTags = false
+    override fun foreignNotes(): List<ForeignNote> {
+        open()
         // process all lines
-        List<String> log = new ArrayList<>(); //Number of element is reader's size
-        List<ForeignNote> notes = new ArrayList<>(); //Number of element is reader's size
-        int lineNum = 0;
-        int ignored = 0;
+        val log: MutableList<String> = ArrayList() // Number of element is reader's size
+        val notes: MutableList<ForeignNote> = ArrayList() // Number of element is reader's size
+        var ignored = 0
         // Note: This differs from libAnki as we don't have csv.reader
-        Iterator<String> data = getDataStream().iterator();
-        CsvReader reader;
-        if (mDelimiter != '\0') {
-            reader = CsvReader.fromDelimiter(data, mDelimiter);
+        val data = dataStream.iterator() as Iterator<String>
+        val reader: CsvReader
+        reader = if (mDelimiter != '\u0000') {
+            fromDelimiter(data, mDelimiter)
         } else {
-            reader = CsvReader.fromDialect(data, mDialect);
+            fromDialect(data, mDialect!!)
         }
         try {
-            for (List<? extends String> row : reader) {
-                if (row == null) {
-                    continue;
-                }
-                List<String> rowAsString = new ArrayList<>(row);
-                if (rowAsString.size() != mNumFields) {
+            for (row in reader) {
+                val rowAsString: List<String?> = ArrayList(row)
+                if (rowAsString.size != mNumFields) {
                     if (!rowAsString.isEmpty()) {
-                        String formatted = getString(R.string.csv_importer_error_invalid_field_count,
-                                TextUtils.join(" ", rowAsString),
-                                rowAsString.size(),
-                                mNumFields);
-                        log.add(formatted);
-                        ignored += 1;
-
+                        val formatted = getString(
+                            R.string.csv_importer_error_invalid_field_count,
+                            TextUtils.join(" ", rowAsString),
+                            rowAsString.size,
+                            mNumFields
+                        )
+                        log.add(formatted)
+                        ignored += 1
                     }
-                    continue;
+                    continue
                 }
-                ForeignNote note = noteFromFields(rowAsString);
-                notes.add(note);
+                val note = noteFromFields(rowAsString)
+                notes.add(note)
             }
-        } catch (CsvException e) {
-            log.add(getString(R.string.csv_importer_error_exception, e));
+        } catch (e: CsvException) {
+            log.add(getString(R.string.csv_importer_error_exception, e))
         }
-        mLog = log;
-        mFileobj.close();
-        return notes;
+        mLog = log
+        mFileobj!!.close()
+        return notes
     }
 
     /**
@@ -107,121 +82,170 @@ public class TextImporter extends NoteImporter {
      * @throws UnknownDelimiterException Could not determine delimiter (example: empty file)
      * @throws EncodingException Non-UTF file (for example: an image)
      */
-    @Override
-    public int fields() {
-        open();
-        return mNumFields;
+    override fun fields(): Int {
+        open()
+        return mNumFields
     }
 
-
-    private ForeignNote noteFromFields(List<String> fields) {
-        ForeignNote note = new ForeignNote();
-        note.mFields.addAll(fields);
-        note.mTags.addAll(Arrays.asList(mTagstoadd));
-        return note;
+    private fun noteFromFields(fields: List<String?>): ForeignNote {
+        val note = ForeignNote()
+        note.mFields.addAll(fields)
+        note.mTags.addAll(mTagstoadd.asList())
+        return note
     }
 
-
-    /** Parse the top line and determine the pattern and number of fields. */
-    @Override
-    protected void open() {
+    /** Parse the top line and determine the pattern and number of fields.  */
+    override fun open() {
         // load & look for the right pattern
-        cacheFile();
+        cacheFile()
     }
 
-    /** Read file into self.lines if not already there. */
-    private void cacheFile() {
+    /** Read file into self.lines if not already there.  */
+    private fun cacheFile() {
         if (mFileobj == null) {
-            openFile();
+            openFile()
         }
     }
 
-
-    private void openFile() {
-        mDialect = null;
-        mFileobj = FileObj.open(mFile);
-
-        String firstLine = getFirstFileLine().orElse(null);
+    private fun openFile() {
+        mDialect = null
+        mFileobj = FileObj.open(mFile)
+        val firstLine = firstFileLine.orElse(null)
         if (firstLine != null) {
             if (firstLine.startsWith("tags:")) {
-                String tags = firstLine.substring("tags:".length()).trim();
-                mTagstoadd = tags.split(" ");
-                this.mFirstLineWasTags = true;
+                val tags = firstLine.substring("tags:".length).trim { it <= ' ' }
+                mTagstoadd = tags.split(" ".toRegex()).toTypedArray()
+                mFirstLineWasTags = true
             }
-            updateDelimiter();
+            updateDelimiter()
         }
-
-        if (mDialect == null && mDelimiter == '\0') {
-            throw new UnknownDelimiterException();
+        if (mDialect == null && mDelimiter == '\u0000') {
+            throw UnknownDelimiterException()
         }
     }
 
     @Contract(" -> fail")
-    private void err() {
-        throw new UnknownDelimiterException();
+    private fun err() {
+        throw UnknownDelimiterException()
     }
 
-    private void updateDelimiter() {
-        mDialect = null;
-        CsvSniffer sniffer = new CsvSniffer();
-        if (mDelimiter == '\0') {
+    private fun updateDelimiter() {
+        mDialect = null
+        val sniffer = CsvSniffer()
+        if (mDelimiter == '\u0000') {
             try {
-                String join = getLinesFromFile(10);
-                mDialect = sniffer.sniff(join, mPatterns.toCharArray());
-            } catch (Exception e) {
+                val join = getLinesFromFile(10)
+                mDialect = sniffer.sniff(join, mPatterns.toCharArray())
+            } catch (e: Exception) {
                 // expected: do not log the exception
                 try {
-                    mDialect = sniffer.sniff(getFirstFileLine().orElse(""), mPatterns.toCharArray());
-                } catch (Exception ex) {
+                    mDialect = sniffer.sniff(firstFileLine.orElse(""), mPatterns.toCharArray())
+                } catch (ex: Exception) {
                     // expected and ignored: do not log the exception
                 }
             }
         }
-
-        Iterator<String> data = getDataStream().iterator();
-
-        CsvReader reader = null;
+        val data = dataStream.iterator()
+        var reader: CsvReader? = null
         if (mDialect != null) {
             try {
-                reader = CsvReader.fromDialect(data, mDialect);
-            } catch (Exception e) {
+                reader = fromDialect(data, mDialect!!)
+            } catch (e: Exception) {
                 // expected and ignored: do not log the exception
-                err();
+                err()
             }
         } else {
             // PERF: This starts the file read twice - whereas we only need the first line
-            String firstLine = getFirstFileLine().orElse("");
-            if (mDelimiter == '\0') {
-                if (firstLine.contains("\t")) {
-                    mDelimiter = '\t';
-                } else if(firstLine.contains(";")) {
-                    mDelimiter = ';';
-                } else if(firstLine.contains(",")) {
-                    mDelimiter = ',';
+            val firstLine = firstFileLine.orElse("")
+            if (mDelimiter == '\u0000') {
+                mDelimiter = if (firstLine!!.contains("\t")) {
+                    '\t'
+                } else if (firstLine.contains(";")) {
+                    ';'
+                } else if (firstLine.contains(",")) {
+                    ','
                 } else {
-                    mDelimiter = ' ';
+                    ' '
                 }
             }
-            reader = CsvReader.fromDelimiter(data, mDelimiter);
+            reader = fromDelimiter(data, mDelimiter)
         }
-
         try {
             while (true) {
-                List<String> row = reader.next();
+                val row = reader!!.next()
                 if (!row.isEmpty()) {
-                    mNumFields = row.size();
-                    break;
+                    mNumFields = row.size
+                    break
                 }
             }
-        } catch (Exception e) {
-            Timber.e(e);
-            err();
+        } catch (e: Exception) {
+            Timber.e(e)
+            err()
         }
-        initMapping();
+        initMapping()
     }
 
+    private fun sub(s: String): String {
+        return COMMENT_PATTERN.matcher(s).replaceAll("__comment")
+    }
 
-    /*
+    private val dataStream: Stream<String>
+        get() {
+            val data: Stream<String> = try {
+                mFileobj!!.readAsUtf8WithoutBOM()
+            } catch (e: IOException) {
+                throw EncodingException(e)
+            }
+            var withoutComments = data.filter { x: String -> "__comment" != sub(x) }.map { s: String ->
+                """
+     $s
+     
+                """.trimIndent()
+            }
+            if (mFirstLineWasTags) {
+                withoutComments = withoutComments.skip(1)
+            }
+            return withoutComments
+        }
+    private val firstFileLine: Optional<String?>
+        get() = try {
+            dataStream.findFirst()
+        } catch (e: UncheckedIOException) {
+            throw EncodingException(e)
+        }
+
+    private fun getLinesFromFile(numberOfLines: Int): String {
+        return try {
+            TextUtils.join("\n", dataStream.limit(numberOfLines.toLong()).collect(Collectors.toList()))
+        } catch (e: UncheckedIOException) {
+            throw EncodingException(e)
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private class FileObj(private val file: File) {
+        fun close() {
+            // No need for anything - closed in the read
+        }
+
+        @Throws(IOException::class)
+        fun readAsUtf8WithoutBOM(): Stream<String> {
+            return Files.lines(Paths.get(file.absolutePath), StandardCharsets.UTF_8)
+        }
+
+        companion object {
+            fun open(file: String): FileObj {
+                return FileObj(File(file))
+            }
+        }
+    }
+
+    class UnknownDelimiterException : RuntimeException("unknownFormat")
+
+    /** Non-UTF file was provided (for example: an image)  */
+    class EncodingException(e: Exception?) : RuntimeException(e)
+    companion object {
+        /*
     In python:
     >>> pp(re.sub("^\#.*$", "__comment", "#\r\n"))
     '__comment\n'
@@ -229,85 +253,10 @@ public class TextImporter extends NoteImporter {
     COMMENT_PATTERN.matcher("#\r\n").replaceAll("__comment") -> "__comment\r\n"
     So we use .DOTALL to ensure we get the \r
     */
-    private static final Pattern COMMENT_PATTERN = Pattern.compile("^#.*$", Pattern.DOTALL);
-
-    private String sub(String s) {
-        return COMMENT_PATTERN.matcher(s).replaceAll("__comment");
+        private val COMMENT_PATTERN = Pattern.compile("^#.*$", Pattern.DOTALL)
     }
 
-
-    private Stream<String> getDataStream() {
-        Stream<String> data;
-        try {
-            data = mFileobj.readAsUtf8WithoutBOM();
-        } catch (IOException e) {
-            throw new EncodingException(e);
-        }
-
-        Stream<String> withoutComments = data.filter(x -> !"__comment".equals(sub(x))).map(s -> s + "\n");
-        if (this.mFirstLineWasTags) {
-            withoutComments = withoutComments.skip(1);
-        }
-        return withoutComments;
-    }
-
-
-    private Optional<String> getFirstFileLine() {
-        try {
-            return getDataStream().findFirst();
-        } catch (UncheckedIOException e) {
-            throw new EncodingException(e);
-        }
-    }
-
-
-    private String getLinesFromFile(int numberOfLines) {
-        try {
-            return TextUtils.join("\n", getDataStream().limit(numberOfLines).collect(Collectors.toList()));
-        } catch (UncheckedIOException e) {
-            throw new EncodingException(e);
-        }
-    }
-
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private static class FileObj {
-
-        private final File mFile;
-
-
-        public FileObj(@NonNull File file) {
-            this.mFile = file;
-        }
-
-
-        @NonNull
-        public static FileObj open(@NonNull String file) {
-            return new FileObj(new File(file));
-        }
-
-
-        public void close() {
-            // No need for anything - closed in the read
-        }
-
-
-        @NonNull
-        public Stream<String> readAsUtf8WithoutBOM() throws IOException {
-            return Files.lines(Paths.get(mFile.getAbsolutePath()), StandardCharsets.UTF_8);
-        }
-    }
-
-    public static class UnknownDelimiterException extends RuntimeException {
-        public UnknownDelimiterException() {
-            super("unknownFormat");
-        }
-    }
-
-    /** Non-UTF file was provided (for example: an image) */
-    public static class EncodingException extends RuntimeException {
-        public EncodingException(Exception e) {
-            super(e);
-        }
+    init {
+        mTagstoadd = emptyArray()
     }
 }
