e1487869d3c157983e53228f34c4241d26cc6b5d
Arthur Milchior <arthur@milchior.fr>
2022-04-12 08:41:03 +0200

    Convert AbstractDeckTreeNode.kt to Kotlin
    com.ichi2.libanki.sched.AbstractDeckTreeNode

--- AnkiDroid/src/main/java/com/ichi2/libanki/sched/AbstractDeckTreeNode.kt
+++ AnkiDroid/src/main/java/com/ichi2/libanki/sched/AbstractDeckTreeNode.kt
@@ -13,17 +13,12 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.libanki.sched
 
-package com.ichi2.libanki.sched;
-
-import com.ichi2.libanki.Collection;
-import com.ichi2.libanki.Decks;
-
-import java.util.List;
-import java.util.Locale;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import com.ichi2.libanki.Collection
+import com.ichi2.libanki.Decks
+import java.lang.UnsupportedOperationException
+import java.util.*
 
 /**
  * Holds the data for a single node (row) in the deck due tree (the user-visible list
@@ -37,80 +32,77 @@ import androidx.annotation.Nullable;
  *
  * T represents the type of children. Required for typing purpose only.
  */
-public abstract class AbstractDeckTreeNode<T extends AbstractDeckTreeNode<T>> implements Comparable<AbstractDeckTreeNode<T>> {
-    private final String mName;
-    private final String[] mNameComponents;
-    private final Collection mCol;
-    private final long mDid;
-    @Nullable
-    private List<T> mChildren = null;
+abstract class AbstractDeckTreeNode<T : AbstractDeckTreeNode<T>?>(
+    val col: Collection,
+    /**
+     * @return The full deck name, e.g. "A::B::C"
+     */
+    val fullDeckName: String,
+    val did: Long
+) : Comparable<AbstractDeckTreeNode<T>> {
+    private val mNameComponents: Array<String>
 
-    public AbstractDeckTreeNode(Collection col, String name, long did) {
-        this.mCol = col;
-        this.mName = name;
-        this.mDid = did;
-        this.mNameComponents = Decks.path(name);
-    }
+    /**
+     * @return The children of this deck. Note that they are set
+     * in the data structure returned by DeckDueTree but are
+     * always empty when the data structure is returned by
+     * deckDueList.
+     */
+    var children: List<T>? = null
+        private set
 
     /**
      * Sort on the head of the node.
      */
-    @Override
-    public int compareTo(AbstractDeckTreeNode<T> rhs) {
-        int minDepth = Math.min(getDepth(), rhs.getDepth()) + 1;
+    override fun compareTo(other: AbstractDeckTreeNode<T>): Int {
+        val minDepth = Math.min(depth, other.depth) + 1
         // Consider each subdeck name in the ordering
-        for (int i = 0; i < minDepth; i++) {
-            int cmp = mNameComponents[i].compareTo(rhs.mNameComponents[i]);
+        for (i in 0 until minDepth) {
+            val cmp = mNameComponents[i].compareTo(other.mNameComponents[i])
             if (cmp == 0) {
-                continue;
+                continue
             }
-            return cmp;
+            return cmp
         }
         // If we made it this far then the arrays are of different length. The longer one should
         // always come after since it contains all of the sections of the shorter one inside it
         // (i.e., the short one is an ancestor of the longer one).
-        return Integer.compare(getDepth(), rhs.getDepth());
+        return Integer.compare(depth, other.depth)
     }
 
-    /** Line representing this string without its children. Used in timbers only. */
-    protected String toStringLine() {
-        return String.format(Locale.US, "%s, %d, %s",
-                             mName, mDid, mChildren);
+    /** Line representing this string without its children. Used in timbers only.  */
+    protected open fun toStringLine(): String? {
+        return String.format(
+            Locale.US, "%s, %d, %s",
+            fullDeckName, did, children
+        )
     }
 
-    @Override
-    public @NonNull String toString() {
-        StringBuffer buf = new StringBuffer();
-        toString(buf);
-        return buf.toString();
+    override fun toString(): String {
+        val buf = StringBuffer()
+        toString(buf)
+        return buf.toString()
     }
 
-    protected void toString(StringBuffer buf) {
-        for (int i = 0; i < getDepth(); i++ ) {
-            buf.append("  ");
+    protected fun toString(buf: StringBuffer) {
+        for (i in 0 until depth) {
+            buf.append("  ")
         }
-        buf.append(toStringLine());
-        if (mChildren == null) {
-            return;
+        buf.append(toStringLine())
+        if (children == null) {
+            return
         }
-        for (T children : mChildren) {
-            children.toString(buf);
+        for (children in children!!) {
+            children!!.toString(buf)
         }
-    }
-
-    /**
-     * @return The full deck name, e.g. "A::B::C"
-     * */
-    public String getFullDeckName() {
-        return mName;
     }
 
     /**
      * For deck "A::B::C", `getDeckNameComponent(0)` returns "A",
      * `getDeckNameComponent(1)` returns "B", etc...
      */
-    public String getDeckNameComponent(int part) {
-        return mNameComponents[part];
+    fun getDeckNameComponent(part: Int): String {
+        return mNameComponents[part]
     }
 
     /**
@@ -118,95 +110,78 @@ public abstract class AbstractDeckTreeNode<T extends AbstractDeckTreeNode<T>> im
      * part that does not belong to its parents. E.g.  for deck
      * "A::B::C", returns "C".
      */
-    public String getLastDeckNameComponent() {
-        return getDeckNameComponent(getDepth());
-    }
-
-    public long getDid() {
-        return mDid;
-    }
+    val lastDeckNameComponent: String
+        get() = getDeckNameComponent(depth)
 
     /**
      * @return The depth of a deck. Top level decks have depth 0,
      * their children have depth 1, etc... So "A::B::C" would have
      * depth 2.
      */
-    public int getDepth() {
-        return mNameComponents.length - 1;
-    }
+    val depth: Int
+        get() = mNameComponents.size - 1
 
     /**
-     * @return The children of this deck. Note that they are set
-     * in the data structure returned by DeckDueTree but are
-     * always empty when the data structure is returned by
-     * deckDueList.*/
-    public List<T> getChildren() {
-        return mChildren;
+     * @return whether this node as any children.
+     */
+    fun hasChildren(): Boolean {
+        return children != null && !children!!.isEmpty()
     }
 
-    /**
-     * @return whether this node as any children. */
-    public boolean hasChildren() {
-        return mChildren != null && !mChildren.isEmpty();
-    }
-
-    public void setChildren(@NonNull List<T> children, boolean addRev) {
+    open fun setChildren(children: List<T>, addRev: Boolean) {
         // addRev present here because it needs to be overridden
-        mChildren = children;
+        this.children = children
     }
 
-    @Override
-    public int hashCode() {
-        int childrenHash = mChildren == null ? 0 : mChildren.hashCode();
-        return getFullDeckName().hashCode() + childrenHash;
+    override fun hashCode(): Int {
+        val childrenHash = if (children == null) 0 else children.hashCode()
+        return fullDeckName.hashCode() + childrenHash
     }
-
 
     /**
      * Whether both elements have the same structure and numbers.
-     * @param object
+     * @param other
      * @return
      */
-    @Override
-    public boolean equals(Object object) {
-        if (!(object instanceof AbstractDeckTreeNode)) {
-            return false;
+    override fun equals(other: Any?): Boolean {
+        if (other !is AbstractDeckTreeNode<*>) {
+            return false
         }
-        AbstractDeckTreeNode<?> tree = (AbstractDeckTreeNode) object;
-        return Decks.equalName(getFullDeckName(), tree.getFullDeckName()) &&
-            (mChildren == null && tree.mChildren == null) || // Would be the case if both are null, or the same pointer
-            (mChildren != null && mChildren.equals(tree.mChildren))
-            ;
+        val tree = other
+        return Decks.equalName(fullDeckName, tree.fullDeckName) &&
+            children == null && tree.children == null || // Would be the case if both are null, or the same pointer
+            children != null && children == tree.children
     }
 
-    public Collection getCol() {
-        return mCol;
-    }
-
-    public boolean shouldDisplayCounts() {
-        return false;
+    open fun shouldDisplayCounts(): Boolean {
+        return false
     }
 
     /* Number of new cards to see today known to be in this deck and its descendants. The number to show to user*/
-    public int getNewCount() {
-        throw new UnsupportedOperationException();
-    }
+    open var newCount: Int = 0
+        get() {
+            throw UnsupportedOperationException()
+        }
 
     /* Number of lrn cards (or repetition) to see today known to be in this deck and its descendants. The number to show to user*/
-    public int getLrnCount() {
-        throw new UnsupportedOperationException();
-    }
+    open var lrnCount: Int = 0
+        get() {
+            throw UnsupportedOperationException()
+        }
 
     /* Number of rev cards to see today known to be in this deck and its descendants. The number to show to user*/
-    public int getRevCount() {
-        throw new UnsupportedOperationException();
+    open var revCount: Int = 0
+        get() {
+            throw UnsupportedOperationException()
+        }
+
+    open fun knownToHaveRep(): Boolean {
+        return false
     }
 
+    abstract fun withChildren(children: List<T>): T
 
-    public boolean knownToHaveRep() {
-        return false;
+    init {
+        mNameComponents = Decks.path(fullDeckName)
     }
-
-
-    public abstract T withChildren(@NonNull List<T> children);
 }
