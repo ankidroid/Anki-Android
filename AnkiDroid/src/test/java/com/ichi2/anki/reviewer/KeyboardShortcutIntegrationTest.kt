aa35c2a78f4c5993e8801394d21c12869c4d06a2
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-13 15:06:18 +0100

    refactor: convert KeyboardShortcutIntegrationTest to Kotlin
    com.ichi2.anki.reviewer.KeyboardShortcutIntegrationTest

--- AnkiDroid/src/test/java/com/ichi2/anki/reviewer/KeyboardShortcutIntegrationTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/reviewer/KeyboardShortcutIntegrationTest.kt
@@ -13,58 +13,47 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.anki.reviewer
 
-package com.ichi2.anki.reviewer;
+import android.Manifest
+import android.app.Application
+import android.content.Context
+import android.content.Intent
+import android.view.KeyEvent
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.anki.Reviewer
+import com.ichi2.anki.RobolectricTest
+import com.ichi2.anki.multimediacard.AudioPlayer
+import com.ichi2.anki.multimediacard.AudioRecorder
+import com.ichi2.anki.multimediacard.AudioView
+import com.ichi2.testutils.KeyEventUtils.Companion.getVKey
+import com.ichi2.utils.KotlinCleanup
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.*
+import org.mockito.kotlin.any
+import org.robolectric.Shadows
+import java.io.IOException
 
-import android.Manifest;
-import android.app.Application;
-import android.content.Intent;
-import android.view.KeyEvent;
-
-import com.ichi2.anki.Reviewer;
-import com.ichi2.anki.RobolectricTest;
-import com.ichi2.anki.multimediacard.AudioPlayer;
-import com.ichi2.anki.multimediacard.AudioRecorder;
-import com.ichi2.anki.multimediacard.AudioView;
-import com.ichi2.testutils.KeyEventUtils;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.Shadows;
-
-import java.io.IOException;
-
-import androidx.test.core.app.ApplicationProvider;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
-
-@RunWith(AndroidJUnit4.class)
-public class KeyboardShortcutIntegrationTest extends RobolectricTest {
-
-    private Reviewer mReviewer;
-
-
+@RunWith(AndroidJUnit4::class)
+@KotlinCleanup("auto IDE fixes")
+@KotlinCleanup("`is` -> equalTo")
+@KotlinCleanup("`when` -> whenever")
+class KeyboardShortcutIntegrationTest : RobolectricTest() {
+    @KotlinCleanup("lateinit")
+    private var mReviewer: Reviewer? = null
     @Before
-    @Override
-    public void setUp() {
-        super.setUp();
-        addNoteUsingBasicModel("Hello", "World");
-        Shadows.shadowOf((Application) ApplicationProvider.getApplicationContext()).grantPermissions(Manifest.permission.RECORD_AUDIO);
-        mReviewer = super.startActivityNormallyOpenCollectionWithIntent(Reviewer.class, new Intent());
-        waitForAsyncTasksToComplete();
+    override fun setUp() {
+        super.setUp()
+        addNoteUsingBasicModel("Hello", "World")
+        Shadows.shadowOf(ApplicationProvider.getApplicationContext<Context>() as Application).grantPermissions(Manifest.permission.RECORD_AUDIO)
+        mReviewer = super.startActivityNormallyOpenCollectionWithIntent(Reviewer::class.java, Intent())
+        waitForAsyncTasksToComplete()
     }
-
 
     /**
      * Press "Shift + V"
@@ -74,13 +63,14 @@ public class KeyboardShortcutIntegrationTest extends RobolectricTest {
      * #7780
      */
     @Test
-    public void testActionsOccurOnKeyDown() throws IOException {
-        AudioRecorder recorder = setupRecorderMock();
+    @Throws(IOException::class)
+    fun testActionsOccurOnKeyDown() {
+        val recorder = setupRecorderMock()
 
-        pressShiftAndVThenRelease();
+        pressShiftAndVThenRelease()
 
-        verify(recorder, times(1)).startRecording(any(), anyString());
-        verifyNoMoreInteractions(recorder);
+        verify(recorder, times(1)).startRecording(any(), anyString())
+        verifyNoMoreInteractions(recorder)
     }
 
     /**
@@ -90,15 +80,15 @@ public class KeyboardShortcutIntegrationTest extends RobolectricTest {
      * Expected: Recording is triggered and is not untriggered
      */
     @Test
-    public void holdIsNotASecondAction() throws IOException {
-        AudioRecorder recorder = setupRecorderMock();
+    @Throws(IOException::class)
+    fun holdIsNotASecondAction() {
+        val recorder = setupRecorderMock()
 
-        pressAndHoldShiftV();
+        pressAndHoldShiftV()
 
-        verify(recorder, times(1)).startRecording(any(), anyString());
-        verifyNoMoreInteractions(recorder);
+        verify(recorder, times(1)).startRecording(any(), anyString())
+        verifyNoMoreInteractions(recorder)
     }
-
 
     /**
      * Press "Shift + V"
@@ -108,26 +98,25 @@ public class KeyboardShortcutIntegrationTest extends RobolectricTest {
      * #7780
      */
     @Test
-    public void playingWhileRecordingStopsRecordingAndStartsPlaying() throws IOException {
-        AudioPlayer player = setupPlayerMock();
-        AudioRecorder recorder = setupRecorderMock();
+    @Throws(IOException::class)
+    fun playingWhileRecordingStopsRecordingAndStartsPlaying() {
+        val player = setupPlayerMock()
+        val recorder = setupRecorderMock()
 
-        pressShiftAndVThenRelease();
+        pressShiftAndVThenRelease()
 
-        verify(recorder, times(1)).startRecording(any(), anyString());
-        verifyNoMoreInteractions(recorder, player);
-        assertStatus(AudioView.Status.RECORDING);
+        verify(recorder, times(1)).startRecording(any(), anyString())
+        verifyNoMoreInteractions(recorder, player)
+        assertStatus(AudioView.Status.RECORDING)
 
         // now we're set up and recording, pressing V should move us to playing
+        pressVThenRelease()
 
-        pressVThenRelease();
-
-        verify(recorder, times(1)).stopRecording();
-        verify(player, times(1)).play(anyString());
-        verifyNoMoreInteractions(recorder, player);
-        assertStatus(AudioView.Status.PLAYING);
+        verify(recorder, times(1)).stopRecording()
+        verify(player, times(1)).play(anyString())
+        verifyNoMoreInteractions(recorder, player)
+        assertStatus(AudioView.Status.PLAYING)
     }
-
 
     /**
      * Press " V"
@@ -137,122 +126,111 @@ public class KeyboardShortcutIntegrationTest extends RobolectricTest {
      * #7780
      */
     @Test
-    public void recordingWhilePlayingStopsPlayingAndStartsRecording() throws IOException {
-        Shadows.shadowOf((Application) ApplicationProvider.getApplicationContext()).grantPermissions(Manifest.permission.RECORD_AUDIO);
+    @Throws(IOException::class)
+    fun recordingWhilePlayingStopsPlayingAndStartsRecording() {
+        Shadows.shadowOf(ApplicationProvider.getApplicationContext<Context>() as Application).grantPermissions(Manifest.permission.RECORD_AUDIO)
 
-        AudioPlayer player = setupPlayerMock();
-        AudioRecorder recorder = setupRecorderMock();
+        val player = setupPlayerMock()
+        val recorder = setupRecorderMock()
 
         // We don't need anything recorded as the mock doesn't mind
-        pressVThenRelease();
+        pressVThenRelease()
 
-        verify(player, times(1)).play(anyString());
-        verifyNoMoreInteractions(recorder, player);
-        assertStatus(AudioView.Status.PLAYING);
+        verify(player, times(1)).play(anyString())
+        verifyNoMoreInteractions(recorder, player)
+        assertStatus(AudioView.Status.PLAYING)
 
         // now we're set up and recording, pressing Shift + V should move us to recording
-        pressShiftAndVThenRelease();
+        pressShiftAndVThenRelease()
 
-        verify(player, times(1)).stop();
-        verify(recorder, times(1)).startRecording(any(), anyString());
-        verifyNoMoreInteractions(recorder, player);
-        assertStatus(AudioView.Status.RECORDING);
-
+        verify(player, times(1)).stop()
+        verify(recorder, times(1)).startRecording(any(), anyString())
+        verifyNoMoreInteractions(recorder, player)
+        assertStatus(AudioView.Status.RECORDING)
     }
 
-
-    protected void assertStatus(AudioView.Status recording) {
-        assertThat(mReviewer.getAudioView().getStatus(), is(recording));
+    protected fun assertStatus(recording: AudioView.Status) {
+        assertThat(mReviewer!!.audioView!!.status, `is`(recording))
     }
 
-
-    protected AudioPlayer setupPlayerMock() {
-        assertThat(mReviewer.openMicToolbar(), is(true));
-        AudioPlayer player = mock(AudioPlayer.class);
-        mReviewer.getAudioView().setPlayer(player);
-        return player;
+    @KotlinCleanup("scope function")
+    protected fun setupPlayerMock(): AudioPlayer {
+        assertThat(mReviewer!!.openMicToolbar(), `is`(true))
+        val player = mock(AudioPlayer::class.java)
+        mReviewer!!.audioView!!.setPlayer(player)
+        return player
     }
 
-
-    protected AudioRecorder setupRecorderMock() {
-        assertThat(mReviewer.openMicToolbar(), is(true));
-        AudioRecorder recorder = mock(AudioRecorder.class);
-        mReviewer.getAudioView().setRecorder(recorder);
-        return recorder;
+    protected fun setupRecorderMock(): AudioRecorder {
+        assertThat(mReviewer!!.openMicToolbar(), `is`(true))
+        val recorder = mock(AudioRecorder::class.java)
+        mReviewer!!.audioView!!.setRecorder(recorder)
+        return recorder
     }
 
-
-    private void pressVThenRelease() {
-        depressVKey();
-        releaseVKey();
+    private fun pressVThenRelease() {
+        depressVKey()
+        releaseVKey()
     }
 
+    private fun pressAndHoldShiftV() {
+        depressShiftKey()
 
-    private void pressAndHoldShiftV() {
-        depressShiftKey();
-
-        KeyEvent vKey = KeyEventUtils.getVKey();
-        when(vKey.isShiftPressed()).thenReturn(true);
-        mReviewer.onKeyDown(KeyEvent.KEYCODE_V, vKey);
-        when(vKey.getRepeatCount()).thenReturn(1);
-        mReviewer.onKeyDown(KeyEvent.KEYCODE_V, vKey);
+        val vKey = getVKey()
+        `when`(vKey.isShiftPressed).thenReturn(true)
+        mReviewer!!.onKeyDown(KeyEvent.KEYCODE_V, vKey)
+        `when`(vKey.repeatCount).thenReturn(1)
+        mReviewer!!.onKeyDown(KeyEvent.KEYCODE_V, vKey)
     }
 
-
-    protected void pressShiftAndVThenRelease() {
-        depressShiftKey();
-        depressVKeyWithShiftHeld();
-        releaseShiftKey();
-        releaseVKey();
+    protected fun pressShiftAndVThenRelease() {
+        depressShiftKey()
+        depressVKeyWithShiftHeld()
+        releaseShiftKey()
+        releaseVKey()
     }
 
-
-    private void releaseVKey() {
-        KeyEvent mock = mock(KeyEvent.class);
-        when(mock.getKeyCode()).thenReturn(KeyEvent.KEYCODE_V);
-        when(mock.getUnicodeChar()).thenReturn((int) 'v');
-        when(mock.getUnicodeChar(anyInt())).thenReturn((int) 'v');
-        mReviewer.onKeyUp(KeyEvent.KEYCODE_V, mock);
+    private fun releaseVKey() {
+        val mock = mock(KeyEvent::class.java)
+        `when`(mock.keyCode).thenReturn(KeyEvent.KEYCODE_V)
+        `when`(mock.unicodeChar).thenReturn('v'.code)
+        `when`(mock.getUnicodeChar(anyInt())).thenReturn('v'.code)
+        mReviewer!!.onKeyUp(KeyEvent.KEYCODE_V, mock)
     }
 
-
-    private void releaseShiftKey() {
-        KeyEvent mock = mock(KeyEvent.class);
-        when(mock.getKeyCode()).thenReturn(KeyEvent.KEYCODE_SHIFT_LEFT);
-        mReviewer.onKeyUp(KeyEvent.KEYCODE_SHIFT_LEFT, mock);
+    private fun releaseShiftKey() {
+        val mock = mock(KeyEvent::class.java)
+        `when`(mock.keyCode).thenReturn(KeyEvent.KEYCODE_SHIFT_LEFT)
+        mReviewer!!.onKeyUp(KeyEvent.KEYCODE_SHIFT_LEFT, mock)
     }
 
-    private void depressVKey() {
-        KeyEvent mock = mock(KeyEvent.class);
-        when(mock.getKeyCode()).thenReturn(KeyEvent.KEYCODE_V);
-        when(mock.getUnicodeChar()).thenReturn((int) 'v');
-        when(mock.getUnicodeChar(anyInt())).thenReturn((int) 'v');
-        mReviewer.onKeyDown(KeyEvent.KEYCODE_V, mock);
+    private fun depressVKey() {
+        val mock = mock(KeyEvent::class.java)
+        `when`(mock.keyCode).thenReturn(KeyEvent.KEYCODE_V)
+        `when`(mock.unicodeChar).thenReturn('v'.code)
+        `when`(mock.getUnicodeChar(anyInt())).thenReturn('v'.code)
+        mReviewer!!.onKeyDown(KeyEvent.KEYCODE_V, mock)
     }
 
-
-    private void depressVKeyWithShiftHeld() {
-        KeyEvent mock = mock(KeyEvent.class);
-        when(mock.isShiftPressed()).thenReturn(true);
-        when(mock.getKeyCode()).thenReturn(KeyEvent.KEYCODE_V);
-        mReviewer.onKeyDown(KeyEvent.KEYCODE_V, mock);
+    private fun depressVKeyWithShiftHeld() {
+        val mock = mock(KeyEvent::class.java)
+        `when`(mock.isShiftPressed).thenReturn(true)
+        `when`(mock.keyCode).thenReturn(KeyEvent.KEYCODE_V)
+        mReviewer!!.onKeyDown(KeyEvent.KEYCODE_V, mock)
     }
 
-
-    private void depressShiftKey() {
-
-        KeyEvent mock = mock(KeyEvent.class);
-        when(mock.getAction()).thenReturn(0);
-        when(mock.getDeviceId()).thenReturn(9);
-        when(mock.getDownTime()).thenReturn(35660208L);
-        when(mock.getEventTime()).thenReturn(35660208L);
-        when(mock.getFlags()).thenReturn(8);
-        when(mock.getKeyCode()).thenReturn(KeyEvent.KEYCODE_SHIFT_LEFT);
-        when(mock.getMetaState()).thenReturn(65);
-        when(mock.getScanCode()).thenReturn(42);
-        when(mock.getSource()).thenReturn(257);
-        when(mock.getRepeatCount()).thenReturn(0);
-
-        mReviewer.onKeyDown(KeyEvent.KEYCODE_SHIFT_LEFT, mock);
+    private fun depressShiftKey() {
+        val mock = mock(KeyEvent::class.java)
+        `when`(mock.action).thenReturn(0)
+        `when`(mock.deviceId).thenReturn(9)
+        `when`(mock.downTime).thenReturn(35660208L)
+        `when`(mock.eventTime).thenReturn(35660208L)
+        `when`(mock.flags).thenReturn(8)
+        `when`(mock.keyCode).thenReturn(KeyEvent.KEYCODE_SHIFT_LEFT)
+        `when`(mock.metaState).thenReturn(65)
+        `when`(mock.scanCode).thenReturn(42)
+        `when`(mock.source).thenReturn(257)
+        `when`(mock.repeatCount).thenReturn(0)
+        mReviewer!!.onKeyDown(KeyEvent.KEYCODE_SHIFT_LEFT, mock)
     }
 }
