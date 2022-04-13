85d20ae2d38ac6fdbf02b6b10a37b520e0d94221
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-13 20:43:08 +0100

    refactor: convert DeckOptionsTest to Kotlin
    com.ichi2.anki.DeckOptionsTest

--- AnkiDroid/src/test/java/com/ichi2/anki/DeckOptionsTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/DeckOptionsTest.kt
@@ -1,32 +1,27 @@
 //noinspection MissingCopyrightHeader #8659
 
-package com.ichi2.anki;
+package com.ichi2.anki
 
-import com.ichi2.libanki.Collection;
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-@RunWith(AndroidJUnit4.class)
-public class DeckOptionsTest extends RobolectricTest {
-
+@RunWith(AndroidJUnit4::class)
+class DeckOptionsTest : RobolectricTest() {
     @Test
-    public void changeHardFactor() {
-        Collection col = getCol();
+    fun changeHardFactor() {
+        val col = col
 
         // Verify that for newly created deck hardFactor is default.
-        double hardFactor = col.get_config("hardFactor", 1.2);
-        Assert.assertEquals(1.2, hardFactor, 0.01);
+        var hardFactor = col.get_config("hardFactor", 1.2)
+        Assert.assertEquals(1.2, hardFactor, 0.01)
 
         // Modify hard factor.
-        col.set_config("hardFactor", 1.0);
+        col.set_config("hardFactor", 1.0)
 
         // Verify that hardFactor value has changed.
-        hardFactor = col.get_config("hardFactor", 1.2);
-        Assert.assertEquals(1.0, hardFactor, 0.01);
+        hardFactor = col.get_config("hardFactor", 1.2)
+        Assert.assertEquals(1.0, hardFactor, 0.01)
     }
-
 }
