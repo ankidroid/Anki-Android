7ab0a61a47f201bd25a1d11c080307cb749ddc1b
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-13 12:50:00 +0100

    refactor: convert AnalyticsConstantsTest to .kt
    com.ichi2.anki.analytics.AnalyticsConstantsTest

--- AnkiDroid/src/test/java/com/ichi2/anki/analytics/AnalyticsConstantsTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/analytics/AnalyticsConstantsTest.kt
@@ -10,153 +10,146 @@
  You should have received a copy of the GNU General Public License along with
  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.anki.analytics
 
-package com.ichi2.anki.analytics;
-
-import org.junit.Test;
-import org.junit.experimental.runners.Enclosed;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import androidx.annotation.NonNull;
-
-import static org.junit.Assert.assertEquals;
+import com.ichi2.utils.KotlinCleanup
+import org.junit.Assert
+import org.junit.Test
+import org.junit.experimental.runners.Enclosed
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import java.lang.RuntimeException
+import java.lang.reflect.Field
+import java.util.*
+import java.util.stream.Collectors
+import java.util.stream.Stream
+import kotlin.Throws
 
 /**
  * This class contains two nested classes and is using the concept of Enclosed runner that internally works as a Suite.
  * The first inner class is a Parameterized class and will run according to the size of test case, while the second
  * inner class is non-parameterized and will run only once.
  */
-@RunWith(Enclosed.class)
-public class AnalyticsConstantsTest {
-    private final static List<String> listOfConstantFields = new ArrayList<>();
+@RunWith(Enclosed::class)
+@KotlinCleanup("auto IDE lint")
+@KotlinCleanup("Move away from Java streams")
+@KotlinCleanup("Remove @Throws")
+object AnalyticsConstantsTest {
+    private val listOfConstantFields: MutableList<String> = ArrayList()
 
-    static {
-        listOfConstantFields.add("Opened HelpDialogBox");
-        listOfConstantFields.add("Opened Using AnkiDroid");
-        listOfConstantFields.add("Opened Get Help");
-        listOfConstantFields.add("Opened Support AnkiDroid");
-        listOfConstantFields.add("Opened Community");
-        listOfConstantFields.add("Opened AnkiDroid Manual");
-        listOfConstantFields.add("Opened Anki Manual");
-        listOfConstantFields.add("Opened AnkiDroid FAQ");
-        listOfConstantFields.add("Opened Mailing List");
-        listOfConstantFields.add("Opened Report a Bug");
-        listOfConstantFields.add("Opened Donate");
-        listOfConstantFields.add("Opened Translate");
-        listOfConstantFields.add("Opened Develop");
-        listOfConstantFields.add("Opened Rate");
-        listOfConstantFields.add("Opened Other");
-        listOfConstantFields.add("Opened Send Feedback");
-        listOfConstantFields.add("Opened Anki Forums");
-        listOfConstantFields.add("Opened Reddit");
-        listOfConstantFields.add("Opened Discord");
-        listOfConstantFields.add("Opened Facebook");
-        listOfConstantFields.add("Opened Twitter");
-        listOfConstantFields.add("Opened Privacy");
-        listOfConstantFields.add("Opened AnkiDroid Privacy Policy");
-        listOfConstantFields.add("Opened AnkiWeb Privacy Policy");
-        listOfConstantFields.add("Opened AnkiWeb Terms and Conditions");
-        listOfConstantFields.add("Exception Report");
-        listOfConstantFields.add("Import APKG");
-        listOfConstantFields.add("Import COLPKG");
+    init {
+        listOfConstantFields.add("Opened HelpDialogBox")
+        listOfConstantFields.add("Opened Using AnkiDroid")
+        listOfConstantFields.add("Opened Get Help")
+        listOfConstantFields.add("Opened Support AnkiDroid")
+        listOfConstantFields.add("Opened Community")
+        listOfConstantFields.add("Opened AnkiDroid Manual")
+        listOfConstantFields.add("Opened Anki Manual")
+        listOfConstantFields.add("Opened AnkiDroid FAQ")
+        listOfConstantFields.add("Opened Mailing List")
+        listOfConstantFields.add("Opened Report a Bug")
+        listOfConstantFields.add("Opened Donate")
+        listOfConstantFields.add("Opened Translate")
+        listOfConstantFields.add("Opened Develop")
+        listOfConstantFields.add("Opened Rate")
+        listOfConstantFields.add("Opened Other")
+        listOfConstantFields.add("Opened Send Feedback")
+        listOfConstantFields.add("Opened Anki Forums")
+        listOfConstantFields.add("Opened Reddit")
+        listOfConstantFields.add("Opened Discord")
+        listOfConstantFields.add("Opened Facebook")
+        listOfConstantFields.add("Opened Twitter")
+        listOfConstantFields.add("Opened Privacy")
+        listOfConstantFields.add("Opened AnkiDroid Privacy Policy")
+        listOfConstantFields.add("Opened AnkiWeb Privacy Policy")
+        listOfConstantFields.add("Opened AnkiWeb Terms and Conditions")
+        listOfConstantFields.add("Exception Report")
+        listOfConstantFields.add("Import APKG")
+        listOfConstantFields.add("Import COLPKG")
     }
 
-    @NonNull
-    protected static Stream<Field> getAnalyticsConstantFields() {
-        return Arrays.stream(UsageAnalytics.Actions.class.getDeclaredFields())
-                .filter(x -> x.isAnnotationPresent(AnalyticsConstant.class));
-    }
+    internal val analyticsConstantFields: Stream<Field>
+        get() = Arrays.stream(UsageAnalytics.Actions::class.java.declaredFields)
+            .filter { x: Field -> x.isAnnotationPresent(AnalyticsConstant::class.java) }
 
-    @RunWith(Parameterized.class)
-    public static class AnalyticsConstantsFieldValuesTest {
-        private final String mAnalyticsString;
-
-
-        public AnalyticsConstantsFieldValuesTest(String analyticsString) {
-            this.mAnalyticsString = analyticsString;
-        }
-
-
-        @Parameterized.Parameters
-        public static List<String> addAnalyticsConstants() {
-            return listOfConstantFields;
-        }
-
-
+    @RunWith(Parameterized::class)
+    class AnalyticsConstantsFieldValuesTest(private val analyticsString: String) {
         /**
          * The message here means that the string being checked cannot be found in Actions class.
          * If encountered with this message, re-check the list present here and constants in Actions class, to resolve
          * the test failure.
          */
         @Test
-        public void checkAnalyticsString() throws IllegalAccessException {
-            assertEquals("Re-check if you renamed any string in the analytics string constants of Actions class or AnalyticsConstantsTest.listOfConstantFields. If so, revert them as those string constants must not change as they are compared in analytics.",
-                    mAnalyticsString, getStringFromReflection(mAnalyticsString));
+        @Throws(IllegalAccessException::class)
+        fun checkAnalyticsString() {
+            Assert.assertEquals(
+                "Re-check if you renamed any string in the analytics string constants of Actions class or AnalyticsConstantsTest.listOfConstantFields. If so, revert them as those string constants must not change as they are compared in analytics.",
+                analyticsString, getStringFromReflection(analyticsString)
+            )
         }
 
-
-        public String getStringFromReflection(String analyticsStringToBeChecked) throws IllegalAccessException {
-            for (Field value : getAnalyticsConstantFields().collect(Collectors.toList())) {
-                Object reflectedValue = value.get(null);
-                if (reflectedValue.equals(analyticsStringToBeChecked)) {
-                    return (String) reflectedValue;
+        @Throws(IllegalAccessException::class)
+        fun getStringFromReflection(analyticsStringToBeChecked: String): String? {
+            for (value in analyticsConstantFields.collect(Collectors.toList())) {
+                val reflectedValue = value[null]
+                if (reflectedValue == analyticsStringToBeChecked) {
+                    return reflectedValue as String
                 }
             }
-            return null;
+            return null
         }
 
+        companion object {
+            @JvmStatic
+            @Parameterized.Parameters
+            fun addAnalyticsConstants(): List<String> {
+                return listOfConstantFields
+            }
+        }
     }
 
-
-    public static class AnalyticsConstantsFieldLengthTest {
-
+    class AnalyticsConstantsFieldLengthTest {
         @Test
-        public void fieldSizeEqualsListOfConstantFields() {
-            if (getFieldSize() > listOfConstantFields.size()) {
-                assertEquals("Add the newly added analytics constant to AnalyticsConstantsTest.listOfConstantFields. NOTE: Constants should not be renamed as we cannot compare these in analytics.",
-                        listOfConstantFields.size(), getFieldSize());
-            } else if (getFieldSize() < listOfConstantFields.size()) {
-                assertEquals("If a constant is removed, it should be removed from AnalyticsConstantsTest.listOfConstantFields. NOTE: Constants should not be renamed as we cannot compare these in analytics.",
-                        listOfConstantFields.size(), getFieldSize());
+        fun fieldSizeEqualsListOfConstantFields() {
+            if (fieldSize > listOfConstantFields.size) {
+                Assert.assertEquals(
+                    "Add the newly added analytics constant to AnalyticsConstantsTest.listOfConstantFields. NOTE: Constants should not be renamed as we cannot compare these in analytics.",
+                    listOfConstantFields.size.toLong(), fieldSize
+                )
+            } else if (fieldSize < listOfConstantFields.size) {
+                Assert.assertEquals(
+                    "If a constant is removed, it should be removed from AnalyticsConstantsTest.listOfConstantFields. NOTE: Constants should not be renamed as we cannot compare these in analytics.",
+                    listOfConstantFields.size.toLong(), fieldSize
+                )
             } else {
-                assertEquals(listOfConstantFields.size(), getFieldSize());
+                Assert.assertEquals(listOfConstantFields.size.toLong(), fieldSize)
             }
-
         }
-
-
-        /**
-         * This method is used to get the size of fields in Actions Class.
-         * Because whenever a new constant is added in Actions Class but not added to the list present in this
-         * class (listOfConstantFields) the test must fail.
-         */
-        public static long getFieldSize() {
-            return getAnalyticsConstantFields().count();
-        }
-
 
         /**
          * This test is used to check whether all the string constants of Actions are annotated with @AnalyticsConstant.
          * If not, then a runtime exception is thrown.
          */
         @Test
-        public void fieldAnnotatedOrNot(){
-            UsageAnalytics.Actions actions = new UsageAnalytics.Actions();
-            Field[] field;
-            field = actions.getClass().getDeclaredFields();
-            for (Field value : field) {
-                if (!value.isAnnotationPresent(AnalyticsConstant.class) && !value.isSynthetic()) {
-                    throw new RuntimeException("All the fields in Actions class must be annotated with @AnalyticsConstant. It seems " + value.getName() + " is not annotated.");
+        fun fieldAnnotatedOrNot() {
+            val actions = UsageAnalytics.Actions()
+            val field: Array<Field>
+            field = actions.javaClass.declaredFields
+            for (value in field) {
+                if (!value.isAnnotationPresent(AnalyticsConstant::class.java) && !value.isSynthetic) {
+                    throw RuntimeException("All the fields in Actions class must be annotated with @AnalyticsConstant. It seems " + value.name + " is not annotated.")
                 }
             }
+        }
+
+        companion object {
+            /**
+             * This method is used to get the size of fields in Actions Class.
+             * Because whenever a new constant is added in Actions Class but not added to the list present in this
+             * class (listOfConstantFields) the test must fail.
+             */
+            val fieldSize: Long
+                get() = analyticsConstantFields.count()
         }
     }
 }
