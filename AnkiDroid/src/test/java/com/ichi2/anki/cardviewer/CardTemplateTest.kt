1069f21d30918c2b7a234102e6f11a2388aa7974
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-13 13:04:03 +0100

    refactor: convert CardTemplateTest to .kt
    com.ichi2.anki.cardviewer.CardTemplateTest

--- AnkiDroid/src/test/java/com/ichi2/anki/cardviewer/CardTemplateTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/cardviewer/CardTemplateTest.kt
@@ -13,63 +13,62 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.anki.cardviewer
 
-package com.ichi2.anki.cardviewer;
+import com.ichi2.testutils.AnkiAssert.assertDoesNotThrow
+import com.ichi2.utils.KotlinCleanup
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.*
+import org.intellij.lang.annotations.Language
+import org.junit.Test
 
-import org.intellij.lang.annotations.Language;
-import org.junit.Test;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
-public class CardTemplateTest {
-
-    @Language("HTML")
-    private static final String data = "<!doctype html>\n" +
-            "<html class=\"mobile android linux js\">\n" +
-            "    <head>\n" +
-            "        <title>AnkiDroid Flashcard</title>\n" +
-            "        <meta charset=\"utf-8\">\n" +
-            "        <link rel=\"stylesheet\" type=\"text/css\" href=\"file:///android_asset/flashcard.css\">\n" +
-            "        <link rel=\"stylesheet\" type=\"text/css\" href=\"file:///android_asset/chess.css\">\n" +
-            "        <link rel=\"stylesheet\" type=\"text/css\" href=\"file:///android_asset/mathjax/mathjax.css\">\n" +
-            "        <style>\n" +
-            "        ::style::\n" +
-            "        </style>\n" +
-            "        ::script::\n" +
-            "        <script src=\"file:///android_asset/mathjax/conf.js\"> </script>\n" +
-            "        <script src=\"file:///android_asset/mathjax/MathJax.js\"> </script>\n" +
-            "        <script src=\"file:///android_asset/scripts/card.js\" type=\"text/javascript\"> </script>\n" +
-            "    </head>\n" +
-            "    <body class=\"::class::\">\n" +
-            "        <div id=\"content\">\n" +
-            "        ::content::\n" +
-            "        </div>\n" +
-            "    </body>\n" +
-            "</html>\n";
-
+@KotlinCleanup("`is`")
+class CardTemplateTest {
     @Test
-    public void replaceTest() {
+    fun replaceTest() {
         // Method is sped up - ensure that it still works.
-        String content = "foo";
-        String style = "bar";
-        String cardClass = "baz";
-        String script = "script";
-        String result = new CardTemplate(data).render(content, style, script, cardClass);
-
-        assertThat(result, is(data.replace("::content::", content).replace("::style::", style).replace("::class::", cardClass).replace("::script::", script)));
+        val content = "foo"
+        val style = "bar"
+        val cardClass = "baz"
+        val script = "script"
+        val result = CardTemplate(data).render(content, style, script, cardClass)
+        assertThat(result, `is`(data.replace("::content::", content).replace("::style::", style).replace("::class::", cardClass).replace("::script::", script)))
     }
 
     @Test
-    public void stressTest() {
+    fun stressTest() {
         // At length = 10000000
         // ~500ms before
         // < 200 after
-        int stringLength = 1000;
-        String content = new String(new char[stringLength]).replace('\0', 'a');
-
-
-        String ret = new CardTemplate(data).render(content, content, "", content);
+        val stringLength = 1000
+        val content = String(CharArray(stringLength)).replace('\u0000', 'a')
+        assertDoesNotThrow { CardTemplate(data).render(content, content, "", content) }
     }
 
+    companion object {
+        @Language("HTML")
+        private val data = """<!doctype html>
+<html class="mobile android linux js">
+    <head>
+        <title>AnkiDroid Flashcard</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="file:///android_asset/flashcard.css">
+        <link rel="stylesheet" type="text/css" href="file:///android_asset/chess.css">
+        <link rel="stylesheet" type="text/css" href="file:///android_asset/mathjax/mathjax.css">
+        <style>
+        ::style::
+        </style>
+        ::script::
+        <script src="file:///android_asset/mathjax/conf.js"> </script>
+        <script src="file:///android_asset/mathjax/MathJax.js"> </script>
+        <script src="file:///android_asset/scripts/card.js" type="text/javascript"> </script>
+    </head>
+    <body class="::class::">
+        <div id="content">
+        ::content::
+        </div>
+    </body>
+</html>
+"""
+    }
 }
