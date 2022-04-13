edbb682c12fd0533842372fb23b414fa422e7958
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-13 15:33:31 +0100

    refactor: convert ReminderServiceTest to Kotlin
    com.ichi2.anki.services.ReminderServiceTest

--- AnkiDroid/src/test/java/com/ichi2/anki/services/ReminderServiceTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/services/ReminderServiceTest.kt
@@ -13,93 +13,78 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.anki.services
 
-package com.ichi2.anki.services;
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.anki.CollectionHelper
+import com.ichi2.anki.RobolectricTest
+import com.ichi2.utils.KotlinCleanup
+import org.hamcrest.MatcherAssert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.*
+import org.robolectric.Shadows
+import org.robolectric.shadows.ShadowNotificationManager
 
-import android.app.NotificationManager;
-import android.content.Context;
-import android.content.Intent;
-
-import com.ichi2.anki.CollectionHelper;
-import com.ichi2.anki.RobolectricTest;
-import com.ichi2.libanki.Collection;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.shadows.ShadowNotificationManager;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import static com.ichi2.anki.services.ReminderService.EXTRA_DECK_ID;
-import static com.ichi2.anki.services.ReminderService.EXTRA_DECK_OPTION_ID;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.robolectric.Shadows.shadowOf;
-
-@RunWith(AndroidJUnit4.class)
-public class ReminderServiceTest extends RobolectricTest {
-
+@RunWith(AndroidJUnit4::class)
+@KotlinCleanup("use matchers for size checks")
+@KotlinCleanup("`when` -> whenever()")
+class ReminderServiceTest : RobolectricTest() {
     @Test
-    public void testReminderServiceNothingDue() {
-        buildDefaultDeckReminders();
-        assertThat("No notifications exist", getNotificationManagerShadow().size() == 0);
+    fun testReminderServiceNothingDue() {
+        buildDefaultDeckReminders()
+        assertThat("No notifications exist", notificationManagerShadow.size() == 0)
     }
 
-
     @Test
-    public void testReminderServiceReviewsDue() {
-        addNoteUsingBasicModel("test front", "test back");
-        assertThat("No notifications exist", getNotificationManagerShadow().size() == 0);
-        buildDefaultDeckReminders();
-        assertThat("No notifications exist", getNotificationManagerShadow().size() == 1);
+    fun testReminderServiceReviewsDue() {
+        addNoteUsingBasicModel("test front", "test back")
+        assertThat("No notifications exist", notificationManagerShadow.size() == 0)
+        buildDefaultDeckReminders()
+        assertThat("No notifications exist", notificationManagerShadow.size() == 1)
     }
 
-
     @Test
-    public void testReminderServiceNullCollection() {
-        addNoteUsingBasicModel("test front", "test back");
-        enableNullCollection();
-        assertThat("No notifications exist", getNotificationManagerShadow().size() == 0);
-        buildDefaultDeckReminders();
+    fun testReminderServiceNullCollection() {
+        addNoteUsingBasicModel("test front", "test back")
+        enableNullCollection()
+        assertThat("No notifications exist", notificationManagerShadow.size() == 0)
+        buildDefaultDeckReminders()
         // The collection was null so no reminders, but we should get here without exception
-        assertThat("No notifications exist", getNotificationManagerShadow().size() == 0);
+        assertThat("No notifications exist", notificationManagerShadow.size() == 0)
     }
-
 
     /**
      * #8264: Crash on sync - getSched().getDueTree() failed
      */
     @Test
-    public void testDatabaseFailureWhileSyncingDoesNotCrash() {
+    fun testDatabaseFailureWhileSyncingDoesNotCrash() {
         // If getCol() fails, it triggers different exception handling in the service.
         // The cause was getSched().deckDueTree()
-        Collection baseCol = getCol();
-        Collection mockCol = spy(baseCol);
-        when(mockCol.getSched()).thenThrow(new IllegalStateException("Unit test: simulating database exception"));
+        val baseCol = col
+        val mockCol = spy(baseCol)
 
-        CollectionHelper.getInstance().setColForTests(mockCol);
+        `when`(mockCol.sched).thenThrow(IllegalStateException("Unit test: simulating database exception"))
 
-        buildDefaultDeckReminders();
+        CollectionHelper.getInstance().setColForTests(mockCol)
+
+        buildDefaultDeckReminders()
 
         // We retry after a database timeout so getSched is called twice
-        //noinspection ResultOfMethodCallIgnored
-        verify(mockCol, times(2)).getSched();
+        verify(mockCol, times(2)).sched
     }
 
+    private val notificationManagerShadow: ShadowNotificationManager
+        get() = Shadows.shadowOf(targetContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
 
-    private ShadowNotificationManager getNotificationManagerShadow() {
-        return shadowOf((NotificationManager) getTargetContext().getSystemService(Context.NOTIFICATION_SERVICE));
-    }
-
-
-    private void buildDefaultDeckReminders() {
-        Intent defaultDeckIntent = new Intent();
-        defaultDeckIntent.putExtra(EXTRA_DECK_ID, 1L);
-        defaultDeckIntent.putExtra(EXTRA_DECK_OPTION_ID, 1L);
-        ReminderService reminders = new ReminderService();
-        reminders.onReceive(getTargetContext(), defaultDeckIntent);
+    private fun buildDefaultDeckReminders() {
+        val defaultDeckIntent = Intent()
+        defaultDeckIntent.putExtra(ReminderService.EXTRA_DECK_ID, 1L)
+        defaultDeckIntent.putExtra(ReminderService.EXTRA_DECK_OPTION_ID, 1L)
+        val reminders = ReminderService()
+        reminders.onReceive(targetContext, defaultDeckIntent)
     }
 }
