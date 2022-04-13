68c7fd480c8736e1ff55a9e5b9a925ae3385df70
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-13 16:10:16 +0100

    refactor: convert AbstractFlashcardViewerKeyboardInputTest to Kotlin
    com.ichi2.anki.AbstractFlashcardViewerKeyboardInputTest

--- AnkiDroid/src/test/java/com/ichi2/anki/AbstractFlashcardViewerKeyboardInputTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/AbstractFlashcardViewerKeyboardInputTest.kt
@@ -13,114 +13,106 @@
  You should have received a copy of the GNU General Public License along with
  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.anki
 
-package com.ichi2.anki;
+import android.view.KeyEvent
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.MatcherAssert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import timber.log.Timber
+import java.lang.Exception
 
-import android.view.KeyEvent;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import timber.log.Timber;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-
-@RunWith(AndroidJUnit4.class)
-public class AbstractFlashcardViewerKeyboardInputTest extends RobolectricTest {
-
+@RunWith(AndroidJUnit4::class)
+class AbstractFlashcardViewerKeyboardInputTest : RobolectricTest() {
     @Test
-    public void spaceShowsAnswer() {
-        KeyboardInputTestCardViewer underTest = KeyboardInputTestCardViewer.create();
+    fun spaceShowsAnswer() {
+        val underTest = KeyboardInputTestCardViewer.create()
 
-        underTest.handleKeyPress(KeyEvent.KEYCODE_SPACE);
+        underTest.handleKeyPress(KeyEvent.KEYCODE_SPACE)
 
-        assertThat("Space should display answer on any card viewer",  underTest.didDisplayAnswer());
+        assertThat("Space should display answer on any card viewer", underTest.didDisplayAnswer())
     }
 
     @Test
     @RunInBackground
-    public void enterShowsAnswer() {
-        KeyboardInputTestCardViewer underTest = KeyboardInputTestCardViewer.create();
+    fun enterShowsAnswer() {
+        val underTest = KeyboardInputTestCardViewer.create()
 
-        underTest.handleKeyPress(KeyEvent.KEYCODE_ENTER);
+        underTest.handleKeyPress(KeyEvent.KEYCODE_ENTER)
 
-        assertThat("Enter should display answer on any card viewer",  underTest.didDisplayAnswer());
+        assertThat("Enter should display answer on any card viewer", underTest.didDisplayAnswer())
     }
 
     @Test
-    public void numPadEnterShowsAnswer() {
-        KeyboardInputTestCardViewer underTest = KeyboardInputTestCardViewer.create();
+    fun numPadEnterShowsAnswer() {
+        val underTest = KeyboardInputTestCardViewer.create()
 
-        underTest.handleKeyPress(KeyEvent.KEYCODE_NUMPAD_ENTER);
+        underTest.handleKeyPress(KeyEvent.KEYCODE_NUMPAD_ENTER)
 
-        assertThat("NumPad Enter should display answer on any card viewer",  underTest.didDisplayAnswer());
+        assertThat("NumPad Enter should display answer on any card viewer", underTest.didDisplayAnswer())
     }
 
     @Test
-    public void spaceDoesNotShowAnswerIfTextFieldFocused() {
-        KeyboardInputTestCardViewer underTest = KeyboardInputTestCardViewer.create();
-        underTest.focusTextField();
+    fun spaceDoesNotShowAnswerIfTextFieldFocused() {
+        val underTest = KeyboardInputTestCardViewer.create()
+        underTest.focusTextField()
 
-        underTest.handleKeyPress(KeyEvent.KEYCODE_SPACE);
+        underTest.handleKeyPress(KeyEvent.KEYCODE_SPACE)
 
-        assertThat("When text field is focused, space should not display answer",
-                !underTest.didDisplayAnswer());
-
+        assertThat(
+            "When text field is focused, space should not display answer",
+            !underTest.didDisplayAnswer()
+        )
     }
 
-
-    private static class KeyboardInputTestCardViewer extends AbstractFlashcardViewer {
-
-        private boolean mDisplayAnswer = false;
-        private boolean mFocusTextField = false;
-
-
-        public static KeyboardInputTestCardViewer create() {
-            AbstractFlashcardViewer.sDisplayAnswer = false;
-            return new KeyboardInputTestCardViewer();
+    private class KeyboardInputTestCardViewer : AbstractFlashcardViewer() {
+        private var mDisplayAnswer = false
+        private var mFocusTextField = false
+        override fun answerFieldIsFocused(): Boolean {
+            return mFocusTextField
         }
 
-        @Override
-        protected boolean answerFieldIsFocused() {
-            return mFocusTextField;
-        }
-
-
-        @Override
-        protected void performReload() {
+        override fun performReload() {
             // intentionally blank
         }
 
-
-        @Override
-        protected void displayCardAnswer() {
-            mDisplayAnswer = true;
+        override fun displayCardAnswer() {
+            mDisplayAnswer = true
         }
 
-        public boolean didDisplayAnswer() { return mDisplayAnswer; }
+        fun didDisplayAnswer(): Boolean {
+            return mDisplayAnswer
+        }
 
-        public void handleKeyPress(int keycode) {
-            //COULD_BE_BETTER: Saves 20 seconds on tests to remove AndroidJUnit4,
+        fun handleKeyPress(keycode: Int) {
+            // COULD_BE_BETTER: Saves 20 seconds on tests to remove AndroidJUnit4,
             // but may let something slip through the cracks.
             try {
-                this.onKeyDown(keycode, new KeyEvent(KeyEvent.ACTION_DOWN, keycode));
-            } catch (Exception e) {
-                Timber.e(e);
+                onKeyDown(keycode, KeyEvent(KeyEvent.ACTION_DOWN, keycode))
+            } catch (e: Exception) {
+                Timber.e(e)
             }
             try {
-                this.onKeyUp(keycode, new KeyEvent(KeyEvent.ACTION_UP, keycode));
-            } catch (Exception e) {
-                Timber.e(e);
+                onKeyUp(keycode, KeyEvent(KeyEvent.ACTION_UP, keycode))
+            } catch (e: Exception) {
+                Timber.e(e)
             }
         }
-        @Override protected void setTitle() {
-            //required for interface. Intentionally left blank
+
+        override fun setTitle() {
+            // required for interface. Intentionally left blank
         }
 
+        fun focusTextField() {
+            mFocusTextField = true
+        }
 
-        public void focusTextField() {
-            mFocusTextField = true;
+        companion object {
+            fun create(): KeyboardInputTestCardViewer {
+                sDisplayAnswer = false
+                return KeyboardInputTestCardViewer()
+            }
         }
     }
 }
