fc5a5147951567f5cd23fb987a0bb6c597e3c402
Propo41 <aliahnaf327@gmail.com>
2022-04-16 03:22:27 +0600

    added empty space to create new commit

--- AnkiDroid/src/main/java/com/ichi2/anki/NavigationDrawerActivity.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/NavigationDrawerActivity.java
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
-package com.ichi2.anki;
+package com.ichi2.anki; 
 
 import android.annotation.TargetApi;
 import android.content.Context;
