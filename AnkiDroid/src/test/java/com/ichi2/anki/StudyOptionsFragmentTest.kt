8e7029f471614e478e9307acbe198a91521e18f2
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-15 14:32:29 +0100

    refactor: Convert StudyOptionsFragmentTest to Kotlin
    com.ichi2.anki.StudyOptionsFragmentTest

--- AnkiDroid/src/test/java/com/ichi2/anki/StudyOptionsFragmentTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/StudyOptionsFragmentTest.kt
@@ -13,84 +13,78 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.anki
 
-package com.ichi2.anki;
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.anki.StudyOptionsFragment.Companion.formatDescription
+import com.ichi2.utils.KotlinCleanup
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import android.text.Spanned;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import static org.junit.Assert.assertEquals;
-
-@RunWith(AndroidJUnit4.class) //required for String -> Spannable conversion in formatDescription
-public class StudyOptionsFragmentTest {
-
-    //Fixes for #5715: In deck description, ignore what is in style and script tag
-
+@RunWith(AndroidJUnit4::class) // required for String -> Spannable conversion in formatDescription
+@KotlinCleanup("add @Language to fromDescription")
+class StudyOptionsFragmentTest {
+    // Fixes for #5715: In deck description, ignore what is in style and script tag
     @Test
-    public void spanTagsAreNotRemoved() {
-        Spanned result = StudyOptionsFragment.formatDescription("a<span style=\"color:red\">a=1</span>a");
-        assertEquals("aa=1a", result.toString()); //Note: This is coloured red on the screen
+    fun spanTagsAreNotRemoved() {
+        val result = formatDescription("a<span style=\"color:red\">a=1</span>a")
+        assertEquals("aa=1a", result.toString()) // Note: This is coloured red on the screen
     }
 
     @Test
-    public void scriptTagContentsAreRemoved() {
-        Spanned result = StudyOptionsFragment.formatDescription("a<script>a=1</script>a");
-        assertEquals("aa", result.toString());
+    fun scriptTagContentsAreRemoved() {
+        val result = formatDescription("a<script>a=1</script>a")
+        assertEquals("aa", result.toString())
     }
 
     @Test
-    public void upperCaseScriptTagContentsAreRemoved() {
-        Spanned result = StudyOptionsFragment.formatDescription("a<SCRIPT>a=1</script>a");
-        assertEquals("aa", result.toString());
+    fun upperCaseScriptTagContentsAreRemoved() {
+        val result = formatDescription("a<SCRIPT>a=1</script>a")
+        assertEquals("aa", result.toString())
     }
 
     @Test
-    public void scriptTagWithAttributesContentsAreRemoved() {
-        Spanned result = StudyOptionsFragment.formatDescription("a<script type=\"application/javascript\">a=1</script>a");
-        assertEquals("aa", result.toString());
+    fun scriptTagWithAttributesContentsAreRemoved() {
+        val result = formatDescription("a<script type=\"application/javascript\">a=1</script>a")
+        assertEquals("aa", result.toString())
     }
 
     @Test
-    public void styleTagContentsAreRemoved() {
-        Spanned result = StudyOptionsFragment.formatDescription("a<style>a=1</style>a");
-        assertEquals("aa", result.toString());
+    fun styleTagContentsAreRemoved() {
+        val result = formatDescription("a<style>a=1</style>a")
+        assertEquals("aa", result.toString())
     }
 
     @Test
-    public void upperCaseStyleTagContentsAreRemoved() {
-        Spanned result = StudyOptionsFragment.formatDescription("a<STYLE>a:1</style>a");
-        assertEquals("aa", result.toString());
+    fun upperCaseStyleTagContentsAreRemoved() {
+        val result = formatDescription("a<STYLE>a:1</style>a")
+        assertEquals("aa", result.toString())
     }
 
     @Test
-    public void styleTagWithAttributesContentsAreRemoved() {
-        Spanned result = StudyOptionsFragment.formatDescription("a<style type=\"text/css\">a:1</style>a");
-        assertEquals("aa", result.toString());
+    fun styleTagWithAttributesContentsAreRemoved() {
+        val result = formatDescription("a<style type=\"text/css\">a:1</style>a")
+        assertEquals("aa", result.toString())
     }
 
-    /** Begin #5188 - newlines weren't displayed */
-
-    @Test //This was originally correct
-    public void brIsDisplayedAsNewline() {
-        Spanned result = StudyOptionsFragment.formatDescription("a<br/>a");
-        assertEquals("a\na", result.toString());
-    }
-
-    @Test
-    public void windowsNewlinesAreNewlines() {
-        Spanned result = StudyOptionsFragment.formatDescription("a\r\na");
-        assertEquals("a\na", result.toString());
+    /* Begin #5188 - newlines weren't displayed  */
+    @Test // This was originally correct
+    fun brIsDisplayedAsNewline() {
+        val result = formatDescription("a<br/>a")
+        assertEquals("a\na", result.toString())
     }
 
     @Test
-    public void unixNewlinesAreNewlines() {
-        Spanned result = StudyOptionsFragment.formatDescription("a\na");
-        assertEquals("a\na", result.toString());
+    fun windowsNewlinesAreNewlines() {
+        val result = formatDescription("a\r\na")
+        assertEquals("a\na", result.toString())
     }
 
+    @Test
+    fun unixNewlinesAreNewlines() {
+        val result = formatDescription("a\na")
+        assertEquals("a\na", result.toString())
+    }
     /* end #5188 */
 }
