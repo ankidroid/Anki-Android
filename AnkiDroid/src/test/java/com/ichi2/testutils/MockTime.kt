044850a3237f878b7fd2cf760f93b56e4c55c745
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-15 15:22:33 +0100

    refactor: Convert MockTime to Kotlin
    com.ichi2.testutils.MockTime

--- AnkiDroid/src/test/java/com/ichi2/testutils/MockTime.kt
+++ AnkiDroid/src/test/java/com/ichi2/testutils/MockTime.kt
@@ -13,112 +13,113 @@
  You should have received a copy of the GNU General Public License along with
  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.testutils
 
-package com.ichi2.testutils;
+import android.annotation.SuppressLint
+import com.ichi2.libanki.utils.Time
+import com.ichi2.utils.KotlinCleanup
+import java.util.*
+import kotlin.jvm.JvmOverloads
 
-import android.annotation.SuppressLint;
+@KotlinCleanup("IDE lint")
+open class MockTime : Time {
+    /** Number of milliseconds between each call.  */
+    private val mStep: Int
 
-import com.ichi2.libanki.utils.Time;
-
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
-
-public class MockTime extends Time {
-
-    /** Number of milliseconds between each call. */
-    private final int mStep;
-    /** Time since epoch in MS. */
-    private long mTime;
-
-    /** A clock at time Time, only changed explicitly*/
-    public MockTime(long time) {
-        this(time, 0);
+    /** Time since epoch in MS.  */
+    protected var time: Long
+        private set
+    /** A clock at time Time, each call advance by step ms. */
+    /** A clock at time Time, only changed explicitly */
+    @JvmOverloads
+    constructor(time: Long, step: Int = 0) {
+        this.time = time
+        mStep = step
     }
 
-    /** A clock at time Time, each call advance by step ms.*/
-    public MockTime(long time, int step) {
-        this.mTime = time;
-        this.mStep = step;
+    /** create a mock time whose initial value is this date. Month is 0-based, in order to stay close to calendar. MS are 0. */
+    constructor(
+        year: Int,
+        month: Int,
+        date: Int,
+        hourOfDay: Int,
+        minute: Int,
+        second: Int,
+        milliseconds: Int,
+        step: Int
+    ) {
+        time = timeStamp(year, month, date, hourOfDay, minute, second, milliseconds)
+        mStep = step
     }
 
-    /** create a mock time whose initial value is this date. Month is 0-based, in order to stay close to calendar. MS are 0.*/
-    public MockTime(int year, int month, int date, int hourOfDay, int minute,
-                    int second, int milliseconds, int step) {
-        mTime = timeStamp(year, month, date, hourOfDay, minute, second, milliseconds);
-        mStep = step;
+    /** Time in millisecond since epoch.  */
+    override fun intTimeMS(): Long {
+        val time = time
+        this.time += mStep.toLong()
+        return time
     }
 
-    /** Time in millisecond since epoch. */
-    @Override
-    public long intTimeMS() {
-        long time = this.mTime;
-        this.mTime += mStep;
-        return time;
+    /** Add ms milliseconds  */
+    fun addMs(ms: Long) {
+        time += ms
     }
 
-    protected long getTime() {
-        return mTime;
+    /** add s seconds  */
+    fun addS(s: Long) {
+        addMs(s * 1000L)
     }
 
-    /** Add ms milliseconds */
-    public void addMs(long ms) {
-        mTime += ms;
+    /** add m minutes  */
+    fun addM(m: Long) {
+        addS(m * 60)
     }
 
-    /** add s seconds */
-    public void addS(long s) {
-        addMs(s * 1000L);
+    /** add h hours */
+    fun addH(h: Long) {
+        addM(h * 60)
     }
 
-    /** add m minutes */
-    public void addM(long m) {
-        addS(m * 60);
+    /** add d days */
+    fun addD(d: Long) {
+        addH(d * 24)
     }
 
-    /** add h hours*/
-    public void addH(long h) {
-        addM(h * 60);
-    }
+    companion object {
+        /**
+         * Allow to get a timestamp which is independent of place where test occurs. MS are set to 0
+         * @param year Year
+         * @param month Month, 0-based
+         * @param date, day of month
+         * @param hourOfDay, hour, from 0 to 23
+         * @param minute, from 0 to 59
+         * @param second, From 0 to 59
+         * @return the time stamp of this instant in GMT calendar
+         */
+        @JvmStatic
+        @KotlinCleanup("After Kotlin Conversion, use default argument and remove this")
+        fun timeStamp(year: Int, month: Int, date: Int, hourOfDay: Int, minute: Int, second: Int): Long {
+            return timeStamp(year, month, date, hourOfDay, minute, second, 0)
+        }
 
-    /** add d days*/
-    public void addD(long d) {
-        addH(d * 24);
-    }
-
-
-    /**
-     * Allow to get a timestamp which is independent of place where test occurs. MS are set to 0
-     * @param year Year
-     * @param month Month, 0-based
-     * @param date, day of month
-     * @param hourOfDay, hour, from 0 to 23
-     * @param minute, from 0 to 59
-     * @param second, From 0 to 59
-     * @return the time stamp of this instant in GMT calendar
-     */
-    public static long timeStamp(int year, int month, int date, int hourOfDay, int minute, int second) {
-        return timeStamp(year, month, date, hourOfDay, minute, second, 0);
-    }
-
-
-    /**
-     * Allow to get a timestamp which is independent of place where test occurs.
-     * @param year Year
-     * @param month Month, 0-based
-     * @param date, day of month
-     * @param hourOfDay, hour, from 0 to 23
-     * @param minute, from 0 to 59
-     * @param second, From 0 to 59
-     * @param milliseconds, from 0 to 999
-     * @return the time stamp of this instant in GMT calendar
-     */
-    @SuppressLint("DirectGregorianInstantiation")
-    public static long timeStamp(int year, int month, int date, int hourOfDay, int minute, int second, int milliseconds) {
-        TimeZone timeZone = TimeZone.getTimeZone("GMT");
-        Calendar gregorianCalendar = new GregorianCalendar(year, month, date, hourOfDay, minute, second);
-        gregorianCalendar.setTimeZone(timeZone);
-        gregorianCalendar.set(Calendar.MILLISECOND, milliseconds);
-        return (gregorianCalendar.getTimeInMillis());
+        /**
+         * Allow to get a timestamp which is independent of place where test occurs.
+         * @param year Year
+         * @param month Month, 0-based
+         * @param date, day of month
+         * @param hourOfDay, hour, from 0 to 23
+         * @param minute, from 0 to 59
+         * @param second, From 0 to 59
+         * @param milliseconds, from 0 to 999
+         * @return the time stamp of this instant in GMT calendar
+         */
+        @JvmStatic
+        @SuppressLint("DirectGregorianInstantiation")
+        fun timeStamp(year: Int, month: Int, date: Int, hourOfDay: Int, minute: Int, second: Int, milliseconds: Int): Long {
+            val timeZone = TimeZone.getTimeZone("GMT")
+            val gregorianCalendar: Calendar = GregorianCalendar(year, month, date, hourOfDay, minute, second)
+            gregorianCalendar.timeZone = timeZone
+            gregorianCalendar[Calendar.MILLISECOND] = milliseconds
+            return gregorianCalendar.timeInMillis
+        }
     }
 }
