df7989fe0f3d3f4aa71d73474c273f283b7e334d
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-15 14:53:03 +0100

    refactor: Convert CollectionTaskSearchCardsTest to Kotlin
    com.ichi2.async.CollectionTaskSearchCardsTest

--- AnkiDroid/src/test/java/com/ichi2/async/CollectionTaskSearchCardsTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/async/CollectionTaskSearchCardsTest.kt
@@ -13,55 +13,48 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.async
 
-package com.ichi2.async;
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.anki.CardBrowser.CardCache
+import com.ichi2.anki.RunInBackground
+import com.ichi2.anki.servicelayer.SearchService.SearchCardsResult
+import com.ichi2.async.CollectionTask.SearchCards
+import com.ichi2.libanki.SortOrder.NoOrdering
+import com.ichi2.utils.KotlinCleanup
+import org.hamcrest.MatcherAssert.*
+import org.hamcrest.Matchers.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.*
+import org.mockito.kotlin.argumentCaptor
 
-import com.ichi2.anki.CardBrowser;
-import com.ichi2.anki.RunInBackground;
-import com.ichi2.anki.servicelayer.SearchService.SearchCardsResult;
-import com.ichi2.libanki.SortOrder;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-
-import java.util.List;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
-@RunWith(AndroidJUnit4.class)
-public class CollectionTaskSearchCardsTest extends AbstractCollectionTaskTest {
-
-    @SuppressWarnings("unchecked")
+@RunWith(AndroidJUnit4::class)
+@KotlinCleanup("is -> equalTo")
+@KotlinCleanup("scope functions for argument matchers")
+class CollectionTaskSearchCardsTest : AbstractCollectionTaskTest() {
     @Test
     @RunInBackground
-    public void searchCardsNumberOfResultCount() {
-        addNoteUsingBasicModel("Hello", "World");
-        addNoteUsingBasicModel("One", "Two");
+    fun searchCardsNumberOfResultCount() {
+        addNoteUsingBasicModel("Hello", "World")
+        addNoteUsingBasicModel("One", "Two")
 
-        int cardsToRender = 1;
-        int numberOfCards = 2;
+        val cardsToRender = 1
+        val numberOfCards = 2
 
-        CollectionTask.SearchCards task = new CollectionTask.SearchCards("", new SortOrder.NoOrdering(), cardsToRender, 0, 0);
-        TaskListener<List<CardBrowser.CardCache>, SearchCardsResult> listener = mock(TaskListener.class);
+        val task = SearchCards("", NoOrdering(), cardsToRender, 0, 0)
+        @Suppress("UNCHECKED_CAST")
+        val listener: TaskListener<List<CardCache>, SearchCardsResult> = mock(TaskListener::class.java) as TaskListener<List<CardCache>, SearchCardsResult>
 
-        waitForTask(task, listener);
+        waitForTask(task, listener)
 
-        verify(listener, times(1)).onPreExecute();
+        verify(listener, times(1)).onPreExecute()
+        val argumentCaptor = argumentCaptor<List<CardCache>>()
+        verify(listener, times(1)).onProgressUpdate(argumentCaptor.capture())
+        assertThat("OnProgress sends the provided number of cards to render", argumentCaptor.firstValue.size, `is`(cardsToRender))
 
-        ArgumentCaptor<List<CardBrowser.CardCache>> argumentCaptor = ArgumentCaptor.forClass(List.class);
-        verify(listener, times(1)).onProgressUpdate(argumentCaptor.capture());
-        assertThat("OnProgress sends the provided number of cards to render", argumentCaptor.getValue().size(), is(cardsToRender));
-
-        ArgumentCaptor<SearchCardsResult> argumentCaptor2 = ArgumentCaptor.forClass(SearchCardsResult.class);
-        verify(listener, times(1)).onPostExecute(argumentCaptor2.capture());
-        assertThat("All cards should be provided on Post Execute", argumentCaptor2.getValue().size(), is(numberOfCards));
-
+        val argumentCaptor2 = argumentCaptor<SearchCardsResult>()
+        verify(listener, times(1)).onPostExecute(argumentCaptor2.capture())
+        assertThat("All cards should be provided on Post Execute", argumentCaptor2.firstValue.size(), `is`(numberOfCards))
     }
 }
