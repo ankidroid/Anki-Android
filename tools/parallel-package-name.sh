12645d037c107153b0b37403f2730a04d958b526
Brayan Oliveira <69634269+BrayanDSO@users.noreply.github.com>
2022-04-15 05:57:03 -0300

    Fix parallel package app name
    A comment added to the app_name constant broke its configuration by parallel-package-name.sh
    
    This fixes it by changing the Regex

--- tools/parallel-package-name.sh
+++ tools/parallel-package-name.sh
@@ -11,9 +11,8 @@ MANIFEST="AndroidManifest.xml"
 CONSTANTS="res/values/constants.xml"
 
 OLD_ID=com.ichi2.anki
-OLD_NAME=AnkiDroid
 
-perl -pi -e "s/name=\"app_name\">$OLD_NAME/name=\"app_name\">$NEW_NAME/g" $ROOT$CONSTANTS
+perl -pi -e "s/name=\"app_name\"(.*?)>.*?<\/string>/name=\"app_name\"\1>$NEW_NAME<\/string>/g" $ROOT$CONSTANTS
 perl -pi -e "s/applicationId \"$OLD_ID/applicationId \"$NEW_ID/g" AnkiDroid/build.gradle
 perl -pi -e "s/android:authorities=\"$OLD_ID/android:authorities=\"$NEW_ID/g" $ROOT$MANIFEST
 perl -pi -e "s/permission android:name=\"$OLD_ID.permission/permission android:name=\"$NEW_ID.permission/g" $ROOT$MANIFEST
