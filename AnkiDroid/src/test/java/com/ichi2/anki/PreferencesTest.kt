7620c69623efdbcf44a21e75eca5cde8ce75e34b
Divyansh Kushwaha <kushwaha.divyansh.dxn@gmail.com>
2022-04-16 21:42:31 +0530

    Improved Preferences instantiation

--- AnkiDroid/src/test/java/com/ichi2/anki/PreferencesTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/PreferencesTest.kt
@@ -18,17 +18,24 @@ package com.ichi2.anki
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.ichi2.anki.Preferences.Companion.getDayOffset
 import com.ichi2.anki.exception.ConfirmModSchemaException
-import com.ichi2.utils.KotlinCleanup
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class PreferencesTest : RobolectricTest() {
+    private lateinit var preferences: Preferences
+
+    @Before
+    fun setup() {
+        preferences = Preferences()
+        preferences.attachBaseContext(targetContext)
+    }
+
     @Test
     fun testDayOffsetExhaustive() {
-        val preferences = instance
         for (i in 0..23) {
             preferences.setDayOffset(i)
             assertThat(getDayOffset(col), equalTo(i))
@@ -39,7 +46,6 @@ class PreferencesTest : RobolectricTest() {
     @Throws(ConfirmModSchemaException::class)
     fun testDayOffsetExhaustiveV2() {
         col.changeSchedulerVer(2)
-        val preferences = instance
         for (i in 0..23) {
             preferences.setDayOffset(i)
             assertThat(getDayOffset(col), equalTo(i))
@@ -50,18 +56,9 @@ class PreferencesTest : RobolectricTest() {
     @Throws(ConfirmModSchemaException::class)
     fun setDayOffsetSetsConfig() {
         col.changeSchedulerVer(2)
-        val preferences = instance
         val offset = getDayOffset(col)
         assertThat("Default offset should be 4", offset, equalTo(4))
         preferences.setDayOffset(2)
         assertThat("rollover config should be set to new value", col.get_config("rollover", 4.toInt()), equalTo(2))
     }
-
-    @KotlinCleanup("use scope function")
-    val instance: Preferences
-        get() {
-            val preferences = Preferences()
-            preferences.attachBaseContext(targetContext)
-            return preferences
-        }
 }
