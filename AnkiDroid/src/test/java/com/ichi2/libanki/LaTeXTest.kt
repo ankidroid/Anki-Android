72451de06db68ebbd7bfdbde7365e4f1d627a220
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-17 16:50:56 +0100

    refactor: Convert LaTeXTest to Kotlin
    com.ichi2.libanki.LaTeXTest

--- AnkiDroid/src/test/java/com/ichi2/libanki/LaTeXTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/libanki/LaTeXTest.kt
@@ -13,68 +13,65 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.libanki
 
-package com.ichi2.libanki;
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.anki.RobolectricTest
+import com.ichi2.utils.KotlinCleanup
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import com.ichi2.anki.RobolectricTest;
-
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.Matchers;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-
-
-@RunWith(AndroidJUnit4.class)
-public class LaTeXTest extends RobolectricTest {
-
-    public static class MockMedia extends Media {
-        public MockMedia(Collection col) {
-            super(col, false);
-        }
-
-
+@RunWith(AndroidJUnit4::class)
+@KotlinCleanup("IDE lint")
+@KotlinCleanup("`is` -> equalTo")
+class LaTeXTest : RobolectricTest() {
+    class MockMedia(col: Collection?) : Media(col, false) {
         /**
          * @param fname A field name
          * @return Always true, given that we want to assume the field exists in test
          */
-        @Override
-        public boolean have(String fname) {
-            return true;
+        override fun have(fname: String): Boolean {
+            return true
         }
     }
 
     @Test
-    public void  _imgLinkTest() {
-        Collection col = getCol();
-        Media m = new MockMedia(col);
-        Model model = col.getModels().byName("Basic");
+    fun _imgLinkTest() {
+        val col = col
+        val m: Media = MockMedia(col)
+        val model = col.models.byName("Basic")
         // The hashing function should never change, as it would broke link. So hard coding the expected hash value is valid
-        // Test with media access
-        assertThat(LaTeX._imgLink("$\\sqrt[3]{2} + \\text{\"var\"}$", model, m),
-                Matchers.is("<img class=latex alt=\"\\$\\\\sqrt[3]{2} + \\\\text{&quot;var&quot;}\\$\" src=\"latex-dd84e5d506179a137f7924d0960609a8c89d491e.png\">"));
+        //  Test with media access
+        assertThat(
+            LaTeX._imgLink("$\\sqrt[3]{2} + \\text{\"var\"}$", model, m),
+            `is`("<img class=latex alt=\"\\$\\\\sqrt[3]{2} + \\\\text{&quot;var&quot;}\\$\" src=\"latex-dd84e5d506179a137f7924d0960609a8c89d491e.png\">")
+        )
 
         // Test without access to media
-        assertThat(LaTeX._imgLink("$\\sqrt[3]{2} + \\text{\"var\"}$", model, col.getMedia()),
-                Matchers.is("\\$\\\\sqrt[3]{2} + \\\\text{\"var\"}\\$"));
+        assertThat(
+            LaTeX._imgLink("$\\sqrt[3]{2} + \\text{\"var\"}$", model, col.media),
+            `is`("\\$\\\\sqrt[3]{2} + \\\\text{\"var\"}\\$")
+        )
     }
 
     @Test
-    public void  mungeQATest() {
-        Collection col = getCol();
-        Media m = new MockMedia(col);
-        Model model = col.getModels().byName("Basic");
+    fun mungeQATest() {
+        val col = col
+        val m: Media = MockMedia(col)
+        val model = col.models.byName("Basic")
 
-        // Test with media access
-        assertThat(LaTeX.mungeQA("[$]\\sqrt[3]{2} + \\text{\"var\"}[/$]", m, model),
-                Matchers.is("<img class=latex alt=\"$\\sqrt[3]{2} + \\text{&quot;var&quot;}$\" src=\"latex-dd84e5d506179a137f7924d0960609a8c89d491e.png\">"));
+        //  Test with media access
+        assertThat(
+            LaTeX.mungeQA("[$]\\sqrt[3]{2} + \\text{\"var\"}[/$]", m, model),
+            `is`("<img class=latex alt=\"$\\sqrt[3]{2} + \\text{&quot;var&quot;}$\" src=\"latex-dd84e5d506179a137f7924d0960609a8c89d491e.png\">")
+        )
 
         // Test without access to media
-        assertThat(LaTeX.mungeQA("[$]\\sqrt[3]{2} + \\text{\"var\"}[/$]", col, model),
-                Matchers.is("$\\sqrt[3]{2} + \\text{\"var\"}$"));
+        assertThat(
+            LaTeX.mungeQA("[$]\\sqrt[3]{2} + \\text{\"var\"}[/$]", col, model),
+            `is`("$\\sqrt[3]{2} + \\text{\"var\"}$")
+        )
     }
 }
