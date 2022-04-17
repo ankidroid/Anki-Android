66e9e98245ad92d5d0df3ecb676e8ee10e3e4aeb
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-17 16:54:47 +0100

    refactor: Convert LegacyDecksTest to Kotlin
    com.ichi2.libanki.LegacyDecksTest

--- AnkiDroid/src/test/java/com/ichi2/libanki/LegacyDecksTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/libanki/LegacyDecksTest.kt
@@ -13,122 +13,114 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.libanki
 
-package com.ichi2.libanki;
-
-import com.ichi2.anki.RobolectricTest;
-import com.ichi2.libanki.backend.exception.DeckRenameException;
-import com.ichi2.utils.JSONObject;
-
-import net.ankiweb.rsdroid.RustCleanup;
-
-import org.apache.http.util.Asserts;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThrows;
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.anki.RobolectricTest
+import com.ichi2.libanki.backend.exception.DeckRenameException
+import com.ichi2.testutils.assertThrows
+import com.ichi2.utils.JSONObject
+import com.ichi2.utils.KotlinCleanup
+import net.ankiweb.rsdroid.RustCleanup
+import org.hamcrest.MatcherAssert.*
+import org.hamcrest.Matchers
+import org.junit.Test
+import org.junit.runner.RunWith
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 
 @RustCleanup("Can be removed once we sunset the Java backend")
-@RunWith(AndroidJUnit4.class)
-public class LegacyDecksTest extends RobolectricTest {
-
+@KotlinCleanup("IDE-based lint")
+@RunWith(AndroidJUnit4::class)
+class LegacyDecksTest : RobolectricTest() {
     @Test
-    public void testEnsureParents() throws DeckRenameException {
-        Decks decks = getDecks();
-        decks.id("test");
-        String subsubdeck_name = decks._ensureParents("  tESt :: sub :: subdeck");
-        assertEquals("test::sub:: subdeck", subsubdeck_name);// Only parents are renamed, not the last deck.
-        assertNotNull(decks.byName("test::sub"));
-        assertNull(decks.byName("test::sub:: subdeck"));
-        assertNull(decks.byName("  test :: sub :: subdeck"));
-        assertNull(decks.byName("  test :: sub "));
+    @Throws(DeckRenameException::class)
+    fun testEnsureParents() {
+        val decks = decks
+        decks.id("test")
+        val subsubdeck_name = decks._ensureParents("  tESt :: sub :: subdeck")
+        assertEquals("test::sub:: subdeck", subsubdeck_name) // Only parents are renamed, not the last deck.
+        assertNotNull(decks.byName("test::sub"))
+        assertNull(decks.byName("test::sub:: subdeck"))
+        assertNull(decks.byName("  test :: sub :: subdeck"))
+        assertNull(decks.byName("  test :: sub "))
 
-        decks.newDyn("filtered");
-        assertThrows(DeckRenameException.class, () -> decks._ensureParents("filtered:: sub :: subdeck"));
-    }
-
-
-    @Test
-    public void testEnsureParentsNotFiltered() throws DeckRenameException {
-        Decks decks = getDecks();
-        decks.id("test");
-        String subsubdeck_name = decks._ensureParentsNotFiltered("  tESt :: sub :: subdeck");
-        assertEquals("test::sub:: subdeck", subsubdeck_name);// Only parents are renamed, not the last deck.
-        assertNotNull(decks.byName("test::sub"));
-        assertNull(decks.byName("test::sub:: subdeck"));
-        assertNull(decks.byName("  test :: sub :: subdeck"));
-        assertNull(decks.byName("  test :: sub "));
-
-        decks.newDyn("filtered");
-        String filtered_subdeck_name = decks._ensureParentsNotFiltered("filtered:: sub :: subdeck");
-        assertEquals("filtered'::sub:: subdeck", filtered_subdeck_name);// Only parents are renamed, not the last deck.
-        assertNotNull(decks.byName("filtered'::sub"));
-        assertNotNull(decks.byName("filtered'"));
-        assertNull(decks.byName("filtered::sub:: subdeck"));
-        assertNull(decks.byName("filtered::sub"));
-    }
-
-
-
-    @Test
-    public void duplicateName() {
-        Decks decks = getDecks();
-        decks.load("{\"2\": {\"name\": \"A\", \"id\":2}, \"3\": {\"name\": \"A\", \"id\":3}, \"4\": {\"name\": \"A::B\", \"id\":4}}", "{}");
-        decks.checkIntegrity();
-        JSONObject deckA = decks.byName("A");
-        Asserts.notNull(deckA, "A deck with name \"A\" should still exists");
-        assertThat("A deck with name \"A\" should have name \"A\"", deckA.getString("name"), is("A"));
-        JSONObject deckAPlus = decks.byName("A+");
-        Asserts.notNull(deckAPlus, "A deck with name \"A+\" should still exists");
+        decks.newDyn("filtered")
+        assertThrows<DeckRenameException> { decks._ensureParents("filtered:: sub :: subdeck") }
     }
 
     @Test
-    public void descendantOfFiltered() throws DeckRenameException {
-        Decks decks = getDecks();
-        long filtered_id = decks.newDyn("filtered");
-        assertThrows(DeckRenameException.class,  () -> decks.id("filtered::subdeck::subsubdeck"));
+    @Throws(DeckRenameException::class)
+    fun testEnsureParentsNotFiltered() {
+        val decks = decks
+        decks.id("test")
+        val subsubdeck_name = decks._ensureParentsNotFiltered("  tESt :: sub :: subdeck")
+        assertEquals("test::sub:: subdeck", subsubdeck_name) // Only parents are renamed, not the last deck.
+        assertNotNull(decks.byName("test::sub"))
+        assertNull(decks.byName("test::sub:: subdeck"))
+        assertNull(decks.byName("  test :: sub :: subdeck"))
+        assertNull(decks.byName("  test :: sub "))
 
-        long subdeck_id = decks.id_safe("filtered::subdeck::subsubdeck");
-        Deck subdeck = decks.get(subdeck_id);
-        assertEquals("filtered'::subdeck::subsubdeck", subdeck.getString("name"));
+        decks.newDyn("filtered")
+        val filtered_subdeck_name = decks._ensureParentsNotFiltered("filtered:: sub :: subdeck")
+        assertEquals("filtered'::sub:: subdeck", filtered_subdeck_name) // Only parents are renamed, not the last deck.
+        assertNotNull(decks.byName("filtered'::sub"))
+        assertNotNull(decks.byName("filtered'"))
+        assertNull(decks.byName("filtered::sub:: subdeck"))
+        assertNull(decks.byName("filtered::sub"))
     }
 
-    protected Decks getDecks() {
-        Collection col = getCol();
+    @Test
+    fun duplicateName() {
+        val decks = decks
+        decks.load("{\"2\": {\"name\": \"A\", \"id\":2}, \"3\": {\"name\": \"A\", \"id\":3}, \"4\": {\"name\": \"A::B\", \"id\":4}}", "{}")
+        decks.checkIntegrity()
+        val deckA: JSONObject? = decks.byName("A")
+        assertNotNull(deckA, "A deck with name \"A\" should still exists")
+        assertThat("A deck with name \"A\" should have name \"A\"", deckA.getString("name"), Matchers.`is`("A"))
+        val deckAPlus: JSONObject? = decks.byName("A+")
+        assertNotNull(deckAPlus, "A deck with name \"A+\" should still exists")
+    }
 
-        if (col.getDecks() instanceof Decks) {
-            return (Decks) col.getDecks();
+    @Test
+    @Throws(DeckRenameException::class)
+    fun descendantOfFiltered() {
+        val decks = decks
+        decks.newDyn("filtered")
+        assertThrows<DeckRenameException> { decks.id("filtered::subdeck::subsubdeck") }
+
+        val subdeck_id = decks.id_safe("filtered::subdeck::subsubdeck")
+        val subdeck = decks.get(subdeck_id)
+        assertEquals("filtered'::subdeck::subsubdeck", subdeck.getString("name"))
+    }
+
+    // following copied from storage:: _setColVars
+    protected val decks: Decks
+        get() {
+            val col = col
+            if (col.decks is Decks) {
+                return col.decks as Decks
+            }
+            val decks = Decks(col)
+
+            // following copied from storage:: _setColVars
+
+            val defaultDeck = JSONObject(Decks.DEFAULT_DECK)
+            defaultDeck.put("id", 1)
+            defaultDeck.put("name", "Default")
+            defaultDeck.put("conf", 1)
+            defaultDeck.put("mod", col.time.intTime())
+
+            val allDecks = JSONObject()
+            allDecks.put("1", defaultDeck)
+            val gc = JSONObject(Decks.DEFAULT_CONF)
+            gc.put("id", 1)
+            val allDeckConfig = JSONObject()
+            allDeckConfig.put("1", gc)
+
+            decks.load(Utils.jsonToString(allDecks), Utils.jsonToString(allDeckConfig))
+
+            return decks
         }
-
-        Decks decks = new Decks(col);
-
-        // following copied from storage:: _setColVars
-
-        JSONObject defaultDeck = new JSONObject(Decks.DEFAULT_DECK);
-        defaultDeck.put("id", 1);
-        defaultDeck.put("name", "Default");
-        defaultDeck.put("conf", 1);
-        defaultDeck.put("mod", col.getTime().intTime());
-
-        JSONObject allDecks = new JSONObject();
-        allDecks.put("1", defaultDeck);
-
-
-        JSONObject gc = new JSONObject(Decks.DEFAULT_CONF);
-        gc.put("id", 1);
-        JSONObject allDeckConfig = new JSONObject();
-        allDeckConfig.put("1", gc);
-
-        decks.load(Utils.jsonToString(allDecks), Utils.jsonToString(allDeckConfig));
-
-
-        return decks;
-    }
 }
