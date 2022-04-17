d0abd5df3cbccbef796a9c10cd4a51933b70b6ea
Brayan Oliveira <69634269+BrayanDSO@users.noreply.github.com>
2022-04-17 11:37:34 -0300

    Convert ListUtil.kt to Kotlin com.ichi2.utils.ListUtil

--- AnkiDroid/src/test/java/com/ichi2/utils/ListUtil.kt
+++ AnkiDroid/src/test/java/com/ichi2/utils/ListUtil.kt
@@ -13,68 +13,70 @@
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
-package com.ichi2.utils;
+package com.ichi2.utils
 
-import org.junit.Test;
-import org.junit.internal.ArrayComparisonFailure;
+import org.junit.Assert
+import org.junit.Assert.assertThrows
+import org.junit.Test
+import org.junit.internal.ArrayComparisonFailure
+import java.lang.AssertionError
+import java.util.*
+import kotlin.Throws
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertThrows;
-
-public class ListUtil {
-
-
-    /**
-     * Asserts that two object lists are equal (same size and components in same order). If they are not, an
-     * {@link AssertionError} is thrown with the given message. It states "array" instead of list If
-     * <code>expecteds</code> and <code>actuals</code> are <code>null</code>,
-     * they are considered equal.
-     *
-     * @param message the identifying message for the {@link AssertionError} (<code>null</code>
-     * okay)
-     * @param expecteds Object list or list of arrays (multi-dimensional array) with
-     * expected values.
-     * @param actuals Object list or list of arrays (multi-dimensional array) with
-     * actual values
-     */
-    public static <T>  void assertListEquals(String message, List<T> expecteds,
-                                         List<T> actuals) throws ArrayComparisonFailure {
-        Object[] expecteds_array = (expecteds == null) ? null : expecteds.toArray();
-        Object[] actuals_array = (actuals == null) ? null : actuals.toArray();
-        assertArrayEquals(message, expecteds_array, actuals_array);
-    }
-
-    /**
-     * Asserts that two object arrays are equal. If they are not, an
-     * {@link AssertionError} is thrown. If <code>expected</code> and
-     * <code>actual</code> are <code>null</code>, they are considered
-     * equal.
-     *
-     * @param expecteds Object list or list of arrays (multi-dimensional array) with
-     * expected values
-     * @param actuals Object list or list of arrays (multi-dimensional array) with
-     * actual values
-     */
-    public static <T> void assertListEquals(List<T> expecteds,
-                                        List<T> actuals) {
-        assertListEquals(null, expecteds, actuals);
-    }
-
+class ListUtil {
     @Test
-    public void EqualsTest() {
-        assertThrows(ArrayComparisonFailure.class, () -> assertListEquals(Arrays.asList(2L, 3L), Arrays.asList(2L, 4L)));
-        assertThrows(ArrayComparisonFailure.class, () -> assertListEquals(Arrays.asList(2L, 3L), Arrays.asList(2L)));
-        assertThrows(ArrayComparisonFailure.class, () -> assertListEquals(Arrays.asList(2L, 3L), Arrays.asList(5L)));
-        assertThrows(ArrayComparisonFailure.class, () -> assertListEquals(Arrays.asList(2L, 3L), Arrays.asList(2L, 3L, 5L)));
-        assertThrows(AssertionError.class, () -> assertListEquals(Arrays.asList(2L, 3L), null));
-        assertThrows(AssertionError.class, () -> assertListEquals(null, Arrays.asList(2L, 4L)));
-        assertListEquals(null, null);
-        assertListEquals(Arrays.asList(2L, 3L), Arrays.asList(2L, 3L));
+    @KotlinCleanup("Use Kotlin's methods instead of Arrays.asList")
+    @KotlinCleanup("Use AnkiAssert.assertThrows<>")
+    fun EqualsTest() {
+        assertThrows(ArrayComparisonFailure::class.java) { assertListEquals(Arrays.asList(2L, 3L), Arrays.asList(2L, 4L)) }
+        assertThrows(ArrayComparisonFailure::class.java) { assertListEquals(Arrays.asList(2L, 3L), Arrays.asList(2L)) }
+        assertThrows(ArrayComparisonFailure::class.java) { assertListEquals(Arrays.asList(2L, 3L), Arrays.asList(5L)) }
+        assertThrows(ArrayComparisonFailure::class.java) { assertListEquals(Arrays.asList(2L, 3L), Arrays.asList(2L, 3L, 5L)) }
+        assertThrows(AssertionError::class.java) { assertListEquals(Arrays.asList(2L, 3L), null) }
+        assertThrows(AssertionError::class.java) { assertListEquals(null, Arrays.asList(2L, 4L)) }
+        assertListEquals(null, null)
+        assertListEquals(Arrays.asList(2L, 3L), Arrays.asList(2L, 3L))
     }
 
+    companion object {
+        /**
+         * Asserts that two object lists are equal (same size and components in same order). If they are not, an
+         * [AssertionError] is thrown with the given message. It states "array" instead of list If
+         * `expecteds` and `actuals` are `null`,
+         * they are considered equal.
+         *
+         * @param message the identifying message for the [AssertionError] (`null`
+         * okay)
+         * @param expecteds Object list or list of arrays (multi-dimensional array) with
+         * expected values.
+         * @param actuals Object list or list of arrays (multi-dimensional array) with
+         * actual values
+         */
+        @Throws(ArrayComparisonFailure::class)
+        fun assertListEquals(
+            message: String?,
+            expecteds: List<Any>?,
+            actuals: List<Any>?
+        ) {
+            val expecteds_array: Array<Any>? = expecteds?.toTypedArray()
+            val actuals_array: Array<Any>? = actuals?.toTypedArray()
+            Assert.assertArrayEquals(message, expecteds_array, actuals_array)
+        }
+
+        /**
+         * Asserts that two object arrays are equal. If they are not, an
+         * [AssertionError] is thrown. If `expected` and
+         * `actual` are `null`, they are considered
+         * equal.
+         *
+         * @param expecteds Object list or list of arrays (multi-dimensional array) with
+         * expected values
+         * @param actuals Object list or list of arrays (multi-dimensional array) with
+         * actual values
+         */
+        @JvmStatic
+        fun assertListEquals(expecteds: List<Any>?, actuals: List<Any>?) {
+            assertListEquals(null, expecteds, actuals)
+        }
+    }
 }
