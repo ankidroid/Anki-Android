aa1cca72031911f4d87d048a3a6089485b1764dc
Brayan Oliveira <69634269+BrayanDSO@users.noreply.github.com>
2022-04-17 11:11:36 -0300

    Convert ImageFieldTest.kt to Kotlin com.ichi2.anki.multimediacard.fields.ImageFieldTest

--- AnkiDroid/src/test/java/com/ichi2/anki/multimediacard/fields/ImageFieldTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/multimediacard/fields/ImageFieldTest.kt
@@ -13,111 +13,104 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.anki.multimediacard.fields
 
-package com.ichi2.anki.multimediacard.fields;
+import androidx.annotation.CheckResult
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.libanki.Collection
+import com.ichi2.libanki.Media
+import com.ichi2.utils.KotlinCleanup
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+import java.io.File
 
-import com.ichi2.libanki.Collection;
-import com.ichi2.libanki.Media;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-
-import java.io.File;
-
-import androidx.annotation.CheckResult;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-@RunWith(AndroidJUnit4.class)
-public class ImageFieldTest {
-
-    /** #5237 - quotation marks on Android differed from Windows */
+@RunWith(AndroidJUnit4::class)
+@KotlinCleanup("`when` -> whenever")
+@KotlinCleanup("Remove redundants Mockito. qualifiers")
+class ImageFieldTest {
+    /** #5237 - quotation marks on Android differed from Windows  */
     @Test
-    public void imageValueIsConsistentWithAnkiDesktop() {
-        //Arrange
-        File mockedFile = Mockito.mock(File.class);
-        Mockito.when(mockedFile.exists()).thenReturn(true);
-        Mockito.when(mockedFile.getName()).thenReturn("paste-abc.jpg");
+    fun imageValueIsConsistentWithAnkiDesktop() {
+        // Arrange
+        val mockedFile = Mockito.mock(File::class.java)
+        Mockito.`when`(mockedFile.exists()).thenReturn(true)
+        Mockito.`when`(mockedFile.name).thenReturn("paste-abc.jpg")
 
-        //Act
-        String actual = ImageField.formatImageFileName(mockedFile);
+        // Act
+        val actual = ImageField.formatImageFileName(mockedFile)
 
-        //Assert
-        //This differs between AnkDesktop Version 2.0.51 and 2.1.22
-        //2.0:  "<img src=\"paste-abc.jpg\" />";
-        //2.1: (note: no trailing slash or space)
-        String expected = "<img src=\"paste-abc.jpg\">";
-        assertThat(actual, equalTo(expected));
+        // Assert
+        // This differs between AnkDesktop Version 2.0.51 and 2.1.22
+        // 2.0:  "<img src=\"paste-abc.jpg\" />";
+        // 2.1: (note: no trailing slash or space)
+        val expected = "<img src=\"paste-abc.jpg\">"
+        assertThat(actual, equalTo(expected))
     }
 
     @Test
-    public void validImageParses() {
-        String goodImage = "<img src='img_202003291657428441724378214970132.png'/>";
-        String imageSrc = ImageField.parseImageSrcFromHtml(goodImage);
-        assertThat(imageSrc, equalTo("img_202003291657428441724378214970132.png"));
+    fun validImageParses() {
+        val goodImage = "<img src='img_202003291657428441724378214970132.png'/>"
+        val imageSrc = ImageField.parseImageSrcFromHtml(goodImage)
+        assertThat(imageSrc, equalTo("img_202003291657428441724378214970132.png"))
     }
 
     @Test
-    public void testImageSubstringParsing() {
-        //5874 - previously failed
-        String previouslyBadImage = "<img src='img_202003291657428441724378214970132.png'/>aaaa'/>";
-        String imageSrc = ImageField.parseImageSrcFromHtml(previouslyBadImage);
-        assertThat(imageSrc, equalTo("img_202003291657428441724378214970132.png"));
+    fun testImageSubstringParsing() {
+        // 5874 - previously failed
+        val previouslyBadImage = "<img src='img_202003291657428441724378214970132.png'/>aaaa'/>"
+        val imageSrc = ImageField.parseImageSrcFromHtml(previouslyBadImage)
+        assertThat(imageSrc, equalTo("img_202003291657428441724378214970132.png"))
     }
 
     @Test
-    public void firstImageIsSelected() {
-        String goodImage = "<img src='1.png'/>aa<img src='2.png'/>";
-        String imageSrc = ImageField.parseImageSrcFromHtml(goodImage);
-        assertThat(imageSrc, equalTo("1.png"));
+    fun firstImageIsSelected() {
+        val goodImage = "<img src='1.png'/>aa<img src='2.png'/>"
+        val imageSrc = ImageField.parseImageSrcFromHtml(goodImage)
+        assertThat(imageSrc, equalTo("1.png"))
     }
 
     @Test
-    public void testNoImage() {
-        String knownBadImage = "<br />";
-        String imageSrc = ImageField.parseImageSrcFromHtml(knownBadImage);
-        assertThat(imageSrc, equalTo(""));
+    fun testNoImage() {
+        val knownBadImage = "<br />"
+        val imageSrc = ImageField.parseImageSrcFromHtml(knownBadImage)
+        assertThat(imageSrc, equalTo(""))
     }
 
     @Test
-    public void testEmptyImage() {
-        String knownBadImage = "<img />";
-        String imageSrc = ImageField.parseImageSrcFromHtml(knownBadImage);
-        assertThat(imageSrc, equalTo(""));
+    fun testEmptyImage() {
+        val knownBadImage = "<img />"
+        val imageSrc = ImageField.parseImageSrcFromHtml(knownBadImage)
+        assertThat(imageSrc, equalTo(""))
     }
 
     @Test
-    public void testNoImagePathIsNothing() {
-        String knownBadImage = "<br />";
-        Collection col = collectionWithMediaDirectory("media");
-
-        String imageSrc = ImageField.getImageFullPath(col, knownBadImage);
-
-        assertThat("no media should return no paths", imageSrc, equalTo(""));
+    fun testNoImagePathIsNothing() {
+        val knownBadImage = "<br />"
+        val col = collectionWithMediaDirectory("media")
+        val imageSrc = ImageField.getImageFullPath(col, knownBadImage)
+        assertThat("no media should return no paths", imageSrc, equalTo(""))
     }
 
     @Test
-    public void testNoImagePathConcat() {
-        String goodImage = "<img src='1.png'/>";
-        Collection col = collectionWithMediaDirectory("media");
-
-        String imageSrc = ImageField.getImageFullPath(col, goodImage);
-
-        assertThat("Valid media should have path", imageSrc, equalTo("media/1.png"));
+    fun testNoImagePathConcat() {
+        val goodImage = "<img src='1.png'/>"
+        val col = collectionWithMediaDirectory("media")
+        val imageSrc = ImageField.getImageFullPath(col, goodImage)
+        assertThat("Valid media should have path", imageSrc, equalTo("media/1.png"))
     }
 
     @CheckResult
-    protected Collection collectionWithMediaDirectory(String dir) {
-        Media media = mock(Media.class);
-        when(media.dir()).thenReturn(dir);
+    protected fun collectionWithMediaDirectory(dir: String): Collection {
+        val media = mock(Media::class.java)
+        `when`(media.dir()).thenReturn(dir)
 
-        Collection collectionMock = mock(Collection.class);
-        when(collectionMock.getMedia()).thenReturn(media);
-        return collectionMock;
+        val collectionMock = mock(Collection::class.java)
+        `when`(collectionMock.media).thenReturn(media)
+        return collectionMock
     }
 }
