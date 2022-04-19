a806cf4658ab1ef0fd575442fc2c7761a97f842a
oyeraghib <raghib.khan76@gmail.com>
2022-04-20 02:21:14 +0530

    Migrate CollectionUtilsTest to Kotlin

---
+++ AnkiDroid/src/test/java/com/ichi2/utils/CollectionUtilsTest.kt
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (c) 2021 Aditya Srivastav <iamaditya2009@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.ichi2.utils
+
+import com.ichi2.utils.CollectionUtils.getLastListElement
+import com.ichi2.utils.CollectionUtils.addAll
+import com.ichi2.testutils.AnkiAssert
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
+import org.junit.Test
+import java.lang.IndexOutOfBoundsException
+import java.util.ArrayList
+
+class CollectionUtilsTest {
+    var mTestList = arrayListOf<Int>(1,2,3)
+
+    @Test
+    fun testGetLastListElement() {
+        MatcherAssert.assertThat(getLastListElement(mTestList), CoreMatchers.`is`(3))
+    }
+
+    @Test(expected = IndexOutOfBoundsException::class)
+    fun testGetLastOnEmptyList() {
+        val emptyList: List<Int> = ArrayList()
+        getLastListElement(emptyList)
+    }
+
+    @Test
+    fun testAddAll() {
+        val toTest = arrayListOf<Int>()
+        addAll(toTest, mTestList)
+        AnkiAssert.assertEqualsArrayList(arrayOf(1, 2, 3), toTest)
+    }
+}
