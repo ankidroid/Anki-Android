1d35d50ed8701d4be2204859afeaeca77a0deead
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-20 00:41:00 +0100

    refactor: convert FileUtilTest to Kotlin
    com.ichi2.utils.FileUtilTest

--- AnkiDroid/src/test/java/com/ichi2/utils/FileUtilTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/utils/FileUtilTest.kt
@@ -13,165 +13,167 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.utils
 
-package com.ichi2.utils;
+import org.acra.util.IOUtils.*
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert.*
+import org.hamcrest.core.Is.*
+import org.junit.Assert.assertThrows
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.io.File
+import java.io.IOException
+import java.lang.Exception
+import java.util.ArrayList
+import kotlin.Throws
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
-
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Map;
-
-
-import static org.acra.util.IOUtils.writeStringToFile;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
-
-public class FileUtilTest {
-
+@KotlinCleanup("is -> equalTo")
+@KotlinCleanup("IDE lint")
+class FileUtilTest {
+    @JvmField
     @Rule
-    public TemporaryFolder temporaryDirectory = new TemporaryFolder();
-    long mTestDirectorySize;
-
-    private File createSrcFilesForTest(File temporaryRoot, String testDirName) throws Exception {
-        File grandParentDir = new File(temporaryRoot, testDirName);
-        File parentDir = new File(grandParentDir, "parent");
-        File childDir = new File(parentDir, "child");
-        final File childDir2 = new File(parentDir, "child2");
-        final File grandChildDir = new File(childDir, "grandChild");
-        final File grandChild2Dir = new File(childDir2, "grandChild2");
-
-        ArrayList<File> files = new ArrayList<>();
-        files.add(new File(grandParentDir, "file1.txt"));
-        files.add(new File(parentDir, "file2.txt"));
-        files.add(new File(childDir, "file3.txt"));
-        files.add(new File(childDir2, "file4.txt"));
-        files.add(new File(grandChildDir, "file5.txt"));
-        files.add(new File(grandChildDir, "file6.txt"));
-
-        grandChildDir.mkdirs();
-        grandChild2Dir.mkdirs();
-
-        for (int i = 0; i < files.size(); ++i) {
-            final File file = files.get(i);
-            writeStringToFile(file, "File " + (i + 1) + " called " + file.getName());
-            mTestDirectorySize += file.length();
+    var temporaryDirectory = TemporaryFolder()
+    var testDirectorySize: Long = 0
+    @Throws(Exception::class)
+    private fun createSrcFilesForTest(temporaryRoot: File, testDirName: String): File {
+        val grandParentDir = File(temporaryRoot, testDirName)
+        val parentDir = File(grandParentDir, "parent")
+        val childDir = File(parentDir, "child")
+        val childDir2 = File(parentDir, "child2")
+        val grandChildDir = File(childDir, "grandChild")
+        val grandChild2Dir = File(childDir2, "grandChild2")
+        val files = ArrayList<File>()
+        files.add(File(grandParentDir, "file1.txt"))
+        files.add(File(parentDir, "file2.txt"))
+        files.add(File(childDir, "file3.txt"))
+        files.add(File(childDir2, "file4.txt"))
+        files.add(File(grandChildDir, "file5.txt"))
+        files.add(File(grandChildDir, "file6.txt"))
+        grandChildDir.mkdirs()
+        grandChild2Dir.mkdirs()
+        for (i in files.indices) {
+            val file = files[i]
+            writeStringToFile(file, "File " + (i + 1) + " called " + file.name)
+            testDirectorySize += file.length()
         }
-
-        return grandParentDir;
+        return grandParentDir
     }
-    
+
     @Test
-    public void testDirectorySize() throws Exception {
+    @Throws(Exception::class)
+    fun testDirectorySize() {
         // Create temporary root directory for holding test directories
-        File temporaryRootDir = temporaryDirectory.newFolder("tempRootDir");
+        val temporaryRootDir = temporaryDirectory.newFolder("tempRootDir")
 
         // Test for success scenario
-        File dir = createSrcFilesForTest(temporaryRootDir, "dir");
-        assertEquals(FileUtil.getDirectorySize(dir), mTestDirectorySize);
+        val dir = createSrcFilesForTest(temporaryRootDir, "dir")
+        assertEquals(FileUtil.getDirectorySize(dir), testDirectorySize)
 
         // Test for failure scenario by passing a file as an argument instead of a directory
-        assertThrows(IOException.class, () -> FileUtil.getDirectorySize(new File(dir, "file1.txt")));
+        assertThrows(IOException::class.java) { FileUtil.getDirectorySize(File(dir, "file1.txt")) }
     }
 
     @Test
-    public void ensureFileIsDirectoryTest() throws Exception {
+    @Throws(Exception::class)
+    fun ensureFileIsDirectoryTest() {
         // Create temporary root directory for holding test directories
-        File temporaryRootDir = temporaryDirectory.newFolder("tempRootDir");
+        val temporaryRootDir = temporaryDirectory.newFolder("tempRootDir")
 
         // Create test data
-        File testDir = createSrcFilesForTest(temporaryRootDir, "testDir");
+        val testDir = createSrcFilesForTest(temporaryRootDir, "testDir")
 
         // Test for file which exists but isn't a directory
-        org.junit.Assert.assertThrows(IOException.class, () -> FileUtil.ensureFileIsDirectory(new File(testDir, "file1.txt")));
+        assertThrows(IOException::class.java) { FileUtil.ensureFileIsDirectory(File(testDir, "file1.txt")) }
 
         // Test for file which exists and is a directory
-        FileUtil.ensureFileIsDirectory(new File(testDir, "parent"));
+        FileUtil.ensureFileIsDirectory(File(testDir, "parent"))
 
         // Test for directory which doesn't exist, but can be created
-        FileUtil.ensureFileIsDirectory(new File(testDir, "parent2"));
+        FileUtil.ensureFileIsDirectory(File(testDir, "parent2"))
 
         // Test for directory which doesn't exist, and cannot be created
-        Assert.assertThrows(IOException.class, () -> FileUtil.ensureFileIsDirectory(
-                new File(testDir.getAbsolutePath() + File.separator + "file1.txt"
-                        + File.separator + "impossibleDir")));
+        assertThrows(IOException::class.java) {
+            FileUtil.ensureFileIsDirectory(
+                File(
+                    testDir.absolutePath + File.separator + "file1.txt" +
+                        File.separator + "impossibleDir"
+                )
+            )
+        }
     }
 
     @Test
-    public void listFilesTest() throws Exception {
+    @Throws(Exception::class)
+    fun listFilesTest() {
         // Create temporary root directory for holding test directories
-        File temporaryRootDir = temporaryDirectory.newFolder("tempRootDir");
+        val temporaryRootDir = temporaryDirectory.newFolder("tempRootDir")
 
         // Create valid input
-        File testDir = createSrcFilesForTest(temporaryRootDir ,"testDir");
-        ArrayList<File> expectedChildren = new ArrayList<>();
-        expectedChildren.add(new File(testDir, "parent"));
-        expectedChildren.add(new File(testDir, "file1.txt"));
+        val testDir = createSrcFilesForTest(temporaryRootDir, "testDir")
+        val expectedChildren = ArrayList<File>()
+        expectedChildren.add(File(testDir, "parent"))
+        expectedChildren.add(File(testDir, "file1.txt"))
 
-        File[] testDirChildren = FileUtil.listFiles(testDir);
+        val testDirChildren = FileUtil.listFiles(testDir)
 
         // Check that listFiles lists all files in the directory
-        for (final File testDirChild : testDirChildren) {
-             assertTrue(expectedChildren.contains(testDirChild));
+        for (testDirChild in testDirChildren) {
+            assertTrue(expectedChildren.contains(testDirChild))
         }
-        assertEquals(expectedChildren.size(), testDirChildren.length);
+        assertEquals(expectedChildren.size.toLong(), testDirChildren.size.toLong())
 
         // Create invalid input
-        assertThrows(IOException.class, () -> FileUtil.listFiles(new File(testDir, "file1.txt")));
+        assertThrows(IOException::class.java) { FileUtil.listFiles(File(testDir, "file1.txt")) }
     }
 
     @Test
-    public void testFileNameNull() {
-        assertThat(FileUtil.getFileNameAndExtension(null), nullValue());
+    fun testFileNameNull() {
+        assertThat(FileUtil.getFileNameAndExtension(null), CoreMatchers.nullValue())
     }
 
     @Test
-    public void testFileNameEmpty() {
-        assertThat(FileUtil.getFileNameAndExtension(""), nullValue());
+    fun testFileNameEmpty() {
+        assertThat(FileUtil.getFileNameAndExtension(""), CoreMatchers.nullValue())
     }
 
     @Test
-    public void testFileNameNoDot() {
-        assertThat(FileUtil.getFileNameAndExtension("abc"), nullValue());
+    fun testFileNameNoDot() {
+        assertThat(FileUtil.getFileNameAndExtension("abc"), CoreMatchers.nullValue())
     }
 
     @Test
-    public void testFileNameNormal() {
-        Map.Entry<String, String> fileNameAndExtension = FileUtil.getFileNameAndExtension("abc.jpg");
-        assertThat(fileNameAndExtension.getKey(), is("abc"));
-        assertThat(fileNameAndExtension.getValue(), is(".jpg"));
+    fun testFileNameNormal() {
+        val fileNameAndExtension = FileUtil.getFileNameAndExtension("abc.jpg")
+        assertThat(fileNameAndExtension!!.key, `is`("abc"))
+        assertThat(fileNameAndExtension.value, `is`(".jpg"))
     }
 
     @Test
-    public void testFileNameTwoDot() {
-        Map.Entry<String, String> fileNameAndExtension = FileUtil.getFileNameAndExtension("a.b.c");
-        assertThat(fileNameAndExtension.getKey(), is("a.b"));
-        assertThat(fileNameAndExtension.getValue(), is(".c"));
+    fun testFileNameTwoDot() {
+        val fileNameAndExtension = FileUtil.getFileNameAndExtension("a.b.c")
+        assertThat(fileNameAndExtension!!.key, `is`("a.b"))
+        assertThat(fileNameAndExtension.value, `is`(".c"))
     }
 
     @Test
-    public void fileSizeTest() throws IOException {
-        assertThat("deleted file should have 0 size", FileUtil.getSize(new File("test.txt")), is(0L));
+    @Throws(IOException::class)
+    fun fileSizeTest() {
+        assertThat("deleted file should have 0 size", FileUtil.getSize(File("test.txt")), `is`(0L))
 
-        File temporaryRootDir = temporaryDirectory.newFolder("tempRootDir");
+        val temporaryRootDir = temporaryDirectory.newFolder("tempRootDir")
 
-        assertThat("empty directory should have 0 size", FileUtil.getSize(temporaryRootDir), is(0L));
+        assertThat("empty directory should have 0 size", FileUtil.getSize(temporaryRootDir), `is`(0L))
 
-        File textFile = new File(temporaryRootDir, "tmp.txt");
-        writeStringToFile(textFile, "Hello World");
+        val textFile = File(temporaryRootDir, "tmp.txt")
+        writeStringToFile(textFile, "Hello World")
 
-        long expectedLength = (long) "Hello World".length();
-        assertThat("File size should return text length", FileUtil.getSize(textFile), is(expectedLength));
+        val expectedLength = "Hello World".length.toLong()
+        assertThat("File size should return text length", FileUtil.getSize(textFile), `is`(expectedLength))
 
-        assertThat("Should return file lengths", FileUtil.getSize(temporaryRootDir), is(expectedLength));
+        assertThat("Should return file lengths", FileUtil.getSize(temporaryRootDir), `is`(expectedLength))
     }
 }
