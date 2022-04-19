3ea7c2e752abd7249763266400754766e64ad8db
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-19 04:28:56 +0100

    refactor: Convert IntentAssert to Kotlin
    com.ichi2.testutils.IntentAssert

--- AnkiDroid/src/test/java/com/ichi2/testutils/IntentAssert.kt
+++ AnkiDroid/src/test/java/com/ichi2/testutils/IntentAssert.kt
@@ -13,31 +13,27 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.testutils
 
-package com.ichi2.testutils;
+import android.content.Intent
+import com.ichi2.utils.KotlinCleanup
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.MatcherAssert.assertThat
+import kotlin.test.assertNotNull
 
-import android.content.Intent;
-
-import java.util.Objects;
-import java.util.Set;
-
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-
-public class IntentAssert {
-
-    public static void doesNotHaveExtra(Intent intent, String extraKey) {
-        Set<String> keySet = Objects.requireNonNull(intent.getExtras()).keySet();
-        assertThat(String.format("Intent should not have extra '%s'", extraKey), keySet, not(hasItem(extraKey)));
+@KotlinCleanup("`is` -> equalTo")
+object IntentAssert {
+    @JvmStatic
+    fun doesNotHaveExtra(intent: Intent, extraKey: String?) {
+        val keySet = assertNotNull(intent.extras).keySet()
+        assertThat(String.format("Intent should not have extra '%s'", extraKey), keySet, not(hasItem(extraKey)))
     }
 
+    @JvmStatic
+    fun hasExtra(intent: Intent, extraKey: String?, value: Long) {
+        val keySet = assertNotNull(intent.extras).keySet()
+        assertThat(String.format("Intent should have extra '%s'", extraKey), keySet, hasItem(extraKey))
 
-    public static void hasExtra(Intent intent, String extraKey, long value) {
-        Set<String> keySet = Objects.requireNonNull(intent.getExtras()).keySet();
-        assertThat(String.format("Intent should have extra '%s'", extraKey), keySet, hasItem(extraKey));
-
-        assertThat(intent.getLongExtra(extraKey, -1337), is(value));
+        assertThat(intent.getLongExtra(extraKey, -1337), `is`(value))
     }
 }
