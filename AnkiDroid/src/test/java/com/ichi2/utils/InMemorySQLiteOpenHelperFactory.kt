8323c5d5876094b14bb85759a202d042269ea3f2
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-20 01:03:25 +0100

    refactor: convert InMemorySQLiteOpenHelperFactory to Kotlin
    com.ichi2.utils.InMemorySQLiteOpenHelperFactory

--- AnkiDroid/src/test/java/com/ichi2/utils/InMemorySQLiteOpenHelperFactory.kt
+++ AnkiDroid/src/test/java/com/ichi2/utils/InMemorySQLiteOpenHelperFactory.kt
@@ -13,25 +13,21 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.utils
 
-package com.ichi2.utils;
+import androidx.sqlite.db.SupportSQLiteOpenHelper
+import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
 
-import androidx.annotation.NonNull;
-import androidx.sqlite.db.SupportSQLiteOpenHelper;
-import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory;
-
-public class InMemorySQLiteOpenHelperFactory implements SupportSQLiteOpenHelper.Factory {
-    @NonNull
-    @Override
-    public SupportSQLiteOpenHelper create(
-            @NonNull SupportSQLiteOpenHelper.Configuration configuration) {
-        SupportSQLiteOpenHelper.Configuration inMemoryConfig = SupportSQLiteOpenHelper.Configuration
-                .builder(configuration.context)
-                .name(null) // in-memory
-                .callback(configuration.callback)
-                .noBackupDirectory(configuration.useNoBackupDirectory)
-                .build();
-
-        return new FrameworkSQLiteOpenHelperFactory().create(inMemoryConfig);
+class InMemorySQLiteOpenHelperFactory : SupportSQLiteOpenHelper.Factory {
+    override fun create(
+        configuration: SupportSQLiteOpenHelper.Configuration
+    ): SupportSQLiteOpenHelper {
+        val inMemoryConfig = SupportSQLiteOpenHelper.Configuration
+            .builder(configuration.context)
+            .name(null) // in-memory
+            .callback(configuration.callback)
+            .noBackupDirectory(configuration.useNoBackupDirectory)
+            .build()
+        return FrameworkSQLiteOpenHelperFactory().create(inMemoryConfig)
     }
 }
