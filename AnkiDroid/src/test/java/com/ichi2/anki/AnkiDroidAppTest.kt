030b87e5b472a3476fd0f8455e88fc58f8c126ca
Prince kushwaha <kushwahaprince418@gmail.com>
2022-04-21 19:36:20 +0530

    Migrate AnkiDroidAppTest.java to Kotlin

--- AnkiDroid/src/test/java/com/ichi2/anki/AnkiDroidAppTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/AnkiDroidAppTest.kt
@@ -13,32 +13,27 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.anki
 
-package com.ichi2.anki;
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.anki.AnkiDroidApp.sendExceptionReport
+import com.ichi2.testutils.AnkiAssert
+import org.junit.Test
+import org.junit.runner.RunWith
 
-
-import com.ichi2.testutils.AnkiAssert;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import static com.ichi2.anki.AnkiDroidApp.sendExceptionReport;
-
-@RunWith(AndroidJUnit4.class)
-public class AnkiDroidAppTest {
+@RunWith(AndroidJUnit4::class)
+class AnkiDroidAppTest {
 
     @Test
-    public void reportingDoesNotThrowException() {
-        AnkiAssert.assertDoesNotThrow(() -> sendExceptionReport("Test", "AnkiDroidAppTest"));
+    fun reportingDoesNotThrowException() {
+        AnkiAssert.assertDoesNotThrow { sendExceptionReport("Test", "AnkiDroidAppTest") }
     }
 
     @Test
-    public void reportingWithNullMessageDoesNotFail() {
-        String message = null;
-        //It's meant to be non-null, but it's developer-defined, and we don't want a crash in the reporting dialog
+    fun reportingWithNullMessageDoesNotFail() {
+        val message: String? = null
+        // It's meant to be non-null, but it's developer-defined, and we don't want a crash in the reporting dialog
         //noinspection ConstantConditions
-        AnkiAssert.assertDoesNotThrow(() -> sendExceptionReport(message, "AnkiDroidAppTest"));
+        AnkiAssert.assertDoesNotThrow { sendExceptionReport(message, "AnkiDroidAppTest") }
     }
 }
