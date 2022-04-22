06cc4635660c1f733720dfc31b606eb952f39100
oyeraghib <raghib.khan76@gmail.com>
2022-04-23 00:26:03 +0530

    Migrate ResourceLoader to Kotlin

--- AnkiDroid/src/test/java/com/ichi2/utils/ResourceLoader.kt
+++ AnkiDroid/src/test/java/com/ichi2/utils/ResourceLoader.kt
@@ -14,22 +14,17 @@
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.utils
 
-package com.ichi2.utils;
+import android.content.Context
+import android.text.TextUtils
+import com.ichi2.libanki.Utils
+import org.junit.Assert.assertTrue
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.InputStream
 
-import android.content.Context;
-import android.text.TextUtils;
-
-import com.ichi2.libanki.Utils;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-
-import static org.junit.Assert.assertTrue;
-
-public class ResourceLoader {
-
+object ResourceLoader {
     /**
      * Copy a file from the application's assets directory and return the absolute path of that
      * copy.
@@ -37,53 +32,51 @@ public class ResourceLoader {
      * Files located inside the application's assets collection are not stored on the file
      * system and can not return a usable path, so copying them to disk is a requirement.
      */
-    public static String getTempFilePath(Context context, String name, String newName) {
+    @KotlinCleanup("is --> inputStream")
+    fun getTempFilePath(context: Context, name: String, newName: String?): String {
         try {
-            InputStream is = context.getClassLoader().getResourceAsStream(name);
-            if (is == null) {
-                throw new FileNotFoundException("Could not find test file: " + name);
-            }
-            File file = new File(getTestDir(context, name), newName);
-            String dst = file.getAbsolutePath();
-            Utils.writeToFile(is, dst);
-            file.deleteOnExit();
-            return dst;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            val `is`: InputStream = context.classLoader.getResourceAsStream(name)
+                ?: throw FileNotFoundException("Could not find test file: $name")
+            val file = File(getTestDir(context, name), newName!!)
+            val dst = file.absolutePath
+            Utils.writeToFile(`is`, dst)
+            file.deleteOnExit()
+            return dst
+        } catch (e: Exception) {
+            throw RuntimeException(e)
         }
     }
 
-    public static String getTempCollection(Context context, String name) {
-        return getTempFilePath(context, name, "collection.anki2");
+    @JvmStatic
+    fun getTempCollection(context: Context, name: String): String {
+        return getTempFilePath(context, name, "collection.anki2")
     }
 
     /**
      * @param name An additional suffix to ensure the test directory is only used by a particular resource.
      * @return A File object pointing to a directory in which temporary test files can be placed. The directory is
-     *      emptied on every invocation of this method so it is suitable to use at the start of each test.
-     *      Only add files (and not subdirectories) to this directory.
+     * emptied on every invocation of this method so it is suitable to use at the start of each test.
+     * Only add files (and not subdirectories) to this directory.
      */
-    private static File getTestDir(Context context, String name) {
-        String suffix = "";
+    @KotlinCleanup("name.isNotEmpty()")
+    private fun getTestDir(context: Context, name: String): File {
+        var suffix = ""
         if (!TextUtils.isEmpty(name)) {
-            suffix = "-" + name;
+            suffix = "-$name"
         }
-        File dir = new File(context.getCacheDir(), "testfiles" + suffix);
+        val dir = File(context.cacheDir, "testfiles$suffix")
         if (!dir.exists()) {
-            assertTrue(dir.mkdir());
+            assertTrue(dir.mkdir())
         }
-        File[] files = dir.listFiles();
-        if (files == null) {
-            // Had this problem on an API 16 emulator after a stress test - directory existed
+        val files = dir.listFiles()
+            ?: // Had this problem on an API 16 emulator after a stress test - directory existed
             // but listFiles() returned null due to EMFILE (Too many open files)
             // Don't throw here - later file accesses will provide a better exception.
             // and the directory exists, even if it's unusable.
-            return dir;
+            return dir
+        for (f in files) {
+            assertTrue(f.delete())
         }
-
-        for (File f : files) {
-            assertTrue(f.delete());
-        }
-        return dir;
+        return dir
     }
 }
