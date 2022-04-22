f2546367d95fefd8a8e2fffc92f1aeb737989b7a
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-22 21:21:26 +0100

    refactor: prepare CollectionUtils for removal
    There is a unit test conversion PR in flight right now,
    so I didn't delete the class just yet.

--- AnkiDroid/src/main/java/com/ichi2/utils/CollectionUtils.kt
+++ AnkiDroid/src/main/java/com/ichi2/utils/CollectionUtils.kt
@@ -18,6 +18,7 @@ package com.ichi2.utils
 object CollectionUtils {
     /** Throws IndexOutOfBoundsException on empty list */
     @JvmStatic
+    @KotlinCleanup("Replace with List<T>.last() Kotlin extension")
     fun <T> getLastListElement(l: List<T>): T {
         return l[l.size - 1]
     }
@@ -26,8 +27,9 @@ object CollectionUtils {
      * @param c A collection in which to add elements of it
      * @param it An iterator returning things to add to C
      * @param <T> Type of elements to copy from iterator to collection
-     </T> */
+     */
     @JvmStatic
+    @KotlinCleanup("replace with Kotlin extension: MutableCollections.addAll")
     fun <T> addAll(c: MutableCollection<T>, it: Iterable<T>) {
         for (elt in it) {
             c.add(elt)
