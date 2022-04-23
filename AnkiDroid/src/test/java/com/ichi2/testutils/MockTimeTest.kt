81aba924308759c1674b003a49b5558119140373
oyeraghib <raghib.khan76@gmail.com>
2022-04-23 10:49:20 +0530

    Migrate MockTimeTest to Kotlin

--- AnkiDroid/src/test/java/com/ichi2/testutils/MockTimeTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/testutils/MockTimeTest.kt
@@ -13,20 +13,18 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.testutils
 
-package com.ichi2.testutils;
+import com.ichi2.utils.KotlinCleanup
+import org.junit.Assert
+import org.junit.Test
 
-import com.ichi2.testutils.MockTime;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-public class MockTimeTest {
+@KotlinCleanup("Function name should start with lowercase letter")
+class MockTimeTest {
     @Test
-    public void DateTest() {
-        MockTime time = new MockTime(2020, 7, 7, 7, 0, 0, 0, 0);
-        Assert.assertEquals(1596783600000L, time.intTimeMS());
-        Assert.assertEquals(1596783600000L, MockTime.timeStamp(2020, 7, 7, 7, 0, 0));
+    fun DateTest() {
+        val time = MockTime(2020, 7, 7, 7, 0, 0, 0, 0)
+        Assert.assertEquals(1596783600000L, time.intTimeMS())
+        Assert.assertEquals(1596783600000L, MockTime.timeStamp(2020, 7, 7, 7, 0, 0))
     }
 }
