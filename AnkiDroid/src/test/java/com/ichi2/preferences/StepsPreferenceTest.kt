a9d874cfbaedb27b176e48bb5dd67d40ef46561a
Abhiram-DLVSS <abhiram19521@mechyd.ac.in>
2022-04-24 11:04:51 +0530

    Update StepsPreferenceTest.kt
    Improved the style of Unit Tests by adding error messages to the assertThat.

--- AnkiDroid/src/test/java/com/ichi2/preferences/StepsPreferenceTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/preferences/StepsPreferenceTest.kt
@@ -12,29 +12,29 @@ class StepsPreferenceTest {
 
     @Test
     fun convertFromJSON() {
-        assertThat(StepsPreference.convertFromJSON(JSONArray().put(1) as JSONArray), IsEqual("1"))
-        assertThat(StepsPreference.convertFromJSON(JSONArray().put(-1).put(1.0) as JSONArray), IsEqual("-1 1.0"))
-        assertThat(StepsPreference.convertFromJSON(JSONArray().put("A") as JSONArray), IsEqual("A"))
+        // Conversion of JSONArray() to String
+        // Any input should get the exact same output in the String format
+        assertThat("A integer should return the same integer as a String", StepsPreference.convertFromJSON(JSONArray().put(1) as JSONArray), IsEqual("1"))
+        assertThat("The conversion of multiple entries of a JSONArray to a String", StepsPreference.convertFromJSON(JSONArray().put(-1).put(1.0) as JSONArray), IsEqual("-1 1.0"))
+        assertThat("A non-numeric should return the same non-numeric as a String", StepsPreference.convertFromJSON(JSONArray().put("A") as JSONArray), IsEqual("A"))
     }
 
     @Test
     fun convertToJSON() {
-
         // When the Input String is Null
-        assertThat(StepsPreference.convertToJSON(""), IsEqual(JSONArray()))
+        assertThat("Empty string should return empty JSONArray()", StepsPreference.convertToJSON(""), IsEqual(JSONArray()))
 
         // When the input String (Step) is less than or equal to 0 (i.e step<=0)
-        assertThat(StepsPreference.convertToJSON("0"), IsEqual(null))
-        assertThat(StepsPreference.convertToJSON("0.0"), IsEqual(null))
-        assertThat(StepsPreference.convertToJSON("-1"), IsEqual(null))
+        assertThat("The string cannot be equal to zero (returns null)", StepsPreference.convertToJSON("0"), IsEqual(null))
+        assertThat("The string cannot contain negative (returns null)", StepsPreference.convertToJSON("-1"), IsEqual(null))
 
         // When the input String (Step) is greater than 0
-        assertThat(StepsPreference.convertToJSON("1"), IsEqual(JSONArray().put(1))) // Testing the integers
-        assertThat(StepsPreference.convertToJSON("1.0"), IsEqual(JSONArray().put(1))) // Testing the conversion of decimal to integer
-        assertThat(StepsPreference.convertToJSON("1.1"), IsEqual(JSONArray().put(1.1))) // Testing the decimals
-        assertThat(StepsPreference.convertToJSON("1.1 2"), IsEqual(JSONArray().put(1.1).put(2))) // Testing multiple entries
+        assertThat("A integer should return the same integer in the JSONArray", StepsPreference.convertToJSON("1"), IsEqual(JSONArray().put(1)))
+        assertThat("A float in x.0 format should return the integer value x", StepsPreference.convertToJSON("1.0"), IsEqual(JSONArray().put(1)))
+        assertThat("A float should return the same float value in the JSONArray", StepsPreference.convertToJSON("1.1"), IsEqual(JSONArray().put(1.1)))
+        assertThat("Multiple entries in the string should return multiple numbers in the JSONArray", StepsPreference.convertToJSON("1.1 2"), IsEqual(JSONArray().put(1.1).put(2)))
 
         // Exceptions
-        assertThat(StepsPreference.convertToJSON("A"), IsEqual(null)) // Checking for non-numbers
+        assertThat("A non-numeric string should return null", StepsPreference.convertToJSON("A"), IsEqual(null))
     }
 }
