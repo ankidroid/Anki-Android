f40239ddadd0c57d053c3ce5fbe313f5c42b40b5
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-24 23:37:52 +0100

    lint: verify plural positional format strings
    `%d Total cards imported: %d`
    
    should be
    
    `%1$d Total cards imported: %2$d`
    
    Apply our existing code: `verifyJavaStringFormat` to
     plurals and their item elements
    
    We also have a more complex case:
      * one (valid) plural with one non-positional element: "%s"
      * one (valid) plural with two positional elements: "%1$s, %2$s"
    
    But together, this implies that the plural is given two strings
    so the first should be positional
    
    Fixes 11072

--- lint-rules/src/test/java/com/ichi2/anki/lint/rules/NonPositionalFormatSubstitutionsTest.kt
+++ lint-rules/src/test/java/com/ichi2/anki/lint/rules/NonPositionalFormatSubstitutionsTest.kt
@@ -43,6 +43,54 @@ class NonPositionalFormatSubstitutionsTest {
     @Language("XML")
     private val encoded = "<resources><string name=\"hello\">%%</string></resources>"
 
+    @Language("XML")
+    private val pluralPass = """
+        <resources>
+            <plurals name="import_complete_message">
+                <item quantity="one">Cards imported: %1${'$'}d</item>
+                <item quantity="other">Files imported :%1${'$'}d" Total cards imported: %2${'$'}d</item>
+            </plurals>
+        </resources>
+    """.trimIndent()
+
+    @Language("XML")
+    private val pluralPartial = """
+        <resources>
+            <plurals name="import_complete_message">
+                <item quantity="one">Cards imported: %d</item>
+                <item quantity="other">Files imported :%1${'$'}d" Total cards imported: %2${'$'}d</item>
+            </plurals>
+        </resources>
+    """.trimIndent()
+
+    @Language("XML")
+    private val pluralFail = """
+        <resources>
+            <plurals name="import_complete_message">
+                <item quantity="one">Cards imported: %d</item>
+                <item quantity="other">Files imported: %d\nTotal cards imported: %d</item>
+            </plurals>
+        </resources>
+    """.trimIndent()
+
+    @Language("XML")
+    private val pluralMultiple = """
+            <plurals name="reschedule_card_dialog_interval">
+                <item quantity="one">Current interval: %d day</item>
+                <item quantity="few">Keisti Kortelių mokymosi dieną</item>
+                <item quantity="many">Current interval: %d days</item>
+                <item quantity="other">Current interval: %d days</item>
+            </plurals>
+    """.trimIndent()
+
+    @Language("XML")
+    private val pluralMultipleTwo = """
+            <plurals name="in_minutes">
+                <item quantity="one">%1${'$'}d मिनट</item>
+                <item quantity="other">मिनट</item>
+            </plurals>
+    """.trimIndent()
+
     @Test
     fun errors_if_ambiguous() {
         TestLintTask.lint()
@@ -82,6 +130,62 @@ class NonPositionalFormatSubstitutionsTest {
             .allowMissingSdk()
             .allowCompilationErrors()
             .files(TestFiles.xml("res/values/string.xml", encoded))
+            .issues(NonPositionalFormatSubstitutions.ISSUE)
+            .run()
+            .expectClean()
+    }
+
+    @Test
+    fun valid_plural_passed() {
+        TestLintTask.lint()
+            .allowMissingSdk()
+            .allowCompilationErrors()
+            .files(TestFiles.xml("res/values/string.xml", pluralPass))
+            .issues(NonPositionalFormatSubstitutions.ISSUE)
+            .run()
+            .expectClean()
+    }
+
+    @Test
+    fun plural_partial_flags() {
+        // If one plural has $1, $2 etc... the other should as well
+        TestLintTask.lint()
+            .allowMissingSdk()
+            .allowCompilationErrors()
+            .files(TestFiles.xml("res/values/string.xml", pluralPartial))
+            .issues(NonPositionalFormatSubstitutions.ISSUE)
+            .run()
+            .expectErrorCount(1)
+    }
+
+    @Test
+    fun errors_on_plural_issue() {
+        TestLintTask.lint()
+            .allowMissingSdk()
+            .allowCompilationErrors()
+            .files(TestFiles.xml("res/values/string.xml", pluralFail))
+            .issues(NonPositionalFormatSubstitutions.ISSUE)
+            .run()
+            .expectErrorCount(1)
+    }
+
+    @Test
+    fun plural_integration_test() {
+        TestLintTask.lint()
+            .allowMissingSdk()
+            .allowCompilationErrors()
+            .files(TestFiles.xml("res/values/string.xml", pluralMultiple))
+            .issues(NonPositionalFormatSubstitutions.ISSUE)
+            .run()
+            .expectClean()
+    }
+
+    @Test
+    fun plural_integration_test_positional_and_nothing() {
+        TestLintTask.lint()
+            .allowMissingSdk()
+            .allowCompilationErrors()
+            .files(TestFiles.xml("res/values/string.xml", pluralMultipleTwo))
             .issues(NonPositionalFormatSubstitutions.ISSUE)
             .run()
             .expectClean()
