fabb8d76aafb1a5bde868e2078b61857de419a1d
codingtosh <codingtosh@gmail.com>
2022-04-24 17:26:11 +0530

    [Kotlin Migration] DatabaseUtils
    Package: com.ichi2.anki.testutil

--- AnkiDroid/src/androidTest/java/com/ichi2/anki/testutil/DatabaseUtils.kt
+++ AnkiDroid/src/androidTest/java/com/ichi2/anki/testutil/DatabaseUtils.kt
@@ -31,13 +31,12 @@
  *
  * From: Android\Sdk\sources\android-29\android\database\DatabaseUtils.java
  */
+package com.ichi2.anki.testutil
 
-package com.ichi2.anki.testutil;
+import android.database.Cursor
+import android.database.CursorWindow
 
-import android.database.Cursor;
-import android.database.CursorWindow;
-
-public class DatabaseUtils {
+object DatabaseUtils {
     /**
      * Fills the specified cursor window by iterating over the contents of the cursor.
      * The window is filled until the cursor is exhausted or the window runs out
@@ -46,63 +45,55 @@ public class DatabaseUtils {
      * The original position of the cursor is left unchanged by this operation.
      *
      * @param cursor The cursor that contains the data to put in the window.
-     * @param position The start position for filling the window.
+     * @param positionParam The start position for filling the window.
      * @param window The window to fill.
      */
-    public static void cursorFillWindow(final Cursor cursor,
-                                        int position, final CursorWindow window) {
-        if (position < 0 || position >= cursor.getCount()) {
-            return;
+    @JvmStatic
+    fun cursorFillWindow(
+        cursor: Cursor,
+        positionParam: Int,
+        window: CursorWindow
+    ) {
+        var position = positionParam
+        if (position < 0 || position >= cursor.count) {
+            return
         }
-        final int oldPos = cursor.getPosition();
-        final int numColumns = cursor.getColumnCount();
-        window.clear();
-        window.setStartPosition(position);
-        window.setNumColumns(numColumns);
+        val oldPos = cursor.position
+        val numColumns = cursor.columnCount
+        window.clear()
+        window.startPosition = position
+        window.setNumColumns(numColumns)
         if (cursor.moveToPosition(position)) {
-            rowloop: do {
+            rowloop@ do {
                 if (!window.allocRow()) {
-                    break;
+                    break
                 }
-                for (int i = 0; i < numColumns; i++) {
-                    final int type = cursor.getType(i);
-                    final boolean success;
-                    switch (type) {
-                        case Cursor.FIELD_TYPE_NULL:
-                            success = window.putNull(position, i);
-                            break;
-
-                        case Cursor.FIELD_TYPE_INTEGER:
-                            success = window.putLong(cursor.getLong(i), position, i);
-                            break;
-
-                        case Cursor.FIELD_TYPE_FLOAT:
-                            success = window.putDouble(cursor.getDouble(i), position, i);
-                            break;
-
-                        case Cursor.FIELD_TYPE_BLOB: {
-                            final byte[] value = cursor.getBlob(i);
-                            success = value != null ? window.putBlob(value, position, i)
-                                    : window.putNull(position, i);
-                            break;
+                for (i in 0 until numColumns) {
+                    val success: Boolean = when (cursor.getType(i)) {
+                        Cursor.FIELD_TYPE_NULL -> window.putNull(position, i)
+                        Cursor.FIELD_TYPE_INTEGER -> window.putLong(cursor.getLong(i), position, i)
+                        Cursor.FIELD_TYPE_FLOAT -> window.putDouble(cursor.getDouble(i), position, i)
+                        Cursor.FIELD_TYPE_BLOB -> {
+                            val value = cursor.getBlob(i)
+                            if (value != null) window.putBlob(value, position, i) else window.putNull(position, i)
                         }
-
-                        default: // assume value is convertible to String
-                        case Cursor.FIELD_TYPE_STRING: {
-                            final String value = cursor.getString(i);
-                            success = value != null ? window.putString(value, position, i)
-                                    : window.putNull(position, i);
-                            break;
+                        Cursor.FIELD_TYPE_STRING -> {
+                            val value = cursor.getString(i)
+                            if (value != null) window.putString(value, position, i) else window.putNull(position, i)
+                        }
+                        else -> {
+                            val value = cursor.getString(i)
+                            if (value != null) window.putString(value, position, i) else window.putNull(position, i)
                         }
                     }
                     if (!success) {
-                        window.freeLastRow();
-                        break rowloop;
+                        window.freeLastRow()
+                        break@rowloop
                     }
                 }
-                position += 1;
-            } while (cursor.moveToNext());
+                position += 1
+            } while (cursor.moveToNext())
         }
-        cursor.moveToPosition(oldPos);
+        cursor.moveToPosition(oldPos)
     }
 }
