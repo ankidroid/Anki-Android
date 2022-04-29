c38cdd63971ca0ee90ec1774c7c0a46e34003645
Prince kushwaha <kushwahaprince418@gmail.com>
2022-04-29 21:26:55 +0530

    Migrate TextCardExporterTest.java to Kotlin

--- AnkiDroid/src/test/java/com/ichi2/libanki/TextCardExporterTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/libanki/TextCardExporterTest.kt
@@ -10,80 +10,83 @@
  You should have received a copy of the GNU General Public License along with
  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.libanki
 
-package com.ichi2.libanki;
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.anki.RobolectricTest
+import com.ichi2.utils.FileOperation.Companion.getFileContents
+import com.ichi2.utils.KotlinCleanup
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.File
+import java.io.IOException
+import java.util.*
+import kotlin.Throws
 
-import com.ichi2.anki.RobolectricTest;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import static com.ichi2.utils.FileOperation.getFileContents;
-import static org.junit.Assert.assertEquals;
-
-@RunWith(AndroidJUnit4.class)
-public class TextCardExporterTest extends RobolectricTest {
-    private Collection mCol;
-    private final List<Note> mNoteList = new ArrayList<>();
-
+@KotlinCleanup("lateinit")
+@RunWith(AndroidJUnit4::class)
+class TextCardExporterTest : RobolectricTest() {
+    private var mCol: Collection? = null
+    private val mNoteList: MutableList<Note> = ArrayList()
 
     @Before
-    public void setUp() {
-        super.setUp();
-        mCol = getCol();
-        Note note = mCol.newNote();
-        note.setItem("Front", "foo");
-        note.setItem("Back", "bar<br>");
-        note.setTagsFromStr("tag, tag2");
-        mCol.addNote(note);
-        mNoteList.add(note);
+    override fun setUp() {
+        super.setUp()
+        mCol = col
+        var note = mCol!!.newNote()
+        note.setItem("Front", "foo")
+        note.setItem("Back", "bar<br>")
+        note.setTagsFromStr("tag, tag2")
+        mCol!!.addNote(note)
+        mNoteList.add(note)
         // with a different note
-        note = mCol.newNote();
-        note.setItem("Front", "baz");
-        note.setItem("Back", "qux");
-        note.model().put("did", addDeck("new col"));
-        mCol.addNote(note);
-        mNoteList.add(note);
+        note = mCol!!.newNote()
+        note.setItem("Front", "baz")
+        note.setItem("Back", "qux")
+        note.model().put("did", addDeck("new col"))
+        mCol!!.addNote(note)
+        mNoteList.add(note)
     }
 
-
     @Test
-    public void testExportTextCardWithHTML() throws IOException {
-        File exportedFile = File.createTempFile("export", ".txt");
+    @Throws(IOException::class)
+    fun testExportTextCardWithHTML() {
+        val exportedFile = File.createTempFile("export", ".txt")
 
-        TextCardExporter exporter = new TextCardExporter(mCol, true);
-        exporter.doExport(exportedFile.getAbsolutePath());
+        val exporter = TextCardExporter(mCol!!, true)
+        exporter.doExport(exportedFile.absolutePath)
         // Getting all the content of the file as a string
-        String content = getFileContents(exportedFile);
+        val content = getFileContents(exportedFile)
 
-        String expected = "";
+        var expected = ""
         // Alternatively we can choose to strip styling from content, instead of adding styling to expected
-        expected += String.format(Locale.US, "<style>%s</style>", mNoteList.get(0).model().getString("css"));
-        expected += "foo\tbar<br>\n";
-        expected += String.format(Locale.US, "<style>%s</style>", mNoteList.get(1).model().getString("css"));
-        expected += "baz\tqux\n";
-        assertEquals(expected, content);
+        expected += String.format(
+            Locale.US,
+            "<style>%s</style>",
+            mNoteList[0].model().getString("css")
+        )
+        expected += "foo\tbar<br>\n"
+        expected += String.format(
+            Locale.US,
+            "<style>%s</style>",
+            mNoteList[1].model().getString("css")
+        )
+        expected += "baz\tqux\n"
+        assertEquals(expected, content)
     }
 
-
     @Test
-    public void testExportTextCardWithoutHTML() throws IOException {
-        File exportedFile = File.createTempFile("export", ".txt");
+    @Throws(IOException::class)
+    fun testExportTextCardWithoutHTML() {
+        val exportedFile = File.createTempFile("export", ".txt")
 
-        TextCardExporter exporter = new TextCardExporter(mCol, false);
-        exporter.doExport(exportedFile.getAbsolutePath());
+        val exporter = TextCardExporter(mCol!!, false)
+        exporter.doExport(exportedFile.absolutePath)
         // Getting all the content of the file as a string
-        String content = getFileContents(exportedFile);
-        String expected = "foo\tbar\nbaz\tqux\n";
-        assertEquals(expected, content);
+        val content = getFileContents(exportedFile)
+        val expected = "foo\tbar\nbaz\tqux\n"
+        assertEquals(expected, content)
     }
 }
