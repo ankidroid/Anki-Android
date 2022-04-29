99986d776b8e27f29f8e256c3a03cf51cb61c48d
David Allison <62114487+david-allison@users.noreply.github.com>
2022-04-29 12:19:12 +0100

    refactor: convert PreferIsEmptyOverSizeCheck to Kotlin
    com.ichi2.anki.lint.rules.PreferIsEmptyOverSizeCheck
    
    Moved the companion object to the top of the class to keep the diff clean

--- lint-rules/src/main/java/com/ichi2/anki/lint/rules/PreferIsEmptyOverSizeCheck.kt
+++ lint-rules/src/main/java/com/ichi2/anki/lint/rules/PreferIsEmptyOverSizeCheck.kt
@@ -13,46 +13,36 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.anki.lint.rules
 
-package com.ichi2.anki.lint.rules;
+import com.android.tools.lint.client.api.UElementHandler
+import com.android.tools.lint.detector.api.*
+import com.android.tools.lint.detector.api.Detector.*
+import com.google.common.annotations.VisibleForTesting
+import com.ichi2.anki.lint.utils.Constants
+import com.ichi2.anki.lint.utils.KotlinCleanup
+import com.intellij.lang.jvm.JvmModifier
+import com.intellij.psi.PsiMethod
+import org.jetbrains.uast.*
+import java.util.*
+import java.util.regex.Pattern
 
-import com.android.annotations.NonNull;
-import com.android.annotations.Nullable;
-import com.android.tools.lint.client.api.UElementHandler;
-import com.android.tools.lint.detector.api.Detector;
-import com.android.tools.lint.detector.api.Implementation;
-import com.android.tools.lint.detector.api.Issue;
-import com.android.tools.lint.detector.api.JavaContext;
-import com.android.tools.lint.detector.api.LintFix;
-import com.android.tools.lint.detector.api.Scope;
-import com.google.common.annotations.VisibleForTesting;
-import com.ichi2.anki.lint.utils.Constants;
-import com.intellij.lang.jvm.JvmModifier;
-import com.intellij.psi.PsiMethod;
+@KotlinCleanup("remove after all converted")
+@KotlinCleanup("IDE lint")
+@KotlinCleanup("requireNonNull")
+class PreferIsEmptyOverSizeCheck : Detector(), UastScanner {
 
-import org.jetbrains.uast.UBinaryExpression;
-import org.jetbrains.uast.UCallExpression;
-import org.jetbrains.uast.UElement;
-import org.jetbrains.uast.ULiteralExpression;
-import org.jetbrains.uast.UastBinaryOperator;
+    companion object {
+        @VisibleForTesting
+        val ID = "PreferIsEmptyOverSize"
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class PreferIsEmptyOverSizeCheck extends Detector implements Detector.UastScanner {
-
-    @VisibleForTesting
-    static final String ID = "PreferIsEmptyOverSize";
-    @VisibleForTesting
-    static final String DESCRIPTION = "Always prefer `isEmpty` instead of size comparison when possible";
-    private static final String EXPLANATION = "To improve code consistency within the codebase you should use `isEmpty` when possible in place" +
-            " of checking that the size is zero.";
-    private static final Implementation implementation = new Implementation(PreferIsEmptyOverSizeCheck.class, Scope.JAVA_FILE_SCOPE);
-    public static final Issue ISSUE = Issue.create(
+        @VisibleForTesting
+        val DESCRIPTION = "Always prefer `isEmpty` instead of size comparison when possible"
+        private const val EXPLANATION = "To improve code consistency within the codebase you should use `isEmpty` when possible in place" +
+            " of checking that the size is zero."
+        private val implementation = Implementation(PreferIsEmptyOverSizeCheck::class.java, Scope.JAVA_FILE_SCOPE)
+        @JvmField
+        val ISSUE: Issue = Issue.create(
             ID,
             DESCRIPTION,
             EXPLANATION,
@@ -60,124 +50,103 @@ public class PreferIsEmptyOverSizeCheck extends Detector implements Detector.Uas
             Constants.ANKI_CODE_STYLE_PRIORITY,
             Constants.ANKI_CODE_STYLE_SEVERITY,
             implementation
-    );
-
-    private static final Pattern pattern = Pattern.compile("\\.size\\(\\)([^=]*)(==|>|<|>=|<=).*\\z");
-
-    public PreferIsEmptyOverSizeCheck() {
-
+        )
+        private val pattern = Pattern.compile("\\.size\\(\\)([^=]*)(==|>|<|>=|<=).*\\z")
     }
 
-    @Nullable
-    @Override
-    public UElementHandler createUastHandler(@NonNull JavaContext context) {
-        return new PreferIsEmptyOverSizeCheck.BinaryExpressionHandler(context);
+    override fun createUastHandler(context: JavaContext): UElementHandler? {
+        return BinaryExpressionHandler(context)
     }
 
-    @Nullable
-    @Override
-    public List<Class<? extends UElement>> getApplicableUastTypes() {
-        List<Class<? extends UElement>> allowed = new ArrayList<>(2);
-        allowed.add(UBinaryExpression.class);
-        allowed.add(UCallExpression.class);
-        return allowed;
+    override fun getApplicableUastTypes(): List<Class<out UElement>>? {
+        val allowed: MutableList<Class<out UElement>> = ArrayList(2)
+        allowed.add(UBinaryExpression::class.java)
+        allowed.add(UCallExpression::class.java)
+        return allowed
     }
 
     /**
      * Report the problematic comparison expression to the lint checker and compute the fix to apply
      * since the pattern().with() seems to behave strangely.
      */
-    public void reportVariable(
-            @NonNull JavaContext context,
-            @NonNull UBinaryExpression node,
-            boolean isAffirmative
+    fun reportVariable(
+        context: JavaContext,
+        node: UBinaryExpression,
+        isAffirmative: Boolean
     ) {
-        String source = node.asSourceString();
-        Matcher matcher = pattern.matcher(source);
-        String fixedSource = matcher.replaceFirst(".isEmpty()$1").trim();
+        val source = node.asSourceString()
+        val matcher = pattern.matcher(source)
+        var fixedSource = matcher.replaceFirst(".isEmpty()$1").trim { it <= ' ' }
         if (!isAffirmative) {
-            fixedSource = String.format("!%s", fixedSource);
+            fixedSource = String.format("!%s", fixedSource)
         }
-        LintFix fix = LintFix
-                .create()
-                .replace()
-                .range(context.getLocation(node))
-                .text(source)
-                .with(fixedSource)
-                .reformat(true)
-                .build();
+        val fix = LintFix.create()
+            .replace()
+            .range(context.getLocation(node))
+            .text(source)
+            .with(fixedSource)
+            .reformat(true)
+            .build()
         context.report(
-                ISSUE,
-                node,
-                context.getLocation(node),
-                DESCRIPTION,
-                fix
-        );
+            ISSUE,
+            node,
+            context.getLocation(node),
+            DESCRIPTION,
+            fix
+        )
     }
 
-    private class BinaryExpressionHandler extends UElementHandler {
-
-        private JavaContext mJavaContext;
-        private UBinaryExpression parentNode;
-        private Boolean isAffirmative = null;
-
-        public BinaryExpressionHandler(JavaContext context) {
-            this.mJavaContext = context;
-        }
-
-        @Override
-        public void visitCallExpression(@NonNull UCallExpression node) {
+    private inner class BinaryExpressionHandler(private val mJavaContext: JavaContext) : UElementHandler() {
+        private var parentNode: UBinaryExpression? = null
+        private var isAffirmative: Boolean? = null
+        override fun visitCallExpression(node: UCallExpression) {
             // Needed to check if the class has an `isEmpty` method.
-            if (this.parentNode == null || this.isAffirmative == null || !"size".equals(node.getMethodName())) {
-                return;
+            if (parentNode == null || isAffirmative == null || "size" != node.methodName) {
+                return
             }
             // Check that this is a child of the parent we have already seen.
-            UElement n = node;
-            while (n != null && n != this.parentNode) {
-                n = n.getUastParent();
+            var n: UElement? = node
+            while (n != null && n !== parentNode) {
+                n = n.uastParent
             }
             if (n == null) {
                 // Optimisation: since the current node is not in the subtree of parentNode we are done
                 // searching for this parentNode (we have already visited all the subtree and exited from it)
-                this.parentNode = null;
-                this.isAffirmative = null;
-                return;
+                parentNode = null
+                isAffirmative = null
+                return
             }
-            PsiMethod[] methods = Objects.requireNonNull(this.mJavaContext.getEvaluator()
-                    .getTypeClass(node.getReceiverType()))
-                    .getAllMethods();
-            if (Arrays.stream(methods).anyMatch(m -> "isEmpty".equals(m.getName()) && m.hasModifier(JvmModifier.PUBLIC))) {
-                reportVariable(this.mJavaContext, this.parentNode, this.isAffirmative);
-                this.parentNode = null;
-                this.isAffirmative = null;
+            val methods = Objects.requireNonNull(
+                mJavaContext.evaluator
+                    .getTypeClass(node.receiverType)
+            )!!.getAllMethods()
+            if (Arrays.stream(methods).anyMatch { m: PsiMethod -> "isEmpty" == m.name && m.hasModifier(JvmModifier.PUBLIC) }) {
+                reportVariable(mJavaContext, parentNode!!, isAffirmative!!)
+                parentNode = null
+                isAffirmative = null
             }
         }
 
-        @Override
-        public void visitBinaryExpression(@NonNull UBinaryExpression node) {
-            UElement left = node.getLeftOperand();
+        override fun visitBinaryExpression(node: UBinaryExpression) {
+            val left: UElement = node.leftOperand
             if (!left.asSourceString().contains(".size()")) {
-                return;
+                return
             }
-            UElement right = node.getRightOperand();
-            if (!(right instanceof ULiteralExpression)) {
-                return;
+            val right: UExpression = node.rightOperand as? ULiteralExpression ?: return
+            val rightOperand = right as ULiteralExpression
+            if (rightOperand.value !is Number) {
+                return
             }
-            ULiteralExpression rightOperand = (ULiteralExpression) right;
-            if (!(rightOperand.getValue() instanceof Number)) {
-                return;
-            }
-            double rightValue = ((Number) rightOperand.getValue()).doubleValue();
-            if (UastBinaryOperator.ComparisonOperator.IDENTITY_EQUALS.equals(node.getOperator()) && rightValue == 0.0
-                    || UastBinaryOperator.ComparisonOperator.LESS.equals(node.getOperator()) && rightValue == 1.0
-                    || UastBinaryOperator.ComparisonOperator.LESS_OR_EQUALS.equals(node.getOperator()) && rightValue == 0.0) {
+            val rightValue = (rightOperand.value as Number?)!!.toDouble()
+            if (UastBinaryOperator.IDENTITY_EQUALS == node.operator && rightValue == 0.0 || UastBinaryOperator.LESS == node.operator && rightValue == 1.0 || UastBinaryOperator.LESS_OR_EQUALS == node.operator && rightValue == 0.0) {
                 // Memoize the node to check later and provide the fix also.
-                this.parentNode = node;
-                this.isAffirmative = true;
-            } else if (UastBinaryOperator.ComparisonOperator.GREATER.equals(node.getOperator()) && rightValue == 0.0
-                    || UastBinaryOperator.ComparisonOperator.GREATER_OR_EQUALS.equals(node.getOperator()) && rightValue == 1.0) {
-                this.parentNode = node;
-                this.isAffirmative = false;
+                parentNode = node
+                isAffirmative = true
+            } else if (UastBinaryOperator.GREATER == node.operator && rightValue == 0.0 ||
+                UastBinaryOperator.GREATER_OR_EQUALS == node.operator && rightValue == 1.0
+            ) {
+                parentNode = node
+                isAffirmative = false
             }
         }
     }
