f2f7baf7cc2b08cedd94be56933be9c82ba8fc14
David Allison <62114487+david-allison@users.noreply.github.com>
2022-05-02 02:04:01 +0100

    refactor: Convert Previewer to Kotlin
    com.ichi2.anki.Previewer

--- AnkiDroid/src/main/java/com/ichi2/anki/Previewer.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/Previewer.kt
@@ -15,335 +15,280 @@
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
+package com.ichi2.anki
 
-package com.ichi2.anki;
-
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.SeekBar;
-import android.widget.TextView;
-
-import com.ichi2.anki.cardviewer.Gesture;
-import com.ichi2.anki.cardviewer.PreviewLayout;
-import com.ichi2.anki.cardviewer.ViewerCommand;
-import com.ichi2.libanki.Collection;
-import com.ichi2.libanki.Utils;
-
-import java.util.HashSet;
-import java.util.List;
-
-import androidx.annotation.CheckResult;
-import androidx.annotation.NonNull;
-import androidx.annotation.VisibleForTesting;
-import timber.log.Timber;
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TextView
+import androidx.annotation.CheckResult
+import androidx.annotation.VisibleForTesting
+import com.ichi2.anki.cardviewer.Gesture
+import com.ichi2.anki.cardviewer.PreviewLayout
+import com.ichi2.anki.cardviewer.ViewerCommand
+import com.ichi2.libanki.Collection
+import com.ichi2.libanki.Utils
+import com.ichi2.utils.KotlinCleanup
+import timber.log.Timber
 
 /**
  * The previewer intent must supply an array of cards to show and the index in the list from where
  * to begin showing them. Special rules are applied if the list size is 1 (i.e., no scrolling
  * buttons will be shown).
  */
-public class Previewer extends AbstractFlashcardViewer {
-    private long[] mCardList;
-    private int mIndex;
-    private boolean mShowingAnswer;
-    private SeekBar mProgressSeekBar;
-    private TextView mProgressText;
+@KotlinCleanup("IDE lint")
+@KotlinCleanup("lateinit")
+class Previewer : AbstractFlashcardViewer() {
+    private var mCardList: LongArray? = null
+    private var mIndex = 0
+    private var mShowingAnswer = false
+    private var mProgressSeekBar: SeekBar? = null
+    private var mProgressText: TextView? = null
 
-    /** Communication with Browser */
-    private boolean mReloadRequired;
-    private boolean mNoteChanged;
-
-    protected PreviewLayout mPreviewLayout;
-
-    @CheckResult
-    @NonNull
-    public static Intent getPreviewIntent(Context context, int index, long[] cardList) {
-        Intent intent = new Intent(context, Previewer.class);
-        intent.putExtra("index", index);
-        intent.putExtra("cardList", cardList);
-        return intent;
-    }
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    /** Communication with Browser  */
+    private var mReloadRequired = false
+    private var mNoteChanged = false
+    protected var previewLayout: PreviewLayout? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
         if (showedActivityFailedScreen(savedInstanceState)) {
-            return;
+            return
         }
-        Timber.d("onCreate()");
-        super.onCreate(savedInstanceState);
-
-        mCardList = getIntent().getLongArrayExtra("cardList");
-        mIndex = getIntent().getIntExtra("index", -1);
-
+        Timber.d("onCreate()")
+        super.onCreate(savedInstanceState)
+        mCardList = intent.getLongArrayExtra("cardList")
+        mIndex = intent.getIntExtra("index", -1)
         if (savedInstanceState != null) {
-            mIndex = savedInstanceState.getInt("index", mIndex);
-            mShowingAnswer = savedInstanceState.getBoolean("showingAnswer", mShowingAnswer);
-            mReloadRequired = savedInstanceState.getBoolean("reloadRequired");
-            mNoteChanged = savedInstanceState.getBoolean("noteChanged");
+            mIndex = savedInstanceState.getInt("index", mIndex)
+            mShowingAnswer = savedInstanceState.getBoolean("showingAnswer", mShowingAnswer)
+            mReloadRequired = savedInstanceState.getBoolean("reloadRequired")
+            mNoteChanged = savedInstanceState.getBoolean("noteChanged")
         }
-
-        if (mCardList.length == 0 || mIndex < 0 || mIndex > mCardList.length - 1) {
-            Timber.e("Previewer started with empty card list or invalid index");
-            finishWithoutAnimation();
-            return;
+        if (mCardList!!.size == 0 || mIndex < 0 || mIndex > mCardList!!.size - 1) {
+            Timber.e("Previewer started with empty card list or invalid index")
+            finishWithoutAnimation()
+            return
         }
-        showBackIcon();
+        showBackIcon()
         // Ensure navigation drawer can't be opened. Various actions in the drawer cause crashes.
-        disableDrawerSwipe();
-        startLoadingCollection();
-        initPreviewProgress();
+        disableDrawerSwipe()
+        startLoadingCollection()
+        initPreviewProgress()
     }
 
-    private void initPreviewProgress() {
-        mProgressSeekBar = findViewById(R.id.preview_progress_seek_bar);
-        mProgressText = findViewById(R.id.preview_progress_text);
-        LinearLayout progressLayout = findViewById(R.id.preview_progress_layout);
+    private fun initPreviewProgress() {
+        mProgressSeekBar = findViewById(R.id.preview_progress_seek_bar)
+        mProgressText = findViewById(R.id.preview_progress_text)
+        val progressLayout = findViewById<LinearLayout>(R.id.preview_progress_layout)
 
-        //Show layout only when the cardList is bigger than 1
-        if (mCardList.length > 1) {
-            progressLayout.setVisibility(View.VISIBLE);
-            mProgressSeekBar.setMax(mCardList.length - 1);
-            setSeekBarListener();
-            updateProgress();
+        // Show layout only when the cardList is bigger than 1
+        if (mCardList!!.size > 1) {
+            progressLayout.visibility = View.VISIBLE
+            mProgressSeekBar!!.setMax(mCardList!!.size - 1)
+            setSeekBarListener()
+            updateProgress()
         }
     }
 
-    private void setSeekBarListener() {
-        mProgressSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+    private fun setSeekBarListener() {
+        mProgressSeekBar!!.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
                 if (fromUser) {
-                    mIndex = progress;
-                    updateProgress();
-                    setCurrentCard(getCol().getCard(mCardList[mIndex]));
-                    displayCardQuestion();
-
+                    mIndex = progress
+                    updateProgress()
+                    currentCard = col.getCard(mCardList!![mIndex])
+                    displayCardQuestion()
                 }
             }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
                 // Mandatory override, but unused
             }
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                if (mIndex >= 0 && mIndex < mCardList.length) {
-                    setCurrentCard(getCol().getCard(mCardList[mIndex]));
-                    displayCardQuestion();
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                if (mIndex >= 0 && mIndex < mCardList!!.size) {
+                    currentCard = col.getCard(mCardList!![mIndex])
+                    displayCardQuestion()
                 }
             }
-        });
+        })
     }
 
-    @Override
-    public void onCollectionLoaded(Collection col) {
-        super.onCollectionLoaded(col);
-        setCurrentCard(col.getCard(mCardList[mIndex]));
-
-        displayCardQuestion();
+    override fun onCollectionLoaded(col: Collection) {
+        super.onCollectionLoaded(col)
+        currentCard = col.getCard(mCardList!![mIndex])
+        displayCardQuestion()
         if (mShowingAnswer) {
-            displayCardAnswer();
+            displayCardAnswer()
         }
-
-        showBackIcon();
+        showBackIcon()
     }
 
     /** Given a new collection of card Ids, find the 'best' valid card given the current collection
      * We define the best as searching to the left, then searching to the right of the current element
-     * This occurs as many cards can be deleted when editing a note (from the Card Template Editor) */
-    private int getNextIndex(List<Long> newCardList) {
-        HashSet<Long> validIndices = new HashSet<>(newCardList);
-
-        for (int i = mIndex; i >= 0; i--) {
-            if (validIndices.contains(mCardList[i])) {
-                return newCardList.indexOf(mCardList[i]);
+     * This occurs as many cards can be deleted when editing a note (from the Card Template Editor)  */
+    private fun getNextIndex(newCardList: List<Long>): Int {
+        val validIndices = HashSet(newCardList)
+        for (i in mIndex downTo 0) {
+            if (validIndices.contains(mCardList!![i])) {
+                return newCardList.indexOf(mCardList!![i])
             }
         }
-
-        for (int i = mIndex + 1; i < validIndices.size(); i++) {
-            if (validIndices.contains(mCardList[i])) {
-                return newCardList.indexOf(mCardList[i]);
+        for (i in mIndex + 1 until validIndices.size) {
+            if (validIndices.contains(mCardList!![i])) {
+                return newCardList.indexOf(mCardList!![i])
             }
         }
-
-        throw new IllegalStateException("newCardList was empty");
+        throw IllegalStateException("newCardList was empty")
     }
 
-
-    @Override
-    protected void setTitle() {
-        getSupportActionBar().setTitle(R.string.preview_title);
+    override fun setTitle() {
+        supportActionBar!!.setTitle(R.string.preview_title)
     }
 
-
-    @Override
-    protected void initLayout() {
-        super.initLayout();
-        mTopBarLayout.setVisibility(View.GONE);
-
-        findViewById(R.id.answer_options_layout).setVisibility(View.GONE);
-
-        mPreviewLayout = PreviewLayout.createAndDisplay(this, mToggleAnswerHandler);
-        mPreviewLayout.setOnNextCard((view) -> changePreviewedCard(true));
-        mPreviewLayout.setOnPreviousCard((view) -> changePreviewedCard(false));
+    override fun initLayout() {
+        super.initLayout()
+        mTopBarLayout!!.visibility = View.GONE
+        findViewById<View>(R.id.answer_options_layout).visibility = View.GONE
+        previewLayout = PreviewLayout.createAndDisplay(this, mToggleAnswerHandler)
+        previewLayout!!.setOnNextCard { changePreviewedCard(true) }
+        previewLayout!!.setOnPreviousCard { changePreviewedCard(false) }
     }
 
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_edit) {
-            editCard();
-            return true;
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_edit) {
+            editCard()
+            return true
         }
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item)
     }
 
-
-    @Override
-    public void onBackPressed() {
-        setResult(RESULT_OK, getResultIntent());
-        super.onBackPressed();
+    override fun onBackPressed() {
+        setResult(RESULT_OK, resultIntent)
+        super.onBackPressed()
     }
 
-
-    @Override
-    protected void onNavigationPressed() {
-        setResult(RESULT_OK, getResultIntent());
-        super.onNavigationPressed();
+    override fun onNavigationPressed() {
+        setResult(RESULT_OK, resultIntent)
+        super.onNavigationPressed()
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.previewer, menu);
-        return super.onCreateOptionsMenu(menu);
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.previewer, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putLongArray("cardList", mCardList);
-        outState.putInt("index", mIndex);
-        outState.putBoolean("showingAnswer", mShowingAnswer);
-        outState.putBoolean("reloadRequired", mReloadRequired);
-        outState.putBoolean("noteChanged", mNoteChanged);
-        super.onSaveInstanceState(outState);
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putLongArray("cardList", mCardList)
+        outState.putInt("index", mIndex)
+        outState.putBoolean("showingAnswer", mShowingAnswer)
+        outState.putBoolean("reloadRequired", mReloadRequired)
+        outState.putBoolean("noteChanged", mNoteChanged)
+        super.onSaveInstanceState(outState)
     }
 
-
-    @Override
-    public void displayCardQuestion() {
-        super.displayCardQuestion();
-        mShowingAnswer = false;
-        updateButtonsState();
+    override fun displayCardQuestion() {
+        super.displayCardQuestion()
+        mShowingAnswer = false
+        updateButtonsState()
     }
-
 
     // Called via mFlipCardListener in parent class when answer button pressed
     @VisibleForTesting(otherwise = VisibleForTesting.PROTECTED)
-    @Override
-    public void displayCardAnswer() {
-        super.displayCardAnswer();
-        mShowingAnswer = true;
-        updateButtonsState();
+    override fun displayCardAnswer() {
+        super.displayCardAnswer()
+        mShowingAnswer = true
+        updateButtonsState()
     }
 
-
-    @Override
-    protected void hideEaseButtons() {
+    override fun hideEaseButtons() {
         /* do nothing */
     }
 
-    @Override
-    protected void displayAnswerBottomBar() {
+    override fun displayAnswerBottomBar() {
         /* do nothing */
     }
 
-
-    @Override
-    public boolean executeCommand(@NonNull ViewerCommand which, Gesture fromGesture) {
+    override fun executeCommand(which: ViewerCommand, fromGesture: Gesture?): Boolean {
         /* do nothing */
-        return false;
+        return false
     }
 
-
-    @Override
-    protected void performReload() {
-        mReloadRequired = true;
-        List<Long> newCardList = getCol().filterToValidCards(mCardList);
-
+    override fun performReload() {
+        mReloadRequired = true
+        val newCardList = col.filterToValidCards(mCardList)
         if (newCardList.isEmpty()) {
-            finishWithoutAnimation();
-            return;
+            finishWithoutAnimation()
+            return
         }
-
-        mIndex = getNextIndex(newCardList);
-        mCardList = Utils.collection2Array(newCardList);
-        setCurrentCard(getCol().getCard(mCardList[mIndex]));
-        displayCardQuestion();
+        mIndex = getNextIndex(newCardList)
+        mCardList = Utils.collection2Array(newCardList)
+        currentCard = col.getCard(mCardList!![mIndex])
+        displayCardQuestion()
     }
 
-
-    @Override
-    protected void onEditedNoteChanged() {
-        super.onEditedNoteChanged();
-        mNoteChanged = true;
+    override fun onEditedNoteChanged() {
+        super.onEditedNoteChanged()
+        mNoteChanged = true
     }
 
-
-    protected void changePreviewedCard(boolean nextCard) {
-        mIndex = nextCard ? mIndex + 1 : mIndex - 1;
-        setCurrentCard(getCol().getCard(mCardList[mIndex]));
-        displayCardQuestion();
-        updateProgress();
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    internal fun changePreviewedCard(nextCard: Boolean) {
+        mIndex = if (nextCard) mIndex + 1 else mIndex - 1
+        currentCard = col.getCard(mCardList!![mIndex])
+        displayCardQuestion()
+        updateProgress()
     }
 
-
-    private final View.OnClickListener mToggleAnswerHandler = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            if (mShowingAnswer) {
-                displayCardQuestion();
-            } else {
-                displayCardAnswer();
-            }
+    private val mToggleAnswerHandler = View.OnClickListener {
+        if (mShowingAnswer) {
+            displayCardQuestion()
+        } else {
+            displayCardAnswer()
         }
-    };
+    }
 
-    private void updateButtonsState() {
-        mPreviewLayout.setShowingAnswer(mShowingAnswer);
+    private fun updateButtonsState() {
+        previewLayout!!.setShowingAnswer(mShowingAnswer)
 
         // If we are in single-card mode, we show the "Show Answer" button on the question side
         // and hide navigation buttons.
-        if (mCardList.length == 1) {
-            mPreviewLayout.hideNavigationButtons();
-            return;
+        if (mCardList!!.size == 1) {
+            previewLayout!!.hideNavigationButtons()
+            return
         }
-
-        mPreviewLayout.setPrevButtonEnabled(mIndex > 0);
-        mPreviewLayout.setNextButtonEnabled(mIndex < mCardList.length - 1);
+        previewLayout!!.setPrevButtonEnabled(mIndex > 0)
+        previewLayout!!.setNextButtonEnabled(mIndex < mCardList!!.size - 1)
     }
 
-    private void updateProgress() {
-        if (mProgressSeekBar.getProgress() != mIndex) {
-            mProgressSeekBar.setProgress(mIndex);
+    private fun updateProgress() {
+        if (mProgressSeekBar!!.progress != mIndex) {
+            mProgressSeekBar!!.progress = mIndex
         }
-
-        String progress = getString(R.string.preview_progress_bar_text, mIndex + 1, mCardList.length);
-        mProgressText.setText(progress);
+        val progress = getString(R.string.preview_progress_bar_text, mIndex + 1, mCardList!!.size)
+        mProgressText!!.text = progress
     }
 
-    @NonNull
-    private Intent getResultIntent() {
-        Intent intent = new Intent();
-        intent.putExtra("reloadRequired", mReloadRequired);
-        intent.putExtra("noteChanged", mNoteChanged);
-        return intent;
+    private val resultIntent: Intent
+        get() {
+            val intent = Intent()
+            intent.putExtra("reloadRequired", mReloadRequired)
+            intent.putExtra("noteChanged", mNoteChanged)
+            return intent
+        }
+
+    companion object {
+        @CheckResult
+        fun getPreviewIntent(context: Context?, index: Int, cardList: LongArray?): Intent {
+            val intent = Intent(context, Previewer::class.java)
+            intent.putExtra("index", index)
+            intent.putExtra("cardList", cardList)
+            return intent
+        }
     }
 }
