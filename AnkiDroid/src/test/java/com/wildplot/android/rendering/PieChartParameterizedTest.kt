bbd6904f00b522bda867ad28b34089057823f3d9
Brayan Oliveira <69634269+BrayanDSO@users.noreply.github.com>
2022-05-02 15:27:48 -0300

    refactor: Convert PieChartParameterizedTest to Kotlin
    com.wildplot.android.rendering.PieChartParameterizedTest

--- AnkiDroid/src/test/java/com/wildplot/android/rendering/PieChartParameterizedTest.kt
+++ AnkiDroid/src/test/java/com/wildplot/android/rendering/PieChartParameterizedTest.kt
@@ -1,160 +1,166 @@
 //noinspection MissingCopyrightHeader #8659
+package com.wildplot.android.rendering
 
-package com.wildplot.android.rendering;
+import android.graphics.Color
+import com.ichi2.utils.KotlinCleanup
+import com.wildplot.android.rendering.PieChartTest.createRectangleMock
+import com.wildplot.android.rendering.graphics.wrapper.ColorWrap
+import com.wildplot.android.rendering.graphics.wrapper.FontMetricsWrap
+import com.wildplot.android.rendering.graphics.wrapper.GraphicsWrap
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.anyFloat
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.ArgumentMatchers.floatThat
+import org.mockito.Mock
+import org.mockito.MockedStatic
+import org.mockito.Mockito
+import org.mockito.Mockito.inOrder
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+import java.util.*
+import java.util.Arrays.asList
 
-import android.graphics.Color;
+@KotlinCleanup("`when` -> whenever")
+@RunWith(Parameterized::class)
+class PieChartParameterizedTest {
+    @Parameterized.Parameter
+    lateinit var values: DoubleArray
 
-import com.wildplot.android.rendering.graphics.wrapper.ColorWrap;
-import com.wildplot.android.rendering.graphics.wrapper.FontMetricsWrap;
-import com.wildplot.android.rendering.graphics.wrapper.GraphicsWrap;
-import com.wildplot.android.rendering.graphics.wrapper.RectangleWrap;
+    @Parameterized.Parameter(1)
+    lateinit var startAngles: DoubleArray
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
-import org.mockito.InOrder;
-import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+    @Parameterized.Parameter(2)
+    lateinit var arcLengths: DoubleArray
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-
-import static com.wildplot.android.rendering.PieChartTest.createRectangleMock;
-import static java.util.Arrays.asList;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyFloat;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.floatThat;
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-@RunWith(Parameterized.class)
-@SuppressWarnings("WeakerAccess")
-public class PieChartParameterizedTest {
-    private static final double PRECISION = 2 * 1E-3F;
-
-    @Parameter()
-    public double[] values;
-    @Parameter(1)
-    public double[] startAngles;
-    @Parameter(2)
-    public double[] arcLengths;
-    @Parameter(3)
-    public ColorWrap[] colors;
+    @Parameterized.Parameter(3)
+    lateinit var colors: Array<ColorWrap>
 
     @Mock
-    GraphicsWrap mGraphics;
+    @KotlinCleanup("lateinit")
+    var graphics: GraphicsWrap? = null
 
     @Mock
-    PlotSheet mPlot;
+    @KotlinCleanup("lateinit")
+    var plot: PlotSheet? = null
 
-    PieChart mPieChart;
+    @KotlinCleanup("lateinit")
+    var pieChart: PieChart? = null
 
-    private MockedStatic<Color> mColorMockedStatic;
+    @KotlinCleanup("lateinit")
+    private var colorMockedStatic: MockedStatic<Color>? = null
 
     @Before
-    public void setUp() {
-        mColorMockedStatic = Mockito.mockStatic(Color.class);
-        MockitoAnnotations.openMocks(this);
-        when(Color.argb(anyInt(), anyInt(), anyInt(), anyInt())).thenReturn(0);
-        when(mPlot.getFrameThickness()).thenReturn(new float[]{0, 0, 0, 0});
+    fun setUp() {
+        colorMockedStatic = Mockito.mockStatic(Color::class.java)
+        MockitoAnnotations.openMocks(this)
+        `when`(Color.argb(anyInt(), anyInt(), anyInt(), anyInt())).thenReturn(0)
+        `when`(plot!!.frameThickness).thenReturn(floatArrayOf(0f, 0f, 0f, 0f))
 
-        FontMetricsWrap fm = mock(FontMetricsWrap.class);
-        when(fm.getHeight()).thenReturn(10f);
-        when(fm.stringWidth(any(String.class))).thenReturn(30f);
-        when(mGraphics.getFontMetrics()).thenReturn(fm);
+        val fm = mock(FontMetricsWrap::class.java)
+        `when`(fm.height).thenReturn(10f)
+        `when`(fm.stringWidth(any(String::class.java))).thenReturn(30f)
+        `when`(graphics!!.fontMetrics).thenReturn(fm)
 
-        RectangleWrap r = createRectangleMock(100, 100);
-        when(mGraphics.getClipBounds()).thenReturn(r);
-        mPieChart = new PieChart(mPlot, values, colors);
+        val r = createRectangleMock(100, 100)
+        `when`(graphics!!.clipBounds).thenReturn(r)
+        pieChart = PieChart(plot!!, values, colors)
     }
 
     @After
-    public void tearDown() {
-        mColorMockedStatic.close();
+    fun tearDown() {
+        colorMockedStatic!!.close()
     }
 
     @Test
-    public void testPaintDrawsAllArcs() {
-        mPieChart.paint(mGraphics);
+    fun testPaintDrawsAllArcs() {
+        pieChart!!.paint(graphics!!)
         // ordered verification is used to prevent failures when there are tiny adjacent sectors
-        InOrder inOrder = inOrder(mGraphics);
-        for (int i = 0; i < values.length; i++) {
-            if (arcLengths[i] == 0) continue;
-            inOrder.verify(mGraphics).setColor(colors[i]);
-            inOrder.verify(mGraphics).fillArc(anyFloat(), anyFloat(), anyFloat(), anyFloat(),
-                    floatThat(closeTo(startAngles[i])),
-                    floatThat(closeTo(arcLengths[i])));
+        val inOrder = inOrder(graphics)
+        for (i in values.indices) {
+            if (arcLengths[i] == 0.0) continue
+            inOrder.verify(graphics)?.color = colors[i]
+            inOrder.verify(graphics)?.fillArc(
+                anyFloat(), anyFloat(), anyFloat(), anyFloat(),
+                floatThat(closeTo(startAngles[i])),
+                floatThat(closeTo(arcLengths[i]))
+            )
         }
     }
 
-    private static FloatMatcher closeTo(double v) {
-        return FloatMatcher.closeTo(v, PRECISION);
-    }
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return createParametersCollection(asList(
-                values(1),
-                values(0, 1),
-                values(1, 0),
-                values(1, 0, 1),
-
-                // arrays of equal values of various sizes
-                equalValues(2),
-                equalValues(3),
-                equalValues(4),
-                equalValues(17),
-
-                // tiny only
-                values(1E-100),
-                values(1E-100, 1E-100, 1E-100),
-
-                // huge only
-                values(1E100),
-                values(1E100, 1E100, 1E100),
-
-                // huge and tiny
-                values(1E-100, 1E100),
-                values(1E100, 1E-100),
-                values(1E100, 1E-100, 1E100),
-                values(1E-100, 1E100, 1E-100),
-                values(1E100, 1E-100, 1E-100, 1E100)
-        ));
-    }
-
-    private static Collection<Object[]> createParametersCollection(Collection<double[]> values) {
-        Collection<Object[]> parameters = new ArrayList<>(values.size());
-        for (double[] v : values) {
-            parameters.add(createParameters(v));
+    companion object {
+        private const val PRECISION = (2 * 1E-3f).toDouble()
+        private fun closeTo(v: Double): FloatMatcher {
+            return FloatMatcher.closeTo(v, PRECISION)
         }
-        return parameters;
-    }
 
-    private static Object[] createParameters(double[] values) {
-        PieChartTestParametersBuilder builder = new PieChartTestParametersBuilder(values, -90);
-        return new Object[] {
+        @Parameterized.Parameters
+        @KotlinCleanup("listOf")
+        @JvmStatic
+        fun data(): Collection<Array<Any>> {
+            return createParametersCollection(
+                asList(
+                    values(1.0),
+                    values(0.0, 1.0),
+                    values(1.0, 0.0),
+                    values(1.0, 0.0, 1.0),
+
+                    // arrays of equal values of various sizes
+                    equalValues(2),
+                    equalValues(3),
+                    equalValues(4),
+                    equalValues(17),
+
+                    // tiny only
+                    values(1E-100),
+                    values(1E-100, 1E-100, 1E-100),
+
+                    // huge only
+                    values(1E100),
+
+                    // huge and tiny
+                    values(1E100, 1E100, 1E100),
+                    values(1E-100, 1E100),
+                    values(1E100, 1E-100),
+                    values(1E100, 1E-100, 1E100),
+                    values(1E-100, 1E100, 1E-100),
+                    values(1E100, 1E-100, 1E-100, 1E100)
+                )
+            )
+        }
+
+        private fun createParametersCollection(values: Collection<DoubleArray>): Collection<Array<Any>> {
+            val parameters: MutableCollection<Array<Any>> = ArrayList(values.size)
+            for (v in values) {
+                parameters.add(createParameters(v))
+            }
+            return parameters
+        }
+
+        private fun createParameters(values: DoubleArray): Array<Any> {
+            val builder = PieChartTestParametersBuilder(values, -90.0)
+            return arrayOf(
                 values,
-                builder.getStartAngles(),
-                builder.getArcLengths(),
-                builder.getColors()
-        };
-    }
+                builder.startAngles,
+                builder.arcLengths,
+                builder.colors
+            )
+        }
 
-    private static double[] values(double... d) {
-        return d;
-    }
+        private fun values(vararg d: Double): DoubleArray {
+            return d
+        }
 
-    private static double[] equalValues(int numberOfValues) {
-        double[] v = new double[numberOfValues];
-        Arrays.fill(v, 1);
-        return v;
+        @KotlinCleanup("Arrays.fill")
+        private fun equalValues(numberOfValues: Int): DoubleArray {
+            val v = DoubleArray(numberOfValues)
+            Arrays.fill(v, 1.0)
+            return v
+        }
     }
 }
