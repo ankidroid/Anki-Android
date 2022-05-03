39d13fbf3197f379e28fef195a32648dfb914b0e
lukstbit <lukstbit@users.noreply.github.com>
2022-05-03 19:12:09 +0300

    Migrate MathJax.java to Kotlin

--- AnkiDroid/src/main/java/com/ichi2/libanki/template/MathJax.kt
+++ AnkiDroid/src/main/java/com/ichi2/libanki/template/MathJax.kt
@@ -13,47 +13,38 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.libanki.template
 
-package com.ichi2.libanki.template;
+import com.ichi2.utils.KotlinCleanup
 
-import java.util.regex.Pattern;
-
-import androidx.annotation.NonNull;
-
-public class MathJax {
+object MathJax {
     // MathJax opening delimiters
-    private static final String[] sMathJaxOpenings = {"\\(", "\\["};
+    private val sMathJaxOpenings = arrayOf("\\(", "\\[")
 
     // MathJax closing delimiters
-    private static final String[] sMathJaxClosings = {"\\)", "\\]"};
+    private val sMathJaxClosings = arrayOf("\\)", "\\]")
 
-
-    public static boolean textContainsMathjax(@NonNull String txt) {
+    @KotlinCleanup("fix IDE lint issues")
+    fun textContainsMathjax(txt: String): Boolean {
         // Do you have the first opening and then the first closing,
         // or the second opening and the second closing...?
 
-        //This assumes that the openings and closings are the same length.
+        // This assumes that the openings and closings are the same length.
+        var opening: String
+        var closing: String
+        for (i in sMathJaxOpenings.indices) {
+            opening = sMathJaxOpenings[i]
+            closing = sMathJaxClosings[i]
 
-        String opening;
-        String closing;
-        for (int i = 0; i < sMathJaxOpenings.length; i++) {
-            opening = sMathJaxOpenings[i];
-            closing = sMathJaxClosings[i];
-
-            //What if there are more than one thing?
-            //Let's look for the first opening, and the last closing, and if they're in the right order,
-            //we are good.
-
-            int first_opening_index = txt.indexOf(opening);
-            int last_closing_index = txt.lastIndexOf(closing);
-
-            if (first_opening_index != -1
-                    && last_closing_index != -1
-                    && first_opening_index < last_closing_index)
-            {
-                return true;
+            // What if there are more than one thing?
+            // Let's look for the first opening, and the last closing, and if they're in the right order,
+            // we are good.
+            val first_opening_index = txt.indexOf(opening)
+            val last_closing_index = txt.lastIndexOf(closing)
+            if (first_opening_index != -1 && last_closing_index != -1 && first_opening_index < last_closing_index) {
+                return true
             }
         }
-        return false;
+        return false
     }
 }
