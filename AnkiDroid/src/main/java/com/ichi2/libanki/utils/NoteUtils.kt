8d19e71ebd446a36569d81b10d8601bf39850f5b
Nishant <getrooted0019@hotmail.com>
2022-05-06 02:46:28 +0530

    Migrated NoteUtils to kotlin

--- AnkiDroid/src/main/java/com/ichi2/libanki/utils/NoteUtils.kt
+++ AnkiDroid/src/main/java/com/ichi2/libanki/utils/NoteUtils.kt
@@ -13,27 +13,22 @@
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
+package com.ichi2.libanki.utils
 
-package com.ichi2.libanki.utils;
+import com.ichi2.libanki.Note
 
-import com.ichi2.libanki.Note;
-
-import java.util.AbstractSet;
-import java.util.List;
-
-public class NoteUtils {
-
+object NoteUtils {
     /** Set the set tags of currentNote to tagsList.  We make no
-     assumption on the content of tagsList, except that its strings
-     are valid tags (i.e. no spaces in it).*/
-    public static void setTags(Note currentNote, List<String> tagsList) {
-        String[] currentTags = currentNote.getTags().toArray(new String[0]);
-        for (String tag : currentTags) {
-            currentNote.delTag(tag);
+     * assumption on the content of tagsList, except that its strings
+     * are valid tags (i.e. no spaces in it). */
+    fun setTags(currentNote: Note, tagsList: List<String>?) {
+        val currentTags = currentNote.tags.toTypedArray()
+        for (tag in currentTags) {
+            currentNote.delTag(tag)
         }
         if (tagsList != null) {
-            AbstractSet<String> tagsSet = currentNote.getCol().getTags().canonify(tagsList);
-            currentNote.addTags(tagsSet);
+            val tagsSet = currentNote.col.tags.canonify(tagsList)
+            currentNote.addTags(tagsSet)
         }
     }
 }
