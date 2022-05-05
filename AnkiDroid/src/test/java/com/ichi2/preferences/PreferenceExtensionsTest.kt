dfbd6d26c540d2805a078c11bae064e7aef39be8
Prince kushwaha <kushwahaprince418@gmail.com>
2022-05-05 18:48:27 +0530

    Migrate PreferenceExtensionsTest.java to Kotlin

--- AnkiDroid/src/test/java/com/ichi2/preferences/PreferenceExtensionsTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/preferences/PreferenceExtensionsTest.kt
@@ -13,91 +13,88 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.preferences
 
-package com.ichi2.preferences;
+import android.content.SharedPreferences
+import com.ichi2.preferences.PreferenceExtensions.getOrSetString
+import com.ichi2.testutils.AnkiAssert.assertDoesNotThrow
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.ArgumentMatchers.eq
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import java.lang.RuntimeException
+import java.util.function.Supplier
 
-import android.content.SharedPreferences;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-
-import java.util.function.Supplier;
-
-import static com.ichi2.testutils.AnkiAssert.assertDoesNotThrow;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-
-//Unknown issue: @CheckResult should provide warnings on this class when return value is unused, but doesn't.
-public class PreferenceExtensionsTest {
-
-    private static final Supplier<String> UNUSED_SUPPLIER = () -> { throw new UnexpectedException();};
-    private static final Supplier<String> EXCEPTION_SUPPLIER = () -> { throw new ExpectedException();};
-
-    private static final String VALID_KEY = "VALID";
-    private static final String VALID_RESULT = "WAS VALID KEY";
-    private static final String MISSING_KEY = "INVALID";
-    private static final String LAMBDA_RETURN = "LAMBDA";
+// Unknown issue: @CheckResult should provide warnings on this class when return value is unused, but doesn't.
+class PreferenceExtensionsTest {
+    @Mock
+    lateinit var mockPreferences: SharedPreferences
 
     @Mock
-    private SharedPreferences mMockPreferences;
-
-    @Mock
-    private SharedPreferences.Editor mMockEditor;
-
-    private String getOrSetString(String key, Supplier<String> supplier) {
-        return PreferenceExtensions.getOrSetString(mMockPreferences, key, supplier);
+    private val mMockEditor: SharedPreferences.Editor? = null
+    private fun getOrSetString(key: String, supplier: Supplier<String?>): String? {
+        return getOrSetString(mockPreferences, key, supplier)
     }
 
     @Before
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-        Mockito.when(mMockPreferences.contains(VALID_KEY)).thenReturn(true);
-        Mockito.when(mMockPreferences.getString(eq(VALID_KEY), anyString())).thenReturn(VALID_RESULT);
-        Mockito.when(mMockPreferences.edit()).thenReturn(mMockEditor);
-        Mockito.when(mMockEditor.putString(anyString(), anyString())).thenReturn(mMockEditor);
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+        Mockito.`when`(mockPreferences.contains(VALID_KEY)).thenReturn(true)
+        Mockito.`when`(
+            mockPreferences.getString(eq(VALID_KEY), anyString())
+        ).thenReturn(
+            VALID_RESULT
+        )
+        Mockito.`when`(mockPreferences.edit()).thenReturn(mMockEditor)
+        Mockito.`when`(
+            mMockEditor!!.putString(anyString(), anyString())
+        ).thenReturn(mMockEditor)
     }
 
-    private String getForMissingKey() {
-        return getOrSetString(MISSING_KEY, () -> LAMBDA_RETURN);
+    private val forMissingKey: String?
+        get() = getOrSetString(MISSING_KEY) { LAMBDA_RETURN }
+
+    @Test
+    fun existingKeyReturnsMappedValue() {
+        val ret = getOrSetString(VALID_KEY, UNUSED_SUPPLIER)
+        assertEquals(ret, VALID_RESULT)
     }
 
     @Test
-    public void existingKeyReturnsMappedValue() {
-        String ret = getOrSetString(VALID_KEY, UNUSED_SUPPLIER);
-        assertEquals(ret, VALID_RESULT);
+    fun missingKeyReturnsLambdaValue() {
+        val ret = forMissingKey
+        assertEquals(ret, LAMBDA_RETURN)
     }
 
     @Test
-    public void missingKeyReturnsLambdaValue() {
-        String ret = getForMissingKey();
-        assertEquals(ret, LAMBDA_RETURN);
+    fun missingKeySetsPreference() {
+        forMissingKey
+        Mockito.verify(mMockEditor)?.putString(MISSING_KEY, LAMBDA_RETURN)
+        Mockito.verify(mMockEditor)?.apply()
     }
 
     @SuppressWarnings("unused")
-    @Test
-    public void missingKeySetsPreference() {
-        getForMissingKey();
-        Mockito.verify(mMockEditor).putString(MISSING_KEY, LAMBDA_RETURN);
-        Mockito.verify(mMockEditor).apply();
+    fun noLambdaExceptionIfKeyExists() {
+        assertDoesNotThrow { getOrSetString(VALID_KEY, EXCEPTION_SUPPLIER) }
     }
 
-    @SuppressWarnings("unused")
-    public void noLambdaExceptionIfKeyExists() {
-         assertDoesNotThrow(() -> getOrSetString(VALID_KEY, EXCEPTION_SUPPLIER));
+    @Test(expected = ExpectedException::class)
+    fun rethrowLambdaExceptionIfKeyIsMissing() {
+        getOrSetString(MISSING_KEY, EXCEPTION_SUPPLIER)
     }
 
-    @SuppressWarnings("unused")
-    @Test(expected = ExpectedException.class)
-    public void rethrowLambdaExceptionIfKeyIsMissing() {
-        getOrSetString(MISSING_KEY, EXCEPTION_SUPPLIER);
-    }
-
-    private static class ExpectedException extends RuntimeException {
-    }
-    private static class UnexpectedException extends RuntimeException {
+    private class ExpectedException : RuntimeException()
+    private class UnexpectedException : RuntimeException()
+    companion object {
+        private val UNUSED_SUPPLIER = Supplier<String?> { throw UnexpectedException() }
+        private val EXCEPTION_SUPPLIER = Supplier<String?> { throw ExpectedException() }
+        private const val VALID_KEY = "VALID"
+        private const val VALID_RESULT = "WAS VALID KEY"
+        private const val MISSING_KEY = "INVALID"
+        private const val LAMBDA_RETURN = "LAMBDA"
     }
 }
