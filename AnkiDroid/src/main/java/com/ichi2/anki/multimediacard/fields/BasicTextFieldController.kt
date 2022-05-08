aa17b8817768440b1e249d93ae4a979179edf130
lukstbit <lukstbit@users.noreply.github.com>
2022-05-08 07:06:46 +0300

    Remove code related to translation from project

--- AnkiDroid/src/main/java/com/ichi2/anki/multimediacard/fields/BasicTextFieldController.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/multimediacard/fields/BasicTextFieldController.kt
@@ -23,20 +23,14 @@ import android.app.Activity
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.view.Gravity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
-import android.widget.TextView
 import com.ichi2.anki.R
 import com.ichi2.anki.UIUtils.showThemedToast
 import com.ichi2.anki.multimediacard.activity.LoadPronunciationActivity
 import com.ichi2.anki.multimediacard.activity.PickStringDialogFragment
-import com.ichi2.anki.multimediacard.activity.TranslationActivity
-import com.ichi2.compat.CompatHelper
 import com.ichi2.ui.FixedEditText
-import com.ichi2.ui.FixedTextView
 import timber.log.Timber
 import java.io.File
 
@@ -61,16 +55,11 @@ class BasicTextFieldController : FieldControllerBase(), IFieldController, Dialog
         val p = LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1F)
         createCloneButton(layoutTools, p)
         createClearButton(layoutTools, p)
-        // search label
-        val searchLabel: TextView = FixedTextView(mActivity)
-        searchLabel.setText(R.string.multimedia_editor_text_field_editing_search_label)
-        layout.addView(searchLabel)
         // search buttons
         val layoutTools2 = LinearLayout(mActivity)
         layoutTools2.orientation = LinearLayout.HORIZONTAL
         layout.addView(layoutTools2)
-        createTranslateButton(layoutTools2, p)
-        createPronounceButton(layoutTools2, p)
+        createPronounceButton(layoutTools2, LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT))
     }
 
     private fun gtxt(id: Int): String {
@@ -102,47 +91,6 @@ class BasicTextFieldController : FieldControllerBase(), IFieldController, Dialog
             mActivity.startActivityForResultWithoutAnimation(intent, REQUEST_CODE_PRONUNCIATION)
         }
         layoutTools.addView(btnPronounce, p)
-    }
-
-    // Here is all the functionality to provide translations
-    private fun createTranslateButton(layoutTool: LinearLayout, ps: LinearLayout.LayoutParams) {
-        val btnTranslate = Button(mActivity)
-        btnTranslate.text = gtxt(R.string.multimedia_editor_text_field_editing_translate)
-        btnTranslate.setOnClickListener {
-            val source = mEditText.text.toString()
-
-            // Checks and warnings
-            if (source.isEmpty()) {
-                showToast(gtxt(R.string.multimedia_editor_text_field_editing_no_text))
-                return@setOnClickListener
-            }
-            if (source.contains(" ")) {
-                showToast(gtxt(R.string.multimedia_editor_text_field_editing_many_words))
-            }
-
-            // Pick from two translation sources
-            val fragment = PickStringDialogFragment()
-            val translationSources = ArrayList<String>(2)
-            translationSources.add("Glosbe.com")
-            // Chromebooks do not support dependent apps yet.
-            if (!CompatHelper.isChromebook) {
-                translationSources.add("ColorDict")
-            }
-            fragment.setChoices(translationSources)
-            fragment.setOnclickListener { _: DialogInterface?, which: Int ->
-                val translationSource = translationSources[which]
-                if ("Glosbe.com" == translationSource) {
-                    startTranslationWithGlosbe()
-                } else if ("ColorDict" == translationSource) {
-                    startTranslationWithColorDict()
-                }
-            }
-            fragment.setTitle(gtxt(R.string.multimedia_editor_trans_pick_translation_source))
-            fragment.show(mActivity.supportFragmentManager, "pick.translation.source")
-        }
-        layoutTool.addView(btnTranslate, ps)
-
-        // flow continues in Start Translation with...
     }
 
     /**
@@ -197,16 +145,7 @@ class BasicTextFieldController : FieldControllerBase(), IFieldController, Dialog
      * activity are received.
      */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_CODE_TRANSLATE_GLOSBE && resultCode == Activity.RESULT_OK) {
-            // Translation returned.
-            try {
-                val translation = data!!.extras!![TranslationActivity.EXTRA_TRANSLATION].toString()
-                mEditText.setText(translation)
-            } catch (e: Exception) {
-                Timber.w(e)
-                showToast(gtxt(R.string.multimedia_editor_something_wrong))
-            }
-        } else if (requestCode == REQUEST_CODE_PRONUNCIATION && resultCode == Activity.RESULT_OK) {
+        if (requestCode == REQUEST_CODE_PRONUNCIATION && resultCode == Activity.RESULT_OK) {
             try {
                 val pronouncePath = data!!.extras!![LoadPronunciationActivity.EXTRA_PRONUNCIATION_FILE_PATH]
                     .toString()
@@ -223,10 +162,6 @@ class BasicTextFieldController : FieldControllerBase(), IFieldController, Dialog
                 Timber.w(e)
                 showToast(gtxt(R.string.multimedia_editor_pron_pronunciation_failed))
             }
-        } else if (requestCode == REQUEST_CODE_TRANSLATE_COLORDICT && resultCode == Activity.RESULT_OK) {
-            // String subject = data.getStringExtra(Intent.EXTRA_SUBJECT);
-            val text = data!!.getStringExtra(Intent.EXTRA_TEXT)
-            mEditText.setText(text)
         }
     }
 
@@ -251,62 +186,12 @@ class BasicTextFieldController : FieldControllerBase(), IFieldController, Dialog
         showThemedToast(mActivity, text, true)
     }
 
-    // Only now not all APIs are used, may be later, they will be.
-    @Suppress("Unused_Variable")
-    private fun startTranslationWithColorDict() {
-        val PICK_RESULT_ACTION = "colordict.intent.action.PICK_RESULT"
-        val SEARCH_ACTION = "colordict.intent.action.SEARCH"
-        val EXTRA_QUERY = "EXTRA_QUERY"
-        val EXTRA_FULLSCREEN = "EXTRA_FULLSCREEN"
-        val EXTRA_HEIGHT = "EXTRA_HEIGHT"
-        val EXTRA_WIDTH = "EXTRA_WIDTH"
-        val EXTRA_GRAVITY = "EXTRA_GRAVITY"
-        val EXTRA_MARGIN_LEFT = "EXTRA_MARGIN_LEFT"
-        val EXTRA_MARGIN_TOP = "EXTRA_MARGIN_TOP"
-        val EXTRA_MARGIN_BOTTOM = "EXTRA_MARGIN_BOTTOM"
-        val EXTRA_MARGIN_RIGHT = "EXTRA_MARGIN_RIGHT"
-        val intent = Intent(PICK_RESULT_ACTION)
-        intent.putExtra(EXTRA_QUERY, mEditText.text.toString()) // Search
-        // Query
-        intent.putExtra(EXTRA_FULLSCREEN, false) //
-        // intent.putExtra(EXTRA_HEIGHT, 400); //400pixel, if you don't specify,
-        // fill_parent"
-        intent.putExtra(EXTRA_GRAVITY, Gravity.BOTTOM)
-        // intent.putExtra(EXTRA_MARGIN_LEFT, 100);
-        if (!isIntentAvailable(mActivity, intent)) {
-            showToast(gtxt(R.string.multimedia_editor_trans_install_color_dict))
-            return
-        }
-        mActivity.startActivityForResultWithoutAnimation(intent, REQUEST_CODE_TRANSLATE_COLORDICT)
-    }
-
-    private fun startTranslationWithGlosbe() {
-        val source = mEditText.text.toString()
-        val intent = Intent(mActivity, TranslationActivity::class.java)
-        intent.putExtra(TranslationActivity.EXTRA_SOURCE, source)
-        mActivity.startActivityForResultWithoutAnimation(intent, REQUEST_CODE_TRANSLATE_GLOSBE)
-    }
-
     override fun onDestroy() {
         // TODO Auto-generated method stub
     }
 
     companion object {
-        // Additional activities are started to perform translation/pronunciation search and
-        // so on, here are their request codes, to differentiate, when they return.
-        private const val REQUEST_CODE_TRANSLATE_GLOSBE = 101
+        // code to identify the request to fetch pronunciations
         private const val REQUEST_CODE_PRONUNCIATION = 102
-        private const val REQUEST_CODE_TRANSLATE_COLORDICT = 103
-
-        /**
-         * @param context context with the PackageManager
-         * @param intent intent for state data
-         * @return Needed to check, if the Color Dict is installed
-         */
-        private fun isIntentAvailable(context: Context, intent: Intent): Boolean {
-            val packageManager = context.packageManager
-            val list: List<*> = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
-            return list.isNotEmpty()
-        }
     }
 }
