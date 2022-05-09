ae3635836b08047d184291f8b992ebec22ed17f4
Nishant <getrooted0019@hotmail.com>
2022-05-09 23:10:42 +0530

    Migrate BasicModel to Kotlin

--- api/src/main/java/com/ichi2/anki/api/BasicModel.kt
+++ api/src/main/java/com/ichi2/anki/api/BasicModel.kt
@@ -1,14 +1,20 @@
 //noinspection MissingCopyrightHeader #8659
-package com.ichi2.anki.api;
+package com.ichi2.anki.api
 
 /**
  * Definitions of the basic model
  */
-class BasicModel {
-    public static final String[] FIELDS = {"Front","Back"};
-    // List of card names that will be used in AnkiDroid (one for each direction of learning)
-    public static final String[] CARD_NAMES = {"Card 1"};
-    // Template for the question of each card
-    static final String[] QFMT = {"{{Front}}"};
-    static final String[] AFMT = {"{{FrontSide}}\n\n<hr id=\"answer\">\n\n{{Back}}"};
+internal class BasicModel {
+    companion object {
+        @JvmField
+        var FIELDS = arrayOf("Front", "Back")
+        // List of card names that will be used in AnkiDroid (one for each direction of learning)
+        @JvmField
+        val CARD_NAMES = arrayOf("Card 1")
+        // Template for the question of each card
+        @JvmField
+        val QFMT = arrayOf("{{Front}}")
+        @JvmField
+        val AFMT = arrayOf("{{FrontSide}}\n\n<hr id=\"answer\">\n\n{{Back}}")
+    }
 }
