9ae744759ab2a389a26d414a683b575e8c0b209a
David Allison <62114487+david-allison@users.noreply.github.com>
2022-05-10 23:12:46 +0100

    refactor: Convert Constants to Kotlin
    com.ichi2.anki.lint.utils.Constants

--- lint-rules/src/main/java/com/ichi2/anki/lint/utils/Constants.kt
+++ lint-rules/src/main/java/com/ichi2/anki/lint/utils/Constants.kt
@@ -1,78 +1,80 @@
-package com.ichi2.anki.lint.utils;
+package com.ichi2.anki.lint.utils
 
-import com.android.tools.lint.detector.api.Category;
-import com.android.tools.lint.detector.api.Severity;
-
-import static com.android.tools.lint.detector.api.Category.create;
+import com.android.tools.lint.detector.api.Category
+import com.android.tools.lint.detector.api.Category.Companion.create
+import com.android.tools.lint.detector.api.Severity
 
 /**
  * Hold some constants applicable to all lint issues.
  */
-public class Constants {
-
+@KotlinCleanup("IDE Lint")
+object Constants {
     /**
-     * A special {@link Category} which groups the Lint issues related to the usage of the new SystemTime class as a
-     * sub category for {@link Category#CORRECTNESS}.
+     * A special [Category] which groups the Lint issues related to the usage of the new SystemTime class as a
+     * sub category for [Category.CORRECTNESS].
      */
-    public static final Category ANKI_TIME_CATEGORY = create(Category.CORRECTNESS, "AnkiTime", 10);
+    @JvmField
+    val ANKI_TIME_CATEGORY: Category = create(Category.CORRECTNESS, "AnkiTime", 10)
 
     /**
      * The priority for the Lint issues used by all rules related to the restrictions introduced by SystemTime.
      */
-    public static final int ANKI_TIME_PRIORITY = 10;
+    const val ANKI_TIME_PRIORITY = 10
 
     /**
      * The severity for the Lint issues used by all rules related to the restrictions introduced by SystemTime.
      */
-    public static final Severity ANKI_TIME_SEVERITY = Severity.FATAL;
+    @JvmField
+    val ANKI_TIME_SEVERITY = Severity.FATAL
 
     /**
      * The priority for the Lint issues used by all rules related to the restrictions introduced by SystemTime.
      */
-    public static final int ANKI_CROWDIN_PRIORITY = 10;
+    const val ANKI_CROWDIN_PRIORITY = 10
 
     /**
-     * A special {@link Category} which groups the Lint issues related to the usage of CrowdIn as a
-     * sub category for {@link Category#CORRECTNESS}.
+     * A special [Category] which groups the Lint issues related to the usage of CrowdIn as a
+     * sub category for [Category.CORRECTNESS].
      */
-    public static final Category ANKI_CROWDIN_CATEGORY = create(Category.CORRECTNESS, "AnkiCrowdIn", ANKI_CROWDIN_PRIORITY);
-
+    val ANKI_CROWDIN_CATEGORY: Category =
+        create(Category.CORRECTNESS, "AnkiCrowdIn", ANKI_CROWDIN_PRIORITY)
 
     /**
      * The severity for the Lint issues used by all rules related to CrowdIn restrictions.
      */
-    public static final Severity ANKI_CROWDIN_SEVERITY = Severity.FATAL;
+    val ANKI_CROWDIN_SEVERITY = Severity.FATAL
 
     /**
-     * A special {@link Category} which groups the Lint issues related to Code Style as a
-     * sub category for {@link Category#COMPLIANCE}.
+     * A special [Category] which groups the Lint issues related to Code Style as a
+     * sub category for [Category.COMPLIANCE].
      */
-    public static final Category ANKI_CODE_STYLE_CATEGORY = create(Category.COMPLIANCE, "CodeStyle", 10);
+    @JvmField
+    val ANKI_CODE_STYLE_CATEGORY: Category = create(Category.COMPLIANCE, "CodeStyle", 10)
 
     /**
      * The priority for the Lint issues used by rules related to Code Style.
      */
-    public static final int ANKI_CODE_STYLE_PRIORITY = 10;
+    const val ANKI_CODE_STYLE_PRIORITY = 10
 
     /**
      * The severity for the Lint issues used by rules related to Code Style.
      */
-    public static final Severity ANKI_CODE_STYLE_SEVERITY = Severity.FATAL;
+    @JvmField
+    val ANKI_CODE_STYLE_SEVERITY = Severity.FATAL
 
     /**
-     * A special {@link Category} which groups the Lint issues related to XML as a
-     * sub category for {@link Category#CORRECTNESS}.
+     * A special [Category] which groups the Lint issues related to XML as a
+     * sub category for [Category.CORRECTNESS].
      */
-    public static final Category ANKI_XML_CATEGORY = create(Category.CORRECTNESS, "XML", 10);
+    val ANKI_XML_CATEGORY: Category = create(Category.CORRECTNESS, "XML", 10)
 
     /**
      * The priority for the Lint issues used by rules related to XML.
      */
-    public static final int ANKI_XML_PRIORITY = 10;
+    const val ANKI_XML_PRIORITY = 10
 
     /**
      * The severity for the Lint issues used by rules related to XML.
      */
-    public static final Severity ANKI_XML_SEVERITY =  Severity.FATAL;
-
+    val ANKI_XML_SEVERITY = Severity.FATAL
 }
