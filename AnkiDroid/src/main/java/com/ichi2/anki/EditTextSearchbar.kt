223d5b977fa0dc36d33d5b02e4b0dc1d1d1579f3
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-05-13 19:00:35 +0430

    set icons' visibility on search open and close, added toggleCaseSensitivity listener

--- AnkiDroid/src/main/java/com/ichi2/anki/EditTextSearchbar.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/EditTextSearchbar.kt
@@ -2,18 +2,35 @@ package com.ichi2.anki
 
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.MenuItem
 import androidx.appcompat.widget.SearchView
 import com.ichi2.ui.FixedEditText
 import timber.log.Timber
 
 class EditTextSearchbar(
     private var querySearchbar: SearchView,
-    private var targetEditText: FixedEditText
+    private var targetEditText: FixedEditText,
+    private var findNextButton: MenuItem,
+    private var findPrevButton: MenuItem,
+    private var toggleCaseSensitivityButton: MenuItem
 ) : TextWatcher, SearchView.OnQueryTextListener {
+    val caseSensitive: Boolean
+        get() = toggleCaseSensitivityButton.isChecked
 
     fun setupListeners() {
         querySearchbar.setOnQueryTextListener(this)
         targetEditText.addTextChangedListener(this)
+
+        querySearchbar.setOnSearchClickListener { setIconsVisibility(true) }
+        querySearchbar.setOnCloseListener { setIconsVisibility(false) }
+    }
+
+    private fun setIconsVisibility(visibility: Boolean): Boolean {
+        findNextButton.isVisible = visibility
+        findPrevButton.isVisible = visibility
+        toggleCaseSensitivityButton.isVisible = visibility
+
+        return false
     }
 
     override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
