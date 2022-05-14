fdaef5cdf5f0389b843ee2b2bc07ce4875434b9c
camo0112 <56369484+camo0112@users.noreply.github.com>
2022-05-14 18:48:44 +0530

    added test for CountModels

--- AnkiDroid/src/test/java/com/ichi2/async/CollectionTaskCountModelsTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/async/CollectionTaskCountModelsTest.kt
@@ -15,43 +15,28 @@
  */
 package com.ichi2.async
 
-import android.util.Pair
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.ichi2.anki.RunInBackground
 import com.ichi2.async.CollectionTask.CountModels
-import com.ichi2.libanki.Model
-import com.ichi2.utils.KotlinCleanup
 import org.hamcrest.MatcherAssert.*
 import org.hamcrest.Matchers.*
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.*
-import java.util.ArrayList
 
 @RunWith(AndroidJUnit4::class)
-@KotlinCleanup("is -> equalTo")
-@KotlinCleanup("scope functions for argument matchers")
 class CollectionTaskCountModelsTest : AbstractCollectionTaskTest() {
+
     @Test
     @RunInBackground
     fun testModelsCount() {
-        @Suppress("UNCHECKED_CAST")
-        val listener: TaskListener<Void, Pair<MutableList<Model>, ArrayList<Int>>> = mock(TaskListener::class.java) as TaskListener<Void, Pair<MutableList<Model>, ArrayList<Int>>>
-        val task: TaskDelegate<Void, Pair<MutableList<Model>, ArrayList<Int>>> = CountModels()
-
-        waitForTask(task, listener)
+        val task = CountModels()
         val initialCount = execute(task)!!.first.size
 
         addNonClozeModel("testModel", arrayOf<String>(), qfmt = "{{ front }}", afmt = "{{FrontSide}}\n\n<hr id=answer>\n\n{{ back }}")
 
-        @Suppress("UNCHECKED_CAST")
-        val listener2: TaskListener<Void, Pair<MutableList<Model>, ArrayList<Int>>> = mock(TaskListener::class.java) as TaskListener<java.lang.Void, Pair<MutableList<Model>, ArrayList<Int>>>
-        val task2: TaskDelegate<Void, Pair<MutableList<Model>, ArrayList<Int>>> = CountModels()
-
-        waitForTask(task2, listener2)
-
-        val finalCount = execute(task2)!!.first.size
+        val finalCount = execute(task)!!.first.size
         assertEquals(initialCount + 1, finalCount)
     }
 }
