e706075d87ea3ffd2e6dedbb23ae7ba060d5b41b
oyeraghib <raghib.khan76@gmail.com>
2022-05-16 17:20:56 +0530

    [Coroutines] Implement BaseCoroutines Class

---
+++ AnkiDroid/src/main/java/com/ichi2/async/BaseCoroutinesTask.kt
@@ -0,0 +1,53 @@
+/****************************************************************************************
+ * Copyright (c) 2022 Mohd Raghib <raghib.khan76@gmail.com>                             *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 3 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
+package com.ichi2.async
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+abstract class BaseCoroutinesTask<Params, Progress, Result> {
+
+//    open fun onPreExecute() {}
+
+    abstract fun onPostExecute(result: Result?)
+
+//    open fun onProgressUpdate(vararg values: Progress) {}
+
+//    open fun onCancelled(result: Boolean) {}
+
+    abstract fun doInBackground(vararg params: Params?): Result?
+
+//    open fun doProgress(value: Progress?) {}
+
+//    protected fun publishProgress(vararg values: Progress?) {}
+
+    fun execute(vararg params: Params?) {
+//            onPreExecute()
+        CoroutineScope(Dispatchers.Default).launch {
+            val result = doInBackground(*params)
+            withContext(Dispatchers.Main) {
+                onPostExecute(result)
+            }
+        }
+    }
+}
+
+// Need to implement
+//    fun cancel(mayInterruptIfRunning: Boolean) {
+//    }
