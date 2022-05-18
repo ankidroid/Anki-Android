5fcae3dfe2dcd17991f983819a236d372b613d1c
Nishant <getrooted0019@hotmail.com>
2022-05-18 21:21:55 +0530

    Migrate Basic2Model to Kotlin

--- api/src/main/java/com/ichi2/anki/api/Basic2Model.kt
+++ api/src/main/java/com/ichi2/anki/api/Basic2Model.kt
@@ -1,15 +1,21 @@
 //noinspection MissingCopyrightHeader #8659
-package com.ichi2.anki.api;
+package com.ichi2.anki.api
 
 /**
  * Definitions of the basic with reverse model
  */
-class Basic2Model {
-    public static final String[] FIELDS = {"Front","Back"};
+object Basic2Model {
+    @JvmField
+    val FIELDS = arrayOf("Front", "Back")
     // List of card names that will be used in AnkiDroid (one for each direction of learning)
-    public static final String[] CARD_NAMES = {"Card 1", "Card 2"};
+    @JvmField
+    val CARD_NAMES = arrayOf("Card 1", "Card 2")
     // Template for the question of each card
-    static final String[] QFMT = {"{{Front}}", "{{Back}}"};
-    static final String[] AFMT = {"{{FrontSide}}\n\n<hr id=\"answer\">\n\n{{Back}}",
-            "{{FrontSide}}\n\n<hr id=\"answer\">\n\n{{Front}}"};
+    @JvmField
+    val QFMT = arrayOf("{{Front}}", "{{Back}}")
+    @JvmField
+    val AFMT = arrayOf(
+        "{{FrontSide}}\n\n<hr id=\"answer\">\n\n{{Back}}",
+        "{{FrontSide}}\n\n<hr id=\"answer\">\n\n{{Front}}"
+    )
 }
