7b5692483b7965a52751fb40f71120d692b8dcc8
Prince kushwaha <kushwahaprince418@gmail.com>
2022-05-19 17:02:30 +0530

    Migrate ModelTest.java to Kotlin

--- AnkiDroid/src/androidTest/java/com/ichi2/anki/tests/libanki/ModelTest.kt
+++ AnkiDroid/src/androidTest/java/com/ichi2/anki/tests/libanki/ModelTest.kt
@@ -15,60 +15,56 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-package com.ichi2.anki.tests.libanki;
+package com.ichi2.anki.tests.libanki
 
-import android.os.Build;
+import android.os.Build
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.anki.tests.InstrumentedTest
+import com.ichi2.libanki.Collection
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assume.assumeTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
 
-import com.ichi2.anki.tests.InstrumentedTest;
-import com.ichi2.libanki.Collection;
-import com.ichi2.libanki.Model;
-import com.ichi2.libanki.ModelManager;
+@RunWith(AndroidJUnit4::class)
+class ModelTest : InstrumentedTest() {
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import java.io.IOException;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assume.assumeTrue;
-
-@RunWith(AndroidJUnit4.class)
-public class ModelTest extends InstrumentedTest {
-
-    private Collection mTestCol;
-
+    private var mTestCol: Collection? = null
     @Before
-    public void setUp() throws IOException {
-        mTestCol = getEmptyCol();
+    @Throws(IOException::class)
+    fun setUp() {
+        mTestCol = emptyCol
     }
 
     @After
-    public void tearDown() {
-        mTestCol.close();
+    fun tearDown() {
+        mTestCol!!.close()
     }
 
     @Test
-    public void bigQuery() {
-        assumeTrue("This test is flaky on API29, ignoring", Build.VERSION.SDK_INT != Build.VERSION_CODES.Q);
-        ModelManager models = mTestCol.getModels();
-        Model model = models.all().get(0);
-        final String testString = "test";
-        final int size = testString.length() * 1024 * 1024;
-        StringBuilder buf = new StringBuilder((int) (size * 1.01));
+    fun bigQuery() {
+        assumeTrue(
+            "This test is flaky on API29, ignoring",
+            Build.VERSION.SDK_INT != Build.VERSION_CODES.Q
+        )
+        val models = mTestCol!!.models
+        val model = models.all()[0]
+        val testString = "test"
+        val size = testString.length * 1024 * 1024
+        val buf = StringBuilder((size * 1.01).toInt())
         // * 1.01 for padding
-        for (int i = 0; i < 1024 * 1024 ; ++i ) {
-            buf.append(testString);
+        for (i in 0 until 1024 * 1024) {
+            buf.append(testString)
         }
-        model.put(testString, buf.toString());
+        model.put(testString, buf.toString())
         // Buf should be more than 4MB, so at least two chunks from database.
-        models.flush();
+        models.flush()
         // Reload models
-        mTestCol.load();
-        Model newModel = models.all().get(0);
-        assertEquals(newModel, model);
+        mTestCol!!.load()
+        val newModel = models.all()[0]
+        assertEquals(newModel, model)
     }
 }
