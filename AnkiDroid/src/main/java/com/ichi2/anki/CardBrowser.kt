92804bd8a57d8b33087915d523c51bd04d772c44
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-05-29 16:52:09 +0430

    onSelectionChanged cleanup

--- AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.kt
@@ -2298,18 +2298,17 @@ open class CardBrowser : NavigationDrawerActivity(), SubtitleListener, DeckSelec
             }
 
             // If we're not in mutliselect, we can select cards if there are cards to select
-            if (!isInMultiSelectMode && mActionBarMenu != null) {
-                val selectAll = mActionBarMenu!!.findItem(R.id.action_select_all)
-                selectAll.isVisible = true && cardCount() != 0L
-            }
             if (!isInMultiSelectMode) {
+                mActionBarMenu?.findItem(R.id.action_select_all)?.apply {
+                    isVisible = cardCount() != 0L
+                }
                 return
             }
             updateMultiselectMenu()
             mActionBarTitle!!.text = String.format(LanguageUtil.getLocaleCompat(resources), "%d", checkedCardCount())
         } finally {
-            if (colIsOpen() && mCardsAdapter != null) {
-                mCardsAdapter!!.notifyDataSetChanged()
+            if (colIsOpen()) {
+                mCardsAdapter?.notifyDataSetChanged()
             }
         }
     }
