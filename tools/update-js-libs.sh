7c40a851ee28b5a7304251fbf4ec6af637be5991
krmanik <12841290+krmanik@users.noreply.github.com>
2022-05-31 14:08:40 +0800

    restore file mode, change paths in conf.js

--- tools/update-js-libs.sh
+++ tools/update-js-libs.sh
@@ -11,7 +11,11 @@ chmod +x bazel && sudo mv bazel /usr/local/bin/
 git clone https://github.com/ankitects/anki ~/tmp/anki
 
 # build for mathjax and jquery
-cd ~/tmp/anki/qt/aqt/data/web/js/vendor; bazel build vendor
+cd ~/tmp/anki/qt/aqt/data/web/js/vendor
+bazel build vendor
+
+cd ~/tmp/anki/ts 
+bazel build mathjax
 
 # change dir to AnkiDroid
 cd /home/runner/work/Anki-Android/Anki-Android
@@ -20,8 +24,21 @@ cd /home/runner/work/Anki-Android/Anki-Android
 cp ~/tmp/anki/.bazel/bin/qt/aqt/data/web/js/vendor/jquery.min.js AnkiDroid/src/main/assets/jquery.min.js
 
 # remove old mathjax file
-rm -rf AnkiDroid/src/main/assets/mathjax
-mkdir AnkiDroid/src/main/assets/mathjax
+rm -rf AnkiDroid/src/main/assets/mathjax/*
 
 # copy latest mathjax to assets dir
 cp -r ~/tmp/anki/.bazel/bin/qt/aqt/data/web/js/vendor/mathjax AnkiDroid/src/main/assets/
+
+cp ~/tmp/anki/.bazel/bin/ts/mathjax/index.js AnkiDroid/src/main/assets/mathjax/conf.js
+
+# replace paths in conf.js for AnkiDroid
+sed -i 's/_anki\/js\/vendor\/mathjax/android_asset\/mathjax/' AnkiDroid/src/main/assets/mathjax/conf.js
+
+# cleanup
+rm AnkiDroid/src/main/assets/mathjax/mathjax-cp.sh
+
+# restore mathjax.css file
+git restore AnkiDroid/src/main/assets/mathjax/mathjax.css
+
+# change mode of files in assets dir
+find AnkiDroid/src/main/assets/mathjax/ -type f -print0 | xargs -0 chmod 644
