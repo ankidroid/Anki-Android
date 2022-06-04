d2e1815557b3f92013c105a22b16dff96d33b84e
Nishant <getrooted0019@hotmail.com>
2022-06-04 23:51:20 +0530

    Revert "Added fullscreen methods to Compat.kt"
    This reverts commit ba4e765e10dcb67b60132097ad1354f8fc63dbfb.

--- AnkiDroid/src/main/java/com/ichi2/compat/Compat.kt
+++ AnkiDroid/src/main/java/com/ichi2/compat/Compat.kt
@@ -27,7 +27,6 @@ import android.media.AudioManager
 import android.media.AudioManager.OnAudioFocusChangeListener
 import android.media.MediaRecorder
 import android.net.Uri
-import android.view.Window
 import android.widget.TimePicker
 import androidx.annotation.IntDef
 import java.io.*
@@ -119,10 +118,6 @@ interface Compat {
     fun hasVideoThumbnail(path: String): Boolean
     fun requestAudioFocus(audioManager: AudioManager, audioFocusChangeListener: OnAudioFocusChangeListener, audioFocusRequest: AudioFocusRequest?)
     fun abandonAudioFocus(audioManager: AudioManager, audioFocusChangeListener: OnAudioFocusChangeListener, audioFocusRequest: AudioFocusRequest?)
-
-    fun setFullscreen(window: Window?)
-    fun hideSystemBars(window: Window?)
-    fun isImmersiveSystemUiVisible(window: Window?): Boolean
 
     @IntDef(
         flag = true,
