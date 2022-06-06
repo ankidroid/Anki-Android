931c7f332503f831d54dd5785102e6da919347a1
krmanik <12841290+krmanik@users.noreply.github.com>
2022-06-06 15:14:09 +0800

    convert update function to ts

--- tools/localization/src/index.ts
+++ tools/localization/src/index.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { uploadI18nFiles } from "./upload";
 import { buildAndDownload, extractZip } from "./download";
+import { updateI18nFiles } from "./update";
 
 // upload latest (english) source i18n files
 uploadI18nFiles();
@@ -9,5 +10,8 @@ uploadI18nFiles();
 buildAndDownload();
 
 // extract to a temp dir
+console.log("extracting...");
 const temp_dir = path.join(__dirname, "../temp_dir");
 extractZip("ankidroid.zip", temp_dir);
+
+updateI18nFiles();
