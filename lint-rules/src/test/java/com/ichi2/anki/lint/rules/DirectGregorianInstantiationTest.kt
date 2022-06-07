3a4ec5a091d26493dbc8a51d93e037c88b7d70dd
Prateek Singh <76490368+prateek-singh-3212@users.noreply.github.com>
2022-06-07 14:29:15 +0530

    Converted DirectGregorianInstantiationTest [Lint Test] to Kotlin

--- lint-rules/src/test/java/com/ichi2/anki/lint/rules/DirectGregorianInstantiationTest.kt
+++ lint-rules/src/test/java/com/ichi2/anki/lint/rules/DirectGregorianInstantiationTest.kt
@@ -1,117 +1,125 @@
-package com.ichi2.anki.lint.rules;
+package com.ichi2.anki.lint.rules
 
-import org.intellij.lang.annotations.Language;
-import org.junit.Test;
+import com.android.tools.lint.checks.infrastructure.TestFile.JavaTestFile
+import com.android.tools.lint.checks.infrastructure.TestLintTask
+import com.google.common.annotations.Beta
+import org.intellij.lang.annotations.Language
+import org.junit.Assert
+import org.junit.Test
 
-import static com.android.tools.lint.checks.infrastructure.TestFile.JavaTestFile.create;
-import static com.android.tools.lint.checks.infrastructure.TestLintTask.lint;
-import static org.junit.Assert.assertTrue;
-
-public class DirectGregorianInstantiationTest {
+@Suppress("UnstableApiUsage")
+@Beta
+class DirectGregorianInstantiationTest {
+    @Language("JAVA")
+    private val stubZoned = """                                      
+package java.time;                                             
+                                                               
+public class ZonedDateTime {                                   
+                                                               
+                                                               
+     public ZonedDateTime() {                                  
+                                                               
+     }                                                         
+}                                                              
+"""
 
     @Language("JAVA")
-    private final String stubZoned = "                                      \n" +
-            "package java.time;                                             \n" +
-            "                                                               \n" +
-            "public class ZonedDateTime {                                   \n" +
-            "                                                               \n" +
-            "                                                               \n" +
-            "     public ZonedDateTime() {                                  \n" +
-            "                                                               \n" +
-            "     }                                                         \n" +
-            "}                                                              \n";
-    @Language("JAVA")
-    private final String stubGregorian = "                                  \n" +
-            "package java.util;                                             \n" +
-            "                                                               \n" +
-            "import java.time.ZonedDateTime;                                \n" +
-            "                                                               \n" +
-            "public class GregorianCalendar {                               \n" +
-            "                                                               \n" +
-            "    public GregorianCalendar() {                               \n" +
-            "    }                                                          \n" +
-            "                                                               \n" +
-            "                                                               \n" +
-            "    public static GregorianCalendar from(ZonedDateTime z) {    \n" +
-            "        return null;                                           \n" +
-            "    }                                                          \n" +
-            "}                                                              \n";
+    private val stubGregorian = """                                  
+package java.util;                                             
+                                                               
+import java.time.ZonedDateTime;                                
+                                                               
+public class GregorianCalendar {                               
+                                                               
+    public GregorianCalendar() {                               
+    }                                                          
+                                                               
+                                                               
+    public static GregorianCalendar from(ZonedDateTime z) {    
+        return null;                                           
+    }                                                          
+}                                                              
+"""
 
     @Language("JAVA")
-    private final String javaFileWithFromCall = "                           \n" +
-            "package com.ichi2.anki.lint.rules;                             \n" +
-            "                                                               \n" +
-            "import java.util.GregorianCalendar;                            \n" +
-            "                                                               \n" +
-            "public class TestJavaClass {                                   \n" +
-            "                                                               \n" +
-            "    public static void main(String[] args) {                   \n" +
-            "        GregorianCalendar gc = GregorianCalendar.from(null);   \n" +
-            "    }                                                          \n" +
-            "}                                                              \n";
-    @Language("JAVA")
-    private final String javaFileWithConstructorInvocation = "              \n" +
-            "package com.ichi2.anki.lint.rules;                             \n" +
-            "                                                               \n" +
-            "import java.util.GregorianCalendar;                            \n" +
-            "                                                               \n" +
-            "public class TestJavaClass {                                   \n" +
-            "                                                               \n" +
-            "    public static void main(String[] args) {                   \n" +
-            "        GregorianCalendar gc = new GregorianCalendar();        \n" +
-            "    }                                                          \n" +
-            "}                                                              \n";
-    @Language("JAVA")
-    private final String javaFileWithTime = "                               \n" +
-            "package com.ichi2.anki.lint.rules;                             \n" +
-            "                                                               \n" +
-            "import java.util.GregorianCalendar;                            \n" +
-            "                                                               \n" +
-            "public class Time {                                            \n" +
-            "                                                               \n" +
-            "    public static void main(String[] args) {                   \n" +
-            "        GregorianCalendar gc = new GregorianCalendar();        \n" +
-            "    }                                                          \n" +
-            "}                                                              \n";
+    private val javaFileWithFromCall = """                           
+package com.ichi2.anki.lint.rules;                             
+                                                               
+import java.util.GregorianCalendar;                            
+                                                               
+public class TestJavaClass {                                   
+                                                               
+    public static void main(String[] args) {                   
+        GregorianCalendar gc = GregorianCalendar.from(null);   
+    }                                                          
+}                                                              
+"""
 
+    @Language("JAVA")
+    private val javaFileWithConstructorInvocation = """              
+package com.ichi2.anki.lint.rules;                             
+                                                               
+import java.util.GregorianCalendar;                            
+                                                               
+public class TestJavaClass {                                   
+                                                               
+    public static void main(String[] args) {                   
+        GregorianCalendar gc = new GregorianCalendar();        
+    }                                                          
+}                                                              
+"""
+
+    @Language("JAVA")
+    private val javaFileWithTime = """                               
+package com.ichi2.anki.lint.rules;                             
+                                                               
+import java.util.GregorianCalendar;                            
+                                                               
+public class Time {                                            
+                                                               
+    public static void main(String[] args) {                   
+        GregorianCalendar gc = new GregorianCalendar();        
+    }                                                          
+}                                                              
+"""
 
     @Test
-    public void showsErrorForInvalidUsage() {
-        lint().
-                allowMissingSdk().
-                allowCompilationErrors()
-                .files(create(stubZoned), create(stubGregorian), create(javaFileWithFromCall))
-                .issues(DirectGregorianInstantiation.ISSUE)
-                .run()
-                .expectErrorCount(1)
-                .check(output -> {
-                    assertTrue(output.contains(DirectGregorianInstantiation.ID));
-                    assertTrue(output.contains(DirectGregorianInstantiation.DESCRIPTION));
-                });
-
-        lint().
-                allowMissingSdk().
-                allowCompilationErrors()
-                .files(create(stubZoned), create(stubGregorian), create(javaFileWithConstructorInvocation))
-                .issues(DirectGregorianInstantiation.ISSUE)
-                .run()
-                .expectErrorCount(1)
-                .check(output -> {
-                    assertTrue(output.contains(DirectGregorianInstantiation.ID));
-                    assertTrue(output.contains(DirectGregorianInstantiation.DESCRIPTION));
-                });
+    fun showsErrorForInvalidUsage() {
+        TestLintTask.lint().allowMissingSdk().allowCompilationErrors()
+            .files(
+                JavaTestFile.create(stubZoned),
+                JavaTestFile.create(stubGregorian),
+                JavaTestFile.create(javaFileWithFromCall)
+            )
+            .issues(DirectGregorianInstantiation.ISSUE)
+            .run()
+            .expectErrorCount(1)
+            .check({ output: String ->
+                Assert.assertTrue(output.contains(DirectGregorianInstantiation.ID))
+                Assert.assertTrue(output.contains(DirectGregorianInstantiation.DESCRIPTION))
+            })
+        TestLintTask.lint().allowMissingSdk().allowCompilationErrors()
+            .files(
+                JavaTestFile.create(stubZoned),
+                JavaTestFile.create(stubGregorian),
+                JavaTestFile.create(javaFileWithConstructorInvocation)
+            )
+            .issues(DirectGregorianInstantiation.ISSUE)
+            .run()
+            .expectErrorCount(1)
+            .check({ output: String ->
+                Assert.assertTrue(output.contains(DirectGregorianInstantiation.ID))
+                Assert.assertTrue(output.contains(DirectGregorianInstantiation.DESCRIPTION))
+            })
     }
-
 
     @Test
-    public void doesNotShowErrorsWhenUsedInTime() {
-        lint().
-                allowMissingSdk()
-                .allowCompilationErrors()
-                .files(create(stubGregorian), create(javaFileWithTime))
-                .issues(DirectGregorianInstantiation.ISSUE)
-                .run()
-                .expectClean();
+    fun doesNotShowErrorsWhenUsedInTime() {
+        TestLintTask.lint().allowMissingSdk()
+            .allowCompilationErrors()
+            .files(JavaTestFile.create(stubGregorian), JavaTestFile.create(javaFileWithTime))
+            .issues(DirectGregorianInstantiation.ISSUE)
+            .run()
+            .expectClean()
     }
-
 }
