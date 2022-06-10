3ccd31f2fd073bfba8734414895ba71188e66a1c
Prince kushwaha <kushwahaprince418@gmail.com>
2022-06-10 21:39:59 +0530

    Migrate FloatMatcher.java to Kotlin

--- AnkiDroid/src/test/java/com/wildplot/android/rendering/FloatMatcher.kt
+++ AnkiDroid/src/test/java/com/wildplot/android/rendering/FloatMatcher.kt
@@ -1,24 +1,18 @@
-//noinspection MissingCopyrightHeader #8659
+package com.wildplot.android.rendering
 
-package com.wildplot.android.rendering;
+import org.mockito.ArgumentMatcher
 
-import org.mockito.ArgumentMatcher;
-
-class FloatMatcher implements ArgumentMatcher<Float> {
-    private final double mExpected;
-    private final double mPrecision;
-
-    private FloatMatcher(double expectedValue, double precision) {
-        mExpected = expectedValue;
-        mPrecision = precision;
+internal class FloatMatcher private constructor(
+    private val mExpected: Double,
+    private val mPrecision: Double
+) : ArgumentMatcher<Float> {
+    override fun matches(actualValue: Float): Boolean {
+        return Math.abs(actualValue.toDouble() - mExpected) <= mPrecision
     }
 
-    static FloatMatcher closeTo(double expectedValue, double precision) {
-        return new FloatMatcher(expectedValue, precision);
-    }
-
-    @Override
-    public boolean matches(Float actualValue) {
-        return Math.abs(((double) actualValue) - mExpected) <= mPrecision;
+    companion object {
+        fun closeTo(expectedValue: Double, precision: Double): FloatMatcher {
+            return FloatMatcher(expectedValue, precision)
+        }
     }
 }
