3c38acc41e42a705b65f48ad42d0cd5a1e093dc4
David Allison <62114487+david-allison@users.noreply.github.com>
2022-06-15 20:02:43 +0100

    refactor: Convert DirectSystemTimeInstantiationTest to Kotlin
    com.ichi2.anki.lint.rules.DirectSystemTimeInstantiationTest

--- lint-rules/src/test/java/com/ichi2/anki/lint/rules/DirectSystemTimeInstantiationTest.kt
+++ lint-rules/src/test/java/com/ichi2/anki/lint/rules/DirectSystemTimeInstantiationTest.kt
@@ -1,104 +1,121 @@
-package com.ichi2.anki.lint.rules;
+package com.ichi2.anki.lint.rules
 
-import org.intellij.lang.annotations.Language;
-import org.junit.Test;
+import com.android.tools.lint.checks.infrastructure.TestFile.JavaTestFile.*
+import com.android.tools.lint.checks.infrastructure.TestLintTask.*
+import org.intellij.lang.annotations.Language
+import org.junit.Assert.*
+import org.junit.Test
 
-import static com.android.tools.lint.checks.infrastructure.TestFile.JavaTestFile.create;
-import static com.android.tools.lint.checks.infrastructure.TestLintTask.lint;
-import static org.junit.Assert.assertTrue;
+class DirectSystemTimeInstantiationTest {
+    @Language("JAVA")
+    private val stubTime = """                         
+package com.ichi2.libanki.utils;                 
+                                                 
+public abstract class Time {                     
+                                                 
+}                                                
+"""
 
-public class DirectSystemTimeInstantiationTest {
     @Language("JAVA")
-    private final String stubTime = "                         \n" +
-            "package com.ichi2.libanki.utils;                 \n" +
-            "                                                 \n" +
-            "public abstract class Time {                     \n" +
-            "                                                 \n" +
-            "}                                                \n";
-    @Language("JAVA")
-    private final String stubSystemTime = "                   \n" +
-            "package com.ichi2.libanki.utils;                 \n" +
-            "                                                 \n" +
-            "public class SystemTime extends Time {           \n" +
-            "                                                 \n" +
-            "    public SystemTime() {                        \n" +
-            "    }                                            \n" +
-            "                                                 \n" +
-            "}                                                \n";
-    @Language("JAVA")
-    private final String javaFileToBeTested = "               \n" +
-            "package com.ichi2.anki.lint.rules;               \n" +
-            "                                                 \n" +
-            "import com.ichi2.libanki.utils.SystemTime;       \n" +
-            "                                                 \n" +
-            "public class TestJavaClass {                     \n" +
-            "                                                 \n" +
-            "    public static void main(String[] args) {     \n" +
-            "        SystemTime st = new SystemTime();        \n" +
-            "    }                                            \n" +
-            "}                                                \n";
-    @Language("JAVA")
-    private final String javaFileWithStorage = "              \n" +
-            "package com.ichi2.anki.lint.rules;               \n" +
-            "                                                 \n" +
-            "import com.ichi2.libanki.utils.SystemTime;       \n" +
-            "                                                 \n" +
-            "public class Storage {                           \n" +
-            "                                                 \n" +
-            "    public static void main(String[] args) {     \n" +
-            "        SystemTime st = new SystemTime();        \n" +
-            "    }                                            \n" +
-            "}                                                \n";
-    @Language("JAVA")
-    private final String javaFileWithCollectionHelper = "     \n" +
-            "package com.ichi2.anki.lint.rules;               \n" +
-            "                                                 \n" +
-            "import com.ichi2.libanki.utils.SystemTime;       \n" +
-            "                                                 \n" +
-            "public class CollectionHelper {                  \n" +
-            "                                                 \n" +
-            "    public static void main(String[] args) {     \n" +
-            "        SystemTime st = new SystemTime();        \n" +
-            "    }                                            \n" +
-            "}                                                \n";
+    private val stubSystemTime = """                   
+package com.ichi2.libanki.utils;                 
+                                                 
+public class SystemTime extends Time {           
+                                                 
+    public SystemTime() {                        
+    }                                            
+                                                 
+}                                                
+"""
 
+    @Language("JAVA")
+    private val javaFileToBeTested = """               
+package com.ichi2.anki.lint.rules;               
+                                                 
+import com.ichi2.libanki.utils.SystemTime;       
+                                                 
+public class TestJavaClass {                     
+                                                 
+    public static void main(String[] args) {     
+        SystemTime st = new SystemTime();        
+    }                                            
+}                                                
+"""
+
+    @Language("JAVA")
+    private val javaFileWithStorage = """              
+package com.ichi2.anki.lint.rules;               
+                                                 
+import com.ichi2.libanki.utils.SystemTime;       
+                                                 
+public class Storage {                           
+                                                 
+    public static void main(String[] args) {     
+        SystemTime st = new SystemTime();        
+    }                                            
+}                                                
+"""
+
+    @Language("JAVA")
+    private val javaFileWithCollectionHelper = """     
+package com.ichi2.anki.lint.rules;               
+                                                 
+import com.ichi2.libanki.utils.SystemTime;       
+                                                 
+public class CollectionHelper {                  
+                                                 
+    public static void main(String[] args) {     
+        SystemTime st = new SystemTime();        
+    }                                            
+}                                                
+"""
 
     @Test
-    public void showsErrorsForInvalidUsage() {
+    fun showsErrorsForInvalidUsage() {
         lint()
-                .allowMissingSdk()
-                .allowCompilationErrors()
-                .files(create(stubTime), create(stubSystemTime), create(javaFileToBeTested))
-                .issues(DirectSystemTimeInstantiation.ISSUE)
-                .run()
-                .expectErrorCount(1)
-                .check(output -> {
-                    assertTrue(output.contains(DirectSystemTimeInstantiation.ID));
-                    assertTrue(output.contains(DirectSystemTimeInstantiation.DESCRIPTION));
-                });
+            .allowMissingSdk()
+            .allowCompilationErrors()
+            .files(
+                create(stubTime),
+                create(stubSystemTime),
+                create(javaFileToBeTested)
+            )
+            .issues(DirectSystemTimeInstantiation.ISSUE)
+            .run()
+            .expectErrorCount(1)
+            .check({ output: String ->
+                assertTrue(output.contains(DirectSystemTimeInstantiation.ID))
+                assertTrue(output.contains(DirectSystemTimeInstantiation.DESCRIPTION))
+            })
     }
 
-
     @Test
-    public void doesNotShowErrorsWhenUsedInStorage() {
-        lint().
-                allowMissingSdk()
-                .allowCompilationErrors()
-                .files(create(stubTime), create(stubSystemTime), create(javaFileWithStorage))
-                .issues(DirectSystemTimeInstantiation.ISSUE)
-                .run()
-                .expectClean();
+    fun doesNotShowErrorsWhenUsedInStorage() {
+        lint()
+            .allowMissingSdk()
+            .allowCompilationErrors()
+            .files(
+                create(stubTime),
+                create(stubSystemTime),
+                create(javaFileWithStorage)
+            )
+            .issues(DirectSystemTimeInstantiation.ISSUE)
+            .run()
+            .expectClean()
     }
 
-
     @Test
-    public void doesNotShowErrorsWhenUsedInCollectionHelper() {
-        lint().
-                allowMissingSdk()
-                .allowCompilationErrors()
-                .files(create(stubTime), create(stubSystemTime), create(javaFileWithCollectionHelper))
-                .issues(DirectSystemTimeInstantiation.ISSUE)
-                .run()
-                .expectClean();
+    fun doesNotShowErrorsWhenUsedInCollectionHelper() {
+        lint()
+            .allowMissingSdk()
+            .allowCompilationErrors()
+            .files(
+                create(stubTime),
+                create(stubSystemTime),
+                create(javaFileWithCollectionHelper)
+            )
+            .issues(DirectSystemTimeInstantiation.ISSUE)
+            .run()
+            .expectClean()
     }
 }
