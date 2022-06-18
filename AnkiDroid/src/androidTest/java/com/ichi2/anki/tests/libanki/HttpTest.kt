9166c35eabbf1fc16388eb512374551cabbd7174
Prince kushwaha <kushwahaprince418@gmail.com>
2022-06-18 11:58:57 +0530

    Migrate HttpTest.java to HttpTest.kt

--- AnkiDroid/src/androidTest/java/com/ichi2/anki/tests/libanki/HttpTest.kt
+++ AnkiDroid/src/androidTest/java/com/ichi2/anki/tests/libanki/HttpTest.kt
@@ -15,115 +15,112 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-package com.ichi2.anki.tests.libanki;
+package com.ichi2.anki.tests.libanki
 
-import android.Manifest;
-import android.os.Build;
+import android.Manifest
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.GrantPermissionRule
+import com.ichi2.async.Connection
+import com.ichi2.libanki.sync.HostNum
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import com.ichi2.async.Connection;
-import com.ichi2.compat.CompatHelper;
-import com.ichi2.libanki.sync.HostNum;
+@RunWith(AndroidJUnit4::class)
+class HttpTest {
 
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+    @get:Rule
+    var runtimeStoragePermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.INTERNET,
+        Manifest.permission.ACCESS_NETWORK_STATE
+    )
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.rule.GrantPermissionRule;
-
-@RunWith(AndroidJUnit4.class)
-public class HttpTest {
-
-    @Rule
-    public GrantPermissionRule mRuntimeStoragePermissionRule = GrantPermissionRule.grant(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.INTERNET,
-            Manifest.permission.ACCESS_NETWORK_STATE);
-
-
+    // #7108: AsyncTask
+    @Suppress("DEPRECATION")
     @Test
-    @SuppressWarnings("deprecation") // #7108: AsyncTask
-    public void testLogin() {
+    fun testLogin() {
 
-        String username = "AnkiDroidInstrumentedTestUser";
-        String password = "AnkiDroidInstrumentedTestInvalidPass";
-        Connection.Payload invalidPayload = new Connection.Payload(new Object[]{username, password, new HostNum(null)});
-        TestTaskListener testListener = new TestTaskListener(invalidPayload);
+        val username = "AnkiDroidInstrumentedTestUser"
+        val password = "AnkiDroidInstrumentedTestInvalidPass"
+        val invalidPayload = Connection.Payload(arrayOf(username, password, HostNum(null)))
+        val testListener = TestTaskListener(invalidPayload)
 
         // We have to carefully run things on the main thread here or the threading protections in BaseAsyncTask throw
         // The first one is just to run the static initializer, really
-        Runnable onlineRunnable = () -> {
+        val onlineRunnable = Runnable {
             try {
-                Class.forName("com.ichi2.async.Connection");
-            } catch (Exception e) {
-                Assert.fail("Unable to load Connection class: " + e.getMessage());
+                Class.forName("com.ichi2.async.Connection")
+            } catch (e: Exception) {
+                Assert.fail("Unable to load Connection class: " + e.message)
             }
-        };
-        InstrumentationRegistry.getInstrumentation().runOnMainSync(onlineRunnable);
+        }
+        InstrumentationRegistry.getInstrumentation().runOnMainSync(onlineRunnable)
 
         // If we are not online this test is not nearly as interesting
         // TODO simulate offline programmatically - currently exercised by manually toggling an emulator offline pre-test
-        if (!Connection.Companion.isOnline()) {
-            Connection.login(testListener, invalidPayload);
-            Assert.assertFalse("Successful login despite being offline", testListener.getPayload().success);
-            Assert.assertTrue("onDisconnected not called despite being offline", testListener.mDisconnectedCalled);
-            return;
+        if (!Connection.Companion.isOnline) {
+            Connection.login(testListener, invalidPayload)
+            Assert.assertFalse(
+                "Successful login despite being offline",
+                testListener.getPayload()!!.success
+            )
+            Assert.assertTrue(
+                "onDisconnected not called despite being offline",
+                testListener.disconnectedCalled
+            )
+            return
         }
 
-        Runnable r = () -> {
-            Connection conn = Connection.login(testListener, invalidPayload);
+        val r = Runnable {
+            val conn = Connection.login(testListener, invalidPayload)
             try {
                 // This forces us to synchronously wait for the AsyncTask to do it's work
-                conn.get();
-            } catch (Exception e) {
-                Assert.fail("Caught exception while trying to login: " + e.getMessage());
+                conn!!.get()
+            } catch (e: Exception) {
+                Assert.fail("Caught exception while trying to login: " + e.message)
             }
-        };
-        InstrumentationRegistry.getInstrumentation().runOnMainSync(r);
-        Assert.assertFalse("Successful login despite invalid credentials", testListener.getPayload().success);
+        }
+        InstrumentationRegistry.getInstrumentation().runOnMainSync(r)
+        Assert.assertFalse(
+            "Successful login despite invalid credentials",
+            testListener.getPayload()!!.success
+        )
     }
 
+    class TestTaskListener(payload: Connection.Payload) :
+        Connection.TaskListener {
 
-    public static class TestTaskListener implements Connection.TaskListener {
-
-        private Connection.Payload mPayload;
-        private boolean mDisconnectedCalled = false;
-
-        private Connection.Payload getPayload() {
-            return mPayload;
+        private var payload: Connection.Payload? = null
+        var disconnectedCalled = false
+        private fun setPayload(payload: Connection.Payload) {
+            this.payload = payload
         }
 
-        private void setPayload(Connection.Payload payload) {
-            mPayload = payload;
-        }
-
-        private TestTaskListener(Connection.Payload payload) {
-            setPayload(payload);
-        }
-
-        @Override
-        public void onPreExecute() {
+        override fun onPreExecute() {
             // do nothing
         }
 
-
-        @Override
-        public void onProgressUpdate(Object... values) {
+        override fun onProgressUpdate(vararg values: Any?) {
             // do nothing
         }
 
+        fun getPayload(): Connection.Payload? {
+            return payload
+        }
 
-        @Override
-        public void onPostExecute(Connection.Payload data) {
+        override fun onPostExecute(data: Connection.Payload) {
             // do nothing
         }
 
+        override fun onDisconnected() {
+            disconnectedCalled = true
+        }
 
-        @Override
-        public void onDisconnected() {
-            mDisconnectedCalled = true;
+        init {
+            setPayload(payload)
         }
     }
 }
