eed19989f140af413ce901c52a8e99731a097042
David Allison <62114487+david-allison@users.noreply.github.com>
2022-06-28 04:01:01 +0100

    refactor: convert DuplicateCrowdInStringsTest to Kotlin
    com.ichi2.anki.lint.rules.DuplicateCrowdInStringsTest

--- lint-rules/src/test/java/com/ichi2/anki/lint/rules/DuplicateCrowdInStringsTest.kt
+++ lint-rules/src/test/java/com/ichi2/anki/lint/rules/DuplicateCrowdInStringsTest.kt
@@ -13,120 +13,121 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.anki.lint.rules
 
-package com.ichi2.anki.lint.rules;
+import com.android.tools.lint.checks.infrastructure.TestFiles.*
+import com.android.tools.lint.checks.infrastructure.TestLintTask.*
+import org.intellij.lang.annotations.Language
+import org.junit.Test
 
-import org.intellij.lang.annotations.Language;
-import org.junit.Test;
-
-import static com.android.tools.lint.checks.infrastructure.TestLintTask.lint;
-import static com.android.tools.lint.checks.infrastructure.TestFiles.xml;
-
-public class DuplicateCrowdInStringsTest {
-
-    /** Easiest test case: Two exact duplicates in the same file */
+class DuplicateCrowdInStringsTest {
+    /** Easiest test case: Two exact duplicates in the same file  */
     @Language("XML")
-    private final String mSelfInvalid = "<resources>\n" +
-            "   <string name=\"hello\">a</string>\n" +
-            "   <string name=\"hello2\">a</string>\n" +
-            "</resources>";
+    private val mSelfInvalid = """<resources>
+   <string name="hello">a</string>
+   <string name="hello2">a</string>
+</resources>"""
 
     @Language("XML")
-    private final String mFirstCommentButInvalid = "<resources>\n" +
-            "   <string name=\"hello\" comment=\"hello\">a</string>\n" +
-            "   <string name=\"hello2\">a</string>\n" +
-            "</resources>";
+    private val mFirstCommentButInvalid = """<resources>
+   <string name="hello" comment="hello">a</string>
+   <string name="hello2">a</string>
+</resources>"""
 
     @Language("XML")
-    private final String mSecondCommentButInvalid = "<resources>\n" +
-            "   <string name=\"hello\">a</string>\n" +
-            "   <string name=\"hello2\" comment=\"hello\">a</string>\n" +
-            "</resources>";
+    private val mSecondCommentButInvalid = """<resources>
+   <string name="hello">a</string>
+   <string name="hello2" comment="hello">a</string>
+</resources>"""
 
     @Language("XML")
-    private final String mDuplicateBothValid = "<resources>\n" +
-            "   <string name=\"hello\" comment=\"hello\">a</string>\n" +
-            "   <string name=\"hello2\" comment=\"hello\">a</string>\n" +
-            "</resources>";
+    private val mDuplicateBothValid = """<resources>
+   <string name="hello" comment="hello">a</string>
+   <string name="hello2" comment="hello">a</string>
+</resources>"""
 
     @Language("XML")
-    private final String mIgnoredFile = "<resources xmlns:tools=\"http://schemas.android.com/tools\" tools:ignore=\"AAA,DuplicateCrowdInStrings\">\n" +
-            "   <string name=\"hello\">a</string>\n" +
-            "   <string name=\"hello2\">a</string>\n" +
-            "</resources>";
+    private val mIgnoredFile =
+        """<resources xmlns:tools="http://schemas.android.com/tools" tools:ignore="AAA,DuplicateCrowdInStrings">
+   <string name="hello">a</string>
+   <string name="hello2">a</string>
+</resources>"""
 
     @Language("XML")
-    private final String mNotIgnored = "<resources>\n" +
-            "   <string name=\"hello3\">a</string>\n" +
-            "</resources>";
+    private val mNotIgnored = """<resources>
+   <string name="hello3">a</string>
+</resources>"""
 
     @Test
-    public void duplicateStringsInSameFileDetected() {
+    fun duplicateStringsInSameFileDetected() {
         // This appears to be a bug in StringCasingDetector - string is self-referential.
         lint()
-        .allowMissingSdk()
-        .allowCompilationErrors()
-        .files(xml("res/values/string.xml", mSelfInvalid))
-        .issues(DuplicateCrowdInStrings.ISSUE)
-        .run()
-        .expectErrorCount(1);
+            .allowMissingSdk()
+            .allowCompilationErrors()
+            .files(xml("res/values/string.xml", mSelfInvalid))
+            .issues(DuplicateCrowdInStrings.ISSUE)
+            .run()
+            .expectErrorCount(1)
     }
 
     @Test
-    public void invalidStringInNonEnglishPasses() {
+    fun invalidStringInNonEnglishPasses() {
         // We only want to check the base resource files, not the translated ones -
         // translators know if values are equivalent and do not require a comment explaining why.
         lint()
-        .allowMissingSdk()
-        .allowCompilationErrors()
-        .files(xml("res/values-af/string.xml", mSelfInvalid))
-        .issues(DuplicateCrowdInStrings.ISSUE)
-        .run()
-        .expectErrorCount(0);
+            .allowMissingSdk()
+            .allowCompilationErrors()
+            .files(xml("res/values-af/string.xml", mSelfInvalid))
+            .issues(DuplicateCrowdInStrings.ISSUE)
+            .run()
+            .expectErrorCount(0)
     }
 
     @Test
-    public void duplicateStringWithFirstCommentFails() {
+    fun duplicateStringWithFirstCommentFails() {
         lint()
-        .allowMissingSdk()
-        .allowCompilationErrors()
-        .files(xml("res/values/string.xml", mFirstCommentButInvalid))
-        .issues(DuplicateCrowdInStrings.ISSUE)
-        .run()
-        .expectErrorCount(1);
+            .allowMissingSdk()
+            .allowCompilationErrors()
+            .files(xml("res/values/string.xml", mFirstCommentButInvalid))
+            .issues(DuplicateCrowdInStrings.ISSUE)
+            .run()
+            .expectErrorCount(1)
     }
 
     @Test
-    public void duplicateStringWithSecondCommentFails() {
+    fun duplicateStringWithSecondCommentFails() {
         lint()
-        .allowMissingSdk()
-        .allowCompilationErrors()
-        .files(xml("res/values/string.xml", mSecondCommentButInvalid))
-        .issues(DuplicateCrowdInStrings.ISSUE)
-        .run()
-        .expectErrorCount(1);
+            .allowMissingSdk()
+            .allowCompilationErrors()
+            .files(xml("res/values/string.xml", mSecondCommentButInvalid))
+            .issues(DuplicateCrowdInStrings.ISSUE)
+            .run()
+            .expectErrorCount(1)
     }
 
     @Test
-    public void duplicateStringWithBothCommentsPasses() {
+    fun duplicateStringWithBothCommentsPasses() {
         lint()
-        .allowMissingSdk()
-        .allowCompilationErrors()
-        .files(xml("res/values/string.xml", mDuplicateBothValid))
-        .issues(DuplicateCrowdInStrings.ISSUE)
-        .run()
-        .expectErrorCount(0);
+            .allowMissingSdk()
+            .allowCompilationErrors()
+            .files(xml("res/values/string.xml", mDuplicateBothValid))
+            .issues(DuplicateCrowdInStrings.ISSUE)
+            .run()
+            .expectErrorCount(0)
     }
 
     @Test
-    public void ignoredFilePasses() {
+    fun ignoredFilePasses() {
         // TODO: this doesn't work over two files. If the tools:ignore is removed, only the single file fails
         lint()
-        .allowMissingSdk()
-        .allowCompilationErrors()
-        .files(xml("res/values/constants.xml", mIgnoredFile), xml("res/values/strings.xml", mNotIgnored))
-        .issues(DuplicateCrowdInStrings.ISSUE)
-        .run()
-        .expectErrorCount(0);
+            .allowMissingSdk()
+            .allowCompilationErrors()
+            .files(
+                xml("res/values/constants.xml", mIgnoredFile),
+                xml("res/values/strings.xml", mNotIgnored)
+            )
+            .issues(DuplicateCrowdInStrings.ISSUE)
+            .run()
+            .expectErrorCount(0)
     }
 }
