e61cc137647b33ec2ebb53fdd6dd4b9d2972a092
Mike Hardy <github@mikehardy.net>
2022-07-05 09:54:46 -0700

    test(ci): ignore a flaky test in CI

--- AnkiDroid/src/test/java/com/ichi2/anki/servicemodel/UpgradeGesturesToControlsTestNoParam.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/servicemodel/UpgradeGesturesToControlsTestNoParam.kt
@@ -25,6 +25,7 @@ import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import timber.log.Timber
@@ -44,6 +45,7 @@ class UpgradeGesturesToControlsTestNoParam : RobolectricTest() {
     }
 
     @Test
+    @Ignore("flaky in CI")
     fun test_preferences_not_opened_happy_path() {
         // if the user has not opened the gestures, then nothing should be mapped
         assertThat(prefs.contains(PREF_KEY_VOLUME_DOWN), equalTo(false))
