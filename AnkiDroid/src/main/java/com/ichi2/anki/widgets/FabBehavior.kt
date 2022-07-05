c43e32b9bb82e204ac058fe4e2af502a4aef5483
oakkitten <oakkitten@users.noreply.github.com>
2022-07-06 00:34:48 +0100

    Adjust fab behavior to work with material snackbar
    Previously, snackbars were sliding from the bottom. Material snackbars
    instead fade in, and slightly scale out. These snackbars are meant to be
    placed above the fab, but our fab opens a menu, and what are you going
    to do with that, huh?
    
    This makes fab slide up with some deceleration when a snackbar starts
    appearing, and down after it is removed. This plays well with fab's
    rotating animation.

--- AnkiDroid/src/main/java/com/ichi2/anki/widgets/FabBehavior.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/widgets/FabBehavior.kt
@@ -18,9 +18,9 @@ package com.ichi2.anki.widgets
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.DecelerateInterpolator
 import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.core.view.ViewCompat
-import androidx.core.view.isVisible
+import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar.SnackbarLayout
 import com.ichi2.anki.AnkiDroidApp
 import com.ichi2.anki.UIUtils
@@ -30,40 +30,45 @@ import com.ichi2.anki.UIUtils
  * Defines the behavior for the floating action button. If the dependency is a Snackbar, move the
  * fab up.
  */
-class FabBehavior : CoordinatorLayout.Behavior<View> {
-    private var mTranslationY = 0f
-
-    constructor() : super() {}
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
+@Suppress("UNUSED")
+class FabBehavior @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
+    CoordinatorLayout.Behavior<View>(context, attrs) {
 
     override fun layoutDependsOn(parent: CoordinatorLayout, child: View, dependency: View): Boolean {
         return dependency is SnackbarLayout
     }
 
-    override fun onDependentViewChanged(parent: CoordinatorLayout, fab: View, dependency: View): Boolean {
-        if (dependency is SnackbarLayout && fab.visibility == View.VISIBLE) {
-            val translationY = getFabTranslationYForSnackbar(parent, fab)
-            if (translationY != mTranslationY) {
-                ViewCompat.animate(fab).cancel()
-                fab.translationY = translationY
-                mTranslationY = translationY
-            }
-        }
+    override fun onDependentViewChanged(parent: CoordinatorLayout, fab: View, snackbar: View): Boolean {
+        val fabTranslationY = calculateFabTranslationYForSnackbar(fab, snackbar)
+        translateFabToYWithAnimation(fab, fabTranslationY, SNACKBAR_FADE_IN_DURATION)
         return false
     }
 
-    override fun onDependentViewRemoved(parent: CoordinatorLayout, fab: View, dependency: View) {
-        super.onDependentViewRemoved(parent, fab, dependency)
-        if (dependency is SnackbarLayout && fab.visibility == View.VISIBLE) {
-            val translationY = getFabTranslationYForSnackbar(parent, fab)
-            if (translationY != mTranslationY) {
-                ViewCompat.animate(fab).cancel()
-                fab.translationY = 0f
-                mTranslationY = 0f
-            }
+    override fun onDependentViewRemoved(parent: CoordinatorLayout, fab: View, snackbar: View) {
+        translateFabToYWithAnimation(fab, 0f, SNACKBAR_FADE_OUT_DURATION)
+    }
+
+    private var lastFabTranslationY = 0f
+
+    private fun translateFabToYWithAnimation(fab: View, fabTranslationY: Float, duration: Long) {
+        if (lastFabTranslationY != fabTranslationY) {
+            lastFabTranslationY = fabTranslationY
+            fab.animate()
+                .setInterpolator(DecelerateInterpolator(2f))
+                .translationY(fabTranslationY)
+                .setDuration(duration)
+                .start()
         }
     }
 }
+
+/**
+ * These are the same as:
+ *   * [BaseTransientBottomBar.ANIMATION_FADE_IN_DURATION]
+ *   * [BaseTransientBottomBar.ANIMATION_FADE_OUT_DURATION]
+ */
+private const val SNACKBAR_FADE_IN_DURATION = 150L
+private const val SNACKBAR_FADE_OUT_DURATION = 75L
 
 /**
  * Here, fab is the entire layout containing the button itself, related menu and the margin.
@@ -72,14 +77,10 @@ class FabBehavior : CoordinatorLayout.Behavior<View> {
  */
 private val MAX_OVERLAP = UIUtils.convertDpToPixel(10f, AnkiDroidApp.getInstance().applicationContext)
 
-private fun getFabTranslationYForSnackbar(parent: CoordinatorLayout, fab: View): Float {
-    return parent.getDependencies(fab)
-        .filter { view -> view is SnackbarLayout && view.isVisible }
-        .minOfOrNull { snackbar ->
-            val untranslatedFabBottom = fab.bottom
-            val effectiveSnackbarTop = snackbar.top + snackbar.translationY
-            val overlap = untranslatedFabBottom - effectiveSnackbarTop
+private fun calculateFabTranslationYForSnackbar(fab: View, snackbar: View): Float {
+    val untranslatedFabBottom = fab.bottom
+    val effectiveSnackbarTop = snackbar.top + snackbar.translationY
+    val overlap = untranslatedFabBottom - effectiveSnackbarTop
 
-            if (overlap > MAX_OVERLAP) -(overlap - MAX_OVERLAP) else 0f
-        } ?: 0f
+    return if (overlap > MAX_OVERLAP) -(overlap - MAX_OVERLAP) else 0f
 }
