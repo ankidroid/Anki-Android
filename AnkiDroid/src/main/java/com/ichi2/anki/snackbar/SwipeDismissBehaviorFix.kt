85ba9177a8b2da6ee00ad0b5307da10b82125aac
oakkitten <oakkitten@users.noreply.github.com>
2022-07-07 16:04:18 +0100

    Fix swiping on snackbar moving other views
    The way SwipeDismissBehavior uses ViewDragHelper, event interception may
    happen well past the initial start of the gesture. ViewDragHelper will
    capture the view under the finger, which at this point might be some
    unrelated view, and *that* view will be moved instead. Looks very funny.
    
    This solves the issue by never intercepting touch events unless the
    finger is over the snackbar.

--- AnkiDroid/src/main/java/com/ichi2/anki/snackbar/SwipeDismissBehaviorFix.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/snackbar/SwipeDismissBehaviorFix.kt
@@ -43,11 +43,23 @@ import com.google.android.material.snackbar.Snackbar
  *
  * This fix solves the issue in a very simple way, by ignoring calls to [onTouchEvent]
  * during the call to [onInterceptTouchEvent].
+ *
+ * Another problem with SwipeDismissBehavior is that the way it uses ViewDragHelper,
+ * event interception may happen well past the initial start of the gesture.
+ * ViewDragHelper will capture the view under the finger,
+ * which at this point might be some unrelated view, and *that* view will be moved instead.
+ *
+ * This solves the issue by never intercepting touch events unless the finger is over the snackbar.
+ * This does prevent the user from moving snackbar if they put a finger on it,
+ * move it directly upwards, and then to the side.
+ * However, this inconvenience is rather minor and fixing it properly might require too much effort.
  */
 class SwipeDismissBehaviorFix<V : View> : SwipeDismissBehavior<V>() {
     private var ignoreCallsToOnTouchEvent = false
 
     override fun onInterceptTouchEvent(parent: CoordinatorLayout, child: V, event: MotionEvent): Boolean {
+        if (!parent.isPointInChildBounds(child, event.x.toInt(), event.y.toInt())) return false
+
         ignoreCallsToOnTouchEvent = true
         return super.onInterceptTouchEvent(parent, child, event).also {
             ignoreCallsToOnTouchEvent = false
