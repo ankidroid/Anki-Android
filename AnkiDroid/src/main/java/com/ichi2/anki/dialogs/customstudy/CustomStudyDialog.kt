d526e58ddb936b277066dfb3321324925cb39b1e
Mohd Raghib <40427461+oyeraghib@users.noreply.github.com>
2022-07-09 11:59:43 +0530

    cleanup in CustomStudyDialog (#11793)
    * cleanup: scope function for args
    add .apply{} scope function for `args` variable in `withArguments()` function
    * cleanup: scope function for dialogOptions
    add .apply{} scope function for `dialogOptions` variable in `getListIds()` function
    * cleanup: IDE lint

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/customstudy/CustomStudyDialog.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/customstudy/CustomStudyDialog.kt
@@ -71,9 +71,11 @@ class CustomStudyDialog(private val collection: Collection, private val customSt
         if (args == null) {
             args = Bundle()
         }
-        args.putInt("id", contextMenuAttribute.value)
-        args.putLong("did", did)
-        args.putBoolean("jumpToReviewer", jumpToReviewer)
+        args.apply {
+            putInt("id", contextMenuAttribute.value)
+            putLong("did", did)
+            putBoolean("jumpToReviewer", jumpToReviewer)
+        }
         this.arguments = args
         return this
     }
@@ -343,14 +345,15 @@ class CustomStudyDialog(private val collection: Collection, private val customSt
         when (dialogId) {
             STANDARD -> {
                 // Standard context menu
-                val dialogOptions = mutableListOf<ContextMenuOption>()
-                dialogOptions.add(STUDY_NEW)
-                dialogOptions.add(STUDY_REV)
-                dialogOptions.add(STUDY_FORGOT)
-                dialogOptions.add(STUDY_AHEAD)
-                dialogOptions.add(STUDY_RANDOM)
-                dialogOptions.add(STUDY_PREVIEW)
-                dialogOptions.add(STUDY_TAGS)
+                val dialogOptions = mutableListOf<ContextMenuOption>().apply {
+                    add(STUDY_NEW)
+                    add(STUDY_REV)
+                    add(STUDY_FORGOT)
+                    add(STUDY_AHEAD)
+                    add(STUDY_RANDOM)
+                    add(STUDY_PREVIEW)
+                    add(STUDY_TAGS)
+                }
                 if (collection.sched.totalNewForCurrentDeck() == 0) {
                     // If no new cards we wont show CUSTOM_STUDY_NEW
                     dialogOptions.remove(STUDY_NEW)
@@ -358,7 +361,7 @@ class CustomStudyDialog(private val collection: Collection, private val customSt
                 return dialogOptions.toList()
             }
             LIMITS -> // Special custom study options to show when the daily study limit has been reached
-                return if (collection.sched.newDue() != true && collection.sched.revDue() != true) {
+                return if (!collection.sched.newDue() && !collection.sched.revDue()) {
                     listOf(STUDY_NEW, STUDY_REV, DECK_OPTIONS, MORE_OPTIONS)
                 } else {
                     if (collection.sched.newDue()) {
