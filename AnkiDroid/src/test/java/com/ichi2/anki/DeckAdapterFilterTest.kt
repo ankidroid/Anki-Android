d63d583187d2470b514145302e37cc2390ed29dd
shaiguelman <shaiguelman@gmail.com>
2022-07-18 01:03:03 -0500

    Remove whitespace

--- AnkiDroid/src/test/java/com/ichi2/anki/DeckAdapterFilterTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/DeckAdapterFilterTest.kt
@@ -13,7 +13,6 @@
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
-
 package com.ichi2.anki
 
 import com.ichi2.anki.widgets.DeckAdapter
