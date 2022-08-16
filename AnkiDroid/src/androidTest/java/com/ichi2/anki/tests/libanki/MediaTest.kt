07b0b779e8dd1c14be6055c12249aaacc5894b19
David Allison <62114487+david-allison@users.noreply.github.com>
2022-08-16 21:49:35 +0100

    refactor: Convert MediaTest to Kotlin
    com.ichi2.anki.tests.libanki.MediaTest

--- AnkiDroid/src/androidTest/java/com/ichi2/anki/tests/libanki/MediaTest.kt
+++ AnkiDroid/src/androidTest/java/com/ichi2/anki/tests/libanki/MediaTest.kt
@@ -13,276 +13,269 @@
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
-package com.ichi2.anki.tests.libanki;
+package com.ichi2.anki.tests.libanki
 
-import android.Manifest;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.GrantPermissionRule;
-
-import com.ichi2.anki.BackupManager;
-import com.ichi2.anki.tests.InstrumentedTest;
-import com.ichi2.libanki.Collection;
-import com.ichi2.libanki.Media;
-import com.ichi2.libanki.Note;
-import com.ichi2.libanki.exception.EmptyMediaException;
-
-import net.ankiweb.rsdroid.BackendFactory;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-import static org.junit.Assume.assumeThat;
-
+import android.Manifest
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.GrantPermissionRule
+import com.ichi2.anki.BackupManager
+import com.ichi2.anki.tests.InstrumentedTest
+import com.ichi2.libanki.Collection
+import com.ichi2.libanki.Media
+import com.ichi2.libanki.exception.EmptyMediaException
+import com.ichi2.utils.KotlinCleanup
+import net.ankiweb.rsdroid.BackendFactory.defaultLegacySchema
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.junit.*
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.*
+import kotlin.test.assertNotNull
 
 /**
- * Unit tests for {@link Media}.
+ * Unit tests for [Media].
  */
-@RunWith(AndroidJUnit4.class)
-public class MediaTest extends InstrumentedTest {
+@RunWith(AndroidJUnit4::class)
+@KotlinCleanup("IDE Lint")
+class MediaTest : InstrumentedTest() {
+    private var mTestCol: Collection? = null
 
-    private Collection mTestCol;
-
-    @Rule
-    public GrantPermissionRule mRuntimePermissionRule =
-            GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    @get:Rule
+    var runtimePermissionRule =
+        GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
     @Before
-    public void setUp() throws IOException {
-        mTestCol = getEmptyCol();
+    @Throws(IOException::class)
+    fun setUp() {
+        mTestCol = emptyCol
     }
 
     @After
-    public void tearDown() {
-        mTestCol.close();
+    fun tearDown() {
+        mTestCol!!.close()
     }
 
     @Test
-    public void testAdd() throws IOException, EmptyMediaException {
+    @Throws(IOException::class, EmptyMediaException::class)
+    fun testAdd() {
         // open new empty collection
-        File dir = getTestDir();
-        BackupManager.removeDir(dir);
-        assertTrue(dir.mkdirs());
-        File path = new File(dir, "foo.jpg");
-        FileOutputStream os = new FileOutputStream(path, false);
-        os.write("hello".getBytes());
-        os.close();
+        val dir = testDir
+        BackupManager.removeDir(dir)
+        assertTrue(dir.mkdirs())
+        val path = File(dir, "foo.jpg")
+        var os = FileOutputStream(path, false)
+        os.write("hello".toByteArray())
+        os.close()
         // new file, should preserve name
-        String r = mTestCol.getMedia().addFile(path);
-        assertEquals("foo.jpg", r);
+        val r = mTestCol!!.media.addFile(path)
+        assertEquals("foo.jpg", r)
         // adding the same file again should not create a duplicate
-        assertEquals("foo.jpg", mTestCol.getMedia().addFile(path));
+        assertEquals("foo.jpg", mTestCol!!.media.addFile(path))
         // but if it has a different md5, it should
-        os = new FileOutputStream(path, false);
-        os.write("world".getBytes());
-        os.close();
-        assertNotEquals("foo.jpg", mTestCol.getMedia().addFile(path));
+        os = FileOutputStream(path, false)
+        os.write("world".toByteArray())
+        os.close()
+        assertNotEquals("foo.jpg", mTestCol!!.media.addFile(path))
     }
 
     @Test
-    public void testAddEmptyFails() throws IOException {
+    @Throws(IOException::class)
+    fun testAddEmptyFails() {
         // open new empty collection
-        File dir = getTestDir();
-        BackupManager.removeDir(dir);
-        assertTrue(dir.mkdirs());
-        File path = new File(dir, "foo.jpg");
-        assertTrue(path.createNewFile());
+        val dir = testDir
+        BackupManager.removeDir(dir)
+        assertTrue(dir.mkdirs())
+        val path = File(dir, "foo.jpg")
+        assertTrue(path.createNewFile())
 
         // new file, should preserve name
         try {
-            mTestCol.getMedia().addFile(path);
-            fail("exception should be thrown");
-        } catch (EmptyMediaException mediaException) {
+            mTestCol!!.media.addFile(path)
+            fail("exception should be thrown")
+        } catch (mediaException: EmptyMediaException) {
             // all good
         }
     }
 
-
     @Test
-    public void testStrings() {
-        Long mid = mTestCol.getModels().getModels().entrySet().iterator().next().getKey();
+    fun testStrings() {
+        val mid = mTestCol!!.models.getModels().entries.iterator().next().key
 
-        List<String> expected = Collections.emptyList();
-        List<String> actual = mTestCol.getMedia().filesInStr(mid, "aoeu");
-        actual.retainAll(expected);
-        assertEquals(expected.size(), actual.size());
+        var expected: List<String?> = emptyList<String>()
+        var actual = mTestCol!!.media.filesInStr(mid, "aoeu")
+        actual.retainAll(expected)
+        assertEquals(expected.size, actual.size)
 
-        expected = Collections.singletonList("foo.jpg");
-        actual = mTestCol.getMedia().filesInStr(mid, "aoeu<img src='foo.jpg'>ao");
-        actual.retainAll(expected);
-        assertEquals(expected.size(), actual.size());
+        expected = listOf("foo.jpg")
+        actual = mTestCol!!.media.filesInStr(mid, "aoeu<img src='foo.jpg'>ao")
+        actual.retainAll(expected)
+        assertEquals(expected.size, actual.size)
 
-        expected = Arrays.asList("foo.jpg", "bar.jpg");
-        actual = mTestCol.getMedia().filesInStr(mid, "aoeu<img src='foo.jpg'><img src=\"bar.jpg\">ao");
-        actual.retainAll(expected);
-        assertEquals(expected.size(), actual.size());
+        expected = Arrays.asList("foo.jpg", "bar.jpg")
+        actual = mTestCol!!.media.filesInStr(mid, "aoeu<img src='foo.jpg'><img src=\"bar.jpg\">ao")
+        actual.retainAll(expected)
+        assertEquals(expected.size, actual.size)
 
-        expected = Collections.singletonList("foo.jpg");
-        actual = mTestCol.getMedia().filesInStr(mid, "aoeu<img src=foo.jpg style=bar>ao");
-        actual.retainAll(expected);
-        assertEquals(expected.size(), actual.size());
+        expected = listOf("foo.jpg")
+        actual = mTestCol!!.media.filesInStr(mid, "aoeu<img src=foo.jpg style=bar>ao")
+        actual.retainAll(expected)
+        assertEquals(expected.size, actual.size)
 
-        expected = Arrays.asList("one", "two");
-        actual = mTestCol.getMedia().filesInStr(mid, "<img src=one><img src=two>");
-        actual.retainAll(expected);
-        assertEquals(expected.size(), actual.size());
+        expected = Arrays.asList("one", "two")
+        actual = mTestCol!!.media.filesInStr(mid, "<img src=one><img src=two>")
+        actual.retainAll(expected)
+        assertEquals(expected.size, actual.size)
 
-        expected = Collections.singletonList("foo.jpg");
-        actual = mTestCol.getMedia().filesInStr(mid, "aoeu<img src=\"foo.jpg\">ao");
-        actual.retainAll(expected);
-        assertEquals(expected.size(), actual.size());
+        expected = listOf("foo.jpg")
+        actual = mTestCol!!.media.filesInStr(mid, "aoeu<img src=\"foo.jpg\">ao")
+        actual.retainAll(expected)
+        assertEquals(expected.size, actual.size)
 
-        expected = Arrays.asList("foo.jpg", "fo");
-        actual = mTestCol.getMedia().filesInStr(mid, "aoeu<img src=\"foo.jpg\"><img class=yo src=fo>ao");
-        actual.retainAll(expected);
-        assertEquals(expected.size(), actual.size());
+        expected = Arrays.asList("foo.jpg", "fo")
+        actual =
+            mTestCol!!.media.filesInStr(mid, "aoeu<img src=\"foo.jpg\"><img class=yo src=fo>ao")
+        actual.retainAll(expected)
+        assertEquals(expected.size, actual.size)
 
-        expected = Collections.singletonList("foo.mp3");
-        actual = mTestCol.getMedia().filesInStr(mid, "aou[sound:foo.mp3]aou");
-        actual.retainAll(expected);
-        assertEquals(expected.size(), actual.size());
+        expected = listOf("foo.mp3")
+        actual = mTestCol!!.media.filesInStr(mid, "aou[sound:foo.mp3]aou")
+        actual.retainAll(expected)
+        assertEquals(expected.size, actual.size)
 
-        assertEquals("aoeu", mTestCol.getMedia().strip("aoeu"));
-        assertEquals("aoeuaoeu", mTestCol.getMedia().strip("aoeu[sound:foo.mp3]aoeu"));
-        assertEquals("aoeu", mTestCol.getMedia().strip("a<img src=yo>oeu"));
-        assertEquals("aoeu", Media.escapeImages("aoeu"));
-        assertEquals("<img src='http://foo.com'>", Media.escapeImages("<img src='http://foo.com'>"));
-        assertEquals("<img src=\"foo%20bar.jpg\">", Media.escapeImages("<img src=\"foo bar.jpg\">"));
+        assertEquals("aoeu", mTestCol!!.media.strip("aoeu"))
+        assertEquals("aoeuaoeu", mTestCol!!.media.strip("aoeu[sound:foo.mp3]aoeu"))
+        assertEquals("aoeu", mTestCol!!.media.strip("a<img src=yo>oeu"))
+        assertEquals("aoeu", Media.escapeImages("aoeu"))
+        assertEquals(
+            "<img src='http://foo.com'>",
+            Media.escapeImages("<img src='http://foo.com'>")
+        )
+        assertEquals(
+            "<img src=\"foo%20bar.jpg\">",
+            Media.escapeImages("<img src=\"foo bar.jpg\">")
+        )
     }
 
     @Test
-    public void testDeckIntegration() throws IOException, EmptyMediaException {
+    @Throws(IOException::class, EmptyMediaException::class)
+    fun testDeckIntegration() {
         // create a media dir
-        mTestCol.getMedia().dir();
+        mTestCol!!.media.dir()
         // Put a file into it
-        File file = createNonEmptyFile("fake.png");
-        mTestCol.getMedia().addFile(file);
+        val file = createNonEmptyFile("fake.png")
+        mTestCol!!.media.addFile(file)
         // add a note which references it
-        Note f = mTestCol.newNote();
-        f.setField(0, "one");
-        f.setField(1, "<img src='fake.png'>");
-        mTestCol.addNote(f);
+        var f = mTestCol!!.newNote()
+        f.setField(0, "one")
+        f.setField(1, "<img src='fake.png'>")
+        mTestCol!!.addNote(f)
         // and one which references a non-existent file
-        f = mTestCol.newNote();
-        f.setField(0, "one");
-        f.setField(1, "<img src='fake2.png'>");
-        mTestCol.addNote(f);
+        f = mTestCol!!.newNote()
+        f.setField(0, "one")
+        f.setField(1, "<img src='fake2.png'>")
+        mTestCol!!.addNote(f)
         // and add another file which isn't used
-        FileOutputStream os = new FileOutputStream(new File(mTestCol.getMedia().dir(), "foo.jpg"), false);
-        os.write("test".getBytes());
-        os.close();
+        val os = FileOutputStream(File(mTestCol!!.media.dir(), "foo.jpg"), false)
+        os.write("test".toByteArray())
+        os.close()
         // check media
-        List<List<String>> ret = mTestCol.getMedia().check();
-        List<String> expected = Collections.singletonList("fake2.png");
-        List<String> actual = ret.get(0);
-        actual.retainAll(expected);
-        assertEquals(expected.size(), actual.size());
-        expected = Collections.singletonList("foo.jpg");
-        actual = ret.get(1);
-        actual.retainAll(expected);
-        assertEquals(expected.size(), actual.size());
+        val ret = mTestCol!!.media.check()
+        var expected = listOf("fake2.png")
+        var actual = ret[0]
+        actual.retainAll(expected)
+        assertEquals(expected.size, actual.size)
+        expected = listOf("foo.jpg")
+        actual = ret[1]
+        actual.retainAll(expected)
+        assertEquals(expected.size, actual.size)
     }
 
     @Test
-    public void testAudioTags() {
-        assertEquals("aoeu", mTestCol.getMedia().strip("a<audio src=yo>oeu"));
-        assertEquals("aoeu", mTestCol.getMedia().strip("a<audio src='yo'>oeu"));
-        assertEquals("aoeu", mTestCol.getMedia().strip("a<audio src=\"yo\">oeu"));
+    fun testAudioTags() {
+        assertEquals("aoeu", mTestCol!!.media.strip("a<audio src=yo>oeu"))
+        assertEquals("aoeu", mTestCol!!.media.strip("a<audio src='yo'>oeu"))
+        assertEquals("aoeu", mTestCol!!.media.strip("a<audio src=\"yo\">oeu"))
     }
 
     @Test
-    public void testObjectTags() {
-        assertEquals("aoeu", mTestCol.getMedia().strip("a<object data=yo>oeu"));
-        assertEquals("aoeu", mTestCol.getMedia().strip("a<object data='yo'>oeu"));
-        assertEquals("aoeu", mTestCol.getMedia().strip("a<object data=\"yo\">oeu"));
+    fun testObjectTags() {
+        assertEquals("aoeu", mTestCol!!.media.strip("a<object data=yo>oeu"))
+        assertEquals("aoeu", mTestCol!!.media.strip("a<object data='yo'>oeu"))
+        assertEquals("aoeu", mTestCol!!.media.strip("a<object data=\"yo\">oeu"))
     }
 
-    private List<String> added(Collection d) {
-        return d.getMedia().getDb().queryStringList("select fname from media where csum is not null");
+    private fun added(d: Collection?): List<String> {
+        return d!!.media.db.queryStringList("select fname from media where csum is not null")
     }
 
-    private List<String> removed(Collection d) {
-        return d.getMedia().getDb().queryStringList("select fname from media where csum is null");
+    private fun removed(d: Collection?): List<String> {
+        return d!!.media.db.queryStringList("select fname from media where csum is null")
     }
 
     @Test
-    public void testChanges() throws IOException, EmptyMediaException {
+    @Throws(IOException::class, EmptyMediaException::class)
+    fun testChanges() {
         // legacy code, not used by backend
-        assumeThat(BackendFactory.getDefaultLegacySchema(), is(true));
-        assertNotNull(mTestCol.getMedia()._changed());
-        assertEquals(0, added(mTestCol).size());
-        assertEquals(0, removed(mTestCol).size());
+        Assume.assumeThat(defaultLegacySchema, Matchers.`is`(true))
+        assertNotNull(mTestCol!!.media._changed())
+        assertEquals(0, added(mTestCol).size)
+        assertEquals(0, removed(mTestCol).size)
         // add a file
-        File dir = getTestDir();
-        File path = new File(dir, "foo.jpg");
-        FileOutputStream os = new FileOutputStream(path, false);
-        os.write("hello".getBytes());
-        os.close();
-        path = new File(mTestCol.getMedia().dir(), mTestCol.getMedia().addFile(path));
+        val dir = testDir
+        var path = File(dir, "foo.jpg")
+        var os = FileOutputStream(path, false)
+        os.write("hello".toByteArray())
+        os.close()
+        path = File(mTestCol!!.media.dir(), mTestCol!!.media.addFile(path))
         // should have been logged
-        mTestCol.getMedia().findChanges();
-        assertThat(added(mTestCol).size(), is(greaterThan(0)));
-        assertEquals(0, removed(mTestCol).size());
+        mTestCol!!.media.findChanges()
+        MatcherAssert.assertThat(added(mTestCol).size, Matchers.`is`(Matchers.greaterThan(0)))
+        assertEquals(0, removed(mTestCol).size)
         // if we modify it, the cache won't notice
-        os = new FileOutputStream(path, true);
-        os.write("world".getBytes());
-        os.close();
-        assertEquals(1, added(mTestCol).size());
-        assertEquals(0, removed(mTestCol).size());
+        os = FileOutputStream(path, true)
+        os.write("world".toByteArray())
+        os.close()
+        assertEquals(1, added(mTestCol).size)
+        assertEquals(0, removed(mTestCol).size)
         // but if we add another file, it will
-        path = new File(path.getAbsolutePath()+"2");
-        os = new FileOutputStream(path, true);
-        os.write("yo".getBytes());
-        os.close();
-        mTestCol.getMedia().findChanges(true);
-        assertEquals(2, added(mTestCol).size());
-        assertEquals(0, removed(mTestCol).size());
+        path = File(path.absolutePath + "2")
+        os = FileOutputStream(path, true)
+        os.write("yo".toByteArray())
+        os.close()
+        mTestCol!!.media.findChanges(true)
+        assertEquals(2, added(mTestCol).size)
+        assertEquals(0, removed(mTestCol).size)
         // deletions should get noticed too
-        assertTrue(path.delete());
-        mTestCol.getMedia().findChanges(true);
-        assertEquals(1, added(mTestCol).size());
-        assertEquals(1, removed(mTestCol).size());
+        assertTrue(path.delete())
+        mTestCol!!.media.findChanges(true)
+        assertEquals(1, added(mTestCol).size)
+        assertEquals(1, removed(mTestCol).size)
     }
 
-
     @Test
-    public void testIllegal() {
-        String aString = "a:b|cd\\e/f\0g*h\\[i\\]j";
-        String good = "abcdefghij";
-        assertEquals(good, mTestCol.getMedia().stripIllegal(aString));
-        for (int i = 0; i < aString.length(); i++) {
-            char c = aString.charAt(i);
-            boolean bad = mTestCol.getMedia().hasIllegal("something" + c + "morestring");
+    fun testIllegal() {
+        val aString = "a:b|cd\\e/f\u0000g*h\\[i\\]j"
+        val good = "abcdefghij"
+        assertEquals(good, mTestCol!!.media.stripIllegal(aString))
+        for (i in 0 until aString.length) {
+            val c = aString[i]
+            val bad = mTestCol!!.media.hasIllegal("something" + c + "morestring")
             if (bad) {
-                assertEquals(-1, good.indexOf(c));
+                assertEquals(-1, good.indexOf(c))
             } else {
-                assertNotEquals(-1, good.indexOf(c));
+                assertNotEquals(-1, good.indexOf(c))
             }
         }
     }
 
-    protected File createNonEmptyFile(String fileName) throws IOException {
-        File file = new File(getTestDir(), fileName);
-        try (FileOutputStream os = new FileOutputStream(file, false)) {
-            os.write("a".getBytes());
-        }
-        return file;
+    @Throws(IOException::class)
+    protected fun createNonEmptyFile(fileName: String): File {
+        val file = File(testDir, fileName)
+        FileOutputStream(file, false).use { os -> os.write("a".toByteArray()) }
+        return file
     }
 }
