ankidroid/Anki-Android

commit e7daa57a3a1a31adbc594330613e061a6e358805

[Kotlin Migration] FabBehavior

AnkiDroid/src/main/java/com/ichi2/anki/widgets/FabBehavior.kt
@@ -13,80 +13,69 @@
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
-package com.ichi2.anki.widgets;
+package com.ichi2.anki.widgets
 
-import android.content.Context;
-
-import androidx.annotation.NonNull;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.core.view.ViewCompat;
-import android.util.AttributeSet;
-import android.view.View;
-
-
-import java.util.List;
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+import com.google.android.material.snackbar.Snackbar.SnackbarLayout
+import kotlin.math.min
 
 /**
  * Originally created by Paul Woitaschek (http://www.paul-woitaschek.de, woitaschek@posteo.de)
  * Defines the behavior for the floating action button. If the dependency is a Snackbar, move the
  * fab up.
  */
-public class FabBehavior extends CoordinatorLayout.Behavior<View> {
+class FabBehavior : CoordinatorLayout.Behavior<View> {
+    private var mTranslationY = 0f
 
-    private float mTranslationY;
+    constructor() : super() {}
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
 
-    public FabBehavior() {
-        super();
+    override fun layoutDependsOn(parent: CoordinatorLayout, child: View, dependency: View): Boolean {
+        return dependency is SnackbarLayout
     }
 
-    public FabBehavior(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    private static float getFabTranslationYForSnackbar(CoordinatorLayout parent, View fab) {
-        float minOffset = 0.0F;
-        List<View> dependencies = parent.getDependencies(fab);
-        int i = 0;
-
-        for (int z = dependencies.size(); i < z; ++i) {
-            View view = dependencies.get(i);
-            if (view instanceof Snackbar.SnackbarLayout && parent.doViewsOverlap(fab, view)) {
-                minOffset = Math.min(minOffset, view.getTranslationY() - (float) view.getHeight());
+    override fun onDependentViewChanged(parent: CoordinatorLayout, fab: View, dependency: View): Boolean {
+        if (dependency is SnackbarLayout && fab.visibility == View.VISIBLE) {
+            val translationY = getFabTranslationYForSnackbar(parent, fab)
+            if (translationY != mTranslationY) {
+                ViewCompat.animate(fab).cancel()
+                fab.translationY = translationY
+                mTranslationY = translationY
             }
         }
-
-        return minOffset;
+        return false
     }
 
-    @Override
-    public boolean layoutDependsOn(@NonNull CoordinatorLayout parent, @NonNull View child, @NonNull View dependency) {
-        return dependency instanceof Snackbar.SnackbarLayout;
-    }
-
-    @Override
-    public boolean onDependentViewChanged(@NonNull CoordinatorLayout parent, @NonNull View fab, @NonNull View dependency) {
-        if (dependency instanceof Snackbar.SnackbarLayout && fab.getVisibility() == View.VISIBLE) {
-            float translationY = getFabTranslationYForSnackbar(parent, fab);
-            if (translationY != this.mTranslationY) {
-                ViewCompat.animate(fab).cancel();
-                fab.setTranslationY(translationY);
-                this.mTranslationY = translationY;
+    override fun onDependentViewRemoved(parent: CoordinatorLayout, fab: View, dependency: View) {
+        super.onDependentViewRemoved(parent, fab, dependency)
+        if (dependency is SnackbarLayout && fab.visibility == View.VISIBLE) {
+            val translationY = getFabTranslationYForSnackbar(parent, fab)
+            if (translationY == mTranslationY) {
+                ViewCompat.animate(fab).cancel()
+                fab.translationY = 0f
+                mTranslationY = 0f
             }
         }
-        return false;
     }
 
-    @Override
-    public void onDependentViewRemoved(@NonNull CoordinatorLayout parent, @NonNull View fab, @NonNull View dependency) {
-        super.onDependentViewRemoved(parent, fab, dependency);
-        if (dependency instanceof Snackbar.SnackbarLayout && fab.getVisibility() == View.VISIBLE) {
-            float translationY = getFabTranslationYForSnackbar(parent, fab);
-            if (translationY == this.mTranslationY) {
-                ViewCompat.animate(fab).cancel();
-                fab.setTranslationY(0);
-                this.mTranslationY = 0;
+    companion object {
+        private fun getFabTranslationYForSnackbar(parent: CoordinatorLayout, fab: View): Float {
+            var minOffset = 0.0f
+            val dependencies = parent.getDependencies(fab)
+            var i = 0
+            val z = dependencies.size
+            while (i < z) {
+                val view = dependencies[i]
+                if (view is SnackbarLayout && parent.doViewsOverlap(fab, view)) {
+                    minOffset = min(minOffset, view.getTranslationY() - view.getHeight().toFloat())
+                }
+                ++i
             }
+            return minOffset
         }
     }
 }

AnkiDroid/kotlinMigration.gradle
@@ -43,7 +43,7 @@ permission notice:
 // Example of class name: "/com/ichi2/anki/UIUtils.kt"
 // Ensure that it starts with '/' (slash)
 def source = Source.MAIN
-def className = "/com/ichi2/anki/widgets/FabBehavior.kt"
+def className = ""
 
 enum Source {
     MAIN("/src/main/java"),

commit 5fc43f45ab07cb5799deed4961d062abae4d15e2

Fix fab getting stuck after removing snackbar with a swipe
This simply fixes a typo introduced in e7daa57a.

AnkiDroid/src/main/java/com/ichi2/anki/widgets/FabBehavior.kt
@@ -56,7 +56,7 @@ class FabBehavior : CoordinatorLayout.Behavior<View> {
         super.onDependentViewRemoved(parent, fab, dependency)
         if (dependency is SnackbarLayout && fab.visibility == View.VISIBLE) {
             val translationY = getFabTranslationYForSnackbar(parent, fab)
-            if (translationY == mTranslationY) {
+            if (translationY != mTranslationY) {
                 ViewCompat.animate(fab).cancel()
                 fab.translationY = 0f
                 mTranslationY = 0f