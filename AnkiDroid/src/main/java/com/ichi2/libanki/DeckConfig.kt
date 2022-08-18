124b6c5af8ea9a6449fc228d6e350500142fb967
David Allison <62114487+david-allison@users.noreply.github.com>
2022-08-18 02:08:40 +0100

    refactor: Convert DeckConfig to Kotlin
    com.ichi2.libanki.DeckConfig

--- AnkiDroid/src/main/java/com/ichi2/libanki/DeckConfig.kt
+++ AnkiDroid/src/main/java/com/ichi2/libanki/DeckConfig.kt
@@ -14,94 +14,85 @@
  You should have received a copy of the GNU General Public License along with
  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.ichi2.libanki;
+package com.ichi2.libanki
 
-import com.ichi2.utils.JSONObject;
+import com.ichi2.utils.JSONObject
+import com.ichi2.utils.KotlinCleanup
+import timber.log.Timber
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import timber.log.Timber;
-
-public class DeckConfig extends JSONObject {
-    @NonNull
-    private final Source mSource;
+class DeckConfig : JSONObject {
+    val source: Source
 
     /**
      * Creates a new empty deck config object
      */
-    private DeckConfig(@NonNull Source source) {
-        super();
-        this.mSource = source;
+    private constructor(source: Source) : super() {
+        this.source = source
     }
 
     /**
-     * Creates a copy from {@link JSONObject} and use it as a string
+     * Creates a copy from [JSONObject] and use it as a string
      *
      * This function will perform deepCopy on the passed object
      *
      */
-    public DeckConfig(JSONObject json, @NonNull Source source) {
-        super(json);
-        mSource = source;
+    @KotlinCleanup("nullability")
+    constructor(json: JSONObject?, source: Source) : super(
+        json!!
+    ) {
+        this.source = source
     }
 
     /**
      * Creates a deck config object form a json string
      */
-    public DeckConfig(String json, @NonNull Source source) {
-        super(json);
-        mSource = source;
+    constructor(json: String?, source: Source) : super(json) {
+        this.source = source
     }
 
-    public static @Nullable Boolean parseTimer(JSONObject config) {
-        //Note: Card.py used != 0, DeckOptions used == 1
-        try {
-            //#6089 - Anki 2.1.24 changed this to a bool, reverted in 2.1.25.
-            return config.getInt("timer") != 0;
-        } catch (Exception e) {
-            Timber.w(e);
-            try {
-                return config.getBoolean("timer");
-            } catch (Exception ex) {
-                Timber.w(ex);
-                return null;
+    val isDyn: Boolean
+        get() = getInt("dyn") == Consts.DECK_DYN
+    val isStd: Boolean
+        get() = getInt("dyn") == Consts.DECK_STD
+
+    override fun deepClone(): DeckConfig {
+        val dc = DeckConfig(source)
+        return deepClonedInto(dc)
+    }
+
+    /** Specifies how to save the config  */
+    enum class Source {
+        /** From an entry in dconf  */
+        DECK_CONFIG,
+
+        /** filtered decks have their config embedded in the deck  */
+        DECK_EMBEDDED
+    }
+
+    companion object {
+        fun parseTimer(config: JSONObject): Boolean? {
+            // Note: Card.py used != 0, DeckOptions used == 1
+            return try {
+                // #6089 - Anki 2.1.24 changed this to a bool, reverted in 2.1.25.
+                config.getInt("timer") != 0
+            } catch (e: Exception) {
+                Timber.w(e)
+                try {
+                    config.getBoolean("timer")
+                } catch (ex: Exception) {
+                    Timber.w(ex)
+                    null
+                }
             }
         }
-    }
 
-    public static boolean parseTimerOpt(JSONObject config, boolean defaultValue) {
-        Boolean ret = parseTimer(config);
-        if (ret == null) {
-            ret = defaultValue;
+        @KotlinCleanup("use ?:")
+        fun parseTimerOpt(config: JSONObject, defaultValue: Boolean): Boolean {
+            var ret = parseTimer(config)
+            if (ret == null) {
+                ret = defaultValue
+            }
+            return ret
         }
-        return ret;
-    }
-
-    public boolean isDyn() {
-        return getInt("dyn") == Consts.DECK_DYN;
-    }
-
-    public boolean isStd() {
-        return getInt("dyn") == Consts.DECK_STD;
-    }
-
-
-    @Override
-    public DeckConfig deepClone() {
-        DeckConfig dc = new DeckConfig(this.getSource());
-        return deepClonedInto(dc);
-    }
-
-    @NonNull
-    public Source getSource() {
-        return mSource;
-    }
-
-    /** Specifies how to save the config */
-    public enum Source {
-        /** From an entry in dconf */
-        DECK_CONFIG,
-        /** filtered decks have their config embedded in the deck */
-        DECK_EMBEDDED
     }
 }
