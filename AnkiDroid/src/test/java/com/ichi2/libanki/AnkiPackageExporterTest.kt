9cb7a2d8e660bb138f3de82b7a73b6bcd781674d
David Allison <62114487+david-allison@users.noreply.github.com>
2022-08-19 13:14:52 +0100

    refactor: Convert AnkiPackageExporterTest to Kotlin
    com.ichi2.libanki.AnkiPackageExporterTest

--- AnkiDroid/src/test/java/com/ichi2/libanki/AnkiPackageExporterTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/libanki/AnkiPackageExporterTest.kt
@@ -13,175 +13,152 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.ichi2.libanki
 
-package com.ichi2.libanki;
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.anki.RobolectricTest
+import com.ichi2.anki.exception.ImportExportException
+import com.ichi2.libanki.exception.EmptyMediaException
+import com.ichi2.utils.CreateTempDir.Companion.tempDir
+import com.ichi2.utils.FileOperation.Companion.getFileContents
+import com.ichi2.utils.KotlinCleanup
+import com.ichi2.utils.UnzipFile.Companion.unzip
+import org.hamcrest.MatcherAssert.*
+import org.hamcrest.Matchers.*
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.File
+import java.io.IOException
+import java.io.PrintWriter
+import java.util.*
 
-import com.ichi2.anki.RobolectricTest;
-import com.ichi2.anki.exception.ImportExportException;
-import com.ichi2.libanki.exception.EmptyMediaException;
-import com.ichi2.utils.CreateTempDir;
-import com.ichi2.utils.UnzipFile;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.List;
-
-import androidx.annotation.NonNull;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import static com.ichi2.utils.FileOperation.getFileContents;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertEquals;
-
-@RunWith(AndroidJUnit4.class)
-public class AnkiPackageExporterTest extends RobolectricTest {
-
-    @Override
-    protected boolean useInMemoryDatabase() {
-        return false;
+@RunWith(AndroidJUnit4::class)
+@KotlinCleanup("IDE Lint")
+class AnkiPackageExporterTest : RobolectricTest() {
+    override fun useInMemoryDatabase(): Boolean {
+        return false
     }
 
     @Test
-    public void missingFileInDeckExportDoesSkipsFile() throws IOException, ImportExportException {
+    @Throws(IOException::class, ImportExportException::class)
+    fun missingFileInDeckExportDoesSkipsFile() {
         // Arrange
-        File mediaFilePath = addTempFileToMediaAndNote();
-        if (!mediaFilePath.delete()) {
-            throw new IllegalStateException("need to delete temp file for test to pass");
-        }
-
-        AnkiPackageExporter exporter = getExporterForDeckWithMedia();
-        File temp = CreateTempDir.tempDir("/AnkiDroid-missingFileInExportDoesNotThrowException-export");
-
-        File exportedFile = new File(temp.getAbsolutePath() + "/export.apkg");
+        val mediaFilePath = addTempFileToMediaAndNote()
+        check(mediaFilePath.delete()) { "need to delete temp file for test to pass" }
+        val exporter = exporterForDeckWithMedia
+        val temp = tempDir("/AnkiDroid-missingFileInExportDoesNotThrowException-export")
+        val exportedFile = File(temp.absolutePath + "/export.apkg")
 
         // Exporting
-        exporter.exportInto(exportedFile.getAbsolutePath(), getTargetContext());
+        exporter.exportInto(exportedFile.absolutePath, targetContext)
 
         // Unzipping the export.apkg file
-        UnzipFile.unzip(exportedFile, temp.getAbsolutePath() + "/unzipped");
-
-        String unzipDirectory = temp.getAbsolutePath() + "/unzipped";
+        unzip(exportedFile, temp.absolutePath + "/unzipped")
+        val unzipDirectory = temp.absolutePath + "/unzipped"
 
         // Storing paths of unzipped files in a list
-        List<String> files = Arrays.asList(new File(unzipDirectory).list());
-        File file_names[] = new File[2];
-        int i = 0;
-        for (String x: files) {
-            File f = new File(unzipDirectory + "/" + x);
-            file_names[i++] = f;
+        val files = Arrays.asList(*File(unzipDirectory).list()!!)
+        val file_names = arrayOfNulls<File>(2)
+        var i = 0
+        for (x in files) {
+            val f = File("$unzipDirectory/$x")
+            file_names[i++] = f
         }
 
         // Checking the unzipped files
-        assertThat(files, containsInAnyOrder("collection.anki2", "media"));
-        assertThat("Only two files should exist", files, hasSize(2));
-        checkMediaExportStringIs(file_names, "{}");
+        assertThat(files, containsInAnyOrder("collection.anki2", "media"))
+        assertThat("Only two files should exist", files, hasSize(2))
+        checkMediaExportStringIs(file_names, "{}")
     }
 
     @Test
-    public void fileInExportIsCopied() throws IOException, ImportExportException {
+    @Throws(IOException::class, ImportExportException::class)
+    fun fileInExportIsCopied() {
         // Arrange
-        File tempFileInCollection = addTempFileToMediaAndNote();
-
-        AnkiPackageExporter exporter = getExporterForDeckWithMedia();
-        File temp = CreateTempDir.tempDir("/AnkiDroid-missingFileInExportDoesNotThrowException-export");
-
-        File exportedFile = new File(temp.getAbsolutePath() + "/export.apkg");
+        val tempFileInCollection = addTempFileToMediaAndNote()
+        val exporter = exporterForDeckWithMedia
+        val temp = tempDir("/AnkiDroid-missingFileInExportDoesNotThrowException-export")
+        val exportedFile = File(temp.absolutePath + "/export.apkg")
 
         // Exporting
-        exporter.exportInto(exportedFile.getAbsolutePath(), getTargetContext());
+        exporter.exportInto(exportedFile.absolutePath, targetContext)
 
         // Unzipping the export.apkg file
-        UnzipFile.unzip(exportedFile, temp.getAbsolutePath() + "/unzipped");
-
-        String unzipDirectory = temp.getAbsolutePath() + "/unzipped";
+        unzip(exportedFile, temp.absolutePath + "/unzipped")
+        val unzipDirectory = temp.absolutePath + "/unzipped"
 
         // Storing paths of unzipped files in a list
-        List<String> files = Arrays.asList(new File(unzipDirectory).list());
-        File fileNames[] = new File[3];
-        int i = 0;
-        for (String x : files) {
-            File f = new File(unzipDirectory + "/" + x);
-            fileNames[i++] = f;
+        val files = Arrays.asList(*File(unzipDirectory).list()!!)
+        val fileNames = arrayOfNulls<File>(3)
+        var i = 0
+        for (x in files) {
+            val f = File("$unzipDirectory/$x")
+            fileNames[i++] = f
         }
         // Checking the unzipped files
-        assertThat(files, containsInAnyOrder("collection.anki2", "media", "0"));
-        assertThat("Three files should exist", files, hasSize(3));
+        assertThat(
+            files,
+            containsInAnyOrder("collection.anki2", "media", "0")
+        )
+        assertThat("Three files should exist", files, hasSize(3))
 
         // {"0":"filename.txt"}
-        String expected = String.format("{\"0\":\"%s\"}", tempFileInCollection.getName());
-        checkMediaExportStringIs(fileNames, expected);
+        val expected = String.format("{\"0\":\"%s\"}", tempFileInCollection.name)
+        checkMediaExportStringIs(fileNames, expected)
     }
-
 
     @Test
-    public void stripHTML_will_remove_html_with_unicode_whitespace() {
-        Exporter exporter = getExporterForDeckWithMedia();
-        final String text = "\n"+"\n\u205F\t<[sound:test.mp3]>" +
-                "\n\u2029" +
-                "\t\u2004" +
-                "<!-- Comment \n \u1680 --> <\"tag\\n><style><s>";
-        final String res = exporter.stripHTML(text);
-
-        assertEquals("", res);
+    fun stripHTML_will_remove_html_with_unicode_whitespace() {
+        val exporter: Exporter = exporterForDeckWithMedia
+        val text = "\n" + "\n\u205F\t<[sound:test.mp3]>" +
+            "\n\u2029" +
+            "\t\u2004" +
+            "<!-- Comment \n \u1680 --> <\"tag\\n><style><s>"
+        val res = exporter.stripHTML(text)
+        Assert.assertEquals("", res)
     }
 
-
-    private void checkMediaExportStringIs(File[] files, String s) throws IOException {
-        for (File f : files) {
-            if (!"media".equals(f.getName())) {
-                continue;
+    @Throws(IOException::class)
+    private fun checkMediaExportStringIs(files: Array<File?>, s: String) {
+        for (f in files) {
+            if ("media" != f!!.name) {
+                continue
             }
-            List<String> lines = Arrays.asList(getFileContents(f).split("\n"));
-            assertThat(lines, contains(s));
-            return;
+            val lines = Arrays.asList(
+                *getFileContents(
+                    f
+                ).split("\n").toTypedArray()
+            )
+            assertThat(lines, contains(s))
+            return
         }
-
-        Assert.fail("media file not found");
+        Assert.fail("media file not found")
     }
 
+    private val exporterForDeckWithMedia: AnkiPackageExporter
+        get() = AnkiPackageExporter(col, 1L, true, true)
 
-
-    @NonNull
-    private AnkiPackageExporter getExporterForDeckWithMedia() {
-        AnkiPackageExporter exporter = new AnkiPackageExporter(getCol(), 1L, true, true);
-        return exporter;
+    @Throws(IOException::class)
+    private fun addTempFileToMediaAndNote(): File {
+        val temp = File.createTempFile("AnkiDroid-missingFileInExportDoesNotThrowException", ".txt")
+        val writer = PrintWriter(temp)
+        writer.println("unit test data")
+        writer.close()
+        val s = addFile(temp)
+        temp.delete()
+        val newFile = File(col.media.dir(), s)
+        check(newFile.exists()) { "Could not create temp file" }
+        addNoteUsingBasicModel(String.format("<img src=\"%s\">", newFile.name), "Back")
+        return newFile
     }
 
-    private File addTempFileToMediaAndNote() throws IOException {
-        File temp = File.createTempFile("AnkiDroid-missingFileInExportDoesNotThrowException", ".txt");
-        PrintWriter writer = new PrintWriter(temp);
-        writer.println("unit test data");
-        writer.close();
-
-        String s = addFile(temp);
-        temp.delete();
-
-        File newFile = new File(getCol().getMedia().dir(), s);
-        if (!newFile.exists()) {
-            throw new IllegalStateException("Could not create temp file");
-        }
-
-        addNoteUsingBasicModel(String.format("<img src=\"%s\">", newFile.getName()), "Back");
-
-        return newFile;
-    }
-
-
-    private String addFile(File temp) throws IOException {
-        try {
-            return getCol().getMedia().addFile(temp);
-        } catch (EmptyMediaException e) {
-            throw new RuntimeException(e);
+    @Throws(IOException::class)
+    private fun addFile(temp: File): String {
+        return try {
+            col.media.addFile(temp)
+        } catch (e: EmptyMediaException) {
+            throw RuntimeException(e)
         }
     }
-
 }
