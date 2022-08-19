850c71ea61c8fd736f6614894fd66b9c12759e07
David Allison <62114487+david-allison@users.noreply.github.com>
2022-08-19 06:41:22 +0100

    refactor: Convert ACRATest to Kotlin
    com.ichi2.anki.tests.ACRATest

--- AnkiDroid/src/androidTest/java/com/ichi2/anki/tests/ACRATest.kt
+++ AnkiDroid/src/androidTest/java/com/ichi2/anki/tests/ACRATest.kt
@@ -14,264 +14,280 @@
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
+package com.ichi2.anki.tests
 
-package com.ichi2.anki.tests;
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.SharedPreferences
+import androidx.annotation.StringRes
+import androidx.test.annotation.UiThreadTest
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.GrantPermissionRule
+import com.ichi2.anki.AnkiDroidApp
+import com.ichi2.anki.CrashReportService
+import com.ichi2.anki.CrashReportService.FEEDBACK_REPORT_ALWAYS
+import com.ichi2.anki.CrashReportService.FEEDBACK_REPORT_ASK
+import com.ichi2.anki.R
+import com.ichi2.utils.KotlinCleanup
+import org.acra.ACRA
+import org.acra.builder.ReportBuilder
+import org.acra.collections.ImmutableList
+import org.acra.config.ACRAConfigurationException
+import org.acra.config.LimitingReportAdministrator
+import org.acra.config.ToastConfiguration
+import org.acra.data.CrashReportDataFactory
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import timber.log.Timber
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.SharedPreferences;
-
-import androidx.annotation.StringRes;
-import androidx.test.annotation.UiThreadTest;
-import androidx.test.rule.GrantPermissionRule;
-
-import com.ichi2.anki.CrashReportService;
-import com.ichi2.anki.AnkiDroidApp;
-import com.ichi2.anki.R;
-
-import org.acra.ACRA;
-import org.acra.builder.ReportBuilder;
-import org.acra.collections.ImmutableList;
-import org.acra.config.ACRAConfigurationException;
-import org.acra.config.Configuration;
-import org.acra.config.CoreConfiguration;
-import org.acra.config.LimitingReportAdministrator;
-import org.acra.config.ToastConfiguration;
-import org.acra.data.CrashReportData;
-import org.acra.data.CrashReportDataFactory;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import java.lang.reflect.Method;
-
-import timber.log.Timber;
-
-import static com.ichi2.anki.CrashReportService.FEEDBACK_REPORT_ALWAYS;
-import static com.ichi2.anki.CrashReportService.FEEDBACK_REPORT_ASK;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-@RunWith(androidx.test.ext.junit.runners.AndroidJUnit4.class)
+@RunWith(AndroidJUnit4::class)
 @SuppressLint("DirectSystemCurrentTimeMillisUsage")
-public class ACRATest extends InstrumentedTest {
+@KotlinCleanup("IDE Lint")
+@KotlinCleanup("is -> equalTo")
+class ACRATest : InstrumentedTest() {
+    @get:Rule
+    var runtimePermissionRule =
+        GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    private var mApp: AnkiDroidApp? = null
+    private val mDebugLogcatArguments = arrayOf("-t", "300", "-v", "long", "ACRA:S")
 
-    @Rule public GrantPermissionRule mRuntimePermissionRule =
-            GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-    private AnkiDroidApp mApp = null;
-
-    private final String[] mDebugLogcatArguments = { "-t", "300", "-v", "long", "ACRA:S"};
-    //private String[] prodLogcatArguments = { "-t", "100", "-v", "time", "ActivityManager:I", "SQLiteLog:W", AnkiDroidApp.TAG + ":D", "*:S" };
-
-
+    // private String[] prodLogcatArguments = { "-t", "100", "-v", "time", "ActivityManager:I", "SQLiteLog:W", AnkiDroidApp.TAG + ":D", "*:S" };
     @Before
     @UiThreadTest
-    public void setUp() {
-        mApp = (AnkiDroidApp) getTestContext().getApplicationContext();
+    fun setUp() {
+        mApp = testContext.applicationContext as AnkiDroidApp
         // Note: attachBaseContext can't be called twice as we're using the same instance between all tests.
-        mApp.onCreate();
+        mApp!!.onCreate()
     }
 
     @Test
-    public void testDebugConfiguration() throws Exception {
+    @Throws(Exception::class)
+    fun testDebugConfiguration() {
 
         // Debug mode overrides all saved state so no setup needed
-        CrashReportService.setDebugACRAConfig(getSharedPrefs());
-        assertArrayEquals("Debug logcat arguments not set correctly",
-                CrashReportService.getAcraCoreConfigBuilder().build().logcatArguments().toArray(),
-                new ImmutableList<>(mDebugLogcatArguments).toArray());
-        verifyDebugACRAPreferences();
+        CrashReportService.setDebugACRAConfig(sharedPrefs)
+        assertArrayEquals(
+            "Debug logcat arguments not set correctly",
+            CrashReportService.getAcraCoreConfigBuilder().build().logcatArguments().toTypedArray(),
+            ImmutableList(*mDebugLogcatArguments).toTypedArray()
+        )
+        verifyDebugACRAPreferences()
     }
 
-    private void verifyDebugACRAPreferences() {
-        assertTrue("ACRA was not disabled correctly",
-                getSharedPrefs()
-                        .getBoolean(ACRA.PREF_DISABLE_ACRA, true));
-        assertEquals("ACRA feedback was not turned off correctly",
-                CrashReportService.FEEDBACK_REPORT_NEVER,
-                getSharedPrefs()
-                        .getString(CrashReportService.FEEDBACK_REPORT_KEY, "undefined"));
+    private fun verifyDebugACRAPreferences() {
+        assertTrue(
+            "ACRA was not disabled correctly",
+            sharedPrefs
+                .getBoolean(ACRA.PREF_DISABLE_ACRA, true)
+        )
+        assertEquals(
+            "ACRA feedback was not turned off correctly",
+            CrashReportService.FEEDBACK_REPORT_NEVER,
+            sharedPrefs
+                .getString(CrashReportService.FEEDBACK_REPORT_KEY, "undefined")
+        )
     }
 
     @Test
-    public void testProductionConfigurationUserDisabled() throws Exception {
+    @Throws(Exception::class)
+    fun testProductionConfigurationUserDisabled() {
 
         // set up as if the user had prefs saved to disable completely
-        setReportConfig(CrashReportService.FEEDBACK_REPORT_NEVER);
+        setReportConfig(CrashReportService.FEEDBACK_REPORT_NEVER)
 
         // ACRA initializes production logcat via annotation and we can't mock Build.DEBUG
         // That means we are restricted from verifying production logcat args and this is the debug case again
-        CrashReportService.setProductionACRAConfig(getSharedPrefs());
-        verifyDebugACRAPreferences();
+        CrashReportService.setProductionACRAConfig(sharedPrefs)
+        verifyDebugACRAPreferences()
     }
 
     @Test
-    public void testProductionConfigurationUserAsk() throws Exception {
+    @Throws(Exception::class)
+    fun testProductionConfigurationUserAsk() {
         // set up as if the user had prefs saved to ask
-        setReportConfig(FEEDBACK_REPORT_ASK);
+        setReportConfig(FEEDBACK_REPORT_ASK)
 
         // If the user is set to ask, then it's production, with interaction mode dialog
-        CrashReportService.setProductionACRAConfig(getSharedPrefs());
-        verifyACRANotDisabled();
+        CrashReportService.setProductionACRAConfig(sharedPrefs)
+        verifyACRANotDisabled()
 
-        assertToastMessage(R.string.feedback_for_manual_toast_text);
-        assertToastIsEnabled();
-        assertDialogEnabledStatus("Dialog should be enabled", true);
+        assertToastMessage(R.string.feedback_for_manual_toast_text)
+        assertToastIsEnabled()
+        assertDialogEnabledStatus("Dialog should be enabled", true)
     }
 
     @Test
-    public void testCrashReportLimit() throws Exception {
+    @Throws(Exception::class)
+    fun testCrashReportLimit() {
         // To test ACRA switch on  reporting, plant a production tree, and trigger a report
-        Timber.plant(new AnkiDroidApp.ProductionCrashReportingTree());
+        Timber.plant(AnkiDroidApp.ProductionCrashReportingTree())
 
         // set up as if the user had prefs saved to full auto
-        setReportConfig(FEEDBACK_REPORT_ALWAYS);
+        setReportConfig(FEEDBACK_REPORT_ALWAYS)
 
         // If the user is set to always, then it's production, with interaction mode toast
         // will be useful with ACRA 5.2.0
-        CrashReportService.setProductionACRAConfig(getSharedPrefs());
+        CrashReportService.setProductionACRAConfig(sharedPrefs)
 
         // The same class/method combo is only sent once, so we face a new method each time (should test that system later)
-        Exception crash = new Exception("testCrashReportSend at " + System.currentTimeMillis());
-        StackTraceElement[] trace = new StackTraceElement[] {
-                new StackTraceElement("Class", "Method" + (int)System.currentTimeMillis(), "File", (int)System.currentTimeMillis())
-        };
-        crash.setStackTrace(trace);
+        val crash = Exception("testCrashReportSend at " + System.currentTimeMillis())
+        val trace = arrayOf(
+            StackTraceElement(
+                "Class",
+                "Method" + System.currentTimeMillis().toInt(),
+                "File",
+                System.currentTimeMillis().toInt()
+            )
+        )
+        crash.stackTrace = trace
 
         // one send should work
-        CrashReportData crashData = new CrashReportDataFactory(getTestContext(),
-                CrashReportService.getAcraCoreConfigBuilder().build()).createCrashData(new ReportBuilder().exception(crash));
-
-        assertTrue(new LimitingReportAdministrator().shouldSendReport(
-                getTestContext(),
+        val crashData = CrashReportDataFactory(
+            testContext,
+            CrashReportService.getAcraCoreConfigBuilder().build()
+        ).createCrashData(ReportBuilder().exception(crash))
+        assertTrue(
+            LimitingReportAdministrator().shouldSendReport(
+                testContext,
                 CrashReportService.getAcraCoreConfigBuilder().build(),
-                crashData)
-        );
+                crashData
+            )
+        )
 
         // A second send should not work
-        assertFalse(new LimitingReportAdministrator().shouldSendReport(
-                getTestContext(),
+        assertFalse(
+            LimitingReportAdministrator().shouldSendReport(
+                testContext,
                 CrashReportService.getAcraCoreConfigBuilder().build(),
-                crashData)
-        );
+                crashData
+            )
+        )
 
         // Now let's clear data
-        CrashReportService.deleteACRALimiterData(getTestContext());
+        CrashReportService.deleteACRALimiterData(testContext)
 
         // A third send should work again
-        assertTrue(new LimitingReportAdministrator().shouldSendReport(
-                getTestContext(),
+        assertTrue(
+            LimitingReportAdministrator().shouldSendReport(
+                testContext,
                 CrashReportService.getAcraCoreConfigBuilder().build(),
-                crashData)
-        );
+                crashData
+            )
+        )
     }
 
-
     @Test
-    public void testProductionConfigurationUserAlways() throws Exception {
+    @Throws(Exception::class)
+    fun testProductionConfigurationUserAlways() {
         // set up as if the user had prefs saved to full auto
-        setReportConfig(FEEDBACK_REPORT_ALWAYS);
+        setReportConfig(FEEDBACK_REPORT_ALWAYS)
 
         // If the user is set to always, then it's production, with interaction mode toast
-        CrashReportService.setProductionACRAConfig(getSharedPrefs());
-        verifyACRANotDisabled();
+        CrashReportService.setProductionACRAConfig(sharedPrefs)
+        verifyACRANotDisabled()
 
-        assertToastMessage(R.string.feedback_auto_toast_text);
-        assertToastIsEnabled();
-        assertDialogEnabledStatus("Dialog should not be enabled", false);
+        assertToastMessage(R.string.feedback_auto_toast_text)
+        assertToastIsEnabled()
+        assertDialogEnabledStatus("Dialog should not be enabled", false)
     }
 
-
     @Test
-    public void testDialogEnabledWhenMovingFromAlwaysToAsk() throws Exception {
+    @Throws(Exception::class)
+    fun testDialogEnabledWhenMovingFromAlwaysToAsk() {
         // Raised in #6891 - we ned to ensure that the dialog is re-enabled after this transition.
-        setReportConfig(FEEDBACK_REPORT_ALWAYS);
+        setReportConfig(FEEDBACK_REPORT_ALWAYS)
 
         // If the user is set to ask, then it's production, with interaction mode dialog
-        CrashReportService.setProductionACRAConfig(getSharedPrefs());
-        verifyACRANotDisabled();
+        CrashReportService.setProductionACRAConfig(sharedPrefs)
+        verifyACRANotDisabled()
 
-        assertDialogEnabledStatus("dialog should be disabled when status is ALWAYS", false);
-        assertToastMessage(R.string.feedback_auto_toast_text);
+        assertDialogEnabledStatus("dialog should be disabled when status is ALWAYS", false)
+        assertToastMessage(R.string.feedback_auto_toast_text)
 
-        setAcraReportingMode(FEEDBACK_REPORT_ASK);
+        setAcraReportingMode(FEEDBACK_REPORT_ASK)
 
-        assertDialogEnabledStatus("dialog should be re-enabled after changed to ASK", true);
-        assertToastMessage(R.string.feedback_for_manual_toast_text);
+        assertDialogEnabledStatus("dialog should be re-enabled after changed to ASK", true)
+        assertToastMessage(R.string.feedback_for_manual_toast_text)
     }
 
     @Test
-    public void testToastTextWhenMovingFromAskToAlways() throws Exception {
+    @Throws(Exception::class)
+    fun testToastTextWhenMovingFromAskToAlways() {
         // Raised in #6891 - we ned to ensure that the text is fixed after this transition.
-        setReportConfig(FEEDBACK_REPORT_ASK);
+        setReportConfig(FEEDBACK_REPORT_ASK)
 
         // If the user is set to ask, then it's production, with interaction mode dialog
-        CrashReportService.setProductionACRAConfig(getSharedPrefs());
-        verifyACRANotDisabled();
+        CrashReportService.setProductionACRAConfig(sharedPrefs)
+        verifyACRANotDisabled()
 
-        assertToastMessage(R.string.feedback_for_manual_toast_text);
+        assertToastMessage(R.string.feedback_for_manual_toast_text)
 
-        setAcraReportingMode(FEEDBACK_REPORT_ALWAYS);
+        setAcraReportingMode(FEEDBACK_REPORT_ALWAYS)
 
-        assertToastMessage(R.string.feedback_auto_toast_text);
+        assertToastMessage(R.string.feedback_auto_toast_text)
     }
 
-
-    private void setAcraReportingMode(String feedbackReportAlways) {
-        CrashReportService.setAcraReportingMode(feedbackReportAlways);
+    private fun setAcraReportingMode(feedbackReportAlways: String) {
+        CrashReportService.setAcraReportingMode(feedbackReportAlways)
     }
 
-
-    private void assertDialogEnabledStatus(String message, boolean isEnabled) throws ACRAConfigurationException {
-        CoreConfiguration config = CrashReportService.getAcraCoreConfigBuilder().build();
-        for (Configuration configuration : config.pluginConfigurations()) {
+    @Throws(ACRAConfigurationException::class)
+    private fun assertDialogEnabledStatus(message: String, isEnabled: Boolean) {
+        val config = CrashReportService.getAcraCoreConfigBuilder().build()
+        for (configuration in config.pluginConfigurations()) {
             // Make sure the dialog is set to pop up
-            if (configuration.getClass().toString().contains("Dialog")) {
-                assertThat(message, configuration.enabled(), is(isEnabled));
+            if (configuration.javaClass.toString().contains("Dialog")) {
+                assertThat(message, configuration.enabled(), `is`(isEnabled))
             }
         }
     }
 
-
-    private void assertToastIsEnabled() throws ACRAConfigurationException {
-        CoreConfiguration config = CrashReportService.getAcraCoreConfigBuilder().build();
-        for (Configuration configuration : config.pluginConfigurations()) {
-
-            if (configuration.getClass().toString().contains("Toast")) {
-                assertThat("Toast should be enabled", configuration.enabled(), is(true));
+    @Throws(ACRAConfigurationException::class)
+    private fun assertToastIsEnabled() {
+        val config = CrashReportService.getAcraCoreConfigBuilder().build()
+        for (configuration in config.pluginConfigurations()) {
+            if (configuration.javaClass.toString().contains("Toast")) {
+                assertThat(
+                    "Toast should be enabled",
+                    configuration.enabled(),
+                    `is`(true)
+                )
             }
         }
     }
 
-    private void assertToastMessage(@StringRes int res) throws ACRAConfigurationException {
-        CoreConfiguration config = CrashReportService.getAcraCoreConfigBuilder().build();
-        for (Configuration configuration : config.pluginConfigurations()) {
-
-            if (configuration.getClass().toString().contains("Toast")) {
-                assertEquals(mApp.getResources().getString(res),
-                        ((ToastConfiguration)configuration).text());
-                assertTrue("Toast should be enabled", configuration.enabled());
+    @Throws(ACRAConfigurationException::class)
+    private fun assertToastMessage(@StringRes res: Int) {
+        val config = CrashReportService.getAcraCoreConfigBuilder().build()
+        for (configuration in config.pluginConfigurations()) {
+            if (configuration.javaClass.toString().contains("Toast")) {
+                assertEquals(
+                    mApp!!.resources.getString(res),
+                    (configuration as ToastConfiguration).text()
+                )
+                assertTrue("Toast should be enabled", configuration.enabled())
             }
         }
     }
 
-    private void verifyACRANotDisabled() {
-        assertFalse("ACRA was not enabled correctly",
-                getSharedPrefs().getBoolean(ACRA.PREF_DISABLE_ACRA, false));
+    private fun verifyACRANotDisabled() {
+        assertFalse(
+            "ACRA was not enabled correctly",
+            sharedPrefs.getBoolean(ACRA.PREF_DISABLE_ACRA, false)
+        )
     }
 
-    private void setReportConfig(String feedbackReportAsk) {
-        getSharedPrefs().edit()
-                .putString(CrashReportService.FEEDBACK_REPORT_KEY, feedbackReportAsk).commit();
+    @KotlinCleanup("edit {}")
+    private fun setReportConfig(feedbackReportAsk: String) {
+        sharedPrefs.edit()
+            .putString(CrashReportService.FEEDBACK_REPORT_KEY, feedbackReportAsk).commit()
     }
 
-    private SharedPreferences getSharedPrefs() {
-        return AnkiDroidApp.getSharedPrefs(getTestContext());
-    }
+    private val sharedPrefs: SharedPreferences
+        get() = AnkiDroidApp.getSharedPrefs(testContext)
 }
