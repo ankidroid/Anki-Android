6029693bf28d6d929ffd2273f736bcd4e679d431
David Allison <62114487+david-allison@users.noreply.github.com>
2022-08-21 05:22:06 +0100

    refactor: Convert Pow to Kotlin
    com.wildplot.android.parsing.Pow

--- AnkiDroid/src/main/java/com/wildplot/android/parsing/Pow.kt
+++ AnkiDroid/src/main/java/com/wildplot/android/parsing/Pow.kt
@@ -13,140 +13,118 @@
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
-package com.wildplot.android.parsing;
+package com.wildplot.android.parsing
 
-
-import android.annotation.SuppressLint;
+import android.annotation.SuppressLint
 
 @SuppressLint("NonPublicNonStaticFieldName")
-public class Pow implements TreeElement {
-    private final TopLevelParser parser;
-
-
-    public enum PowType {ATOM, ATOM_POW_FACTOR, ATOM_SQRT_FACTOR, INVALID}
-
-
-    private PowType powType = PowType.INVALID;
-    private Atom atom;
-    private Factor factor;
-
-
-    public Pow(String powString, TopLevelParser parser) {
-        this.parser = parser;
-        if (!TopLevelParser.stringHasValidBrackets(powString)) {
-            this.powType = PowType.INVALID;
-            return;
-        }
-
-        boolean isReady = initAsAtom(powString);
-        if (!isReady) {
-            isReady = initAsAtomPowFactor(powString);
-        }
-        if (!isReady) {
-            isReady = initAsAtomSqrtFactor(powString);
-        }
-        if (!isReady) {
-            this.powType = PowType.INVALID;
-        }
+class Pow(powString: String, private val parser: TopLevelParser) : TreeElement {
+    enum class PowType {
+        ATOM, ATOM_POW_FACTOR, ATOM_SQRT_FACTOR, INVALID
     }
 
-
-    private boolean initAsAtom(String powString) {
-        Atom atom = new Atom(powString, parser);
-        boolean isValidAtom = atom.getAtomType() != Atom.AtomType.INVALID;
+    var powType = PowType.INVALID
+        private set
+    private var atom: Atom? = null
+    private var factor: Factor? = null
+    private fun initAsAtom(powString: String): Boolean {
+        val atom = Atom(powString, parser)
+        val isValidAtom = atom.atomType !== Atom.AtomType.INVALID
         if (isValidAtom) {
-            this.powType = PowType.ATOM;
-            this.atom = atom;
-            return true;
+            powType = PowType.ATOM
+            this.atom = atom
+            return true
         }
-        return false;
+        return false
     }
 
-
-    private boolean initAsAtomPowFactor(String powString) {
-        int opPos = powString.indexOf("^");
+    private fun initAsAtomPowFactor(powString: String): Boolean {
+        val opPos = powString.indexOf("^")
         if (opPos > 0) {
-            String leftAtomString = powString.substring(0, opPos);
-            String rightFactorString = powString.substring(opPos + 1);
-            if (!TopLevelParser.stringHasValidBrackets(leftAtomString) || !TopLevelParser.stringHasValidBrackets(rightFactorString)) {
-                return false;
+            val leftAtomString = powString.substring(0, opPos)
+            val rightFactorString = powString.substring(opPos + 1)
+            if (!TopLevelParser.stringHasValidBrackets(leftAtomString) || !TopLevelParser.stringHasValidBrackets(
+                    rightFactorString
+                )
+            ) {
+                return false
             }
-            Atom leftAtom = new Atom(leftAtomString, parser);
-            boolean isValidAtom = leftAtom.getAtomType() != Atom.AtomType.INVALID;
+            val leftAtom = Atom(leftAtomString, parser)
+            val isValidAtom = leftAtom.atomType !== Atom.AtomType.INVALID
             if (isValidAtom) {
-                Factor rightFactor = new Factor(rightFactorString, parser);
-                boolean isValidFactor = rightFactor.getFactorType() != Factor.FactorType.INVALID;
+                val rightFactor = Factor(rightFactorString, parser)
+                val isValidFactor = rightFactor.factorType != Factor.FactorType.INVALID
                 if (isValidFactor) {
-                    this.powType = PowType.ATOM_POW_FACTOR;
-                    this.atom = leftAtom;
-                    this.factor = rightFactor;
-                    return true;
+                    powType = PowType.ATOM_POW_FACTOR
+                    atom = leftAtom
+                    factor = rightFactor
+                    return true
                 }
             }
         }
-
-        return false;
+        return false
     }
 
-
-    private boolean initAsAtomSqrtFactor(String powString) {
-        int opPos = powString.indexOf("**");
+    private fun initAsAtomSqrtFactor(powString: String): Boolean {
+        val opPos = powString.indexOf("**")
         if (opPos > 0) {
-            String leftAtomString = powString.substring(0, opPos);
-            String rightFactorString = powString.substring(opPos + 2);
-            if (!TopLevelParser.stringHasValidBrackets(leftAtomString) || !TopLevelParser.stringHasValidBrackets(rightFactorString)) {
-                return false;
+            val leftAtomString = powString.substring(0, opPos)
+            val rightFactorString = powString.substring(opPos + 2)
+            if (!TopLevelParser.stringHasValidBrackets(leftAtomString) || !TopLevelParser.stringHasValidBrackets(
+                    rightFactorString
+                )
+            ) {
+                return false
             }
-            Atom leftAtom = new Atom(leftAtomString, parser);
-            boolean isValidAtom = leftAtom.getAtomType() != Atom.AtomType.INVALID;
+            val leftAtom = Atom(leftAtomString, parser)
+            val isValidAtom = leftAtom.atomType !== Atom.AtomType.INVALID
             if (isValidAtom) {
-                Factor rightFactor = new Factor(rightFactorString, parser);
-                boolean isValidFactor = rightFactor.getFactorType() != Factor.FactorType.INVALID;
+                val rightFactor = Factor(rightFactorString, parser)
+                val isValidFactor = rightFactor.factorType != Factor.FactorType.INVALID
                 if (isValidFactor) {
-                    this.powType = PowType.ATOM_SQRT_FACTOR;
-                    this.atom = leftAtom;
-                    this.factor = rightFactor;
-                    return true;
+                    powType = PowType.ATOM_SQRT_FACTOR
+                    atom = leftAtom
+                    factor = rightFactor
+                    return true
                 }
             }
         }
-
-        return false;
+        return false
     }
 
-
-    @Override
-    public double getValue() throws ExpressionFormatException {
-        switch (powType) {
-            case ATOM:
-                return atom.getValue();
-            case ATOM_POW_FACTOR:
-                return Math.pow(atom.getValue(), factor.getValue());
-            case ATOM_SQRT_FACTOR:
-                return Math.pow(atom.getValue(), 1.0 / factor.getValue());
-            case INVALID:
-            default:
-                throw new ExpressionFormatException("cannot parse Atom expression");
+    @Throws(ExpressionFormatException::class)
+    override fun getValue(): Double {
+        return when (powType) {
+            PowType.ATOM -> atom!!.value
+            PowType.ATOM_POW_FACTOR -> Math.pow(atom!!.value, factor!!.value)
+            PowType.ATOM_SQRT_FACTOR -> Math.pow(atom!!.value, 1.0 / factor!!.value)
+            PowType.INVALID -> throw ExpressionFormatException("cannot parse Atom expression")
         }
     }
 
-
-    @Override
-    public boolean isVariable() throws ExpressionFormatException {
-        switch (powType) {
-            case ATOM:
-                return atom.isVariable();
-            case ATOM_POW_FACTOR:
-            case ATOM_SQRT_FACTOR:
-                return atom.isVariable() || factor.isVariable();
-            case INVALID:
-            default:
-                throw new ExpressionFormatException("cannot parse Atom expression");
+    @Throws(ExpressionFormatException::class)
+    override fun isVariable(): Boolean {
+        return when (powType) {
+            PowType.ATOM -> atom!!.isVariable
+            PowType.ATOM_POW_FACTOR, PowType.ATOM_SQRT_FACTOR -> atom!!.isVariable || factor!!.isVariable
+            PowType.INVALID -> throw ExpressionFormatException("cannot parse Atom expression")
         }
     }
 
-
-    public PowType getPowType() {
-        return powType;
+    init {
+        if (!TopLevelParser.stringHasValidBrackets(powString)) {
+            powType = PowType.INVALID
+        } else {
+            var isReady = initAsAtom(powString)
+            if (!isReady) {
+                isReady = initAsAtomPowFactor(powString)
+            }
+            if (!isReady) {
+                isReady = initAsAtomSqrtFactor(powString)
+            }
+            if (!isReady) {
+                powType = PowType.INVALID
+            }
+        }
     }
 }
