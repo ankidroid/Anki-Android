d5ef9e108edf894eccab942f0c97dd561acc132f
David Allison <62114487+david-allison@users.noreply.github.com>
2022-08-24 09:24:56 +0100

    refactor: Convert Replacement to Kotlin
    com.ichi2.libanki.template.Replacement

--- AnkiDroid/src/main/java/com/ichi2/libanki/template/Replacement.kt
+++ AnkiDroid/src/main/java/com/ichi2/libanki/template/Replacement.kt
@@ -14,98 +14,89 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-package com.ichi2.libanki.template;
+package com.ichi2.libanki.template
 
-import android.content.res.Resources;
+import androidx.annotation.VisibleForTesting
+import com.ichi2.anki.AnkiDroidApp
+import com.ichi2.anki.R
+import com.ichi2.libanki.template.TemplateError.FieldNotFound
+import com.ichi2.libanki.template.TemplateFilters.apply_filter
+import com.ichi2.utils.KotlinCleanup
+import java.lang.StringBuilder
+import java.util.*
+import kotlin.Throws
 
-import com.ichi2.anki.AnkiDroidApp;
-import com.ichi2.anki.R;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.VisibleForTesting;
-
-public class Replacement extends ParsedNode {
+@KotlinCleanup("IDE Lint")
+class Replacement(
     /**
      * The name of the field to show
      */
-    private final String mKey;
+    private val key: String,
     /**
      * List of filter to apply (from right to left)
      */
-    private final List<String> mFilters;
+    private val filters: List<String>,
     /**
      * The entire content between {{ and }}
      */
-    private final String mTag;
-
-    public Replacement(String key, List<String> filters, String tag) {
-        mKey = key;
-        mFilters = filters;
-        mTag = tag;
-    }
-
+    private val tag: String
+) : ParsedNode() {
     // Only used for test
     @VisibleForTesting
-    public Replacement(String key, String... filters) {
-        this(key, Arrays.asList(filters), "");
+    constructor(key: String, vararg filters: String?) : this(
+        key,
+        Arrays.asList<String?>(*filters),
+        ""
+    ) {
     }
 
-
-    @Override
-    public boolean template_is_empty(@NonNull Set<String> nonempty_fields) {
-        return !nonempty_fields.contains(mKey);
+    override fun template_is_empty(nonempty_fields: Set<String>): Boolean {
+        return !nonempty_fields.contains(key)
     }
 
-    private static String runHint(String txt, String tag) {
-        if (txt.trim().length() == 0) {
-            return "";
-        }
-        Resources res = AnkiDroidApp.getAppResources();
-        // random id
-        String domId = "hint" + txt.hashCode();
-        return "<a class=hint href=\"#\" onclick=\"this.style.display='none';document.getElementById('" +
-                domId + "').style.display='block';_relinquishFocus();return false;\">" +
-                res.getString(R.string.show_hint, tag) + "</a><div id=\"" +
-                domId + "\" class=hint style=\"display: none\">" + txt + "</div>";
-    }
-
-
-    @NonNull
-    @Override
-    public void render_into(Map<String, String> fields, Set<String> nonempty_fields, StringBuilder builder) throws TemplateError.FieldNotFound {
-        String txt = fields.get(mKey);
+    @Throws(FieldNotFound::class)
+    override fun render_into(
+        fields: Map<String, String>,
+        nonempty_fields: Set<String>,
+        builder: StringBuilder
+    ) {
+        var txt = fields[key]
         if (txt == null) {
-            if (mKey.trim().isEmpty() && !mFilters.isEmpty()) {
-                txt = "";
+            txt = if (key.trim { it <= ' ' }.isEmpty() && !filters.isEmpty()) {
+                ""
             } else {
-                throw new TemplateError.FieldNotFound(mFilters, mKey);
+                throw FieldNotFound(filters, key)
             }
         }
-        for (String filter: mFilters) {
-            txt = TemplateFilters.apply_filter(txt, filter, mKey, mTag);
+        for (filter in filters) {
+            txt = apply_filter(txt!!, filter, key, tag)
         }
-        builder.append(txt);
+        builder.append(txt)
     }
 
-    @Override
-    public boolean equals(@Nullable Object obj) {
-        if (! (obj instanceof Replacement)) {
-            return false;
+    override fun equals(other: Any?): Boolean {
+        if (other !is Replacement) {
+            return false
         }
-        Replacement other = (Replacement) obj;
-        return other.mKey.equals(mKey) && other.mFilters.equals(mFilters);
+        return other.key == key && other.filters == filters
     }
 
-    @NonNull
-    @Override
-    public String toString() {
-        return "new Replacement(\"" + mKey.replace("\\", "\\\\")+ ", " + mFilters + "\")";
+    override fun toString(): String {
+        return "new Replacement(\"" + key.replace("\\", "\\\\") + ", " + filters + "\")"
+    }
+
+    companion object {
+        private fun runHint(txt: String, tag: String): String {
+            if (txt.trim { it <= ' ' }.length == 0) {
+                return ""
+            }
+            val res = AnkiDroidApp.getAppResources()
+            // random id
+            val domId = "hint" + txt.hashCode()
+            return "<a class=hint href=\"#\" onclick=\"this.style.display='none';document.getElementById('" +
+                domId + "').style.display='block';_relinquishFocus();return false;\">" +
+                res.getString(R.string.show_hint, tag) + "</a><div id=\"" +
+                domId + "\" class=hint style=\"display: none\">" + txt + "</div>"
+        }
     }
 }
