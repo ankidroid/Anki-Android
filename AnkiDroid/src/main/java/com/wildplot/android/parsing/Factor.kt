f52f4cd9d8e36b62a13b13bb753fba51c433fb64
lukstbit <52494258+lukstbit@users.noreply.github.com>
2022-08-28 09:58:55 +0300

    refactor: Convert Factor to Kotlin
    com.wildplot.android.parsing.Factor

--- AnkiDroid/src/main/java/com/wildplot/android/parsing/Factor.kt
+++ AnkiDroid/src/main/java/com/wildplot/android/parsing/Factor.kt
@@ -13,124 +13,96 @@
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
-package com.wildplot.android.parsing;
+package com.wildplot.android.parsing
 
-
-import android.annotation.SuppressLint;
+import android.annotation.SuppressLint
 
 @SuppressLint("NonPublicNonStaticFieldName")
-public class Factor implements TreeElement {
-    private final TopLevelParser parser;
-
-
-
-    public enum FactorType {PLUS_FACTOR, MINUS_FACTOR, POW, INVALID}
-
-
-
-    private FactorType factorType = FactorType.INVALID;
-    private Factor factor;
-    private Pow pow;
-
-
-    public Factor(String factorString, TopLevelParser parser) {
-        this.parser = parser;
-        if (!TopLevelParser.stringHasValidBrackets(factorString)) {
-            this.factorType = FactorType.INVALID;
-            return;
-        }
-
-        boolean isReady = initAsPlusFactor(factorString);
-        if (!isReady) {
-            isReady = initAsMinusFactor(factorString);
-        }
-        if (!isReady) {
-            isReady = initAsPow(factorString);
-        }
-        if (!isReady) {
-            this.factorType = FactorType.INVALID;
-        }
+class Factor(factorString: String, private val parser: TopLevelParser) : TreeElement {
+    enum class FactorType {
+        PLUS_FACTOR, MINUS_FACTOR, POW, INVALID
     }
 
-
-    private boolean initAsPlusFactor(String factorString) {
-        if (factorString.length() > 0 && factorString.charAt(0) == '+') {
-            String leftSubString = factorString.substring(1);
-            Factor leftFactor = new Factor(leftSubString, parser);
-            boolean isValidFactor = leftFactor.getFactorType() != FactorType.INVALID;
+    var factorType = FactorType.INVALID
+        private set
+    private var factor: Factor? = null
+    private var pow: Pow? = null
+    private fun initAsPlusFactor(factorString: String): Boolean {
+        if (factorString.length > 0 && factorString[0] == '+') {
+            val leftSubString = factorString.substring(1)
+            val leftFactor = Factor(leftSubString, parser)
+            val isValidFactor = leftFactor.factorType != FactorType.INVALID
             if (isValidFactor) {
-                this.factorType = FactorType.PLUS_FACTOR;
-                this.factor = leftFactor;
-                return true;
+                factorType = FactorType.PLUS_FACTOR
+                factor = leftFactor
+                return true
             }
         }
-
-        return false;
+        return false
     }
 
-
-    private boolean initAsMinusFactor(String factorString) {
-        if (factorString.length() > 0 && factorString.charAt(0) == '-') {
-            String leftSubString = factorString.substring(1);
-            Factor leftFactor = new Factor(leftSubString, parser);
-            boolean isValidFactor = leftFactor.getFactorType() != FactorType.INVALID;
+    private fun initAsMinusFactor(factorString: String): Boolean {
+        if (factorString.length > 0 && factorString[0] == '-') {
+            val leftSubString = factorString.substring(1)
+            val leftFactor = Factor(leftSubString, parser)
+            val isValidFactor = leftFactor.factorType != FactorType.INVALID
             if (isValidFactor) {
-                this.factorType = FactorType.MINUS_FACTOR;
-                this.factor = leftFactor;
-                return true;
+                factorType = FactorType.MINUS_FACTOR
+                factor = leftFactor
+                return true
             }
         }
-
-        return false;
+        return false
     }
 
-
-    private boolean initAsPow(String factorString) {
-        Pow pow = new Pow(factorString, parser);
-        boolean isValidPow = pow.getPowType() != Pow.PowType.INVALID;
+    private fun initAsPow(factorString: String): Boolean {
+        val pow = Pow(factorString, parser)
+        val isValidPow = pow.powType !== Pow.PowType.INVALID
         if (isValidPow) {
-            this.factorType = FactorType.POW;
-            this.pow = pow;
-            return true;
+            factorType = FactorType.POW
+            this.pow = pow
+            return true
         }
-        return false;
+        return false
     }
 
-
-    public FactorType getFactorType() {
-        return factorType;
-    }
-
-
-    public double getValue() throws ExpressionFormatException {
-        switch (factorType) {
-            case PLUS_FACTOR:
-                return factor.getValue();
-            case MINUS_FACTOR:
-                return -factor.getValue();
-            case POW:
-                return pow.getValue();
-            case INVALID:
-            default:
-                throw new ExpressionFormatException("cannot parse expression at factor level");
-        }
-
-    }
-
-
-    @Override
-    public boolean isVariable() throws ExpressionFormatException {
-        switch (factorType) {
-            case PLUS_FACTOR:
-            case MINUS_FACTOR:
-                return factor.isVariable();
-            case POW:
-                return pow.isVariable();
-            case INVALID:
-            default:
-                throw new ExpressionFormatException("cannot parse expression at factor level");
+    // @Suppress because the IDE thinks factor.value is a recursive call, this is most likely
+    // an IDE bug because getValue() is invoked on another object(of the same type) which
+    // eventually will end in one of the two other cases(POW or INVALID)
+    @Suppress("RecursivePropertyAccessor")
+    @Throws(ExpressionFormatException::class)
+    override fun getValue(): Double {
+        return when (factorType) {
+            FactorType.PLUS_FACTOR -> factor!!.value
+            FactorType.MINUS_FACTOR -> -factor!!.value
+            FactorType.POW -> pow!!.value
+            FactorType.INVALID -> throw ExpressionFormatException("cannot parse expression at factor level")
         }
     }
 
+    @Throws(ExpressionFormatException::class)
+    override fun isVariable(): Boolean {
+        return when (factorType) {
+            FactorType.PLUS_FACTOR, FactorType.MINUS_FACTOR -> factor!!.isVariable
+            FactorType.POW -> pow!!.isVariable
+            FactorType.INVALID -> throw ExpressionFormatException("cannot parse expression at factor level")
+        }
+    }
 
+    init {
+        if (!TopLevelParser.stringHasValidBrackets(factorString)) {
+            factorType = FactorType.INVALID
+        } else {
+            var isReady = initAsPlusFactor(factorString)
+            if (!isReady) {
+                isReady = initAsMinusFactor(factorString)
+            }
+            if (!isReady) {
+                isReady = initAsPow(factorString)
+            }
+            if (!isReady) {
+                factorType = FactorType.INVALID
+            }
+        }
+    }
 }
