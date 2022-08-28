416dae285c5dfad82fbeb0da0419d3a4d1d645af
lukstbit <52494258+lukstbit@users.noreply.github.com>
2022-08-28 08:18:36 +0300

    refactor: Convert FunctionXAtom to Kotlin
    com.wildplot.android.parsing.AtomTypes.FunctionXAtom

--- AnkiDroid/src/main/java/com/wildplot/android/parsing/AtomTypes/FunctionXAtom.kt
+++ AnkiDroid/src/main/java/com/wildplot/android/parsing/AtomTypes/FunctionXAtom.kt
@@ -13,89 +13,71 @@
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
-package com.wildplot.android.parsing.AtomTypes;
+package com.wildplot.android.parsing.AtomTypes
 
-import android.annotation.SuppressLint;
-
-import com.wildplot.android.parsing.Atom;
-import com.wildplot.android.parsing.Expression;
-import com.wildplot.android.parsing.ExpressionFormatException;
-import com.wildplot.android.parsing.TopLevelParser;
-import com.wildplot.android.parsing.TreeElement;
-
-import java.util.regex.Pattern;
+import android.annotation.SuppressLint
+import com.wildplot.android.parsing.Atom.AtomType
+import com.wildplot.android.parsing.Expression
+import com.wildplot.android.parsing.ExpressionFormatException
+import com.wildplot.android.parsing.TopLevelParser
+import com.wildplot.android.parsing.TreeElement
+import java.util.regex.Pattern
 
 @SuppressLint("NonPublicNonStaticFieldName")
-public class FunctionXAtom implements TreeElement {
-    private Atom.AtomType atomType = Atom.AtomType.FUNCTION_X;
-    private final TopLevelParser parser;
-    private Expression expression;
-    private String funcName;
-
-
-    public FunctionXAtom(String funcString, TopLevelParser parser) {
-        this.parser = parser;
-
-        boolean isValid = init(funcString);
-        if (!isValid) {
-            this.atomType = Atom.AtomType.INVALID;
-        }
-    }
-
-
-    private boolean init(String funcString) {
-        int leftBracket = funcString.indexOf("(");
-        int rightBracket = funcString.lastIndexOf(")");
+class FunctionXAtom(funcString: String, private val parser: TopLevelParser) : TreeElement {
+    var atomType = AtomType.FUNCTION_X
+        private set
+    private var expression: Expression? = null
+    private var funcName: String? = null
+    private fun init(funcString: String): Boolean {
+        val leftBracket = funcString.indexOf("(")
+        val rightBracket = funcString.lastIndexOf(")")
         if (leftBracket > 1 && rightBracket > leftBracket + 1) {
-            String funcName = funcString.substring(0, leftBracket);
-
-            Pattern p = Pattern.compile("[^a-zA-Z0-9]");
-            boolean hasSpecialChar = p.matcher(funcName).find();
-            if (!hasSpecialChar && (funcName.length() > 0)) {
-                String expressionString = funcString.substring(leftBracket + 1, rightBracket);
-                Expression expressionInBrackets = new Expression(expressionString, parser);
-                boolean isValidExpression = expressionInBrackets.getExpressionType() != Expression.ExpressionType.INVALID;
+            val funcName = funcString.substring(0, leftBracket)
+            val p = Pattern.compile("[^a-zA-Z0-9]")
+            val hasSpecialChar = p.matcher(funcName).find()
+            if (!hasSpecialChar && funcName.length > 0) {
+                val expressionString = funcString.substring(leftBracket + 1, rightBracket)
+                val expressionInBrackets = Expression(expressionString, parser)
+                val isValidExpression =
+                    expressionInBrackets.expressionType != Expression.ExpressionType.INVALID
                 if (isValidExpression) {
-                    this.atomType = Atom.AtomType.FUNCTION_X;
-                    this.funcName = funcName;
-                    this.expression = expressionInBrackets;
-                    return true;
+                    atomType = AtomType.FUNCTION_X
+                    this.funcName = funcName
+                    expression = expressionInBrackets
+                    return true
                 }
             } else {
-                this.atomType = Atom.AtomType.INVALID;
-                return false;
+                atomType = AtomType.INVALID
+                return false
             }
-
         }
-
-        return false;
+        return false
     }
 
-
-    @Override
-    public double getValue() throws ExpressionFormatException {
-        if (atomType != Atom.AtomType.INVALID) {
-
-            return parser.getFuncVal(funcName, expression.getValue());
+    @Throws(ExpressionFormatException::class)
+    override fun getValue(): Double {
+        return if (atomType !== AtomType.INVALID) {
+            parser.getFuncVal(funcName, expression!!.value)
         } else {
-            throw new ExpressionFormatException("Number is Invalid, cannot parse");
+            throw ExpressionFormatException("Number is Invalid, cannot parse")
         }
     }
 
-
-    @Override
-    public boolean isVariable() throws ExpressionFormatException {
-        //TODO check how changed related function definitions are handled
-        if (atomType != Atom.AtomType.INVALID) {
-
-            return expression.isVariable();
+    @Throws(ExpressionFormatException::class)
+    override fun isVariable(): Boolean {
+        // TODO check how changed related function definitions are handled
+        return if (atomType !== AtomType.INVALID) {
+            expression!!.isVariable
         } else {
-            throw new ExpressionFormatException("Number is Invalid, cannot parse");
+            throw ExpressionFormatException("Number is Invalid, cannot parse")
         }
     }
 
-
-    public Atom.AtomType getAtomType() {
-        return atomType;
+    init {
+        val isValid = init(funcString)
+        if (!isValid) {
+            atomType = AtomType.INVALID
+        }
     }
 }
