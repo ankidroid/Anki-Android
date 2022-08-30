da1917509c65f00282e2402bcc803f86913f0a0c
sairaj <sairaj.b2249@gmail.com>
2022-08-30 09:35:14 +0530

    "Used showSnackbar(), removed suprressing lint, added informative comment"
    The backendUndoAndShowPopup() was showing the undo snackbar on top of the answer button but now this issue has been fixed by changing the position of the snackbar using the setanchorview method to make a Snackbar appear above a specific view.

--- AnkiDroid/src/main/java/com/ichi2/anki/BackendUndo.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/BackendUndo.kt
@@ -16,17 +16,16 @@
 
 package com.ichi2.anki
 
-import android.annotation.SuppressLint
 import android.widget.Button
 import androidx.fragment.app.FragmentActivity
 import com.google.android.material.snackbar.Snackbar
 import com.ichi2.anki.CollectionManager.TR
+import com.ichi2.anki.snackbar.showSnackbar
 import com.ichi2.libanki.undoNew
 import com.ichi2.libanki.undoableOp
 import com.ichi2.utils.BlocksSchemaUpgrade
 import net.ankiweb.rsdroid.BackendException
 
-@SuppressLint("DirectSnackbarMakeUsage")
 suspend fun FragmentActivity.backendUndoAndShowPopup(): Boolean {
     return try {
         val changes = withProgress() {
@@ -34,10 +33,13 @@ suspend fun FragmentActivity.backendUndoAndShowPopup(): Boolean {
                 undoNew()
             }
         }
-        val ans_button = findViewById(R.id.flip_card) as Button
-        val snackbar = Snackbar.make(findViewById(android.R.id.content), TR.undoActionUndone(changes.operation), Snackbar.LENGTH_LONG)
-        snackbar.setAnchorView(ans_button) // sets the anchorview as answerbutton
-        snackbar.show()
+
+        showSnackbar(TR.undoActionUndone(changes.operation), Snackbar.LENGTH_LONG) {
+
+            val ans_button = findViewById(R.id.flip_card) as Button
+            this.setAnchorView(ans_button) /* A snackbar may obscure vital elements on some screens (e.g: the answer buttons on the Reviewer)
+                                               To fix this, a view may be [tagged/provided with an ID], and the snackbar will be displayed above it */
+        }
 
         true
     } catch (exc: BackendException) {
