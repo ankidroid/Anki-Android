8d3c44be1af69665fc0e1e19778a3cc380ac0fe5
sairaj <sairaj.b2249@gmail.com>
2022-09-02 08:24:17 +0530

    "removed length_long argument"

--- AnkiDroid/src/main/java/com/ichi2/anki/AbstractFlashcardViewer.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/AbstractFlashcardViewer.kt
@@ -845,7 +845,7 @@ abstract class AbstractFlashcardViewer :
                 legacyUndo()
             } else {
                 return launchCatchingTask {
-                    // TODO make backendUndoAndShowPopup() snackbar don't show on top of the answer buttons
+
                     if (!backendUndoAndShowPopup(findViewById(R.id.flip_card))) {
                         legacyUndo()
                     }
