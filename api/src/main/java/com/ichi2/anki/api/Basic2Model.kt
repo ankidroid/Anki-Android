a45f795ee7c59e3f2e4b9beb2bc0e54b399f4672
lukstbit <52494258+lukstbit@users.noreply.github.com>
2022-09-14 19:13:42 +0300

    Set Basic2Model as internal to follow original visibility

--- api/src/main/java/com/ichi2/anki/api/Basic2Model.kt
+++ api/src/main/java/com/ichi2/anki/api/Basic2Model.kt
@@ -4,7 +4,7 @@ package com.ichi2.anki.api
 /**
  * Definitions of the basic with reverse model
  */
-object Basic2Model {
+internal object Basic2Model {
     @JvmField // required for Java API
     val FIELDS = arrayOf("Front", "Back")
     // List of card names that will be used in AnkiDroid (one for each direction of learning)
@@ -12,9 +12,9 @@ object Basic2Model {
     val CARD_NAMES = arrayOf("Card 1", "Card 2")
     // Template for the question of each card
     @JvmField // required for Java API
-    val QFMT = arrayOf("{{Front}}", "{{Back}}")
+    internal val QFMT = arrayOf("{{Front}}", "{{Back}}")
     @JvmField // required for Java API
-    val AFMT = arrayOf(
+    internal val AFMT = arrayOf(
         "{{FrontSide}}\n\n<hr id=\"answer\">\n\n{{Back}}",
         "{{FrontSide}}\n\n<hr id=\"answer\">\n\n{{Front}}"
     )
