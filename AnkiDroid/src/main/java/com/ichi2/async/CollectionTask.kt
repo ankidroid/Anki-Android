64bba8b280d78e9e94e330c3b2e49d9b1efa274b
Divyansh Kushwaha <kushwaha.divyansh.dxn@gmail.com>
2022-09-23 23:23:30 +0530

    Removed unused class SearchNotes

--- AnkiDroid/src/main/java/com/ichi2/async/CollectionTask.kt
+++ AnkiDroid/src/main/java/com/ichi2/async/CollectionTask.kt
@@ -29,7 +29,6 @@ import com.ichi2.anki.StudyOptionsFragment.DeckStudyData
 import com.ichi2.anki.exception.ConfirmModSchemaException
 import com.ichi2.anki.exception.ImportExportException
 import com.ichi2.anki.servicelayer.NoteService
-import com.ichi2.anki.servicelayer.SearchService.SearchCardsResult
 import com.ichi2.libanki.*
 import com.ichi2.libanki.Collection
 import com.ichi2.libanki.Collection.CheckDatabaseResult
@@ -37,7 +36,6 @@ import com.ichi2.libanki.importer.AnkiPackageImporter
 import com.ichi2.libanki.sched.DeckDueTreeNode
 import com.ichi2.libanki.sched.TreeNode
 import com.ichi2.utils.*
-import net.ankiweb.rsdroid.BackendFactory
 import org.apache.commons.compress.archivers.zip.ZipFile
 import timber.log.Timber
 import java.io.File
@@ -435,63 +433,6 @@ open class CollectionTask<Progress, Result>(val task: TaskDelegateBase<Progress,
             // mark undo for all at once
             col.markUndo(changeDeckMulti)
             return true
-        }
-    }
-
-    class SearchNotes(private val query: String, private val order: SortOrder, private val numCardsToRender: Int, private val column1Index: Int, private val column2Index: Int) : TaskDelegate<List<CardCache>, SearchCardsResult>() {
-        override fun task(col: Collection, collectionTask: ProgressSenderAndCancelListener<List<CardCache>>): SearchCardsResult {
-            Timber.d("doInBackgroundSearchCards")
-            if (collectionTask.isCancelled()) {
-                Timber.d("doInBackgroundSearchCards was cancelled so return null")
-                return SearchCardsResult.invalidResult()
-            }
-            val searchResult: MutableList<CardCache> = ArrayList()
-            val searchResult_: List<Long>
-
-            if (BackendFactory.defaultLegacySchema) {
-                searchResult_ = try {
-                    col.findOneCardByNote(query).requireNoNulls()
-                } catch (e: Exception) {
-                    // exception can occur via normal operation
-                    Timber.w(e)
-                    return SearchCardsResult.error(e)
-                }
-                Timber.d("The search found %d cards", searchResult_.size)
-                var position = 0
-                for (cid in searchResult_) {
-                    val card = CardCache(cid, col, position++, true)
-                    searchResult.add(card)
-                }
-            } else {
-                searchResult_ = try {
-                    col.findNotes(query, order).requireNoNulls()
-                } catch (e: Exception) {
-                    // exception can occur via normal operation
-                    Timber.w(e)
-                    return SearchCardsResult.error(e)
-                }
-                Timber.d("The search found %d notes", searchResult_.size)
-                var position = 0
-                for (nid in searchResult_) {
-                    val card = CardCache(Note(col, nid).firstCard().id, col, position++, false)
-                    searchResult.add(card)
-                }
-            }
-            // Render the first few items
-            for (i in 0 until Math.min(numCardsToRender, searchResult.size)) {
-                if (collectionTask.isCancelled()) {
-                    Timber.d("doInBackgroundSearchCards was cancelled so return null")
-                    return SearchCardsResult.invalidResult()
-                }
-                searchResult[i].load(false, column1Index, column2Index)
-            }
-            // Finish off the task
-            return if (collectionTask.isCancelled()) {
-                Timber.d("doInBackgroundSearchCards was cancelled so return null")
-                SearchCardsResult.invalidResult()
-            } else {
-                SearchCardsResult.success(searchResult)
-            }
         }
     }
 
