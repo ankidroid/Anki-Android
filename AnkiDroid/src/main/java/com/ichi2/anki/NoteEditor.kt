4a89a581dcee78a9069c80acdb85dc601e0d3709
SySagar <sysagar07@gmail.com>
2022-09-27 12:20:11 +0530

    redundant pin removed from noteeditor

--- AnkiDroid/src/main/java/com/ichi2/anki/NoteEditor.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/NoteEditor.kt
@@ -1324,9 +1324,13 @@ class NoteEditor : AnkiActivity(), DeckSelectionListener, SubtitleListener, Tags
                 // Use media editor button if not changing note type
                 mediaButton!!.setBackgroundResource(icons[0])
                 setMMButtonListener(mediaButton, i)
-                // toggle sticky button
-                toggleStickyButton!!.setBackgroundResource(icons[2])
-                setToggleStickyButtonListener(toggleStickyButton, i)
+
+                // fixed issue #12475
+                if (addNote) {
+                    toggleStickyButton!!.setBackgroundResource(icons[2])
+                    setToggleStickyButtonListener(toggleStickyButton, i)
+                } else
+                    toggleStickyButton!!.setBackgroundResource(0)
             }
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
                 previous.lastViewInTabOrder!!.nextFocusForwardId = R.id.CardEditorTagButton
