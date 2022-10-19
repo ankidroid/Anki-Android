e1f0e686fec5731c9d3cf08e43505ac308b461b7
oyeraghib <raghib.khan76@gmail.com>
2022-10-19 17:35:56 +0530

    update NoteEditor.kt with `closeNoteDialog` function

--- AnkiDroid/src/main/java/com/ichi2/anki/NoteEditor.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/NoteEditor.kt
@@ -1052,7 +1052,7 @@ class NoteEditor : AnkiActivity(), DeckSelectionListener, SubtitleListener, Tags
     }
 
     private fun showDiscardChangesDialog() {
-        DiscardChangesDialog.showDialog(this) {
+        DiscardChangesDialog.closeNoteDialog(this) {
             Timber.i("NoteEditor:: OK button pressed to confirm discard changes")
             closeNoteEditor()
         }
