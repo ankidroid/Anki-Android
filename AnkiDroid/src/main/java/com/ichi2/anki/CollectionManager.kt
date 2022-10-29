ffe0bf23d59513d59210a2539d9e5bc23cf345cd
oakkitten <oakkitten@users.noreply.github.com>
2022-10-29 20:39:48 +0100

    Create & use a call-site withQueue
    `withQueue`, like `withCol`, changes the thread, and it might be more
    prudent to let the logic that's closer to business to decide how and
    when to change threads.
    
    This creates a public `withQueue` that calls a block with a special,
    publicly inaccessible, instance of `Queue` as a context receiver,
    which makes it possible to define functions that can only be run inside
    of `withQueue`.
    
    See also:
    https://github.com/ankidroid/Anki-Android/pull/12371#discussion_r990797635

--- AnkiDroid/src/main/java/com/ichi2/anki/CollectionManager.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/CollectionManager.kt
@@ -51,8 +51,6 @@ object CollectionManager {
      */
     private var collection: Collection? = null
 
-    private var queue: CoroutineDispatcher = Dispatchers.IO.limitedParallelism(1)
-
     private val robolectric = "robolectric" == Build.FINGERPRINT
 
     @VisibleForTesting
@@ -67,11 +65,10 @@ object CollectionManager {
      * TODO Allow suspendable blocks, rely on locking instead.
      *
      * TODO Disallow running functions that are supposed to be run inside the queue outside of it.
-     *   For instance, this can be done by marking the [block] with a context
-     *   that cannot be instantiated outside of this class.
+     *   See [com.ichi2.anki.withQueue]
      */
     private suspend fun<T> withQueue(block: CollectionManager.() -> T): T {
-        return withContext(queue) {
+        return withContext(collectionOperationsDispatcher) {
             this@CollectionManager.block()
         }
     }
@@ -216,12 +213,9 @@ object CollectionManager {
         }
     }
 
-    // TODO Move withQueue to call site
-    suspend fun deleteCollectionDirectory() {
-        withQueue {
-            ensureClosedInner(save = false)
-            getCollectionDirectory().deleteRecursively()
-        }
+    context(Queue) fun deleteCollectionDirectory() {
+        ensureClosedInner(save = false)
+        getCollectionDirectory().deleteRecursively()
     }
 
     fun getCollectionDirectory() =
@@ -380,6 +374,39 @@ object CollectionManager {
     fun setTestDispatcher(dispatcher: CoroutineDispatcher) {
         // note: we avoid the call to .limitedParallelism() here,
         // as it does not seem to be compatible with the test scheduler
-        queue = dispatcher
+        collectionOperationsDispatcher = dispatcher
+    }
+}
+
+private var collectionOperationsDispatcher = Dispatchers.IO.limitedParallelism(1)
+
+/**
+ * A marker for functions that can only be run inside [withQueue], for instance,
+ *
+ *     context(Queue) fun foo() {}
+ */
+class Queue private constructor() {
+    companion object { init { queue = Queue() } }
+}
+
+private lateinit var queue: Queue
+
+// This is silly, but I can't think of a better way to have a public Queue
+// and a private instance of it
+@Suppress("unused") private val forceQueueInitialization = Queue.Companion
+
+/**
+ * Like [CollectionManager.withQueue], but also allows running functions marked with [Queue].
+ * Experimental.
+ *
+ *     foo() // Error: No required context receiver found ...
+ *
+ *     withQueue {
+ *         foo() // Ok
+ *     }
+ */
+suspend fun<T> withQueue(block: context(Queue) () -> T): T {
+    return withContext(collectionOperationsDispatcher) {
+        block(queue)
     }
 }
