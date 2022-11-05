8e651676e2dfbb706e6df4cbf0b5fdebe94fe116
Nishant <getrooted0019@hotmail.com>
2022-11-05 08:03:02 +0530

    Fixed getPackageInfo deprecation in ActivityStartupMetaTest

--- AnkiDroid/src/test/java/com/ichi2/anki/ActivityStartupMetaTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/ActivityStartupMetaTest.kt
@@ -18,6 +18,7 @@ package com.ichi2.anki
 import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.compat.CompatHelper.Companion.getPackageInfoCompat
 import com.ichi2.testutils.ActivityList
 import com.ichi2.testutils.ActivityList.ActivityLaunchParam
 import com.ichi2.utils.KotlinCleanup
@@ -34,13 +35,11 @@ class ActivityStartupMetaTest : RobolectricTest() {
     @Test
     @Throws(PackageManager.NameNotFoundException::class)
     @KotlinCleanup("remove throws; remove stream(), remove : String")
-    @Suppress("deprecation") // getPackageInfo
     fun ensureAllActivitiesAreTested() {
         // if this fails, you may need to add the missing activity to ActivityList.allActivitiesAndIntents()
 
         // we can't access this in a static context
-        val pm = targetContext.packageManager
-        val packageInfo = pm.getPackageInfo(targetContext.packageName, PackageManager.GET_ACTIVITIES)
+        val packageInfo = targetContext.getPackageInfoCompat(targetContext.packageName, PackageManager.GET_ACTIVITIES)
         val manifestActivities = packageInfo.activities
         val testedActivityClassNames = ActivityList.allActivitiesAndIntents().stream().map { obj: ActivityLaunchParam -> obj.className }.collect(Collectors.toSet())
         val manifestActivityNames = Arrays.stream(manifestActivities)
