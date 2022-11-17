a601fa12a1c69420ae723a607cc36102341d8bb6
Sanjaykumar Sargam <sargamsanjaykumar@gmail.com>
2022-11-17 20:32:31 +0530

    Used triple quote

--- api/src/main/java/com/ichi2/anki/api/Basic2Model.kt
+++ api/src/main/java/com/ichi2/anki/api/Basic2Model.kt
@@ -15,7 +15,17 @@ internal object Basic2Model {
     internal val QFMT = arrayOf("{{Front}}", "{{Back}}")
     @JvmField // required for Java API
     internal val AFMT = arrayOf(
-        "{{FrontSide}}\n\n<hr id=\"answer\">\n\n{{Back}}",
-        "{{FrontSide}}\n\n<hr id=\"answer\">\n\n{{Front}}"
+        """{{FrontSide}}
+    
+    |<hr id="answer">
+    
+    |{{Back}}
+        """.trimMargin(),
+        """{{FrontSide}}
+    
+    |<hr id="answer">
+    
+    |{{Front}}
+        """.trimMargin()
     )
 }
