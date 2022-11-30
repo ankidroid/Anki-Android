bff3cea57e5155fa105861914394177d1747b8df
RobozinhoD <65715921+RobozinhoD@users.noreply.github.com>
2022-11-30 07:15:43 -0300

    Remove restartActivity()
    Replaced by recreate()

--- AnkiDroid/src/main/java/com/ichi2/anki/NavigationDrawerActivity.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/NavigationDrawerActivity.kt
@@ -234,7 +234,7 @@ abstract class NavigationDrawerActivity :
             // because onDestroy() of old Activity interferes with TTS in new Activity
             finishWithoutAnimation()
         } else {
-            restartActivity()
+            recreate()
         }
     }
 
