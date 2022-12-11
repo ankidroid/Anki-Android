074fb3c3f73145d20f635580f52cd6a4a70aaca9
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-12-11 17:54:01 +0530

    revert formatting changes

--- AnkiDroid/src/main/java/com/ichi2/anki/cardviewer/CardAppearance.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/cardviewer/CardAppearance.kt
@@ -23,12 +23,7 @@ import com.ichi2.themes.Theme
 import com.ichi2.themes.Themes.currentTheme
 
 /** Responsible for calculating CSS and element styles and modifying content on a flashcard  */
-class CardAppearance(
-    private val customFonts: ReviewerCustomFonts,
-    private val cardZoom: Int,
-    private val imageZoom: Int,
-    private val centerVertically: Boolean
-) {
+class CardAppearance(private val customFonts: ReviewerCustomFonts,private val cardZoom: Int,private val imageZoom: Int,private val centerVertically: Boolean) {
     /** Below could be in a better abstraction.  */
     fun appendCssStyle(style: StringBuilder) {
         // Zoom cards
