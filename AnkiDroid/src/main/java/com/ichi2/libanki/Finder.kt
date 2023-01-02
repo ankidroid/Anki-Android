bade907f7117254b0f3cb61fa944ec9702345192
AdityaKumar <adi54321kumar@outlook.com>
2023-01-02 17:01:45 +0530

    Reviewed Finder file

--- AnkiDroid/src/main/java/com/ichi2/libanki/Finder.kt
+++ AnkiDroid/src/main/java/com/ichi2/libanki/Finder.kt
@@ -217,7 +217,7 @@ class Finder(private val col: Collection) {
             // failed command?
             @Suppress("NAME_SHADOWING")
             var txt = txt
-            if (txt?.isEmpty() == true) {
+            if (txt.isNullOrEmpty()) {
                 // if it was to be negated then we can just ignore it
                 if (isnot) {
                     isnot = false
