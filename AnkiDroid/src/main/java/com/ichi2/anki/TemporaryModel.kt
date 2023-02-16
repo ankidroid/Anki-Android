b8d3230bc9b3877211cb835ef230afb807a9415c
Abhishek kumar patel <abhishek835.hitit2020@gmail.com>
2023-02-16 16:48:57 +0530

    Kotlin Cleanup#10489 changed getSerializable to getSerializableWithCast()

--- AnkiDroid/src/main/java/com/ichi2/anki/TemporaryModel.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/TemporaryModel.kt
@@ -22,6 +22,7 @@ import com.ichi2.async.saveModel
 import com.ichi2.compat.CompatHelper.Companion.compat
 import com.ichi2.libanki.Model
 import com.ichi2.libanki.NoteTypeId
+import com.ichi2.utils.BundleUtils.getSerializableWithCast
 import com.ichi2.utils.KotlinCleanup
 import org.json.JSONObject
 import timber.log.Timber
@@ -55,9 +56,8 @@ class TemporaryModel(model: Model) {
     private fun loadTemplateChanges(bundle: Bundle) {
         try {
             @Suppress("UNCHECKED_CAST")
-            @KotlinCleanup("use bundle.getSerializableWithCast() to improve nullability")
             mTemplateChanges =
-                (bundle.getSerializable("mTemplateChanges") as ArrayList<Array<Any>>)
+                (bundle.getSerializableWithCast("mTemplateChanges") as ArrayList<Array<Any>>)
         } catch (e: ClassCastException) {
             Timber.e(e, "Unexpected cast failure")
         }
