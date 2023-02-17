6da4ef0d565f89c52fec491fa6a42e37f35b01da
David Allison <62114487+david-allison@users.noreply.github.com>
2023-02-17 17:06:22 +0000

    remove additional JSON tests
    From code review, in the 'Ankidroid specific test' section
    
    ----
    
    The JSONObject class was removed in c7376f63b692b584d5bd9f864619c84cda4049b7
    
    No point in testing framework methods

--- AnkiDroid/src/test/java/com/ichi2/utils/JSONObjectTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/utils/JSONObjectTest.kt
@@ -19,16 +19,12 @@ import android.annotation.SuppressLint
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.ichi2.testutils.EmptyApplication
 import junit.framework.TestCase.*
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.core.IsNull.notNullValue
-import org.json.JSONException
 import org.json.JSONObject
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
-import kotlin.test.assertFailsWith
 
 /**
  * This black box test was written without inspecting the non-free org.json sourcecode.
@@ -37,16 +33,11 @@ import kotlin.test.assertFailsWith
 @Config(application = EmptyApplication::class)
 @SuppressLint("CheckResult") // many usages: checking exceptions
 class JSONObjectTest {
-    private val mEmptyJson = "{}"
     private val mCorrectJsonBasic = "{\"key1\":\"value1\"}"
     private val mCorrectJsonNested = "{\"key1\":{\"key1a\":\"value1a\",\"key1b\":\"value1b\"},\"key2\":\"value2\"}"
     private val mCorrectJsonWithArray = "{\"key1\":\"value1\",\"key2\":[{\"key2a\":\"value2a\"},{\"key2b\":\"value2b\"}],\"key3\":\"value3\"}"
     private val mCorrectJsonNestedWithArray = "{\"key1\":{\"key1a\":\"value1a\",\"key1b\":\"value1b\"},\"key2\":[{\"key2a\":\"value2a\"},{\"key2b\":\"value2b\"}],\"key3\":\"value3\"}"
-    private val mNoOpeningBracket = "\"key1\":\"value1\"}"
-    private val mExtraOpeningBracket = "{{\"key1\": \"value1\"}"
-    private val mNoClosingBracket = "{\"key1\":value1"
-    private val mWrongKeyValueSeparator = "{\"key1\":\"value1\",\"key2\" \"value2\"}"
-    private val mDuplicateKey = "{\"key1\":\"value1\",\"key1\":\"value2\"}"
+
     private lateinit var mCorrectJsonObjectBasic: JSONObject
     private lateinit var mCorrectJsonObjectNested: JSONObject
     private lateinit var mCorrectJsonObjectWithArray: JSONObject
@@ -66,53 +57,10 @@ class JSONObjectTest {
     }
 
     @Test
-    fun objectNullIsNotNull() {
-        // #6289
-        assertThat(JSONObject.NULL, notNullValue())
-    }
-
-    @Test
-    fun formatTest() {
-        // Correct formats
-        JSONObject(mCorrectJsonBasic)
-        JSONObject(mCorrectJsonNested)
-        JSONObject(mCorrectJsonWithArray)
-        JSONObject(mEmptyJson)
-
-        // Incorrect formats
-        Assert.assertThrows(JSONException::class.java) { JSONObject(mNoOpeningBracket) }
-        Assert.assertThrows(JSONException::class.java) { JSONObject(mExtraOpeningBracket) }
-        Assert.assertThrows(JSONException::class.java) { JSONObject(mNoClosingBracket) }
-        Assert.assertThrows(JSONException::class.java) { JSONObject(mWrongKeyValueSeparator) }
-        // Assert.assertThrows(JSONException::class.java) { JSONObject(mDuplicateKey) }
-    }
-
-    @Test
     fun copyJsonTest() {
         Assert.assertEquals(mCorrectJsonObjectBasic.toString(), mCorrectJsonObjectBasic.deepClone().toString())
         Assert.assertEquals(mCorrectJsonObjectNested.toString(), mCorrectJsonObjectNested.deepClone().toString())
         Assert.assertEquals(mCorrectJsonObjectWithArray.toString(), mCorrectJsonObjectWithArray.deepClone().toString())
-    }
-
-    fun getTest() {
-        val correctJsonObjectBasicCopy = JSONObject(mCorrectJsonBasic)
-        correctJsonObjectBasicCopy.put("int-key", 2)
-        correctJsonObjectBasicCopy.put("int_key", 6)
-        correctJsonObjectBasicCopy.put("long_key", 2L)
-        correctJsonObjectBasicCopy.put("double_key", 2.0)
-        correctJsonObjectBasicCopy.putOpt("boolean_key", true)
-        correctJsonObjectBasicCopy.putOpt("object_key", mCorrectJsonBasic)
-
-        Assert.assertEquals(6, correctJsonObjectBasicCopy.getInt("int_key").toLong())
-        Assert.assertEquals(2L, correctJsonObjectBasicCopy.getLong("long_key"))
-        Assert.assertEquals(2.0, correctJsonObjectBasicCopy.getDouble("double_key"), 1e-10)
-        Assert.assertTrue(correctJsonObjectBasicCopy.getBoolean("boolean_key"))
-        Assert.assertEquals(mCorrectJsonBasic, correctJsonObjectBasicCopy["object_key"])
-
-        // Check that putOpt doesn't add pair when one is null
-        correctJsonObjectBasicCopy.putOpt("boolean_key_2", null)
-        Assert.assertFalse(correctJsonObjectBasicCopy.has("boolean_key_2"))
-        Assert.assertThrows(JSONException::class.java) { correctJsonObjectBasicCopy["boolean_key_2"] }
     }
 
     private class JSONObjectSubType : JSONObject() {
@@ -151,23 +99,6 @@ class JSONObjectTest {
         val fromMapJsonObject = fromMap(booleanMap)
         for (i in 0..9) {
             Assert.assertEquals(fromMapJsonObject.getBoolean("key$i"), i % 2 == 0)
-        }
-    }
-    /**
-     * Tests that exception is caught in the catch statement
-     */
-    @Test
-    fun testGetThrows() {
-        val testObject = JSONObject()
-        assertFailsWith<JSONException> { testObject.getBoolean("key") }
-        assertFailsWith<JSONException> {
-            testObject.getInt("key")
-        }
-        assertFailsWith<JSONException> {
-            testObject.getLong("key")
-        }
-        assertFailsWith<JSONException> {
-            testObject.getString("key")
         }
     }
 
