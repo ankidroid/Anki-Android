fbd12f41d31832a782cd9aa37a1fa57793f1167a
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2023-02-19 16:06:03 -0300

    NF: remove trimRight method
    same as Kotlin's trimEnd

--- AnkiDroid/src/main/java/com/ichi2/libanki/Sound.kt
+++ AnkiDroid/src/main/java/com/ichi2/libanki/Sound.kt
@@ -34,7 +34,6 @@ import com.ichi2.anki.ReadText
 import com.ichi2.compat.CompatHelper
 import com.ichi2.utils.DisplayUtils
 import com.ichi2.utils.KotlinCleanup
-import com.ichi2.utils.StringUtil.trimRight
 import net.ankiweb.rsdroid.BackendFactory.defaultLegacySchema
 import timber.log.Timber
 import java.lang.ref.WeakReference
@@ -588,7 +587,7 @@ class Sound {
             return if (hasURIScheme(trimmedSound)) {
                 trimmedSound
             } else {
-                soundDir + Uri.encode(trimRight(sound))
+                soundDir + Uri.encode(sound.trimEnd())
             }
         }
 
