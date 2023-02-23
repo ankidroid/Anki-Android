ankidroid/Anki-Android

commit da96237d3c48c80a3db5940db593f33d16f829e2

create `exportSelectedAsApkg` to export selected cards and notes using the new backend schema

AnkiDroid/src/main/java/com/ichi2/anki/dialogs/ExportDialog.kt
@@ -18,10 +18,13 @@ package com.ichi2.anki.dialogs
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import anki.import_export.ExportLimit
+import anki.import_export.exportLimit
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.list.listItemsMultiChoice
 import com.ichi2.anki.R
 import com.ichi2.anki.analytics.AnalyticsDialogFragment
+import com.ichi2.annotations.NeedsTest
 import com.ichi2.libanki.DeckId
 import com.ichi2.utils.BundleUtils.getNullableLong
 import com.ichi2.utils.contentNullable
@@ -30,6 +33,7 @@ class ExportDialog(private val listener: ExportDialogListener) : AnalyticsDialog
     interface ExportDialogListener {
         fun exportColAsApkg(path: String?, includeSched: Boolean, includeMedia: Boolean)
         fun exportDeckAsApkg(path: String?, did: DeckId, includeSched: Boolean, includeMedia: Boolean)
+        fun exportSelectedAsApkg(path: String?, limit: ExportLimit, includeSched: Boolean, includeMedia: Boolean)
         fun dismissAllDialogFragments()
     }
 
@@ -58,8 +62,11 @@ class ExportDialog(private val listener: ExportDialogListener) : AnalyticsDialog
         super.onCreate(savedInstanceState)
         val res = resources
         val did = getNullableLong(arguments, "did")
+        val cardIds = arguments?.getLongArray("cardIds")?.toList()
+        val noteIds = arguments?.getLongArray("noteIds")?.toList()
         val checked: Array<Int>
-        if (did != null) {
+        @NeedsTest("Test that correct options are checked given different arguments")
+        if (did != null || cardIds == null || noteIds == null) {
             mIncludeSched = false
             checked = arrayOf()
         } else {
@@ -76,6 +83,15 @@ class ExportDialog(private val listener: ExportDialogListener) : AnalyticsDialog
             positiveButton(android.R.string.ok) {
                 if (did != null) {
                     listener.exportDeckAsApkg(null, did, mIncludeSched, mIncludeMedia)
+                } else if (cardIds != null || noteIds != null) {
+                    val limit = exportLimit {
+                        if (cardIds != null) {
+                            this.cardIds = this.cardIds.toBuilder().addAllCids(cardIds).build()
+                        } else {
+                            this.noteIds = this.noteIds.toBuilder().addAllNoteIds(noteIds).build()
+                        }
+                    }
+                    listener.exportSelectedAsApkg(null, limit, mIncludeSched, mIncludeMedia)
                 } else {
                     listener.exportColAsApkg(null, mIncludeSched, mIncludeMedia)
                 }

AnkiDroid/src/main/java/com/ichi2/anki/export/ActivityExportingDelegate.kt
@@ -143,6 +143,15 @@ class ActivityExportingDelegate(private val activity: AnkiActivity, private val 
         }
     }
 
+    /**
+     * Export selected cards or notes using the new backend
+     * TODO: Once new backend is default, exportColAsApkg and exportDeckAsApkg can be merged into this function
+     */
+    override fun exportSelectedAsApkg(path: String?, limit: ExportLimit, includeSched: Boolean, includeMedia: Boolean) {
+        val exportPath = getExportFileName(limit, path, includeSched)
+        exportNewBackendApkg(exportPath, includeSched, includeMedia, limit)
+    }
+
     private fun exportApkgLegacy(exportPath: File, did: DeckId?, includeSched: Boolean, includeMedia: Boolean) {
         val exportListener = ExportListener(activity, mDialogsFactory)
         TaskManager.launchCollectionTask(

commit db7ada3bd0a57f3e8f71eec53a803df02945f620

fix: 'include Scheduling' & refactor exporting
* Fixed 'include Scheduling' for collection export
  * Previously was not set
  * Broken in da96237d3c48c80a3db5940db593f33d16f829e2
* Add Tests
* Split out ExportType
* Split out ExportDialogParams
* remove isCardList parameter
* remove Hungarian notation
* Removes 3 `showExportDialog`/`withArguments` methods
  which all do similar but different things based on the intent

I don't believe `@Parcelize` handles the inheritance of `ExportType`
yet, but this will reduce code (`appendToBundle`) when it does.

Future:
I will add additional parameters to a request to `ExportCollection`:
  * includeScheduling
  * includeMedia

This structure will reduce the complexity

AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.kt
@@ -56,6 +56,7 @@ import com.ichi2.anki.dialogs.tags.TagsDialog
 import com.ichi2.anki.dialogs.tags.TagsDialogFactory
 import com.ichi2.anki.dialogs.tags.TagsDialogListener
 import com.ichi2.anki.export.ActivityExportingDelegate
+import com.ichi2.anki.export.ExportType
 import com.ichi2.anki.receiver.SdCardReceiver
 import com.ichi2.anki.servicelayer.CardService.selectedNoteIds
 import com.ichi2.anki.servicelayer.NoteService.isMarked
@@ -1376,12 +1377,20 @@ open class CardBrowser :
         }
 
         if (inCardsMode) {
-            val msg = resources.getQuantityString(R.plurals.confirm_apkg_export_selected_cards, selectedCardIds.size, selectedCardIds.size)
-            mExportingDelegate.showExportDialog(msg, selectedCardIds, inCardsMode)
+            mExportingDelegate.showExportDialog(
+                ExportDialogParams(
+                    message = resources.getQuantityString(R.plurals.confirm_apkg_export_selected_cards, selectedCardIds.size, selectedCardIds.size),
+                    exportType = ExportType.ExportCards(selectedCardIds)
+                )
+            )
         } else {
             val selectedNoteIds = selectedNoteIds(selectedCardIds, col)
-            val msg = resources.getQuantityString(R.plurals.confirm_apkg_export_selected_notes, selectedNoteIds.size, selectedNoteIds.size)
-            mExportingDelegate.showExportDialog(msg, selectedNoteIds, inCardsMode)
+            mExportingDelegate.showExportDialog(
+                ExportDialogParams(
+                    message = resources.getQuantityString(R.plurals.confirm_apkg_export_selected_notes, selectedNoteIds.size, selectedNoteIds.size),
+                    exportType = ExportType.ExportNotes(selectedNoteIds)
+                )
+            )
         }
     }
 

AnkiDroid/src/main/java/com/ichi2/anki/DeckPicker.kt
@@ -79,6 +79,7 @@ import com.ichi2.anki.dialogs.customstudy.CustomStudyDialog.CustomStudyListener
 import com.ichi2.anki.dialogs.customstudy.CustomStudyDialogFactory
 import com.ichi2.anki.exception.ConfirmModSchemaException
 import com.ichi2.anki.export.ActivityExportingDelegate
+import com.ichi2.anki.export.ExportType
 import com.ichi2.anki.notetype.ManageNotetypes
 import com.ichi2.anki.pages.CsvImporter
 import com.ichi2.anki.preferences.AdvancedSettingsFragment
@@ -895,8 +896,12 @@ open class DeckPicker :
             }
             R.id.action_export -> {
                 Timber.i("DeckPicker:: Export collection button pressed")
-                val msg = resources.getString(R.string.confirm_apkg_export)
-                mExportingDelegate.showExportDialog(msg)
+                mExportingDelegate.showExportDialog(
+                    ExportDialogParams(
+                        message = resources.getString(R.string.confirm_apkg_export),
+                        exportType = ExportType.ExportCollection
+                    )
+                )
                 return true
             }
             else -> return super.onOptionsItemSelected(item)
@@ -2405,8 +2410,12 @@ open class DeckPicker :
     }
 
     fun exportDeck(did: DeckId) {
-        val msg = resources.getString(R.string.confirm_apkg_export_deck, col.decks.get(did).getString("name"))
-        mExportingDelegate.showExportDialog(msg, did)
+        mExportingDelegate.showExportDialog(
+            ExportDialogParams(
+                message = resources.getString(R.string.confirm_apkg_export_deck, col.decks.get(did).getString("name")),
+                exportType = ExportType.ExportDeck(did)
+            )
+        )
     }
 
     fun createIcon(context: Context, did: DeckId) {

AnkiDroid/src/main/java/com/ichi2/anki/dialogs/ExportDialog.kt
@@ -18,13 +18,17 @@ package com.ichi2.anki.dialogs
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import anki.cards.cardIds
 import anki.import_export.ExportLimit
 import anki.import_export.exportLimit
+import anki.notes.noteIds
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.list.listItemsMultiChoice
 import com.ichi2.anki.R
 import com.ichi2.anki.analytics.AnalyticsDialogFragment
-import com.ichi2.annotations.NeedsTest
+import com.ichi2.anki.dialogs.ExportDialogParams.Companion.toExportDialogParams
+import com.ichi2.anki.export.ExportType
+import com.ichi2.anki.export.ExportType.*
 import com.ichi2.libanki.DeckId
 import com.ichi2.utils.BundleUtils.getNullableLong
 import com.ichi2.utils.contentNullable
@@ -37,80 +41,36 @@ class ExportDialog(private val listener: ExportDialogListener) : AnalyticsDialog
         fun dismissAllDialogFragments()
     }
 
-    private var mIncludeSched = false
-    private var mIncludeMedia = false
+    private var includeSched = false
+    private var includeMedia = false
 
-    /**
-     * Creates a new instance of ExportDialog to export a deck of cards
-     *
-     * @param did A long which specifies the deck to be exported,
-     *            if did is null then the whole collection of decks will be exported
-     * @param dialogMessage A string which can be used to show a custom message or specify import path
-     */
-    fun withArguments(dialogMessage: String, did: DeckId? = null): ExportDialog {
-        val args = this.arguments ?: Bundle()
-        if (did != null) {
-            args.putLong("did", did)
-        }
-        args.putString("dialogMessage", dialogMessage)
-        this.arguments = args
-        return this
-    }
-
-    /**
-     * @param ids A list of longs which specifies the card/note ids to be exported
-     * @param isCardList A boolean which specifies whether the list of ids is a list of card ids or note ids
-     * @param dialogMessage A string which can be used to show a custom message or specify import path
-     */
-    fun withArguments(dialogMessage: String, ids: List<Long>, isCardList: Boolean): ExportDialog {
-        val args = this.arguments ?: Bundle()
-        args.putString("dialogMessage", dialogMessage)
-        if (isCardList) {
-            args.putLongArray("cardIds", ids.toLongArray())
-        } else {
-            args.putLongArray("noteIds", ids.toLongArray())
-        }
-        this.arguments = args
+    fun withArguments(data: ExportDialogParams): ExportDialog {
+        this.arguments = data.appendToBundle(this.arguments ?: Bundle())
         return this
     }
 
     @SuppressLint("CheckResult")
     override fun onCreateDialog(savedInstanceState: Bundle?): MaterialDialog {
         super.onCreate(savedInstanceState)
-        val res = resources
-        val did = getNullableLong(arguments, "did")
-        val cardIds = arguments?.getLongArray("cardIds")?.toList()
-        val noteIds = arguments?.getLongArray("noteIds")?.toList()
-        val checked: Array<Int>
-        @NeedsTest("Test that correct options are checked given different arguments")
-        if (did != null || cardIds == null || noteIds == null) {
-            mIncludeSched = false
-            checked = arrayOf()
-        } else {
-            mIncludeSched = true
-            checked = arrayOf(INCLUDE_SCHED)
-        }
-        val items = listOf(
-            res.getString(R.string.export_include_schedule),
-            res.getString(R.string.export_include_media)
-        )
+        val exportData = requireArguments().toExportDialogParams()
+        val initialSelection: Array<Int> = if (exportData.includeScheduling) arrayOf(INCLUDE_SCHED) else arrayOf()
+        includeSched = initialSelection.contains(INCLUDE_SCHED)
+
         return MaterialDialog(requireActivity()).show {
             title(R.string.export)
-            contentNullable(requireArguments().getString("dialogMessage"))
+            contentNullable(exportData.message)
             positiveButton(android.R.string.ok) {
-                if (did != null) {
-                    listener.exportDeckAsApkg(null, did, mIncludeSched, mIncludeMedia)
-                } else if (cardIds != null || noteIds != null) {
-                    val limit = exportLimit {
-                        if (cardIds != null) {
-                            this.cardIds = this.cardIds.toBuilder().addAllCids(cardIds).build()
-                        } else {
-                            this.noteIds = this.noteIds.toBuilder().addAllNoteIds(noteIds).build()
-                        }
+                when (val exportType = exportData.exportType) {
+                    is ExportDeck -> listener.exportDeckAsApkg(null, exportType.deckId, includeSched, includeMedia)
+                    is ExportCollection -> listener.exportColAsApkgOrColpkg(null, includeSched, includeMedia)
+                    is ExportNotes -> {
+                        val limit = exportLimit { this.noteIds = noteIds { this.noteIds.addAll(exportType.nodeIds) } }
+                        listener.exportSelectedAsApkg(null, limit, includeSched, includeMedia)
                     }
-                    listener.exportSelectedAsApkg(null, limit, mIncludeSched, mIncludeMedia)
-                } else {
-                    listener.exportColAsApkgOrColpkg(null, mIncludeSched, mIncludeMedia)
+                    is ExportCards -> {
+                        val limit = exportLimit { this.cardIds = cardIds { cids.addAll(exportType.cardIds) } }
+                        listener.exportSelectedAsApkg(null, limit, includeSched, includeMedia)
+                    }
                 }
                 dismissAllDialogFragments()
             }
@@ -119,13 +79,16 @@ class ExportDialog(private val listener: ExportDialogListener) : AnalyticsDialog
             }
             cancelable(true)
             listItemsMultiChoice(
-                items = items,
-                initialSelection = checked.toIntArray(),
+                items = listOf(
+                    resources.getString(R.string.export_include_schedule),
+                    resources.getString(R.string.export_include_media)
+                ),
+                initialSelection = initialSelection.toIntArray(),
                 allowEmptySelection = true,
                 waitForPositiveButton = false
             ) { _: MaterialDialog, ints: IntArray, _: List<CharSequence> ->
-                mIncludeMedia = ints.contains(INCLUDE_MEDIA)
-                mIncludeSched = ints.contains(INCLUDE_SCHED)
+                includeMedia = ints.contains(INCLUDE_MEDIA)
+                includeSched = ints.contains(INCLUDE_SCHED)
             }
         }
     }
@@ -137,5 +100,58 @@ class ExportDialog(private val listener: ExportDialogListener) : AnalyticsDialog
     companion object {
         private const val INCLUDE_SCHED = 0
         private const val INCLUDE_MEDIA = 1
+    }
+}
+
+/**
+ * @param message A dialog to display to the user when exporting
+ */
+class ExportDialogParams(val message: String, val exportType: ExportType) {
+    val includeScheduling: Boolean = when (this.exportType) {
+        is ExportNotes -> false
+        is ExportCards -> false
+        is ExportDeck -> false
+        is ExportCollection -> true
+    }
+
+    fun appendToBundle(bundle: Bundle): Bundle {
+        bundle.putString(MESSAGE, this.message)
+
+        when (this.exportType) {
+            is ExportNotes -> bundle.putLongArray(NOTE_IDS, this.exportType.nodeIds.toLongArray())
+            is ExportCards -> bundle.putLongArray(CARD_IDS, this.exportType.cardIds.toLongArray())
+            is ExportDeck -> bundle.putLong(DECK_ID, this.exportType.deckId)
+            is ExportCollection -> {}
+        }
+        return bundle
+    }
+
+    companion object {
+        private const val MESSAGE = "dialogMessage"
+        private const val DECK_ID = "did"
+        private const val CARD_IDS = "cardIds"
+        private const val NOTE_IDS = "noteIds"
+
+        private fun Bundle.toExportType(): ExportType {
+            val did = getNullableLong(DECK_ID)
+            val cardIds = getLongArray(CARD_IDS)
+            val noteIds = getLongArray(NOTE_IDS)
+
+            if (did != null) {
+                return ExportDeck(did)
+            }
+            if (cardIds != null) {
+                return ExportCards(cardIds.toList())
+            }
+            if (noteIds != null) {
+                return ExportNotes(noteIds.toList())
+            }
+            return ExportCollection
+        }
+
+        fun Bundle.toExportDialogParams(): ExportDialogParams = ExportDialogParams(
+            message = getString(MESSAGE)!!,
+            exportType = this.toExportType()
+        )
     }
 }

AnkiDroid/src/main/java/com/ichi2/anki/export/ActivityExportingDelegate.kt
@@ -35,6 +35,7 @@ import com.ichi2.anki.CollectionManager.withCol
 import com.ichi2.anki.UIUtils.showThemedToast
 import com.ichi2.anki.dialogs.ExportCompleteDialog.ExportCompleteDialogListener
 import com.ichi2.anki.dialogs.ExportDialog.ExportDialogListener
+import com.ichi2.anki.dialogs.ExportDialogParams
 import com.ichi2.anki.snackbar.showSnackbar
 import com.ichi2.compat.CompatHelper
 import com.ichi2.libanki.AnkiPackageExporter
@@ -62,22 +63,8 @@ class ActivityExportingDelegate(private val activity: AnkiActivity, private val 
     private val mSaveFileLauncher: ActivityResultLauncher<Intent>
     private lateinit var mExportFileName: String
 
-    fun showExportDialog(msg: String) {
-        activity.showDialogFragment(mDialogsFactory.newExportDialog().withArguments(msg))
-    }
-
-    fun showExportDialog(msg: String, did: DeckId) {
-        activity.showDialogFragment(mDialogsFactory.newExportDialog().withArguments(msg, did))
-    }
-
-    /**
-     * Show the export dialog in the Browser to export selected cards or notes
-     * @param msg the message to show in the dialog
-     * @param ids the selected card/note ids
-     * @param isCardList true if the ids are card ids, false if they are note ids
-     */
-    fun showExportDialog(msg: String, ids: List<Long>, isCardList: Boolean) {
-        activity.showDialogFragment(mDialogsFactory.newExportDialog().withArguments(msg, ids, isCardList))
+    fun showExportDialog(params: ExportDialogParams) {
+        activity.showDialogFragment(mDialogsFactory.newExportDialog().withArguments(params))
     }
 
     private fun getTimeStampSuffix() =

AnkiDroid/src/main/java/com/ichi2/anki/export/ExportType.kt
@@ -1,0 +1,31 @@
+/*
+ *  Copyright (c) 2023 David Allison <davidallisongithub@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ichi2.anki.export
+
+import com.ichi2.libanki.CardId
+import com.ichi2.libanki.DeckId
+import com.ichi2.libanki.NoteId
+
+/**
+ * Ways that a user may export data
+ */
+sealed interface ExportType {
+    class ExportNotes(val nodeIds: List<NoteId>) : ExportType
+    class ExportCards(val cardIds: List<CardId>) : ExportType
+    class ExportDeck(val deckId: DeckId) : ExportType
+    object ExportCollection : ExportType
+}

AnkiDroid/src/main/java/com/ichi2/utils/BundleUtils.kt
@@ -24,16 +24,15 @@ object BundleUtils {
     /**
      * Retrieves a [Long] value from a [Bundle] using a key, returns null if not found
      *
-     * @param bundle the bundle to look into
      * can be null to support nullable bundles like [androidx.fragment.app.Fragment.getArguments]
      * @param key the key to use
      * @return the long value, or null if not found
      */
-    fun getNullableLong(bundle: Bundle?, key: String): Long? {
-        return if (bundle == null || !bundle.containsKey(key)) {
+    fun Bundle.getNullableLong(key: String): Long? {
+        return if (!containsKey(key)) {
             null
         } else {
-            bundle.getLong(key)
+            getLong(key)
         }
     }
 

AnkiDroid/src/test/java/com/ichi2/anki/export/ExportDialogParamsTest.kt
@@ -1,0 +1,38 @@
+/*
+ *  Copyright (c) 2023 David Allison <davidallisongithub@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ichi2.anki.export
+
+import com.ichi2.anki.dialogs.ExportDialogParams
+import com.ichi2.anki.export.ExportType.*
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.Test
+
+class ExportDialogParamsTest {
+    @Test
+    fun includeSchedulingDefaultValue() {
+        assertIncludeScheduling(ExportDeck(1), false)
+        assertIncludeScheduling(ExportCards(listOf(1, 2)), false)
+        assertIncludeScheduling(ExportNotes(listOf(1, 2)), false)
+        assertIncludeScheduling(ExportCollection, true)
+    }
+
+    private fun assertIncludeScheduling(type: ExportType, expected: Boolean) {
+        val includeScheduling = ExportDialogParams(message = "", exportType = type).includeScheduling
+        assertThat("${type.javaClass.simpleName}: includeScheduling", includeScheduling, equalTo(expected))
+    }
+}

AnkiDroid/src/test/java/com/ichi2/utils/BundleUtilsTest.kt
@@ -16,6 +16,7 @@
 package com.ichi2.utils
 
 import android.os.Bundle
+import com.ichi2.utils.BundleUtils.getNullableLong
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.mockito.Mockito.*
@@ -25,18 +26,12 @@ import kotlin.test.assertNull
 
 class BundleUtilsTest {
     @Test
-    fun test_GetNullableLong_NullBundle_ReturnsNull() {
-        val value = BundleUtils.getNullableLong(null, KEY)
-        assertNull(value)
-    }
-
-    @Test
     fun test_GetNullableLong_NotFound_ReturnsNull() {
         val b = mock(Bundle::class.java)
 
         whenever(b.containsKey(anyString())).thenReturn(false)
 
-        val value = BundleUtils.getNullableLong(b, KEY)
+        val value = b.getNullableLong(KEY)
 
         verify(b, times(0)).getLong(eq(KEY))
 
@@ -52,7 +47,7 @@ class BundleUtilsTest {
 
         whenever(b.getLong(anyString())).thenReturn(expected)
 
-        val value = BundleUtils.getNullableLong(b, KEY)
+        val value = b.getNullableLong(KEY)
 
         verify(b).getLong(eq(KEY))
 