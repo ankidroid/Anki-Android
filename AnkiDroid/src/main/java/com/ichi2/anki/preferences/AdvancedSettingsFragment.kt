adbba18dea136cb926586005841ab000d179eebf
David Allison <62114487+david-allison@users.noreply.github.com>
2023-02-24 20:00:00 +0000

    Disable some settings if migrating storage
    Migrating storage is a high-risk operation as it involves copying
    the user's media
    
    Disable:
    * AnkiDroid Directory
      * Corruption: Media from the one collection is moved to the other
    * V3 Migration
      * High Risk: Disabling defensively
    
    Grey the preference and set the summary
    
    Issue 5304
    Issue 13094

--- AnkiDroid/src/main/java/com/ichi2/anki/preferences/AdvancedSettingsFragment.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/preferences/AdvancedSettingsFragment.kt
@@ -21,13 +21,13 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
-import androidx.preference.EditTextPreference
-import androidx.preference.Preference
-import androidx.preference.SwitchPreference
+import androidx.preference.*
 import com.ichi2.anki.*
 import com.ichi2.anki.CollectionManager.withCol
+import com.ichi2.anki.R
 import com.ichi2.anki.exception.StorageAccessException
 import com.ichi2.anki.provider.CardContentProvider
+import com.ichi2.anki.servicelayer.ScopedStorageService
 import com.ichi2.anki.snackbar.showSnackbar
 import com.ichi2.compat.CompatHelper
 import com.ichi2.utils.show
@@ -50,6 +50,7 @@ class AdvancedSettingsFragment : SettingsFragment() {
 
         // Check that input is valid before committing change in the collection path
         requirePreference<EditTextPreference>(CollectionHelper.PREF_COLLECTION_PATH).apply {
+            disableIfStorageMigrationInProgress()
             setOnPreferenceChangeListener { _, newValue: Any? ->
                 val newPath = newValue as String
                 try {
@@ -150,6 +151,7 @@ class AdvancedSettingsFragment : SettingsFragment() {
                 isEnabled = false
                 isChecked = true
             }
+            disableIfStorageMigrationInProgress()
             setOnPreferenceChangeListener { newValue ->
                 if (newValue == true) {
                     confirmExperimentalChange(
@@ -202,6 +204,20 @@ class AdvancedSettingsFragment : SettingsFragment() {
             if (doubleScrolling != null) {
                 preferenceScreen.removePreference(doubleScrolling)
             }
+        }
+    }
+
+    private fun Preference.disableIfStorageMigrationInProgress() {
+        try {
+            if (ScopedStorageService.userMigrationIsInProgress(requireContext())) {
+                isEnabled = false
+                summaryProvider = null // needs to be disabled to set .summary
+                summary = getString(R.string.functionality_disabled_during_storage_migration)
+            }
+        } catch (e: Exception) {
+            // This screen is vital and must not crash. Trust the user knows what they're doing.
+            // This exists only as a precaution.
+            Timber.w(e)
         }
     }
 
