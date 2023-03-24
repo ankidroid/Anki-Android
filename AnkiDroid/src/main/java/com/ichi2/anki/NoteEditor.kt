593c8fb13c1eb1fcd002dfa4d0710035325fab94
Robozinho <65715921+robozinhod@users.noreply.github.com>
2023-03-24 18:33:34 -0300

    Fix activityResult deprecation
    request code wasn't used

--- AnkiDroid/src/main/java/com/ichi2/anki/NoteEditor.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/NoteEditor.kt
@@ -1023,7 +1023,7 @@ class NoteEditor : AnkiActivity(), DeckSelectionListener, SubtitleListener, Tags
         addInstanceStateToBundle(noteEditorBundle)
         noteEditorBundle.putBundle("editFields", fieldsAsBundleForPreview)
         previewer.putExtra("noteEditorBundle", noteEditorBundle)
-        startActivityForResultWithoutAnimation(previewer, REQUEST_PREVIEW)
+        startActivityWithoutAnimation(previewer)
     }
 
     /**
@@ -2225,7 +2225,6 @@ class NoteEditor : AnkiActivity(), DeckSelectionListener, SubtitleListener, Tags
         const val REQUEST_ADD = 0
         const val REQUEST_MULTIMEDIA_EDIT = 2
         const val REQUEST_TEMPLATE_EDIT = 3
-        const val REQUEST_PREVIEW = 4
 
         // preferences keys
         const val PREF_NOTE_EDITOR_SCROLL_TOOLBAR = "noteEditorScrollToolbar"
