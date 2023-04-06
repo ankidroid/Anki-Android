ab510f67a4224ad19ff70a3cd630fba8ab43962f
Sumit Singh <sumitsinghkoranga7@gmail.com>
2023-04-06 14:38:36 +0530

    Removed Extra line

--- AnkiDroid/src/main/java/com/ichi2/anki/workarounds/AppLoadedFromBackupWorkaround.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/workarounds/AppLoadedFromBackupWorkaround.kt
@@ -19,7 +19,6 @@ package com.ichi2.anki.workarounds
 import android.app.Activity
 import android.os.Bundle
 import android.os.Process
-import com.google.android.material.snackbar.Snackbar
 import com.ichi2.anki.AnkiActivity
 import com.ichi2.anki.AnkiDroidApp
 import com.ichi2.anki.R
@@ -52,10 +51,7 @@ object AppLoadedFromBackupWorkaround {
         // * A restore took place
         // * The app is reopened (until it exits: finish() does not do this - and removes it from the app list)
         Timber.w("Activity started with no application instance")
-        showSnackbar(
-            getString(R.string.ankidroid_cannot_open_after_backup_try_again),
-            Snackbar.LENGTH_LONG
-        )
+        showSnackbar(getString(R.string.ankidroid_cannot_open_after_backup_try_again))
 
         // fixes: java.lang.IllegalStateException: You need to use a Theme.AppCompat theme (or descendant) with this activity.
         // on Importer
