4eb0e24ec59f6d160c64a6b8a1200e7aa177e864
jocoand <joco.learn@gmail.com>
2023-04-07 09:10:02 +0700

    Use setMessage method

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/SimpleMessageDialog.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/SimpleMessageDialog.kt
@@ -20,7 +20,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import com.ichi2.anki.AnkiDroidApp
 import com.ichi2.anki.R
-import com.ichi2.utils.message
 import com.ichi2.utils.positiveButton
 import com.ichi2.utils.title
 
@@ -33,7 +32,7 @@ class SimpleMessageDialog : AsyncDialogFragment() {
         super.onCreateDialog(savedInstanceState)
         return AlertDialog.Builder(requireContext()).apply {
             title(text = notificationTitle)
-            message(text = notificationMessage)
+            setMessage(notificationMessage)
             positiveButton(R.string.dialog_ok) {
                 (activity as SimpleMessageDialogListener?)
                     ?.dismissSimpleMessageDialog(
