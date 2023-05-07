49745702d121df81c2fabd59ec284c8b63b89bba
nihil-admirari <50202386+nihil-admirari@users.noreply.github.com>
2023-05-07 12:21:13 +0300

    JS module loading for older Androids (fixes #9311)

--- AnkiDroid/src/androidTest/java/com/ichi2/anki/utils/AssetHelperTest.kt
+++ AnkiDroid/src/androidTest/java/com/ichi2/anki/utils/AssetHelperTest.kt
@@ -13,15 +13,23 @@
  You should have received a copy of the GNU General Public License along with
  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.ichi2.utils
+package com.ichi2.anki.utils
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.utils.AssetHelper
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class AssetHelperTest {
     @Test
     fun guessMimeTypeTest() {
+        assertThat(AssetHelper.guessMimeType("test.js"), equalTo("text/javascript"))
+        assertThat(AssetHelper.guessMimeType("test.mjs"), equalTo("text/javascript"))
+        assertThat(AssetHelper.guessMimeType("test.json"), equalTo("application/json"))
+        assertThat(AssetHelper.guessMimeType("test.css"), equalTo("text/css"))
         assertThat(AssetHelper.guessMimeType("test.txt"), equalTo("text/plain"))
         assertThat(AssetHelper.guessMimeType("test.png"), equalTo("image/png"))
         assertThat(AssetHelper.guessMimeType("test.zip"), equalTo("application/zip"))
