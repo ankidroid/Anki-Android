6236adc3d7e6d19986ecaddef72cd1897e3397fb
Sanjaykumar Sargam <sargamsanjaykumar@gmail.com>
2023-06-06 14:52:00 +0530

    implement reset to default button

--- AnkiDroid/src/main/java/com/ichi2/anki/preferences/AccessibilitySettingsFragment.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/preferences/AccessibilitySettingsFragment.kt
@@ -15,7 +15,15 @@
  */
 package com.ichi2.anki.preferences
 
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.edit
+import androidx.preference.Preference
+import com.ichi2.anki.AnkiDroidApp
 import com.ichi2.anki.R
+import com.ichi2.utils.negativeButton
+import com.ichi2.utils.positiveButton
+import com.ichi2.utils.show
+import com.ichi2.utils.title
 
 /**
  * Fragment with preferences related to notifications
@@ -27,5 +35,26 @@ class AccessibilitySettingsFragment : SettingsFragment() {
         get() = "prefs.accessibility"
 
     override fun initSubscreen() {
+        // Reset accessibility button to default
+        val resetAccessibilityButtons = requirePreference<Preference>("reset_accessibility_buttons")
+        resetAccessibilityButtons.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            AlertDialog.Builder(requireContext()).show {
+                title(R.string.reset_settings_to_default)
+                positiveButton(R.string.reset) {
+                    // Reset the settings to default
+                    AnkiDroidApp.getSharedPrefs(requireContext()).edit {
+                        remove("cardZoom")
+                        remove("imageZoom")
+                        remove("answerButtonSize")
+                        remove("showLargeAnswerButtons")
+                        remove("relativeCardBrowserFontSize")
+                    }
+                    // Refresh the screen to display the changes
+                    refreshScreen()
+                }
+                negativeButton(R.string.dialog_cancel, null)
+            }
+            true
+        }
     }
 }
