5069ad604b21cdd17b404894c828a6b9e1f42836
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2023-06-23 01:26:33 +0530

    checking for bitmap size to avoid `illegalArgumentException` (#14001)

--- AnkiDroid/src/main/java/com/ichi2/anki/Whiteboard.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/Whiteboard.kt
@@ -223,6 +223,11 @@ class Whiteboard(activity: AnkiActivity, handleMultiTouch: Boolean, inverted: Bo
      */
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
+        // createScaledBitmap requires a width and height > 0; #13972
+        if (w <= 0 || h <= 0) {
+            Timber.w("Width or height <= 0: w: $w h: $h Bitmap couldn't be created with the new size")
+            return
+        }
         val scaledBitmap: Bitmap = Bitmap.createScaledBitmap(mBitmap, w, h, true)
         mBitmap = scaledBitmap
         mCanvas = Canvas(mBitmap)
