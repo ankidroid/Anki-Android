6e74576e91a9284cb1f4c9684910f169e3930600
Aditya kumar <adi54321kumar@gmail.com>
2023-11-03 09:49:59 +0530

    dismissed snackbar for created deck message

--- AnkiDroid/src/main/java/com/ichi2/anki/DeckPickerFloatingActionMenu.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/DeckPickerFloatingActionMenu.kt
@@ -66,10 +66,10 @@ class DeckPickerFloatingActionMenu(
         mLinearLayout.alpha = 0.5f
         mStudyOptionsFrame?.let { it.alpha = 0.5f }
         isFABOpen = true
-        if (deckPicker.syncSnackbar != null && deckPicker.syncSnackbar!!.isShown)deckPicker.syncSnackbar!!.dismiss()
-        if (deckPicker.undoAllSnackbar != null && deckPicker.undoAllSnackbar!!.isShown)deckPicker.undoAllSnackbar!!.dismiss()
-        if (deckPicker.allDeckpickerSnackbar != null && deckPicker.allDeckpickerSnackbar!!.isShown)deckPicker.allDeckpickerSnackbar!!.dismiss()
-        if (createDeckSnackBar != null && createDeckSnackBar!!.isShown)createDeckSnackBar!!.dismiss()
+        if (deckPicker.syncSnackbar != null && deckPicker.syncSnackbar?.isShown == true)deckPicker.syncSnackbar?.dismiss()
+        if (deckPicker.undoAllSnackbar != null && deckPicker.undoAllSnackbar?.isShown == true)deckPicker.undoAllSnackbar?.dismiss()
+        if (deckPicker.allDeckpickerSnackbar != null && deckPicker.allDeckpickerSnackbar?.isShown == true)deckPicker.allDeckpickerSnackbar?.dismiss()
+        if (createDeckSnackBar != null && createDeckSnackBar?.isShown == true)createDeckSnackBar?.dismiss()
 
         if (deckPicker.animationEnabled()) {
             // Show with animation
