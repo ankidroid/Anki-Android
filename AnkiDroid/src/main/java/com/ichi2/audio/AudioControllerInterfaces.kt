ab3c89e157324de42fba6aca4f998bf430c76a80
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2023-11-14 16:57:36 +0530

    audio recorder and player UI revamp

--- AnkiDroid/src/main/java/com/ichi2/audio/AudioControllerInterfaces.kt
+++
@@ -1,8 +0,0 @@
-package com.ichi2.audio
-
-interface AudioControllerInterfaces {
-    fun onStartStopRecording()
-    fun onPlayPauseRecording()
-    fun onCancelRecording()
-    fun inReviewer(): Boolean
-}
