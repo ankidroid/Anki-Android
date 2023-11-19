28fc69f70d6b3001aebf68a1c90917129055c33d
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2023-11-20 00:12:18 +0530

    feat: allow printing or exporting stats

--- AnkiDroid/src/main/java/com/ichi2/anki/pages/Statistics.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/pages/Statistics.kt
@@ -17,13 +17,69 @@ package com.ichi2.anki.pages
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.print.PrintAttributes
+import android.print.PrintManager
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import com.ichi2.anki.CollectionManager
 import com.ichi2.anki.R
+import com.ichi2.anki.utils.getTimestamp
+import com.ichi2.libanki.utils.TimeManager
 
 class Statistics : PageFragment() {
     override val title = R.string.statistics
     override val pageName = "graphs"
     override var webViewClient = PageWebViewClient()
     override var webChromeClient = PageChromeClient()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().invalidateOptionsMenu()
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(
+            object : MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.statistics, menu)
+                    val exportStats = menu.findItem(R.id.action_export_stats)
+                    exportStats?.title = CollectionManager.TR.statisticsSavePdf()
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    return when (menuItem.itemId) {
+                        R.id.action_export_stats -> {
+                            exportWebViewContentAsPDF()
+                            true
+                        }
+                        else -> false
+                    }
+                }
+            },
+            viewLifecycleOwner,
+            Lifecycle.State.RESUMED
+        )
+    }
+
+    /**Prepares and initiates a printing task for the content(stats) displayed in the WebView.
+     * It uses the Android PrintManager service to create a print job, based on the content of the WebView.
+     * The resulting output is a PDF document. **/
+    fun exportWebViewContentAsPDF() {
+        val printManager = getSystemService(requireContext(), PrintManager::class.java)
+        val currentDateTime = getTimestamp(TimeManager.time)
+        val jobName = "${getString(R.string.app_name)}-stats-$currentDateTime"
+        val printAdapter = webView.createPrintDocumentAdapter(jobName)
+        printManager?.print(
+            jobName,
+            printAdapter,
+            PrintAttributes.Builder().build()
+        )
+    }
 
     companion object {
         fun getIntent(context: Context): Intent {
