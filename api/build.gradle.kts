4ac94b14feb30004eb3d7f68cdb88f075b0fc459
Mike Hardy <github@mikehardy.net>
2023-11-21 17:07:39 -0500

    build(api): fix artifact construction and publishing
    none of the old stuff worked anymore, redid following current docs

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,4 @@
 import com.android.build.gradle.internal.tasks.factory.dependsOn
-import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
     id("maven-publish")
@@ -21,7 +20,6 @@ android {
 
     defaultConfig {
         minSdk = 16
-        targetSdk = 33
         buildConfigField(
             "String",
             "READ_WRITE_PERMISSION",
@@ -53,6 +51,13 @@ android {
         freeCompilerArgs += "-Xexplicit-api=strict"
         jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
+
+    publishing {
+        singleVariant("release") {
+            withJavadocJar()
+            withSourcesJar()
+        }
+    }
 }
 
 apply(from = "../lint.gradle")
@@ -69,86 +74,65 @@ dependencies {
     lintChecks(project(":lint-rules"))
 }
 
-val androidSourcesJar = tasks.register("androidSourcesJar", Jar::class) {
-    archiveClassifier.set("sources")
-    // For Android libraries
-    from(android.sourceSets["main"].java.srcDirs)
-}
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                version = version
+                groupId = group.toString()
+                artifactId = "api"
 
-val dokkaJavadocJar = tasks.register("dokkaJavadocJar", Jar::class) {
-    val dokkaJavadocProvider: TaskProvider<DokkaTask> = tasks.named<DokkaTask>("dokkaJavadoc")
-    dependsOn(dokkaJavadocProvider)
-    from(dokkaJavadocProvider)
-    archiveClassifier.set("javadoc")
-    doLast {
-        println("API javadocs output directory: ${dokkaJavadocProvider.get().outputDirectory.get()}")
-        println("API javadocs jar output directory: ${destinationDirectory.get()}")
-    }
-}
-
-publishing {
-    publications {
-        create("mavenJava", MavenPublication::class) {
-            artifactId = "api"
-
-            artifact("$buildDir/outputs/aar/${project.getName()}-release.aar")
-            artifact(androidSourcesJar)
-            artifact(dokkaJavadocJar)
-
-            versionMapping {
-                usage("java-api") {
-                    fromResolutionOf("runtimeClasspath")
-                }
-                usage("java-runtime") {
-                    fromResolutionResult()
-                }
-            }
-            pom {
-                name = "AnkiDroid API"
-                description = "A programmatic API exported by AnkiDroid"
-                url = "https://github.com/ankidroid/Anki-Android/tree/main/api"
-                licenses {
-                    license {
-                        name = "GNU LESSER GENERAL PUBLIC LICENSE, v3"
-                        url =
-                            "https://github.com/ankidroid/Anki-Android/blob/main/api/COPYING.LESSER"
+                pom {
+                    name = "AnkiDroid API"
+                    description = "A programmatic API exported by AnkiDroid"
+                    url = "https://github.com/ankidroid/Anki-Android/tree/main/api"
+                    licenses {
+                        license {
+                            name = "GNU LESSER GENERAL PUBLIC LICENSE, v3"
+                            url =
+                                "https://github.com/ankidroid/Anki-Android/blob/main/api/COPYING.LESSER"
+                        }
+                    }
+                    scm {
+                        connection = "scm:git:git://github.com/ankidroid/Anki-Android.git"
+                        url = "https://github.com/ankidroid/Anki-Android"
                     }
                 }
-                scm {
-                    connection = "scm:git:git://github.com/ankidroid/Anki-Android.git"
-                    url = "https://github.com/ankidroid/Anki-Android"
+
+                afterEvaluate {
+                    from(components["release"])
                 }
             }
         }
-    }
-    repositories {
-        maven {
-            // change URLs to point to your repos, e.g. http://my.org/repo
-            val releasesRepoUrl = layout.buildDirectory.dir("repos/releases")
-            val snapshotsRepoUrl = layout.buildDirectory.dir("repos/snapshots")
-            url = uri(
-                if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-            )
+        repositories {
+            maven {
+                // change URLs to point to your repos, e.g. http://my.org/repo
+                val releasesRepoUrl = layout.buildDirectory.dir("repos/releases")
+                val snapshotsRepoUrl = layout.buildDirectory.dir("repos/snapshots")
+                url = uri(
+                    if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+                )
+            }
         }
     }
 }
 
 val zipReleaseProvider = tasks.register("zipRelease", Zip::class) {
     from(layout.buildDirectory.dir("repos/releases"))
-    destinationDirectory = buildDir
-    archiveFileName = "$buildDir/release-${archiveVersion.get()}.zip"
+    destinationDirectory = layout.buildDirectory
+    archiveFileName = "${layout.buildDirectory.get()}/release-${archiveVersion.get()}.zip"
 }
 
 // Use this task to make a release you can send to someone
 // You may like `./gradlew :api:publishToMavenLocal for development
 val generateRelease: TaskProvider<Task> = tasks.register("generateRelease") {
     doLast {
-        println("Release $version can be found at $buildDir/repos/releases/")
-        println("Release $version zipped can be found $buildDir/release-$version.zip")
+        println("Release $version can be found at ${layout.buildDirectory.get()}/repos/releases/")
+        println("Release $version zipped can be found ${layout.buildDirectory.get()}/release-$version.zip")
     }
 }
 
-tasks.named("publishMavenJavaPublicationToMavenRepository").dependsOn(tasks.named("assemble"))
-tasks.named("publish").dependsOn(tasks.named("assemble"))
+// tasks.named("publishMavenJavaPublicationToMavenRepository").dependsOn(tasks.named("assemble"))
+// tasks.named("publish").dependsOn(tasks.named("assemble"))
 generateRelease.dependsOn(tasks.named("publish"))
 generateRelease.dependsOn(zipReleaseProvider)
