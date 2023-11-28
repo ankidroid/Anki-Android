ankidroid/Anki-Android

commit bac2b6718f86e64b4ba748bb0880d70531776ce6

remove video full screen player

AnkiDroid/src/main/java/com/ichi2/libanki/Sound.kt
@@ -23,8 +23,6 @@ import android.media.AudioManager.OnAudioFocusChangeListener
 import android.media.MediaPlayer.OnCompletionListener
 import android.net.Uri
 import android.os.Build
-import android.webkit.MimeTypeMap
-import android.widget.VideoView
 import androidx.annotation.CheckResult
 import androidx.annotation.VisibleForTesting
 import com.ichi2.anki.AnkiDroidApp
@@ -32,7 +30,6 @@ import com.ichi2.anki.ReadText
 import com.ichi2.compat.CompatHelper
 import com.ichi2.libanki.Sound.OnErrorListener.ErrorHandling.CONTINUE_AUDIO
 import com.ichi2.libanki.Sound.SoundSide.*
-import com.ichi2.utils.DisplayUtils
 import timber.log.Timber
 import java.util.*
 import java.util.regex.Pattern
@@ -43,11 +40,11 @@ private typealias SoundPath = String
  * Parses, loads and plays sound & video files
  * Called `Sound` Anki uses `[sound:]` for both audio and video
  */
-class Sound(private val soundPlayer: SoundPlayer, private val soundDir: String) : SoundPlayer by soundPlayer {
+class Sound(private val soundPlayer: SoundPlayer, private val soundDir: String) : SoundPlayer() {
     /**
      * @param soundDir base path to the media files
      */
-    constructor(soundDir: String) : this(SoundPlayerImpl(), soundDir)
+    constructor(soundDir: String) : this(SoundPlayer(), soundDir)
 
     /**
      * The subset of sounds to involve
@@ -251,54 +248,17 @@ class Sound(private val soundPlayer: SoundPlayer, private val soundDir: String) 
     }
 }
 
-interface SoundPlayer {
-    /**
-     * Plays the given sound or video.
-     * Video requires a surface: [hasVideoSurface]
-     * [playVideoExternallyCallback] will be called if this is unavailable
-     */
-    fun playSound(
-        soundPath: String,
-        onCompletionListener: OnCompletionListener?,
-        errorListener: Sound.OnErrorListener?
-    )
-
-    val hasVideoSurface: Boolean
-
-    // When an audio finishes and I'm trying to replay it again, this method should check if the mMediaPlayer is null which means
-    // the audio finished to return true, so that I would be able to play the same sound again.
-    val isCurrentAudioFinished: Boolean
-
-    /**
-     * The Uri of the currently playing audio (or `null` if no audio playing)
-     */
+open class SoundPlayer {
     val currentAudioUri: String?
-
-    /**
-     * Stops the playing sounds.
-     */
-    fun stopSounds()
-
-    /**
-     * Play or Pause the running sound. Called on pressing the content inside span tag.
-     */
-    fun playOrPauseSound()
-
-    /** @return Whether the video was handled externally. Only used if [hasVideoSurface] is false */
-    var playVideoExternallyCallback: ((soundPath: String, onCompletion: OnCompletionListener) -> Boolean)?
-}
-
-open class SoundPlayerImpl : SoundPlayer {
-    override val currentAudioUri: String?
         get() = mCurrentAudioUri?.toString()
 
-    override val isCurrentAudioFinished: Boolean
+    val isCurrentAudioFinished: Boolean
         get() = mMediaPlayer == null
 
     /**
      * Media player used to play the sounds. It's Nullable and that it is set only if a sound is playing or paused, otherwise it is null.
      */
-    protected var mMediaPlayer: MediaPlayer? = null
+    private var mMediaPlayer: MediaPlayer? = null
 
     private var mCurrentAudioUri: Uri? = null
 
@@ -309,13 +269,11 @@ open class SoundPlayerImpl : SoundPlayer {
 
     private var mAudioFocusRequest: AudioFocusRequest? = null
 
-    override val hasVideoSurface: Boolean = false
-
     /**
      * Plays the given sound or video and sets playAllListener if available on media player to start next media.
      * If videoView is null and the media is a video, then a request is sent to start the VideoPlayer Activity
      */
-    override fun playSound(
+    fun playSound(
         soundPath: String,
         onCompletionListener: OnCompletionListener?,
         errorListener: Sound.OnErrorListener?
@@ -340,15 +298,10 @@ open class SoundPlayerImpl : SoundPlayer {
     ) {
         Timber.d("Playing %s", soundPath)
         val soundUri = Uri.parse(soundPath)
-        val soundUriPath = soundUri.path.toString()
         mCurrentAudioUri = soundUri
 
         val context = AnkiDroidApp.instance.applicationContext
 
-        val isVideo = isVideo(soundUriPath)
-        if (isVideo && !hasVideoSurface && playVideoExternallyCallback?.invoke(soundPath, completionListener) == true) {
-            return
-        }
         // Play media
         fun playMedia() {
             try {
@@ -363,10 +316,6 @@ open class SoundPlayerImpl : SoundPlayer {
                 val mediaPlayer = mMediaPlayer!!
                 mAudioManager =
                     mAudioManager ?: context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-
-                if (isVideo) {
-                    prepareVideo(mediaPlayer)
-                }
 
                 mediaPlayer.setOnErrorListener { mp: MediaPlayer?, which: Int, extra: Int ->
                     val errorHandling = errorHandler.onError(
@@ -438,28 +387,6 @@ open class SoundPlayerImpl : SoundPlayer {
         playMedia()
     }
 
-    open fun prepareVideo(mediaPlayer: MediaPlayer) {
-        // in the base class: playVideoExternallyCallback should have been called
-    }
-
-    override var playVideoExternallyCallback: ((soundPath: String, onCompletion: OnCompletionListener) -> Boolean)? = null
-
-    private fun isVideo(soundPath: String): Boolean {
-        // Check if the file extension is that of a known video format
-        val extension = soundPath.getFileExtension()
-        val isVideoExtension = listOf(*VIDEO_WHITELIST).contains(extension) || extension.isVideoMimeTypeExtension()
-        // Also check that there is a video thumbnail, as some formats like mp4 can be audio only
-        // No thumbnail: no video after all. (Or maybe not a video we can handle on the specific device.)
-        return isVideoExtension && CompatHelper.compat.hasVideoThumbnail(soundPath)
-    }
-
-    // TODO: This seems wrong
-    private fun String.getFileExtension() = substring(this.lastIndexOf(".") + 1).lowercase(Locale.getDefault())
-    private fun String.isVideoMimeTypeExtension(): Boolean {
-        val guessedType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(this) ?: return false
-        return guessedType.startsWith("video/")
-    }
-
     /**
      * Releases the sound.
      */
@@ -479,7 +406,7 @@ open class SoundPlayerImpl : SoundPlayer {
         }
     }
 
-    override fun stopSounds() {
+    open fun stopSounds() {
         mMediaPlayer?.let {
             it.stop()
             // TODO: Inefficient. Determine whether we want to release or stop, don't do both
@@ -488,7 +415,7 @@ open class SoundPlayerImpl : SoundPlayer {
         }
     }
 
-    override fun playOrPauseSound() {
+    fun playOrPauseSound() {
         mMediaPlayer?.let {
             if (it.isPlaying) {
                 it.pause()
@@ -508,59 +435,9 @@ open class SoundPlayerImpl : SoundPlayer {
 
     companion object {
         /**
-         * Whitelist for video extensions
-         */
-        private val VIDEO_WHITELIST = arrayOf("3gp", "mp4", "webm", "mkv", "flv")
-
-        /**
          * Listener to handle audio focus. Currently blank because we're not respecting losing focus from other apps.
          */
         private val audioFocusChangeListener = OnAudioFocusChangeListener { }
-    }
-}
-
-class VideoPlayer(private val videoView: VideoView) : SoundPlayerImpl() {
-
-    // don't call out to external video players
-    override val hasVideoSurface = true
-
-    /** Plays the given video */
-    fun play(
-        path: String,
-        onCompletionListener: OnCompletionListener?,
-        onErrorListener: Sound.OnErrorListener?
-    ) = playSound(path, onCompletionListener, onErrorListener)
-    fun notifyConfigurationChanged() {
-        mMediaPlayer?.let {
-            configureVideo(videoView, it.videoWidth, it.videoHeight)
-        }
-    }
-
-    override fun prepareVideo(mediaPlayer: MediaPlayer) {
-        mediaPlayer.setDisplay(videoView.holder)
-        mediaPlayer.setOnVideoSizeChangedListener { _, width: Int, height: Int ->
-            configureVideo(videoView, width, height)
-        }
-    }
-
-    private fun configureVideo(videoView: VideoView, videoWidth: Int, videoHeight: Int) {
-        // get the display
-        val context = AnkiDroidApp.instance.applicationContext
-        // adjust the size of the video so it fits on the screen
-        val videoProportion = videoWidth.toFloat() / videoHeight.toFloat()
-        val point = DisplayUtils.getDisplayDimensions(context)
-        val screenWidth = point.x
-        val screenHeight = point.y
-        val screenProportion = screenWidth.toFloat() / screenHeight.toFloat()
-        val lp = videoView.layoutParams
-        if (videoProportion > screenProportion) {
-            lp.width = screenWidth
-            lp.height = (screenWidth.toFloat() / videoProportion).toInt()
-        } else {
-            lp.width = (videoProportion * screenHeight.toFloat()).toInt()
-            lp.height = screenHeight
-        }
-        videoView.layoutParams = lp
     }
 }
 

AnkiDroid/src/main/AndroidManifest.xml
@@ -267,12 +267,6 @@
             </intent-filter>
         </activity>
         <activity
-            android:name="com.ichi2.anki.VideoPlayer"
-            android:exported="false"
-            android:configChanges="keyboardHidden|orientation|screenSize"
-            android:theme="@android:style/Theme.NoTitleBar.Fullscreen"
-            />
-        <activity
             android:name="com.ichi2.anki.MyAccount"
             android:label="@string/menu_my_account"
             android:exported="false"

AnkiDroid/src/main/java/com/ichi2/anki/AbstractFlashcardViewer.kt
@@ -86,7 +86,6 @@ import com.ichi2.libanki.Consts.BUTTON_TYPE
 import com.ichi2.libanki.Sound.OnErrorListener.ErrorHandling
 import com.ichi2.libanki.Sound.SingleSoundSide
 import com.ichi2.libanki.Sound.SoundSide
-import com.ichi2.libanki.SoundPlayer
 import com.ichi2.themes.Themes
 import com.ichi2.themes.Themes.getResFromAttr
 import com.ichi2.ui.FixedEditText
@@ -101,7 +100,6 @@ import kotlinx.coroutines.Job
 import net.ankiweb.rsdroid.RustCleanup
 import timber.log.Timber
 import java.io.*
-import java.lang.ref.WeakReference
 import java.net.URLDecoder
 import java.util.*
 import java.util.concurrent.locks.Lock
@@ -523,9 +521,7 @@ abstract class AbstractFlashcardViewer :
         super.onCollectionLoaded(col)
         val mediaDir = col.media.dir
         mBaseUrl = getBaseUrl(mediaDir).also { baseUrl ->
-            mSoundPlayer = Sound(baseUrl).also { sound ->
-                sound.setupVideoActivityCallback()
-            }
+            mSoundPlayer = Sound(baseUrl)
             mViewerUrl = baseUrl + "__viewer__.html"
         }
         mAssetLoader = WebViewAssetLoader.Builder()
@@ -586,9 +582,6 @@ abstract class AbstractFlashcardViewer :
 
     override fun onResume() {
         super.onResume()
-        if (this::mSoundPlayer.isInitialized) {
-            mSoundPlayer.setupVideoActivityCallback()
-        }
         automaticAnswer.enable()
         // Reset the activity title
         updateActionBar()
@@ -2595,28 +2588,6 @@ abstract class AbstractFlashcardViewer :
                 Gesture.SWIPE_RIGHT -> ActivityTransitionAnimation.Direction.RIGHT
                 Gesture.SWIPE_LEFT -> ActivityTransitionAnimation.Direction.LEFT
                 else -> ActivityTransitionAnimation.Direction.FADE
-            }
-        }
-    }
-
-    /**
-     * Set the context for the calling activity (necessary for playing videos)
-     */
-    private fun SoundPlayer.setupVideoActivityCallback() {
-        val activityRef = WeakReference(this@AbstractFlashcardViewer)
-        this.playVideoExternallyCallback = { soundPath, onCompletionListener ->
-            val activity = activityRef.get()
-            if (activity == null) {
-                false
-            } else {
-                Timber.d("Requesting AbstractFlashcardViewer->VideoPlayer for video")
-                VideoPlayer.mediaCompletionListener = onCompletionListener
-                activity.startActivityWithoutAnimation(
-                    Intent(activity, VideoPlayer::class.java).apply {
-                        putExtra("path", soundPath)
-                    }
-                )
-                true
             }
         }
     }

AnkiDroid/src/main/java/com/ichi2/anki/VideoPlayer.kt
@@ -1,99 +1,0 @@
-/****************************************************************************************
- * Copyright (c) 2014 Timothy Rae <perceptualchaos2@gmail.com>                          *
- *                                                                                      *
- * This program is free software; you can redistribute it and/or modify it under        *
- * the terms of the GNU General Public License as published by the Free Software        *
- * Foundation; either version 3 of the License, or (at your option) any later           *
- * version.                                                                             *
- *                                                                                      *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
- * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
- *                                                                                      *
- * You should have received a copy of the GNU General Public License along with         *
- * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
- ****************************************************************************************/
-
-package com.ichi2.anki
-
-import android.app.Activity
-import android.content.res.Configuration
-import android.media.MediaPlayer
-import android.os.Bundle
-import android.view.SurfaceHolder
-import android.view.WindowManager.LayoutParams
-import android.widget.VideoView
-import com.ichi2.anki.UIUtils.showThemedToast
-import com.ichi2.libanki.VideoPlayer
-import com.ichi2.themes.Themes
-import timber.log.Timber
-
-class VideoPlayer : Activity(), SurfaceHolder.Callback {
-    private lateinit var mVideoView: VideoView
-    private lateinit var videoPlayer: VideoPlayer
-    private lateinit var path: String
-
-    /** Called when the activity is first created.  */
-    @Suppress("DEPRECATION") // #9332: UI Visibility -> Insets
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Themes.disableXiaomiForceDarkMode(this)
-        setContentView(R.layout.video_player)
-        this.path = intent.getStringExtra("path").let { path ->
-            if (path == null) {
-                // #5911 - May happen if launched externally. Not possible inside AnkiDroid
-                Timber.w("video path was null")
-                showThemedToast(this, getString(R.string.video_creation_error), true)
-                finish()
-                return
-            }
-            path
-        }
-
-        Timber.i("Video Player launched successfully")
-        window.apply {
-            setFlags(
-                LayoutParams.FLAG_FULLSCREEN,
-                LayoutParams.FLAG_FULLSCREEN
-            )
-            addFlags(LayoutParams.FLAG_KEEP_SCREEN_ON)
-        }
-        mVideoView = findViewById(R.id.video_surface)
-        videoPlayer = VideoPlayer(mVideoView)
-        mVideoView.holder.addCallback(this)
-    }
-
-    override fun surfaceCreated(holder: SurfaceHolder) {
-        Timber.i("surfaceCreated")
-        videoPlayer.play(path, { mp: MediaPlayer? ->
-            finish()
-            mediaCompletionListener?.onCompletion(mp)
-        }, null)
-    }
-
-    override fun surfaceChanged(
-        holder: SurfaceHolder,
-        format: Int,
-        width: Int,
-        height: Int
-    ) {
-        // intentionally blank: required for interface
-    }
-
-    override fun surfaceDestroyed(holder: SurfaceHolder) {
-        videoPlayer.stopSounds()
-        finish()
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        videoPlayer.notifyConfigurationChanged()
-    }
-
-    companion object {
-        /**
-         * OnCompletionListener so that external video player can notify to play next sound
-         */
-        var mediaCompletionListener: MediaPlayer.OnCompletionListener? = null
-    }
-}

AnkiDroid/src/main/res/layout/video_player.xml
@@ -1,13 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android" 
-  android:orientation="vertical"
-  android:layout_width="match_parent"
-  android:layout_height="match_parent">
-    <VideoView 
-        android:id="@+id/video_surface"
-        android:layout_width="match_parent"
-        android:layout_height="match_parent"
-        android:layout_gravity="center"
-        android:layout_centerInParent="true" />
-</RelativeLayout>
-    

AnkiDroid/src/main/res/values/03-dialogs.xml
@@ -152,8 +152,6 @@
     <string name="integrity_check_insufficient_space">Check Database uses a large amount of temporary storage.\n\nIt is strongly recommended that you have at least %s free space on your device before continuing.</string>
     <string name="integrity_check_insufficient_space_extra_content">\n\nYou currently have %s free.</string>
 
-    <string name="video_creation_error">Could not play video</string>
-
     <!-- Card Browser -->
     <!-- AbstractFlashCardViewer -->
     <string name="card_viewer_url_decode_error">Error decoding data from card</string>

AnkiDroid/src/test/java/com/ichi2/anki/ActivityStartupUnderBackupTest.kt
@@ -51,7 +51,6 @@ class ActivityStartupUnderBackupTest : RobolectricTest() {
     fun before() {
         notYetHandled(CropImageActivity::class.java.simpleName, "cannot implemented - activity from canhub.cropper")
         notYetHandled(IntentHandler::class.java.simpleName, "Not working (or implemented) - inherits from Activity")
-        notYetHandled(VideoPlayer::class.java.simpleName, "Not working (or implemented) - inherits from Activity")
         notYetHandled(Preferences::class.java.simpleName, "Not working (or implemented) - inherits from AppCompatPreferenceActivity")
         notYetHandled(FilteredDeckOptions::class.java.simpleName, "Not working (or implemented) - inherits from AppCompatPreferenceActivity")
     }

AnkiDroid/src/test/java/com/ichi2/testutils/ActivityList.kt
@@ -53,7 +53,6 @@ object ActivityList {
             get(ModelFieldEditor::class.java),
             // Likely has unhandled intents
             get(Reviewer::class.java),
-            get(VideoPlayer::class.java),
             get(MyAccount::class.java),
             get(Preferences::class.java),
             get(CropImageActivity::class.java),

commit 3f7b9263fa6e47128498a513b732f0b98d9bbee3

fix: audio is not stopped when changing cards
* `Sound` both inherited and contained a SoundPlayer

Broken in bac2b6718f86e64b4ba748bb0880d70531776ce6

Fixes 14616

AnkiDroid/src/main/java/com/ichi2/libanki/Sound.kt
@@ -42,7 +42,7 @@ private typealias SoundPath = String
  * Parses, loads and plays sound & video files
  * Called `Sound` Anki uses `[sound:]` for both audio and video
  */
-class Sound(private val soundPlayer: SoundPlayer, private val soundDir: String) : SoundPlayer() {
+class Sound(private val soundPlayer: SoundPlayer, private val soundDir: String) {
     /**
      * @param soundDir base path to the media files
      */
@@ -109,12 +109,20 @@ class Sound(private val soundPlayer: SoundPlayer, private val soundDir: String) 
         soundPathCollection.addAll(paths)
     }
 
+    fun playSound(
+        replacedUrl: String,
+        onCompletionListener: OnCompletionListener?,
+        soundErrorListener: OnErrorListener
+    ) {
+        soundPlayer.playSound(replacedUrl, onCompletionListener, soundErrorListener)
+    }
+
     /** Plays all the sounds for the indicated side(s)  */
     fun playSounds(side: SoundSide, errorListener: OnErrorListener?) {
         // If there are sounds to play for the current card, start with the first one
         val soundPaths = getSounds(side) ?: return
         Timber.d("playSounds: playing $side")
-        this.playSound(
+        this.soundPlayer.playSound(
             soundPaths[0],
             PlayAllCompletionListener(side, errorListener),
             errorListener
@@ -156,7 +164,7 @@ class Sound(private val soundPlayer: SoundPlayer, private val soundDir: String) 
             // If there are still more sounds to play for the current card, play the next one
             if (nextIndexToPlay < paths.size) {
                 Timber.i("Play all: Playing next sound")
-                playSound(paths[nextIndexToPlay++], this, errorListener)
+                soundPlayer.playSound(paths[nextIndexToPlay++], this, errorListener)
             } else {
                 Timber.i("Play all: Completed - releasing sound")
                 soundPlayer.stopSounds()
@@ -164,7 +172,8 @@ class Sound(private val soundPlayer: SoundPlayer, private val soundDir: String) 
         }
     }
 
-    override fun stopSounds() {
+    fun stopSounds() {
+        Timber.d("stopping sounds")
         soundPlayer.stopSounds()
         ReadText.stopTts() // TODO: Reconsider design
     }