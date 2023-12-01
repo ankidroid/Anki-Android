46c9893e6c26f819303a61c82c3f7c15d7e6b06b
peyman79 <peyman.eb15@gmail.com>
2023-12-01 21:20:58 +0330

    Fix changing the cards order to 'NoOrdering' after toggling to 'Notes'

--- AnkiDroid/src/main/java/com/ichi2/libanki/Collection.kt
+++ AnkiDroid/src/main/java/com/ichi2/libanki/Collection.kt
@@ -407,28 +407,6 @@ open class Collection(
         return findCards(search, SortOrder.NoOrdering())
     }
 
-    /** Return a list of card ids  */
-    @RustCleanup("Remove in V16.") // Not in libAnki
-    fun findOneCardByNote(query: String): List<CardId> {
-        // This function shouldn't exist and CardBrowser should be modified to use Notes,
-        // so not much effort was expended here
-
-        val noteIds = findNotes(query, SortOrder.NoOrdering())
-        // select the card with the lowest `ord` to show
-        return db.queryLongList(
-            """
-SELECT c.id
-FROM (
-  SELECT nid, MIN(ord) AS ord
-  FROM cards
-  WHERE nid IN ${Utils.ids2str(noteIds)} 
-  GROUP BY nid
-) AS card_with_min_ord
-JOIN cards AS c ON card_with_min_ord.nid = c.nid AND card_with_min_ord.ord = c.ord;
-            """.trimMargin()
-        )
-    }
-
     @RustCleanup("Calling code should handle returned OpChanges")
     fun findReplace(nids: List<Long>, src: String, dst: String, regex: Boolean = false, field: String? = null, fold: Boolean = true): Int {
         return backend.findAndReplace(nids, src, dst, regex, !fold, field ?: "").count
