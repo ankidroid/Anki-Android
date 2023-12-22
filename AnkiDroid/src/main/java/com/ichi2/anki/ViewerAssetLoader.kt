9e108f90864d68fe0fb0fcb8705b7d82f6fc4117
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2023-12-22 18:48:44 -0300

    refactor: remove Activity dependency from AnkiServer()
    which also allowed removing the ReviewerServer and moving the responsibilities back to the Reviewer

---
+++ AnkiDroid/src/main/java/com/ichi2/anki/ViewerAssetLoader.kt
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (c) 2023 Brayan Oliveira <brayandso.dev@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.ichi2.anki
+
+import android.content.Context
+import android.webkit.WebResourceResponse
+import androidx.webkit.WebViewAssetLoader
+import com.ichi2.utils.AssetHelper.guessMimeType
+import timber.log.Timber
+import java.io.File
+import java.io.FileInputStream
+
+fun Context.getViewerAssetLoader(domain: String): WebViewAssetLoader {
+    val mediaDir = CollectionHelper.getMediaDirectory(this)
+    return WebViewAssetLoader.Builder()
+        .setHttpAllowed(true)
+        .setDomain(domain)
+        .addPathHandler("/web/") { path: String ->
+            val inputStream = this.javaClass.classLoader!!.getResourceAsStream("web/$path")
+            WebResourceResponse(guessMimeType(path), null, inputStream)
+        }
+        .addPathHandler("/") { path: String ->
+            try {
+                val file = File(mediaDir, path)
+                val inputStream = FileInputStream(file)
+                WebResourceResponse(guessMimeType(path), null, inputStream)
+            } catch (e: Exception) {
+                Timber.d("File not found: %s", path)
+                null
+            }
+        }
+        .build()
+}
