262f35643370be60f59b641a7205389f9ad640c6
David Allison <62114487+david-allison@users.noreply.github.com>
2023-12-23 14:12:31 +0000

    refactor: kotlin cleanup
    * no longer exists

--- AnkiDroid/src/test/java/com/ichi2/libanki/MathJaxClozeTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/libanki/MathJaxClozeTest.kt
@@ -13,7 +13,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-@KotlinCleanup("removeFormattingFromMathjax was imported to stop bug in Kotlin: java.lang.NoSuchFieldError: INSTANCE")
 @KotlinCleanup("add testing function returning c.models.byName(\"Cloze\")")
 class MathJaxClozeTest : JvmTest() {
 
