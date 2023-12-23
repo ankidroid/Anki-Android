dfcf441f063d8eef9699611013dad7aa476b44fc
David Allison <62114487+david-allison@users.noreply.github.com>
2023-12-23 14:03:44 +0000

    refactor: kotlin cleanup
    already completed

--- AnkiDroid/src/androidTest/java/com/ichi2/anki/tests/ContentProviderTest.kt
+++ AnkiDroid/src/androidTest/java/com/ichi2/anki/tests/ContentProviderTest.kt
@@ -75,10 +75,6 @@ class ContentProviderTest : InstrumentedTest() {
      * Initially create one note for each model.
      */
     @Before
-    @Throws(
-        Exception::class
-    )
-    @KotlinCleanup("remove 'requireNoNulls' and fix mDummyFields")
     fun setUp() {
         Timber.i("setUp()")
         mCreatedNotes = ArrayList()
