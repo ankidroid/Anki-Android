dcc01fc292e0dee8a8605875c610a3d4f38a44df
David Allison <62114487+david-allison@users.noreply.github.com>
2024-01-01 15:36:38 +0000

    fix: white screen flicker in night mode
    Fixes 14194

--- AnkiDroid/src/main/java/com/ichi2/anki/pages/PageWebViewClient.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/pages/PageWebViewClient.kt
@@ -15,23 +15,33 @@
  */
 package com.ichi2.anki.pages
 
+import android.graphics.Bitmap
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.core.view.isVisible
 import com.google.android.material.color.MaterialColors
-import com.ichi2.anki.R
 import com.ichi2.utils.toRGBHex
+import timber.log.Timber
 
 /**
  * Base WebViewClient to be used on [PageFragment]
  */
 open class PageWebViewClient : WebViewClient() {
+
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        super.onPageStarted(view, url, favicon)
+        view?.let { webView ->
+            // 14194: background-color should be set before onPageFinished to stop a white flash
+            val bgColor = MaterialColors.getColor(webView, android.R.attr.colorBackground).toRGBHex()
+            webView.evaluateJavascript("""document.body.style.setProperty("background-color", "$bgColor", "important")""") {
+                Timber.v("backgroundColor set")
+            }
+        }
+    }
+
     override fun onPageFinished(view: WebView?, url: String?) {
         super.onPageFinished(view, url)
         view?.let { webView ->
-            val bgColor = MaterialColors.getColor(webView, android.R.attr.colorBackground).toRGBHex()
-            webView.evaluateJavascript("document.body.style.backgroundColor = '$bgColor';") {}
-
             /** [PageFragment.webView] is invisible by default to avoid flashes while
              * the page is loaded, and can be made visible again after it finishes loading */
             webView.isVisible = true
