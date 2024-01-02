96e31cdbef987a3d227b0ebbd8b904aeae83ae8a
Brayan Oliveira <69634269+BrayanDSO@users.noreply.github.com>
2024-01-02 11:47:33 -0300

    fix: mathjax render

--- AnkiDroid/src/main/assets/mathjax/conf.js
+++ AnkiDroid/src/main/assets/mathjax/conf.js
@@ -19,7 +19,7 @@ window.MathJax = {
     loader: {
         load: packagesForLoading(packages),
         paths: {
-            mathjax: "/assets/mathjax",
+            mathjax: "file:///android_asset/mathjax",
         },
     },
     startup: {
