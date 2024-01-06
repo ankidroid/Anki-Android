494b515db8f2c34f52f306628982be5e95d054f8
lukstbit <52494258+lukstbit@users.noreply.github.com>
2024-01-06 10:40:56 +0200

    Refactor DeckPickerContextMenu specific tests
    Changes:
    - migrates DeckPickerContextMenu MaterialDialog to AlertDialog
    - inline here the tests from DeckPickerContexMenuAndroidTest here.
    Didn't make sense to have two different files for the same class(also
    the name Android is a bit misleading)
    - add more tests to enforce the structure of the dialog based on the
    received arguments at start
    - removes the connection to RobolectricTest as the dialog doesn't need
    collection access

--- AnkiDroid/src/test/java/com/ichi2/anki/dialogs/DeckPickerContextMenuTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/dialogs/DeckPickerContextMenuTest.kt
@@ -17,22 +17,31 @@
 package com.ichi2.anki.dialogs
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.edit
-import androidx.fragment.app.testing.FragmentScenario
+import androidx.core.os.bundleOf
+import androidx.fragment.app.testing.FragmentScenario.Companion.launch
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.ichi2.anki.*
+import com.ichi2.anki.preferences.sharedPrefs
+import com.ichi2.libanki.DeckId
 import com.ichi2.testutils.BackupManagerTestUtilities.setupSpaceForBackup
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
 
 @RunWith(AndroidJUnit4::class)
-class DeckPickerContextMenuTest : RobolectricTest() {
+class DeckPickerContextMenuTest {
     @Before
     fun before() {
-        getPreferences().edit { putBoolean(IntroductionActivity.INTRODUCTION_SLIDES_SHOWN, true) }
-        setupSpaceForBackup(this.targetContext)
+        val context = ApplicationProvider.getApplicationContext<AnkiDroidApp>()
+        context.sharedPrefs().edit { putBoolean(IntroductionActivity.INTRODUCTION_SLIDES_SHOWN, true) }
+        setupSpaceForBackup(context)
     }
 
     @Test
@@ -56,11 +65,128 @@ class DeckPickerContextMenuTest : RobolectricTest() {
      */
     private fun startContextMenuWithMissingArgument(excluded: String) {
         val arguments = Bundle().apply {
-            if (excluded != "id") { DeckPickerContextMenu.ARG_DECK_ID to 1000L }
-            if (excluded != "name") { DeckPickerContextMenu.ARG_DECK_NAME to "Deck" }
-            if (excluded != "dynamic") { DeckPickerContextMenu.ARG_DECK_IS_DYN to false }
-            if (excluded != "hasBuried") { DeckPickerContextMenu.ARG_DECK_HAS_BURIED_IN_DECK to false }
+            if (excluded != "id") {
+                DeckPickerContextMenu.ARG_DECK_ID to 1000L
+            }
+            if (excluded != "name") {
+                DeckPickerContextMenu.ARG_DECK_NAME to "Deck"
+            }
+            if (excluded != "dynamic") {
+                DeckPickerContextMenu.ARG_DECK_IS_DYN to false
+            }
+            if (excluded != "hasBuried") {
+                DeckPickerContextMenu.ARG_DECK_HAS_BURIED_IN_DECK to false
+            }
         }
-        FragmentScenario.launch(DeckPickerContextMenu::class.java, arguments, R.style.Theme_Light)
+        launch(DeckPickerContextMenu::class.java, arguments, R.style.Theme_Light)
     }
+
+    @Test
+    fun `Shows standard options`() {
+        launch(
+            DeckPickerContextMenu::class.java,
+            withArguments(),
+            R.style.Theme_Light
+        ).onFragment { fragment ->
+            fragment.assertOptionPresent(R.string.menu_add)
+            fragment.assertOptionPresent(R.string.browse_cards)
+            fragment.assertOptionPresent(R.string.rename_deck)
+            fragment.assertOptionPresent(R.string.menu__deck_options)
+            fragment.assertOptionPresent(R.string.export_deck)
+            fragment.assertOptionPresent(R.string.create_shortcut)
+            fragment.assertOptionPresent(R.string.contextmenu_deckpicker_delete_deck)
+        }
+    }
+
+    private fun DeckPickerContextMenu.assertOptionPresent(optionStringRes: Int) {
+        val optionTitle = getString(optionStringRes)
+        assertTrue(
+            foundOptions().contains(optionTitle),
+            "'$optionTitle' should be present"
+        )
+    }
+
+    @Test
+    fun `DELETE_DECK is the last option in the menu(issue 10283)`() {
+        // "Delete deck" was previously close to "Custom study" which caused misclicks.
+        // This is less likely at the bottom of the list
+        launch(
+            DeckPickerContextMenu::class.java,
+            withArguments(),
+            R.style.Theme_Light
+        ).onFragment { fragment ->
+            MatcherAssert.assertThat(
+                "'Delete deck' should be last item in the menu",
+                fragment.foundOptions().last(),
+                equalTo(fragment.getString(R.string.contextmenu_deckpicker_delete_deck))
+            )
+        }
+    }
+
+    @Test
+    fun `Shows options to empty and rebuild when deck is dynamic`() {
+        launch(
+            DeckPickerContextMenu::class.java,
+            withArguments(isDynamic = true),
+            R.style.Theme_Light
+        ).onFragment { fragment ->
+            assertTrue(
+                fragment.foundOptions().contains(fragment.getString(R.string.empty_cram_label)),
+                "'Empty' should be present when deck is dynamic"
+            )
+            assertTrue(
+                fragment.foundOptions().contains(fragment.getString(R.string.rebuild_cram_label)),
+                "'Rebuild' should be present when deck is dynamic"
+            )
+        }
+    }
+
+    @Test
+    fun `Shows option to create subdeck when deck is not dynamic`() {
+        launch(
+            DeckPickerContextMenu::class.java,
+            withArguments(),
+            R.style.Theme_Light
+        ).onFragment { fragment ->
+            assertTrue(
+                fragment.foundOptions().contains(fragment.getString(R.string.create_subdeck)),
+                "'Create subdeck' should be present when deck is not dynamic"
+            )
+        }
+    }
+
+    @Test
+    fun `Shows option to unbury if deck has buried cards`() {
+        launch(
+            DeckPickerContextMenu::class.java,
+            withArguments(hasBuriedCards = true),
+            R.style.Theme_Light
+        ).onFragment { fragment ->
+            assertTrue(
+                fragment.foundOptions().contains(fragment.getString(R.string.unbury)),
+                "'Unbury' should be present when deck has buried cards"
+            )
+        }
+    }
+
+    private fun DeckPickerContextMenu.foundOptions(): List<String> {
+        val foundOptions = mutableListOf<String>()
+        val menuAdapter = (dialog as AlertDialog).listView.adapter
+        for (index in 0 until menuAdapter.count) {
+            foundOptions.add(menuAdapter.getItem(index).toString())
+        }
+        return foundOptions
+    }
+
+    private fun withArguments(
+        deckId: DeckId = 1000L,
+        deckName: String = "Deck 1",
+        isDynamic: Boolean = false,
+        hasBuriedCards: Boolean = false
+    ) = bundleOf(
+        DeckPickerContextMenu.ARG_DECK_ID to deckId,
+        DeckPickerContextMenu.ARG_DECK_NAME to deckName,
+        DeckPickerContextMenu.ARG_DECK_IS_DYN to isDynamic,
+        DeckPickerContextMenu.ARG_DECK_HAS_BURIED_IN_DECK to hasBuriedCards
+    )
 }
