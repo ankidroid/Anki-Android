047afc6a814ceea5a608196f93b1ce26bddbc63a
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2024-01-12 19:02:26 -0300

    feat: handle unbury link in congrats page
    TODO: handle some kind of dialog that I found in the code but I wasn't able to reproduce it in the desktop version, nor I was interested enough to keep trying. So I left as a TODO. source: https://github.com/ankitects/anki/blob/b23f17df276e2523a838be4cae5a08ca6fda1d71/qt/aqt/overview.py#L153

--- AnkiDroid/src/main/java/com/ichi2/anki/pages/CongratsPage.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/pages/CongratsPage.kt
@@ -20,12 +20,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.webkit.JavascriptInterface
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import com.ichi2.anki.OnErrorListener
 import com.ichi2.anki.OnPageFinishedCallback
+import com.ichi2.anki.R
 import com.ichi2.anki.SingleFragmentActivity
+import com.ichi2.anki.StudyOptionsActivity
+import com.ichi2.anki.launchCatching
+import com.ichi2.libanki.undoableOp
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class CongratsPage : PageFragment() {
     override val title: String = ""
     override val pageName = "congrats"
+
+    private val viewModel by viewModels<CongratsViewModel>()
 
     override fun onCreateWebViewClient(savedInstanceState: Bundle?): PageWebViewClient {
         return super.onCreateWebViewClient(savedInstanceState).also { client ->
@@ -39,19 +54,58 @@ class CongratsPage : PageFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.onError
+            .flowWithLifecycle(lifecycle)
+            .onEach { errorMessage ->
+                AlertDialog.Builder(requireContext())
+                    .setTitle(R.string.vague_error)
+                    .setMessage(errorMessage)
+                    .show()
+            }
+            .launchIn(lifecycleScope)
+
+        viewModel.openStudyOptions
+            .onEach { openStudyOptionsAndFinish() }
+            .launchIn(lifecycleScope)
+
         webView.addJavascriptInterface(BridgeCommand(), "ankidroid")
     }
 
     inner class BridgeCommand {
         @JavascriptInterface
         fun bridgeCommand(request: String) {
-            TODO("$request will be handled in the next commit ;)")
+            when (request) {
+                "unbury" -> viewModel.onUnbury()
+            }
         }
+    }
+
+    private fun openStudyOptionsAndFinish() {
+        val intent = Intent(requireContext(), StudyOptionsActivity::class.java).apply {
+            putExtra("withDeckOptions", false)
+        }
+        startActivity(intent, null)
+        requireActivity().finish()
     }
 
     companion object {
         fun getIntent(context: Context): Intent {
             return SingleFragmentActivity.getIntent(context, CongratsPage::class)
+        }
+    }
+}
+
+class CongratsViewModel : ViewModel(), OnErrorListener {
+    override val onError = MutableSharedFlow<String>()
+    val openStudyOptions = MutableSharedFlow<Boolean>()
+
+    fun onUnbury() {
+        launchCatching {
+            undoableOp {
+                sched.unburyDeck(decks.getCurrentId())
+            }
+            openStudyOptions.emit(true)
         }
     }
 }
