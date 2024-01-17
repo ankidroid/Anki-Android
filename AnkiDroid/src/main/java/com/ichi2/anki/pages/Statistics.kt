2fb2e5ee817f179bc60bb0b65e471f9175430a90
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2024-01-16 21:02:33 -0300

    refactor: move Statistics view inflation

--- AnkiDroid/src/main/java/com/ichi2/anki/pages/Statistics.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/pages/Statistics.kt
@@ -20,9 +20,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.print.PrintAttributes
 import android.print.PrintManager
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.core.content.ContextCompat.getSystemService
 import com.google.android.material.appbar.MaterialToolbar
 import com.ichi2.anki.CollectionManager
@@ -37,14 +35,8 @@ class Statistics : PageFragment() {
 
     override val pageName = "graphs"
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = super.onCreateView(inflater, container, savedInstanceState)
-
-        view?.findViewById<MaterialToolbar>(R.id.toolbar)?.apply {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        view.findViewById<MaterialToolbar>(R.id.toolbar)?.apply {
             inflateMenu(R.menu.statistics)
             menu.findItem(R.id.action_export_stats).title = CollectionManager.TR.statisticsSavePdf()
             setOnMenuItemClickListener { item ->
@@ -54,8 +46,6 @@ class Statistics : PageFragment() {
                 true
             }
         }
-
-        return view
     }
 
     /**Prepares and initiates a printing task for the content(stats) displayed in the WebView.
