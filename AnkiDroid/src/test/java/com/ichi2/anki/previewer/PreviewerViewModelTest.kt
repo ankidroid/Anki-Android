717c7f19e3ee557eb3417f3bfa8f163d299f34d9
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2024-01-17 20:29:05 -0300

    feat: handle type-in answers in the new previewer the desktop previewer doesn't show type-in answer previews (https://docs.ankiweb.net/templates/fields.html#checking-your-answer), neither AnkiMobile does
    AnkiDroid currently has a TextInput to insert the answer and check it when the answer is shown
    
    For the new previewer, I'm going with a midterm of not showing the type box in the question like Anki Desktop/Mobile, since the previewer goal should only be previewing stuff, but showing the result in the answer so the user can preview what they want

---
+++ AnkiDroid/src/test/java/com/ichi2/anki/previewer/PreviewerViewModelTest.kt
@@ -0,0 +1,30 @@
+/*
+ *  Copyright (c) 2024 Brayan Oliveira <brayandso.dev@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.ichi2.anki.previewer
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.jupiter.api.Test
+
+class PreviewerViewModelTest {
+    @Test
+    fun `type answer fields are removed in questions`() {
+        assertThat(
+            PreviewerViewModel.typeAnsQuestionFilter("creu [[type:leu]]"),
+            equalTo("creu ")
+        )
+    }
+}
