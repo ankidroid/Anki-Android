3fef242bfeda7b3f44763e1f32438e44f205cbdf
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2024-01-21 11:38:38 -0300

    refactor: remove col parameter from loadCardSounds

--- AnkiDroid/src/main/java/com/ichi2/anki/cardviewer/SoundPlayer.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/cardviewer/SoundPlayer.kt
@@ -40,7 +40,6 @@ import com.ichi2.anki.cardviewer.SoundSide.QUESTION_AND_ANSWER
 import com.ichi2.annotations.NeedsTest
 import com.ichi2.libanki.AvTag
 import com.ichi2.libanki.Card
-import com.ichi2.libanki.Collection
 import com.ichi2.libanki.SoundOrVideoTag
 import com.ichi2.libanki.TTSTag
 import com.ichi2.libanki.TtsPlayer
@@ -115,11 +114,11 @@ class SoundPlayer(
         close()
     }
 
-    suspend fun loadCardSounds(col: Collection, card: Card, side: Side) {
+    suspend fun loadCardSounds(card: Card, side: Side) {
         Timber.i("loading sounds for card %s (%s)", card.id, side)
         stopSounds()
-        this.questions = card.renderOutput(col).questionAvTags
-        this.answers = card.renderOutput(col).answerAvTags
+        this.questions = withCol { card.renderOutput(this).questionAvTags }
+        this.answers = withCol { card.renderOutput(this).answerAvTags }
         this.side = side
 
         if (!this::config.isInitialized || !config.appliesTo(card)) {
