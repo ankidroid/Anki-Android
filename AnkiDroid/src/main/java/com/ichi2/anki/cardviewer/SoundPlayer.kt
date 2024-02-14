ankidroid/Anki-Android

commit ee5e26702ff55cd83a8dad5bbdab3550c27b26de

feat: Rewrite SoundPlayer + add TTS

AnkiDroid/src/main/java/com/ichi2/anki/cardviewer/SoundPlayer.kt
@@ -16,26 +16,382 @@
 
 package com.ichi2.anki.cardviewer
 
+import android.media.MediaPlayer
+import android.net.Uri
+import androidx.annotation.CheckResult
+import androidx.core.net.toFile
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.coroutineScope
+import androidx.lifecycle.lifecycleScope
+import com.ichi2.anki.AbstractFlashcardViewer
+import com.ichi2.anki.AndroidTtsPlayer
+import com.ichi2.anki.cardviewer.SoundErrorBehavior.CONTINUE_AUDIO
+import com.ichi2.anki.cardviewer.SoundErrorBehavior.RETRY_AUDIO
+import com.ichi2.anki.cardviewer.SoundErrorBehavior.STOP_AUDIO
+import com.ichi2.annotations.NeedsTest
+import com.ichi2.libanki.AvTag
+import com.ichi2.libanki.Card
+import com.ichi2.libanki.Sound.SoundSide
+import com.ichi2.libanki.Sound.SoundSide.ANSWER
+import com.ichi2.libanki.Sound.SoundSide.QUESTION
+import com.ichi2.libanki.Sound.SoundSide.QUESTION_AND_ANSWER
 import com.ichi2.libanki.SoundOrVideoTag
 import com.ichi2.libanki.TTSTag
+import com.ichi2.libanki.TtsPlayer
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeoutOrNull
+import timber.log.Timber
+import java.io.Closeable
+import java.io.File
 
 /**
- * Work in Progress
- *
  * Handles the two ways an Anki card defines sound:
  * * Regular Sound (file-based, mp3 etc..): [SoundOrVideoTag]
+ *   *  No docs for [sound:], but this handles Sound or Video with a reference to the file
+ *   * `[sound:audio.mp3]` in a field
+ *  * in the media directory.
  * * Text to Speech [TTSTag]
- *
- * https://docs.ankiweb.net/templates/fields.html?highlight=tts#text-to-speech
- * No manual reference for [sound:], but this handles Sound or Video with a reference to the file
- * in the media directory.
- *
- * AnkiDroid also introduced a "tts" setting, which existed before Anki Desktop TTS.
- * This only allowed TTS if a setting was enabled,
+ *   * [docs][https://docs.ankiweb.net/templates/fields.html?highlight=tts#text-to-speech]
+ *   * `{{tts en_GB:Front}}` on the card template
  *
  * This class combines the above concerns behind an "adapter" interface in order to simplify complexity.
  *
- * I hope that we can then test and reduce the complexity of this class.
+ * **Public interface**
+ * * [playAllSounds]
+ * * [replayAllSounds]
+ * * [playOneSound]
+ * * [stopSounds]
+ * * [loadCardSounds] - informs the class of whether we're on the front/back of a card
+ *
+ * @see AvTag
+ *
+ * @param onSoundGroupCompleted Function to be called when [playAllSounds] or [replayAllSounds] completes
+ *
+ * **Out of scope**
+ * [com.ichi2.anki.ReadText]: AnkiDroid has a legacy "tts" setting, before Anki Desktop TTS.
+ * This uses [com.ichi2.anki.MetaDB], and may either read `<tts>` or all text on a card
+ *
  */
-@Suppress("unused")
-class SoundPlayer
+class SoundPlayer(
+    private val soundTagPlayer: SoundTagPlayer,
+    private val ttsPlayer: Deferred<TtsPlayer>,
+    private val lifecycle: Lifecycle,
+    private val onSoundGroupCompleted: () -> Unit,
+    private val soundErrorListener: SoundErrorListener
+) : DefaultLifecycleObserver, Closeable {
+
+    private lateinit var questions: List<AvTag>
+    private lateinit var answers: List<AvTag>
+    private lateinit var side: Side
+
+    lateinit var config: CardSoundConfig
+
+    private var playSoundsJob: Job? = null
+
+    val scope get() = lifecycle.coroutineScope
+
+    override fun onPause(owner: LifecycleOwner) {
+        super.onPause(owner)
+        scope.launch { stopSounds() }
+    }
+
+    override fun onDestroy(owner: LifecycleOwner) {
+        super.onDestroy(owner)
+        close()
+    }
+
+    suspend fun loadCardSounds(card: Card, side: Side) {
+        Timber.i("loading sounds for card %s (%s)", card.id, side)
+        stopSounds()
+        this.questions = card.renderOutput().questionAvTags
+        this.answers = card.renderOutput().answerAvTags
+        this.side = side
+
+        if (!this::config.isInitialized || !config.appliesTo(card)) {
+            config = CardSoundConfig.create(card)
+        }
+    }
+
+    private suspend fun playAllSoundsForSide(soundSide: SoundSide) {
+        if (!canPlaySounds()) {
+            return
+        }
+        cancelPlaySoundsJob()
+        Timber.i("playing sounds for %s", soundSide)
+        this.playSoundsJob = scope.launch(Dispatchers.IO) {
+            playAllSoundsInternal(soundSide)
+            playSoundsJob = null
+        }
+    }
+
+    suspend fun playOneSound(tag: AvTag) {
+        if (!canPlaySounds()) {
+            return
+        }
+        cancelPlaySoundsJob()
+        Timber.i("playing one sound")
+
+        suspend fun retry() {
+            try {
+                play(tag)
+            } catch (e: CancellationException) {
+                throw e
+            } catch (e: Exception) {
+                Timber.w(e, "failed to replay audio")
+            }
+        }
+
+        playSoundsJob = scope.launch(Dispatchers.IO) {
+            try {
+                play(tag)
+            } catch (e: SoundException) {
+                when (e.continuationBehavior) {
+                    RETRY_AUDIO -> retry()
+                    CONTINUE_AUDIO, STOP_AUDIO -> { }
+                }
+            } catch (e: CancellationException) {
+                throw e
+            } catch (e: Exception) {
+                Timber.w(e, "Exception playing sound")
+            }
+            Timber.v("completed playing one sound")
+            playSoundsJob = null
+        }
+    }
+
+    suspend fun stopSounds() {
+        Timber.i("stopping sounds")
+        cancelPlaySoundsJob()
+    }
+
+    override fun close() {
+        soundTagPlayer.releaseSound()
+        try {
+            ttsPlayer.getCompleted().close()
+        } catch (e: Exception) {
+            Timber.i(e, "ttsPlayer close()")
+        }
+    }
+
+    private suspend fun cancelPlaySoundsJob() {
+        playSoundsJob?.let {
+            Timber.i("cancelling job")
+            withContext(Dispatchers.IO) {
+                playSoundsJob?.cancelAndJoin()
+            }
+        }
+    }
+
+    /**
+     * Obtains all the sounds for the [soundSide] and plays them sequentially
+     */
+    private suspend fun playAllSoundsInternal(soundSide: SoundSide) {
+        if (!canPlaySounds()) {
+            return
+        }
+        val soundList = when (soundSide) {
+            QUESTION -> questions
+            ANSWER -> answers
+            QUESTION_AND_ANSWER -> questions + answers
+        }
+
+        try {
+            for ((index, sound) in soundList.withIndex()) {
+                Timber.d("playing sound %d/%d", index + 1, soundList.size)
+                if (!play(sound)) {
+                    Timber.d("stopping sound playback early")
+                    return
+                }
+            }
+        } finally {
+            // call the completion listener, even if a CancellationException was thrown
+            onSoundGroupCompleted()
+        }
+    }
+
+    /**
+     * Plays the provided [tag] and returns whether playback should continue
+     * @return whether playback should continue: `true`: continue, `false`: stop playback
+     */
+    private suspend fun play(tag: AvTag): Boolean = withContext(Dispatchers.IO) {
+        suspend fun play() {
+            ensureActive()
+            when (tag) {
+                is SoundOrVideoTag -> soundTagPlayer.play(tag, soundErrorListener)
+                is TTSTag -> {
+                    awaitTtsPlayer()?.play(tag)
+                }
+                else -> Timber.w("unknown audio: ${tag.javaClass}")
+            }
+            ensureActive()
+        }
+
+        try {
+            play()
+        } catch (e: SoundException) {
+            when (e.continuationBehavior) {
+                STOP_AUDIO -> return@withContext false
+                CONTINUE_AUDIO -> return@withContext true
+                RETRY_AUDIO -> {
+                    try {
+                        play()
+                    } catch (e: Exception) {
+                        Timber.w("failed to replay audio", e)
+                    }
+                }
+            }
+        } catch (e: CancellationException) {
+            throw e
+        } catch (e: Exception) {
+            Timber.w("Unexpected audio exception. Continuing", e)
+        }
+        return@withContext true
+    }
+
+    fun hasSounds(displayAnswer: Boolean): Boolean =
+        if (displayAnswer) answers.any() else questions.any()
+
+    /**
+     * Plays all sounds for the current side, calling [onSoundGroupCompleted] when completed
+     */
+    suspend fun playAllSounds() = when (side) {
+        Side.FRONT -> playAllSoundsForSide(QUESTION)
+        Side.BACK -> playAllSoundsForSide(ANSWER)
+    }
+
+    /**
+     * Replays all sounds for the current side, calling [onSoundGroupCompleted] when completed
+     */
+    suspend fun replayAllSounds() = when (side) {
+        Side.BACK -> if (config.replayQuestion) playAllSoundsForSide(QUESTION_AND_ANSWER) else playAllSoundsForSide(ANSWER)
+        Side.FRONT -> playAllSoundsForSide(QUESTION)
+    }
+
+    private suspend fun awaitTtsPlayer(): TtsPlayer? {
+        return withTimeoutOrNull(TTS_PLAYER_TIMEOUT_MS) {
+            ttsPlayer.await()
+        }
+    }
+
+    private fun canPlaySounds(): Boolean {
+        if (!lifecycle.currentState.isAtLeast(Lifecycle.State.RESUMED)) {
+            Timber.w("sounds are not played as the activity is inactive")
+            return false
+        }
+        return true
+    }
+    companion object {
+        const val TTS_PLAYER_TIMEOUT_MS = 2_500L
+
+        /**
+         * @param soundUriBase The base path to the sound directory as a `file://` URI
+         */
+        @NeedsTest("ensure the lifecycle is subscribed to in a Reviewer")
+        fun newInstance(viewer: AbstractFlashcardViewer, soundUriBase: String): SoundPlayer {
+            val scope = viewer.lifecycleScope
+            val soundErrorListener = viewer.createSoundErrorListener(soundUriBase)
+            // tts can take a long time to init, this defers the operation until it's needed
+            val tts = scope.async(Dispatchers.IO) { AndroidTtsPlayer.createInstance(viewer, viewer.lifecycleScope) }
+
+            val soundPlayer = SoundTagPlayer(soundUriBase)
+
+            return SoundPlayer(
+                soundTagPlayer = soundPlayer,
+                ttsPlayer = tts,
+                lifecycle = viewer.lifecycle,
+                onSoundGroupCompleted = viewer::onSoundGroupCompleted,
+                soundErrorListener = soundErrorListener
+            ).apply {
+                viewer.lifecycle.addObserver(this)
+            }
+        }
+    }
+}
+
+interface SoundErrorListener {
+    @CheckResult
+    fun onError(uri: Uri): SoundErrorBehavior
+
+    @CheckResult
+    fun onMediaPlayerError(mp: MediaPlayer?, which: Int, extra: Int, tag: SoundOrVideoTag): SoundErrorBehavior
+}
+
+enum class SoundErrorBehavior {
+    /** Stop playing audio */
+    STOP_AUDIO,
+
+    /** Continue to the next audio (if any) */
+    CONTINUE_AUDIO,
+
+    /** Retry the current audio */
+    RETRY_AUDIO
+}
+
+fun AbstractFlashcardViewer.createSoundErrorListener(baseUri: String): SoundErrorListener {
+    return object : SoundErrorListener {
+        private var handledError: HashSet<String> = hashSetOf()
+
+        private fun AbstractFlashcardViewer.handleStorageMigrationError(file: File): Boolean {
+            val migrationService = migrationService ?: return false
+            if (handledError.contains(file.absolutePath)) {
+                return false
+            }
+            handledError.add(file.absolutePath)
+            return migrationService.migrateFileImmediately(file)
+        }
+
+        override fun onMediaPlayerError(
+            mp: MediaPlayer?,
+            which: Int,
+            extra: Int,
+            tag: SoundOrVideoTag
+        ): SoundErrorBehavior {
+            Timber.w("Media Error: (%d, %d)", which, extra)
+            val uri = try {
+                Uri.parse(baseUri + tag.filename)
+            } catch (e: Exception) {
+                Timber.w(e)
+                return CONTINUE_AUDIO
+            }
+            return onError(uri)
+        }
+
+        override fun onError(uri: Uri): SoundErrorBehavior {
+            try {
+                val file = uri.toFile()
+                if (file.exists()) return CONTINUE_AUDIO
+                // file doesn't exist - may be due to scoped storage
+                if (handleStorageMigrationError(file)) {
+                    return RETRY_AUDIO
+                }
+                // just doesn't exist - process the error
+                AbstractFlashcardViewer.mMissingImageHandler.processMissingSound(file) { filename: String? -> displayCouldNotFindMediaSnackbar(filename) }
+                return CONTINUE_AUDIO
+            } catch (e: Exception) {
+                Timber.w(e)
+                return CONTINUE_AUDIO
+            }
+        }
+    }
+}
+
+/** An exception thrown when playing a sound, and how to continue playing sounds */
+class SoundException : Exception {
+    val continuationBehavior: SoundErrorBehavior
+    constructor(errorHandling: SoundErrorBehavior) : super() {
+        this.continuationBehavior = errorHandling
+    }
+    constructor(errorHandling: SoundErrorBehavior, exception: Exception) : super(exception) {
+        this.continuationBehavior = errorHandling
+    }
+}

AnkiDroid/src/main/java/com/ichi2/anki/cardviewer/SoundTagPlayer.kt
@@ -1,0 +1,156 @@
+/*
+ *  Copyright (c) 2023 David Allison <davidallisongithub@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ichi2.anki.cardviewer
+
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.net.Uri
+import androidx.media.AudioFocusRequestCompat
+import androidx.media.AudioManagerCompat
+import com.ichi2.anki.AnkiDroidApp
+import com.ichi2.libanki.SoundOrVideoTag
+import kotlinx.coroutines.suspendCancellableCoroutine
+import timber.log.Timber
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+/** Player for the sounds of [SoundOrVideoTag] */
+class SoundTagPlayer(private val soundUriBase: String) {
+    private var mediaPlayer: MediaPlayer? = null
+
+    private val music = AudioAttributes.Builder()
+        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+        .build()
+
+    /**
+     * AudioManager to request/release audio focus
+     */
+    private var audioManager: AudioManager =
+        AnkiDroidApp.instance.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+    // the same instance of an AudioFocusRequestCompat must be used to cancel focus
+    private val audioFocusRequest: AudioFocusRequestCompat by lazy {
+        AudioFocusRequestCompat.Builder(AudioManagerCompat.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK)
+            .setOnAudioFocusChangeListener { }
+            .build()
+    }
+
+    /**
+     * @throws SoundException if the file does not exist, or if media playing fails
+     * @param soundErrorListener handles a sound error and returns how to continue playing sounds
+     */
+    suspend fun play(
+        tag: SoundOrVideoTag,
+        soundErrorListener: SoundErrorListener
+    ) = suspendCancellableCoroutine { continuation ->
+        Timber.d("Playing SoundOrVideoTag")
+        requireNewMediaPlayer().apply {
+            continuation.invokeOnCancellation {
+                Timber.i("stopping MediaPlayer due to cancellation")
+                stopSounds()
+            }
+            setOnCompletionListener {
+                Timber.v("finished playing SoundOrVideoTag successfully")
+                abandonAudioFocus()
+                continuation.resume(Unit)
+            }
+
+            setAudioAttributes(music)
+            setOnErrorListener { mp, what, extra ->
+                abandonAudioFocus()
+                val continuationBehavior =
+                    soundErrorListener.onMediaPlayerError(mp, what, extra, tag)
+                continuation.resumeWithException(SoundException(continuationBehavior))
+                true // do not call onCompletionListen
+            }
+
+            val soundUri = Uri.parse(soundUriBase + tag.filename)
+            try {
+                awaitSetDataSource(soundUri)
+            } catch (e: Exception) {
+                val continuationBehavior = soundErrorListener.onError(soundUri)
+                val exception = SoundException(continuationBehavior, e)
+                return@suspendCancellableCoroutine continuation.resumeWithException(exception)
+            }
+
+            requestAudioFocus()
+            start()
+        }
+    }
+
+    /**
+     * Releases the sound.
+     */
+    fun releaseSound() {
+        Timber.d("Releasing sounds and abandoning audio focus")
+        mediaPlayer?.let {
+            // Required to remove warning: "mediaplayer went away with unhandled events"
+            // https://stackoverflow.com/questions/9609479/android-mediaplayer-went-away-with-unhandled-events
+            it.reset()
+            it.release()
+            mediaPlayer = null
+        }
+        abandonAudioFocus()
+    }
+
+    fun stopSounds() {
+        try {
+            mediaPlayer?.stop()
+        } catch (e: Exception) {
+            Timber.w(e, "stopSounds()")
+        }
+        abandonAudioFocus()
+    }
+
+    /**
+     * Produces a usable [MediaPlayer], either creating a new instance or resetting the current
+     * instance
+     */
+    private fun requireNewMediaPlayer(): MediaPlayer {
+        if (mediaPlayer == null) {
+            Timber.d("Creating media player for playback")
+            // PERF: see if this is slow, maybe move to a task on instantiation
+            mediaPlayer = MediaPlayer()
+        } else {
+            Timber.d("Resetting media for playback")
+            mediaPlayer!!.reset()
+        }
+        return mediaPlayer!!
+    }
+
+    /**
+     * @throws IllegalStateException if the sound player is in an invalid state
+     * @throws java.io.FileNotFoundException file is not found
+     * @throws java.io.IOException: Prepare failed.: status=0x1
+     */
+    private fun MediaPlayer.awaitSetDataSource(uri: Uri) {
+        setDataSource(AnkiDroidApp.instance.applicationContext, uri)
+        prepare()
+    }
+
+    private fun requestAudioFocus() {
+        Timber.d("Requesting audio focus")
+        AudioManagerCompat.requestAudioFocus(audioManager, audioFocusRequest)
+    }
+
+    private fun abandonAudioFocus() {
+        Timber.d("Abandoning audio focus")
+        AudioManagerCompat.abandonAudioFocusRequest(audioManager, audioFocusRequest)
+    }
+}

AnkiDroid/src/main/java/com/ichi2/anki/reviewer/AutomaticAnswer.kt
@@ -70,6 +70,14 @@ class AutomaticAnswer(
      */
     var isDisabled: Boolean = false
         private set
+
+    /**
+     * Whether the sounds for the current side of the card have been played
+     * So [scheduleAutomaticDisplayAnswer] and [scheduleAutomaticDisplayQuestion] do nothing
+     * if called multiple times on the same side of the card
+     */
+    private var hasPlayedSounds: Boolean = false
+
     private val showAnswerTask = Runnable {
         if (isDisabled) {
             Timber.d("showAnswer: disabled")
@@ -136,6 +144,7 @@ class AutomaticAnswer(
     fun onDisplayQuestion() {
         if (!settings.useTimer) return
         if (!settings.autoAdvanceAnswer) return
+        hasPlayedSounds = false
 
         stopShowAnswerTask()
     }
@@ -144,6 +153,7 @@ class AutomaticAnswer(
     fun onDisplayAnswer() {
         if (!settings.useTimer) return
         if (!settings.autoAdvanceQuestion) return
+        hasPlayedSounds = false
 
         stopShowQuestionTask()
     }
@@ -170,6 +180,8 @@ class AutomaticAnswer(
     fun scheduleAutomaticDisplayAnswer(additionalDelay: Long = 0) {
         if (!settings.useTimer) return
         if (!settings.autoAdvanceAnswer) return
+        if (hasPlayedSounds) return
+        hasPlayedSounds = true
         delayedShowAnswer(settings.answerDelayMilliseconds + additionalDelay)
     }
 
@@ -180,6 +192,8 @@ class AutomaticAnswer(
     fun scheduleAutomaticDisplayQuestion(additionalMediaDelay: Long = 0) {
         if (!settings.useTimer) return
         if (!settings.autoAdvanceQuestion) return
+        if (hasPlayedSounds) return
+        hasPlayedSounds = true
         delayedShowQuestion(settings.questionDelayMilliseconds + additionalMediaDelay)
     }
 
@@ -190,6 +204,7 @@ class AutomaticAnswer(
     @VisibleForTesting(otherwise = VisibleForTesting.NONE)
     internal fun simulateCardFlip() {
         Timber.d("simulateCardFlip")
+        hasPlayedSounds = false
     }
 
     interface AutomaticallyAnswered {

commit 67adb20d6280fd3af49e29eff101fd32fd9ba2bd

fix: legacy TTS should stop on card flip
Issue introduced in: ee5e267

Cause: mis-abstraction, and didn't stop the legacy TTS in the new TTS Player

https://github.com/ankidroid/Anki-Android/commit/ee5e26702ff55cd83a8dad5bbdab3550c27b26de#diff-7cd4e14b716b1bcd35f3cb3318da36eb706289a8e79658266d1f37d4ff8df796L178

Fixes 15423

AnkiDroid/src/main/java/com/ichi2/anki/cardviewer/SoundPlayer.kt
@@ -29,6 +29,7 @@ import com.ichi2.anki.AndroidTtsPlayer
 import com.ichi2.anki.AnkiDroidApp
 import com.ichi2.anki.CollectionHelper.Companion.getMediaDirectory
 import com.ichi2.anki.CollectionManager.withCol
+import com.ichi2.anki.ReadText
 import com.ichi2.anki.cardviewer.SoundErrorBehavior.CONTINUE_AUDIO
 import com.ichi2.anki.cardviewer.SoundErrorBehavior.RETRY_AUDIO
 import com.ichi2.anki.cardviewer.SoundErrorBehavior.STOP_AUDIO
@@ -186,6 +187,7 @@ class SoundPlayer : Closeable {
     suspend fun stopSounds() {
         if (playSoundsJob != null) Timber.i("stopping sounds")
         cancelPlaySoundsJob(playSoundsJob)
+        ReadText.stopTts() // TODO: Reconsider design
     }
 
     override fun close() {