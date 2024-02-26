e12d341e0bdb18d25f5329a2fc6e74044643de03
yoksire <79806563+yoksire@users.noreply.github.com>
2024-02-26 16:17:16 +0530

    no changes

--- AnkiDroid/src/main/java/com/ichi2/libanki/Consts.kt
+++ AnkiDroid/src/main/java/com/ichi2/libanki/Consts.kt
@@ -93,7 +93,7 @@ object Consts {
     // The labels defined in consts.py are in AnkiDroid's resources files.
     const val DEFAULT_DECK_ID: Long = 1
 
-    val FIELD_SEPARATOR = Character.toString('\u001f')
+    val FIELD_SEPARATOR: String = Character.toString('\u001f')
 
     /** Time duration for toast **/
     const val SHORT_TOAST_DURATION: Long = 2000
