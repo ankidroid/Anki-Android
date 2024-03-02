f36f6a47a994afcf8ea67544f4982b1344e98b17
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2024-03-02 22:54:45 +0530

    revert: Disable widget if no storage access * On  (<= API 30 [Android 11]), widgets cannot be enabled in code if defined as enabled=false in the manifest (Android bug #15745).

--- AnkiDroid/src/main/java/com/ichi2/anki/ui/windows/permissions/PermissionsActivity.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/ui/windows/permissions/PermissionsActivity.kt
@@ -24,8 +24,6 @@ import androidx.appcompat.widget.AppCompatButton
 import androidx.core.content.IntentCompat
 import androidx.fragment.app.commit
 import com.ichi2.anki.AnkiActivity
-import com.ichi2.anki.IntentHandler
-import com.ichi2.anki.NavigationDrawerActivity
 import com.ichi2.anki.PermissionSet
 import com.ichi2.anki.R
 import com.ichi2.annotations.NeedsTest
@@ -70,11 +68,6 @@ class PermissionsActivity : AnkiActivity() {
 
     fun setContinueButtonEnabled(isEnabled: Boolean) {
         findViewById<AppCompatButton>(R.id.continue_button).isEnabled = isEnabled
-        // in case it's not play build
-        if (isEnabled) {
-            IntentHandler.enableWidgets(this)
-            NavigationDrawerActivity.enablePostShortcut(this)
-        }
     }
 
     companion object {
