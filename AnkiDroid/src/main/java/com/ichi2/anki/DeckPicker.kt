61adc92fc58094a918da5e3851e30533d31e857c
CyberButton <87766623307Kk>
2024-03-07 15:43:18 +0500

    fix: dont reset scroll state

--- AnkiDroid/src/main/java/com/ichi2/anki/DeckPicker.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/DeckPicker.kt
@@ -2131,6 +2131,7 @@ open class DeckPicker :
                     decks.removeDecks(listOf(did))
                 }
             }
+            focusedDeck = 1
             showSnackbar(TR.browsingCardsDeleted(changes.count), Snackbar.LENGTH_SHORT) {
                 setAction(R.string.undo) { undo() }
             }
