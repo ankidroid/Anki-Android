33cc3fa2e0f0e55817638b5715a68bd72659e170
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2024-03-09 16:04:17 +0530

    fix: Widget intent not updated if added before storage permission
    (cherry picked from commit 46924e78a9d8e087515ec4c9a02eb9cc4aab15e8)

--- AnkiDroid/src/main/java/com/ichi2/widget/AddNoteWidget.kt
+++ AnkiDroid/src/main/java/com/ichi2/widget/AddNoteWidget.kt
@@ -44,11 +44,30 @@ class AddNoteWidget : AppWidgetProvider() {
             return
         }
         Timber.d("onUpdate")
-        val remoteViews = RemoteViews(context.packageName, R.layout.widget_add_note)
-        val intent = Intent(context, NoteEditor::class.java)
-        intent.putExtra(NoteEditor.EXTRA_CALLER, NoteEditor.CALLER_DECKPICKER)
-        val pendingIntent = PendingIntentCompat.getActivity(context, 0, intent, 0, false)
-        remoteViews.setOnClickPendingIntent(R.id.widget_add_note_button, pendingIntent)
-        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews)
+        updateWidgets(context, appWidgetManager, appWidgetIds)
+    }
+
+    companion object {
+
+        /**
+         * Updates the widgets displayed in the provided context using the given AppWidgetManager
+         * and widget IDs, setting up an intent to open the NoteEditor with the caller as the deck picker.
+         *
+         * @param context The context in which the widgets are updated.
+         * @param appWidgetManager The AppWidgetManager instance used to update widgets.
+         * @param appWidgetIds The array of widget IDs to be updated.
+         */
+        fun updateWidgets(
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetIds: IntArray
+        ) {
+            val remoteViews = RemoteViews(context.packageName, R.layout.widget_add_note)
+            val intent = Intent(context, NoteEditor::class.java)
+            intent.putExtra(NoteEditor.EXTRA_CALLER, NoteEditor.CALLER_DECKPICKER)
+            val pendingIntent = PendingIntentCompat.getActivity(context, 0, intent, 0, false)
+            remoteViews.setOnClickPendingIntent(R.id.widget_add_note_button, pendingIntent)
+            appWidgetManager.updateAppWidget(appWidgetIds, remoteViews)
+        }
     }
 }
