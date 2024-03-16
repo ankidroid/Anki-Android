d1e0bc4a043afa7bdea091d14e03478a101d9346
abdelrahmanesam <abdelrahmanesam>
2024-03-16 22:10:55 +0200

    implementing version catalog for stable versioning between modules

--- lint-rules/build.gradle.kts
+++ lint-rules/build.gradle.kts
@@ -17,13 +17,13 @@ tasks.withType(KotlinCompile::class).all {
 }
 
 dependencies {
-    compileOnly(libs.com.android.tools.lint.api)
-    compileOnly(libs.com.android.tools.lint)
-    testImplementation(libs.org.hamcrest.hamcrest)
-    testImplementation(libs.org.hamcrest.hamcrest.library)
+    compileOnly(libs.android.lint.api)
+    compileOnly(libs.android.lint)
+    testImplementation(libs.hamcrest)
+    testImplementation(libs.hamcrest.library)
     testImplementation(libs.junit.jupiter)
     testImplementation(libs.junit.vintage.engine)
-    testImplementation(libs.com.android.tools.lint.api)
-    testImplementation(libs.com.android.tools.lint)
-    testImplementation(libs.com.android.tools.lint.tests)
+    testImplementation(libs.android.lint.api)
+    testImplementation(libs.android.lint)
+    testImplementation(libs.android.lint.tests)
 }
