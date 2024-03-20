c97b0fd65d64962c130c9239490aa64e96b07499
awanishyadav967 <91160609+awanishyadav967@users.noreply.github.com>
2024-03-21 02:25:30 +0530

    Correct date

--- AnkiDroid/src/main/java/com/ichi2/utils/ImageUtils.kt
+++ AnkiDroid/src/main/java/com/ichi2/utils/ImageUtils.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Avanish Yadav <mailto:awanishyadav967@gmail.com>
+ * Copyright (c) 2024 Avanish Yadav <mailto:awanishyadav967@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
