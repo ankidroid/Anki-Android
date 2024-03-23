203f606c7106a59df025fe722fc5b8dc713c22e1
Aditya <adityasharma131003@gmail.com>
2024-03-23 15:11:48 +0530

    Test case for checking if ok button is enabled when deck name matches

--- AnkiDroid/src/test/java/com/ichi2/anki/dialogs/CreateDeckDialogTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/dialogs/CreateDeckDialogTest.kt
@@ -194,6 +194,15 @@ class CreateDeckDialogTest : RobolectricTest() {
         assertEquals(deckPicker.optionsMenuState!!.searchIcon, true)
     }
 
+    @Test
+    fun positiveButtonEnabledOnMatchingDeckNames() {
+        val previousDeckName = "Deck Name"
+        testDialog(DeckDialogType.RENAME_DECK) {
+            input = previousDeckName
+            assertThat("Ok is enabled when deck names match", positiveButton.isEnabled, equalTo(true))
+        }
+    }
+
     /**
      * Executes [callback] on the [MaterialDialog] created from [CreateDeckDialog]
      */
