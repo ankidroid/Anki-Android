c81ad877389755da50e88b31bd00a0c5394b42dc
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2024-03-23 12:31:45 -0300

    refactor: restrict CardViewerActivity to CardViewerFragment

--- AnkiDroid/src/main/java/com/ichi2/anki/previewer/CardViewerActivity.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/previewer/CardViewerActivity.kt
@@ -18,7 +18,6 @@ package com.ichi2.anki.previewer
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import com.ichi2.anki.SingleFragmentActivity
 import kotlin.reflect.KClass
 import kotlin.reflect.jvm.jvmName
@@ -29,7 +28,7 @@ import kotlin.reflect.jvm.jvmName
  */
 class CardViewerActivity : SingleFragmentActivity() {
     companion object {
-        fun getIntent(context: Context, fragmentClass: KClass<out Fragment>, arguments: Bundle? = null): Intent {
+        fun getIntent(context: Context, fragmentClass: KClass<out CardViewerFragment>, arguments: Bundle? = null): Intent {
             return Intent(context, CardViewerActivity::class.java).apply {
                 putExtra(FRAGMENT_NAME_EXTRA, fragmentClass.jvmName)
                 putExtra(FRAGMENT_ARGS_EXTRA, arguments)
