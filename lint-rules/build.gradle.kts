bb0c4a85bf71f442170cf7626a60bd6892c35649
AbdelrahmanEsam <abdelrahmanesam20000@gmail.com>
2024-03-27 02:53:28 +0200

    Migrate version catalog (#15966)
    * version catalog conflict
     solve
    
    * implementing version catalog for stable versioning between modules
    
    * implementing version catalog for stable versioning between modules
    
    ---------
    
    Co-authored-by: abdelrahmanesam <abdelrahmanesam>

--- lint-rules/build.gradle.kts
+++ lint-rules/build.gradle.kts
@@ -17,14 +17,13 @@ tasks.withType(KotlinCompile::class).all {
 }
 
 dependencies {
-    compileOnly("com.android.tools.lint:lint-api:${rootProject.extra["lint_version"]}")
-    compileOnly("com.android.tools.lint:lint:${rootProject.extra["lint_version"]}")
-
-    testImplementation("org.hamcrest:hamcrest:${rootProject.extra["hamcrest_version"]}")
-    testImplementation("org.hamcrest:hamcrest-library:${rootProject.extra["hamcrest_version"]}")
-    testImplementation("org.junit.jupiter:junit-jupiter:${rootProject.extra["junit_version"]}")
-    testImplementation("org.junit.vintage:junit-vintage-engine:${rootProject.extra["junit_version"]}")
-    testImplementation("com.android.tools.lint:lint:${rootProject.extra["lint_version"]}")
-    testImplementation("com.android.tools.lint:lint-api:${rootProject.extra["lint_version"]}")
-    testImplementation("com.android.tools.lint:lint-tests:${rootProject.extra["lint_version"]}")
+    compileOnly(libs.android.lint.api)
+    compileOnly(libs.android.lint)
+    testImplementation(libs.hamcrest)
+    testImplementation(libs.hamcrest.library)
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.junit.vintage.engine)
+    testImplementation(libs.android.lint.api)
+    testImplementation(libs.android.lint)
+    testImplementation(libs.android.lint.tests)
 }
