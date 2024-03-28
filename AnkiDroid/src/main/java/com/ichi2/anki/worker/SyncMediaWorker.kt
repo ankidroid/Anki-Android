2f95aa4ab782e27b54808f79fd84f37d79feefcf
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2024-03-28 20:21:37 -0300

    refactor: SyncMediaWorker
    hideNotification isn't necessary because the notification is gone when the work completes

--- AnkiDroid/src/main/java/com/ichi2/anki/worker/SyncMediaWorker.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/worker/SyncMediaWorker.kt
@@ -27,6 +27,7 @@ import androidx.work.Data
 import androidx.work.ExistingWorkPolicy
 import androidx.work.ForegroundInfo
 import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.OutOfQuotaPolicy
 import androidx.work.WorkManager
@@ -36,13 +37,13 @@ import anki.sync.SyncAuth
 import anki.sync.syncAuth
 import com.ichi2.anki.Channel
 import com.ichi2.anki.CollectionManager
-import com.ichi2.anki.CollectionManager.withCol
 import com.ichi2.anki.R
 import com.ichi2.anki.cancelMediaSync
 import com.ichi2.anki.notifications.NotificationId
 import com.ichi2.anki.utils.ext.trySetForeground
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.delay
+import net.ankiweb.rsdroid.Backend
 import timber.log.Timber
 
 class SyncMediaWorker(
@@ -50,49 +51,59 @@ class SyncMediaWorker(
     parameters: WorkerParameters
 ) : CoroutineWorker(context, parameters) {
 
-    private val workManager = WorkManager.getInstance(context)
     private val notificationManager = NotificationManagerCompat.from(context)
+    private val cancelIntent = WorkManager.getInstance(context).createCancelPendingIntent(id)
 
     override suspend fun doWork(): Result {
         Timber.v("SyncMediaWorker::doWork")
         trySetForeground(getForegroundInfo())
 
         try {
-            val syncAuth = syncAuth {
+            val auth = syncAuth {
                 hkey = inputData.getString(HKEY_KEY)!!
                 inputData.getString(ENDPOINT_KEY)?.let {
                     endpoint = it
                 }
             }
-            withCol { backend.syncMedia(syncAuth) }
+
             val backend = CollectionManager.getBackend()
-            var syncProgress: MediaSyncProgress? = null
-            while (true) {
-                val status = backend.mediaSyncStatus()
-                if (!status.active) {
-                    hideNotification()
-                    return Result.success()
-                }
-                // avoid sending repeated notifications
-                if (syncProgress != status.progress) {
-                    syncProgress = status.progress
-                    // TODO display better the result. In tests, using setContentText lead to
-                    //  truncated text if it had more than two lines.
-                    // `added`, `removed` and `checked` already come translated from the backend
-                    val notificationText = syncProgress.run { "$added $removed $checked" }
-                    notify(getProgressNotification(notificationText))
-                }
-                delay(100)
-            }
+            backend.syncMedia(auth)
+
+            monitorProgress(backend)
         } catch (cancellationException: CancellationException) {
             Timber.w(cancellationException)
             cancelMediaSync(CollectionManager.getBackend())
-            hideNotification()
             throw cancellationException
         } catch (throwable: Throwable) {
             Timber.w(throwable)
-            notify(buildNotification { setContentTitle(CollectionManager.TR.syncMediaFailed()) })
+            notify {
+                setContentTitle(CollectionManager.TR.syncMediaFailed())
+            }
             return Result.failure()
+        }
+
+        Timber.d("SyncMediaWorker: success")
+        return Result.success()
+    }
+
+    private suspend fun monitorProgress(backend: Backend) {
+        var syncProgress: MediaSyncProgress? = null
+        while (true) {
+            val status = backend.mediaSyncStatus()
+            if (!status.active) {
+                Timber.i("Ended media sync notification updates")
+                break
+            }
+            // avoid sending repeated notifications
+            if (syncProgress != status.progress) {
+                syncProgress = status.progress
+                // TODO display better the result. Using setContentText leads to
+                //  truncated text if it has more than two lines.
+                // `added`, `removed` and `checked` already come translated from the
+                val notificationText = syncProgress.run { "$added $removed $checked" }
+                notify(getProgressNotification(notificationText))
+            }
+            delay(100)
         }
     }
 
@@ -113,8 +124,9 @@ class SyncMediaWorker(
     private fun notify(notification: Notification) =
         notificationManager.notify(NotificationId.SYNC_MEDIA, notification)
 
-    private fun hideNotification() =
-        notificationManager.cancel(NotificationId.SYNC_MEDIA)
+    private fun notify(builder: NotificationCompat.Builder.() -> Unit) {
+        notify(buildNotification(builder))
+    }
 
     private fun buildNotification(block: NotificationCompat.Builder.() -> Unit): Notification {
         return NotificationCompat.Builder(applicationContext, Channel.SYNC.id).apply {
@@ -129,9 +141,7 @@ class SyncMediaWorker(
 
     private fun getProgressNotification(progress: CharSequence): Notification {
         val title = applicationContext.getString(R.string.syncing_media)
-
         val cancelTitle = applicationContext.getString(R.string.dialog_cancel)
-        val cancelIntent = workManager.createCancelPendingIntent(id)
 
         return buildNotification {
             setContentTitle(title)
@@ -145,25 +155,28 @@ class SyncMediaWorker(
         private const val HKEY_KEY = "hkey"
         private const val ENDPOINT_KEY = "endpoint"
 
-        fun start(context: Context, syncAuth: SyncAuth) {
+        fun getWorkRequest(auth: SyncAuth): OneTimeWorkRequest {
             val constraints = Constraints.Builder()
                 .setRequiredNetworkType(NetworkType.CONNECTED)
                 .build()
 
             val data = Data.Builder()
-                .putString(HKEY_KEY, syncAuth.hkey)
-                .putString(ENDPOINT_KEY, syncAuth.endpoint)
+                .putString(HKEY_KEY, auth.hkey)
+                .putString(ENDPOINT_KEY, auth.endpoint)
                 .build()
 
-            val request = OneTimeWorkRequestBuilder<SyncMediaWorker>()
+            return OneTimeWorkRequestBuilder<SyncMediaWorker>()
                 .setInputData(data)
                 .setConstraints(constraints)
                 .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
                 .build()
+        }
+
+        fun start(context: Context, auth: SyncAuth) {
+            val request = getWorkRequest(auth)
 
             WorkManager.getInstance(context)
-                .beginUniqueWork(UniqueWorkNames.SYNC_MEDIA, ExistingWorkPolicy.KEEP, request)
-                .enqueue()
+                .enqueueUniqueWork(UniqueWorkNames.SYNC_MEDIA, ExistingWorkPolicy.KEEP, request)
         }
     }
 }
