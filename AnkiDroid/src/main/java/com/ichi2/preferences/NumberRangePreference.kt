95624d876043436592ce761ae02e700d54f2e37b
David Allison <62114487+david-allison@users.noreply.github.com>
2024-05-21 13:33:27 +0100

    refactor: Kotlin 2.0 prep
    ## Deprecations
    
    * `@Deprecated`/ `@Suppress("OVERRIDE_DEPRECATION")`
    
    ====
    
    This declaration overrides a deprecated member but is not marked as deprecated itself.
    Please add the '@Deprecated' annotation or suppress the diagnostic.
    See https://youtrack.jetbrains.com/issue/KT-47902 for details
    
    ## `LinkedList.first`
    
    * `LinkedList.first` to `LinkedList.first()`
    
    ----
    
    'val first' is deprecated. This declaration will be renamed in a future version of Kotlin.
    Please consider using the 'first()' stdlib extension if the collection supports fast random access.
    
    ## `kotlinOptions`
    
    * `kotlinOptions` -> `compilerOptions`
    
     'kotlinOptions(KotlinJvmOptionsDeprecated /* = KotlinJvmOptions */.() -> Unit): Unit' is deprecated.
     Please migrate to the compilerOptions DSL. More details are here: https://kotl.in/u1r8ln

--- AnkiDroid/src/main/java/com/ichi2/preferences/NumberRangePreference.kt
+++ AnkiDroid/src/main/java/com/ichi2/preferences/NumberRangePreference.kt
@@ -23,7 +23,7 @@ import android.view.View
 import com.ichi2.anki.AnkiDroidApp
 import timber.log.Timber
 
-@Suppress("deprecation") // TODO Tracked in https://github.com/ankidroid/Anki-Android/issues/5019 : use NumberRangePreferenceCompat
+@Suppress("deprecation", "OVERRIDE_DEPRECATION") // TODO Tracked in https://github.com/ankidroid/Anki-Android/issues/5019 : use NumberRangePreferenceCompat
 open class NumberRangePreference : android.preference.EditTextPreference, AutoFocusable {
     protected val min: Int
     private val max: Int
