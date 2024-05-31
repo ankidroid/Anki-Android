637ce8bd96958cacf89b09fa82ce735ea5974957
abdelrahmanesam <abdelrahmanesam>
2024-05-31 13:12:49 +0300

    migrate gradle to build-logic / composite build  module to share

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -9,12 +9,12 @@ group = "com.ichi2.anki.buildlogic"
 // Configure the build-logic plugins to target JDK 17
 // This matches the JDK used to build the project, and is not related to what is running on device.
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 tasks.withType<KotlinCompile>().configureEach {
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 }
 
