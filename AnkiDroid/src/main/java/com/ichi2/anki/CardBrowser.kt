ankidroid/Anki-Android

commit 0d69cacde9886feba40b98e19f851e62d5a29fcd

KotlinCleanup: CardBrowser.kt

AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.kt
@@ -1004,26 +1004,22 @@ open class CardBrowser :
      * 0: No Flag, 1: RED, 2: ORANGE, 3: GREEN
      * 4: BLUE, 5: PINK, 6: Turquoise, 7: PURPLE
      *
-     * @return list of cards with updated flags
      */
     @VisibleForTesting
     suspend fun updateSelectedCardsFlag(flag: Int) {
+        // list of cards with updated flags
         val updatedCards = withProgress {
             withCol {
-                val cards = selectedCardIds.map { getCard(it) }
-                col.db.database.beginTransaction()
-                try {
-                    col.setUserFlag(flag, selectedCardIds)
-                    for (c in cards) {
-                        c.load()
-                    }
-                    col.db.database.setTransactionSuccessful()
-                } finally {
-                    DB.safeEndInTransaction(col.db)
+                db.executeInTransaction {
+                    setUserFlag(flag, selectedCardIds)
+                    selectedCardIds
+                        .map { getCard(it) }
+                        .onEach { load() }
                 }
-                cards
             }
         }
+        // TODO: try to offload the cards processing in updateCardsInList() on a background thread,
+        // otherwise it could hang the main thread
         updateCardsInList(updatedCards)
         invalidateOptionsMenu() // maybe the availability of undo changed
         if (updatedCards.map { card -> card.id }.contains(reviewerCardId)) {

commit b3ec690c0d7d0757aecc28ee735968830a85264a

refactor(card-browser-view-model): update flags
* onEach { load() } called collection.load
  * broken in 0d69cacde9886feba40b98e19f851e62d5a29fcd

AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.kt
@@ -903,12 +903,12 @@ open class CardBrowser :
     @VisibleForTesting
     suspend fun updateSelectedCardsFlag(flag: Flag) {
         // list of cards with updated flags
-        val updatedCards = withProgress { viewModel.updateSelectedCardsFlag(flag) }
+        val updatedCardIds = withProgress { viewModel.updateSelectedCardsFlag(flag) }
         // TODO: try to offload the cards processing in updateCardsInList() on a background thread,
         // otherwise it could hang the main thread
-        updateCardsInList(updatedCards.map { it.id })
+        updateCardsInList(updatedCardIds)
         invalidateOptionsMenu() // maybe the availability of undo changed
-        if (updatedCards.any { card -> card.id == reviewerCardId }) {
+        if (updatedCardIds.any { it == reviewerCardId }) {
             reloadRequired = true
         }
     }

AnkiDroid/src/main/java/com/ichi2/anki/browser/CardBrowserViewModel.kt
@@ -686,14 +686,10 @@ class CardBrowserViewModel(
         }
     }
 
-    suspend fun updateSelectedCardsFlag(flag: Flag): List<Card> {
+    suspend fun updateSelectedCardsFlag(flag: Flag): List<CardId> {
         val idsToChange = queryAllSelectedCardIds()
-        return withCol {
-            setUserFlag(flag, cids = idsToChange)
-            selectedRowIds
-                .map { getCard(it) }
-                .onEach { load() }
-        }
+        withCol { setUserFlag(flag, cids = idsToChange) }
+        return idsToChange
     }
 
     /**