ankidroid/Anki-Android

commit 0d69cacde9886feba40b98e19f851e62d5a29fcd

KotlinCleanup: CardBrowser.kt

AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.kt
@@ -1004,26 +1004,22 @@ open class CardBrowser :
      * 0: No Flag, 1: RED, 2: ORANGE, 3: GREEN
      * 4: BLUE, 5: PINK, 6: Turquoise, 7: PURPLE
      *
-     * @return list of cards with updated flags
      */
     @VisibleForTesting
     suspend fun updateSelectedCardsFlag(flag: Int) {
+        // list of cards with updated flags
         val updatedCards = withProgress {
             withCol {
-                val cards = selectedCardIds.map { getCard(it) }
-                col.db.database.beginTransaction()
-                try {
-                    col.setUserFlag(flag, selectedCardIds)
-                    for (c in cards) {
-                        c.load()
-                    }
-                    col.db.database.setTransactionSuccessful()
-                } finally {
-                    DB.safeEndInTransaction(col.db)
+                db.executeInTransaction {
+                    setUserFlag(flag, selectedCardIds)
+                    selectedCardIds
+                        .map { getCard(it) }
+                        .onEach { load() }
                 }
-                cards
             }
         }
+        // TODO: try to offload the cards processing in updateCardsInList() on a background thread,
+        // otherwise it could hang the main thread
         updateCardsInList(updatedCards)
         invalidateOptionsMenu() // maybe the availability of undo changed
         if (updatedCards.map { card -> card.id }.contains(reviewerCardId)) {

commit 20add73857261a1562b5ce793d9ec0b86b2d92d2

refactor(card-browser): updateSelectedCardsFlag (#16547)
* refactor(card-browser): Card param -> CardId

We're going to refactor `updateSelectedCardsFlag` to
return cids and these were all that is required

* refactor(card-browser): remove unused method

* searchWithFilterQuery

* refactor(card-browser-view-model): update flags

* onEach { load() } called collection.load
  * broken in 0d69cacde9886feba40b98e19f851e62d5a29fcd

AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.kt
@@ -172,7 +172,7 @@ open class CardBrowser :
         }
         if (result.resultCode != RESULT_CANCELED) {
             Timber.i("CardBrowser:: CardBrowser: Saving card...")
-            launchCatchingTask { saveEditedCard() }
+            saveEditedCard()
         }
         val data = result.data
         if (data != null &&
@@ -481,10 +481,6 @@ open class CardBrowser :
         viewModel.flowOfCardsUpdated.launchCollectionInLifecycleScope(::cardsUpdatedChanged)
         viewModel.flowOfSearchState.launchCollectionInLifecycleScope(::searchStateChanged)
         viewModel.flowOfInitCompleted.launchCollectionInLifecycleScope(::initCompletedChanged)
-    }
-
-    fun searchWithFilterQuery(filterQuery: String) = launchCatchingTask {
-        viewModel.setFilterQuery(filterQuery)
     }
 
     // Finish initializing the activity after the collection has been correctly loaded
@@ -907,12 +903,12 @@ open class CardBrowser :
     @VisibleForTesting
     suspend fun updateSelectedCardsFlag(flag: Flag) {
         // list of cards with updated flags
-        val updatedCards = withProgress { viewModel.updateSelectedCardsFlag(flag) }
+        val updatedCardIds = withProgress { viewModel.updateSelectedCardsFlag(flag) }
         // TODO: try to offload the cards processing in updateCardsInList() on a background thread,
         // otherwise it could hang the main thread
-        updateCardsInList(updatedCards)
+        updateCardsInList(updatedCardIds)
         invalidateOptionsMenu() // maybe the availability of undo changed
-        if (updatedCards.any { card -> card.id == reviewerCardId }) {
+        if (updatedCardIds.any { it == reviewerCardId }) {
             reloadRequired = true
         }
     }
@@ -1422,13 +1418,6 @@ open class CardBrowser :
             return resources.getQuantityString(subtitleId, count, count)
         }
 
-    // convenience method for updateCardsInList(...)
-    private fun updateCardInList(card: Card) {
-        val cards: MutableList<Card> = ArrayList(1)
-        cards.add(card)
-        updateCardsInList(cards)
-    }
-
     /** Returns the decks which are valid targets for "Change Deck"  */
     suspend fun getValidDecksForChangeDeck(): List<DeckNameId> =
         deckSpinnerSelection.computeDropDownDecks(includeFiltered = false)
@@ -1474,29 +1463,23 @@ open class CardBrowser :
     fun filterByFlag(flag: Flag) = launchCatchingTask { viewModel.setFlagFilter(flag) }
 
     /**
-     * Loads/Reloads (Updates the Q, A & etc) of cards in the [cards] list
-     * @param cards Cards that were changed
+     * Loads/Reloads (Updates the Q, A & etc) of cards in the [cardIds] list
+     * @param cardIds Card IDs that were changed
      */
-    private fun updateCardsInList(cards: List<Card>) {
+    private fun updateCardsInList(cardIds: List<CardId>) {
         val idToPos = viewModel.cardIdToPositionMap
         // TODO: Inefficient
-        cards
-            .mapNotNull { c -> idToPos[c.id] }
+        cardIds
+            .mapNotNull { cid -> idToPos[cid] }
             .filterNot { pos -> pos >= viewModel.rowCount }
             .map { pos -> viewModel.getRowAtPosition(pos) }
             .forEach { it.load(true, viewModel.column1Index, viewModel.column2Index) }
         updateList()
     }
 
-    private suspend fun saveEditedCard() {
+    private fun saveEditedCard() {
         Timber.d("CardBrowser - saveEditedCard()")
-        val card = try {
-            withCol { getCard(currentCardId) }
-        } catch (e: Exception) {
-            Timber.i("edited card no longer exists")
-            return
-        }
-        updateCardInList(card)
+        updateCardsInList(listOf(currentCardId))
     }
 
     /**

AnkiDroid/src/main/java/com/ichi2/anki/browser/CardBrowserViewModel.kt
@@ -691,14 +691,10 @@ class CardBrowserViewModel(
         }
     }
 
-    suspend fun updateSelectedCardsFlag(flag: Flag): List<Card> {
+    suspend fun updateSelectedCardsFlag(flag: Flag): List<CardId> {
         val idsToChange = queryAllSelectedCardIds()
-        return withCol {
-            setUserFlag(flag, cids = idsToChange)
-            selectedRowIds
-                .map { getCard(it) }
-                .onEach { load() }
-        }
+        withCol { setUserFlag(flag, cids = idsToChange) }
+        return idsToChange
     }
 
     /**

AnkiDroid/src/main/java/com/ichi2/libanki/Card.kt
@@ -68,7 +68,7 @@ open class Card : Cloneable {
     private var elapsedTime: Long = 0
 
     @set:VisibleForTesting
-    var id: Long = 0
+    var id: CardId = 0
     var nid: NoteId = 0
     var did: DeckId = 0
     var ord = 0