004739ca563f1bdc9bd6e5811a5f547df751cbdd
David Allison <62114487+david-allison@users.noreply.github.com>
2024-08-22 13:20:22 +0100

    feat(libAnki): implement decks.cardCount()
    Sourced from
    
    https://github.com/ankitects/anki/blob/a179da382759b9c7f8265faf37efdcbb47d8ee08/pylib/anki/decks.py#L225

--- AnkiDroid/src/test/java/com/ichi2/libanki/DecksTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/libanki/DecksTest.kt
@@ -170,6 +170,48 @@ class DecksTest : JvmTest() {
         assertThat(filtered.isFiltered, equalTo(true))
     }
 
+    @Test
+    fun testCardCount() {
+        val decks = col.decks
+        var addedNoteCount = 0
+
+        fun addNote(did: DeckId): NoteId {
+            val note = col.newNote().apply {
+                setItem("Front", (++addedNoteCount).toString())
+                notetype.put("did", did)
+            }
+            col.addNote(note)
+            return note.id
+        }
+
+        val parentDid = addDeck("Deck").also { did -> addNote(did) }
+        val childDid = addDeck("Deck::Subdeck").also { did -> addNote(did) }
+
+        val noteToMakeDynamic: NoteId
+        val deckWithNoChildren = addDeck("DeckWithTwo").also { did ->
+            addNote(did)
+            addNote(did)
+            noteToMakeDynamic = addNote(did)
+        }
+        val filteredDeck = addDynamicDeck("filtered", search = "nid:$noteToMakeDynamic")
+
+        assertThat("all decks", decks.cardCount(parentDid, childDid, deckWithNoChildren, includeSubdecks = false), equalTo(5))
+        assertThat("all decks with subdecks", decks.cardCount(parentDid, childDid, deckWithNoChildren, includeSubdecks = true), equalTo(5))
+
+        assertThat("top level decks, no children", decks.cardCount(parentDid, deckWithNoChildren, includeSubdecks = false), equalTo(4))
+        assertThat("top level decks, with children", decks.cardCount(parentDid, deckWithNoChildren, includeSubdecks = true), equalTo(5))
+
+        assertThat("parent deck, no children", decks.cardCount(parentDid, includeSubdecks = false), equalTo(1))
+        assertThat("parent deck, with children", decks.cardCount(parentDid, includeSubdecks = true), equalTo(2))
+
+        assertThat("single deck, multiple cards, no children", decks.cardCount(deckWithNoChildren, includeSubdecks = false), equalTo(3))
+        assertThat("single deck, multiple cards, with children", decks.cardCount(deckWithNoChildren, includeSubdecks = true), equalTo(3))
+
+        assertThat("filtered deck", decks.cardCount(filteredDeck, includeSubdecks = false), equalTo(1))
+
+        assertThat("filtered and home deck", decks.cardCount(deckWithNoChildren, filteredDeck, includeSubdecks = false), equalTo(3))
+    }
+
     companion object {
         // Used in other class to populate decks.
         @Suppress("SpellCheckingInspection")
