6c04c009a4a6666d60db78647236ac843e27a58b
David Allison <62114487+david-allison@users.noreply.github.com>
2024-10-10 01:43:24 +0100

    fix(custom-buttons): revert custom button name to 'Reschedule'
    Partial revert of b0de535d4c760072807120f1b99662c0f71ff3cf
    
    In the above commit, "Reschedule" was renamed to "Set Due Date"
    but the affected menu item in the Reviewer is "Reschedule":
    offering "Set Due Date" and "Reset progress"
    
    Fixes 16598

--- AnkiDroid/src/main/java/com/ichi2/anki/preferences/CustomButtonsSettingsFragment.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/preferences/CustomButtonsSettingsFragment.kt
@@ -20,11 +20,8 @@ import android.content.Intent
 import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.edit
-import androidx.preference.ListPreference
 import androidx.preference.Preference
-import com.ichi2.anki.CollectionManager.TR
 import com.ichi2.anki.R
-import com.ichi2.anki.ui.internationalization.toSentenceCase
 import com.ichi2.utils.negativeButton
 import com.ichi2.utils.positiveButton
 import com.ichi2.utils.show
@@ -55,15 +52,6 @@ class CustomButtonsSettingsFragment : SettingsFragment() {
                 negativeButton(R.string.dialog_cancel)
             }
             true
-        }
-        setDynamicTitle()
-    }
-
-    private fun setDynamicTitle() {
-        findPreference<ListPreference>(getString(R.string.custom_button_schedule_card_key))?.let {
-            val preferenceTitle = TR.actionsSetDueDate().toSentenceCase(this, R.string.sentence_set_due_date)
-            it.title = preferenceTitle
-            it.dialogTitle = preferenceTitle
         }
     }
 
