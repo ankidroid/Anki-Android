d0ca613bf4da14aca843b32470eb52933d38d068
Arthur Milchior <arthur@milchior.fr>
2024-11-03 17:03:05 +0100

    NF: use enum and const in ImportDialog

--- AnkiDroid/src/test/java/com/ichi2/anki/dialogs/AsyncDialogFragmentsTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/dialogs/AsyncDialogFragmentsTest.kt
@@ -61,7 +61,7 @@ class AsyncDialogFragmentsTest {
 
     @Test
     fun `ImportDialog does not require context`() {
-        for (dialogType in ImportDialog.dialogTypes) {
+        for (dialogType in ImportDialog.Type.entries) {
             val instance = ImportDialog.newInstance(dialogType, "path")
             assertDoesNotThrow("$dialogType message required a context") { instance.notificationMessage }
             assertDoesNotThrow("$dialogType title required a context") { instance.notificationTitle }
