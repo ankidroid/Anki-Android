e1ef4f3599c8f1a9e7308af6bfd59b3456459cc5
Mritunjay Tiwari <shreyashtiwari14.11@gmail.com>
2024-11-20 20:16:47 +0530

    Resolved the Issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ import java.util.Properties
 import kotlin.math.max
 import kotlin.system.exitProcess
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level Build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
