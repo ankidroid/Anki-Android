2df6e67f768119b4d95affd77e15d700db9bae2d
Prince kushwaha <kushwahaprince418@gmail.com>
2024-12-12 23:10:55 +0530

    fix onBackPressed() deprecations

--- AnkiDroid/src/main/java/com/ichi2/anki/NavigationDrawerActivity.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/NavigationDrawerActivity.kt
@@ -66,7 +66,7 @@ abstract class NavigationDrawerActivity :
     private var navButtonGoesBack = false
 
     // Navigation drawer list item entries
-    private lateinit var drawerLayout: DrawerLayout
+    lateinit var drawerLayout: DrawerLayout
     private var navigationView: NavigationView? = null
     lateinit var drawerToggle: ActionBarDrawerToggle
         private set
@@ -413,7 +413,7 @@ abstract class NavigationDrawerActivity :
         drawerLayout.openDrawer(GravityCompat.START, animationEnabled())
     }
 
-    private fun closeDrawer() {
+    protected fun closeDrawer() {
         drawerLayout.closeDrawer(GravityCompat.START, animationEnabled())
     }
 
