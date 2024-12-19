07eca88749ff9d1b23248097b7540b81310ba0c5
Hari Srinivasan <harisrini21@gmail.com>
2024-12-19 13:48:21 +0530

    Refactoring Visibility to isVisible

--- AnkiDroid/src/main/java/com/ichi2/anki/export/ExportDialogFragment.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/export/ExportDialogFragment.kt
@@ -166,7 +166,7 @@ class ExportDialogFragment : DialogFragment() {
             if (selectedDeck != null) {
                 deckSelector.setSelection(findDeckPosition(selectedDeck))
             }
-            loadingIndicator.visibility = View.GONE
+            loadingIndicator.isVisible = false
             deckSelector.isEnabled = true
         }
     }
@@ -291,24 +291,25 @@ class ExportDialogFragment : DialogFragment() {
         container: View,
         targetConfig: ExportConfiguration,
     ) {
-        // show the legacy checkbox only for collection and apkg exports
-        ExportConfiguration.entries.forEach { config ->
-            container.findViewById<View>(config.layoutId).isVisible =
-                config.layoutId == targetConfig.layoutId
-        }
+        // if we export as collection there's no deck/selected items to choose from
         if (targetConfig.layoutId == R.id.export_extras_collection) {
-            decksSelectorContainer.visibility = View.GONE
-            selectedLabel.visibility = View.GONE
+            decksSelectorContainer.isVisible = false
+            selectedLabel.isVisible = false
         } else {
             if (arguments?.getSerializableCompat<ExportType>(ARG_TYPE) != null) {
-                decksSelectorContainer.visibility = View.GONE
-                selectedLabel.visibility = View.VISIBLE
+                decksSelectorContainer.isVisible = false
+                selectedLabel.isVisible = true
             } else {
-                decksSelectorContainer.visibility = View.VISIBLE
-                selectedLabel.visibility = View.GONE
+                decksSelectorContainer.isVisible = true
+                selectedLabel.isVisible = false
             }
         }
         exportTypeSelector.setSelection(targetConfig.index)
+        // Show the legacy checkbox for the selected export type
+        ExportConfiguration.entries.forEach { config ->
+            container.findViewById<View>(config.layoutId).visibility =
+                if (config.layoutId == targetConfig.layoutId) View.VISIBLE else View.GONE
+        }
     }
 
     private fun handleCollectionExport() {
