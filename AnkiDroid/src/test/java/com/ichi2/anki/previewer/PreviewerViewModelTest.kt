48f19259c0f1b55f741251a6fe133599ef93df1e
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2024-12-22 06:13:06 -0300

    refactor: rename typeAnsQuestionFilter

--- AnkiDroid/src/test/java/com/ichi2/anki/previewer/PreviewerViewModelTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/previewer/PreviewerViewModelTest.kt
@@ -23,7 +23,7 @@ class PreviewerViewModelTest {
     @Test
     fun `type answer fields are removed in questions`() {
         assertThat(
-            PreviewerViewModel.typeAnsQuestionFilter("creu [[type:leu]]"),
+            PreviewerViewModel.removeTypeAnswerTags("creu [[type:leu]]"),
             equalTo("creu "),
         )
     }
