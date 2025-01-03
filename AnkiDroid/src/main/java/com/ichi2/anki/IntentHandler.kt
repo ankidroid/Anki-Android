ankidroid/Anki-Android

commit 15b748a60a59748b1b572ba4b209128991446e9d

IntentHandler decides which reviewer is used.
This also cause shortcuts, reminder and widget to use the
IntentHandler instead of directly opening the Reviewer.

I should note that the reminders that were created before this commit
will break when the Reviewer class will be removed. Unless we
specifically keep the Reviewer to cause it to redirect to the new
Reviewer.

Given that the reminder is quite broken anyway, must be redone from
scratch, at this point, I think it's an acceptable loss. Trying to
correct it while the reminder does not work properly will be a
nightmare.

There are still plenty of places in the code that use the previous
reviewer inconditionally. I'm not touching them, because I don't need
them to unblock my sync in reviewer issue

Test: I tested shortcut and deck picker widget. I've not tested
notification as they are quite harder to use

Fixes #17420

AnkiDroid/src/main/java/com/ichi2/anki/IntentHandler.kt
@@ -31,8 +31,10 @@ import com.ichi2.anki.dialogs.DialogHandlerMessage
 import com.ichi2.anki.preferences.sharedPrefs
 import com.ichi2.anki.servicelayer.ScopedStorageService
 import com.ichi2.anki.services.ReminderService
+import com.ichi2.anki.ui.windows.reviewer.ReviewerFragment
 import com.ichi2.anki.worker.SyncWorker
 import com.ichi2.annotations.NeedsTest
+import com.ichi2.libanki.DeckId
 import com.ichi2.utils.FileUtil
 import com.ichi2.utils.ImportUtils.handleFileImport
 import com.ichi2.utils.ImportUtils.isInvalidViewIntent
@@ -138,7 +140,11 @@ class IntentHandler : AbstractIntentHandler() {
     private fun handleReviewIntent(intent: Intent) {
         val deckId = intent.getLongExtra(ReminderService.EXTRA_DECK_ID, 0)
         Timber.i("Handling intent to review deck '%d'", deckId)
-        val reviewIntent = Intent(this, Reviewer::class.java)
+        val reviewIntent = if (sharedPrefs().getBoolean("newReviewer", false)) {
+            ReviewerFragment.getIntent(this)
+        } else {
+            Intent(this, Reviewer::class.java)
+        }
         CollectionManager.getColUnsafe().decks.select(deckId)
         startActivity(reviewIntent)
         finish()
@@ -375,5 +381,18 @@ class IntentHandler : AbstractIntentHandler() {
                     ).toLong()
             }
         }
+
+        /**
+         * Returns an intent to review a specific deck.
+         * This does not states which reviewer to use, instead IntentHandler will choose whether to use the
+         * legacy or the new reviewer based on the "newReviewer" preference.
+         * It is expected to be used from widget, shortcut, reminders but not from ankidroid directly because of the CLEAR_TOP flag.
+         */
+        fun intentToReviewDeckFromShorcuts(context: Context, deckId: DeckId) =
+            Intent(context, IntentHandler::class.java).apply {
+                setAction(Intent.ACTION_VIEW)
+                putExtra(ReminderService.EXTRA_DECK_ID, deckId)
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            }
     }
 }

AnkiDroid/src/main/java/com/ichi2/anki/DeckPicker.kt
@@ -97,6 +97,7 @@ import com.ichi2.anki.InitialActivity.StartupFailure.DISK_FULL
 import com.ichi2.anki.InitialActivity.StartupFailure.FUTURE_ANKIDROID_VERSION
 import com.ichi2.anki.InitialActivity.StartupFailure.SD_CARD_NOT_MOUNTED
 import com.ichi2.anki.InitialActivity.StartupFailure.WEBVIEW_FAILED
+import com.ichi2.anki.IntentHandler.Companion.intentToReviewDeckFromShorcuts
 import com.ichi2.anki.StudyOptionsFragment.StudyOptionsListener
 import com.ichi2.anki.analytics.UsageAnalytics
 import com.ichi2.anki.deckpicker.BITMAP_BYTES_PER_PIXEL
@@ -2202,10 +2203,7 @@ open class DeckPicker :
         // This code should not be reachable with lower versions
         val shortcut = ShortcutInfoCompat.Builder(this, did.toString())
             .setIntent(
-                Intent(context, Reviewer::class.java)
-                    .setAction(Intent.ACTION_VIEW)
-                    .putExtra("deckId", did)
-                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                intentToReviewDeckFromShorcuts(context, did)
             )
             .setIcon(IconCompat.createWithResource(context, R.mipmap.ic_launcher))
             .setShortLabel(Decks.basename(getColUnsafe.decks.name(did)))

AnkiDroid/src/main/java/com/ichi2/widget/cardanalysis/CardAnalysisWidget.kt
@@ -26,8 +26,8 @@ import android.view.View
 import android.widget.RemoteViews
 import com.ichi2.anki.AnkiDroidApp
 import com.ichi2.anki.CrashReportService
+import com.ichi2.anki.IntentHandler.Companion.intentToReviewDeckFromShorcuts
 import com.ichi2.anki.R
-import com.ichi2.anki.Reviewer
 import com.ichi2.anki.analytics.UsageAnalytics
 import com.ichi2.anki.isCollectionEmpty
 import com.ichi2.anki.pages.DeckOptions
@@ -181,11 +181,7 @@ class CardAnalysisWidget : AnalyticsWidgetProvider() {
             val isEmptyDeck = deckData.newCount == 0 && deckData.reviewCount == 0 && deckData.learnCount == 0
 
             val intent = if (!isEmptyDeck) {
-                Intent(context, Reviewer::class.java).apply {
-                    action = Intent.ACTION_VIEW
-                    putExtra("deckId", deckData.deckId)
-                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                }
+                intentToReviewDeckFromShorcuts(context, deckData.deckId)
             } else {
                 DeckOptions.getIntent(context, deckData.deckId)
             }

AnkiDroid/src/main/java/com/ichi2/widget/deckpicker/DeckPickerWidget.kt
@@ -27,8 +27,8 @@ import android.widget.RemoteViews
 import com.ichi2.anki.AnkiDroidApp
 import com.ichi2.anki.CollectionManager.withCol
 import com.ichi2.anki.CrashReportService
+import com.ichi2.anki.IntentHandler.Companion.intentToReviewDeckFromShorcuts
 import com.ichi2.anki.R
-import com.ichi2.anki.Reviewer
 import com.ichi2.anki.analytics.UsageAnalytics
 import com.ichi2.anki.isCollectionEmpty
 import com.ichi2.anki.pages.DeckOptions
@@ -141,11 +141,7 @@ class DeckPickerWidget : AnalyticsWidgetProvider() {
                 val isEmptyDeck = deck.newCount == 0 && deck.reviewCount == 0 && deck.learnCount == 0
 
                 val intent = if (!isEmptyDeck) {
-                    Intent(context, Reviewer::class.java).apply {
-                        action = Intent.ACTION_VIEW
-                        putExtra("deckId", deck.deckId)
-                        addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                    }
+                    intentToReviewDeckFromShorcuts(context, deck.deckId)
                 } else {
                     DeckOptions.getIntent(context, deck.deckId)
                 }

commit c0dc16db6be5903284862b9950aae1dfeea33aff

Deeplink to reviewer closes everything
Currently, the code ensured there was at most one IntentHandler. We
honestly don't care about it. The intended meaning was that there is a
single reviewer.

This bug was probably introduced in #17422 with commit
f26aa95bdf4bb0fb378da1bf626a70eecb46a2e5.

I would love to test to avoid another regression. However, it's really
not clear to me how to deal with shortcuts, widget or remainders as
they are outside of the main app.

I discovered that having a single reviewer is still not
enough. Because what is below the reviewer could be nonsense. Instead,
I followed standard practice for deep link. I close everything and
create a fresh stack.

Bug #16956.
Fixes #17664.

AnkiDroid/src/main/java/com/ichi2/anki/IntentHandler.kt
@@ -91,7 +91,7 @@ class IntentHandler : AbstractIntentHandler() {
                     finish()
                 }
             LaunchType.SYNC -> runIfStoragePermissions { handleSyncIntent(reloadIntent, action) }
-            LaunchType.REVIEW -> runIfStoragePermissions { handleReviewIntent(intent) }
+            LaunchType.REVIEW -> runIfStoragePermissions { handleReviewIntent(reloadIntent, intent) }
             LaunchType.DEFAULT_START_APP_IF_NEW -> {
                 Timber.d("onCreate() performing default action")
                 launchDeckPickerIfNoOtherTasks(reloadIntent)
@@ -144,17 +144,27 @@ class IntentHandler : AbstractIntentHandler() {
         }
     }
 
-    private fun handleReviewIntent(intent: Intent) {
+    private fun handleReviewIntent(
+        reloadIntent: Intent,
+        reviewerIntent: Intent,
+    ) {
         val deckId = intent.getLongExtra(ReminderService.EXTRA_DECK_ID, 0)
         Timber.i("Handling intent to review deck '%d'", deckId)
+
         val reviewIntent =
             if (sharedPrefs().getBoolean("newReviewer", false)) {
                 ReviewerFragment.getIntent(this)
             } else {
-                Intent(this, Reviewer::class.java)
+                Intent(this, Reviewer::class.java).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                }
             }
         CollectionManager.getColUnsafe().decks.select(deckId)
-        startActivity(reviewIntent)
+        TaskStackBuilder
+            .create(applicationContext)
+            .addNextIntent(reloadIntent)
+            .addNextIntent(reviewIntent)
+            .startActivities()
         finish()
     }
 
@@ -439,7 +449,6 @@ class IntentHandler : AbstractIntentHandler() {
         ) = Intent(context, IntentHandler::class.java).apply {
             setAction(Intent.ACTION_VIEW)
             putExtra(ReminderService.EXTRA_DECK_ID, deckId)
-            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         }
     }
 }