4cff67048aeffb0e3b81348892baad2184187ffa
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2025-01-05 03:02:59 +0530

    fix: Syncing media notification getting stuck

--- AnkiDroid/src/main/java/com/ichi2/anki/worker/SyncMediaWorker.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/worker/SyncMediaWorker.kt
@@ -84,6 +84,9 @@ class SyncMediaWorker(
                 setContentTitle(CollectionManager.TR.syncMediaFailed())
             }
             return Result.failure()
+        } finally {
+            Timber.i("SyncMediaWorker stopped, notification cleared")
+            notificationManager.cancel(NotificationId.SYNC_MEDIA)
         }
 
         Timber.d("SyncMediaWorker: success")
