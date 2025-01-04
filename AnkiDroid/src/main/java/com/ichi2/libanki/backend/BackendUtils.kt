8d0cb50dc357a6262f0add0ba69b4a22cb5f7f8b
Arthur Milchior <arthur@milchior.fr>
2025-01-04 20:09:47 +0100

    NF: BackendUtils gets better type
    I had a bug due to an erroneous type sent to `toJsonBytes`. I believe
    it's better to indicates explicitly which type we know it can accept,
    and if needed add more type.

--- AnkiDroid/src/main/java/com/ichi2/libanki/backend/BackendUtils.kt
+++ AnkiDroid/src/main/java/com/ichi2/libanki/backend/BackendUtils.kt
@@ -20,6 +20,7 @@ package com.ichi2.libanki.backend
 
 import com.google.protobuf.ByteString
 import com.ichi2.libanki.utils.LibAnkiAlias
+import com.ichi2.utils.JSONObjectHolder
 import net.ankiweb.rsdroid.RustCleanup
 import org.json.JSONArray
 import org.json.JSONObject
@@ -31,12 +32,14 @@ object BackendUtils {
     fun jsonToArray(json: ByteString): JSONArray = JSONArray(json.toStringUtf8())
 
     @RustCleanup("Confirm edge cases")
-    fun toByteString(conf: Any?): ByteString {
+    fun toByteString(conf: JSONObject): ByteString {
         val asString: String = conf.toString()
         return ByteString.copyFromUtf8(asString)
     }
 
     @RustCleanup("Confirm edge cases")
     @LibAnkiAlias("to_json_bytes")
-    fun toJsonBytes(json: Any?): ByteString = toByteString(json)
+    fun toJsonBytes(json: JSONObject): ByteString = toByteString(json)
+
+    fun toJsonBytes(json: JSONObjectHolder): ByteString = toJsonBytes(json.jsonObject)
 }
