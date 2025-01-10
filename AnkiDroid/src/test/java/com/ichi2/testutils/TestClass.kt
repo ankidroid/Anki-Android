704fe3d4b5423824c41ac98b191fbd75e32b787e
David Allison <62114487+david-allison@users.noreply.github.com>
2025-01-10 02:31:51 +0000

    fix: addRevBasicNoteDueToday

--- AnkiDroid/src/test/java/com/ichi2/testutils/TestClass.kt
+++ AnkiDroid/src/test/java/com/ichi2/testutils/TestClass.kt
@@ -63,6 +63,7 @@ interface TestClass {
         card.queue = QueueType.Rev
         card.type = CardType.Rev
         card.due = col.sched.today
+        col.updateCards(listOf(card), skipUndoEntry = true)
         return note
     }
 
