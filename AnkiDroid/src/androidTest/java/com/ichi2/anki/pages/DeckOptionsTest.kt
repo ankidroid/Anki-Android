687c69829d7d770f0785e3284ee57d59bdbf6c10
David Allison <62114487+david-allison@users.noreply.github.com>
2025-01-22 01:58:02 +0000

    test(deck-options): discard dialog if changes made
    Issue 14438
    
    ----
    
    This was a difficult change to make:
    
    * timing considerations on WebView callbacks
    
    I spent time time trying to use `onView` inside `onActivity`.
    
    After research, `onView` should be outside the `onActivity`
    hook: https://stackoverflow.com/a/64284563

--- AnkiDroid/src/androidTest/java/com/ichi2/anki/pages/DeckOptionsTest.kt
+++ AnkiDroid/src/androidTest/java/com/ichi2/anki/pages/DeckOptionsTest.kt
@@ -17,6 +17,12 @@
 package com.ichi2.anki.pages
 
 import androidx.lifecycle.Lifecycle.State
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.isDialog
+import androidx.test.espresso.matcher.ViewMatchers.isEnabled
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.ichi2.anki.R
@@ -25,9 +31,11 @@ import com.ichi2.anki.tests.InstrumentedTest
 import com.ichi2.anki.testutil.findFragmentById
 import com.ichi2.libanki.Consts
 import com.ichi2.testutils.common.assertTrueWithTimeout
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import timber.log.Timber
 import kotlin.test.assertEquals
 
 /** Tests [DeckOptions] */
@@ -42,8 +50,29 @@ class DeckOptionsTest : InstrumentedTest() {
     @Test
     fun discardChangesIsNotShownIfNoChanges() = testDeckOptions { assertBackPressClosesOptions() }
 
+    @Test
+    fun discardChangesIsShownIfChangeMade() {
+        testDeckOptions { makeChange() }
+
+        Espresso.pressBack()
+
+        onView(withText("Discard current input?"))
+            .inRoot(isDialog())
+            .check(matches(isEnabled()))
+    }
+
+    @Test
+    @Ignore("broken on main")
+    fun discardChangesIsNotShownIfChangeIsReversed() =
+        testDeckOptions {
+            makeChangeAndUndo()
+            assertBackPressClosesOptions()
+        }
+
     /**
-     * Runs [block] with [DeckOptions] as the receiver. Intended for tests
+     * Runs [block] with [DeckOptions] as the receiver. Intended for test setup
+     *
+     * `onView` should not be called inside this method, instead call it afterwards
      */
     private fun testDeckOptions(block: (DeckOptions).() -> Unit) {
         assertEquals(State.RESUMED, activityRule.scenario.state)
@@ -58,4 +87,37 @@ private fun DeckOptions.assertBackPressClosesOptions() {
     requireActivity().onBackPressedDispatcher.onBackPressed()
     // we assert this as [actuallyClose] launches a task
     assertTrueWithTimeout("fragment closing") { isClosingFragment }
+}
+
+/** Changes an option, so there are unsaved changes */
+private fun DeckOptions.makeChange() {
+    toggleFsrs()
+}
+
+/** Changes an option, and undoes the change so there are no unsaved changes */
+private fun DeckOptions.makeChangeAndUndo() {
+    toggleFsrs()
+    toggleFsrs()
+}
+
+private fun DeckOptions.toggleFsrs() {
+    val js =
+        """
+        // Find the FSRS heading. This is not translated. Exclude the FSRS modal. 
+        const element = Array.from(document.getElementsByTagName("h1")).filter(x => x.innerText == "FSRS" && !x.classList.contains("modal-title"))[0];
+        // Find the 'FSRS' container
+        const container = element.closest(".container");
+        // Find the only checkbox, and click it
+        container.querySelectorAll('input[type="checkbox"]')[0].click()
+        """.trimIndent()
+
+    fun execToggleFsrsJs() = this.webView.evaluateJavascript(js) {}
+
+    if (pageWebViewClient.callbacksExecuted) {
+        Timber.v("scheduling JS callback: 'toggleFsrs'")
+        execToggleFsrsJs()
+    } else {
+        Timber.v("scheduling JS callback: 'toggleFsrs'")
+        pageWebViewClient.onPageFinishedCallbacks.add { execToggleFsrsJs() }
+    }
 }
