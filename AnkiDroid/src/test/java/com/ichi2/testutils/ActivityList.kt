24167402a2c6d8fb42506a964213fb50446e470a
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2025-02-01 03:38:44 +0530

    add activity to the activity list

--- AnkiDroid/src/test/java/com/ichi2/testutils/ActivityList.kt
+++ AnkiDroid/src/test/java/com/ichi2/testutils/ActivityList.kt
@@ -38,6 +38,7 @@ import com.ichi2.anki.Reviewer
 import com.ichi2.anki.SharedDecksActivity
 import com.ichi2.anki.SingleFragmentActivity
 import com.ichi2.anki.StudyOptionsActivity
+import com.ichi2.anki.account.AccountActivity
 import com.ichi2.anki.instantnoteeditor.InstantNoteEditorActivity
 import com.ichi2.anki.multimedia.MultimediaActivity
 import com.ichi2.anki.notetype.ManageNotetypes
@@ -93,6 +94,7 @@ object ActivityList {
             get(MultimediaActivity::class.java),
             get(DeckPickerWidgetConfig::class.java),
             get(CardAnalysisWidgetConfig::class.java),
+            get(AccountActivity::class.java),
         )
 
     private fun intentForCardTemplateBrowserAppearanceEditor(): Intent {
