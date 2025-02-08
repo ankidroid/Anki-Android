4329840c9683d34cfd3ecaa4c8a7caf9056445ab
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2025-02-08 15:00:01 +0530

    feat: implement media check option in the dialog - allow tagging option in media check dialog - split report generation methods

--- AnkiDroid/src/main/java/com/ichi2/anki/dialogs/MediaCheckDialog.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/dialogs/MediaCheckDialog.kt
@@ -6,6 +6,7 @@ import android.app.Dialog
 import android.os.Bundle
 import android.os.Message
 import android.text.method.ScrollingMovementMethod
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -13,12 +14,15 @@ import androidx.annotation.CheckResult
 import androidx.appcompat.app.AlertDialog
 import androidx.core.os.bundleOf
 import com.ichi2.anki.AnkiActivity
+import com.ichi2.anki.CollectionManager.TR
 import com.ichi2.anki.DeckPicker
 import com.ichi2.anki.R
 import com.ichi2.anki.dialogs.MediaCheckDialog.Type.DIALOG_CONFIRM_MEDIA_CHECK
 import com.ichi2.anki.dialogs.MediaCheckDialog.Type.DIALOG_MEDIA_CHECK_RESULTS
 import com.ichi2.anki.showError
+import com.ichi2.anki.ui.internationalization.toSentenceCase
 import com.ichi2.anki.utils.ext.dismissAllDialogFragments
+import com.ichi2.compat.CompatHelper.Companion.getSerializableCompat
 import com.ichi2.libanki.MediaCheckResult
 
 class MediaCheckDialog : AsyncDialogFragment() {
@@ -33,6 +37,8 @@ class MediaCheckDialog : AsyncDialogFragment() {
         fun mediaCheck()
 
         fun deleteUnused(unused: List<String>)
+
+        fun tagMissing(missingMediaNotes: List<Long>?)
     }
 
     private val dialogType: Type
@@ -46,6 +52,9 @@ class MediaCheckDialog : AsyncDialogFragment() {
 
     private val invalid: List<String>?
         get() = requireArguments().getStringArrayList(INVALID)
+
+    private val missingMediaNotes: List<Long>?
+        get() = requireArguments().getSerializableCompat<ArrayList<Long>>(MISSING_MEDIA_NOTES)
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         super.onCreate(savedInstanceState)
@@ -69,61 +78,114 @@ class MediaCheckDialog : AsyncDialogFragment() {
                 val unused = unused!!
                 val invalid = invalid!!
                 // Generate report
-                val report = StringBuilder()
-                if (invalid.isNotEmpty()) {
-                    report.append(String.format(res().getString(R.string.check_media_invalid), invalid.size))
-                }
-                if (unused.isNotEmpty()) {
-                    if (report.isNotEmpty()) {
-                        report.append("\n")
-                    }
-                    report.append(String.format(res().getString(R.string.check_media_unused), unused.size))
-                }
-                if (noHave.isNotEmpty()) {
-                    if (report.isNotEmpty()) {
-                        report.append("\n")
-                    }
-                    report.append(String.format(res().getString(R.string.check_media_nohave), noHave.size))
-                }
-                if (report.isEmpty()) {
-                    report.append(res().getString(R.string.check_media_no_unused_missing))
-                }
+                val report = generateReport(unused, noHave, invalid)
 
-                // We also prefix the report with a message about the media db being rebuilt, since
-                // we do a full media scan and update the db on each media check on AnkiDroid.
-                val reportStr =
-                    """
-                    |${res().getString(R.string.check_media_db_updated)}
-                    
-                    |$report
-                    """.trimMargin().trimIndent()
-                val dialogBody = layoutInflater.inflate(R.layout.media_check_dialog_body, null) as LinearLayout
-                val reportTextView = dialogBody.findViewById<TextView>(R.id.reportTextView)
-                val fileListTextView = dialogBody.findViewById<TextView>(R.id.fileListTextView)
-                reportTextView.text = reportStr
-                if (unused.isNotEmpty()) {
-                    reportTextView.append(getString(R.string.unused_strings))
-                    fileListTextView.append(unused.joinToString("\n"))
-                    fileListTextView.isScrollbarFadingEnabled = unused.size <= fileListTextView.maxLines
-                    fileListTextView.movementMethod = ScrollingMovementMethod.getInstance()
-                    fileListTextView.setTextIsSelectable(true)
-                    dialog
-                        .setPositiveButton(R.string.check_media_delete_unused) { _, _ ->
-                            (activity as MediaCheckDialogListener?)?.deleteUnused(unused)
-                            activity?.dismissAllDialogFragments()
-                        }.setNegativeButton(R.string.dialog_cancel) { _, _ ->
-                            activity?.dismissAllDialogFragments()
-                        }
-                } else {
-                    fileListTextView.visibility = View.GONE
-                    dialog.setNegativeButton(R.string.dialog_ok) { _, _ ->
-                        activity?.dismissAllDialogFragments()
-                    }
-                }
+                val dialogBody = setupDialogBody(layoutInflater, report, noHave, unused)
+
                 dialog
                     .setView(dialogBody)
                     .setCancelable(false)
-                    .create()
+                    .apply {
+                        if (unused.isEmpty() && noHave.isEmpty()) {
+                            setNegativeButton(R.string.dialog_ok) { _, _ -> activity?.dismissAllDialogFragments() }
+                            return@apply
+                        }
+
+                        setPositiveButton(
+                            TR.mediaCheckDeleteUnused().toSentenceCase(requireContext(), R.string.check_media_delete_unused),
+                        ) { _, _ ->
+                            (activity as MediaCheckDialogListener?)?.deleteUnused(unused)
+                            activity?.dismissAllDialogFragments()
+                        }
+                        setNeutralButton(R.string.dialog_cancel) { _, _ -> activity?.dismissAllDialogFragments() }
+
+                        if (noHave.isNotEmpty()) {
+                            setNegativeButton(TR.mediaCheckAddTag().toSentenceCase(requireContext(), R.string.tag_missing)) { _, _ ->
+                                (activity as MediaCheckDialogListener?)?.tagMissing(missingMediaNotes)
+                                activity?.dismissAllDialogFragments()
+                            }
+                        }
+                    }.create()
+            }
+        }
+    }
+
+    private fun generateReport(
+        unused: List<String>,
+        noHave: List<String>,
+        invalid: List<String>,
+    ): String {
+        val report = StringBuilder()
+        if (invalid.isNotEmpty()) {
+            report.append(String.format(res().getString(R.string.check_media_invalid), invalid.size))
+        }
+
+        if (noHave.isNotEmpty()) {
+            if (report.isNotEmpty()) {
+                report.append("\n")
+            }
+            report.append(TR.mediaCheckMissingCount(noHave.size))
+        }
+
+        if (unused.isNotEmpty()) {
+            if (report.isNotEmpty()) {
+                report.append("\n")
+            }
+            report.append(TR.mediaCheckUnusedCount(unused.size))
+        }
+        if (report.isEmpty()) {
+            report.append(res().getString(R.string.check_media_no_unused_missing))
+        }
+        return report.toString()
+    }
+
+    private fun setupDialogBody(
+        inflater: LayoutInflater,
+        report: String,
+        noHave: List<String>,
+        unused: List<String>,
+    ): LinearLayout {
+        val dialogBody = inflater.inflate(R.layout.media_check_dialog_body, null) as LinearLayout
+        val reportTextView = dialogBody.findViewById<TextView>(R.id.reportTextView)
+        val fileListTextView = dialogBody.findViewById<TextView>(R.id.fileListTextView)
+
+        reportTextView.text = createReportString(report)
+
+        if (unused.isNotEmpty() || noHave.isNotEmpty()) {
+            fileListTextView.text = formatMissingAndUnusedFiles(noHave, unused)
+            fileListTextView.isScrollbarFadingEnabled = unused.size + noHave.size <= fileListTextView.maxLines
+            fileListTextView.movementMethod = ScrollingMovementMethod.getInstance()
+            fileListTextView.setTextIsSelectable(true)
+        } else {
+            fileListTextView.visibility = View.GONE
+        }
+
+        return dialogBody
+    }
+
+    private fun createReportString(report: String): String =
+        """
+        |$report
+        """.trimMargin().trimIndent()
+
+    private fun formatMissingAndUnusedFiles(
+        noHave: List<String>,
+        unused: List<String>,
+    ): String {
+        val noHaveFormatted = noHave.joinToString("\n") { missingMedia -> TR.mediaCheckMissingFile(missingMedia) }
+        val unusedFormatted = unused.joinToString("\n") { unusedMedia -> TR.mediaCheckUnusedFile(unusedMedia) }
+
+        return buildString {
+            if (noHaveFormatted.isNotEmpty()) {
+                append(TR.mediaCheckMissingHeader())
+                append("\n")
+                append(noHaveFormatted)
+                append("\n\n")
+            }
+            if (unusedFormatted.isNotEmpty()) {
+                append(TR.mediaCheckUnusedHeader())
+                append("\n")
+                append(unusedFormatted)
             }
         }
     }
@@ -139,12 +201,12 @@ class MediaCheckDialog : AsyncDialogFragment() {
         get() =
             when (dialogType) {
                 DIALOG_CONFIRM_MEDIA_CHECK -> res().getString(R.string.check_media_title)
-                DIALOG_MEDIA_CHECK_RESULTS -> res().getString(R.string.app_name)
+                DIALOG_MEDIA_CHECK_RESULTS -> TR.mediaCheckCheckMediaAction()
             }
 
     override val dialogHandlerMessage: MediaCheckCompleteDialog
         get() {
-            return MediaCheckCompleteDialog(dialogType, noHave, unused, invalid)
+            return MediaCheckCompleteDialog(dialogType, noHave, unused, invalid, missingMediaNotes)
         }
 
     enum class Type(
@@ -180,6 +242,11 @@ class MediaCheckDialog : AsyncDialogFragment() {
          */
         const val INVALID = "invalid"
 
+        /**
+         * Key for a list of notes with missing media
+         */
+        const val MISSING_MEDIA_NOTES = "missingMediaNotes"
+
         @CheckResult
         fun newInstance(dialogType: Type) =
             MediaCheckDialog().apply { arguments = bundleOf(MEDIA_CHECK_DIALOG_TYPE_KEY to dialogType.code) }
@@ -196,6 +263,7 @@ class MediaCheckDialog : AsyncDialogFragment() {
                     UNUSED to ArrayList(checkList.unusedFileNames),
                     INVALID to ArrayList(checkList.invalidFileNames),
                     MEDIA_CHECK_DIALOG_TYPE_KEY to dialogType.code,
+                    MISSING_MEDIA_NOTES to ArrayList(checkList.missingMediaNotes).toList(),
                 )
         }
     }
@@ -205,6 +273,7 @@ class MediaCheckDialog : AsyncDialogFragment() {
         private val noHave: List<String>?,
         private val unused: List<String>?,
         private val invalid: List<String>?,
+        private val missingMediaNotes: List<Long>?,
     ) : DialogHandlerMessage(WhichDialogHandler.MSG_SHOW_MEDIA_CHECK_COMPLETE_DIALOG, "MediaCheckCompleteDialog") {
         override fun handleAsyncMessage(activity: AnkiActivity) {
             // Media check results
@@ -220,7 +289,13 @@ class MediaCheckDialog : AsyncDialogFragment() {
                         )
                         return
                     }
-                    val checkList = MediaCheckResult(noHave ?: arrayListOf(), unused ?: arrayListOf(), invalid ?: arrayListOf())
+                    val checkList =
+                        MediaCheckResult(
+                            noHave ?: arrayListOf(),
+                            unused ?: arrayListOf(),
+                            invalid ?: arrayListOf(),
+                            missingMediaNotes ?: arrayListOf(),
+                        )
                     activity.showMediaCheckDialog(dialogType, checkList)
                 }
                 DIALOG_CONFIRM_MEDIA_CHECK -> { }
@@ -236,6 +311,7 @@ class MediaCheckDialog : AsyncDialogFragment() {
                         UNUSED to unused,
                         INVALID to invalid,
                         MEDIA_CHECK_DIALOG_TYPE_KEY to dialogType,
+                        MISSING_MEDIA_NOTES to missingMediaNotes,
                     )
             }
 
@@ -245,7 +321,9 @@ class MediaCheckDialog : AsyncDialogFragment() {
                 val noHave = message.data.getStringArrayList(NO_HAVE)
                 val unused = message.data.getStringArrayList(UNUSED)
                 val invalid = message.data.getStringArrayList(INVALID)
-                return MediaCheckCompleteDialog(dialogType, noHave, unused, invalid)
+                val missingMediaNotes = message.data.getLongArray(MISSING_MEDIA_NOTES)?.toList()
+
+                return MediaCheckCompleteDialog(dialogType, noHave, unused, invalid, missingMediaNotes)
             }
         }
     }
