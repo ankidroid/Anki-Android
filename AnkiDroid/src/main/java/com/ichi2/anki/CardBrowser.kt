9261218816eaac2f0cb5352e4c6b949fdb727772
Henrique Costa <henrique.garcia.costa@tecnico.ulisboa.pt>
2025-03-26 13:05:15 +0000

    Fix #15993: Scroll position wrong after editing When editing a card in the CardBrowser, after scrolling down and selecting a card at the bottom of the list, the scroll position is lost when returning to the CardBrowser. This happens because the scroll returns to an incorrect position after saving the edited card. To fix this, I now save the position of the first visible item when clicking on a card (onTap()), and when returning to the CardBrowser after saving, the scroll is adjusted back to the previously saved position (onResume()).

--- AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.kt
@@ -352,6 +352,8 @@ open class CardBrowser :
                 viewModel.saveScrollingState(id)
                 viewModel.oldCardTopOffset = calculateTopOffset(viewModel.lastSelectedPosition)
             } else {
+                viewModel.lastSelectedPosition = (cardsListView.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+                viewModel.oldCardTopOffset = calculateTopOffset(viewModel.lastSelectedPosition)
                 val cardId = viewModel.queryDataForCardEdit(id)
                 openNoteEditorForCard(cardId)
             }
@@ -895,6 +897,7 @@ open class CardBrowser :
     override fun onResume() {
         super.onResume()
         selectNavigationItem(R.id.nav_browser)
+        autoScrollTo(viewModel.lastSelectedPosition, viewModel.oldCardTopOffset)
     }
 
     @KotlinCleanup("Add a few variables to get rid of the !!")
