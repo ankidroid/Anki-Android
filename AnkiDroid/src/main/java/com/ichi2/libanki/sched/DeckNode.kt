e99b3b4112b0534a9a4f93c971dac279159f3ad3
Arthur Milchior <arthur@milchior.fr>
2025-05-08 06:57:57 +0200

    NF: Comment DeckNode
    I found DeckNode slightly hard to use, as it was not clear exactly
    what each value was and what each function does.
    
    For example, I would not have guessed that the parent of the top-level
    deck is a root DeckNode that is not actually a deck. Or that the
    counts included the cards to review in children decks.
    
    I also modified an access to the parent to explain why `get()!!` is
    safe.
    
    Admittedly, I don't think that we should have used a WeakReference for
    the parent here. The whole tree should have the same lifespan. If it's
    not the case and a children outlive its parents, it's already an issue
    and being able to release its parent may not save thas much memory overall.

--- AnkiDroid/src/main/java/com/ichi2/libanki/sched/DeckNode.kt
+++ AnkiDroid/src/main/java/com/ichi2/libanki/sched/DeckNode.kt
@@ -20,15 +20,44 @@ import com.ichi2.libanki.DeckId
 import java.lang.ref.WeakReference
 import java.util.Locale
 
+/**
+ * An object of this class represent a [com.ichi2.libanki.Deck], with all information that may be useful for its usage, such as its subdecks and the number of cards to review today.
+ * This object can also represent the root of the tree, of depth -1, that is the parent of all top-level decks. All non-root decks are called read decks.
+ * No change to this object are retained in the collection. Any change must be made through collections methods such as the ones of [com.ichi2.libanki.Decks].
+ *
+ * Objects of this type should not be retained, as this could cause them to outlive their parent. The only exception being the above-mentioned root deck that has no parent.
+ *
+ * The hierarchy is exactly the same as the one returned by [net.ankiweb.rsdroid.Backend.deckTree].
+ */
 data class DeckNode(
     val node: DeckTreeNode,
     val fullDeckName: String,
+    /**
+     * Non-null for real decks. The parent of a top-level deck is the root of the tree.
+     */
     val parent: WeakReference<DeckNode>? = null,
 ) : Iterable<DeckNode> {
     // TODO: make this immutable and simplify 'DisplayDeckNode' to contain a 'DeckNode'
+    /**
+     * Whether or not the deck should appear as collapsed in the deck picker.
+     * Change to this variable is not retained in the collection. The caller must change the [com.ichi2.libanki.Deck] object with the same id and save it.
+     */
     var collapsed = node.collapsed
+
+    /**
+     * Number of review cards to review today, as displayed in the deck picker, including count of subdecks.
+     */
     val revCount = node.reviewCount
+
+    /**
+     * Number of new cards to review today, as displayed in the deck picker, including count of subdecks.
+     */
     val newCount = node.newCount
+
+    /**
+     * Number of cards in learning to review today, as displayed in the deck picker, including count of subdecks.
+     * Note that this value will quite often be smaller than the value that would be returned by the back-end. This is because a card in learning may be due at any second.
+     */
     val lrnCount = node.learnCount
     val did = node.deckId
     val filtered = node.filtered
@@ -57,6 +86,12 @@ data class DeckNode(
      */
     val depth = node.level - 1
 
+    /**
+     * True for the root of the deck tree.
+     * False if this node corresponds to an actual deck that the user can interact with.
+     */
+    val isSyntheticDeck = node.level <= 0
+
     override fun toString(): String =
         String.format(
             Locale.US,
@@ -68,8 +103,15 @@ data class DeckNode(
             newCount,
         )
 
+    /**
+     * Whether we expect that tapping on this deck would allow to start reviewing.
+     * There can be false negative but no false positive.
+     */
     fun hasCardsReadyToStudy(): Boolean = revCount > 0 || newCount > 0 || lrnCount > 0
 
+    /**
+     * The node with [did] [deckId], if it is either this node or a descendant.
+     */
     fun find(deckId: DeckId): DeckNode? {
         if (node.deckId == deckId) {
             return this
@@ -80,8 +122,11 @@ data class DeckNode(
         return null
     }
 
+    /**
+     * Run [fn] on this node (unless its the root) and its descendants.
+     */
     fun forEach(fn: (DeckNode) -> Unit) {
-        if (node.level > 0) {
+        if (!isSyntheticDeck) {
             fn(this)
         }
         for (child in children) {
@@ -89,9 +134,12 @@ data class DeckNode(
         }
     }
 
+    /**
+     * An iterator returning this deck (unless it's the root) and all descendant.
+     */
     override fun iterator(): Iterator<DeckNode> =
         sequence {
-            if (node.level > 0) yield(this@DeckNode)
+            if (!isSyntheticDeck) yield(this@DeckNode)
             for (child in children) yieldAll(child)
         }.iterator()
 }
