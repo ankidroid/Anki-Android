83c8c1a8c7ac4801173b570fb89f9bfb5de4e249
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2025-06-07 09:22:03 -0300

    refactor: move WebViewClient creation to a separate class
    so single components can be overriden

--- AnkiDroid/src/main/java/com/ichi2/anki/previewer/CardViewerFragment.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/previewer/CardViewerFragment.kt
@@ -128,94 +128,97 @@ abstract class CardViewerFragment(
             .launchIn(lifecycleScope)
     }
 
-    private fun onCreateWebViewClient(savedInstanceState: Bundle?): WebViewClient {
-        val resourceHandler = ViewerResourceHandler(requireContext())
-        return object : WebViewClient() {
-            override fun shouldInterceptRequest(
-                view: WebView?,
-                request: WebResourceRequest,
-            ): WebResourceResponse? = resourceHandler.shouldInterceptRequest(request)
+    protected open fun onCreateWebViewClient(savedInstanceState: Bundle?): WebViewClient = CardViewerWebViewClient(savedInstanceState)
 
-            override fun onPageFinished(
-                view: WebView?,
-                url: String?,
-            ) {
-                viewModel.onPageFinished(isAfterRecreation = savedInstanceState != null)
-            }
+    open inner class CardViewerWebViewClient(
+        val savedInstanceState: Bundle?,
+    ) : WebViewClient() {
+        private val resourceHandler = ViewerResourceHandler(requireContext())
 
-            override fun shouldOverrideUrlLoading(
-                view: WebView,
-                request: WebResourceRequest,
-            ): Boolean = handleUrl(request.url)
+        override fun shouldInterceptRequest(
+            view: WebView?,
+            request: WebResourceRequest,
+        ): WebResourceResponse? = resourceHandler.shouldInterceptRequest(request)
 
-            @Suppress("DEPRECATION") // necessary in API 23
-            @Deprecated("Deprecated in Java")
-            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                if (view == null || url == null) return super.shouldOverrideUrlLoading(view, url)
-                return handleUrl(url.toUri())
-            }
+        override fun onPageFinished(
+            view: WebView?,
+            url: String?,
+        ) {
+            viewModel.onPageFinished(isAfterRecreation = savedInstanceState != null)
+        }
 
-            private fun handleUrl(url: Uri): Boolean {
-                when (url.scheme) {
-                    "playsound" -> viewModel.playSoundFromUrl(url.toString())
-                    "videoended" -> viewModel.onVideoFinished()
-                    "videopause" -> viewModel.onVideoPaused()
-                    "tts-voices" -> TtsVoicesDialogFragment().show(childFragmentManager, null)
-                    "android-app" -> handleIntentUrl(url, Intent.URI_ANDROID_APP_SCHEME)
-                    "intent" -> handleIntentUrl(url, Intent.URI_INTENT_SCHEME)
-                    "missing-user-action" -> {
-                        val actionNumber = url.toString().substringAfter(":")
-                        val message = getString(R.string.missing_user_action_dialog_message, actionNumber)
-                        AlertDialog.Builder(requireContext()).show {
-                            setMessage(message)
-                            setPositiveButton(R.string.dialog_ok) { _, _ -> }
-                            setNeutralButton(R.string.help) { _, _ ->
-                                openUrl(R.string.link_user_actions_help)
-                            }
-                        }
-                    }
-                    else -> {
-                        try {
-                            openUrl(url)
-                        } catch (_: Throwable) {
-                            Timber.w("Could not open url")
-                            return false
+        override fun shouldOverrideUrlLoading(
+            view: WebView,
+            request: WebResourceRequest,
+        ): Boolean = handleUrl(request.url)
+
+        @Suppress("DEPRECATION") // necessary in API 23
+        @Deprecated("Deprecated in Java")
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+            if (view == null || url == null) return super.shouldOverrideUrlLoading(view, url)
+            return handleUrl(url.toUri())
+        }
+
+        private fun handleUrl(url: Uri): Boolean {
+            when (url.scheme) {
+                "playsound" -> viewModel.playSoundFromUrl(url.toString())
+                "videoended" -> viewModel.onVideoFinished()
+                "videopause" -> viewModel.onVideoPaused()
+                "tts-voices" -> TtsVoicesDialogFragment().show(childFragmentManager, null)
+                "android-app" -> handleIntentUrl(url, Intent.URI_ANDROID_APP_SCHEME)
+                "intent" -> handleIntentUrl(url, Intent.URI_INTENT_SCHEME)
+                "missing-user-action" -> {
+                    val actionNumber = url.toString().substringAfter(":")
+                    val message = getString(R.string.missing_user_action_dialog_message, actionNumber)
+                    AlertDialog.Builder(requireContext()).show {
+                        setMessage(message)
+                        setPositiveButton(R.string.dialog_ok) { _, _ -> }
+                        setNeutralButton(R.string.help) { _, _ ->
+                            openUrl(R.string.link_user_actions_help)
                         }
                     }
                 }
-                return true
-            }
-
-            private fun handleIntentUrl(
-                url: Uri,
-                flags: Int,
-            ) {
-                try {
-                    val intent = Intent.parseUri(url.toString(), flags)
-                    if (packageManager.resolveActivityCompat(intent) != null) {
-                        startActivity(intent)
-                    } else {
-                        val packageName = intent.getPackage() ?: return
-                        val marketUri = "market://details?id=$packageName".toUri()
-                        val marketIntent = Intent(Intent.ACTION_VIEW, marketUri)
-                        Timber.d("Trying to open market uri %s", marketUri)
-                        if (packageManager.resolveActivityCompat(marketIntent) != null) {
-                            startActivity(marketIntent)
-                        }
+                else -> {
+                    try {
+                        openUrl(url)
+                    } catch (_: Throwable) {
+                        Timber.w("Could not open url")
+                        return false
                     }
-                } catch (t: Throwable) {
-                    Timber.w("Unable to parse intent uri: %s because: %s", url, t.message)
                 }
             }
+            return true
+        }
 
-            override fun onReceivedError(
-                view: WebView,
-                request: WebResourceRequest,
-                error: WebResourceError,
-            ) {
-                viewModel.mediaErrorHandler.processFailure(request) { filename: String ->
-                    showMediaErrorSnackbar(filename)
+        private fun handleIntentUrl(
+            url: Uri,
+            flags: Int,
+        ) {
+            try {
+                val intent = Intent.parseUri(url.toString(), flags)
+                if (packageManager.resolveActivityCompat(intent) != null) {
+                    startActivity(intent)
+                } else {
+                    val packageName = intent.getPackage() ?: return
+                    val marketUri = "market://details?id=$packageName".toUri()
+                    val marketIntent = Intent(Intent.ACTION_VIEW, marketUri)
+                    Timber.d("Trying to open market uri %s", marketUri)
+                    if (packageManager.resolveActivityCompat(marketIntent) != null) {
+                        startActivity(marketIntent)
+                    }
                 }
+            } catch (t: Throwable) {
+                Timber.w("Unable to parse intent uri: %s because: %s", url, t.message)
+            }
+        }
+
+        override fun onReceivedError(
+            view: WebView,
+            request: WebResourceRequest,
+            error: WebResourceError,
+        ) {
+            viewModel.mediaErrorHandler.processFailure(request) { filename: String ->
+                showMediaErrorSnackbar(filename)
             }
         }
     }
