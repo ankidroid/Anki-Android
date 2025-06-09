f992eea4a3899eb406915598a6e69d0a15036951
David Allison <62114487+david-allison@users.noreply.github.com>
2025-06-09 18:24:14 +0100

    refactor(card-browser): convert to fragment
    * move flows to fragment
    
    Flows needed to go together to handle lifecycle
    changes: LinearProgressIndicator
    
    Issue 17901

---
+++ AnkiDroid/src/main/java/com/ichi2/anki/browser/CardBrowserFragment.kt
@@ -0,0 +1,189 @@
+/*
+ *  Copyright (c) 2025 David Allison <davidallisongithub@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ichi2.anki.browser
+
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.VisibleForTesting
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.progressindicator.LinearProgressIndicator
+import com.ichi2.anki.CardBrowser
+import com.ichi2.anki.R
+import com.ichi2.anki.browser.CardBrowserViewModel.SearchState
+import com.ichi2.anki.browser.CardBrowserViewModel.SearchState.Initializing
+import com.ichi2.anki.browser.CardBrowserViewModel.SearchState.Searching
+import com.ichi2.anki.common.utils.android.isRobolectric
+import com.ichi2.anki.launchCatchingTask
+import com.ichi2.anki.ui.attachFastScroller
+import com.ichi2.utils.HandlerUtils
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import timber.log.Timber
+
+class CardBrowserFragment : Fragment(R.layout.cardbrowser) {
+    val viewModel: CardBrowserViewModel by activityViewModels()
+
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    lateinit var cardsAdapter: BrowserMultiColumnAdapter
+
+    @VisibleForTesting
+    lateinit var cardsListView: RecyclerView
+
+    @VisibleForTesting
+    lateinit var browserColumnHeadings: ViewGroup
+
+    private lateinit var progressIndicator: LinearProgressIndicator
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        cardsListView =
+            view.findViewById<RecyclerView?>(R.id.card_browser_list).apply {
+                attachFastScroller(R.id.browser_scroller)
+            }
+        DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL).apply {
+            setDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.browser_divider)!!)
+            cardsListView.addItemDecoration(this)
+        }
+        cardsAdapter =
+            BrowserMultiColumnAdapter(
+                requireContext(),
+                viewModel,
+                onTap = ::onTap,
+                onLongPress = viewModel::handleRowLongPress,
+            )
+        cardsListView.adapter = cardsAdapter
+        cardsAdapter.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+        val layoutManager = LinearLayoutManager(requireContext())
+        cardsListView.layoutManager = layoutManager
+        cardsListView.addItemDecoration(DividerItemDecoration(requireContext(), layoutManager.orientation))
+
+        this.browserColumnHeadings = view.findViewById(R.id.browser_column_headings)
+
+        progressIndicator = view.findViewById(R.id.browser_progress)
+
+        setupFlows()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        if (::cardsListView.isInitialized) {
+            cardsListView.adapter = null
+        }
+    }
+
+    @Suppress("UNUSED_PARAMETER", "unused")
+    private fun setupFlows() {
+        fun onIsTruncatedChanged(isTruncated: Boolean) = cardsAdapter.notifyDataSetChanged()
+
+        fun cardsUpdatedChanged(unit: Unit) = cardsAdapter.notifyDataSetChanged()
+
+        fun onColumnsChanged(columnCollection: BrowserColumnCollection) {
+            Timber.d("columns changed")
+            cardsAdapter.notifyDataSetChanged()
+        }
+
+        fun isInMultiSelectModeChanged(inMultiSelect: Boolean) {
+            // update adapter to remove check boxes
+            cardsAdapter.notifyDataSetChanged()
+        }
+
+        fun searchStateChanged(searchState: SearchState) {
+            cardsAdapter.notifyDataSetChanged()
+            progressIndicator.isVisible = searchState == Initializing || searchState == Searching
+        }
+
+        fun onSelectedRowUpdated(id: CardOrNoteId?) {
+            cardsAdapter.focusedRow = id
+            if (!viewModel.isInMultiSelectMode || viewModel.lastSelectedId == null) {
+                viewModel.oldCardTopOffset =
+                    calculateTopOffset(viewModel.lastSelectedPosition)
+            }
+        }
+
+        viewModel.flowOfIsTruncated.launchCollectionInLifecycleScope(::onIsTruncatedChanged)
+        viewModel.flowOfActiveColumns.launchCollectionInLifecycleScope(::onColumnsChanged)
+        viewModel.flowOfCardsUpdated.launchCollectionInLifecycleScope(::cardsUpdatedChanged)
+        viewModel.flowOfIsInMultiSelectMode.launchCollectionInLifecycleScope(::isInMultiSelectModeChanged)
+        viewModel.flowOfSearchState.launchCollectionInLifecycleScope(::searchStateChanged)
+        viewModel.rowLongPressFocusFlow.launchCollectionInLifecycleScope(::onSelectedRowUpdated)
+    }
+
+    // TODO: Move this to ViewModel and test
+    @VisibleForTesting
+    fun onTap(id: CardOrNoteId) =
+        launchCatchingTask {
+            cardsAdapter.focusedRow = id
+            if (viewModel.isInMultiSelectMode) {
+                val wasSelected = viewModel.selectedRows.contains(id)
+                viewModel.toggleRowSelection(id)
+                viewModel.saveScrollingState(id)
+                viewModel.oldCardTopOffset = calculateTopOffset(viewModel.lastSelectedPosition)
+                // Load NoteEditor on trailing side if card is selected
+                if (wasSelected) {
+                    viewModel.currentCardId = id.toCardId(viewModel.cardsOrNotes)
+                    requireCardBrowserActivity().loadNoteEditorFragmentIfFragmented()
+                }
+            } else {
+                viewModel.lastSelectedPosition = (cardsListView.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+                viewModel.oldCardTopOffset = calculateTopOffset(viewModel.lastSelectedPosition)
+                val cardId = viewModel.queryDataForCardEdit(id)
+                requireCardBrowserActivity().openNoteEditorForCard(cardId)
+            }
+        }
+
+    private fun calculateTopOffset(cardPosition: Int): Int {
+        val layoutManager = cardsListView.layoutManager as LinearLayoutManager
+        val firstVisiblePosition = layoutManager.findFirstVisibleItemPosition()
+        val view = cardsListView.getChildAt(cardPosition - firstVisiblePosition)
+        return view?.top ?: 0
+    }
+
+    private fun requireCardBrowserActivity(): CardBrowser = requireActivity() as CardBrowser
+
+    // TODO: Move this to an extension method once we have context parameters
+    private fun <T> Flow<T>.launchCollectionInLifecycleScope(block: suspend (T) -> Unit) {
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                this@launchCollectionInLifecycleScope.collect {
+                    if (isRobolectric) {
+                        // hack: lifecycleScope/runOnUiThread do not handle our
+                        // test dispatcher overriding both IO and Main
+                        // in tests, waitForAsyncTasksToComplete may be required.
+                        HandlerUtils.postOnNewHandler { runBlocking { block(it) } }
+                    } else {
+                        block(it)
+                    }
+                }
+            }
+        }
+    }
+}
