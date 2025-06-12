62302788884296a137f18f4b3294cad7d984f82d
David Allison <62114487+david-allison@users.noreply.github.com>
2025-06-12 18:37:00 +0100

    refactor: rename NoteTypeEditorContextMenu
    to NoteTypeFieldEditorContextMenu

--- AnkiDroid/src/androidTest/java/com/ichi2/anki/dialogs/NoteTypeFieldEditorContextMenuTest.kt
+++ AnkiDroid/src/androidTest/java/com/ichi2/anki/dialogs/NoteTypeFieldEditorContextMenuTest.kt
@@ -19,29 +19,33 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.testing.launchFragment
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.isDialog
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.ichi2.anki.R
-import com.ichi2.anki.dialogs.NoteTypeEditorContextMenu.NoteTypeEditorContextMenuAction
+import com.ichi2.anki.dialogs.NoteTypeFieldEditorContextMenu.NoteTypeFieldEditorContextMenuAction
 import com.ichi2.anki.tests.InstrumentedTest
 import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 
+/** Tests [NoteTypeFieldEditorContextMenu] */
 @RunWith(AndroidJUnit4::class)
-class NoteTypeEditorContextMenuTest : InstrumentedTest() {
+class NoteTypeFieldEditorContextMenuTest : InstrumentedTest() {
     private val testDialogTitle = "test editor title"
 
     @Test
     @Ignore("flaky")
     fun showsAllOptions() {
         launchFragment(
-            fragmentArgs = bundleOf(NoteTypeEditorContextMenu.KEY_LABEL to testDialogTitle),
+            fragmentArgs = bundleOf(NoteTypeFieldEditorContextMenu.KEY_LABEL to testDialogTitle),
             themeResId = R.style.Theme_Light,
-        ) { NoteTypeEditorContextMenu() }
-        onView(withText(testDialogTitle)).check(matches(isDisplayed()))
-        NoteTypeEditorContextMenuAction.entries.forEach {
+        ) { NoteTypeFieldEditorContextMenu() }
+        onView(withText(testDialogTitle))
+            .inRoot(isDialog())
+            .check(matches(isDisplayed()))
+        NoteTypeFieldEditorContextMenuAction.entries.forEach {
             onView(withText(it.actionTextId)).check(matches(isDisplayed()))
         }
     }
