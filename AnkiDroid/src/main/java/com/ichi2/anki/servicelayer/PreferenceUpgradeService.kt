a69c206f50779214e19584cbcac7df1f48d687d7
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2025-06-14 13:07:18 -0300

    fix: remove long gesture remnant after upgrade
    follow-up 11d21be8ae4dfdd3e9ed3829c855bc4a2f83b3ac

--- AnkiDroid/src/main/java/com/ichi2/anki/servicelayer/PreferenceUpgradeService.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/servicelayer/PreferenceUpgradeService.kt
@@ -51,6 +51,7 @@ import com.ichi2.anki.reviewer.FullScreenMode
 import com.ichi2.anki.reviewer.MappableBinding
 import com.ichi2.anki.reviewer.MappableBinding.Companion.toPreferenceString
 import com.ichi2.anki.reviewer.ReviewerBinding
+import com.ichi2.annotations.NeedsTest
 import com.ichi2.libanki.Consts
 import com.ichi2.utils.HashUtil.hashSetInit
 import timber.log.Timber
@@ -126,6 +127,7 @@ object PreferenceUpgradeService {
                     yield(RemoveNoCodeFormatting())
                     yield(UpgradeBrowserColumns())
                     yield(RemoveLastExportedAtTime())
+                    yield(RemoveLongTouchGesture())
                 }
 
             /** Returns a list of preference upgrade classes which have not been applied */
@@ -674,6 +676,22 @@ object PreferenceUpgradeService {
                 preferences.edit {
                     remove("last_successful_export_mod")
                     remove("last_successful_export_second")
+                }
+            }
+        }
+
+        @NeedsTest("long touch gesture is removed from preferences")
+        internal class RemoveLongTouchGesture : PreferenceUpgrade(21) {
+            override fun upgrade(preferences: SharedPreferences) {
+                for (command in ViewerCommand.entries) {
+                    val value = preferences.getString(command.preferenceKey, null) ?: continue
+                    val bindings = ReviewerBinding.fromPreferenceString(value)
+                    val unknown = bindings.filter { it.binding is Binding.UnknownBinding }
+                    if (unknown.isEmpty()) continue
+                    val newBindings = bindings - unknown
+                    preferences.edit {
+                        putString(command.preferenceKey, newBindings.toPreferenceString())
+                    }
                 }
             }
         }
