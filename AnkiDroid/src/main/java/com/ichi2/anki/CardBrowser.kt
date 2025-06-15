18a95d03ee89e590c4ea3f250453764848be0bc2
Arthur Milchior <arthur@milchior.fr>
2025-06-15 15:34:43 +0200

    NF: some types are changed from int to enum
    This makes clearer what the type is.
    Sadly, this can't be done everywhere when the Int is a Android enum
    encoded as an interface

--- AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.kt
@@ -1368,7 +1368,7 @@ open class CardBrowser :
                     savedFilters,
                     mySearchesDialogListener,
                     "",
-                    CardBrowserMySearchesDialog.CARD_BROWSER_MY_SEARCHES_TYPE_LIST,
+                    CardBrowserMySearchesDialog.SavedSearchesType.List,
                 ),
             )
         }
@@ -1381,7 +1381,7 @@ open class CardBrowser :
                 null,
                 mySearchesDialogListener,
                 searchTerms,
-                CardBrowserMySearchesDialog.CARD_BROWSER_MY_SEARCHES_TYPE_SAVE,
+                CardBrowserMySearchesDialog.SavedSearchesType.Save,
             ),
         )
     }
