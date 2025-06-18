760bee32089c093436e6aa4e63a702428a1c6cfc
David Allison <62114487+david-allison@users.noreply.github.com>
2025-06-19 00:57:46 +0100

    chore: add 'libanki' dependencies
    `com.ichi2.anki.libanki` files will be moved here
    
    Issue 18015

--- libanki/build.gradle.kts
+++ libanki/build.gradle.kts
@@ -1,13 +1,69 @@
+import java.util.Properties
+
 plugins {
-    id("java-library")
-    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+
+android {
+    namespace = "com.ichi2.anki.libanki"
+    compileSdk =
+        libs.versions.compileSdk
+            .get()
+            .toInt()
+
+    defaultConfig {
+        minSdk =
+            libs.versions.minSdk
+                .get()
+                .toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    // Project dependencies
+    implementation(project(":common"))
+
+    // Backend libraries
+    implementation(libs.protobuf.kotlin.lite) // This is required when loading from a file
+
+    val localProperties = Properties()
+    if (project.rootProject.file("local.properties").exists()) {
+        localProperties.load(project.rootProject.file("local.properties").inputStream())
+    }
+    if (localProperties["local_backend"] == "true") {
+        implementation(files("../../Anki-Android-Backend/rsdroid/build/outputs/aar/rsdroid-release.aar"))
+        testImplementation(files("../../Anki-Android-Backend/rsdroid-testing/build/libs/rsdroid-testing.jar"))
+    } else {
+        implementation(libs.ankiBackend.backend)
+        testImplementation(libs.ankiBackend.testing)
+    }
+
+    // JVM dependencies
+    implementation(libs.jakewharton.timber)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.serialization.json)
+
+    // Android dependencies
+    implementation(libs.androidx.annotation)
 }
--- libanki/src/main/java/com/ichi2/anki/libanki/MyClass.kt
+++
@@ -1,3 +0,0 @@
-package com.ichi2.anki.libanki
-
-class MyClass
