44820b42fdbafed63891f66dc16e8f0c460a8ca4
David Allison <62114487+david-allison@users.noreply.github.com>
2025-06-23 09:51:43 +0100

    refactor(deck-picker): use `deck.id`
    (cherry picked from commit 80f850d432ec25c079a1822ac65d994b2e6d4bf9)

--- AnkiDroid/src/main/java/com/ichi2/anki/DeckPicker.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/DeckPicker.kt
@@ -1050,7 +1050,7 @@ open class DeckPicker :
                     override fun onQueryTextChange(newText: String): Boolean {
                         val adapter = recyclerView.adapter as DeckAdapter
                         launchCatchingTask {
-                            val selectedDeckId = withCol { decks.current().getLong("id") }
+                            val selectedDeckId = withCol { decks.current().id }
                             dueTree?.let {
                                 adapter.submit(
                                     data = it.filterAndFlattenDisplay(newText),
@@ -2341,7 +2341,7 @@ open class DeckPicker :
         deckListAdapter.submit(
             data = tree.filterAndFlattenDisplay(currentFilter),
             hasSubDecks = tree.children.any { it.children.any() },
-            currentDeckId = withCol { decks.current().getLong("id") },
+            currentDeckId = withCol { decks.current().id },
         )
 
         // Set the "x due" subtitle
@@ -2355,7 +2355,7 @@ open class DeckPicker :
         }.onFailure {
             Timber.w(it, "Failed to set the due count as the subtitle in the toolbar")
         }
-        val current = withCol { decks.current().optLong("id") }
+        val current = withCol { decks.current().id }
         if (viewModel.focusedDeck != current) {
             scrollDecklistToDeck(current)
             viewModel.focusedDeck = current
