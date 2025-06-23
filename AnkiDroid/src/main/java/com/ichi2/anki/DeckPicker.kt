ankidroid/Anki-Android

commit 5f1bd436e53a5821b3039d84ed77156d9dc0bb5f

Remove suspended code from DeckAdapter
The suspended code was removed and a similar implementation was done
in DeckPicker which will now do the necessary work and then will pass the
calculated data to the adapter.

The adapter was changed to extend ListAdapter which uses diff-ing to
observe changes so we get free animations. The filtering was removed
from the adapter, DeckPicker computes the data as it has all info needed,
the query and the deck tree. The adapter becomes dumb and just reacts to
the changes. A bit wasteful to start a coroutine for each change in the
query text, but I wanted to keep the change count small.

Note: the methods findDeckPosition() and getNodeByDid() were moved in
DeckPicker, they are not needed by the adapter and the DeckPicker already
has a reference to the deck tree that it can use in the methods.

AnkiDroid/src/main/java/com/ichi2/anki/DeckPicker.kt
@@ -43,7 +43,6 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewPropertyAnimator
-import android.widget.Filterable
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
@@ -981,21 +980,17 @@ open class DeckPicker :
                     }
 
                     override fun onQueryTextChange(newText: String): Boolean {
-                        val adapter = recyclerView.adapter as? Filterable
-                        if (adapter == null || adapter.filter == null) {
-                            Timber.w(
-                                "DeckPicker.onQueryTextChange: adapter is null: %s, filter is null: %s, adapter type: %s",
-                                adapter == null,
-                                adapter?.filter == null,
-                                adapter?.javaClass?.simpleName ?: "Unknown",
-                            )
-                            CrashReportService.sendExceptionReport(
-                                Exception("DeckPicker.onQueryTextChanged with unexpected null adapter or filter. Carefully examine logcat"),
-                                "DeckPicker",
-                            )
-                            return true
+                        val adapter = recyclerView.adapter as DeckAdapter
+                        launchCatchingTask {
+                            val selectedDeckId = withCol { decks.current().getLong("id") }
+                            dueTree?.let {
+                                adapter.submit(
+                                    data = it.filterAndFlatten(newText),
+                                    hasSubDecks = it.children.any { deckNode -> deckNode.children.any() },
+                                    currentDeckId = selectedDeckId,
+                                )
+                            }
                         }
-                        adapter.filter.filter(newText)
                         return true
                     }
                 },
@@ -2136,12 +2131,12 @@ open class DeckPicker :
             promptUserToUpdateScheduler()
             return
         }
-
         withCol { decks.select(did) }
+        deckListAdapter.updateSelectedDeck(did)
         // Also forget the last deck used by the Browser
         CardBrowser.clearLastDeckId()
         viewModel.focusedDeck = did
-        val deck = deckListAdapter.getNodeByDid(did)
+        val deck = getNodeByDid(did)
         if (deck.hasCardsReadyToStudy()) {
             openReviewerOrStudyOptions(selectionType)
             return
@@ -2169,8 +2164,28 @@ open class DeckPicker :
      * @param did The deck ID of the deck to select.
      */
     private fun scrollDecklistToDeck(did: DeckId) {
-        val position = deckListAdapter.findDeckPosition(did)
+        val position = findDeckPosition(did)
         recyclerViewLayoutManager.scrollToPositionWithOffset(position, recyclerView.height / 2)
+    }
+
+    /**
+     * Return the position of the deck in the deck list. If the deck is a child of a collapsed deck
+     * (i.e., not visible in the deck list), then the position of the parent deck is returned instead.
+     *
+     * An invalid deck ID will return position 0.
+     */
+    private fun findDeckPosition(did: DeckId): Int {
+        deckListAdapter.currentList.forEachIndexed { index, treeNode ->
+            if (treeNode.did == did) {
+                return index
+            }
+        }
+
+        // If the deck is not in our list, we search again using the immediate parent
+        // If the deck is not found, return 0
+        val collapsedDeck = dueTree?.find(did) ?: return 0
+        val parent = collapsedDeck.parent?.get() ?: return 0
+        return findDeckPosition(parent.did)
     }
 
     /**
@@ -2282,20 +2297,26 @@ open class DeckPicker :
                 }
             }
         }
-        val currentFilter = if (toolbarSearchView != null) toolbarSearchView!!.query else null
+        val currentFilter = toolbarSearchView?.query
 
         if (isEmpty) {
-            if (supportActionBar != null) {
-                supportActionBar!!.subtitle = null
-            }
+            supportActionBar?.subtitle = null
             if (toolbarSearchView != null) {
-                deckListAdapter.filter?.filter(currentFilter)
+                deckListAdapter.submit(
+                    data = emptyList(),
+                    hasSubDecks = false,
+                    currentDeckId = -1,
+                )
             }
             Timber.d("Not rendering deck list as there are no cards")
             // We're done here
             return
         }
-        deckListAdapter.buildDeckList(tree, currentFilter)
+        deckListAdapter.submit(
+            data = tree.filterAndFlatten(currentFilter),
+            hasSubDecks = tree.children.any { it.children.any() },
+            currentDeckId = withCol { decks.current().getLong("id") },
+        )
 
         // Set the "x due" subtitle
         runCatchingKotlin {
@@ -2314,6 +2335,11 @@ open class DeckPicker :
             viewModel.focusedDeck = current
         }
     }
+
+    /**
+     * Get the [DeckNode] identified by [did] from [DeckAdapter].
+     */
+    private fun DeckPicker.getNodeByDid(did: DeckId): DeckNode = deckListAdapter.currentList[findDeckPosition(did)]
 
     // Callback to show study options for currently selected deck
     fun showContextMenuDeckOptions(did: DeckId) {
@@ -2654,7 +2680,10 @@ open class DeckPicker :
             withCol { sched.hasCardsTodayAfterStudyAheadLimit() } -> CompletedDeckStatus.LEARN_AHEAD_LIMIT_REACHED
             withCol { sched.newDue() || sched.revDue() } -> CompletedDeckStatus.LEARN_AHEAD_LIMIT_REACHED
             withCol { decks.isFiltered(did) } -> CompletedDeckStatus.DYNAMIC_DECK_NO_LIMITS_REACHED
-            deckListAdapter.getNodeByDid(did).children.isEmpty() && withCol { decks.isEmpty(did) } -> CompletedDeckStatus.EMPTY_REGULAR_DECK
+            getNodeByDid(did).children.isEmpty() &&
+                withCol {
+                    decks.isEmpty(did)
+                } -> CompletedDeckStatus.EMPTY_REGULAR_DECK
             else -> CompletedDeckStatus.REGULAR_DECK_NO_MORE_CARDS_TODAY
         }
 

AnkiDroid/src/main/java/com/ichi2/anki/widgets/DeckAdapter.kt
@@ -21,27 +21,21 @@ import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Filter
-import android.widget.Filterable
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.TextView
-import androidx.annotation.CheckResult
-import androidx.annotation.VisibleForTesting
 import androidx.core.content.res.getDrawableOrThrow
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.ichi2.anki.CollectionManager.withCol
 import com.ichi2.anki.OnContextAndLongClickListener.Companion.setOnContextAndLongClickListener
 import com.ichi2.anki.R
 import com.ichi2.anki.utils.ext.findViewById
 import com.ichi2.libanki.DeckId
 import com.ichi2.libanki.sched.DeckNode
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import net.ankiweb.rsdroid.RustCleanup
-import timber.log.Timber
 
 /**
  * A [RecyclerView.Adapter] used to show the list of decks inside [com.ichi2.anki.DeckPicker].
@@ -55,7 +49,6 @@ import timber.log.Timber
  * @param onDeckContextRequested callback triggered when the user requested to see extra actions for
  * a deck. This consists in a context menu brought in by either a long touch or a right click.
  */
-@RustCleanup("Lots of bad code: should not be using suspend functions inside an adapter")
 @RustCleanup("Differs from legacy backend: Create deck 'One', create deck 'One::two'. 'One::two' was not expanded")
 class DeckAdapter(
     context: Context,
@@ -64,13 +57,8 @@ class DeckAdapter(
     private val onDeckCountsSelected: (DeckId) -> Unit,
     private val onDeckChildrenToggled: (DeckId) -> Unit,
     private val onDeckContextRequested: (DeckId) -> Unit,
-) : RecyclerView.Adapter<DeckAdapter.ViewHolder>(),
-    Filterable {
+) : ListAdapter<DeckNode, DeckAdapter.ViewHolder>(deckNodeDiffCallback) {
     private val layoutInflater = LayoutInflater.from(context)
-    private var deckTree: DeckNode? = null
-
-    /** The non-collapsed subset of the deck tree that matches the current search. */
-    private var filteredDeckList: List<DeckNode> = ArrayList()
     private val zeroCountColor: Int
     private val newCountColor: Int
     private val learnCountColor: Int
@@ -103,32 +91,44 @@ class DeckAdapter(
         val deckRev: TextView = findViewById(R.id.deckpicker_rev)
     }
 
-    private val mutex = Mutex()
-
     /**
-     * Consume a list of [DeckNode]s to render a new deck list.
-     * @param filter The string to filter the deck by
+     * Set new data in the adapter. This should be used instead of [submitList] (which is called
+     * by this method) so there's no need to call [notifyDataSetChanged] on the adapter.
      */
-    suspend fun buildDeckList(
-        node: DeckNode,
-        filter: CharSequence?,
+    fun submit(
+        data: List<DeckNode>,
+        hasSubDecks: Boolean,
+        currentDeckId: DeckId,
     ) {
-        Timber.d("buildDeckList")
-        // TODO: This is a lazy hack to fix a bug. We hold the lock for far too long
-        // and do I/O inside it. Better to calculate the new lists outside the lock, then swap
-        mutex.withLock {
-            deckTree = node
-            hasSubdecks = node.children.any { it.children.any() }
-            currentDeckId = withCol { decks.current().optLong("id") }
-            // Filtering performs notifyDataSetChanged after the async work is complete
-            getFilter()?.filter(filter)
+        // submitList is smart to not trigger a refresh if the new list is the same, but we do need
+        // an adapter refresh if the other two properties have changed even if the new data is the
+        // same as they modify some of the adapter's content appearance
+        val forceRefresh =
+            areDataSetsEqual(currentList, data) &&
+                (this.hasSubdecks != hasSubDecks || this.currentDeckId != currentDeckId)
+        this.hasSubdecks = hasSubDecks
+        this.currentDeckId = currentDeckId
+        submitList(data)
+        if (forceRefresh) notifyDataSetChanged()
+    }
+
+    private fun areDataSetsEqual(
+        currentSet: List<DeckNode>,
+        newSet: List<DeckNode>,
+    ): Boolean {
+        if (currentSet.size != newSet.size) return false
+        return currentSet.zip(newSet).all { (fst, snd) ->
+            fst.fullDeckName == snd.fullDeckName
         }
     }
 
-    @CheckResult
-    fun getNodeByDid(did: DeckId): DeckNode {
-        val pos = findDeckPosition(did)
-        return deckList[pos]
+    /**
+     * Update the current selected deck so the adapter shows the proper backgrounds.
+     * Calls [notifyDataSetChanged].
+     */
+    fun updateSelectedDeck(deckId: DeckId) {
+        this.currentDeckId = deckId
+        notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(
@@ -140,8 +140,7 @@ class DeckAdapter(
         holder: ViewHolder,
         position: Int,
     ) {
-        // Update views for this node
-        val node = filteredDeckList[position]
+        val node = getItem(position)
         // Set the expander icon and padding according to whether or not there are any subdecks
         val deckLayout = holder.deckLayout
         if (hasSubdecks) {
@@ -191,8 +190,6 @@ class DeckAdapter(
         holder.countsLayout.setOnClickListener { onDeckCountsSelected(node.did) }
     }
 
-    override fun getItemCount(): Int = filteredDeckList.size
-
     private fun setDeckExpander(
         expander: ImageButton,
         indent: ImageButton,
@@ -214,54 +211,6 @@ class DeckAdapter(
         }
         // Add some indenting for each nested level
         indent.minimumWidth = nestedIndent * node.depth
-    }
-
-    /**
-     * Return the position of the deck in the deck list. If the deck is a child of a collapsed deck
-     * (i.e., not visible in the deck list), then the position of the parent deck is returned instead.
-     *
-     * An invalid deck ID will return position 0.
-     */
-    fun findDeckPosition(did: DeckId): Int {
-        filteredDeckList.forEachIndexed { index, treeNode ->
-            if (treeNode.did == did) {
-                return index
-            }
-        }
-
-        // If the deck is not in our list, we search again using the immediate parent
-        // If the deck is not found, return 0
-        val collapsedDeck = deckTree?.find(did) ?: return 0
-        val parent = collapsedDeck.parent?.get() ?: return 0
-        return findDeckPosition(parent.did)
-    }
-
-    private val deckList: List<DeckNode>
-        get() = filteredDeckList
-
-    override fun getFilter(): Filter? = deckTree?.let { DeckFilter(it) }
-
-    @VisibleForTesting
-    inner class DeckFilter(
-        private val top: DeckNode,
-    ) : Filter() {
-        override fun performFiltering(constraint: CharSequence?): FilterResults {
-            val out = top.filterAndFlatten(constraint)
-            Timber.i("deck filter: %d (%s)", out.size, constraint)
-            return FilterResults().also {
-                it.values = out
-                it.count = out.size
-            }
-        }
-
-        override fun publishResults(
-            constraint: CharSequence?,
-            results: FilterResults,
-        ) {
-            @Suppress("unchecked_cast")
-            filteredDeckList = results.values as List<DeckNode>
-            notifyDataSetChanged()
-        }
     }
 
     companion object {
@@ -301,3 +250,22 @@ class DeckAdapter(
         ta.recycle()
     }
 }
+
+private val deckNodeDiffCallback =
+    object : DiffUtil.ItemCallback<DeckNode>() {
+        override fun areItemsTheSame(
+            oldItem: DeckNode,
+            newItem: DeckNode,
+        ): Boolean = oldItem.did == newItem.did
+
+        override fun areContentsTheSame(
+            oldItem: DeckNode,
+            newItem: DeckNode,
+        ): Boolean =
+            oldItem.did == newItem.did &&
+                oldItem.filtered == newItem.filtered &&
+                oldItem.fullDeckName == newItem.fullDeckName &&
+                oldItem.newCount == newItem.newCount &&
+                oldItem.lrnCount == newItem.lrnCount &&
+                oldItem.revCount == newItem.revCount
+    }

AnkiDroid/src/test/java/com/ichi2/anki/DeckPickerTest.kt
@@ -505,6 +505,7 @@ class DeckPickerTest : RobolectricTest() {
                 val card = addBasicNote("front", "back").firstCard()
                 getColUnsafe.sched.buryCards(listOf(card.id))
                 updateDeckList()
+                advanceRobolectricLooper()
                 assertEquals(1, visibleDeckCount)
                 assertTrue(getColUnsafe.sched.haveBuried(), "Deck should have buried cards")
                 supportFragmentManager.selectContextMenuOption(DeckPickerContextMenuOption.UNBURY, deckId)

commit 19e1df65aefe0fb25a3a6c03d57005aff833d218

fix(deck-picker): multiple decks appeared selected
`DeckAdapter::submit` assumed that `submitList` would refresh the items
But this was not the case if a new filtered deck was added to the list

As this only added an item, but the structural identity of the previously
selected deck was unchanged

To fix this:

Define `isSelected` in `DisplayDeckNode`, and trust `submitList` to do the
right thing.

Broken in 5f1bd436e53a5821b3039d84ed77156d9dc0bb5f

Fixes 18599

AnkiDroid/src/main/java/com/ichi2/anki/DeckPicker.kt
@@ -1058,9 +1058,8 @@ open class DeckPicker :
                             val selectedDeckId = withCol { decks.current().id }
                             dueTree?.let {
                                 adapter.submit(
-                                    data = it.filterAndFlattenDisplay(newText),
+                                    data = it.filterAndFlattenDisplay(newText, selectedDeckId),
                                     hasSubDecks = it.children.any { deckNode -> deckNode.children.any() },
-                                    currentDeckId = selectedDeckId,
                                 )
                             }
                         }
@@ -2336,17 +2335,16 @@ open class DeckPicker :
                 deckListAdapter.submit(
                     data = emptyList(),
                     hasSubDecks = false,
-                    currentDeckId = -1,
                 )
             }
             Timber.d("Not rendering deck list as there are no cards")
             // We're done here
             return
         }
+        val currentDeckId = withCol { decks.current().getLong("id") }
         deckListAdapter.submit(
-            data = tree.filterAndFlattenDisplay(currentFilter),
+            data = tree.filterAndFlattenDisplay(currentFilter, currentDeckId),
             hasSubDecks = tree.children.any { it.children.any() },
-            currentDeckId = withCol { decks.current().id },
         )
 
         // Set the "x due" subtitle

AnkiDroid/src/main/java/com/ichi2/anki/deckpicker/DisplayDeckNode.kt
@@ -41,6 +41,7 @@ data class DisplayDeckNode(
     val newCount: Int,
     val lrnCount: Int,
     val revCount: Int,
+    val isSelected: Boolean,
 ) {
     // DeckNode is mutable, so use a lateinit var so '==' doesn't include it in the comparison
     lateinit var deckNode: DeckNode
@@ -49,6 +50,7 @@ data class DisplayDeckNode(
         fun from(
             node: DeckNode,
             matchesSearchOrChild: Boolean,
+            selectedDeckId: DeckId,
         ): DisplayDeckNode =
             DisplayDeckNode(
                 did = node.did,
@@ -61,6 +63,7 @@ data class DisplayDeckNode(
                 newCount = node.newCount,
                 lrnCount = node.lrnCount,
                 revCount = node.revCount,
+                isSelected = node.did == selectedDeckId,
             ).apply {
                 this.deckNode = node
             }
@@ -69,7 +72,10 @@ data class DisplayDeckNode(
 
 /** Convert the tree into a flat list of [DisplayDeckNode]s, where matching decks and the children/parents
  * are included. Decks inside collapsed decks are not considered. */
-fun DeckNode.filterAndFlattenDisplay(filter: CharSequence?): List<DisplayDeckNode> {
+fun DeckNode.filterAndFlattenDisplay(
+    filter: CharSequence?,
+    selectedDeckId: DeckId,
+): List<DisplayDeckNode> {
     val filterPattern =
         if (filter.isNullOrBlank()) {
             null
@@ -77,7 +83,7 @@ fun DeckNode.filterAndFlattenDisplay(filter: CharSequence?): List<DisplayDeckNod
             filter.toString().lowercase(Locale.getDefault()).trim { it <= ' ' }
         }
     val list = mutableListOf<DisplayDeckNode>()
-    filterAndFlattenDisplayInner(filterPattern, list, false)
+    filterAndFlattenDisplayInner(filterPattern, list, parentMatched = false, selectedDeckId)
     return list
 }
 
@@ -85,9 +91,10 @@ private fun DeckNode.filterAndFlattenDisplayInner(
     filter: CharSequence?,
     list: MutableList<DisplayDeckNode>,
     parentMatched: Boolean,
+    selectedDeckId: DeckId,
 ) {
     if (!isSyntheticDeck && (nameMatchesFilter((filter)) || parentMatched)) {
-        addVisibleToList(list, true)
+        this.addVisibleToList(list, matchesSearchOrChild = true, selectedDeckId)
         return
     }
 
@@ -98,11 +105,17 @@ private fun DeckNode.filterAndFlattenDisplayInner(
     }
 
     if (!isSyntheticDeck) {
-        list.append(DisplayDeckNode.from(this, false))
+        list.append(
+            DisplayDeckNode.from(
+                this,
+                matchesSearchOrChild = false,
+                selectedDeckId = selectedDeckId,
+            ),
+        )
     }
     val startingLen = list.size
     for (child in children) {
-        child.filterAndFlattenDisplayInner(filter, list, false)
+        child.filterAndFlattenDisplayInner(filter, list, parentMatched = false, selectedDeckId)
     }
     if (!isSyntheticDeck && startingLen == list.size) {
         // we don't include ourselves if no children matched
@@ -113,11 +126,12 @@ private fun DeckNode.filterAndFlattenDisplayInner(
 private fun DeckNode.addVisibleToList(
     list: MutableList<DisplayDeckNode>,
     matchesSearchOrChild: Boolean,
+    selectedDeckId: DeckId,
 ) {
-    list.append(DisplayDeckNode.from(this, matchesSearchOrChild))
+    list.append(DisplayDeckNode.from(this, matchesSearchOrChild, selectedDeckId))
     if (!collapsed) {
         for (child in children) {
-            child.addVisibleToList(list, matchesSearchOrChild)
+            child.addVisibleToList(list, matchesSearchOrChild, selectedDeckId)
         }
     }
 }

AnkiDroid/src/main/java/com/ichi2/anki/widgets/DeckAdapter.kt
@@ -77,7 +77,6 @@ class DeckAdapter(
     private val startPadding: Int = context.resources.getDimension(R.dimen.deck_picker_left_padding).toInt()
     private val startPaddingSmall: Int = context.resources.getDimension(R.dimen.deck_picker_left_padding_small).toInt()
     private val nestedIndent = context.resources.getDimension(R.dimen.keyline_1).toInt()
-    private var currentDeckId: DeckId = 0
 
     // Flags
     private var hasSubdecks = false
@@ -102,16 +101,14 @@ class DeckAdapter(
     fun submit(
         data: List<DisplayDeckNode>,
         hasSubDecks: Boolean,
-        currentDeckId: DeckId,
     ) {
         // submitList is smart to not trigger a refresh if the new list is the same, but we do need
         // an adapter refresh if the other two properties have changed even if the new data is the
         // same as they modify some of the adapter's content appearance
         val forceRefresh =
             areDataSetsEqual(currentList, data) &&
-                (this.hasSubdecks != hasSubDecks || this.currentDeckId != currentDeckId)
+                (this.hasSubdecks != hasSubDecks)
         this.hasSubdecks = hasSubDecks
-        this.currentDeckId = currentDeckId
         submitList(data)
         if (forceRefresh) notifyDataSetChanged()
     }
@@ -131,8 +128,11 @@ class DeckAdapter(
      * Calls [notifyDataSetChanged].
      */
     fun updateSelectedDeck(deckId: DeckId) {
-        this.currentDeckId = deckId
-        notifyDataSetChanged()
+        submitList(
+            this.currentList.map {
+                it.copy(isSelected = it.did == deckId)
+            },
+        )
     }
 
     override fun onCreateViewHolder(
@@ -167,7 +167,7 @@ class DeckAdapter(
         }
         holder.deckLayout.setBackgroundResource(rowCurrentDrawable)
         // set a different background color for the current selected deck
-        if (node.did == currentDeckId) {
+        if (node.isSelected) {
             holder.deckLayout.setBackgroundResource(rowCurrentDrawable)
             if (activityHasBackground) {
                 val background = holder.deckLayout.background.mutate()

AnkiDroid/src/test/java/com/ichi2/libanki/DeckNodeTest.kt
@@ -110,4 +110,4 @@ class DeckNodeTest {
     }
 }
 
-fun DeckNode.filterAndFlatten(filter: CharSequence?) = this.filterAndFlattenDisplay(filter).map { it.deckNode }
+fun DeckNode.filterAndFlatten(filter: CharSequence?) = this.filterAndFlattenDisplay(filter, selectedDeckId = 1337).map { it.deckNode }