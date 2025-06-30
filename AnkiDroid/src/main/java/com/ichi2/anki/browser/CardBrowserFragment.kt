36495bea79ac8b941d0e86d85e7043a01893f4eb
David Allison <62114487+david-allison@users.noreply.github.com>
2025-06-30 20:15:17 +0100

    refactor(card-browser): inline 'search*' methods
    * searchForMarkedNotes
    * searchForSuspendedCards

--- AnkiDroid/src/main/java/com/ichi2/anki/browser/CardBrowserFragment.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/browser/CardBrowserFragment.kt
@@ -224,11 +224,11 @@ class CardBrowserFragment :
                             return true
                         }
                         R.id.action_show_marked -> {
-                            searchForMarkedNotes()
+                            activityViewModel.searchForMarkedNotes()
                             return true
                         }
                         R.id.action_show_suspended -> {
-                            searchForSuspendedCards()
+                            activityViewModel.searchForSuspendedCards()
                             return true
                         }
                         R.id.action_search_by_tag -> {
@@ -520,7 +520,7 @@ class CardBrowserFragment :
                     // Ctrl+Alt+S / Ctrl+S in the activity take priority
                 } else if (!event.isCtrlPressed && event.isAltPressed) {
                     Timber.i("Alt+S: Show suspended cards")
-                    searchForSuspendedCards()
+                    activityViewModel.searchForSuspendedCards()
                     return true
                 }
             }
@@ -545,7 +545,7 @@ class CardBrowserFragment :
             KeyEvent.KEYCODE_M -> {
                 if (event.isCtrlPressed) {
                     Timber.i("Ctrl+M: Search marked notes")
-                    searchForMarkedNotes()
+                    activityViewModel.searchForMarkedNotes()
                     return true
                 }
             }
@@ -741,20 +741,6 @@ class CardBrowserFragment :
                 activityViewModel.changeCardOrder(SortType.fromCardBrowserLabelIndex(which))
             },
         )
-    }
-
-    /**
-     * @see CardBrowserViewModel.searchForSuspendedCards
-     */
-    fun searchForSuspendedCards() {
-        launchCatchingTask { activityViewModel.searchForSuspendedCards() }
-    }
-
-    /**
-     * @see CardBrowserViewModel.searchForMarkedNotes
-     */
-    fun searchForMarkedNotes() {
-        launchCatchingTask { activityViewModel.searchForMarkedNotes() }
     }
 
     fun updateFlagForSelectedRows(flag: Flag) =
