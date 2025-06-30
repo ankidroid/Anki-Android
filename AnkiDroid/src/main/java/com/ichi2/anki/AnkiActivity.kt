2abdadc804cab512297a66f4e6322b1a9e8283ed
David Allison <62114487+david-allison@users.noreply.github.com>
2025-06-30 07:01:17 +0100

    logs: diagnostics for collectionLoadError
    Issue 18716

--- AnkiDroid/src/main/java/com/ichi2/anki/AnkiActivity.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/AnkiActivity.kt
@@ -432,6 +432,7 @@ open class AnkiActivity :
 
     /** The action to take when there was an error loading the collection  */
     fun onCollectionLoadError() {
+        Timber.w("onCollectionLoadError")
         val deckPicker = Intent(this, DeckPicker::class.java)
         deckPicker.putExtra("collectionLoadError", true) // don't currently do anything with this
         deckPicker.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
