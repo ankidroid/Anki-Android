861b1acba2f5c1caa5c66834a41a76824a8deb89
David Allison <62114487+david-allison@users.noreply.github.com>
2025-07-07 19:39:07 +0100

    build(architecture): introduce `:libanki:testutils`
    This is for test extensions to `libanki` (depending on `col`)
    which are used in both `libanki` and `AnkiDroid` tests
    
    These have an `anki` dependency, but no strong `android` dependencies
    
    Primarily: `TestClass`, but also a number of methods which
     have neither an Android nor an AnkiDroid dependency
    
    These can't exist in `libanki` tests, as you can't depend on test modules
    These shouldn't exist in `libanki`, as they should only be visible to
    test modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,4 +17,4 @@ dependencyResolutionManagement {
     }
 }
 
-include(":lint-rules", ":api", ":AnkiDroid", ":testlib", ":common", ":libanki")
+include(":lint-rules", ":api", ":AnkiDroid", ":testlib", ":common", ":libanki", ":libanki:testutils")
