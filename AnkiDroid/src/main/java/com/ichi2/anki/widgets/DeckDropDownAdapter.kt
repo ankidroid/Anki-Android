ec15320d88a40439100e72ce97268212e6302351
Arthur Milchior <arthur@milchior.fr>
2025-07-14 02:06:42 +0200

    NF: Remove nullability
    Both values where always used with !! so where not null

--- AnkiDroid/src/main/java/com/ichi2/anki/widgets/DeckDropDownAdapter.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/widgets/DeckDropDownAdapter.kt
@@ -37,10 +37,10 @@ class DeckDropDownAdapter(
         val subtitleText: String?
     }
 
-    internal class DeckDropDownViewHolder {
-        var deckNameView: TextView? = null
-        var deckCountsView: TextView? = null
-    }
+    internal data class DeckDropDownViewHolder(
+        val deckNameView: TextView,
+        val deckCountsView: TextView,
+    )
 
     fun addDeck(deck: DeckNameId) {
         deckList.add(deck)
@@ -65,15 +65,13 @@ class DeckDropDownAdapter(
     ): View? {
         var convertView = view
         val viewHolder: DeckDropDownViewHolder
-        val deckNameView: TextView?
-        val deckCountsView: TextView?
+        val deckNameView: TextView
+        val deckCountsView: TextView
         if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.dropdown_deck_selected_item, parent, false)
             deckNameView = convertView.findViewById(R.id.dropdown_deck_name)
             deckCountsView = convertView.findViewById(R.id.dropdown_deck_counts)
-            viewHolder = DeckDropDownViewHolder()
-            viewHolder.deckNameView = deckNameView
-            viewHolder.deckCountsView = deckCountsView
+            viewHolder = DeckDropDownViewHolder(deckNameView, deckCountsView)
             convertView.tag = viewHolder
         } else {
             viewHolder = convertView.tag as DeckDropDownViewHolder
@@ -81,13 +79,13 @@ class DeckDropDownAdapter(
             deckCountsView = viewHolder.deckCountsView
         }
         if (position == 0) {
-            deckNameView!!.text = context.resources.getString(R.string.card_browser_all_decks)
+            deckNameView.text = context.resources.getString(R.string.card_browser_all_decks)
         } else {
             val deck = deckList[position - 1]
             val deckName = deck.name
-            deckNameView!!.text = deckName
+            deckNameView.text = deckName
         }
-        deckCountsView!!.text = (context as? SubtitleListener)?.subtitleText ?: ""
+        deckCountsView.text = (context as? SubtitleListener)?.subtitleText ?: ""
         return convertView
     }
 
