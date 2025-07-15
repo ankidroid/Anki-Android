a39825d19c4358182e972b5229c97547ef29aca5
David Allison <62114487+david-allison@users.noreply.github.com>
2025-07-15 09:33:16 +0100

    refactor(deck-picker): remove 'onDecksLoaded' (now in ViewModel)

--- AnkiDroid/src/main/java/com/ichi2/anki/DeckPicker.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/DeckPicker.kt
@@ -113,7 +113,6 @@ import com.ichi2.anki.deckpicker.BackgroundImage
 import com.ichi2.anki.deckpicker.DeckDeletionResult
 import com.ichi2.anki.deckpicker.DeckPickerViewModel
 import com.ichi2.anki.deckpicker.DeckPickerViewModel.FlattenedDeckList
-import com.ichi2.anki.deckpicker.DeckPickerViewModel.OnDecksLoadedResult
 import com.ichi2.anki.deckpicker.EmptyCardsResult
 import com.ichi2.anki.deckpicker.filterAndFlattenDisplay
 import com.ichi2.anki.dialogs.AsyncDialogFragment
@@ -728,10 +727,6 @@ open class DeckPicker :
             }
         }
 
-        fun onDecksLoadedChanged(result: OnDecksLoadedResult) {
-            onDecksLoaded(result.deckDueTree, result.collectionHasNoCards)
-        }
-
         fun onStudiedTodayChanged(studiedToday: String) {
             reviewSummaryTextView.text = studiedToday
             val fabLinearLayout = findViewById<LinearLayout>(R.id.fabLinearLayout)
@@ -814,7 +809,11 @@ open class DeckPicker :
         }
 
         fun onFocusedDeckChanged(deckId: DeckId?) {
-            scrollDecklistToDeck(deckId)
+            val position = deckId?.let { findDeckPosition(it) } ?: 0
+            // HACK: a small delay is required before scrolling works
+            recyclerView.postDelayed({
+                recyclerViewLayoutManager.scrollToPositionWithOffset(position, recyclerView.height / 2)
+            }, 10)
         }
 
         fun onError(errorMessage: String) {
@@ -832,7 +831,6 @@ open class DeckPicker :
         viewModel.onError.launchCollectionInLifecycleScope(::onError)
         viewModel.flowOfPromptUserToUpdateScheduler.launchCollectionInLifecycleScope(::onPromptUserToUpdateScheduler)
         viewModel.flowOfUndoUpdated.launchCollectionInLifecycleScope(::onUndoUpdated)
-        viewModel.flowOfOnDecksLoaded.launchCollectionInLifecycleScope(::onDecksLoadedChanged)
         viewModel.flowOfStudiedTodayStats.launchCollectionInLifecycleScope(::onStudiedTodayChanged)
         viewModel.flowOfDeckListInInitialState.filterNotNull().launchCollectionInLifecycleScope(::onCollectionStatusChanged)
         viewModel.flowOfCardsDue.launchCollectionInLifecycleScope(::onCardsDueChanged)
@@ -2269,16 +2267,6 @@ open class DeckPicker :
     }
 
     /**
-     * Scroll the deck list so that it is centered on the current deck.
-     *
-     * @param did The deck ID of the deck to select.
-     */
-    private fun scrollDecklistToDeck(did: DeckId?) {
-        val position = did?.let { findDeckPosition(it) } ?: 0
-        recyclerViewLayoutManager.scrollToPositionWithOffset(position, recyclerView.height / 2)
-    }
-
-    /**
      * Return the position of the deck in the deck list. If the deck is a child of a collapsed deck
      * (i.e., not visible in the deck list), then the position of the parent deck is returned instead.
      *
@@ -2317,17 +2305,10 @@ open class DeckPicker :
             performBackupInBackground()
         }
         Timber.d("updateDeckList")
-        return launchCatchingTask { withProgress { viewModel.reloadDeckCounts()?.join() } }
-    }
-
-    private fun onDecksLoaded(
-        result: DeckNode,
-        collectionHasNoCards: Boolean,
-    ) {
-        Timber.i("Updating deck list UI")
-        hideProgressBar()
-        launchCatchingTask { renderPage(collectionHasNoCards) }
-        Timber.d("Startup - Deck List UI Completed")
+        return launchCatchingTask {
+            withProgress { viewModel.reloadDeckCounts()?.join() }
+            hideProgressBar()
+        }
     }
 
     private suspend fun renderPage(collectionIsEmpty: Boolean) {
