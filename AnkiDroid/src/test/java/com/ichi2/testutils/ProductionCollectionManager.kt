c2c4596112a4d220ff05f917d941dc5eee1ee1a9
David Allison <62114487+david-allison@users.noreply.github.com>
2025-07-21 06:57:28 +0100

    refactor: rename CollectionManagerAdapter
    The intent of `ProductionCollectionManager` is more obvious

--- AnkiDroid/src/test/java/com/ichi2/testutils/ProductionCollectionManager.kt
+++ AnkiDroid/src/test/java/com/ichi2/testutils/ProductionCollectionManager.kt
@@ -23,7 +23,7 @@ import kotlinx.coroutines.CoroutineDispatcher
 /**
  * Adapts [CollectionManager] to [TestCollectionManager]
  */
-object CollectionManagerTestAdapter : TestCollectionManager {
+object ProductionCollectionManager : TestCollectionManager {
     override fun getColUnsafe() = CollectionManager.getColUnsafe()
 
     override suspend fun discardBackend() {
