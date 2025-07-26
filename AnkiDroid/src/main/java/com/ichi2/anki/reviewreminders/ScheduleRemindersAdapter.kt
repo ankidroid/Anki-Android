517c2020017fbfc73de2e5190059d50b9ff94b61
Song Eric Yu Li <ericli3690@gmail.com>
2025-07-26 14:39:28 -0400

    feat(reminders): review reminders RecyclerView
    GSoC 2025: Review Reminders
    
    - Added RecyclerView to ScheduleReminders fragment.
    - Created ScheduleRemindersAdapter, which holds the state of each individual row in the RecyclerView. The adapter has functionality in the form of functions passed to it via its constructor to keep most of the key functionality centralized within ScheduleReminders and to avoid injecting a Context into the adapter, which should optimally be context-free. I tried to keep the adapter as short as possible; it only governs the UI of each individual reminder in the RecyclerView.
    - Added `setDeckNameFromScopeView` method, which retrieves deck names from the collection for displaying in the UI. Deck names are cached to prevent repeated calls to the collection.
    - Added empty placeholder methods for adding reminders and editing reminders that will be filled in by a future PR.
    - Added logic for toggling whether reminders are enabled and saving that state.
    - Added `onDeckSelected`, a method that will allow the code to detect if the AddEditReminderDialog (coming soon) has had the deck selected in the deck selection dialog changed. We must catch this in ScheduleReminders because this onChange event has to be caught by a Fragment, whereas the AddEditReminderDialog is just a dialog that gets briefly removed when the deck selection dialog appears. The data is forwarded to the AddEditReminderDialog via a FragmentResult, a very simple way of passing information between fragments.
    - Added constants that will be used for pushing information to and from the AddEditReminderDialog to the ScheduleReminders companion object.
    - Fixed a small Timber call to use format arguments rather than interpolation.

---
+++ AnkiDroid/src/main/java/com/ichi2/anki/reviewreminders/ScheduleRemindersAdapter.kt
@@ -0,0 +1,72 @@
+/*
+ *  Copyright (c) 2025 Eric Li <ericli3690@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ichi2.anki.reviewreminders
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.materialswitch.MaterialSwitch
+import com.ichi2.anki.R
+
+class ScheduleRemindersAdapter(
+    val reminders: MutableList<ReviewReminder>,
+    private val setDeckNameFromScopeForView: (ReviewReminderScope, TextView) -> Unit,
+    private val toggleReminderEnabled: (ReviewReminderId, ReviewReminderScope, Int) -> Unit,
+    private val editReminder: (ReviewReminder) -> Unit,
+) : RecyclerView.Adapter<ScheduleRemindersAdapter.ViewHolder>() {
+    inner class ViewHolder(
+        holder: View,
+    ) : RecyclerView.ViewHolder(holder) {
+        var reminder: ReviewReminder? = null
+        val deckTextView: TextView = holder.findViewById(R.id.reminders_list_deck_text)
+        val timeTextView: TextView = holder.findViewById(R.id.reminders_list_time_text)
+        val switchView: MaterialSwitch = holder.findViewById(R.id.reminders_list_switch)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): ViewHolder {
+        val view =
+            LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.schedule_reminders_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    @SuppressLint("DefaultLocale")
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int,
+    ) {
+        val reminder = reminders[position]
+        holder.reminder = reminder
+
+        setDeckNameFromScopeForView(reminder.scope, holder.deckTextView)
+        holder.timeTextView.text = reminder.time.toString()
+
+        holder.itemView.setOnClickListener { editReminder(reminder) }
+
+        holder.switchView.isChecked = reminder.enabled
+        holder.switchView.setOnClickListener { toggleReminderEnabled(reminder.id, reminder.scope, position) }
+    }
+
+    override fun getItemCount(): Int = reminders.size
+}
