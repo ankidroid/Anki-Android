a393e571f0875afdb108a0e2e82920257d096971
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2025-08-02 21:19:29 -0300

    test: PrefEnum entry values match
    their respective preferences

--- AnkiDroid/src/test/java/com/ichi2/anki/settings/PrefsRobolectricTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/settings/PrefsRobolectricTest.kt
@@ -20,9 +20,11 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.ivanshafran.sharedpreferencesmock.SPMockBuilder
 import com.ichi2.anki.AnkiDroidApp
 import com.ichi2.anki.RobolectricTest
+import com.ichi2.anki.libanki.utils.append
 import com.ichi2.anki.preferences.PreferenceTestUtils
 import com.ichi2.anki.preferences.PreferenceTestUtils.getAttrsFromXml
 import com.ichi2.anki.preferences.SettingsFragment
+import com.ichi2.anki.settings.enums.PrefEnum
 import com.ichi2.testutils.EmptyApplication
 import io.mockk.every
 import io.mockk.mockk
@@ -40,8 +42,14 @@ import org.mockito.Mockito.spy
 import org.mockito.kotlin.anyOrNull
 import org.mockito.kotlin.whenever
 import org.robolectric.annotation.Config
+import kotlin.reflect.KClass
 import kotlin.reflect.KVisibility
+import kotlin.reflect.full.createType
+import kotlin.reflect.full.isSubtypeOf
 import kotlin.reflect.full.memberProperties
+import kotlin.sequences.map
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
 
 @RunWith(AndroidJUnit4::class)
 @Config(application = EmptyApplication::class)
@@ -91,6 +99,68 @@ class PrefsRobolectricTest : RobolectricTest() {
             if (key !in prefs || key in devOptionsKeys) continue
             val prefsDefaultValue = prefs.getValue(key)
             assertThat("The default value of '$key' matches the preference XML", defaultValue.toString(), equalTo(prefsDefaultValue))
+        }
+    }
+
+    private fun getPropertyNamesAndKeys(): MutableMap<String, String> {
+        val spy = spy(SPMockBuilder().createSharedPreferences())
+        AnkiDroidApp.sharedPreferencesTestingOverride = spy
+        val keys = mutableListOf<String>()
+
+        val mockResources = mockk<Resources>()
+        every { mockResources.getString(any()) } answers { invocation.args[0].toString() }
+        mockkObject(Prefs)
+        every { Prefs.resources } returns mockResources
+        doAnswer { invocation ->
+            val key = PreferenceTestUtils.attrValueToString("@${invocation.arguments[0]}", targetContext)
+            keys.append(key)
+            invocation.callRealMethod()
+        }.run {
+            whenever(spy).getBoolean(anyString(), anyBoolean())
+            whenever(spy).getString(anyString(), anyOrNull())
+            whenever(spy).getInt(anyString(), anyInt())
+        }
+        val propertyNamesAndKeys = mutableMapOf<String, String>()
+        for (property in Prefs::class.memberProperties) {
+            if (property.visibility != KVisibility.PUBLIC) continue
+            property.getter.call(Prefs)
+            propertyNamesAndKeys[property.name] = keys.last()
+        }
+        unmockkObject(Prefs)
+        return propertyNamesAndKeys
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    @Test
+    fun `PrefEnum values match their preference entries`() {
+        val allPropertiesAndKeys = getPropertyNamesAndKeys()
+        val enumProperties =
+            Prefs::class.memberProperties.filter {
+                it.returnType.isSubtypeOf(PrefEnum::class.createType())
+            }
+        val enumPropertiesMap = enumProperties.associateBy { allPropertiesAndKeys.getValue(it.name) }
+
+        val prefsEnumKeysAndValues = mutableMapOf<String, List<String>>()
+        for ((key, property) in enumPropertiesMap.entries) {
+            val enumConstants = ((property.returnType.classifier as KClass<*>).java.enumConstants) as Array<PrefEnum>
+            prefsEnumKeysAndValues[key] = enumConstants.map { it.entryValue }
+        }
+
+        val listPreferences =
+            PreferenceTestUtils
+                .getAllPreferencesFragments(targetContext)
+                .filterIsInstance<SettingsFragment>()
+                .map { it.preferenceResource }
+                .flatMap { getAttrsFromXml(targetContext, it, listOf("key", "entryValues")) }
+                .filter { it["entryValues"] != null }
+                .associate {
+                    PreferenceTestUtils.attrValueToString(it["key"]!!, targetContext) to
+                        PreferenceTestUtils.attrToStringArray(it["entryValues"]!!, targetContext).toList()
+                }
+
+        for ((key, enumValues) in prefsEnumKeysAndValues) {
+            assertContains(listPreferences, key)
+            assertEquals(enumValues, listPreferences[key])
         }
     }
 }
