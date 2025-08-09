684a184ec5129541284a5d2b3283973a2b0c295d
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2025-08-09 06:42:39 -0300

    chore: fix copyright headers

--- AnkiDroid/src/main/java/com/ichi2/anki/ui/windows/reviewer/ReviewerMenu.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/ui/windows/reviewer/ReviewerMenu.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Brayan Oliveira <69634269+brayandso@users.noreply.github.con>
+ * Copyright (c) 2025 Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
