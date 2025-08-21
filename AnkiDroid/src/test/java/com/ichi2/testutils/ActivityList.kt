465d784b03afb75dc3ed0b606a1cdc0aa95fbdbc
Song Eric Yu Li <ericli3690@gmail.com>
2025-08-20 23:13:33 -0400

    refactor: delete ReminderService
    GSoC 2025: Review Reminders
    
    - ReminderService was an old attempt at implementing review reminders, but the main methods inside are never actually called anymore. The only code that was still referencing the class was only accessing a constant in its companion object. Here, I've moved that constant out into IntentHandler (where it was previously used) and deleted ReminderService and its test.
    - I've decided to fully delete the class instead of only marking it as ready for deprecation because the code is never actually run.
    - Deleted unused strings, too.

--- AnkiDroid/src/test/java/com/ichi2/testutils/ActivityList.kt
+++ AnkiDroid/src/test/java/com/ichi2/testutils/ActivityList.kt
@@ -29,6 +29,7 @@ import com.ichi2.anki.DrawingActivity
 import com.ichi2.anki.FilteredDeckOptions
 import com.ichi2.anki.Info
 import com.ichi2.anki.IntentHandler
+import com.ichi2.anki.IntentHandler.Companion.getReviewDeckIntent
 import com.ichi2.anki.IntentHandler2
 import com.ichi2.anki.IntroductionActivity
 import com.ichi2.anki.LoginActivity
@@ -44,7 +45,6 @@ import com.ichi2.anki.multimedia.MultimediaActivity
 import com.ichi2.anki.notetype.ManageNotetypes
 import com.ichi2.anki.preferences.PreferencesActivity
 import com.ichi2.anki.previewer.CardViewerActivity
-import com.ichi2.anki.services.ReminderService.Companion.getReviewDeckIntent
 import com.ichi2.anki.ui.windows.managespace.ManageSpaceActivity
 import com.ichi2.anki.ui.windows.permissions.PermissionsActivity
 import com.ichi2.testutils.ActivityList.ActivityLaunchParam.Companion.get
