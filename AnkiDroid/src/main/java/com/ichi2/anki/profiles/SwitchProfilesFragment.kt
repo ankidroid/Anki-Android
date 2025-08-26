a44733fac4fe1eb50aa0b740f5cde9bb9717d627
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2025-08-27 00:15:07 +0530

    dev: add multiple profile as developer option - enables dev option for multiple profile in dev settings - feat: created empty preference screen for multiple profile once the dev option is enabled

---
+++ AnkiDroid/src/main/java/com/ichi2/anki/profiles/SwitchProfilesFragment.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2025 Ashish Yadav <mailtoashish693@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ichi2.anki.profiles
+
+import android.content.Context
+import android.content.Intent
+import androidx.fragment.app.Fragment
+import com.ichi2.anki.R
+import com.ichi2.anki.SingleFragmentActivity
+
+class SwitchProfilesFragment : Fragment(R.layout.fragment_switch_profiles) {
+    companion object {
+        fun getIntent(context: Context): Intent =
+            SingleFragmentActivity.getIntent(context = context, fragmentClass = SwitchProfilesFragment::class)
+    }
+}
