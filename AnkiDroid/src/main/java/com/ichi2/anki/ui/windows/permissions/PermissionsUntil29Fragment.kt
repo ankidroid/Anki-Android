407039f8c8a77f5312d0e2ee49cd8c26dd4e1263
Song Eric Yu Li <ericli3690@gmail.com>
2025-08-27 17:20:53 -0600

    improvement: stop reliance on shouldShowRequestPermissionRationale
    GSoC 2025: Review Reminders
    
    Deletes the `hasAnyOfPermissionBeenDenied` helper method. This was previously only used by PermissionsUntil29Fragment in checking whether it should try to use a dialog to request legacy storage permissions or if it should just redirect the user to the OS settings. In my own testing, relying on the `shouldShowRequestPermissionRationale` method provided by Android is a bad idea as it returns true or false in a very non-intuitive way. See [this post](https://stackoverflow.com/questions/41310510/what-is-the-difference-between-shouldshowrequestpermissionrationale-and-requestp) for more details. As an example, the method will return false if the user has never requested the permission before, which is a case where we should just immediately show the request dialog rather than delegating to the OS settings.
    
    My change modifies the storageLauncher so that if something fails with the permissions request dialog, we still fall back to opening the OS settings. This allows me to simplify the `if` clauses in the onViewCreated method of PermissionsUntil29Fragment.

--- AnkiDroid/src/main/java/com/ichi2/anki/ui/windows/permissions/PermissionsUntil29Fragment.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/ui/windows/permissions/PermissionsUntil29Fragment.kt
@@ -22,7 +22,6 @@ import androidx.activity.result.contract.ActivityResultContracts
 import com.ichi2.anki.AnkiActivity
 import com.ichi2.anki.R
 import com.ichi2.utils.Permissions
-import com.ichi2.utils.hasAnyOfPermissionsBeenDenied
 
 /**
  * Permissions screen for requesting permissions until API 29.
@@ -36,7 +35,14 @@ class PermissionsUntil29Fragment : PermissionsFragment(R.layout.permissions_unti
     private val storageLauncher =
         registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions(),
-        ) {}
+        ) { requestedPermissions ->
+            if (!requestedPermissions.all { it.value }) {
+                // The permission dialog did not show up of the user denied the permission.
+                // Offers to open the OS settings section for AnkiDroid. In this section, the user can
+                // manually grant the permission.
+                showToastAndOpenAppSettingsScreen(R.string.startup_no_storage_permission)
+            }
+        }
 
     override fun onViewCreated(
         view: View,
@@ -45,12 +51,10 @@ class PermissionsUntil29Fragment : PermissionsFragment(R.layout.permissions_unti
         val storagePermission = view.findViewById<PermissionsItem>(R.id.storage_permission)
         storagePermission.setOnPermissionsRequested { areAlreadyGranted ->
             if (areAlreadyGranted) return@setOnPermissionsRequested
-            if (!userCanGrantWriteExternalStorage()) {
-                AndroidPermanentlyRevokedPermissionsDialog.show(requireActivity() as AnkiActivity)
-            } else if (!hasAnyOfPermissionsBeenDenied(storagePermission.permissions)) {
+            if (userCanGrantWriteExternalStorage()) {
                 storageLauncher.launch(storagePermission.permissions.toTypedArray())
             } else {
-                showToastAndOpenAppSettingsScreen(R.string.startup_no_storage_permission)
+                AndroidPermanentlyRevokedPermissionsDialog.show(requireActivity() as AnkiActivity)
             }
         }
     }
