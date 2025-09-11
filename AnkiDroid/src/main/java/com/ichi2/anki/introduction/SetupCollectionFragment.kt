b007cc4f2bd76c33d0746614f1b94415ec702148
David Allison <62114487+david-allison@users.noreply.github.com>
2025-09-11 01:00:33 +0100

    chore: convert SetupCollectionFragment to ViewBinding
    Issue 11116

--- AnkiDroid/src/main/java/com/ichi2/anki/introduction/SetupCollectionFragment.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/introduction/SetupCollectionFragment.kt
@@ -36,13 +36,14 @@ package com.ichi2.anki.introduction
 import android.os.Bundle
 import android.os.Parcelable
 import android.view.View
-import android.widget.Button
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
 import com.ichi2.anki.R
+import com.ichi2.anki.databinding.IntroductionLayoutBinding
 import com.ichi2.anki.introduction.SetupCollectionFragment.CollectionSetupOption.DeckPickerWithNewCollection
 import com.ichi2.anki.introduction.SetupCollectionFragment.CollectionSetupOption.SyncFromExistingAccount
+import dev.androidbroadcast.vbpd.viewBinding
 import kotlinx.parcelize.Parcelize
 
 /**
@@ -58,18 +59,16 @@ import kotlinx.parcelize.Parcelize
  * if the app is uninstalled.
  */
 class SetupCollectionFragment : Fragment(R.layout.introduction_layout) {
+    val binding by viewBinding(IntroductionLayoutBinding::bind)
+
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.get_started).apply {
-            setOnClickListener { setResult(DeckPickerWithNewCollection) }
-        }
-        view.findViewById<Button>(R.id.sync_profile).apply {
-            setOnClickListener { setResult(SyncFromExistingAccount) }
-        }
+        binding.getStarted.setOnClickListener { setResult(DeckPickerWithNewCollection) }
+        binding.syncProfile.setOnClickListener { setResult(SyncFromExistingAccount) }
     }
 
     private fun setResult(option: CollectionSetupOption) {
