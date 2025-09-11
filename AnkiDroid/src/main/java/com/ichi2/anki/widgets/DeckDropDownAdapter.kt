6adcae060d61eac09857d6fadf65a7c9ecda09a2
David Allison <62114487+david-allison@users.noreply.github.com>
2025-09-11 05:00:31 +0100

    chore: convert DeckDropDownAdapter to ViewBinding
    Issue 11116

--- AnkiDroid/src/main/java/com/ichi2/anki/widgets/DeckDropDownAdapter.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/widgets/DeckDropDownAdapter.kt
@@ -23,6 +23,8 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
 import com.ichi2.anki.R
+import com.ichi2.anki.databinding.DropdownDeckItemBinding
+import com.ichi2.anki.databinding.DropdownDeckSelectedItemBinding
 import com.ichi2.anki.libanki.DeckNameId
 
 class DeckDropDownAdapter(
@@ -45,8 +47,7 @@ class DeckDropDownAdapter(
     }
 
     internal data class DeckDropDownViewHolder(
-        val deckNameView: TextView,
-        val deckCountsView: TextView,
+        val binding: DropdownDeckSelectedItemBinding,
     )
 
     fun addDeck(deck: DeckNameId) {
@@ -72,28 +73,24 @@ class DeckDropDownAdapter(
     ): View? {
         var convertView = view
         val viewHolder: DeckDropDownViewHolder
-        val deckNameView: TextView
-        val deckCountsView: TextView
         if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.dropdown_deck_selected_item, parent, false)
-            deckNameView = convertView.findViewById(R.id.dropdown_deck_name)
-            deckCountsView = convertView.findViewById(R.id.dropdown_deck_counts)
-            viewHolder = DeckDropDownViewHolder(deckNameView, deckCountsView)
+            val layoutInflater = LayoutInflater.from(context)
+            val binding = DropdownDeckSelectedItemBinding.inflate(layoutInflater, parent, false)
+            convertView = binding.root
+            viewHolder = DeckDropDownViewHolder(binding)
             convertView.tag = viewHolder
         } else {
             viewHolder = convertView.tag as DeckDropDownViewHolder
-            deckNameView = viewHolder.deckNameView
-            deckCountsView = viewHolder.deckCountsView
         }
         if (position == 0) {
-            deckNameView.text = context.resources.getString(R.string.card_browser_all_decks)
+            viewHolder.binding.deckName.text = context.resources.getString(R.string.card_browser_all_decks)
         } else {
             val deck = deckList[position - 1]
             val deckName = deck.name
-            deckNameView.text = deckName
+            viewHolder.binding.deckName.text = deckName
         }
-        deckCountsView.text = subtitleProvider?.deckDropDownSubtitle ?: ""
-        return convertView
+        viewHolder.binding.deckCounts.text = subtitleProvider?.deckDropDownSubtitle ?: ""
+        return viewHolder.binding.root
     }
 
     override fun getDropDownView(
@@ -101,14 +98,16 @@ class DeckDropDownAdapter(
         view: View?,
         parent: ViewGroup,
     ): View? {
-        var convertView = view
+        var convertView = view // support mutation of view
         val deckNameView: TextView
         if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.dropdown_deck_item, parent, false)
-            deckNameView = convertView.findViewById(R.id.dropdown_deck_name)
-            convertView.tag = deckNameView
+            val layoutInflater = LayoutInflater.from(context)
+            val binding = DropdownDeckItemBinding.inflate(layoutInflater, parent, false)
+            deckNameView = binding.dropdownDeckName
+            binding.root.tag = deckNameView
+            convertView = binding.root
         } else {
-            deckNameView = convertView.tag as TextView
+            deckNameView = view.tag as TextView
         }
         if (position == 0) {
             deckNameView.text = context.resources.getString(R.string.card_browser_all_decks)
