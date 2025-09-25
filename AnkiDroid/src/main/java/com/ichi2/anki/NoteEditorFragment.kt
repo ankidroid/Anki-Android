569d775ee413361fa0698e1c193a932f0351112e
lukstbit <52494258+lukstbit@users.noreply.github.com>
2025-09-25 12:34:38 +0300

    Hide fields in NoteEditorFragment for image occlusion notetypes
    Showing the fields is not needed because we handle the image occlusion
    with a backend page, desktop app also doesn't show the fields for image
    occlusion and it's confusing for the users.

--- AnkiDroid/src/main/java/com/ichi2/anki/NoteEditorFragment.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/NoteEditorFragment.kt
@@ -1789,6 +1789,9 @@ class NoteEditorFragment :
         editNoteTypeMode: Boolean,
     ) {
         val editLines = fieldState.loadFieldEditLines(type)
+        // showing the fields is not needed for image occlusion notetypes as they are handled by the
+        // backend page
+        fieldsLayoutContainer?.isVisible = !currentNotetypeIsImageOcclusion()
         fieldsLayoutContainer!!.removeAllViews()
         customViewIds.clear()
         imageOcclusionButtonsContainer?.isVisible = currentNotetypeIsImageOcclusion()
