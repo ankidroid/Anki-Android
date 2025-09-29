cae8a003b6b67dd461453fdfffa7b7a46e91cf8a
Fox-RealCodeFixes <foxyfnaf.93yt@gmail.com>
2025-09-29 16:18:57 +0200

    Change copyrights to reflect my new pseudonym.

--- AnkiDroid/src/main/java/com/ichi2/anki/AppLifecycleObserver.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/AppLifecycleObserver.kt
@@ -1,5 +1,5 @@
 /****************************************************************************************
- * Copyright (c) 2025 Fox Programmer <foxyfnaf.93yt@gmail.com>                          *
+ * Copyright (c) 2025 Fox - Real Code Fixes <foxyfnaf.93yt@gmail.com>                          *
  *                                                                                      *
  * This program is free software; you can redistribute it and/or modify it under        *
  * the terms of the GNU General Public License as published by the Free Software        *
