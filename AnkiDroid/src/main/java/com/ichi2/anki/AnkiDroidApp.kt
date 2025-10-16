ankidroid/Anki-Android

commit 32a1b44ee14c7e8915ecd427a9fa70f07f93a2ad

fix: Web debugging not working
this fixes:
1. web debugging not working on all the web screens immediately after the setting is changed (the user needed to open the reviewer for that)
2. web debugging not working on release builds on the new study screen

AnkiDroid/src/main/java/com/ichi2/anki/AnkiDroidApp.kt
@@ -28,8 +28,8 @@ import android.os.Bundle
 import android.os.Environment
 import android.system.Os
 import android.webkit.CookieManager
+import android.webkit.WebView
 import androidx.annotation.VisibleForTesting
-import androidx.core.content.edit
 import androidx.core.net.toUri
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.MutableLiveData
@@ -40,7 +40,6 @@ import com.ichi2.anki.browser.SharedPreferencesLastDeckIdRepository
 import com.ichi2.anki.common.annotations.LegacyNotifications
 import com.ichi2.anki.common.annotations.NeedsTest
 import com.ichi2.anki.common.utils.annotation.KotlinCleanup
-import com.ichi2.anki.common.utils.isRunningAsUnitTest
 import com.ichi2.anki.contextmenu.AnkiCardContextMenu
 import com.ichi2.anki.contextmenu.CardBrowserContextMenu
 import com.ichi2.anki.exception.StorageAccessException
@@ -163,10 +162,8 @@ open class AnkiDroidApp :
             showThemedToast(this.applicationContext, getString(R.string.user_is_a_robot), false)
         }
 
-        // make default HTML / JS debugging true for debug build and disable for unit/android tests
-        if (BuildConfig.DEBUG && !isRunningAsUnitTest) {
-            preferences.edit { putBoolean("html_javascript_debugging", true) }
-        }
+        WebView.setWebContentsDebuggingEnabled(Prefs.isWebDebugEnabled)
+
         CardBrowserContextMenu.ensureConsistentStateWithPreferenceStatus(
             this,
             preferences.getBoolean(

AnkiDroid/src/main/java/com/ichi2/anki/preferences/DevOptionsFragment.kt
@@ -15,6 +15,7 @@
  */
 package com.ichi2.anki.preferences
 
+import android.webkit.WebView
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.edit
@@ -189,6 +190,17 @@ class DevOptionsFragment : SettingsFragment() {
         requirePreference<Preference>(R.string.pref_new_review_reminders).setOnPreferenceChangeListener { _, _ ->
             ActivityCompat.recreate(requireActivity())
             true
+        }
+
+        setupWebDebugPreference()
+    }
+
+    private fun setupWebDebugPreference() {
+        requirePreference<SwitchPreferenceCompat>(R.string.html_javascript_debugging_key).apply {
+            isVisible = !BuildConfig.DEBUG
+            setOnPreferenceChangeListener { isEnabled ->
+                WebView.setWebContentsDebuggingEnabled(isEnabled)
+            }
         }
     }
 

AnkiDroid/src/main/java/com/ichi2/anki/AbstractFlashcardViewer.kt
@@ -163,7 +163,6 @@ import com.ichi2.ui.FixedEditText
 import com.ichi2.utils.HandlerUtils.newHandler
 import com.ichi2.utils.HashUtil.hashSetInit
 import com.ichi2.utils.Stopwatch
-import com.ichi2.utils.WebViewDebugging.initializeDebugging
 import com.ichi2.utils.message
 import com.ichi2.utils.negativeButton
 import com.ichi2.utils.positiveButton
@@ -1269,7 +1268,6 @@ abstract class AbstractFlashcardViewer :
     protected open fun recreateWebView() {
         if (webView == null) {
             webView = createWebView()
-            initializeDebugging(this.sharedPrefs())
             cardFrame!!.addView(webView)
             gestureDetectorImpl.onWebViewCreated(webView!!)
         }

AnkiDroid/src/main/java/com/ichi2/anki/settings/Prefs.kt
@@ -23,6 +23,7 @@ import com.ichi2.anki.AnkiDroidApp
 import com.ichi2.anki.BuildConfig
 import com.ichi2.anki.R
 import com.ichi2.anki.cardviewer.TapGestureMode
+import com.ichi2.anki.common.utils.isRunningAsUnitTest
 import com.ichi2.anki.settings.enums.FrameStyle
 import com.ichi2.anki.settings.enums.HideSystemBars
 import com.ichi2.anki.settings.enums.PrefEnum
@@ -294,6 +295,9 @@ object Prefs {
         get() = getBoolean(R.string.dev_card_browser_fragmented, false)
 
     val devUsingCardBrowserSearchView: Boolean by booleanPref(R.string.dev_card_browser_search_view, false)
+
+    val isWebDebugEnabled: Boolean
+        get() = (getBoolean(R.string.html_javascript_debugging_key, false) || BuildConfig.DEBUG) && !isRunningAsUnitTest
 
     // **************************************** UI Config *************************************** //
 

AnkiDroid/src/main/java/com/ichi2/utils/WebViewDebugging.kt
@@ -16,28 +16,12 @@
 
 package com.ichi2.utils
 
-import android.content.SharedPreferences
 import android.os.Build
 import android.webkit.WebView
 import androidx.annotation.RequiresApi
-import androidx.annotation.UiThread
 
 object WebViewDebugging {
     private var sHasSetDataDirectory = false
-
-    @UiThread
-    fun initializeDebugging(sharedPrefs: SharedPreferences) {
-        // DEFECT: We might be able to cache this value: check what happens on WebView Renderer crash
-        // On your desktop use chrome://inspect to connect to emulator WebViews
-        // Beware: Crash in AnkiDroidApp.onCreate() with:
-        //
-        //     java.lang.RuntimeException: Using WebView from more than one process at once with the same data directory
-        //     is not supported. https://crbug.com/558377 : Lock owner com.ichi2.anki:acra at
-        //     org.chromium.android_webview.AwDataDirLock.a(PG:26)
-        //
-        val enableDebugging = sharedPrefs.getBoolean("html_javascript_debugging", false)
-        WebView.setWebContentsDebuggingEnabled(enableDebugging)
-    }
 
     /** Throws IllegalStateException if a WebView has been initialized  */
     @RequiresApi(api = Build.VERSION_CODES.P)

commit 5b70db3f6472b4010810efff10820226530830d5

fix: crash on startup when WebView uninstalled
Caused by 32a1b44ee14c7e8915ecd427a9fa70f07f93a2ad

Tested on API 29 emulator with:

adb shell pm uninstall --user 0 com.google.android.webview

AnkiDroid/src/main/java/com/ichi2/anki/AnkiDroidApp.kt
@@ -28,12 +28,12 @@ import android.os.Bundle
 import android.os.Environment
 import android.system.Os
 import android.webkit.CookieManager
-import android.webkit.WebView
 import androidx.annotation.VisibleForTesting
 import androidx.core.net.toUri
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.MutableLiveData
 import anki.collection.OpChanges
+import com.ichi2.anki.AnkiDroidApp.Companion.sharedPreferencesTestingOverride
 import com.ichi2.anki.CrashReportService.sendExceptionReport
 import com.ichi2.anki.analytics.UsageAnalytics
 import com.ichi2.anki.browser.SharedPreferencesLastDeckIdRepository
@@ -60,6 +60,7 @@ import com.ichi2.utils.AdaptionUtil
 import com.ichi2.utils.ExceptionUtil
 import com.ichi2.utils.LanguageUtil
 import com.ichi2.utils.Permissions
+import com.ichi2.utils.setWebContentsDebuggingEnabled
 import com.ichi2.widget.cardanalysis.CardAnalysisWidget
 import com.ichi2.widget.deckpicker.DeckPickerWidget
 import kotlinx.coroutines.CoroutineScope
@@ -162,7 +163,7 @@ open class AnkiDroidApp :
             showThemedToast(this.applicationContext, getString(R.string.user_is_a_robot), false)
         }
 
-        WebView.setWebContentsDebuggingEnabled(Prefs.isWebDebugEnabled)
+        setWebContentsDebuggingEnabled(Prefs.isWebDebugEnabled)
 
         CardBrowserContextMenu.ensureConsistentStateWithPreferenceStatus(
             this,

AnkiDroid/src/main/java/com/ichi2/anki/preferences/DevOptionsFragment.kt
@@ -15,7 +15,6 @@
  */
 package com.ichi2.anki.preferences
 
-import android.webkit.WebView
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.edit
@@ -34,6 +33,7 @@ import com.ichi2.anki.showThemedToast
 import com.ichi2.anki.snackbar.showSnackbar
 import com.ichi2.anki.withProgress
 import com.ichi2.preferences.IncrementerNumberRangePreferenceCompat
+import com.ichi2.utils.setWebContentsDebuggingEnabled
 import com.ichi2.utils.show
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -199,7 +199,7 @@ class DevOptionsFragment : SettingsFragment() {
         requirePreference<SwitchPreferenceCompat>(R.string.html_javascript_debugging_key).apply {
             isVisible = !BuildConfig.DEBUG
             setOnPreferenceChangeListener { isEnabled ->
-                WebView.setWebContentsDebuggingEnabled(isEnabled)
+                setWebContentsDebuggingEnabled(isEnabled)
             }
         }
     }

AnkiDroid/src/main/java/com/ichi2/utils/WebViewUtils.kt
@@ -153,3 +153,32 @@ private fun getAndroidSystemWebViewPackageInfo(packageManager: PackageManager): 
     return getPackage("com.google.android.webview")
         ?: getPackage("com.android.webview") // com.android.webview is used on API 24
 }
+
+/**
+ * Enables debugging of web contents (HTML / CSS / JavaScript)
+ * loaded into any WebViews of this application. This flag can be enabled
+ * in order to facilitate debugging of web layouts and JavaScript
+ * code running inside WebViews. Please refer to WebView documentation
+ * for the debugging guide.
+ *
+ * In WebView 113.0.5656.0 and later, this is enabled automatically if the
+ * app is declared as
+ * [`android:debuggable="true"`](https://developer.android.com/guide/topics/manifest/application-element#debug)
+ * in its manifest; otherwise, the
+ * default is {@code false}.
+ *
+ * Enabling web contents debugging allows the state of any WebView in the
+ * app to be inspected and modified by the user via adb. This is a security
+ * liability and should not be enabled in production builds of apps unless
+ * this is an explicitly intended use of the app. More info on
+ * [secure debug settings](https://developer.android.com/topic/security/risks/android-debuggable)
+ *
+ * @param enabled whether to enable web contents debugging
+ */
+fun setWebContentsDebuggingEnabled(enabled: Boolean) =
+    try {
+        WebView.setWebContentsDebuggingEnabled(enabled)
+    } catch (e: Exception) {
+        // android.util.AndroidRuntimeException: android.webkit.WebViewFactory$MissingWebViewPackageException: Failed to load WebView provider: No WebView installed
+        Timber.w(e, "setWebContentsDebuggingEnabled")
+    }