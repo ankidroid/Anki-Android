f8b873483cbc1f201c01c106ed0d4bb300e8d411
Fox-RealCodeFixes <foxyfnaf.93yt@gmail.com>
2025-10-22 16:54:08 +0200

    Apply patch and clean up dependencies

--- AnkiDroid/src/main/java/com/ichi2/anki/AppLifecycleObserver.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/AppLifecycleObserver.kt
@@ -1,44 +1,40 @@
-/****************************************************************************************
- * Copyright (c) 2025 Fox - Real Code Fixes <foxyfnaf.93yt@gmail.com>                          *
- *                                                                                      *
- * This program is free software; you can redistribute it and/or modify it under        *
- * the terms of the GNU General Public License as published by the Free Software        *
- * Foundation; either version 3 of the License, or (at your option) any later           *
- * version.                                                                             *
- *                                                                                      *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
- * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
- *                                                                                      *
- * You should have received a copy of the GNU General Public License along with         *
- * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
- ****************************************************************************************/
+/*
+ *  Copyright (c) 2025 David Allison <davidallisongithub@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package com.ichi2.anki
 
 import android.content.Context
 import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
-import com.ichi2.anki.worker.SmallWidgetUpdateWorker
-import java.util.concurrent.TimeUnit
+import com.ichi2.widget.WidgetStatus
+import timber.log.Timber
 
 class AppLifecycleObserver(
-    private val appContext: Context,
+    private val context: Context,
 ) : DefaultLifecycleObserver {
     override fun onStop(owner: LifecycleOwner) {
         super.onStop(owner)
 
-        val workRequest =
-            PeriodicWorkRequestBuilder<SmallWidgetUpdateWorker>(15, TimeUnit.MINUTES)
-                .build()
-
-        WorkManager.getInstance(appContext).enqueueUniquePeriodicWork(
-            R.string.small_widget_update_work.toString(),
-            ExistingPeriodicWorkPolicy.UPDATE,
-            workRequest,
-        )
+        if (owner.lifecycle.currentState != Lifecycle.State.DESTROYED && CollectionManager.isOpenUnsafe()) {
+            try {
+                WidgetStatus.updateInBackground(context)
+            } catch (e: Exception) {
+                Timber.w(e)
+            }
+        }
     }
 }
