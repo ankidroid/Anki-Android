7172237f36bc1f5ff8cdb046b051b677421e358c
David Allison <62114487+david-allison@users.noreply.github.com>
2025-10-27 09:19:58 +0000

    chore(architecture): setup `vbpd` module skeleton
    ViewBindingPropertyDelegate
    
    Added in 2c696aec043d876c3ce05c5f6aa251bfda1d0fb1
    
    Was requested to be moved to a module:
    
    https://redirect.github.com/ankidroid/Anki-Android/issues/11116#issuecomment-3432354012
    
    ----
    
    https://github.com/androidbroadcast/ViewBindingPropertyDelegate/
    
    Issue 11116

---
+++ vbpd/build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "dev.androidbroadcast.vbpd"
+    compileSdk =
+        libs.versions.compileSdk
+            .get()
+            .toInt()
+
+    defaultConfig {
+        minSdk =
+            libs.versions.minSdk
+                .get()
+                .toInt()
+
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+}
