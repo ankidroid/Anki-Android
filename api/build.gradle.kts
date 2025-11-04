330ef360879ba3c3048f81603bc8d1e429a9ed6b
Mike Hardy <github@mikehardy.net>
2025-11-04 13:46:38 -0500

    chore: AGP9 has kotlin built in, so applying it as plugin is redundant

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
     id("maven-publish")
 }
 
--- libanki/build.gradle.kts
+++ libanki/build.gradle.kts
@@ -3,7 +3,6 @@ import java.util.Properties
 
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
 }
 
