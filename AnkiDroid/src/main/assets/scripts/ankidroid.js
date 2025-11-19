ec1c4186ba22568e4eb048d3a9df0a3097fd59f8
raiyyan <f20241312@pilani.bits-pilani.ac.in>
2025-11-19 20:03:42 +0530

    Update ankidroid.js

--- AnkiDroid/src/main/assets/scripts/ankidroid.js
+++ AnkiDroid/src/main/assets/scripts/ankidroid.js
@@ -42,7 +42,6 @@ document.addEventListener("focusout", event => {
 (() => {
     const SCHEME = "gesture";
     const MULTI_TOUCH_TIMEOUT = 300;
-    const GESTURE_TIMEOUT = 800;
 
     let startX = 0,
         startY = 0,
@@ -56,7 +55,7 @@ document.addEventListener("focusout", event => {
             startX = event.touches[0].pageX;
             startY = event.touches[0].pageY;
             // start counting from the first finger touch
-            if (touchCount === 1) {
+            if (touchCount == 1) {
                 touchStartTime = Date.now();
             }
         },
@@ -81,11 +80,6 @@ document.addEventListener("focusout", event => {
                     return;
                 }
                 window.location.href = `${SCHEME}://multiFingerTap/?touchCount=${touchCount}`;
-                return;
-            }
-
-            // Ignore gesture if it takes too long
-            if (Date.now() - touchStartTime > GESTURE_TIMEOUT) {
                 return;
             }
 
