b94d4fab352ad0b59a0d664e59a8cd836fa1bc8b
infiniteflux <Vabhinav12112003@gmail.com>
2025-11-20 00:25:43 +0530

    Fix: added GPL header and resolved duplicate cancel string

--- AnkiDroid/src/main/java/com/ichi2/anki/account/AccountRemovalExplanationDialog.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/account/AccountRemovalExplanationDialog.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2025 Abhinav Varma <vabhinav12112003@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.ichi2.anki.account
 import android.annotation.SuppressLint
 import android.app.Dialog
@@ -47,7 +63,7 @@ class AccountRemovalExplanationDialog : DialogFragment() {
             .setView(view)
             .setPositiveButton(R.string.proceed) { _, _ ->
                 setFragmentResult(REQUEST_KEY, bundleOf(RESULT_PROCEED to true))
-            }.setNegativeButton(R.string.cancel, null)
+            }.setNegativeButton(R.string.cancel_btn, null)
             .create()
     }
 
