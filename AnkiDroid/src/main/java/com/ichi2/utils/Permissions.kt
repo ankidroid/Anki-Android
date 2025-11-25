953008928772554ecdab1cad9e0da8b706844865
Song Eric Yu Li <ericli3690@gmail.com>
2025-11-24 23:28:28 -0500

    feat(reminders): showNotificationsPermissionBottomSheetIfNeeded
    GSoC 2025: Review Reminders
    
    This method takes a callback so that I can do some conditional preference-setting in a later commit. Launches the notification permission BottomSheet if notification permissions are not granted and the user has not conclusively rejected notification permissions.

--- AnkiDroid/src/main/java/com/ichi2/utils/Permissions.kt
+++ AnkiDroid/src/main/java/com/ichi2/utils/Permissions.kt
@@ -32,9 +32,13 @@ import androidx.annotation.StringRes
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.ichi2.anki.PermissionSet
 import com.ichi2.anki.R
 import com.ichi2.anki.common.utils.android.isRobolectric
+import com.ichi2.anki.settings.Prefs
 import com.ichi2.anki.showThemedToast
+import com.ichi2.anki.ui.windows.permissions.PermissionsBottomSheet
 import com.ichi2.compat.CompatHelper.Companion.getPackageInfoCompat
 import com.ichi2.compat.PackageInfoFlagsCompat
 import com.ichi2.utils.Permissions.MANAGE_EXTERNAL_STORAGE
@@ -111,6 +115,38 @@ object Permissions {
             permissionRequestLauncher.launch(permission)
         } else {
             showToastAndOpenAppSettingsScreen(R.string.manually_grant_permissions)
+        }
+    }
+
+    /**
+     * Shows the [com.ichi2.anki.ui.windows.permissions.NotificationsPermissionFragment] in the [PermissionsBottomSheet]
+     * if notification permissions have not been granted. Does nothing if the permission does not need to
+     * be requested (i.e. API < 33), if the permission has already been granted,
+     * or if the user has previously denied the permission and selected "Don't ask again".
+     *
+     * @param activity Used for checking whether notification permissions have been granted, or if the user has clicked
+     * "Don't ask again" on previous requests.
+     * @param fragmentManager Used for launching the BottomSheet, if necessary.
+     * @param callback Executed only if the BottomSheet is actually shown.
+     */
+    fun showNotificationsPermissionBottomSheetIfNeeded(
+        activity: Activity,
+        fragmentManager: FragmentManager,
+        callback: () -> Unit,
+    ) {
+        postNotification?.let { notificationPermission ->
+            if (
+                !canPostNotifications(context = activity) &&
+                canPermissionBeRequested(
+                    activity,
+                    notificationPermission,
+                    Prefs::notificationsPermissionRequested,
+                )
+            ) {
+                Timber.i("Showing notifications bottom sheet")
+                PermissionsBottomSheet.launch(fragmentManager, PermissionSet.NOTIFICATIONS)
+                callback()
+            }
         }
     }
 
