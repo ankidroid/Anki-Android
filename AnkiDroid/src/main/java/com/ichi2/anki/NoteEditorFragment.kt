af1b135974009c229831435f746bfb6736afc677
David Allison <62114487+david-allison@users.noreply.github.com>
2025-12-11 22:02:36 +0700

    fix(note-editor): Updating Type and 'Decide by note type'
    Fixes 19750
    
    Co-authored-by: lukstbit <52494258+lukstbit@users.noreply.github.com>
    (cherry picked from commit 22ac839a17171b128f33a9728f9fb8ec25512486)

--- AnkiDroid/src/main/java/com/ichi2/anki/NoteEditorFragment.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/NoteEditorFragment.kt
@@ -2785,7 +2785,7 @@ class NoteEditorFragment :
 
         // Update deck
         if (!getColUnsafe.config.getBool(ConfigKey.Bool.ADDING_DEFAULTS_TO_CURRENT_DECK)) {
-            deckId = noteType.did
+            deckId = getColUnsafe.defaultsForAdding().deckId
         }
 
         refreshNoteData(FieldChangeType.changeFieldCount(shouldReplaceNewlines()))
