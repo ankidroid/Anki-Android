ankidroid/Anki-Android

commit 14a076980d142b638420e4e35204b1801192de63

Bring up numeric keyboard if answer is a number #1617

AnkiDroid/src/main/java/com/ichi2/anki/ui/windows/reviewer/ReviewerFragment.kt
@@ -20,6 +20,7 @@ import android.content.Intent
 import android.hardware.SensorManager
 import android.net.Uri
 import android.os.Bundle
+import android.text.InputType
 import android.text.SpannableString
 import android.text.style.UnderlineSpan
 import android.view.KeyEvent
@@ -76,6 +77,7 @@ import com.ichi2.anki.preferences.reviewer.ReviewerMenuView
 import com.ichi2.anki.preferences.reviewer.ViewerAction
 import com.ichi2.anki.previewer.CardViewerActivity
 import com.ichi2.anki.previewer.CardViewerFragment
+import com.ichi2.anki.previewer.TypeAnswer
 import com.ichi2.anki.previewer.stdHtml
 import com.ichi2.anki.reviewer.BindingMap
 import com.ichi2.anki.reviewer.ReviewerBinding
@@ -98,12 +100,13 @@ import com.ichi2.anki.utils.isWindowCompact
 import com.ichi2.themes.Themes
 import com.ichi2.utils.dp
 import com.ichi2.utils.show
+import com.ichi2.utils.stripHtml
 import com.squareup.seismic.ShakeDetector
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import org.jetbrains.annotations.VisibleForTesting
 import timber.log.Timber
-import java.lang.IllegalArgumentException
 import kotlin.math.roundToInt
 import kotlin.reflect.jvm.jvmName
 
@@ -322,6 +325,8 @@ class ReviewerFragment :
 
                     typeAnswerContainer.isVisible = true
                     typeAnswerEditText.apply {
+                        inputType = chooseInputType(typeInAnswer)
+
                         if (imeHintLocales != typeInAnswer.imeHintLocales) {
                             imeHintLocales = typeInAnswer.imeHintLocales
                             context?.getSystemService<InputMethodManager>()?.restartInput(this)
@@ -337,6 +342,15 @@ class ReviewerFragment :
             typeAnswerEditText.text = null
         }
     }
+
+    /** Chooses the input type based on whether the expected answer is a number or text */
+    @VisibleForTesting
+    fun chooseInputType(typeAnswer: TypeAnswer): Int =
+        if (stripHtml(typeAnswer.expectedAnswer).matches(Regex("^-?\\d+([.,]\\d*)?$"))) {
+            InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL or InputType.TYPE_NUMBER_FLAG_SIGNED
+        } else {
+            InputType.TYPE_CLASS_TEXT
+        }
 
     private fun resetZoom() {
         webView.settings.loadWithOverviewMode = false

AnkiDroid/src/androidTest/java/com/ichi2/anki/ReviewerFragmentTest.kt
@@ -13,6 +13,7 @@
  */
 package com.ichi2.anki
 
+import android.text.InputType
 import androidx.core.content.edit
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
@@ -21,6 +22,7 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.android.material.textfield.TextInputEditText
 import com.ichi2.anki.libanki.Collection
 import com.ichi2.anki.preferences.sharedPrefs
 import com.ichi2.anki.tests.InstrumentedTest
@@ -40,6 +42,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.concurrent.TimeUnit
+import kotlin.time.Duration.Companion.seconds
 
 @RunWith(AndroidJUnit4::class)
 class ReviewerFragmentTest : InstrumentedTest() {
@@ -118,6 +121,55 @@ class ReviewerFragmentTest : InstrumentedTest() {
         ensureAnswerButtonsAreDisplayed()
     }
 
+    @Test
+    fun testSelectedKeyboardType() {
+        setNewReviewer()
+        closeGetStartedScreenIfExists()
+        closeBackupCollectionDialogIfExists()
+
+        val inputTypeNumber =
+            InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL or InputType.TYPE_NUMBER_FLAG_SIGNED
+        val inputTypeText = InputType.TYPE_CLASS_TEXT
+
+        val testValues: List<Pair<String, Int>> =
+            listOf(
+                "123" to inputTypeNumber,
+                "-123.45" to inputTypeNumber,
+                "123.45" to inputTypeNumber,
+                "123,45" to inputTypeNumber,
+                "<b>123</b>" to inputTypeNumber,
+                "AnkiDroid" to inputTypeText,
+                "123abc" to inputTypeText,
+                "" to inputTypeText,
+            )
+
+        testValues.forEachIndexed { index, (typedAnswer, _) ->
+            addTypedAnswerNote(answer = typedAnswer).firstCard(col).update {
+                did = col.decks.id("Default$index")
+            }
+        }
+
+        // Check decks after adding all notes to ensure that the deck list is updated with the new cards
+        testValues.forEachIndexed { index, (_, expectedInputType) ->
+            // Ensures that we are in the deckpicker screen to make reviewDeckWithName work
+            if (index > 0) onView(withId(R.id.back_button)).perform(click())
+            checkInputType(expectedInputType, index)
+        }
+    }
+
+    fun checkInputType(
+        expectedInputType: Int,
+        index: Int,
+    ) {
+        reviewDeckWithName("Default$index")
+        ensureKeyboardIsDisplayed()
+        onView(withId(R.id.type_answer_edit_text)).check { view, _ ->
+            val editText = view as TextInputEditText
+            val inputType = editText.inputType
+            assertThat(inputType, equalTo(expectedInputType))
+        }
+    }
+
     private fun clickShowAnswerAndAnswerGood() {
         clickShowAnswer()
         ensureAnswerButtonsAreDisplayed()
@@ -126,6 +178,14 @@ class ReviewerFragmentTest : InstrumentedTest() {
 
     private fun clickShowAnswer() {
         onView(withId(R.id.show_answer)).perform(click())
+    }
+
+    private fun ensureKeyboardIsDisplayed() {
+        onView(withId(R.id.type_answer_edit_text)).checkWithTimeout(
+            matches(isDisplayed()),
+            100,
+            30.seconds.inWholeMilliseconds,
+        )
     }
 
     private fun ensureAnswerButtonsAreDisplayed() {

AnkiDroid/src/androidTest/java/com/ichi2/anki/tests/InstrumentedTest.kt
@@ -190,6 +190,11 @@ abstract class InstrumentedTest {
         return n
     }
 
+    internal fun addTypedAnswerNote(
+        front: String = "Front",
+        answer: String = "Answer",
+    ): Note = addNoteUsingNoteTypeName("Basic (type in the answer)", front, answer)
+
     @DuplicatedCode("This is copied from RobolectricTest. This will be refactored into a shared library later")
     fun addClozeNote(
         text: String,

AnkiDroid/src/main/java/com/ichi2/utils/HtmlUtils.kt
@@ -15,6 +15,7 @@
  */
 package com.ichi2.utils
 
+import androidx.core.text.HtmlCompat
 import com.ichi2.anki.common.utils.htmlEncode
 
 object HtmlUtils {
@@ -30,3 +31,6 @@ object HtmlUtils {
 
     fun escape(html: String): String = html.htmlEncode()
 }
+
+/** Removes HTML tags from a string */
+fun stripHtml(html: String): String = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_LEGACY).toString()

commit 184d7a695a2ddafebb58b1a53bddfffb240e8040

fix(study-screen): type in answer showing suggestions
caused by 14a076980d142b638420e4e35204b1801192de63

AnkiDroid/src/main/java/com/ichi2/anki/ui/windows/reviewer/ReviewerFragment.kt
@@ -309,7 +309,7 @@ class ReviewerFragment :
         if (stripHtml(typeAnswer.expectedAnswer).matches(Regex("^-?\\d+([.,]\\d*)?$"))) {
             InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL or InputType.TYPE_NUMBER_FLAG_SIGNED
         } else {
-            InputType.TYPE_CLASS_TEXT
+            InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
         }
 
     private fun resetZoom() {

AnkiDroid/src/androidTest/java/com/ichi2/anki/ReviewerFragmentTest.kt
@@ -129,7 +129,7 @@ class ReviewerFragmentTest : InstrumentedTest() {
 
         val inputTypeNumber =
             InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL or InputType.TYPE_NUMBER_FLAG_SIGNED
-        val inputTypeText = InputType.TYPE_CLASS_TEXT
+        val inputTypeText = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
 
         val testValues: List<Pair<String, Int>> =
             listOf(