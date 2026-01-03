ankidroid/Anki-Android

commit 036707c5a6d47f20250a8de43d154c8893fea335

fix(tts): MissingResourceException in `getISO3Language`
> MissingResourceException – Throws MissingResourceException if three-letter language abbreviation is not available for this locale.

`iso3Code` is our extension which returns null
 in this situation

Fixes 19813

AnkiDroid/src/main/java/com/ichi2/anki/ReadText.kt
@@ -26,6 +26,7 @@ import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AlertDialog
 import com.ichi2.anki.cardviewer.SingleCardSide
 import com.ichi2.anki.common.annotations.NeedsTest
+import com.ichi2.anki.i18n.iso3Code
 import com.ichi2.anki.libanki.Card
 import com.ichi2.anki.libanki.Collection
 import com.ichi2.anki.libanki.DeckId
@@ -121,11 +122,18 @@ object ReadText {
             val localeMappings: List<Pair<String, CharSequence>> =
                 mutableListOf<Pair<String, String>>().apply {
                     add(Pair(NO_TTS, res.getString(R.string.tts_no_tts))) // add option: "no tts"
-                    addAll(
+                    val (validLocales, invalidLocales) =
                         availableLocales()
                             .sortedWith(compareBy { it.displayName })
-                            .map { Pair(it.isO3Language, it.displayName) },
-                    )
+                            .map { Pair(it.iso3Code, it.displayName) }
+                            // iso3Code returns null if invalid
+                            // we could work around this, but ReadText is deprecated
+                            .partition { it.first != null }
+
+                    if (invalidLocales.isNotEmpty()) {
+                        Timber.w("%d invalid languages", invalidLocales.size)
+                    }
+                    addAll(validLocales.map { Pair(it.first!!, it.second) })
                 }
             Timber.i("showing 'select language' dialog")
             dialog

commit 6f32397a50df234640db57ca0f8bb6e35c48dcc6

fix(legacy-tts): fix most languages
Any language with a 'country' no longer worked
as I used 'iso3Code', which also included a country

Tested with albanian: `sq-al` => `sqi`

Broken in 036707c5a6d47f20250a8de43d154c8893fea335

In the bad commit, I tested Arabic,
 one of the very few languages which are not affected

Fixes 19996

AnkiDroid/src/main/java/com/ichi2/anki/ReadText.kt
@@ -26,7 +26,7 @@ import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AlertDialog
 import com.ichi2.anki.cardviewer.SingleCardSide
 import com.ichi2.anki.common.annotations.NeedsTest
-import com.ichi2.anki.i18n.iso3Code
+import com.ichi2.anki.i18n.getIso3LanguageOrNull
 import com.ichi2.anki.libanki.Card
 import com.ichi2.anki.libanki.Collection
 import com.ichi2.anki.libanki.DeckId
@@ -125,8 +125,8 @@ object ReadText {
                     val (validLocales, invalidLocales) =
                         availableLocales()
                             .sortedWith(compareBy { it.displayName })
-                            .map { Pair(it.iso3Code, it.displayName) }
-                            // iso3Code returns null if invalid
+                            .map { Pair(it.getIso3LanguageOrNull(), it.displayName) }
+                            // getIso3LanguageOrNull returns null if invalid
                             // we could work around this, but ReadText is deprecated
                             .partition { it.first != null }
 

AnkiDroid/src/main/java/com/ichi2/anki/i18n/LocaleCodes.kt
@@ -100,3 +100,29 @@ val Locale.iso3Code: String?
             // example: MissingResourceException: Couldn't find 3-letter language code for cz
             null
         }
+
+/**
+ * Returns a three-letter abbreviation of this locale's language, or `null` if a three-letter
+ * language abbreviation is not available for this locale.
+ *
+ * WARN: This is lossy, as the two-letter-code was usable.
+ *
+ * If the language matches an ISO 639-1 two-letter code, the corresponding ISO 639-2/T three-letter
+ * lowercase code is returned.
+ *
+ * The ISO 639-2 language codes can be found on-line, see "Codes for the Representation of Names of
+ * Languages Part 2: Alpha-3 Code".
+ *
+ * If the locale specifies a three-letter language, the language is returned as is.
+ *
+ * If the locale does not specify a language the empty string is returned.
+ */
+fun Locale.getIso3LanguageOrNull(): String? =
+    try {
+        // use Java-style 'get' methods; `val` properties are badly named: 'isO3Language'
+        this.getISO3Language()
+    } catch (_: Exception) {
+        // MissingResourceException can be thrown, in which case return null
+        // example: MissingResourceException: Couldn't find 3-letter language code for cz
+        null
+    }