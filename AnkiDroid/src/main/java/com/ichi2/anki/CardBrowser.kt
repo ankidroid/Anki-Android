afc2a49ca9fbefcd84122c300ae924d54735ae7b
David Allison <62114487+david-allison@users.noreply.github.com>
2026-01-27 10:44:03 +0000

    feat(libanki): sync `findCards/Notes` with upstream
    * Renamed original implementations:
      * findCards => findCardsLegacy
      * findNotes => findNotesLegacy
      * BuiltinSortKind => LegacyBuiltinSortKind
    * Adds:
      * BuiltinColumnSortKind
      * collection.buildSortMode
    * updates `FinderTest` and `SortOrderTest`
    
    All but one method was updated to the non-legacy
     implementation due to:
    
    * only `query` was set
    * a valid `SortOrder` was provided (typically `NoOrdering`)
    * test-only method
    
    https://github.com/ankitects/anki/blob/6247c92dcce0204f0e666b9e9e5355d2a15649d6/pylib/anki/collection.py#L634-L712

--- AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.kt
@@ -1362,6 +1362,7 @@ open class CardBrowser :
     }
 }
 
+@Suppress("deprecation") // legacy calls to findCards/findNotes
 suspend fun searchForRows(
     query: String,
     order: SortOrder,
@@ -1369,8 +1370,8 @@ suspend fun searchForRows(
 ): BrowserRowCollection =
     withCol {
         when (cardsOrNotes) {
-            CARDS -> findCards(query, order)
-            NOTES -> findNotes(query, order)
+            CARDS -> findCardsLegacy(query, order)
+            NOTES -> findNotesLegacy(query, order)
         }
     }.let { ids ->
         BrowserRowCollection(cardsOrNotes, ids.map { CardOrNoteId(it) }.toMutableList())
