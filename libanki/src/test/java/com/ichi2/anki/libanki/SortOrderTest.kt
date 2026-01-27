56f3387342f2d733a0ea3278604a9f60cb078222
David Allison <62114487+david-allison@users.noreply.github.com>
2026-01-27 09:29:22 +0000

    docs(libanki): improve SortOrder string representation

---
+++ libanki/src/test/java/com/ichi2/anki/libanki/SortOrderTest.kt
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (c) 2026 David Allison <davidallisongithub@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ichi2.anki.libanki
+
+import com.ichi2.anki.libanki.SortOrder.AfterSqlOrderBy
+import com.ichi2.anki.libanki.SortOrder.BuiltinSortKind
+import com.ichi2.anki.libanki.SortOrder.NoOrdering
+import com.ichi2.anki.libanki.SortOrder.UseCollectionOrdering
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class SortOrderTest {
+    @Test
+    fun `NoOrdering toString`() {
+        assertEquals("NoOrdering", NoOrdering().toString())
+    }
+
+    @Test
+    fun `UseCollectionOrdering toString`() {
+        assertEquals("UseCollectionOrdering", UseCollectionOrdering().toString())
+    }
+
+    @Test
+    fun `AfterSqlOrderBy toString`() {
+        assertEquals(
+            "AfterSqlOrderBy(customOrdering=c.ivl asc, c.due desc)",
+            AfterSqlOrderBy("c.ivl asc, c.due desc").toString(),
+        )
+    }
+
+    @Test
+    fun `BuiltinSortKind toString`() {
+        assertEquals(
+            "BuiltinSortKind(value=cardDue, reverse=true)",
+            BuiltinSortKind("cardDue", reverse = true).toString(),
+        )
+    }
+}
