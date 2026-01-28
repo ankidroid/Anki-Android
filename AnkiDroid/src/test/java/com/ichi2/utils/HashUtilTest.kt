7724b61006d866aa9f5fd8113798ab3f334eb347
Kota-Jagadeesh <kota.jagadesh123@gmail.com>
2026-01-28 10:28:01 +0530

    Add unit tests for HashUtil and include license header

--- AnkiDroid/src/test/java/com/ichi2/utils/HashUtilTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/utils/HashUtilTest.kt
@@ -1,7 +1,20 @@
-package com.ichi2.testutils
+/*
+ * Copyright (c) 2026 Kota Jagadeesh <kota.jagadesh123@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.ichi2.utils
 
-import com.ichi2.utils.HashUtil
-import com.ichi2.utils.hash
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.notNullValue
@@ -13,7 +26,7 @@ class HashUtilTest {
         val size = 10
         val set = HashUtil.hashSetInit<String>(size)
         assertThat(set, notNullValue())
-        // verifyy we can add elements
+        // verify we can add elements
         set.add("test")
         assertThat(set.size, `is`(1))
     }
