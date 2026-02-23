be83e8a23b92c2de5cd597529fe48ec828c013fa
PrathamDevX <mainprathamchawda1@gmail.com>
2026-02-23 21:52:17 +0530

    Fix Android Studio unused import directive warnings in PreviewerViewModel.kt

--- AnkiDroid/src/main/java/com/ichi2/anki/previewer/PreviewerViewModel.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/previewer/PreviewerViewModel.kt
@@ -17,7 +17,6 @@ package com.ichi2.anki.previewer
 
 import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.viewModelScope
 import anki.collection.OpChanges
 import com.ichi2.anki.CollectionManager.withCol
 import com.ichi2.anki.Flag
@@ -34,7 +33,6 @@ import com.ichi2.anki.pages.AnkiServer
 import com.ichi2.anki.reviewer.CardSide
 import com.ichi2.anki.servicelayer.MARKED_TAG
 import com.ichi2.anki.servicelayer.NoteService
-import com.ichi2.anki.utils.ext.collectIn
 import com.ichi2.anki.utils.ext.flag
 import com.ichi2.anki.utils.ext.require
 import com.ichi2.anki.utils.ext.setUserFlagForCards
