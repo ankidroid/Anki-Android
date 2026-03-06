054b072b73ce4e92e7371acfeda01060cfd1a3c4
unknown <aadityashah2807@gmail.com>
2026-03-06 21:10:04 +0530

    Upgraded AGP version to 9.0.1
    This commit upgrades the AGP version from 8.13.2 to 9.0.1 with required
    changes in gradle files according to the official release notes provided
    at https://developer.android.com/build/releases/agp-9-0-0-release-notes.

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -48,7 +48,7 @@ android {
         }
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
