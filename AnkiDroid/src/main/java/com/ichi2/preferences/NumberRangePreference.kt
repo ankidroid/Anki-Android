ankidroid/Anki-Android

commit f157e172f2158c69f71f070fa9c9e0651072d8b0

fix: use standard res-auto namespace for app attributes
fix NumberRangePreference

remove XML_CUSTOM_NAMESPACE

used RES_AUTO_NAMESPACE everywhere its needed and change app1 to app

Remove RES_AUTO_NAMESPACE and instead used declare-styleable

fix spacing in attrs.xml

AnkiDroid/src/main/java/com/ichi2/preferences/NumberRangePreference.kt
@@ -20,7 +20,7 @@ import android.text.InputFilter.LengthFilter
 import android.text.InputType
 import android.util.AttributeSet
 import android.view.View
-import com.ichi2.anki.AnkiDroidApp
+import com.ichi2.anki.R
 import timber.log.Timber
 
 @Suppress(
@@ -115,7 +115,9 @@ open class NumberRangePreference :
      * This method should only be called once from the constructor.
      */
     private fun getMinFromAttributes(attrs: AttributeSet?): Int =
-        attrs?.getAttributeIntValue(AnkiDroidApp.XML_CUSTOM_NAMESPACE, "min", 0) ?: 0
+        context.obtainStyledAttributes(attrs, R.styleable.NumberRangePreference).use {
+            it.getInt(R.styleable.NumberRangePreference_min, 0)
+        }
 
     /**
      * Returns the value of the max attribute, or its default value if not specified
@@ -124,8 +126,9 @@ open class NumberRangePreference :
      * This method should only be called once from the constructor.
      */
     private fun getMaxFromAttributes(attrs: AttributeSet?): Int =
-        attrs?.getAttributeIntValue(AnkiDroidApp.XML_CUSTOM_NAMESPACE, "max", Int.MAX_VALUE)
-            ?: Int.MAX_VALUE
+        context.obtainStyledAttributes(attrs, R.styleable.NumberRangePreference).use {
+            it.getInt(R.styleable.NumberRangePreference_max, Int.MAX_VALUE)
+        }
 
     /**
      * Update settings to only allow integer input and set the maximum number of digits allowed in the text field based

AnkiDroid/src/main/java/com/ichi2/anki/AnkiDroidApp.kt
@@ -396,7 +396,6 @@ open class AnkiDroidApp :
         /** Running under instrumentation. a "/androidTest" directory will be created which contains a test collection  */
         @Suppress("ktlint:standard:property-naming")
         var INSTRUMENTATION_TESTING = false
-        const val XML_CUSTOM_NAMESPACE = "http://arbitrary.app.namespace/com.ichi2.anki"
         const val ANDROID_NAMESPACE = "http://schemas.android.com/apk/res/android"
 
         // Tag for logging messages.

AnkiDroid/src/main/java/com/ichi2/preferences/NumberRangePreferenceCompat.kt
@@ -26,7 +26,6 @@ import android.widget.EditText
 import androidx.core.content.withStyledAttributes
 import androidx.preference.EditTextPreference
 import androidx.preference.EditTextPreferenceDialogFragmentCompat
-import com.ichi2.anki.AnkiDroidApp
 import com.ichi2.anki.R
 import com.ichi2.anki.common.annotations.NeedsTest
 import com.ichi2.anki.utils.getFormattedStringOrPlurals
@@ -49,8 +48,10 @@ open class NumberRangePreferenceCompat
             private set
 
         init {
-            min = attrs?.getAttributeIntValue(AnkiDroidApp.XML_CUSTOM_NAMESPACE, "min", 0) ?: 0
-            max = attrs?.getAttributeIntValue(AnkiDroidApp.XML_CUSTOM_NAMESPACE, "max", Int.MAX_VALUE) ?: Int.MAX_VALUE
+            context.withStyledAttributes(attrs, R.styleable.NumberRangePreferenceCompat) {
+                min = getInt(R.styleable.NumberRangePreferenceCompat_min, 0)
+                max = getInt(R.styleable.NumberRangePreferenceCompat_max, Int.MAX_VALUE)
+            }
             defaultValue = attrs?.getAttributeValue("http://schemas.android.com/apk/res/android", "defaultValue")
 
             context.withStyledAttributes(attrs, R.styleable.CustomPreference) {
@@ -124,7 +125,7 @@ open class NumberRangePreferenceCompat
         private fun getDefaultValue(): Int {
             return try {
                 return defaultValue?.toInt() ?: min
-            } catch (e: Exception) {
+            } catch (_: Exception) {
                 min
             }
         }

AnkiDroid/src/main/java/com/ichi2/preferences/StepsPreference.kt
@@ -20,7 +20,6 @@ import android.content.Context
 import android.text.InputType
 import android.util.AttributeSet
 import android.view.View
-import com.ichi2.anki.AnkiDroidApp
 import com.ichi2.anki.R
 import com.ichi2.anki.common.utils.ext.stringIterable
 import com.ichi2.anki.showThemedToast
@@ -105,8 +104,9 @@ class StepsPreference :
     }
 
     private fun getAllowEmptyFromAttributes(attrs: AttributeSet?): Boolean =
-        attrs?.getAttributeBooleanValue(AnkiDroidApp.XML_CUSTOM_NAMESPACE, "allowEmpty", true)
-            ?: true
+        context.obtainStyledAttributes(attrs, R.styleable.StepsPreference).use {
+            it.getBoolean(R.styleable.StepsPreference_allowEmpty, true)
+        }
 
     companion object {
         /**

AnkiDroid/src/main/res/values/attrs.xml
@@ -25,7 +25,8 @@
 
     <!-- Custom preferences -->
     <attr name="allowEmpty" format="boolean" />
-
+    <attr name="min" format="integer" />
+    <attr name="max" format="integer" />
 
     <declare-styleable name="CustomPreference">
         <!-- Whether the preference should automatically set its summary to the value saved for the
@@ -35,6 +36,20 @@
              The placeholder is replaced by the preference value, and it must be only one placeholder.
              If the preference value is updated, the summary is automatically updated as well. -->
         <attr name="summaryFormat" format="string"/>
+    </declare-styleable>
+
+    <declare-styleable name="NumberRangePreference">
+        <attr name="min"/>
+        <attr name="max"/>
+    </declare-styleable>
+
+    <declare-styleable name="StepsPreference">
+        <attr name="allowEmpty"/>
+    </declare-styleable>
+
+    <declare-styleable name="NumberRangePreferenceCompat">
+        <attr name="min"/>
+        <attr name="max"/>
     </declare-styleable>
 
     <declare-styleable name="HtmlHelpPreference">

AnkiDroid/src/main/res/xml/cram_deck_options.xml
@@ -16,7 +16,7 @@
 -->
 
 <PreferenceScreen xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:app="http://arbitrary.app.namespace/com.ichi2.anki" >
+    xmlns:app="http://schemas.android.com/apk/res-auto" >
 
     <PreferenceCategory android:title="@string/deck_conf_cram_filter" >
         <com.ichi2.preferences.AutoFocusEditTextPreference

AnkiDroid/src/main/res/xml/preferences_advanced.xml
@@ -22,14 +22,14 @@
 
 <!-- Advanced Preferences -->
 <PreferenceScreen xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:app1="http://schemas.android.com/apk/res-auto"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
     android:title="@string/pref_cat_advanced"
     android:key="@string/pref_advanced_screen_key">
         <EditTextPreference
             android:defaultValue="/sdcard/AnkiDroid"
             android:key="@string/pref_ankidroid_directory_key"
             android:title="@string/col_path"
-            app1:useSimpleSummaryProvider="true"/>
+            app:useSimpleSummaryProvider="true"/>
         <Preference
             android:summary="@string/reset_languages_summ"
             android:title="@string/reset_languages"

AnkiDroid/src/main/res/xml/preferences_backup_limits.xml
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="utf-8"?>
 <PreferenceScreen
     xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:app="http://arbitrary.app.namespace/com.ichi2.anki"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
     android:title="@string/button_backup"
     android:key="@string/pref_backups_screen_key">
     <!-- Includes modified text from `TR.preferencesBackupExplanation()`

commit 7c82ee6866f2b5c74b60673d0da17074c3ee1ff1

fix(deck-options): crash on API 30 and below
Introduced by f157e172f2158c69f71f070fa9c9e0651072d8b0

TypedArray crashes when cast to AutoCloseable on API 30 or below

Fixes 20456

AnkiDroid/src/main/java/com/ichi2/preferences/NumberRangePreference.kt
@@ -21,6 +21,7 @@ import android.text.InputType
 import android.util.AttributeSet
 import android.view.View
 import com.ichi2.anki.R
+import com.ichi2.anki.utils.ext.usingStyledAttributes
 import timber.log.Timber
 
 @Suppress(
@@ -115,8 +116,8 @@ open class NumberRangePreference :
      * This method should only be called once from the constructor.
      */
     private fun getMinFromAttributes(attrs: AttributeSet?): Int =
-        context.obtainStyledAttributes(attrs, R.styleable.NumberRangePreference).use {
-            it.getInt(R.styleable.NumberRangePreference_min, 0)
+        context.usingStyledAttributes(attrs, R.styleable.NumberRangePreference) {
+            getInt(R.styleable.NumberRangePreference_min, 0)
         }
 
     /**
@@ -126,8 +127,8 @@ open class NumberRangePreference :
      * This method should only be called once from the constructor.
      */
     private fun getMaxFromAttributes(attrs: AttributeSet?): Int =
-        context.obtainStyledAttributes(attrs, R.styleable.NumberRangePreference).use {
-            it.getInt(R.styleable.NumberRangePreference_max, Int.MAX_VALUE)
+        context.usingStyledAttributes(attrs, R.styleable.NumberRangePreference) {
+            getInt(R.styleable.NumberRangePreference_max, Int.MAX_VALUE)
         }
 
     /**