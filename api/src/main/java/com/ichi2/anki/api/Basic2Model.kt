399699e7e7c91f90630b232a9d3da751f0968825
Begarudev <begarudev@gmail.com>
2026-03-30 18:44:14 +0530

    chore(api): add copyright headers and fix BasicModel inconsistencies
    - Add LGPL copyright headers to BasicModel.kt, Basic2Model.kt,
      ApiUtilsTest.kt (tracked in #8659)
    - Convert BasicModel from class with companion object to object,
      matching the pattern used by Basic2Model
    - Change BasicModel.FIELDS from var to val to prevent external
      reassignment of the field names array

--- api/src/main/java/com/ichi2/anki/api/Basic2Model.kt
+++ api/src/main/java/com/ichi2/anki/api/Basic2Model.kt
@@ -1,4 +1,18 @@
-//noinspection MissingCopyrightHeader #8659
+/*
+ * Copyright (c) 2022 Sanjaykumar Sargam <sargamsanjaykumar@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.ichi2.anki.api
 
 /**
@@ -20,15 +34,15 @@ internal object Basic2Model {
     internal val AFMT =
         arrayOf(
             """{{FrontSide}}
-    
+
     |<hr id="answer">
-    
+
     |{{Back}}
             """.trimMargin(),
             """{{FrontSide}}
-    
+
     |<hr id="answer">
-    
+
     |{{Front}}
             """.trimMargin(),
         )
--- api/src/main/java/com/ichi2/anki/api/BasicModel.kt
+++ api/src/main/java/com/ichi2/anki/api/BasicModel.kt
@@ -1,32 +1,44 @@
-//noinspection MissingCopyrightHeader #8659
+/*
+ * Copyright (c) 2022 Sanjaykumar Sargam <sargamsanjaykumar@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.ichi2.anki.api
 
 /**
  * Definitions of the basic model
  */
-internal class BasicModel {
-    companion object {
-        @JvmField // required for API
-        @Suppress("ktlint:standard:property-naming")
-        var FIELDS = arrayOf("Front", "Back")
+internal object BasicModel {
+    @JvmField // required for API
+    @Suppress("ktlint:standard:property-naming")
+    val FIELDS = arrayOf("Front", "Back")
 
-        // List of card names that will be used in AnkiDroid (one for each direction of learning)
-        @JvmField // required for API
-        val CARD_NAMES = arrayOf("Card 1")
+    // List of card names that will be used in AnkiDroid (one for each direction of learning)
+    @JvmField // required for API
+    val CARD_NAMES = arrayOf("Card 1")
 
-        // Template for the question of each card
-        @JvmField // required for API
-        val QFMT = arrayOf("{{Front}}")
+    // Template for the question of each card
+    @JvmField // required for API
+    val QFMT = arrayOf("{{Front}}")
 
-        @JvmField // required for API
-        val AFMT =
-            arrayOf(
-                """{{FrontSide}}
-        
+    @JvmField // required for API
+    val AFMT =
+        arrayOf(
+            """{{FrontSide}}
+
         |<hr id="answer">
-        
+
         |{{Back}}
-                """.trimMargin(),
-            )
-    }
+            """.trimMargin(),
+        )
 }
