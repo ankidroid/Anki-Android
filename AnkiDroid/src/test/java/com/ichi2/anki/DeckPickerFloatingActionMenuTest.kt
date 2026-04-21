e0ac7355e222a0f8e63986ebc0b047d2763f1015
MandeepT19 <mandeepthakkar19@gmail.com>
2026-04-22 01:35:57 +0530

    test(DeckPicker): reimplement FAB tests as Robolectric unit tests

--- AnkiDroid/src/test/java/com/ichi2/anki/DeckPickerFloatingActionMenuTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/DeckPickerFloatingActionMenuTest.kt
@@ -16,16 +16,48 @@
 
 package com.ichi2.anki
 
-import com.ichi2.anki.common.annotations.NeedsTest
-import com.ichi2.testutils.EmptyApplication
-import org.junit.experimental.categories.Category
+import android.view.View
+import androidx.core.content.edit
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.anki.preferences.sharedPrefs
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 
 /**
  * Test for [DeckPickerFloatingActionMenu]
  */
-// @RunWith(AndroidJUnit4::class)
-@Config(application = EmptyApplication::class)
-@Category(EmptyApplicationCategory::class)
-@NeedsTest("reimplement: doubleTapAddsNote; singleTapTogglesFab")
-class DeckPickerFloatingActionMenuTest
+@RunWith(AndroidJUnit4::class)
+@Config
+class DeckPickerFloatingActionMenuTest : RobolectricTest() {
+    override fun setUp() {
+        super.setUp()
+        targetContext.sharedPrefs().edit {
+            putBoolean(IntroductionActivity.INTRODUCTION_SLIDES_SHOWN, true)
+        }
+    }
+
+    @Test
+    fun showFloatingActionButtonMakesFabVisible() {
+        val scenario = ActivityScenario.launch(DeckPicker::class.java)
+        scenario.onActivity { deckPicker ->
+            deckPicker.floatingActionMenu.hideFloatingActionButton()
+            deckPicker.floatingActionMenu.showFloatingActionButton()
+            assertEquals(View.VISIBLE, deckPicker.findViewById<View>(R.id.fab_main).visibility)
+        }
+        scenario.close()
+    }
+
+    @Test
+    fun hideFloatingActionButtonMakesFabGone() {
+        val scenario = ActivityScenario.launch(DeckPicker::class.java)
+        scenario.onActivity { deckPicker ->
+            deckPicker.floatingActionMenu.showFloatingActionButton()
+            deckPicker.floatingActionMenu.hideFloatingActionButton()
+            assertEquals(View.GONE, deckPicker.findViewById<View>(R.id.fab_main).visibility)
+        }
+        scenario.close()
+    }
+}
