e73fd7ec84d3c8d7908ef9eea365c186fcdefda9
Song Eric Yu Li <ericli3690@gmail.com>
2026-05-25 01:00:54 -0400

    feat(reminders): render ScheduleRemindersFragment differently based on host
    ScheduleReminders can be hosted in multiple places, each of which presents their own unique UI challenges:
    
    1. Long-pressing a deck from DeckPicker: On small screens, there is no suitable activity currently available, and so we should launch one to host ScheduleRemindersFragment in the form of SingleFragmentActivity. On large screens, there MAY be a right-side panel available (it is hidden in the edge case where the collection is empty); the UI looks cleanest if it is used.
    2. Clicking on the "bell" icon from StudyOptionsFragment: On small screens, StudyOptionsActivity is currently active, and so it should be used as the host rather than launching a new activity. However, it manages an external toolbar and so using an internal toolbar would not work. We need to latch onto the external support action bar and modify it. On large screens, the right-side panel is available similar to option 1 if the collection is non-empty.
    3. The screen may be launched from Settings, in which case the user is inspecting all reminders across all decks. Here, it should match the style of the other Settings Preferences screens, i.e. a collapsing toolbar, and should align whether on a small screen or large screen.
    
    In my opinion, the cleanest way to handle this is to enumerate the possible hosts ScheduleReminders can have via FragmentHost, define what the ToolbarType should be for each host, and then modify the toolbar and styling appropriately. This keeps all logic centralized within ScheduleRemindersFragment and maintains a single source of truth.
    
    Additionally, due to an issue with `fitsSystemWindows`, we sometimes need to set the upper padding programmatically. See the TODO comment in this commit. Also, as edge-to-edge is gradually implemented, fitsSystemWindows will stop being necessary and some of the UI code here will be not needed. This is the case, for instance, in DeckPicker. Hence we must also record whether a host has edge-to-edge enabled. See the TODO comment.
    
    Since ReminderTroubleshootingFragment is launched from ScheduleRemindersFragment, it also has similar UI issues and also needs to be aware of its host.
    
    Proper launching of ReminderTroubleshootingFragment is also implemented. A subtitle style is added.

--- AnkiDroid/src/main/java/com/ichi2/anki/reviewreminders/ScheduleRemindersFragment.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/reviewreminders/ScheduleRemindersFragment.kt
@@ -19,13 +19,18 @@ package com.ichi2.anki.reviewreminders
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
+import androidx.annotation.IdRes
 import androidx.core.view.MenuProvider
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat.Type.displayCutout
+import androidx.core.view.WindowInsetsCompat.Type.statusBars
 import androidx.core.view.isVisible
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.commit
@@ -36,21 +41,24 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import com.ichi2.anki.CollectionManager.withCol
 import com.ichi2.anki.R
-import com.ichi2.anki.SingleFragmentActivity
 import com.ichi2.anki.canUserAccessDeck
 import com.ichi2.anki.databinding.FragmentScheduleRemindersBinding
 import com.ichi2.anki.launchCatchingTask
 import com.ichi2.anki.libanki.DeckId
+import com.ichi2.anki.requireAnkiActivity
 import com.ichi2.anki.reviewreminders.AddEditReminderDialog.Companion.registerAddEditReminderHandler
+import com.ichi2.anki.runCatching
 import com.ichi2.anki.services.AlarmManagerService
 import com.ichi2.anki.snackbar.BaseSnackbarBuilderProvider
 import com.ichi2.anki.snackbar.SnackbarBuilder
 import com.ichi2.anki.snackbar.showSnackbar
+import com.ichi2.anki.utils.ConfigAwareSingleFragmentActivity
 import com.ichi2.anki.utils.ext.getParcelableCompat
 import com.ichi2.anki.utils.ext.showDialogFragment
 import com.ichi2.anki.withProgress
 import dev.androidbroadcast.vbpd.viewBinding
 import kotlinx.coroutines.launch
+import kotlinx.parcelize.Parcelize
 import timber.log.Timber
 
 /**
@@ -60,12 +68,108 @@ class ScheduleRemindersFragment :
     Fragment(R.layout.fragment_schedule_reminders),
     BaseSnackbarBuilderProvider {
     /**
+     * Possible toolbar types that can be displayed with this fragment.
+     * @see FragmentHost
+     */
+    @Parcelize
+    enum class ToolbarType : Parcelable {
+        /** For if this fragment is hosted within an external activity which handles its own toolbar. */
+        EXTERNAL,
+
+        /** For if the toolbar should look like the collapsible ones present in the Settings screen. */
+        INTERNAL_COLLAPSIBLE,
+
+        /** For if the toolbar should be simple, fragment-owned, and non-collapsible. */
+        INTERNAL_NON_COLLAPSIBLE,
+    }
+
+    /**
+     * Possible hosts of this fragment. Certain stylistic changes need to be made based on where this
+     * fragment is opened from / nested within.
+     *
+     * TODO: Implement edge-to-edge for Settings, StudyOptionsActivity, and ConfigAwareSingleFragmentActivity.
+     * Then, remove the supportsEdgeToEdge property below and test this fragment's UI behavior 1) on both small and wide screens,
+     * 2) with all app display themes, and 3) from all possible locations this fragment can be opened from. In particular,
+     * make sure there is no weird clipping of the collapsible toolbar content scrim when this fragment is opened from the Settings screen upon scrolling.
+     *
+     * @param containerId The XML ID of the container in which this fragment is hosted.
+     * @param toolbarType The type of toolbar to display for this fragment.
+     * @param supportsEdgeToEdge Whether the host of this fragment currently supports edge-to-edge rendering.
+     * The legacy fitsSystemWindows property is deprecated and should be migrated away from.
+     */
+    @Parcelize
+    enum class FragmentHost(
+        @IdRes val containerId: Int,
+        val toolbarType: ToolbarType,
+        val supportsEdgeToEdge: Boolean,
+    ) : Parcelable {
+        /**
+         * App-wide review reminders editing screen accessed via Settings.
+         * @see com.ichi2.anki.preferences.PreferencesActivity
+         */
+        SETTINGS(
+            containerId = R.id.settings_container,
+            toolbarType = ToolbarType.INTERNAL_COLLAPSIBLE,
+            supportsEdgeToEdge = false,
+        ),
+
+        /**
+         * Side-by-side view of a specific deck's review reminders on large screens.
+         * @see com.ichi2.anki.DeckPicker.tryShowScheduleRemindersPanel
+         */
+        STUDY_OPTIONS_FRAGMENT(
+            containerId = R.id.studyoptions_fragment,
+            toolbarType = ToolbarType.INTERNAL_NON_COLLAPSIBLE,
+            supportsEdgeToEdge = true,
+        ),
+
+        /**
+         * Full-screen view of a specific deck's review reminders on small screens when launched after
+         * viewing the study options screen.
+         * @see com.ichi2.anki.StudyOptionsActivity
+         */
+        STUDY_OPTIONS_FRAME(
+            containerId = R.id.studyoptions_frame,
+            toolbarType = ToolbarType.EXTERNAL,
+            supportsEdgeToEdge = false,
+        ),
+
+        /**
+         * Full-screen view of a specific deck's review reminders on small screens when launched
+         * via long-pressing a deck in the DeckPicker.
+         * @see com.ichi2.anki.deckpicker.DeckPickerViewModel.scheduleReviewReminders
+         */
+        STANDALONE_ACTIVITY(
+            containerId = R.id.fragment_container,
+            toolbarType = ToolbarType.INTERNAL_NON_COLLAPSIBLE,
+            supportsEdgeToEdge = false,
+        ),
+    }
+
+    /**
      * Whether this fragment has been opened to edit all review reminders or just a specific deck's reminders.
+     * If no arguments have been passed to this fragment, defaults to [ReviewReminderScope.Global]. This default is a used
+     * code path, as opening this fragment from the Settings screen is accomplished via a preference_headers android:fragment field
+     * rather than a custom on-click handler for the sake of simplicity.
+     *
      * @see ReviewReminderScope
      */
     private val scheduleRemindersScope: ReviewReminderScope by lazy {
-        requireArguments().getParcelableCompat<ReviewReminderScope>(ARGS_SCOPE)
+        (arguments ?: Bundle()).getParcelableCompat<ReviewReminderScope>(ARGS_SCOPE)
             ?: ReviewReminderScope.Global
+    }
+
+    /**
+     * The [FragmentHost] of this fragment, which determines how certain UI elements are displayed.
+     * If no arguments have been passed to this fragment, defaults to [FragmentHost.SETTINGS]. This default is a used
+     * code path, as opening this fragment from the Settings screen is accomplished via a preference_headers android:fragment field
+     * rather than a custom on-click handler for the sake of simplicity.
+     *
+     * @see FragmentHost
+     */
+    private val host: FragmentHost by lazy {
+        (arguments ?: Bundle()).getParcelableCompat<FragmentHost>(ARGS_HOST)
+            ?: FragmentHost.SETTINGS
     }
 
     private val binding by viewBinding(FragmentScheduleRemindersBinding::bind)
@@ -102,29 +206,24 @@ class ScheduleRemindersFragment :
     ) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Set up toolbar
-        reloadToolbarText()
-        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
-        requireActivity().addMenuProvider(
-            object : MenuProvider {
-                override fun onCreateMenu(
-                    menu: Menu,
-                    menuInflater: MenuInflater,
-                ) {
-                    menuInflater.inflate(R.menu.schedule_reminders, menu)
-                }
+        // Set up root layout insets if the host of this fragment does not support edge-to-edge
+        if (host.supportsEdgeToEdge) {
+            binding.rootLayout.fitsSystemWindows = false // No need for legacy insets behavior
+        } else {
+            binding.rootLayout.fitsSystemWindows = true // Legacy insets behavior to avoid overlapping the status bar
+            if (host.toolbarType == ToolbarType.INTERNAL_NON_COLLAPSIBLE) {
+                // The legacy behavior is broken for the non-collapsible toolbar, also implement a manual workaround for it
+                setNonCollapsibleToolbarInsets()
+            }
+        }
 
-                override fun onMenuItemSelected(menuItem: MenuItem): Boolean =
-                    when (menuItem.itemId) {
-                        R.id.action_troubleshoot -> {
-                            openTroubleshootingScreen()
-                            true
-                        }
-                        else -> false
-                    }
-            },
-            viewLifecycleOwner,
-        )
+        // Set up toolbar
+        when (host.toolbarType) {
+            ToolbarType.EXTERNAL -> setupExternalActivityToolbar()
+            ToolbarType.INTERNAL_COLLAPSIBLE -> setupInternalFragmentToolbar(isCollapsible = true)
+            ToolbarType.INTERNAL_NON_COLLAPSIBLE -> setupInternalFragmentToolbar(isCollapsible = false)
+        }
+
         // Set up add button
         binding.floatingActionButtonAdd.setOnClickListener { addReminder() }
 
@@ -177,17 +276,116 @@ class ScheduleRemindersFragment :
         }
     }
 
-    private fun reloadToolbarText() {
-        Timber.d("Reloading toolbar text")
-        binding.toolbar.title = getString(R.string.schedule_reminders_do_not_translate)
-        when (val scope = scheduleRemindersScope) {
-            is ReviewReminderScope.Global -> {}
-            is ReviewReminderScope.DeckSpecific ->
-                launchCatchingTask {
-                    binding.toolbar.subtitle = scope.getDeckName()
-                }
+    override fun onDestroyView() {
+        troubleshootingSnackbar?.dismiss()
+        super.onDestroyView()
+    }
+
+    private fun setupExternalActivityToolbar() {
+        binding.appbar.isVisible = false
+        requireAnkiActivity().apply {
+            addMenuProvider(
+                menuProvider,
+                viewLifecycleOwner,
+                Lifecycle.State.RESUMED,
+            )
+            retrieveSubtitle { subtitle ->
+                setToolbarText(title = "Review reminders", subtitle = subtitle)
+            }
+            invalidateMenu()
         }
     }
+
+    private fun setupInternalFragmentToolbar(isCollapsible: Boolean) {
+        binding.appbar.isVisible = true
+        val toolbar =
+            if (isCollapsible) {
+                binding.collapsingToolbarLayout.isVisible = true
+                binding.nonCollapsibleToolbar.isVisible = false
+                binding.toolbar // Use collapsible toolbar
+            } else {
+                binding.collapsingToolbarLayout.isVisible = false
+                binding.nonCollapsibleToolbar.isVisible = true
+                binding.nonCollapsibleToolbar // Use non-collapsible toolbar
+            }
+
+        toolbar.apply {
+            addMenuProvider(
+                menuProvider,
+                viewLifecycleOwner,
+                Lifecycle.State.RESUMED,
+            )
+
+            title = "Review reminders"
+            retrieveSubtitle { subtitle = it }
+
+            setNavigationOnClickListener {
+                if (parentFragmentManager.backStackEntryCount > 0) {
+                    parentFragmentManager.popBackStack()
+                } else {
+                    requireActivity().onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        }
+    }
+
+    /**
+     * The collapsible and non-collapsible toolbar are both located within the appbar.
+     * At most one is visible at a time (both are hidden if an external toolbar is being used).
+     * They must be nested within the same appbar because having more than one appbar causes issues with where
+     * the second one is rendered on the screen. If edge-to-edge is not implemented for the [FragmentHost] of this fragment
+     * yet, this fragment's layout and appbar has the fitsSystemWindows attribute set to ensure its
+     * child toolbar is rendered below the status bar.
+     *
+     * However, because the collapsible toolbar is before the non-collapsible toolbar in the layout file,
+     * it consumes the fitsSystemWindows inset first and does not pass any to the non-collapsible toolbar.
+     * Hence, we manually set the insets of the non-collapsible toolbar when it is visible
+     * via the modern setOnApplyWindowInsetsListener API. We cannot use this API for both the
+     * collapsible and non-collapsible toolbars and then omit fitsSystemWindows on the appbar. This is
+     * because doing so causes UI glitches within the status bar when the collapsible toolbar transitions
+     * between its expanded and collapsed states.
+     *
+     * This should only be used for the non-collapsible toolbar if edge-to-edge is not implemented on the host yet.
+     */
+    private fun setNonCollapsibleToolbarInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.rootLayout) { _, insets ->
+            val bars = insets.getInsets(statusBars() or displayCutout())
+            binding.appbar.updatePadding(left = bars.left, top = bars.top, right = bars.right)
+            insets
+        }
+    }
+
+    private fun retrieveSubtitle(setSubtitle: (String?) -> Unit) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            requireActivity().runCatching {
+                val subtitle =
+                    when (val scope = scheduleRemindersScope) {
+                        is ReviewReminderScope.Global -> null
+                        is ReviewReminderScope.DeckSpecific -> scope.getDeckName()
+                    }
+                setSubtitle(subtitle)
+            }
+        }
+    }
+
+    private val menuProvider: MenuProvider =
+        object : MenuProvider {
+            override fun onCreateMenu(
+                menu: Menu,
+                menuInflater: MenuInflater,
+            ) {
+                menuInflater.inflate(R.menu.schedule_reminders, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean =
+                when (menuItem.itemId) {
+                    R.id.action_troubleshoot -> {
+                        openTroubleshootingScreen()
+                        true
+                    }
+                    else -> false
+                }
+        }
 
     /**
      * Fetch all reminders from the database and put them into the RecyclerView.
@@ -371,9 +569,8 @@ class ScheduleRemindersFragment :
         troubleshootingSnackbar?.dismiss()
         parentFragmentManager.commit {
             replace(
-                R.id.fragment_container,
-                ReminderTroubleshootingFragment(),
-                SingleFragmentActivity.FRAGMENT_TAG,
+                host.containerId,
+                ReminderTroubleshootingFragment.newInstance(host),
             )
             addToBackStack(null)
         }
@@ -427,6 +624,11 @@ class ScheduleRemindersFragment :
         private const val ARGS_SCOPE = "scope"
 
         /**
+         * Arguments key for passing information about the [FragmentHost] to this fragment.
+         */
+        private const val ARGS_HOST = "host"
+
+        /**
          * Wrapper for database access in this fragment.
          * Shows an error dialog via [ReviewRemindersDatabase.checkDeserializationErrors] if there are deserialization errors.
          * Shows a progress dialog if database access takes a long time.
@@ -437,7 +639,11 @@ class ScheduleRemindersFragment :
             }
 
         /**
-         * Creates an intent to launch this fragment in a [SingleFragmentActivity].
+         * Creates an intent to launch this fragment in a [ConfigAwareSingleFragmentActivity].
+         * We should not manually handle orientation changes because the dialogs nested within this
+         * fragment are complicated and best handled by the default behavior. Hence, we must use the
+         * config-aware version of SingleFragmentActivity.
+         *
          * @param context
          * @param scope The editing scope of this fragment.
          * @return The new intent.
@@ -446,15 +652,39 @@ class ScheduleRemindersFragment :
             context: Context,
             scope: ReviewReminderScope,
         ): Intent =
-            SingleFragmentActivity
+            ConfigAwareSingleFragmentActivity
                 .getIntent(
                     context,
                     ScheduleRemindersFragment::class,
                     Bundle().apply {
                         putParcelable(ARGS_SCOPE, scope)
+                        putParcelable(ARGS_HOST, FragmentHost.STANDALONE_ACTIVITY)
                     },
                 ).apply {
                     Timber.i("launching ScheduleRemindersFragment for %s scope", scope)
                 }
+
+        /**
+         * Returns an instance of this fragment for a specific [ReviewReminderScope] and [FragmentHost].
+         * @param scope The editing scope of this fragment.
+         * @param host Where this fragment is embedded; determines some UI behaviour.
+         * @return The new fragment instance.
+         */
+        fun newInstance(
+            scope: ReviewReminderScope,
+            host: FragmentHost,
+        ): ScheduleRemindersFragment =
+            ScheduleRemindersFragment().apply {
+                arguments =
+                    Bundle().apply {
+                        putParcelable(ARGS_SCOPE, scope)
+                        putParcelable(ARGS_HOST, host)
+                    }
+                Timber.i(
+                    "Creating ScheduleRemindersFragment for %s scope, host=%s",
+                    scope,
+                    host.name,
+                )
+            }
     }
 }
