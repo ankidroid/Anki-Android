097c7cdc6c244d95d7a0f45943e61476cde34db1
David Allison <62114487+david-allison@users.noreply.github.com>
2026-06-13 11:55:45 +0100

    refactor: move CollectionManager to :anki-common
    This is required for our multimodule migration
    
    libs.kotlinx.coroutines.core is required for:
    CoroutineDispatcher, Dispatchers, runBlocking, withContext etc...
    
    * Part of 20737

--- anki-common/build.gradle.kts
+++ anki-common/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation(libs.androidx.sqlite.framework)
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.jakewharton.timber)
+    implementation(libs.kotlinx.coroutines.core)
 
     testFixturesImplementation(libs.androidx.test.core)
 }
