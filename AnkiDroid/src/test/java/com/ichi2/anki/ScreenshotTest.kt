d82c4f13e2df6368990dc7e5e41db6da3f2719d4
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2026-07-14 07:18:07 -0300

    test: add e-ink theme to screenshot tests

--- AnkiDroid/src/test/java/com/ichi2/anki/ScreenshotTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/ScreenshotTest.kt
@@ -54,7 +54,7 @@ abstract class ScreenshotTest : RobolectricTest() {
 
     var fileNamePrefix = ""
 
-    enum class ThemeConfig { LIGHT, PLAIN, DARK, BLACK }
+    enum class ThemeConfig { LIGHT, PLAIN, DARK, BLACK, EINK }
 
     enum class DeviceConfig { PHONE, TABLET, FOLDABLE, DESKTOP }
 
@@ -96,6 +96,7 @@ abstract class ScreenshotTest : RobolectricTest() {
         }
     }
 
+    // ./gradlew :AnkiDroid:recordRoborazziPlayDebug -Pscreenshot -Ptheme=black,plain
     protected open fun applyThemeConfig() {
         val isNightMode = theme == ThemeConfig.DARK || theme == ThemeConfig.BLACK
         if (isNightMode) {
@@ -109,6 +110,7 @@ abstract class ScreenshotTest : RobolectricTest() {
         when (theme) {
             ThemeConfig.LIGHT -> prefs.dayTheme = DayTheme.LIGHT
             ThemeConfig.PLAIN -> prefs.dayTheme = DayTheme.PLAIN
+            ThemeConfig.EINK -> prefs.dayTheme = DayTheme.EINK
             ThemeConfig.DARK -> prefs.nightTheme = NightTheme.DARK
             ThemeConfig.BLACK -> prefs.nightTheme = NightTheme.BLACK
         }
