a5770d4c275e7281f3652bab83b987a18f72da78
David Allison <62114487+david-allison@users.noreply.github.com>
2026-09-13 14:18:24 +0100

    Revert "build(deps): bump kotlin from 2.3.21 to 2.4.20"
    This reverts commit f00d2b99c65dc104ed669656bd559b43ac4781db.
    
    Not yet supported by CodeQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -127,7 +127,12 @@ subprojects {
 
             compilerOptions {
                 allWarningsAsErrors = fatalWarnings
-                val compilerArgs = mutableListOf<String>()
+                val compilerArgs = mutableListOf(
+                    // https://youtrack.jetbrains.com/issue/KT-73255
+                    // Apply @StringRes to both constructor params and generated properties
+                    "-Xannotation-default-target=param-property",
+                    "-Xexplicit-backing-fields"
+                )
 
                 if (isInIdeaSync) {
                     compilerArgs += "-XXLanguage:+ExplicitBackingFields"
@@ -135,6 +140,9 @@ subprojects {
 
                 if (project.path !in listOf(":anki-common", ":api", ":common", ":common:android")) {
                     compilerArgs += "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi"
+                }
+                if (project.path != ":api") {
+                    compilerArgs += "-Xcontext-parameters"
                 }
                 // Opt in to Material3 APIs marked experimental once at the module level
                 // (currently only :AnkiDroid uses Compose Material3). Avoids littering
