95b33eda2d8afe391c9aebe08ad5cc5473fed0c4
anugrah-pal <pal07anugrah@gmail.com>
2026-09-23 13:55:12 +0530

    test: add screenshot test for NoteTypeFieldEditor delete dialog

---
+++ AnkiDroid/src/test/java/com/ichi2/anki/NoteTypeFieldEditorScreenshotTest.kt
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package com.ichi2.anki
+
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import com.ichi2.anki.dialogs.NoteTypeFieldEditorContextMenu.NoteTypeFieldEditorContextMenuAction
+import org.junit.Test
+
+/**
+ * Screenshot tests for [NoteTypeFieldEditor]
+ *
+ * `./gradlew :AnkiDroid:recordRoborazziPlayDebug -Pscreenshot --tests "com.ichi2.anki.NoteTypeFieldEditorScreenshotTest"`
+ */
+class NoteTypeFieldEditorScreenshotTest : ScreenshotTest() {
+    @Test
+    fun base() =
+        withNoteTypeFieldEditor {
+            captureScreen("base")
+        }
+
+    @Test
+    fun deleteFieldDialog() =
+        withNoteTypeFieldEditor { activity ->
+            activity.handleAction(NoteTypeFieldEditorContextMenuAction.Delete)
+            advanceRobolectricLooper()
+            captureScreen("delete_field_dialog")
+        }
+
+    private fun withNoteTypeFieldEditor(block: (NoteTypeFieldEditor) -> Unit) {
+        val notetype = getCurrentDatabaseNoteTypeCopy("Basic")
+        val intent =
+            Intent(targetContext, NoteTypeFieldEditor::class.java).apply {
+                putExtra(NoteTypeFieldEditor.EXTRA_NOTETYPE_NAME, notetype.name)
+                putExtra(NoteTypeFieldEditor.EXTRA_NOTETYPE_ID, notetype.id)
+            }
+        ActivityScenario.launch<NoteTypeFieldEditor>(intent).use { scenario ->
+            scenario.onActivity { activity ->
+                advanceRobolectricLooper()
+                block(activity)
+            }
+        }
+    }
+}
